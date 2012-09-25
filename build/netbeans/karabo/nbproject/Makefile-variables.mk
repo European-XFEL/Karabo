d55354fa75b680b8f3f43a6e20624540d7677428
heisenb <burkard.heisen@xfel.eu>
2012-09-25 15:34:27 +0000

    Karabo Build System
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/users/heisen/karabo/karaboFramework/trunk@7311 a23ab9f9-1ea2-4934-960d-990259f9ea8c

--- build/netbeans/karabo/nbproject/Makefile-variables.mk
+++ build/netbeans/karabo/nbproject/Makefile-variables.mk
@@ -7,21 +7,21 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=GNU-MacOSX
-CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-MacOSX/lib
-CND_ARTIFACT_NAME_Debug=libkarabo.dylib
-CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-MacOSX/lib/libkarabo.dylib
-CND_PACKAGE_DIR_Debug=dist/Debug/GNU-MacOSX/package
-CND_PACKAGE_NAME_Debug=libkarabo.dylib.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/GNU-MacOSX/package/libkarabo.dylib.tar
+CND_PLATFORM_Debug=GNU-Linux-x86
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux-x86/lib
+CND_ARTIFACT_NAME_Debug=libkarabo.so
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux-x86/lib/libkarabo.so
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Debug=libkarabo.so.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux-x86/package/libkarabo.so.tar
 # Release configuration
-CND_PLATFORM_Release=GNU-MacOSX
-CND_ARTIFACT_DIR_Release=dist/Release/GNU-MacOSX/lib
-CND_ARTIFACT_NAME_Release=libkarabo.dylib
-CND_ARTIFACT_PATH_Release=dist/Release/GNU-MacOSX/lib/libkarabo.dylib
-CND_PACKAGE_DIR_Release=dist/Release/GNU-MacOSX/package
-CND_PACKAGE_NAME_Release=libkarabo.dylib.tar
-CND_PACKAGE_PATH_Release=dist/Release/GNU-MacOSX/package/libkarabo.dylib.tar
+CND_PLATFORM_Release=GNU-Linux-x86
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux-x86/lib
+CND_ARTIFACT_NAME_Release=libkarabo.so
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux-x86/lib/libkarabo.so
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Release=libkarabo.so.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux-x86/package/libkarabo.so.tar
 #
 # include compiler specific variables
 #
