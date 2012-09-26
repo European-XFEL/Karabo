333cb348bca9ba9900d2dc4244434aa27562f6d1
heisenb <burkard.heisen@xfel.eu>
2012-09-26 13:44:37 +0000

    Karabo Framework
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/users/heisen/karabo/karaboFramework/trunk@7325 a23ab9f9-1ea2-4934-960d-990259f9ea8c

--- extern/install.sh
+++ extern/install.sh
@@ -1,21 +1,35 @@
 #!/bin/bash
 
 CWD=$(pwd)
+DIR=$(dirname $0)
+cd $DIR
 
+# $1 RESOURCE_NAME   -> Name of the installed dependency
+# $2 INSTALL_PREFIX  -> Installation prefix
 RESOURCE_NAME=$1
-RESOURCE_PATH=$CWD/resources/$RESOURCE_NAME
-INSTALL_PREFIX=$CWD
+INSTALL_PREFIX=$2
 
-if [ -d $RESOURCE_PATH ]; then
-
-    cd $RESOURCE_PATH
-    ./install.sh $INSTALL_PREFIX
-
-else
-    echo
-    echo "Resource $RESOURCE_NAME does not exist."
-    echo
+if [ -z $INSTALL_PREFIX ]; then
+    echo "### WARNING  No install-prefix given (second argument), using default location: $DIR"
+    INSTALL_PREFIX=$DIR
 fi
 
+mkdir -p $INSTALL_PREFIX/include
+mkdir -p $INSTALL_PREFIX/lib
+mkdir -p $INSTALL_PREFIX/bin
 
+RESOURCE_PATH=$DIR/resources/$RESOURCE_NAME
+if [ -d $RESOURCE_PATH ]; then
+    $RESOURCE_PATH/install.sh $INSTALL_PREFIX
+    rv=$?
+else
+    echo
+    echo "### ERROR  Resource $RESOURCE_NAME does not exist."
+    echo
+    rv=1
+fi
+
+cd $CWD
+
+exit $rv
 
--- src/deviceServer/deviceServer.cc
+++ src/deviceServer/deviceServer.cc
@@ -19,10 +19,13 @@ int main(int argc, char** argv) {
       DeviceServer::Pointer deviceServer = Runner<DeviceServer>::instantiate(argc, argv);
       if (deviceServer) deviceServer->run();
       
+  } catch (const exfel::util::TimeoutException& e) {
+    std::cout << std::endl << "An error has occurred: Network response timed out." << std::endl;
+    std::cout << "Make sure that a master-device-server is running under the configured broker/topic." << std::endl;
   } catch (const exfel::util::Exception& e) {
-    std::cout << e;
+      std::cout << e;
   } catch (...) {
-    RETHROW
+      RETHROW
   }
   return EXIT_SUCCESS;
 }
