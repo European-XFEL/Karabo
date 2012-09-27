9a6496cff74763341e61a732f5548db796977342
heisenb <burkard.heisen@xfel.eu>
2012-09-27 20:25:16 +0000

    Karabo
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/users/heisen/karabo/karaboFramework/trunk@7364 a23ab9f9-1ea2-4934-960d-990259f9ea8c

--- build/netbeans/deviceServer/nbproject/Karabo-variables-Linux.mk
+++ build/netbeans/deviceServer/nbproject/Karabo-variables-Linux.mk
@@ -8,20 +8,20 @@ CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
 CND_PLATFORM_Debug=GNU-Linux-x86
-CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux-x86/lib
-CND_ARTIFACT_NAME_Debug=libkarabo.so
-CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux-x86/lib/libkarabo.so
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux-x86
+CND_ARTIFACT_NAME_Debug=karabo-deviceserver
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux-x86/karabo-deviceserver
 CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux-x86/package
-CND_PACKAGE_NAME_Debug=karabo.tar.gz
-CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux-x86/package/karabo.tar.gz
+CND_PACKAGE_NAME_Debug=deviceserver.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux-x86/package/deviceserver.tar
 # Release configuration
 CND_PLATFORM_Release=GNU-Linux-x86
-CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux-x86/lib
-CND_ARTIFACT_NAME_Release=libkarabo.so
-CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux-x86/lib/libkarabo.so
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux-x86
+CND_ARTIFACT_NAME_Release=karabo-deviceserver
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux-x86/karabo-deviceserver
 CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux-x86/package
-CND_PACKAGE_NAME_Release=libkarabo.so.tar
-CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux-x86/package/libkarabo.so.tar
+CND_PACKAGE_NAME_Release=deviceserver.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux-x86/package/deviceserver.tar
 #
 # include compiler specific variables
 #
