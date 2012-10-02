d0fb7bb568592870244b6597bf33970f63d3ce0f
heisenb <heisenb@a23ab9f9-1ea2-4934-960d-990259f9ea8c>
2012-10-02 07:18:44 +0000

    Improved packaging
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/users/heisen/karabo/karaboFramework/trunk@7405 a23ab9f9-1ea2-4934-960d-990259f9ea8c

--- build/netbeans/deviceServer/nbproject/Karabo-variables-Linux.mk
+++ build/netbeans/deviceServer/nbproject/Karabo-variables-Linux.mk
@@ -8,17 +8,17 @@ CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
 CND_PLATFORM_Debug=GNU-Linux-x86
-CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux-x86
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux-x86/bin
 CND_ARTIFACT_NAME_Debug=karabo-deviceserver
-CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux-x86/karabo-deviceserver
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux-x86/bin/karabo-deviceserver
 CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux-x86/package
 CND_PACKAGE_NAME_Debug=deviceserver.tar
 CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux-x86/package/deviceserver.tar
 # Release configuration
 CND_PLATFORM_Release=GNU-Linux-x86
-CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux-x86
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux-x86/bin
 CND_ARTIFACT_NAME_Release=karabo-deviceserver
-CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux-x86/karabo-deviceserver
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux-x86/bin/karabo-deviceserver
 CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux-x86/package
 CND_PACKAGE_NAME_Release=deviceserver.tar
 CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux-x86/package/deviceserver.tar
