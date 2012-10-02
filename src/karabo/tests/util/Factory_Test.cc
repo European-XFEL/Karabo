a1ea53383e5aefd6bf239be6f7db79f7c8cfd84b
irinak <irina.kozlova@xfel.eu>
2012-10-02 15:27:31 +0000

    restructuring tests
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/users/heisen/karabo/karaboFramework/trunk@7410 a23ab9f9-1ea2-4934-960d-990259f9ea8c

---
+++ src/karabo/tests/util/Factory_Test.cc
@@ -0,0 +1,32 @@
+/* 
+ * File:   Factory_Test.cc
+ * Author: heisenb
+ * 
+ * Created on September 19, 2012, 10:34 AM
+ */
+
+#include "Factory_Test.hh"
+
+CPPUNIT_TEST_SUITE_REGISTRATION(Factory_Test);
+
+Factory_Test::Factory_Test() {
+}
+
+Factory_Test::~Factory_Test() {
+}
+
+void Factory_Test::setUp() {
+    this->example = new int(1);
+}
+
+void Factory_Test::tearDown() {
+    delete this->example;
+}
+
+void Factory_Test::testMethod() {
+    CPPUNIT_ASSERT(*example == 1);
+}
+
+void Factory_Test::testFailedMethod() {
+    //CPPUNIT_ASSERT(++*example == 1);
+}
