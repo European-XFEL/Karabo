91f583eb826b397837605fb6e3dfd799599668bf
heisenb <burkard.heisen@xfel.eu>
2012-10-09 08:57:30 +0000

    Karabp
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/users/heisen/karabo/karaboFramework/trunk@7460 a23ab9f9-1ea2-4934-960d-990259f9ea8c

--- build/netbeans/pythonGui/make.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-CWD=$(pwd)
-cd ../../../src/pythonGui
-export PYTHONPATH=../../build/netbeans/karabo/dist/Debug/GNU-Linux-x86/lib
-../../extern/GNU-Linux-x86/bin/python setup.py build
-cd $CWD
