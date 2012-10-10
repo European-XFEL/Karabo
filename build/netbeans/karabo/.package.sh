de4c99f53b253e3e882dec76b810e121945e54b5
heisenb <burkard.heisen@xfel.eu>
2012-10-10 09:56:44 +0000

    Deleted wrongly commited makefile
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/trunk@7479 a23ab9f9-1ea2-4934-960d-990259f9ea8c

--- build/netbeans/karabo/.package.sh
+++ build/netbeans/karabo/.package.sh
@@ -6,24 +6,31 @@ PLATFORM=$3
 OS=$(uname -s)
 MACHINE=$(uname -m)
 PACKAGEDIR=$(pwd)/../../../package/karabo-$CONF-$OS-$MACHINE
+NUM_CORES=2
+# Find number of cores on machine
+if [ "$(uname -s)" = "Linux" ]; then
+    NUM_CORES=`grep "processor" /proc/cpuinfo | wc -l`
+    NUM_CORES=$(($NUM_CORES*2/3))
+fi
+echo "### INFO Building is preferentially parallelized into $NUM_CORES threads."
 
 if [ -d $PACKAGEDIR ]; then rm -rf $PACKAGEDIR; fi
 mkdir -p $PACKAGEDIR
 
 # karabo
-make -j4 CONF=$CONF CMD=y
+make -j$NUM_CORES CONF=$CONF CMD=y
 cp -rf $DISTDIR/$CONF/$PLATFORM/lib $PACKAGEDIR/
 cp -rf $DISTDIR/$CONF/$PLATFORM/include $PACKAGEDIR/
 cp -rf ../../../extern/$PLATFORM $PACKAGEDIR/extern
 
 # deviceServer
 cd ../deviceServer
-make -j4 CONF=$CONF CMD=y
+make -j$NUM_CORES CONF=$CONF CMD=y
 cp -rf $DISTDIR/$CONF/$PLATFORM/bin $PACKAGEDIR/
 
 # brokerMessageLogger
 cd ../brokerMessageLogger
-make -j4 CONF=$CONF CMD=y
+make -j$NUM_CORES CONF=$CONF CMD=y
 cp -rf $DISTDIR/$CONF/$PLATFORM/bin $PACKAGEDIR/
 
 # pythonGui
