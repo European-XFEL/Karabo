5a4351791cb9e5bcc0c97b96f1616a5088040785
heisenb <burkard.heisen@xfel.eu>
2012-10-14 15:57:51 +0000

    Added MacOSX Makefiles
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/trunk@7521 a23ab9f9-1ea2-4934-960d-990259f9ea8c

---
+++ build/netbeans/karabo/nbproject/Karabo-variables-Darwin.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-MacOSX
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-MacOSX/lib
+CND_ARTIFACT_NAME_Debug=libkarabo.dylib
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-MacOSX/lib/libkarabo.dylib
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-MacOSX/package
+CND_PACKAGE_NAME_Debug=karabo.tar.gz
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-MacOSX/package/karabo.tar.gz
+# Release configuration
+CND_PLATFORM_Release=GNU-MacOSX
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-MacOSX/lib
+CND_ARTIFACT_NAME_Release=libkarabo.dylib
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-MacOSX/lib/libkarabo.dylib
+CND_PACKAGE_DIR_Release=dist/Release/GNU-MacOSX/package
+CND_PACKAGE_NAME_Release=libkarabo.dylib.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-MacOSX/package/libkarabo.dylib.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
