4ceace7dda1a0a2e866c536d9a7cd179cf8ed41c
heisenb <burkard.heisen@xfel.eu>
2012-10-16 19:20:51 +0000

    Bug fix in CORE detection for build
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/trunk@7544 a23ab9f9-1ea2-4934-960d-990259f9ea8c

--- auto_build_all.sh
+++ auto_build_all.sh
@@ -5,15 +5,15 @@ cd $(dirname $0)
 
 # Get some information about our system
 OS=$(uname -s)
+NUM_CORES=2
 if [ "$OS" = "Linux" ]; then
     DISTRO_ID=$(lsb_release -is)
     DISTRO_RELEASE=$(lsb_release -rs)
-    tmp=`grep "processor" /proc/cpuinfo | wc -l`
-    NUM_CORES=$(($tmp*2/3))
+    NUM_CORES=`grep "processor" /proc/cpuinfo | wc -l`
+    if [ "$NUM_CORES" -gt "8" ]; then NUM_CORES=8; fi
 elif [ "$OS" = "Darwin" ]; then
     DISTRO_ID=MacOSX
     DISTRO_RELEASE=$(uname -r)
-    NUM_CORES=2
 fi
 
 echo -e "\n\n### Fetching dependencies from ${DISTRO_ID}'s package management system. ###\n\n"
@@ -23,7 +23,7 @@ if [ "$DISTRO_ID" == "Ubuntu" ]; then
     sudo apt-get install subversion krb5-user build-essential doxygen libqt4-dev libnss3-dev libnspr4-dev libreadline-dev libsqlite3-dev libX11-dev zlib1g-dev libXext-dev gfortran liblapack-dev
 fi
 
-echo -e "\n\n### Starting compilation and packaging of the karaboFramework.\n\n ###"
+echo -e "\n\n### Starting compilation (using $NUM_CORES threads) and packaging of the karaboFramework. ###\n\n"
 sleep 2
 
 cd build/netbeans/karabo
