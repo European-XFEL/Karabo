50b92f4e42e5e48bb88987f370be4a39d20c5b5e
irinak <irinak@a23ab9f9-1ea2-4934-960d-990259f9ea8c>
2012-10-16 09:38:50 +0000

    CppUnit tests added for testing Reader, Writer.
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/trunk@7528 a23ab9f9-1ea2-4934-960d-990259f9ea8c

--- src/karabo/tests/io/ReaderWriter_Test.cc
+++ src/karabo/tests/io/ReaderWriter_Test.cc
@@ -1,11 +1,14 @@
 /*
- * File:   ReaderWriter_Test.cc
- * Author: irinak
+ * $Id$
  *
- * Created on Oct 1, 2012, 5:32:32 PM
+ * Author: <irina.kozlova@xfel.eu>
+ *
+ * Created on Oct 4, 2012, 5:30:35 PM
  */
 
 #include "ReaderWriter_Test.hh"
+#include <iostream>
+#include <fstream>
 
 using namespace karabo::util;
 using namespace karabo::io;
@@ -19,11 +22,10 @@ ReaderWriter_Test::ReaderWriter_Test() {
 ReaderWriter_Test::~ReaderWriter_Test() {
 }
 
-void ReaderWriter_Test::testWritingReadingBinaryFormat() {
-    string plcstr;
+void ReaderWriter_Test::setUp() {
 
-    //WritingBinaryFormat >>>>>>
-    Hash hash;
+    //Define 'hash', using setFromPath function  
+    //used in the test 'testWritingReadingBinaryFormat'
     hash.setFromPath("Motor.name", "Beckhoff Motor");
     hash.setFromPath("Motor.Init.position", 10);
     hash.setFromPath("Motor.Init.velocity", 3.0);
@@ -37,6 +39,15 @@ void ReaderWriter_Test::testWritingReadingBinaryFormat() {
     hash.setFromPath("Motor.Init.sAxisName[2]", "Data transmission as an asynchronous task!");
     hash.setFromPath("Motor.Init.a.b.number", static_cast<double> (123.45));
 
+    //Define 'runDir' directory containing all needed resources
+    runDir = string(TESTPATH) + string("io/resources/");
+
+}
+
+void ReaderWriter_Test::testWritingReadingBinaryFormat() {
+    string plcstr;
+    plcstr.clear();
+    //WritingBinaryFormat >>>>>>
     Hash c;
     plcstr.clear();
     c.setFromPath("StringStream.format.Bin");
@@ -48,60 +59,167 @@ void ReaderWriter_Test::testWritingReadingBinaryFormat() {
     //ReadingBinaryFormat >>>>>>>
     Hash c2;
     Hash hash2;
-
     c2.setFromPath("StringStream.format.Bin");
     c2.setFromPath("StringStream.string", plcstr); // plcstr contains encoded PLC message
     Reader<Hash>::Pointer in = Reader<Hash>::create(c2);
-    in->read(hash2); // <--- decoding into hash
+    in->read(hash2); // <--- decoding 'plcstr' into hash 'hash2'
 
     //check that original 'hash' and decoded 'hash2' are identical
-    bool flag = true;
-    for (Hash::const_iterator it = hash.begin(); it != hash.end(); it++){
-        if( !hash2.identical(it) ) {
-            flag = false;
-            CPPUNIT_ASSERT(flag);
+    for (Hash::const_iterator it = hash.begin(); it != hash.end(); it++) {
+        if (!hash2.identical(it)) {
+            CPPUNIT_ASSERT(false);
         }
     }
-    for (Hash::const_iterator it = hash2.begin(); it != hash2.end(); it++){
-        if( !hash.identical(it) ) {
-            flag = false;
-            CPPUNIT_ASSERT(flag);
+    for (Hash::const_iterator it = hash2.begin(); it != hash2.end(); it++) {
+        if (!hash.identical(it)) {
+            CPPUNIT_ASSERT(false);
         }
     }
-    CPPUNIT_ASSERT(flag);
+    CPPUNIT_ASSERT(true);
 }
 
-void ReaderWriter_Test::testWriting() {
+void ReaderWriter_Test::testWritingReadingXmlFormat() {
+
+    //ReadingXmlFormat >>>>>>
+    //read from file  'xmlForReading.xml' (stored in 'resources' directory) XML-Format 
+    //and create configuration object 'configuration'
+    Hash configuration, conf, confw;
+
+    conf.setFromPath("TextFile.filename", runDir + "xmlForReading_readonly.xml");
+
+    Reader<Hash>::Pointer in = Reader<Hash>::create(conf);
+    in->read(configuration);
+
+    //if needed uncomment for check:
+    //cout << "Reading file " << conf.getFromPath<string > ("TextFile.filename") << ". Created object 'configuration': " << endl;
+    //cout << configuration << endl;
+
+    //WritingXmlFormat >>>>>>
+    //write configuration object 'configuration' into newly created file 'newTestXml.xml'
+
+    //Default: simple data types will not be written into XML.
+    //the following line reflects the default behavior:
+    //conf.setFromPath("TextFile.format.Xml.printDataType", false); //false or 0
+
+    //In order to write simple data types as an attribute in XML-element,
+    //for example: <myname dataType="STRING">Hallo</myname>
+    //set the value of parameter 'printDataType' to 'true' :
+    confw.setFromPath("TextFile.format.Xml.printDataType", true); //true or 1
+
+    string fnameNew = runDir + "newTestXml.xml";
+    boost::filesystem::path filePathNew(fnameNew.c_str());
+    confw.setFromPath("TextFile.filename", filePathNew);
+
+    Writer<Hash>::Pointer out = Writer<Hash>::create(confw);
+    out->write(configuration);
+
+    //Compare files:  'etalonXML_readonly.xml' with 
+    //the newly created file 'newTestXml.xml' (that was created from the 'configuration' object 
+    //and contains all 'dataTypes' written as an attribute for each xml-element; 
+    //therefore it is slightly different from the input file for reading 'xmlForReading_readonly.xml')   
+    ifstream newFile(fnameNew.c_str());
+
+    string fnameEtalon = runDir + "etalonXML_readonly.xml";
+    ifstream etalonFile(fnameEtalon.c_str());
+
+    if (etalonFile.is_open()) {
+        if (newFile.is_open()) {
+
+            while (etalonFile.good()) {
+                while (newFile.good()) {
+                    string etalonLine, newLine;
+
+                    getline(etalonFile, etalonLine);
+
+                    getline(newFile, newLine);
+
+                    if (!etalonLine.compare(newLine) == 0) {
+                        CPPUNIT_ASSERT(false);
+                    }
+                }
+            }
+            etalonFile.close();
+            newFile.close();
+        } else {
+            cout << "Error opening file " << fnameNew << endl;
+        }
+    } else {
+        cout << "Error opening etalon file " << fnameEtalon << endl;
+    }
+
+}
+
+
+void ReaderWriter_Test::testWritingSchema() {
+    
+    //This test takes as a test-schema the expected parameters of 'TextFileReader' class   
     Schema schemaForTest = Reader<Hash>::expectedParameters("TextFile");
+    
+    //Writing Schema into Stream (Xsd format).   
+    //Test-schema will be written into xsd-file 'expectedParamsTextFile.xsd'
+    
     Hash input;
-    string fileName = "expected.xsd";
+    string fileName = runDir + "expectedParamsTextFile.xsd";
     input.setFromPath("TextFile.filename", fileName);
     input.setFromPath("TextFile.format.Xsd");
-    //input.setFromPath("TextFile.format.Xsd.indentation", -1);
     input.setFromPath("TextFile.format.Xsd.indentation", 3);
-    cout << "Check  format: \n" << input << endl;
+    
+    //if needed uncomment for check:
+    //cout << "Check  format: \n" << input << endl;
 
     Writer<Schema>::Pointer out = Writer<Schema>::create(input);
     out->write(schemaForTest);
-    cout << "Schema is now written into file " << input.getFromPath<string > ("TextFile.filename") << endl;
+    
+    //if needed uncomment for check:
+    //cout << "Schema is now written into file " << input.getFromPath<string > ("TextFile.filename") << endl;
 
     string checkFileName = input.getFromPath<string > ("TextFile.filename");
     CPPUNIT_ASSERT(fileName.compare(checkFileName) == 0);
 
-    cout << "\nTEST 2B. Writing Schema into Stream (Xsd format)" << endl;
+    ifstream newFile(fileName.c_str());
 
+    string fnameEtalon = runDir + "etalonXSD_readonly.xsd";
+    ifstream etalonFile(fnameEtalon.c_str());
+
+    if (etalonFile.is_open()) {
+        if (newFile.is_open()) {
+
+            while (etalonFile.good()) {
+                while (newFile.good()) {
+                    string etalonLine, newLine;
+
+                    getline(etalonFile, etalonLine);
+
+                    getline(newFile, newLine);
+
+                    if (!etalonLine.compare(newLine) == 0) {
+                        CPPUNIT_ASSERT(false);
+                    }
+                }
+            }
+            etalonFile.close();
+            newFile.close();
+        } else {
+            cout << "Error opening file " << fileName << endl;
+        }
+    } else {
+        cout << "Error opening etalon file " << fnameEtalon << endl;
+    }
+    
+    //Writing Schema into Stream (Xsd format).
+    //Test-schema will be written into xsd-file 'expectedParamsTextFile.xsd'
+    
     string outputString;
 
     Hash inStream;
     inStream.setFromPath("StringStream.format.Xsd.indentation", -1);
     inStream.setFromPath("StringStream.stringPointer", &outputString);
-    cout << "Check  format: \n" << inStream << endl;
+    
+    //if needed uncomment for check:
+    //cout << "Check  format: \n" << inStream << endl;
 
     Writer<Schema>::Pointer writerSchema = Writer<Schema>::create(inStream);
 
     writerSchema->write(schemaForTest);
     CPPUNIT_ASSERT(outputString.length() == 3623);
-    cout << "Result: outputString.length()  = " << outputString.length() << endl;
 }
-
-
