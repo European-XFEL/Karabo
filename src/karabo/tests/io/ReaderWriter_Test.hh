a9237625051d57b9b0202170c523b46af7b7ab05
irinak <irina.kozlova@xfel.eu>
2012-10-16 09:38:50 +0000

    CppUnit tests added for testing Reader, Writer.
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/trunk@7528 a23ab9f9-1ea2-4934-960d-990259f9ea8c

--- src/karabo/tests/io/ReaderWriter_Test.hh
+++ src/karabo/tests/io/ReaderWriter_Test.hh
@@ -1,8 +1,9 @@
 /*
- * File:   ReaderWriter_Test.hh
- * Author: irinak
+ * $Id$
  *
- * Created on Oct 1, 2012, 5:32:32 PM
+ * Author: <irina.kozlova@xfel.eu>
+ *
+ * Created on Oct 4, 2012, 5:30:35 PM
  */
 
 #ifndef READERWRITER_TEST_HH
@@ -13,25 +14,31 @@
 #include <karabo/io/Writer.hh>
 #include <karabo/io/StringStreamWriter.hh>
 #include <karabo/io/StringStreamReader.hh>
+#include <karabo/io/SchemaXsdFormat.hh>
 #include <karabo/util/Hash.hh>
+#include <boost/filesystem/path.hpp>
 
 class ReaderWriter_Test : public CPPUNIT_NS::TestFixture {
     CPPUNIT_TEST_SUITE(ReaderWriter_Test);
 
     CPPUNIT_TEST(testWritingReadingBinaryFormat);
-    CPPUNIT_TEST(testWriting);
+    CPPUNIT_TEST(testWritingReadingXmlFormat);
+    CPPUNIT_TEST(testWritingSchema);
 
     CPPUNIT_TEST_SUITE_END();
 
 public:
     ReaderWriter_Test();
     virtual ~ReaderWriter_Test();
+    void setUp();
+    karabo::util::Hash hash;
+    std::string runDir;
 
 private:
     void testWritingReadingBinaryFormat();
-    void testWriting();
+    void testWritingReadingXmlFormat();
+    void testWritingSchema();
 
 };
 
 #endif	/* READERWRITER_TEST_HH */
-
