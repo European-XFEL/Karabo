b1d67da4189f842d45181151e5fdd35f2d802673
irinak <irina.kozlova@xfel.eu>
2012-10-29 16:33:10 +0000

    CppUnit test for ClassInfo
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/trunk@7699 a23ab9f9-1ea2-4934-960d-990259f9ea8c

---
+++ src/karabo/tests/util/ClassInfo_Test.cc
@@ -0,0 +1,58 @@
+/* 
+ * $Id$
+ * 
+ * Author: <irina.kozlova@xfel.eu>
+ * 
+ * Created on October 29, 2012, 12:38 PM
+ */
+
+#include "ClassInfo_Test.hh"
+
+using namespace std;
+using namespace karabo::util;
+
+CPPUNIT_TEST_SUITE_REGISTRATION(ClassInfo_Test);
+
+ClassInfo_Test::ClassInfo_Test() {
+}
+
+ClassInfo_Test::~ClassInfo_Test() {
+}
+
+void ClassInfo_Test::setUp() {
+
+    Schema ex;
+    Schema& tmp = ex.initParameterDescription("test");
+
+    BobbyCar::expectedParameters(tmp);
+
+    Hash c;
+    c.setFromPath("BobbyCar.name", "Auto");
+    c.setFromPath("BobbyCar.equipment", "Radio");
+
+    vp = Vehicle::create(c);
+    vp->start();
+
+}
+
+void ClassInfo_Test::testGetClassId() {
+
+    string cId = vp->getClassInfo().getClassId();
+    CPPUNIT_ASSERT(cId == "BobbyCar");
+
+}
+
+void ClassInfo_Test::testGetClassName() {
+
+    string cName = vp->getClassInfo().getClassName();
+    CPPUNIT_ASSERT(cName == "BobbyCar");
+
+}
+
+void ClassInfo_Test::testGetNamespace() {
+
+    string cNamespace = vp->getClassInfo().getNamespace();
+    CPPUNIT_ASSERT(cNamespace == "karabo::util");
+
+}
+
