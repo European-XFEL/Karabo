fb35fa34dc196af0685f487f83b003a621eb7277
irinak <irina.kozlova@xfel.eu>
2012-11-23 16:09:40 +0000

    changed binding, python tests according to changes in ImageElement (INT32_IMAGE_ELEMENT, DOUBLE_IMAGE_ELEMENT --> IMAGE_ELEMENT)
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/trunk@8006 a23ab9f9-1ea2-4934-960d-990259f9ea8c

--- src/karabo/python/PyUtilImageElement.cc
+++ src/karabo/python/PyUtilImageElement.cc
@@ -16,9 +16,7 @@ using namespace karabo::util;
 using namespace std;
 
 void exportImageElement() {
-    //karabo::util::ImageElement<int> , In Python: INT32_IMAGE_ELEMENT
-    KARABO_PYTHON_IMAGE_ELEMENT(int, INT32)
+    //karabo::util::ImageElement , In Python: IMAGE_ELEMENT
+    KARABO_PYTHON_IMAGE_ELEMENT
 
-    //karabo::util::ImageElement<double> , In Python: DOUBLE_IMAGE_ELEMENT       
-    KARABO_PYTHON_IMAGE_ELEMENT(double, DOUBLE)
 }
