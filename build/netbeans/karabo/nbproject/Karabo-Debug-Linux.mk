a41836d061fa0263681411f1fcb63200e1e53f79
heisenb <heisenb@a23ab9f9-1ea2-4934-960d-990259f9ea8c>
2012-12-13 21:52:36 +0000

    Updated build file
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/trunk@8227 a23ab9f9-1ea2-4934-960d-990259f9ea8c

--- build/netbeans/karabo/nbproject/Karabo-Debug-Linux.mk
+++ build/netbeans/karabo/nbproject/Karabo-Debug-Linux.mk
@@ -149,16 +149,16 @@ OBJECTFILES= \
 	${OBJECTDIR}/_ext/163016059/Test.o \
 	${OBJECTDIR}/_ext/1060241295/Column.o \
 	${OBJECTDIR}/_ext/1103122740/tinystr.o \
-	${OBJECTDIR}/_ext/1060241295/File.o \
-	${OBJECTDIR}/_ext/1060241295/Scalar.o \
-	${OBJECTDIR}/_ext/1072794519/ArrayDimensions.o \
-	${OBJECTDIR}/_ext/1072794519/HashXmlFormat.o \
-	${OBJECTDIR}/_ext/2117156511/PyUtilSchemaSimple.o \
 	${OBJECTDIR}/_ext/1060241295/FLArrayFilterArrayViewBuffer.o \
 	${OBJECTDIR}/_ext/1103112890/SnmpConnection.o \
 	${OBJECTDIR}/_ext/2117156511/PyXmsSlotElement.o \
+	${OBJECTDIR}/_ext/1060241295/File.o \
+	${OBJECTDIR}/_ext/1060241295/Scalar.o \
+	${OBJECTDIR}/_ext/1072794519/ArrayDimensions.o \
 	${OBJECTDIR}/_ext/2117156511/PyUtilSchema.o \
-	${OBJECTDIR}/_ext/1060241295/FixedLengthArray.o
+	${OBJECTDIR}/_ext/1072794519/HashXmlFormat.o \
+	${OBJECTDIR}/_ext/1060241295/FixedLengthArray.o \
+	${OBJECTDIR}/_ext/2117156511/PyUtilSchemaSimple.o
 
 # Test Directory
 TESTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}/tests
@@ -765,31 +765,6 @@ ${OBJECTDIR}/_ext/1103122740/tinystr.o: ../../../src/karabo/xml/tinystr.cpp
 	${RM} $@.d
 	$(COMPILE.cc) -g -I../../../src -I${KARABO}/extern/include -I${KARABO}/extern/include/python2.7 -fPIC  -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/1103122740/tinystr.o ../../../src/karabo/xml/tinystr.cpp
 
-${OBJECTDIR}/_ext/1060241295/File.o: ../../../src/karabo/io/hdf5/File.cc 
-	${MKDIR} -p ${OBJECTDIR}/_ext/1060241295
-	${RM} $@.d
-	$(COMPILE.cc) -g -I${KARABO}/extern/include/hdf5 -I../../../src -I${KARABO}/extern/include -I${KARABO}/extern/include/python2.7 -fPIC  -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/1060241295/File.o ../../../src/karabo/io/hdf5/File.cc
-
-${OBJECTDIR}/_ext/1060241295/Scalar.o: ../../../src/karabo/io/hdf5/Scalar.cc 
-	${MKDIR} -p ${OBJECTDIR}/_ext/1060241295
-	${RM} $@.d
-	$(COMPILE.cc) -g -I${KARABO}/extern/include/hdf5 -I../../../src -I${KARABO}/extern/include -I${KARABO}/extern/include/python2.7 -fPIC  -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/1060241295/Scalar.o ../../../src/karabo/io/hdf5/Scalar.cc
-
-${OBJECTDIR}/_ext/1072794519/ArrayDimensions.o: ../../../src/karabo/io/ArrayDimensions.cc 
-	${MKDIR} -p ${OBJECTDIR}/_ext/1072794519
-	${RM} $@.d
-	$(COMPILE.cc) -g -I../../../src -I${KARABO}/extern/include -I${KARABO}/extern/include/python2.7 -fPIC  -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/1072794519/ArrayDimensions.o ../../../src/karabo/io/ArrayDimensions.cc
-
-${OBJECTDIR}/_ext/1072794519/HashXmlFormat.o: ../../../src/karabo/io/HashXmlFormat.cc 
-	${MKDIR} -p ${OBJECTDIR}/_ext/1072794519
-	${RM} $@.d
-	$(COMPILE.cc) -g -I../../../src -I${KARABO}/extern/include -I${KARABO}/extern/include/python2.7 -fPIC  -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/1072794519/HashXmlFormat.o ../../../src/karabo/io/HashXmlFormat.cc
-
-${OBJECTDIR}/_ext/2117156511/PyUtilSchemaSimple.o: ../../../src/karabo/python/PyUtilSchemaSimple.cc 
-	${MKDIR} -p ${OBJECTDIR}/_ext/2117156511
-	${RM} $@.d
-	$(COMPILE.cc) -g -I../../../src -I${KARABO}/extern/include -I${KARABO}/extern/include/python2.7 -fPIC  -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/2117156511/PyUtilSchemaSimple.o ../../../src/karabo/python/PyUtilSchemaSimple.cc
-
 ${OBJECTDIR}/_ext/1060241295/FLArrayFilterArrayViewBuffer.o: ../../../src/karabo/io/hdf5/FLArrayFilterArrayViewBuffer.cc 
 	${MKDIR} -p ${OBJECTDIR}/_ext/1060241295
 	${RM} $@.d
@@ -805,15 +780,40 @@ ${OBJECTDIR}/_ext/2117156511/PyXmsSlotElement.o: ../../../src/karabo/python/PyXm
 	${RM} $@.d
 	$(COMPILE.cc) -g -I../../../src -I${KARABO}/extern/include -I${KARABO}/extern/include/python2.7 -fPIC  -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/2117156511/PyXmsSlotElement.o ../../../src/karabo/python/PyXmsSlotElement.cc
 
+${OBJECTDIR}/_ext/1060241295/File.o: ../../../src/karabo/io/hdf5/File.cc 
+	${MKDIR} -p ${OBJECTDIR}/_ext/1060241295
+	${RM} $@.d
+	$(COMPILE.cc) -g -I${KARABO}/extern/include/hdf5 -I../../../src -I${KARABO}/extern/include -I${KARABO}/extern/include/python2.7 -fPIC  -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/1060241295/File.o ../../../src/karabo/io/hdf5/File.cc
+
+${OBJECTDIR}/_ext/1060241295/Scalar.o: ../../../src/karabo/io/hdf5/Scalar.cc 
+	${MKDIR} -p ${OBJECTDIR}/_ext/1060241295
+	${RM} $@.d
+	$(COMPILE.cc) -g -I${KARABO}/extern/include/hdf5 -I../../../src -I${KARABO}/extern/include -I${KARABO}/extern/include/python2.7 -fPIC  -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/1060241295/Scalar.o ../../../src/karabo/io/hdf5/Scalar.cc
+
+${OBJECTDIR}/_ext/1072794519/ArrayDimensions.o: ../../../src/karabo/io/ArrayDimensions.cc 
+	${MKDIR} -p ${OBJECTDIR}/_ext/1072794519
+	${RM} $@.d
+	$(COMPILE.cc) -g -I../../../src -I${KARABO}/extern/include -I${KARABO}/extern/include/python2.7 -fPIC  -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/1072794519/ArrayDimensions.o ../../../src/karabo/io/ArrayDimensions.cc
+
 ${OBJECTDIR}/_ext/2117156511/PyUtilSchema.o: ../../../src/karabo/python/PyUtilSchema.cc 
 	${MKDIR} -p ${OBJECTDIR}/_ext/2117156511
 	${RM} $@.d
 	$(COMPILE.cc) -g -I../../../src -I${KARABO}/extern/include -I${KARABO}/extern/include/python2.7 -fPIC  -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/2117156511/PyUtilSchema.o ../../../src/karabo/python/PyUtilSchema.cc
 
+${OBJECTDIR}/_ext/1072794519/HashXmlFormat.o: ../../../src/karabo/io/HashXmlFormat.cc 
+	${MKDIR} -p ${OBJECTDIR}/_ext/1072794519
+	${RM} $@.d
+	$(COMPILE.cc) -g -I../../../src -I${KARABO}/extern/include -I${KARABO}/extern/include/python2.7 -fPIC  -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/1072794519/HashXmlFormat.o ../../../src/karabo/io/HashXmlFormat.cc
+
 ${OBJECTDIR}/_ext/1060241295/FixedLengthArray.o: ../../../src/karabo/io/hdf5/FixedLengthArray.cc 
 	${MKDIR} -p ${OBJECTDIR}/_ext/1060241295
 	${RM} $@.d
 	$(COMPILE.cc) -g -I${KARABO}/extern/include/hdf5 -I../../../src -I${KARABO}/extern/include -I${KARABO}/extern/include/python2.7 -fPIC  -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/1060241295/FixedLengthArray.o ../../../src/karabo/io/hdf5/FixedLengthArray.cc
+
+${OBJECTDIR}/_ext/2117156511/PyUtilSchemaSimple.o: ../../../src/karabo/python/PyUtilSchemaSimple.cc 
+	${MKDIR} -p ${OBJECTDIR}/_ext/2117156511
+	${RM} $@.d
+	$(COMPILE.cc) -g -I../../../src -I${KARABO}/extern/include -I${KARABO}/extern/include/python2.7 -fPIC  -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/2117156511/PyUtilSchemaSimple.o ../../../src/karabo/python/PyUtilSchemaSimple.cc
 
 # Subprojects
 .build-subprojects:
@@ -2431,71 +2431,6 @@ ${OBJECTDIR}/_ext/1103122740/tinystr_nomain.o: ${OBJECTDIR}/_ext/1103122740/tiny
 	    ${CP} ${OBJECTDIR}/_ext/1103122740/tinystr.o ${OBJECTDIR}/_ext/1103122740/tinystr_nomain.o;\
 	fi
 
-${OBJECTDIR}/_ext/1060241295/File_nomain.o: ${OBJECTDIR}/_ext/1060241295/File.o ../../../src/karabo/io/hdf5/File.cc 
-	${MKDIR} -p ${OBJECTDIR}/_ext/1060241295
-	@NMOUTPUT=`${NM} ${OBJECTDIR}/_ext/1060241295/File.o`; \
-	if (echo "$$NMOUTPUT" | ${GREP} '|main$$') || \
-	   (echo "$$NMOUTPUT" | ${GREP} 'T main$$') || \
-	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
-	then  \
-	    ${RM} $@.d;\
-	    $(COMPILE.cc) -g -I${KARABO}/extern/include/hdf5 -I../../../src -I${KARABO}/extern/include -I${KARABO}/extern/include/python2.7 -fPIC  -Dmain=__nomain -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/1060241295/File_nomain.o ../../../src/karabo/io/hdf5/File.cc;\
-	else  \
-	    ${CP} ${OBJECTDIR}/_ext/1060241295/File.o ${OBJECTDIR}/_ext/1060241295/File_nomain.o;\
-	fi
-
-${OBJECTDIR}/_ext/1060241295/Scalar_nomain.o: ${OBJECTDIR}/_ext/1060241295/Scalar.o ../../../src/karabo/io/hdf5/Scalar.cc 
-	${MKDIR} -p ${OBJECTDIR}/_ext/1060241295
-	@NMOUTPUT=`${NM} ${OBJECTDIR}/_ext/1060241295/Scalar.o`; \
-	if (echo "$$NMOUTPUT" | ${GREP} '|main$$') || \
-	   (echo "$$NMOUTPUT" | ${GREP} 'T main$$') || \
-	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
-	then  \
-	    ${RM} $@.d;\
-	    $(COMPILE.cc) -g -I${KARABO}/extern/include/hdf5 -I../../../src -I${KARABO}/extern/include -I${KARABO}/extern/include/python2.7 -fPIC  -Dmain=__nomain -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/1060241295/Scalar_nomain.o ../../../src/karabo/io/hdf5/Scalar.cc;\
-	else  \
-	    ${CP} ${OBJECTDIR}/_ext/1060241295/Scalar.o ${OBJECTDIR}/_ext/1060241295/Scalar_nomain.o;\
-	fi
-
-${OBJECTDIR}/_ext/1072794519/ArrayDimensions_nomain.o: ${OBJECTDIR}/_ext/1072794519/ArrayDimensions.o ../../../src/karabo/io/ArrayDimensions.cc 
-	${MKDIR} -p ${OBJECTDIR}/_ext/1072794519
-	@NMOUTPUT=`${NM} ${OBJECTDIR}/_ext/1072794519/ArrayDimensions.o`; \
-	if (echo "$$NMOUTPUT" | ${GREP} '|main$$') || \
-	   (echo "$$NMOUTPUT" | ${GREP} 'T main$$') || \
-	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
-	then  \
-	    ${RM} $@.d;\
-	    $(COMPILE.cc) -g -I../../../src -I${KARABO}/extern/include -I${KARABO}/extern/include/python2.7 -fPIC  -Dmain=__nomain -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/1072794519/ArrayDimensions_nomain.o ../../../src/karabo/io/ArrayDimensions.cc;\
-	else  \
-	    ${CP} ${OBJECTDIR}/_ext/1072794519/ArrayDimensions.o ${OBJECTDIR}/_ext/1072794519/ArrayDimensions_nomain.o;\
-	fi
-
-${OBJECTDIR}/_ext/1072794519/HashXmlFormat_nomain.o: ${OBJECTDIR}/_ext/1072794519/HashXmlFormat.o ../../../src/karabo/io/HashXmlFormat.cc 
-	${MKDIR} -p ${OBJECTDIR}/_ext/1072794519
-	@NMOUTPUT=`${NM} ${OBJECTDIR}/_ext/1072794519/HashXmlFormat.o`; \
-	if (echo "$$NMOUTPUT" | ${GREP} '|main$$') || \
-	   (echo "$$NMOUTPUT" | ${GREP} 'T main$$') || \
-	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
-	then  \
-	    ${RM} $@.d;\
-	    $(COMPILE.cc) -g -I../../../src -I${KARABO}/extern/include -I${KARABO}/extern/include/python2.7 -fPIC  -Dmain=__nomain -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/1072794519/HashXmlFormat_nomain.o ../../../src/karabo/io/HashXmlFormat.cc;\
-	else  \
-	    ${CP} ${OBJECTDIR}/_ext/1072794519/HashXmlFormat.o ${OBJECTDIR}/_ext/1072794519/HashXmlFormat_nomain.o;\
-	fi
-
-${OBJECTDIR}/_ext/2117156511/PyUtilSchemaSimple_nomain.o: ${OBJECTDIR}/_ext/2117156511/PyUtilSchemaSimple.o ../../../src/karabo/python/PyUtilSchemaSimple.cc 
-	${MKDIR} -p ${OBJECTDIR}/_ext/2117156511
-	@NMOUTPUT=`${NM} ${OBJECTDIR}/_ext/2117156511/PyUtilSchemaSimple.o`; \
-	if (echo "$$NMOUTPUT" | ${GREP} '|main$$') || \
-	   (echo "$$NMOUTPUT" | ${GREP} 'T main$$') || \
-	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
-	then  \
-	    ${RM} $@.d;\
-	    $(COMPILE.cc) -g -I../../../src -I${KARABO}/extern/include -I${KARABO}/extern/include/python2.7 -fPIC  -Dmain=__nomain -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/2117156511/PyUtilSchemaSimple_nomain.o ../../../src/karabo/python/PyUtilSchemaSimple.cc;\
-	else  \
-	    ${CP} ${OBJECTDIR}/_ext/2117156511/PyUtilSchemaSimple.o ${OBJECTDIR}/_ext/2117156511/PyUtilSchemaSimple_nomain.o;\
-	fi
-
 ${OBJECTDIR}/_ext/1060241295/FLArrayFilterArrayViewBuffer_nomain.o: ${OBJECTDIR}/_ext/1060241295/FLArrayFilterArrayViewBuffer.o ../../../src/karabo/io/hdf5/FLArrayFilterArrayViewBuffer.cc 
 	${MKDIR} -p ${OBJECTDIR}/_ext/1060241295
 	@NMOUTPUT=`${NM} ${OBJECTDIR}/_ext/1060241295/FLArrayFilterArrayViewBuffer.o`; \
@@ -2535,6 +2470,45 @@ ${OBJECTDIR}/_ext/2117156511/PyXmsSlotElement_nomain.o: ${OBJECTDIR}/_ext/211715
 	    ${CP} ${OBJECTDIR}/_ext/2117156511/PyXmsSlotElement.o ${OBJECTDIR}/_ext/2117156511/PyXmsSlotElement_nomain.o;\
 	fi
 
+${OBJECTDIR}/_ext/1060241295/File_nomain.o: ${OBJECTDIR}/_ext/1060241295/File.o ../../../src/karabo/io/hdf5/File.cc 
+	${MKDIR} -p ${OBJECTDIR}/_ext/1060241295
+	@NMOUTPUT=`${NM} ${OBJECTDIR}/_ext/1060241295/File.o`; \
+	if (echo "$$NMOUTPUT" | ${GREP} '|main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
+	then  \
+	    ${RM} $@.d;\
+	    $(COMPILE.cc) -g -I${KARABO}/extern/include/hdf5 -I../../../src -I${KARABO}/extern/include -I${KARABO}/extern/include/python2.7 -fPIC  -Dmain=__nomain -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/1060241295/File_nomain.o ../../../src/karabo/io/hdf5/File.cc;\
+	else  \
+	    ${CP} ${OBJECTDIR}/_ext/1060241295/File.o ${OBJECTDIR}/_ext/1060241295/File_nomain.o;\
+	fi
+
+${OBJECTDIR}/_ext/1060241295/Scalar_nomain.o: ${OBJECTDIR}/_ext/1060241295/Scalar.o ../../../src/karabo/io/hdf5/Scalar.cc 
+	${MKDIR} -p ${OBJECTDIR}/_ext/1060241295
+	@NMOUTPUT=`${NM} ${OBJECTDIR}/_ext/1060241295/Scalar.o`; \
+	if (echo "$$NMOUTPUT" | ${GREP} '|main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
+	then  \
+	    ${RM} $@.d;\
+	    $(COMPILE.cc) -g -I${KARABO}/extern/include/hdf5 -I../../../src -I${KARABO}/extern/include -I${KARABO}/extern/include/python2.7 -fPIC  -Dmain=__nomain -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/1060241295/Scalar_nomain.o ../../../src/karabo/io/hdf5/Scalar.cc;\
+	else  \
+	    ${CP} ${OBJECTDIR}/_ext/1060241295/Scalar.o ${OBJECTDIR}/_ext/1060241295/Scalar_nomain.o;\
+	fi
+
+${OBJECTDIR}/_ext/1072794519/ArrayDimensions_nomain.o: ${OBJECTDIR}/_ext/1072794519/ArrayDimensions.o ../../../src/karabo/io/ArrayDimensions.cc 
+	${MKDIR} -p ${OBJECTDIR}/_ext/1072794519
+	@NMOUTPUT=`${NM} ${OBJECTDIR}/_ext/1072794519/ArrayDimensions.o`; \
+	if (echo "$$NMOUTPUT" | ${GREP} '|main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
+	then  \
+	    ${RM} $@.d;\
+	    $(COMPILE.cc) -g -I../../../src -I${KARABO}/extern/include -I${KARABO}/extern/include/python2.7 -fPIC  -Dmain=__nomain -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/1072794519/ArrayDimensions_nomain.o ../../../src/karabo/io/ArrayDimensions.cc;\
+	else  \
+	    ${CP} ${OBJECTDIR}/_ext/1072794519/ArrayDimensions.o ${OBJECTDIR}/_ext/1072794519/ArrayDimensions_nomain.o;\
+	fi
+
 ${OBJECTDIR}/_ext/2117156511/PyUtilSchema_nomain.o: ${OBJECTDIR}/_ext/2117156511/PyUtilSchema.o ../../../src/karabo/python/PyUtilSchema.cc 
 	${MKDIR} -p ${OBJECTDIR}/_ext/2117156511
 	@NMOUTPUT=`${NM} ${OBJECTDIR}/_ext/2117156511/PyUtilSchema.o`; \
@@ -2548,6 +2522,19 @@ ${OBJECTDIR}/_ext/2117156511/PyUtilSchema_nomain.o: ${OBJECTDIR}/_ext/2117156511
 	    ${CP} ${OBJECTDIR}/_ext/2117156511/PyUtilSchema.o ${OBJECTDIR}/_ext/2117156511/PyUtilSchema_nomain.o;\
 	fi
 
+${OBJECTDIR}/_ext/1072794519/HashXmlFormat_nomain.o: ${OBJECTDIR}/_ext/1072794519/HashXmlFormat.o ../../../src/karabo/io/HashXmlFormat.cc 
+	${MKDIR} -p ${OBJECTDIR}/_ext/1072794519
+	@NMOUTPUT=`${NM} ${OBJECTDIR}/_ext/1072794519/HashXmlFormat.o`; \
+	if (echo "$$NMOUTPUT" | ${GREP} '|main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
+	then  \
+	    ${RM} $@.d;\
+	    $(COMPILE.cc) -g -I../../../src -I${KARABO}/extern/include -I${KARABO}/extern/include/python2.7 -fPIC  -Dmain=__nomain -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/1072794519/HashXmlFormat_nomain.o ../../../src/karabo/io/HashXmlFormat.cc;\
+	else  \
+	    ${CP} ${OBJECTDIR}/_ext/1072794519/HashXmlFormat.o ${OBJECTDIR}/_ext/1072794519/HashXmlFormat_nomain.o;\
+	fi
+
 ${OBJECTDIR}/_ext/1060241295/FixedLengthArray_nomain.o: ${OBJECTDIR}/_ext/1060241295/FixedLengthArray.o ../../../src/karabo/io/hdf5/FixedLengthArray.cc 
 	${MKDIR} -p ${OBJECTDIR}/_ext/1060241295
 	@NMOUTPUT=`${NM} ${OBJECTDIR}/_ext/1060241295/FixedLengthArray.o`; \
@@ -2559,6 +2546,19 @@ ${OBJECTDIR}/_ext/1060241295/FixedLengthArray_nomain.o: ${OBJECTDIR}/_ext/106024
 	    $(COMPILE.cc) -g -I${KARABO}/extern/include/hdf5 -I../../../src -I${KARABO}/extern/include -I${KARABO}/extern/include/python2.7 -fPIC  -Dmain=__nomain -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/1060241295/FixedLengthArray_nomain.o ../../../src/karabo/io/hdf5/FixedLengthArray.cc;\
 	else  \
 	    ${CP} ${OBJECTDIR}/_ext/1060241295/FixedLengthArray.o ${OBJECTDIR}/_ext/1060241295/FixedLengthArray_nomain.o;\
+	fi
+
+${OBJECTDIR}/_ext/2117156511/PyUtilSchemaSimple_nomain.o: ${OBJECTDIR}/_ext/2117156511/PyUtilSchemaSimple.o ../../../src/karabo/python/PyUtilSchemaSimple.cc 
+	${MKDIR} -p ${OBJECTDIR}/_ext/2117156511
+	@NMOUTPUT=`${NM} ${OBJECTDIR}/_ext/2117156511/PyUtilSchemaSimple.o`; \
+	if (echo "$$NMOUTPUT" | ${GREP} '|main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
+	then  \
+	    ${RM} $@.d;\
+	    $(COMPILE.cc) -g -I../../../src -I${KARABO}/extern/include -I${KARABO}/extern/include/python2.7 -fPIC  -Dmain=__nomain -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/2117156511/PyUtilSchemaSimple_nomain.o ../../../src/karabo/python/PyUtilSchemaSimple.cc;\
+	else  \
+	    ${CP} ${OBJECTDIR}/_ext/2117156511/PyUtilSchemaSimple.o ${OBJECTDIR}/_ext/2117156511/PyUtilSchemaSimple_nomain.o;\
 	fi
 
 # Run Test Targets
