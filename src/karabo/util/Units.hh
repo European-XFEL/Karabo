a54073aa8c90933baed48db73c063c3fd1d98d6b
heisenb <burkard.heisen@xfel.eu>
2013-03-01 08:49:31 +0000

    Added some unit translations
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/branches/hash-v2@8805 a23ab9f9-1ea2-4934-960d-990259f9ea8c

--- src/karabo/util/Units.hh
+++ src/karabo/util/Units.hh
@@ -77,8 +77,6 @@ namespace karabo {
                 MINUTE,
                 HOUR,
                 DAY,
-                WEEK,
-                MONTH,
                 YEAR,
                 BAR,
                 PIXEL,
@@ -107,7 +105,29 @@ namespace karabo {
         #define _KARABO_HELPER_MACRO(metricEnum, symbol) \
         template <> inline std::pair<std::string, std::string> Units::getMetricPrefix<karabo::util::Units::metricEnum>(){ std::string name(#metricEnum); boost::to_lower(name); return std::make_pair(name, symbol); }
 
+        _KARABO_HELPER_MACRO(YOTTA, "Y")
+        _KARABO_HELPER_MACRO(ZETTA, "Z")
+        _KARABO_HELPER_MACRO(EXA, "E")
+        _KARABO_HELPER_MACRO(PETA, "P")
+        _KARABO_HELPER_MACRO(TERA, "T")
+        _KARABO_HELPER_MACRO(GIGA, "G")
+        _KARABO_HELPER_MACRO(MEGA, "M")
+        _KARABO_HELPER_MACRO(KILO, "k")
+        _KARABO_HELPER_MACRO(HECTO, "h")
+        _KARABO_HELPER_MACRO(DECA, "da")
+        template <> inline std::pair<std::string, std::string> Units::getMetricPrefix<Units::NONE>() {
+            return std::make_pair("", "");
+        }
+        _KARABO_HELPER_MACRO(DECI, "d")
+        _KARABO_HELPER_MACRO(CENT, "c")
         _KARABO_HELPER_MACRO(MILLI, "m")
+        _KARABO_HELPER_MACRO(MICRO, "u")
+        _KARABO_HELPER_MACRO(NANO, "n")
+        _KARABO_HELPER_MACRO(PICO, "p")
+        _KARABO_HELPER_MACRO(FEMTO, "f")
+        _KARABO_HELPER_MACRO(ATTO, "a")
+        _KARABO_HELPER_MACRO(ZEPTO, "z")
+        _KARABO_HELPER_MACRO(YOCTO, "y")
 
         #undef _KARABO_HELPER_MACRO
 
@@ -116,6 +136,42 @@ namespace karabo {
          template <> inline std::pair<std::string, std::string> Units::getUnit<karabo::util::Units::unitEnum>() { std::string name(#unitEnum); boost::to_lower(name); return std::make_pair(name, symbol); }
 
         _KARABO_HELPER_MACRO(METER, "m")
+        _KARABO_HELPER_MACRO(GRAM, "g")
+        _KARABO_HELPER_MACRO(SECOND, "s")
+        _KARABO_HELPER_MACRO(AMPERE, "A")
+        _KARABO_HELPER_MACRO(KELVIN, "K")
+        _KARABO_HELPER_MACRO(MOLE, "mol")
+        _KARABO_HELPER_MACRO(CANDELA, "cd")
+        _KARABO_HELPER_MACRO(HERTZ, "Hz")
+        _KARABO_HELPER_MACRO(RADIAN, "rad")
+        _KARABO_HELPER_MACRO(STERADIAN, "sr")
+        _KARABO_HELPER_MACRO(NEWTON, "N")
+        _KARABO_HELPER_MACRO(PASCAL, "Pa")
+        _KARABO_HELPER_MACRO(JOULE, "J")
+        _KARABO_HELPER_MACRO(WATT, "W")
+        _KARABO_HELPER_MACRO(COULOMB, "C")
+        _KARABO_HELPER_MACRO(VOLT, "V")
+        _KARABO_HELPER_MACRO(FARAD, "F")
+        _KARABO_HELPER_MACRO(OHM, "Ω")
+        _KARABO_HELPER_MACRO(SIEMENS, "S")
+        _KARABO_HELPER_MACRO(WEBER, "Wb")
+        _KARABO_HELPER_MACRO(TESLA, "T")
+        _KARABO_HELPER_MACRO(HENRY, "H")
+        _KARABO_HELPER_MACRO(DEGREE_CELSIUS, "°C")
+        _KARABO_HELPER_MACRO(LUMEN, "lm")
+        _KARABO_HELPER_MACRO(LUX, "lx")
+        _KARABO_HELPER_MACRO(BECQUEREL, "Bq")
+        _KARABO_HELPER_MACRO(GRAY, "Gy")
+        _KARABO_HELPER_MACRO(SIEVERT, "Sv")
+        _KARABO_HELPER_MACRO(KATAL, "kat")
+        _KARABO_HELPER_MACRO(MINUTE, "min")
+        _KARABO_HELPER_MACRO(HOUR, "h")
+        _KARABO_HELPER_MACRO(DAY, "d")
+        _KARABO_HELPER_MACRO(YEAR, "a")
+        _KARABO_HELPER_MACRO(BAR, "bar")
+        _KARABO_HELPER_MACRO(PIXEL, "px")
+        _KARABO_HELPER_MACRO(BYTE, "B")
+        _KARABO_HELPER_MACRO(BIT, "B")
 
         #undef _KARABO_HELPER_MACRO
     }
