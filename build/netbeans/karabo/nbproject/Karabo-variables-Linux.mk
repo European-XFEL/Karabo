ead404c655ca3647da614415aa9ccf3989dd602a
heisenb <heisenb@a23ab9f9-1ea2-4934-960d-990259f9ea8c>
2013-03-08 07:44:47 +0000

    Cleaned configuration.xml and prepared for release mode. Some warnings exists (needs discussion with DB and KWr), fixed a but in FileInputOutput_Test
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/branches/hash-v2@8904 a23ab9f9-1ea2-4934-960d-990259f9ea8c

--- build/netbeans/karabo/nbproject/Karabo-variables-Linux.mk
+++ build/netbeans/karabo/nbproject/Karabo-variables-Linux.mk
@@ -16,9 +16,9 @@ CND_PACKAGE_NAME_Debug=karabo.tar.gz
 CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux-x86/package/karabo.tar.gz
 # Release configuration
 CND_PLATFORM_Release=GNU-Linux-x86
-CND_ARTIFACT_DIR_Release=build/Release/GNU-Linux-x86/tests/TestFiles
-CND_ARTIFACT_NAME_Release=f4
-CND_ARTIFACT_PATH_Release=build/Release/GNU-Linux-x86/tests/TestFiles/f4
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux-x86
+CND_ARTIFACT_NAME_Release=libkarabo.so
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux-x86/libkarabo.so
 CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux-x86/package
 CND_PACKAGE_NAME_Release=libkarabo.so.tar
 CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux-x86/package/libkarabo.so.tar
