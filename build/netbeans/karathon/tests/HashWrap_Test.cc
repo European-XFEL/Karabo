6903d2cf39f98da259467f000687a089c07161aa
esenov <sergey.esenov@xfel.eu>
2013-03-11 15:55:59 +0000

    Working boost python test
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/branches/hash-v2@8924 a23ab9f9-1ea2-4934-960d-990259f9ea8c

--- build/netbeans/karathon/tests/HashWrap_Test.cc
+++ build/netbeans/karathon/tests/HashWrap_Test.cc
@@ -6,9 +6,11 @@
  */
 
 #include <boost/python.hpp>
+#include <iostream>
 #include "HashWrap_Test.hh"
 
 namespace bp = boost::python;
+using namespace std;
 
 CPPUNIT_TEST_SUITE_REGISTRATION(HashWrap_Test);
 
@@ -23,19 +25,48 @@ HashWrap_Test::~HashWrap_Test() {
 
 void HashWrap_Test::setUp() {
     bp::exec(
-            "from libkarathon import *\n"
-            , o_global, o_global);
-    bp::object o_global = o_main.attr("__dict__");
+             "import os\nimport sys\nsys.path.append(os.getcwd()+'/dist/Debug/GNU-Linux-x86')\nfrom libkarathon import *\n"
+             , o_global, o_global);
 }
 
 void HashWrap_Test::tearDown() {
 }
 
-void HashWrap_Test::testMethod() {
+void HashWrap_Test::testConstructors() {
+    try {
+        bp::object o_Hash = o_main.attr("Hash");
+        bp::object h = o_Hash();
+        CPPUNIT_ASSERT(bp::extract<int>(h.attr("__len__")()) == 0);
+        CPPUNIT_ASSERT(bp::extract<bool>(h.attr("empty")()) == true);
+    } catch (const bp::error_already_set&) {
+        PyErr_Print();
+        CPPUNIT_ASSERT(false);
+    }
+}
+
+void HashWrap_Test::testGetSet() {
+    //    bp::exec("h = Hash('a.b', 12)\nprint ' '\nprint h\n"
+    //             , o_global, o_global);
     CPPUNIT_ASSERT(true);
 }
 
-void HashWrap_Test::testFailedMethod() {
-    CPPUNIT_ASSERT(false);
+void HashWrap_Test::testGetAs() {
+    CPPUNIT_ASSERT(true);
+}
+
+void HashWrap_Test::testFind() {
+    CPPUNIT_ASSERT(true);
+}
+
+void HashWrap_Test::testAttributes() {
+    CPPUNIT_ASSERT(true);
+}
+
+void HashWrap_Test::testIteration() {
+    CPPUNIT_ASSERT(true);
+}
+
+void HashWrap_Test::testMerge() {
+    CPPUNIT_ASSERT(true);
 }
 
