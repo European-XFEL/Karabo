90d9d21e54030ed250c4c9ae75e8ee5ddef221e3
esenov <esenov@a23ab9f9-1ea2-4934-960d-990259f9ea8c>
2013-03-15 12:59:07 +0000

    Fix configuration (again!) to avoid full paths
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/branches/hash-v2@8953 a23ab9f9-1ea2-4934-960d-990259f9ea8c

--- build/netbeans/karathon/nbproject/Karabo-Debug-.mk
+++ build/netbeans/karathon/nbproject/Karabo-Debug-.mk
@@ -35,9 +35,9 @@ OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
 
 # Object Files
 OBJECTFILES= \
-	${OBJECTDIR}/_ext/684620707/HashWrap.o \
-	${OBJECTDIR}/_ext/684620707/PyUtilHash.o \
-	${OBJECTDIR}/_ext/684620707/karathon.o
+	${OBJECTDIR}/_ext/381567218/HashWrap.o \
+	${OBJECTDIR}/_ext/381567218/PyUtilHash.o \
+	${OBJECTDIR}/_ext/381567218/karathon.o
 
 # Test Directory
 TESTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}/tests
@@ -60,36 +60,33 @@ FFLAGS=
 ASFLAGS=
 
 # Link Libraries and Options
-LDLIBSOPTIONS=-L/opt/local/nss -L/opt/local/nspr -L${KARATHON}/karabo -L${KARATHON}/karabo/extern/lib -Wl,-rpath,\$$ORIGIN/karabo -Wl,-rpath,\$$ORIGIN/karabo/extern/lib -Wl,-rpath,/home/esenov/branch/karaboFramework/build/netbeans/karabo/dist/Debug/GNU-Linux-x86 -L/home/esenov/branch/karaboFramework/build/netbeans/karabo/dist/Debug/GNU-Linux-x86 -lkarabo -lboost_chrono -lboost_date_time -lboost_filesystem -lboost_numpy -lboost_python -lboost_regex -lboost_signals -lboost_system -lboost_thread -lcppunit -lhdf5 -lhdf5_cpp -lhdf5_hl -lhdf5_hl_cpp -llog4cpp -lnetsnmp -lnetsnmpagent -lnetsnmphelpers -lnetsnmpmibs -lnetsnmptrapd -lopenmqc -lpython2.7
+LDLIBSOPTIONS=-L/opt/local/nss -L/opt/local/nspr -L${KARATHON}/karabo -L${KARATHON}/karabo/extern/lib -Wl,-rpath,\$$ORIGIN/karabo -Wl,-rpath,\$$ORIGIN/karabo/extern/lib -lkarabo -lboost_chrono -lboost_date_time -lboost_filesystem -lboost_numpy -lboost_python -lboost_regex -lboost_signals -lboost_system -lboost_thread -lcppunit -lhdf5 -lhdf5_cpp -lhdf5_hl -lhdf5_hl_cpp -llog4cpp -lnetsnmp -lnetsnmpagent -lnetsnmphelpers -lnetsnmpmibs -lnetsnmptrapd -lopenmqc -lpython2.7
 
 # Build Targets
 .build-conf: ${BUILD_SUBPROJECTS}
 	"${MAKE}"  -f nbproject/Makefile-${CND_CONF}.mk ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/libkarathon.${CND_DLIB_EXT}
 
-${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/libkarathon.${CND_DLIB_EXT}: /home/esenov/branch/karaboFramework/build/netbeans/karabo/dist/Debug/GNU-Linux-x86/libkarabo.so
-
 ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/libkarathon.${CND_DLIB_EXT}: ${OBJECTFILES}
 	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
 	${LINK.cc} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/libkarathon.${CND_DLIB_EXT} ${OBJECTFILES} ${LDLIBSOPTIONS} -shared -fPIC
 
-${OBJECTDIR}/_ext/684620707/HashWrap.o: /home/esenov/branch/karaboFramework/src/karathon/HashWrap.cc 
-	${MKDIR} -p ${OBJECTDIR}/_ext/684620707
+${OBJECTDIR}/_ext/381567218/HashWrap.o: ../../../src/karathon/HashWrap.cc 
+	${MKDIR} -p ${OBJECTDIR}/_ext/381567218
 	${RM} $@.d
-	$(COMPILE.cc) -g -DKARATHON_BOOST_NUMPY -I../../../src -I${KARATHON}/karabo/include -I${KARATHON}/karabo/extern/include/hdf5 -I${KARATHON}/karabo/extern/include/python2.7 -I${KARATHON}/karabo/extern/include -fPIC  -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/684620707/HashWrap.o /home/esenov/branch/karaboFramework/src/karathon/HashWrap.cc
+	$(COMPILE.cc) -g -DKARATHON_BOOST_NUMPY -I../../../src -I${KARATHON}/karabo/include -I${KARATHON}/karabo/extern/include/hdf5 -I${KARATHON}/karabo/extern/include/python2.7 -I${KARATHON}/karabo/extern/include -fPIC  -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/381567218/HashWrap.o ../../../src/karathon/HashWrap.cc
 
-${OBJECTDIR}/_ext/684620707/PyUtilHash.o: /home/esenov/branch/karaboFramework/src/karathon/PyUtilHash.cc 
-	${MKDIR} -p ${OBJECTDIR}/_ext/684620707
+${OBJECTDIR}/_ext/381567218/PyUtilHash.o: ../../../src/karathon/PyUtilHash.cc 
+	${MKDIR} -p ${OBJECTDIR}/_ext/381567218
 	${RM} $@.d
-	$(COMPILE.cc) -g -DKARATHON_BOOST_NUMPY -I../../../src -I${KARATHON}/karabo/include -I${KARATHON}/karabo/extern/include/hdf5 -I${KARATHON}/karabo/extern/include/python2.7 -I${KARATHON}/karabo/extern/include -fPIC  -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/684620707/PyUtilHash.o /home/esenov/branch/karaboFramework/src/karathon/PyUtilHash.cc
+	$(COMPILE.cc) -g -DKARATHON_BOOST_NUMPY -I../../../src -I${KARATHON}/karabo/include -I${KARATHON}/karabo/extern/include/hdf5 -I${KARATHON}/karabo/extern/include/python2.7 -I${KARATHON}/karabo/extern/include -fPIC  -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/381567218/PyUtilHash.o ../../../src/karathon/PyUtilHash.cc
 
-${OBJECTDIR}/_ext/684620707/karathon.o: /home/esenov/branch/karaboFramework/src/karathon/karathon.cc 
-	${MKDIR} -p ${OBJECTDIR}/_ext/684620707
+${OBJECTDIR}/_ext/381567218/karathon.o: ../../../src/karathon/karathon.cc 
+	${MKDIR} -p ${OBJECTDIR}/_ext/381567218
 	${RM} $@.d
-	$(COMPILE.cc) -g -DKARATHON_BOOST_NUMPY -I../../../src -I${KARATHON}/karabo/include -I${KARATHON}/karabo/extern/include/hdf5 -I${KARATHON}/karabo/extern/include/python2.7 -I${KARATHON}/karabo/extern/include -fPIC  -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/684620707/karathon.o /home/esenov/branch/karaboFramework/src/karathon/karathon.cc
+	$(COMPILE.cc) -g -DKARATHON_BOOST_NUMPY -I../../../src -I${KARATHON}/karabo/include -I${KARATHON}/karabo/extern/include/hdf5 -I${KARATHON}/karabo/extern/include/python2.7 -I${KARATHON}/karabo/extern/include -fPIC  -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/381567218/karathon.o ../../../src/karathon/karathon.cc
 
 # Subprojects
 .build-subprojects:
-	cd /home/esenov/branch/karaboFramework/build/netbeans/karabo && ${MAKE}  -f Makefile CONF=Debug
 
 # Build Test Targets
 .build-tests-conf: .build-conf ${TESTFILES}
@@ -110,43 +107,43 @@ ${TESTDIR}/tests/karathonTestRunner.o: tests/karathonTestRunner.cc
 	$(COMPILE.cc) -g -DKARATHON_BOOST_NUMPY -I../../../src -I${KARATHON}/karabo/include -I${KARATHON}/karabo/extern/include/hdf5 -I${KARATHON}/karabo/extern/include/python2.7 -I${KARATHON}/karabo/extern/include `cppunit-config --cflags` -MMD -MP -MF $@.d -o ${TESTDIR}/tests/karathonTestRunner.o tests/karathonTestRunner.cc
 
 
-${OBJECTDIR}/_ext/684620707/HashWrap_nomain.o: ${OBJECTDIR}/_ext/684620707/HashWrap.o /home/esenov/branch/karaboFramework/src/karathon/HashWrap.cc 
-	${MKDIR} -p ${OBJECTDIR}/_ext/684620707
-	@NMOUTPUT=`${NM} ${OBJECTDIR}/_ext/684620707/HashWrap.o`; \
+${OBJECTDIR}/_ext/381567218/HashWrap_nomain.o: ${OBJECTDIR}/_ext/381567218/HashWrap.o ../../../src/karathon/HashWrap.cc 
+	${MKDIR} -p ${OBJECTDIR}/_ext/381567218
+	@NMOUTPUT=`${NM} ${OBJECTDIR}/_ext/381567218/HashWrap.o`; \
 	if (echo "$$NMOUTPUT" | ${GREP} '|main$$') || \
 	   (echo "$$NMOUTPUT" | ${GREP} 'T main$$') || \
 	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
 	then  \
 	    ${RM} $@.d;\
-	    $(COMPILE.cc) -g -DKARATHON_BOOST_NUMPY -I../../../src -I${KARATHON}/karabo/include -I${KARATHON}/karabo/extern/include/hdf5 -I${KARATHON}/karabo/extern/include/python2.7 -I${KARATHON}/karabo/extern/include -fPIC  -Dmain=__nomain -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/684620707/HashWrap_nomain.o /home/esenov/branch/karaboFramework/src/karathon/HashWrap.cc;\
+	    $(COMPILE.cc) -g -DKARATHON_BOOST_NUMPY -I../../../src -I${KARATHON}/karabo/include -I${KARATHON}/karabo/extern/include/hdf5 -I${KARATHON}/karabo/extern/include/python2.7 -I${KARATHON}/karabo/extern/include -fPIC  -Dmain=__nomain -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/381567218/HashWrap_nomain.o ../../../src/karathon/HashWrap.cc;\
 	else  \
-	    ${CP} ${OBJECTDIR}/_ext/684620707/HashWrap.o ${OBJECTDIR}/_ext/684620707/HashWrap_nomain.o;\
+	    ${CP} ${OBJECTDIR}/_ext/381567218/HashWrap.o ${OBJECTDIR}/_ext/381567218/HashWrap_nomain.o;\
 	fi
 
-${OBJECTDIR}/_ext/684620707/PyUtilHash_nomain.o: ${OBJECTDIR}/_ext/684620707/PyUtilHash.o /home/esenov/branch/karaboFramework/src/karathon/PyUtilHash.cc 
-	${MKDIR} -p ${OBJECTDIR}/_ext/684620707
-	@NMOUTPUT=`${NM} ${OBJECTDIR}/_ext/684620707/PyUtilHash.o`; \
+${OBJECTDIR}/_ext/381567218/PyUtilHash_nomain.o: ${OBJECTDIR}/_ext/381567218/PyUtilHash.o ../../../src/karathon/PyUtilHash.cc 
+	${MKDIR} -p ${OBJECTDIR}/_ext/381567218
+	@NMOUTPUT=`${NM} ${OBJECTDIR}/_ext/381567218/PyUtilHash.o`; \
 	if (echo "$$NMOUTPUT" | ${GREP} '|main$$') || \
 	   (echo "$$NMOUTPUT" | ${GREP} 'T main$$') || \
 	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
 	then  \
 	    ${RM} $@.d;\
-	    $(COMPILE.cc) -g -DKARATHON_BOOST_NUMPY -I../../../src -I${KARATHON}/karabo/include -I${KARATHON}/karabo/extern/include/hdf5 -I${KARATHON}/karabo/extern/include/python2.7 -I${KARATHON}/karabo/extern/include -fPIC  -Dmain=__nomain -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/684620707/PyUtilHash_nomain.o /home/esenov/branch/karaboFramework/src/karathon/PyUtilHash.cc;\
+	    $(COMPILE.cc) -g -DKARATHON_BOOST_NUMPY -I../../../src -I${KARATHON}/karabo/include -I${KARATHON}/karabo/extern/include/hdf5 -I${KARATHON}/karabo/extern/include/python2.7 -I${KARATHON}/karabo/extern/include -fPIC  -Dmain=__nomain -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/381567218/PyUtilHash_nomain.o ../../../src/karathon/PyUtilHash.cc;\
 	else  \
-	    ${CP} ${OBJECTDIR}/_ext/684620707/PyUtilHash.o ${OBJECTDIR}/_ext/684620707/PyUtilHash_nomain.o;\
+	    ${CP} ${OBJECTDIR}/_ext/381567218/PyUtilHash.o ${OBJECTDIR}/_ext/381567218/PyUtilHash_nomain.o;\
 	fi
 
-${OBJECTDIR}/_ext/684620707/karathon_nomain.o: ${OBJECTDIR}/_ext/684620707/karathon.o /home/esenov/branch/karaboFramework/src/karathon/karathon.cc 
-	${MKDIR} -p ${OBJECTDIR}/_ext/684620707
-	@NMOUTPUT=`${NM} ${OBJECTDIR}/_ext/684620707/karathon.o`; \
+${OBJECTDIR}/_ext/381567218/karathon_nomain.o: ${OBJECTDIR}/_ext/381567218/karathon.o ../../../src/karathon/karathon.cc 
+	${MKDIR} -p ${OBJECTDIR}/_ext/381567218
+	@NMOUTPUT=`${NM} ${OBJECTDIR}/_ext/381567218/karathon.o`; \
 	if (echo "$$NMOUTPUT" | ${GREP} '|main$$') || \
 	   (echo "$$NMOUTPUT" | ${GREP} 'T main$$') || \
 	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
 	then  \
 	    ${RM} $@.d;\
-	    $(COMPILE.cc) -g -DKARATHON_BOOST_NUMPY -I../../../src -I${KARATHON}/karabo/include -I${KARATHON}/karabo/extern/include/hdf5 -I${KARATHON}/karabo/extern/include/python2.7 -I${KARATHON}/karabo/extern/include -fPIC  -Dmain=__nomain -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/684620707/karathon_nomain.o /home/esenov/branch/karaboFramework/src/karathon/karathon.cc;\
+	    $(COMPILE.cc) -g -DKARATHON_BOOST_NUMPY -I../../../src -I${KARATHON}/karabo/include -I${KARATHON}/karabo/extern/include/hdf5 -I${KARATHON}/karabo/extern/include/python2.7 -I${KARATHON}/karabo/extern/include -fPIC  -Dmain=__nomain -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/381567218/karathon_nomain.o ../../../src/karathon/karathon.cc;\
 	else  \
-	    ${CP} ${OBJECTDIR}/_ext/684620707/karathon.o ${OBJECTDIR}/_ext/684620707/karathon_nomain.o;\
+	    ${CP} ${OBJECTDIR}/_ext/381567218/karathon.o ${OBJECTDIR}/_ext/381567218/karathon_nomain.o;\
 	fi
 
 # Run Test Targets
@@ -165,7 +162,6 @@ ${OBJECTDIR}/_ext/684620707/karathon_nomain.o: ${OBJECTDIR}/_ext/684620707/karat
 
 # Subprojects
 .clean-subprojects:
-	cd /home/esenov/branch/karaboFramework/build/netbeans/karabo && ${MAKE}  -f Makefile CONF=Debug clean
 
 # Enable dependency checking
 .dep.inc: .depcheck-impl
