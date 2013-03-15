b0f5669a2020d9c8a680094d489b4d371a973130
irinak <irina.kozlova@xfel.eu>
2013-03-15 13:22:13 +0000

    unit test for XsdSerializer
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/branches/hash-v2@8954 a23ab9f9-1ea2-4934-960d-990259f9ea8c

---
+++ src/karabo/SchemaXsdSerializer_Test.cc
@@ -0,0 +1,205 @@
+/* 
+ * File:   SchemaXsdSerializer_Test.cc
+ * Author: irinak
+ * 
+ * Created on March 15, 2013, 10:30 AM
+ */
+
+#include <karabo/io/SchemaXsdSerializer.hh>
+#include "SchemaXsdSerializer_Test.hh"
+
+#include <karabo/util/SimpleElement.hh>
+#include <karabo/util/ChoiceElement.hh>
+#include <karabo/util/NodeElement.hh>
+#include <karabo/util/ListElement.hh>
+#include <karabo/util/VectorElement.hh>
+
+CPPUNIT_TEST_SUITE_REGISTRATION(SchemaXsdSerializer_Test);
+
+using namespace karabo::io;
+using namespace karabo::util;
+
+
+SchemaXsdSerializer_Test::SchemaXsdSerializer_Test() {
+}
+
+
+SchemaXsdSerializer_Test::~SchemaXsdSerializer_Test() {
+}
+
+
+void SchemaXsdSerializer_Test::setUp() {
+
+
+}
+
+
+void SchemaXsdSerializer_Test::tearDown() {
+
+}
+
+
+struct TestSchemaXsdSerializer {
+    KARABO_CLASSINFO(TestSchemaXsdSerializer, "TestXsd", "1.0");
+
+
+    static void expectedParameters(karabo::util::Schema & expected) {
+
+        STRING_ELEMENT(expected).key("exampleKey1")
+                .tags("hardware, poll")
+                .displayedName("Example key 1")
+                .description("Example key 1 description")
+                .options("Radio,Air Condition,Navigation", ",")
+                .assignmentOptional().defaultValue("Navigation")
+                .advanced()
+                .commit();
+
+        NODE_ELEMENT(expected).key("MyNodeElement")
+                .tags("myNode")
+                .displayedName("MyNodeElem")
+                .description("Description of my node elem")
+                .commit();
+
+        DOUBLE_ELEMENT(expected).key("MyNodeElement.a")
+                .tags("myNode")
+                .displayedName("MyNodeElem_A")
+                .description("Description of my node elem A")
+                .assignmentMandatory()
+                .commit();
+
+        STRING_ELEMENT(expected).key("MyNodeElement.b")
+                .tags("myNode")
+                .displayedName("MyNodeElem_B")
+                .description("Description of my node elem B")
+                .assignmentMandatory()
+                .commit();
+
+        INT64_ELEMENT(expected).key("exampleKey5").alias("exampleAlias5")
+                .tags("h/w; d.m.y", ";")
+                .displayedName("Example key 5")
+                .description("Example key 5 description")
+                .readOnly()
+                .initialValue(1442244)
+                .commit();
+
+        INT64_ELEMENT(expected).key("exampleKeyINTERNAL")
+                .displayedName("INTERNAL")
+                .description("Example key INTERNAL")
+                .assignmentInternal();
+
+        //test for CHOICE element
+        CHOICE_ELEMENT(expected).key("shapes")
+                .displayedName("shapesAsChoice")
+                .description("Description of Choice-element shapes")
+                .assignmentOptional().defaultValue("circle")
+                .assignmentMandatory()
+                .commit();
+
+        //or: test for LIST element (min/max appears only in 'annotation') 
+        //        LIST_ELEMENT(expected).key("shapes")
+        //                .displayedName("shapesAsList")
+        //                .description("Description of List-element shapes")
+        //                .min(1)
+        //                .max(5)
+        //                .assignmentMandatory()
+        //                .commit();
+
+        NODE_ELEMENT(expected).key("shapes.circle")
+                .tags("shape")
+                .displayedName("Circle")
+                .description("Description of circle")
+                .commit();
+
+        INT32_ELEMENT(expected).key("shapes.circle.radius")
+                .tags("shape")
+                .displayedName("radius")
+                .description("Radius of circle")
+                .minInc(5)
+                .maxExc(10)
+                .assignmentOptional().defaultValue(5)
+                .commit();
+
+        INT32_ELEMENT(expected).key("shapes.circle.color")
+                .tags("shape")
+                .displayedName("color")
+                .description("Color of circle")
+                .minExc(2)
+                .maxInc(20)
+                .assignmentOptional().defaultValue(5)
+                .commit();
+
+        NODE_ELEMENT(expected).key("shapes.circle.newnode")
+                .tags("shape")
+                .displayedName("NewNodeOfCircle")
+                .description("Description of NEW NODE of circle")
+                .commit();
+
+        INT32_ELEMENT(expected).key("shapes.circle.newnode.mynewint")
+                .tags("shape")
+                .displayedName("MyNewInt")
+                .description("Descr of shapes circle newnode MyNewInt")
+                .assignmentOptional().defaultValue(555)
+                .commit();
+
+        NODE_ELEMENT(expected).key("shapes.rectangle")
+                .tags("shape")
+                .displayedName("rectangle")
+                .description("Description of rectangle")
+                .commit();
+
+        DOUBLE_ELEMENT(expected).key("shapes.rectangle.square")
+                .tags("shape")
+                .displayedName("square")
+                .description("Description of square of rectangle")
+                .assignmentOptional().noDefaultValue()
+                .commit();
+
+
+        vector<string> vecStr;
+        vecStr.push_back("first line");
+        vecStr.push_back("second line");
+        VECTOR_STRING_ELEMENT(expected).key("strVector")
+                .displayedName("myVectorString")
+                .assignmentOptional().defaultValue(vecStr)
+                .reconfigurable()
+                .commit();
+
+        vector<int> vecInt;
+        vecInt.push_back(5);
+        vecInt.push_back(15);
+        VECTOR_INT32_ELEMENT(expected).key("intVector")
+                .displayedName("MyVectorInt")
+                .minSize(2)
+                .maxSize(5)
+                .assignmentOptional().defaultValue(vecInt)
+                .reconfigurable()
+                .commit();
+
+        INT32_ELEMENT(expected).key("SimpleElem")
+                .displayedName("SimpleElem")
+                .description("Description of SimpleElem")
+                .unit(Units::METER)
+                .metricPrefix(Units::MILLI)
+                .readOnly()
+                .alarmHigh(7)
+                .alarmLow(2)
+                .warnHigh(1)
+                .warnLow(0)
+                .commit();
+
+    }
+};
+
+
+void SchemaXsdSerializer_Test::testXsdSerialization() {
+
+    Schema testSchema("TestSchema", Schema::AssemblyRules(READ | WRITE | INIT));
+    TestSchemaXsdSerializer::expectedParameters(testSchema);
+    //cout << "Schema : \n" << testSchema << endl;
+
+    TextSerializer<Schema>::Pointer p = TextSerializer<Schema>::create("Xsd");
+
+    std::string archive;
+    p->save(testSchema, archive);
+    //cout << "\n XSD representation: \n" << archive << endl;
+}
