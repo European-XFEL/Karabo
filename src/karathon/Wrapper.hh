dbf7d206aa1b4e9e7e00fe80a4c46a43d46544b3
esenov <sergey.esenov@xfel.eu>
2013-03-19 17:21:39 +0000

    Add Wrapper
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/branches/hash-v2@9007 a23ab9f9-1ea2-4934-960d-990259f9ea8c

---
+++ src/karathon/Wrapper.hh
@@ -0,0 +1,82 @@
+/* 
+ * File:   Wrapper.hh
+ * Author: esenov
+ *
+ * Created on March 17, 2013, 10:52 PM
+ */
+
+#ifndef WRAPPER_HH
+#define	WRAPPER_HH
+
+#include <boost/python.hpp>
+#include <boost/any.hpp>
+#ifdef KARATHON_BOOST_NUMPY
+#include <boost/numpy.hpp>
+namespace bn = boost::numpy;
+#endif
+
+namespace bp = boost::python;
+
+namespace karabo {
+    namespace pyexfel {
+
+        struct Wrapper {
+
+            template<class ValueType>
+            static bp::object fromStdVectorToPyArray(const std::vector<ValueType>& v) {
+                #ifdef KARATHON_BOOST_NUMPY
+                Py_intptr_t shape[1] = {v.size()};
+                bn::ndarray result = bn::zeros(1, shape, bn::dtype::get_builtin<ValueType>());
+                std::copy(v.begin(), v.end(), reinterpret_cast<ValueType*> (result.get_data()));
+                return result;
+                #else
+                return fromStdVectorToPyList(v);
+                #endif
+            }
+
+            template<class ValueType>
+            static bp::object fromStdVectorToPyList(const std::vector<ValueType>& v) {
+                bp::object it = bp::iterator<std::vector<ValueType> >();
+                bp::object iter = it(v);
+                bp::list l(iter);
+                return l;
+            }
+
+            static bp::object fromStdVectorToPyByteArray(const std::vector<char>& v) {
+                const char* data = &v[0];
+                Py_ssize_t size = v.size();
+                return bp::object(bp::handle<>(PyByteArray_FromStringAndSize(data, size)));
+            }
+
+            static bp::object fromStdVectorToPyByteArray(const std::vector<signed char>& v) {
+                const char* data = reinterpret_cast<const char*>(&v[0]);
+                Py_ssize_t size = v.size();
+                return bp::object(bp::handle<>(PyByteArray_FromStringAndSize(data, size)));
+            }
+
+            static bp::object fromStdVectorToPyByteArray(const std::vector<unsigned char>& v) {
+                const char* data = reinterpret_cast<const char*>(&v[0]);
+                Py_ssize_t size = v.size();
+                return bp::object(bp::handle<>(PyByteArray_FromStringAndSize(data, size)));
+            }
+
+            static bp::str fromStdVectorToPyStr(const std::vector<char>& v) {
+                return bp::str(&v[0], v.size());
+            }
+
+            static bp::str fromStdVectorToPyStr(const std::vector<signed char>& v) {
+                return bp::str(reinterpret_cast<const char*> (&v[0]), v.size());
+            }
+
+            static bp::str fromStdVectorToPyStr(const std::vector<unsigned char>& v) {
+                return bp::str(reinterpret_cast<const char*> (&v[0]), v.size());
+            }
+            
+            static bp::object toObject(const boost::any& operand);
+            static void toAny(const bp::object& operand, boost::any& any);
+        };
+    }
+}
+
+#endif	/* WRAPPER_HH */
+
