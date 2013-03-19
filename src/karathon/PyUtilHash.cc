cd288d2620edd8b89e38238cd0ff8d47e8b3bc62
esenov <sergey.esenov@xfel.eu>
2013-03-19 17:23:11 +0000

    Update
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/branches/hash-v2@9011 a23ab9f9-1ea2-4934-960d-990259f9ea8c

--- src/karathon/PyUtilHash.cc
+++ src/karathon/PyUtilHash.cc
@@ -9,7 +9,9 @@
 
 #include <boost/python.hpp>
 #include <string>
+#include <karabo/util/Hash.hh>
 #include "HashWrap.hh"
+#include "NodeWrap.hh"
 
 namespace bp = boost::python;
 using namespace karabo::util;
@@ -21,6 +23,24 @@ void exportPyUtilHash() {
 #    ifdef KARATHON_BOOST_NUMPY
     bn::initialize();
 #    endif
+
+
+    bp::class_<Hash::Attributes> a("Hash.Attributes", bp::init<>());
+
+
+    bp::class_<boost::optional<Hash::Node&> > n("Hash.Node", bp::no_init);
+
+    n.def("getKey", &NodeWrap().pythonGetKey);
+    n.def("setValue", &NodeWrap().pythonSetValue, (bp::arg("value")));
+    n.def("getValue", &NodeWrap().pythonGetValue);
+    n.def("getValueAs", &NodeWrap().pythonGetValueAs, (bp::arg("type")));
+    n.def("setAttribute", &NodeWrap().pythonSetAttribute, (bp::arg("key"), bp::arg("value")));
+    n.def("getAttribute", &NodeWrap().pythonGetAttribute, (bp::arg("key")));
+    n.def("getAttributeAs", &NodeWrap().pythonGetAttributeAs, (bp::arg("key"), bp::arg("type")));
+    n.def("hasAttribute", &NodeWrap().pythonHasAttribute, (bp::arg("key")));
+    n.def("setAttributes", &NodeWrap().pythonSetAttributes, (bp::arg("attributes")));
+    n.def("getAttributes", &NodeWrap().pythonGetAttributes);
+
 
     bp::class_<Hash > h("Hash");
 
@@ -89,49 +109,44 @@ void exportPyUtilHash() {
 
     h.def("__iter__", bp::iterator<Hash > ());
 
-    h.def("getAsStr", &HashWrap().pythonGetAsStr, (bp::arg("path"), bp::arg("sep") = "."));
-    
-    h.def("getAsStrArr", &HashWrap().pythonGetAsStrArr, (bp::arg("path"), bp::arg("sep") = "."));
-    
-//    h.def("getAsBool", (bool (Hash::*)(const std::string&, const char))&Hash::get<bool>, (bp::arg("path"), bp::arg("sep") = "."));
-    
-    h.def("getAsInt", &HashWrap().pythonGetAsInt, (bp::arg("path"), bp::arg("sep") = "."));
-    
-    h.def("getAsIntArr", &HashWrap().pythonGetAsIntArr, (bp::arg("path"), bp::arg("sep") = "."));
-    
-    h.def("getAsLong", &HashWrap().pythonGetAsLong, (bp::arg("path"), bp::arg("sep") = "."));
-    
-    h.def("getAsLongArr", &HashWrap().pythonGetAsLongArr, (bp::arg("path"), bp::arg("sep") = "."));
-    
-    h.def("getAsFloat", &HashWrap().pythonGetAsFloat, (bp::arg("path"), bp::arg("sep") = "."));
-    
-    h.def("getAsFloatArr", &HashWrap().pythonGetAsFloatArr, (bp::arg("path"), bp::arg("sep") = "."));
-    
-    h.def("getAsDouble", &HashWrap().pythonGetAsDouble, (bp::arg("path"), bp::arg("sep") = "."));
-    
-    h.def("getAsDoubleArr", &HashWrap().pythonGetAsDoubleArr, (bp::arg("path"), bp::arg("sep") = "."));
-    
+    h.def("getAs", &HashWrap().pythonGetAs, (bp::arg("path"), bp::arg("type"), bp::arg("sep") = "."));
+
     h.def("getType", &HashWrap().pythonGetType, (bp::arg("key"), bp::arg("sep") = "."));
 
     h.def("getTypeAsId", &HashWrap().pythonGetTypeAsId, (bp::arg("key"), bp::arg("sep") = "."));
 
     h.def("merge", &Hash::merge, (bp::arg("hash")));
-    
-//    h.def("__add__", &Hash::operator+, (bp::arg("hash1"), bp::arg("hash2")), bp::return_value_policy<copy_non_const_reference>());
+
+    //    h.def("__add__", &Hash::operator+, (bp::arg("hash1"), bp::arg("hash2")), bp::return_value_policy<copy_non_const_reference>());
     h.def("__iadd__", &Hash::operator+=, (bp::arg("hash")), bp::return_internal_reference<>());
 
     h.def("is_type", &HashWrap().pythonIs, (bp::arg("path"), bp::arg("type"), bp::arg("sep") = "."));
 
     h.def(bp::self_ns::str(bp::self));
 
-    h.def("copy", &HashWrap().pyDict2Hash, (bp::arg("dict"), bp::arg("sep") = "."), bp::return_value_policy<bp::copy_const_reference > ());
+    //    h.def("copy", &HashWrap().pyDict2Hash, (bp::arg("dict"), bp::arg("sep") = "."), bp::return_value_policy<bp::copy_const_reference > ());
 
-//    h.def("update", &Hash::update, (bp::arg("hash")));
+    //    h.def("update", &Hash::update, (bp::arg("hash")));
 
     h.def("flatten", &HashWrap().pythonFlatten, (bp::arg("flat"), bp::arg("sep") = "."));
 
     h.def("unflatten", &HashWrap().pythonUnFlatten, (bp::arg("tree"), bp::arg("sep") = "."));
+    
+    h.def("find", &HashWrap().pythonFind, (bp::arg("path"), bp::arg("sep") = "."));
 
+    h.def("setNode", &HashWrap().pythonSetNode, (bp::arg("node")));
+
+    h.def("getNode", &HashWrap().pythonGetNode, (bp::arg("path"), bp::arg("sep") = "."));
+
+    h.def("getAttribute", &HashWrap().pythonGetAttribute, (bp::arg("path"), bp::arg("attribute"), bp::arg("sep") = "."));
+
+    h.def("getAttributeAs", &HashWrap().pythonGetAttributeAs, (bp::arg("path"), bp::arg("attribute"), bp::arg("type"), bp::arg("sep") = "."));
+
+    h.def("getAttributes", &HashWrap().pythonGetAttributes, (bp::arg("path"), bp::arg("sep") = "."));
+
+    h.def("setAttribute", &HashWrap().pythonSetAttribute, (bp::arg("path"), bp::arg("attribute"), bp::arg("value"), bp::arg("sep") = "."));
+
+    h.def("setAttributes", &HashWrap().pythonSetAttributes, (bp::arg("path"), bp::arg("attributes"), bp::arg("sep") = "."));
 
     bp::class_<std::vector<Hash> > v("VectorHash");
 
