f7c9126b224423b921c94e7f956947bd83ec7819
heisenb <heisenb@a23ab9f9-1ea2-4934-960d-990259f9ea8c>
2013-03-20 15:37:54 +0000

    Broker testing
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/branches/hash-v2@9024 a23ab9f9-1ea2-4934-960d-990259f9ea8c

---
+++ src/karabo/tests/net/JmsBroker_Test.hh
@@ -0,0 +1,50 @@
+/*
+ * File:   JmsBroker_Test.hh
+ * Author: heisenb
+ *
+ * Created on Mar 20, 2013, 3:31:07 PM
+ */
+
+#ifndef JMSBROKER_TEST_HH
+#define	JMSBROKER_TEST_HH
+
+#include <cppunit/extensions/HelperMacros.h>
+
+#include <karabo/net/BrokerIOService.hh>
+#include <karabo/net/BrokerConnection.hh>
+#include <karabo/net/BrokerChannel.hh>
+
+class JmsBroker_Test : public CPPUNIT_NS::TestFixture {
+    
+    int m_messagesRead;
+    
+    karabo::util::Hash m_hash;
+
+    CPPUNIT_TEST_SUITE(JmsBroker_Test);
+
+    CPPUNIT_TEST(testMethod);
+    
+    CPPUNIT_TEST(testBinaryTransport);
+
+    CPPUNIT_TEST_SUITE_END();
+
+public:
+    JmsBroker_Test();
+    
+    virtual ~JmsBroker_Test();
+    
+    void readHandler1(karabo::net::BrokerChannel::Pointer channel, const std::string& body, const karabo::util::Hash& header);
+    
+    void readHandler2(karabo::net::BrokerChannel::Pointer channel, const karabo::util::Hash& body, const karabo::util::Hash& header);
+    
+    
+private:
+    
+    void testMethod();
+    
+    void testBinaryTransport();
+
+};
+
+#endif	/* JMSBROKER_TEST_HH */
+
