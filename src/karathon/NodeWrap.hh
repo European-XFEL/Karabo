e6f4aab5f8bcd0e0e8089cd009f4a72590441431
esenov <sergey.esenov@xfel.eu>
2013-03-21 11:06:33 +0000

    Fix bug in HashWrap::pythonFind: returns now boost::shared_ptr to the Node with no_deleter, i.e. pointer looks directly into node in Hash container
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/branches/hash-v2@9042 a23ab9f9-1ea2-4934-960d-990259f9ea8c

--- src/karathon/NodeWrap.hh
+++ src/karathon/NodeWrap.hh
@@ -25,178 +25,180 @@ namespace karabo {
 
         class NodeWrap {
         public:
-
-            static bp::object pythonGetKey(karabo::util::Hash::Node& node) {
-                return bp::object(node.getKey());
+            
+            typedef boost::shared_ptr<karabo::util::Hash::Node> Pointer;
+            
+            static bp::object pythonGetKey(const Pointer& node) {
+                return bp::object(node->getKey());
             }
 
-            static void pythonSetValue(karabo::util::Hash::Node& node, const bp::object& obj) {
+            static void pythonSetValue(const Pointer& node, const bp::object& obj) {
                 boost::any any;
                 Wrapper::toAny(obj, any);
-                node.setValue(any);
+                node->setValue(any);
             }
 
-            static bp::object pythonGetValue(karabo::util::Hash::Node& node) {
-                return Wrapper::toObject(node.getValueAsAny());
+            static bp::object pythonGetValue(const Pointer& node) {
+                return Wrapper::toObject(node->getValueAsAny());
             }
 
-            static bp::object pythonGetValueAs(karabo::util::Hash::Node& node, const std::string& type) {
+            static bp::object pythonGetValueAs(const Pointer& node, const std::string& type) {
                 using namespace karabo::util;
                 Types::ReferenceType reftype = Types::from<FromLiteral>(type);
                 switch (reftype) {
                     case Types::ANY:
-                        return Wrapper::toObject(node.getValueAsAny());
+                        return Wrapper::toObject(node->getValueAsAny());
                     case Types::BOOL:
-                        return bp::object(node.getValueAs<bool>());
+                        return bp::object(node->getValueAs<bool>());
                     case Types::CHAR:
-                        return bp::object(node.getValueAs<char>());
+                        return bp::object(node->getValueAs<char>());
                     case Types::INT8:
-                        return bp::object(node.getValueAs<signed char>());
+                        return bp::object(node->getValueAs<signed char>());
                     case Types::UINT8:
-                        return bp::object(node.getValueAs<unsigned char>());
+                        return bp::object(node->getValueAs<unsigned char>());
                     case Types::INT16:
-                        return bp::object(node.getValueAs<short>());
+                        return bp::object(node->getValueAs<short>());
                     case Types::UINT16:
-                        return bp::object(node.getValueAs<unsigned short>());
+                        return bp::object(node->getValueAs<unsigned short>());
                     case Types::INT32:
-                        return bp::object(node.getValueAs<int>());
+                        return bp::object(node->getValueAs<int>());
                     case Types::UINT32:
-                        return bp::object(node.getValueAs<unsigned int>());
+                        return bp::object(node->getValueAs<unsigned int>());
                     case Types::INT64:
-                        return bp::object(node.getValueAs<long long>());
+                        return bp::object(node->getValueAs<long long>());
                     case Types::UINT64:
-                        return bp::object(node.getValueAs<unsigned long long>());
+                        return bp::object(node->getValueAs<unsigned long long>());
                     case Types::FLOAT:
-                        return bp::object(node.getValueAs<float>());
+                        return bp::object(node->getValueAs<float>());
                     case Types::DOUBLE:
-                        return bp::object(node.getValueAs<double>());
+                        return bp::object(node->getValueAs<double>());
                     case Types::STRING:
-                        return bp::object(node.getValueAs<std::string>());
+                        return bp::object(node->getValueAs<std::string>());
                     case Types::VECTOR_BOOL:
-                        return Wrapper::fromStdVectorToPyArray(node.getValueAs<bool, std::vector > ());
+                        return Wrapper::fromStdVectorToPyArray(node->getValueAs<bool, std::vector > ());
                     case Types::VECTOR_CHAR:
-                        return Wrapper::fromStdVectorToPyByteArray(node.getValueAs<char, std::vector>());
+                        return Wrapper::fromStdVectorToPyByteArray(node->getValueAs<char, std::vector>());
                     case Types::VECTOR_INT8:
-                        return Wrapper::fromStdVectorToPyByteArray(node.getValueAs<signed char, std::vector > ());
+                        return Wrapper::fromStdVectorToPyByteArray(node->getValueAs<signed char, std::vector > ());
                     case Types::VECTOR_UINT8:
-                        return Wrapper::fromStdVectorToPyByteArray(node.getValueAs<unsigned char, std::vector>());
+                        return Wrapper::fromStdVectorToPyByteArray(node->getValueAs<unsigned char, std::vector>());
                     case Types::VECTOR_INT16:
-                        return Wrapper::fromStdVectorToPyArray(node.getValueAs<short, std::vector>());
+                        return Wrapper::fromStdVectorToPyArray(node->getValueAs<short, std::vector>());
                     case Types::VECTOR_UINT16:
-                        return Wrapper::fromStdVectorToPyArray(node.getValueAs<unsigned short, std::vector>());
+                        return Wrapper::fromStdVectorToPyArray(node->getValueAs<unsigned short, std::vector>());
                     case Types::VECTOR_INT32:
-                        return Wrapper::fromStdVectorToPyArray(node.getValueAs<int, std::vector>());
+                        return Wrapper::fromStdVectorToPyArray(node->getValueAs<int, std::vector>());
                     case Types::VECTOR_UINT32:
-                        return Wrapper::fromStdVectorToPyArray(node.getValueAs<unsigned int, std::vector>());
+                        return Wrapper::fromStdVectorToPyArray(node->getValueAs<unsigned int, std::vector>());
                     case Types::VECTOR_INT64:
-                        return Wrapper::fromStdVectorToPyArray(node.getValueAs<long long, std::vector>());
+                        return Wrapper::fromStdVectorToPyArray(node->getValueAs<long long, std::vector>());
                     case Types::VECTOR_UINT64:
-                        return Wrapper::fromStdVectorToPyArray(node.getValueAs<unsigned long long, std::vector>());
+                        return Wrapper::fromStdVectorToPyArray(node->getValueAs<unsigned long long, std::vector>());
                         //                    case Types::HASH:
-                        //                        return bp::object(node.getValueAs<karabo::util::Hash>());
+                        //                        return bp::object(node->getValueAs<karabo::util::Hash>());
                         //                    case Types::VECTOR_HASH:
-                        //                        return Wrapper::fromStdVectorToPyList(node.getValueAs<karabo::util::Hash, std::vector>());
+                        //                        return Wrapper::fromStdVectorToPyList(node->getValueAs<karabo::util::Hash, std::vector>());
                     default:
                         break;
                 }
                 throw KARABO_NOT_SUPPORTED_EXCEPTION("Type is not yet supported");
             }
 
-            static void pythonSetAttribute(karabo::util::Hash::Node& node, const std::string& key, const bp::object& obj) {
+            static void pythonSetAttribute(const Pointer& node, const std::string& key, const bp::object& obj) {
                 boost::any value;
                 Wrapper::toAny(obj, value);
-                node.setAttribute(key, value);
+                node->setAttribute(key, value);
             }
 
-            static bp::object pythonGetAttribute(karabo::util::Hash::Node& node, const std::string& key) {
-                return Wrapper::toObject(node.getAttributeAsAny(key));
+            static bp::object pythonGetAttribute(const Pointer& node, const std::string& key) {
+                return Wrapper::toObject(node->getAttributeAsAny(key));
             }
 
-            static bp::object pythonGetAttributeAs(karabo::util::Hash::Node& node, const std::string& key, const std::string& type) {
+            static bp::object pythonGetAttributeAs(const Pointer& node, const std::string& key, const std::string& type) {
                 using namespace karabo::util;
                 Types::ReferenceType reftype = Types::from<FromLiteral>(type);
                 switch (reftype) {
                     case Types::ANY:
-                        return Wrapper::toObject(node.getAttributeAsAny(key));
+                        return Wrapper::toObject(node->getAttributeAsAny(key));
                     case Types::BOOL:
-                        return bp::object(node.getAttributeAs<bool>(key));
+                        return bp::object(node->getAttributeAs<bool>(key));
                     case Types::CHAR:
-                        return bp::object(node.getAttributeAs<char>(key));
+                        return bp::object(node->getAttributeAs<char>(key));
                     case Types::INT8:
-                        return bp::object(node.getAttributeAs<signed char>(key));
+                        return bp::object(node->getAttributeAs<signed char>(key));
                     case Types::UINT8:
-                        return bp::object(node.getAttributeAs<unsigned char>(key));
+                        return bp::object(node->getAttributeAs<unsigned char>(key));
                     case Types::INT16:
-                        return bp::object(node.getAttributeAs<short>(key));
+                        return bp::object(node->getAttributeAs<short>(key));
                     case Types::UINT16:
-                        return bp::object(node.getAttributeAs<unsigned short>(key));
+                        return bp::object(node->getAttributeAs<unsigned short>(key));
                     case Types::INT32:
-                        return bp::object(node.getAttributeAs<int>(key));
+                        return bp::object(node->getAttributeAs<int>(key));
                     case Types::UINT32:
-                        return bp::object(node.getAttributeAs<unsigned int>(key));
+                        return bp::object(node->getAttributeAs<unsigned int>(key));
                     case Types::INT64:
-                        return bp::object(node.getAttributeAs<long long>(key));
+                        return bp::object(node->getAttributeAs<long long>(key));
                     case Types::UINT64:
-                        return bp::object(node.getAttributeAs<unsigned long long>(key));
+                        return bp::object(node->getAttributeAs<unsigned long long>(key));
                     case Types::FLOAT:
-                        return bp::object(node.getAttributeAs<float>(key));
+                        return bp::object(node->getAttributeAs<float>(key));
                     case Types::DOUBLE:
-                        return bp::object(node.getAttributeAs<double>(key));
+                        return bp::object(node->getAttributeAs<double>(key));
                     case Types::STRING:
-                        return bp::object(node.getAttributeAs<std::string>(key));
+                        return bp::object(node->getAttributeAs<std::string>(key));
                         //                    case Types::VECTOR_BOOL:
-                        //                        return Wrapper::fromStdVectorToPyArray(node.getAttributeAs<bool, std::vector>(key));
+                        //                        return Wrapper::fromStdVectorToPyArray(node->getAttributeAs<bool, std::vector>(key));
                         //                    case Types::VECTOR_CHAR:
-                        //                        return Wrapper::fromStdVectorToPyByteArray(node.getAttributeAs<char, std::vector>(key));
+                        //                        return Wrapper::fromStdVectorToPyByteArray(node->getAttributeAs<char, std::vector>(key));
                         //                    case Types::VECTOR_INT8:
-                        //                        return Wrapper::fromStdVectorToPyByteArray(node.getAttributeAs <signed char, std::vector>(key));
+                        //                        return Wrapper::fromStdVectorToPyByteArray(node->getAttributeAs <signed char, std::vector>(key));
                         //                    case Types::VECTOR_UINT8:
-                        //                        return Wrapper::fromStdVectorToPyByteArray(node.getAttributeAs<unsigned char, std::vector>(key));
+                        //                        return Wrapper::fromStdVectorToPyByteArray(node->getAttributeAs<unsigned char, std::vector>(key));
                         //                    case Types::VECTOR_INT16:
-                        //                        return Wrapper::fromStdVectorToPyArray(node.getAttributeAs<short, std::vector>(key));
+                        //                        return Wrapper::fromStdVectorToPyArray(node->getAttributeAs<short, std::vector>(key));
                         //                    case Types::VECTOR_UINT16:
-                        //                        return Wrapper::fromStdVectorToPyArray(node.getAttributeAs<unsigned short, std::vector>(key));
+                        //                        return Wrapper::fromStdVectorToPyArray(node->getAttributeAs<unsigned short, std::vector>(key));
                         //                    case Types::VECTOR_INT32:
-                        //                        return Wrapper::fromStdVectorToPyArray(node.getAttributeAs<int, std::vector>(key));
+                        //                        return Wrapper::fromStdVectorToPyArray(node->getAttributeAs<int, std::vector>(key));
                         //                    case Types::VECTOR_UINT32:
-                        //                        return Wrapper::fromStdVectorToPyArray(node.getAttributeAs<unsigned int, std::vector>(key));
+                        //                        return Wrapper::fromStdVectorToPyArray(node->getAttributeAs<unsigned int, std::vector>(key));
                         //                    case Types::VECTOR_INT64:
-                        //                        return Wrapper::fromStdVectorToPyArray(node.getAttributeAs<long long, std::vector>(key));
+                        //                        return Wrapper::fromStdVectorToPyArray(node->getAttributeAs<long long, std::vector>(key));
                         //                    case Types::VECTOR_UINT64:
-                        //                        return Wrapper::fromStdVectorToPyArray(node.getAttributeAs<unsigned long long, std::vector>(key));
+                        //                        return Wrapper::fromStdVectorToPyArray(node->getAttributeAs<unsigned long long, std::vector>(key));
                         //                    case Types::HASH:
-                        //                        return bp::object(node.getAttributeAs<karabo::util::Hash>(key));
+                        //                        return bp::object(node->getAttributeAs<karabo::util::Hash>(key));
                         //                    case Types::VECTOR_HASH:
-                        //                        return Wrapper::fromStdVectorToPyList(node.getAttributeAs<karabo::util::Hash, std::vector>(key));
+                        //                        return Wrapper::fromStdVectorToPyList(node->getAttributeAs<karabo::util::Hash, std::vector>(key));
                     default:
                         break;
                 }
                 throw KARABO_NOT_SUPPORTED_EXCEPTION("Type is not yet supported");
             }
 
-            static bool pythonHasAttribute(karabo::util::Hash::Node& node, const std::string& key) {
-                return node.hasAttribute(key);
+            static bool pythonHasAttribute(const Pointer& node, const std::string& key) {
+                return node->hasAttribute(key);
             }
 
-            static void pythonSetAttributes(karabo::util::Hash::Node& node, const bp::object& obj) {
+            static void pythonSetAttributes(const Pointer& node, const bp::object& obj) {
                 if (bp::extract<karabo::util::Hash::Attributes>(obj).check())
-                    node.setAttributes(bp::extract<karabo::util::Hash::Attributes>(obj));
+                    node->setAttributes(bp::extract<karabo::util::Hash::Attributes>(obj));
                 else
                     throw KARABO_PYTHON_EXCEPTION("Python object is not of a Hash.Attributes type");
             }
 
-            static bp::object pythonGetAttributes(karabo::util::Hash::Node& node) {
-                return bp::object(node.getAttributes());
+            static bp::object pythonGetAttributes(const Pointer& node) {
+                return bp::object(node->getAttributes());
             }
 
-            static bp::object pythonGetType(karabo::util::Hash::Node& node) {
+            static bp::object pythonGetType(const Pointer& node) {
                 using namespace karabo::util;
-                return bp::str(Types::to<ToLiteral>(node.getType()));
+                return bp::str(Types::to<ToLiteral>(node->getType()));
             }
             
-            static void pythonSetType(karabo::util::Hash::Node& node, const std::string& type) {
+            static void pythonSetType(const Pointer& node, const std::string& type) {
                 using namespace karabo::util;
-                node.setType(Types::from<FromLiteral>(type));
+                node->setType(Types::from<FromLiteral>(type));
             }
         };
     }
