5f595c631aad78aacebc1947b4f4a7d049971c2e
esenov <sergey.esenov@xfel.eu>
2013-03-27 14:20:18 +0000

    Fix bug when converting from std::vector<std::string> into python list of strings;
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/branches/hash-v2@9177 a23ab9f9-1ea2-4934-960d-990259f9ea8c

--- src/karathon/Wrapper.hh
+++ src/karathon/Wrapper.hh
@@ -10,33 +10,39 @@
 
 #include <boost/python.hpp>
 #include <boost/any.hpp>
-#ifdef KARATHON_BOOST_NUMPY
 #include <boost/numpy.hpp>
-namespace bn = boost::numpy;
-#endif
+#include <karabo/util/Hash.hh>
 
+namespace bn = boost::numpy;
 namespace bp = boost::python;
 
 namespace karabo {
     namespace pyexfel {
 
+
         struct Wrapper {
+            static bool try_to_use_numpy;
 
             template<class ValueType>
             static bp::object fromStdVectorToPyArray(const std::vector<ValueType>& v) {
-                #ifdef KARATHON_BOOST_NUMPY
-                Py_intptr_t shape[1] = {v.size()};
-                bn::ndarray result = bn::zeros(1, shape, bn::dtype::get_builtin<ValueType>());
-                std::copy(v.begin(), v.end(), reinterpret_cast<ValueType*> (result.get_data()));
-                return result;
-                #else
+                if (try_to_use_numpy) {
+                    Py_intptr_t shape[1] = {v.size()};
+                    bn::ndarray result = bn::zeros(1, shape, bn::dtype::get_builtin<ValueType>());
+                    std::copy(v.begin(), v.end(), reinterpret_cast<ValueType*> (result.get_data()));
+                    return result;
+                }
                 return fromStdVectorToPyList(v);
-                #endif
             }
 
             template<class ValueType>
             static bp::object fromStdVectorToPyList(const std::vector<ValueType>& v) {
-                bp::object it = bp::iterator<std::vector<ValueType> >();
+                bp::list pylist;
+                for (size_t i = 0; i < v.size(); i++) pylist.append(bp::object(v[i]));
+                return pylist;
+            }
+
+            static bp::object fromStdVectorToPyHashList(const std::vector<karabo::util::Hash>& v) {
+                bp::object it = bp::iterator<std::vector<karabo::util::Hash> >();
                 bp::object iter = it(v);
                 bp::list l(iter);
                 return l;
@@ -49,13 +55,13 @@ namespace karabo {
             }
 
             static bp::object fromStdVectorToPyByteArray(const std::vector<signed char>& v) {
-                const char* data = reinterpret_cast<const char*>(&v[0]);
+                const char* data = reinterpret_cast<const char*> (&v[0]);
                 Py_ssize_t size = v.size();
                 return bp::object(bp::handle<>(PyByteArray_FromStringAndSize(data, size)));
             }
 
             static bp::object fromStdVectorToPyByteArray(const std::vector<unsigned char>& v) {
-                const char* data = reinterpret_cast<const char*>(&v[0]);
+                const char* data = reinterpret_cast<const char*> (&v[0]);
                 Py_ssize_t size = v.size();
                 return bp::object(bp::handle<>(PyByteArray_FromStringAndSize(data, size)));
             }
@@ -71,7 +77,7 @@ namespace karabo {
             static bp::str fromStdVectorToPyStr(const std::vector<unsigned char>& v) {
                 return bp::str(reinterpret_cast<const char*> (&v[0]), v.size());
             }
-            
+
             static bp::object toObject(const boost::any& operand);
             static void toAny(const bp::object& operand, boost::any& any);
         };
