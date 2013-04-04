7943f1c36a9a3b147566f6c03a2d4829b2581377
esenov <sergey.esenov@xfel.eu>
2013-04-04 10:35:12 +0000

    Add one more test for conversions vector values in attributes
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/branches/hash-v2@9242 a23ab9f9-1ea2-4934-960d-990259f9ea8c

--- src/karathonTest/tests/attributes.py
+++ src/karathonTest/tests/attributes.py
@@ -84,7 +84,18 @@ class  AttributesTestCase(unittest.TestCase):
             setStdVectorDefaultConversion(Types.PYTHON)
         except Exception,e:
             self.fail("test_attributes exception group 6: " + str(e))
-
+            
+        try:
+            h = Hash('a.b.c', 1, 'b.x', 2.22, 'b.y', 7.432, 'c', [1,2,3])
+            h.setAttribute('a.b.c','attr1',[1.234,2.987,5.555])
+            if isStdVectorDefaultConversion(Types.PYTHON):
+                self.assertEqual(h.getAttribute('a.b.c','attr1'), [1.234,2.987,5.555])
+                self.assertEqual(h.getAttributeAs('a.b.c','attr1',Types.NDARRAY_DOUBLE).all(), np.array([1.234,2.987,5.555], dtype=np.double).all())
+            if isStdVectorDefaultConversion(Types.NUMPY):
+                self.assertEqual(h.getAttribute('a.b.c','attr1').all(), np.array([1.234,2.987,5.555], dtype=np.double).all())
+                self.assertEqual(h.getAttributeAs('a.b.c','attr1',Types.VECTOR_DOUBLE), [1.234,2.987,5.555])
+        except Exception,e:
+            self.fail("test_attributes exception group 7: " + str(e))
 
 if __name__ == '__main__':
     unittest.main()
