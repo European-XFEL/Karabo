dcc26a1d78501880b0228c8e0c0e653c2c1c6699
wrona <krzysztof.wrona@xfel.eu>
2013-04-09 16:04:32 +0000

    added Tracer.hh
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/branches/hash-v2@9269 a23ab9f9-1ea2-4934-960d-990259f9ea8c

---
+++ src/karabo/log/Tracer.hh
@@ -0,0 +1,90 @@
+
+
+/* 
+ * $Id$
+ *
+ * Author: <krzysztof.wrona@xfel.eu>
+ *
+ * Copyright (C) European XFEL GmbH Hamburg. All rights reserved.
+ * 
+ * Created on April 5, 2013, 1:36 PM
+ */
+
+#ifndef KARABO_LOG_TRACER_HH
+#define	KARABO_LOG_TRACER_HH
+
+
+#include <karabo/log/Logger.hh>
+#include <karabo/util/Hash.hh>
+
+namespace karabo {
+
+    namespace log {
+
+        class Tracer {
+
+            karabo::util::Hash m_conf;
+            int m_numCategories;
+
+        public:
+
+            Tracer() : m_numCategories(0) {
+                m_conf.set("priority", "DEBUG");
+                m_conf.set("appenders[0].Ostream.layout", "Pattern");
+            }
+
+            void enableAll() {
+                m_conf.clear();
+                m_conf.set("priority", "DEBUG");
+                m_conf.set("appenders[0].Ostream.layout", "Pattern");
+                m_numCategories = 0;
+            }
+
+            void disableAll() {
+                m_conf.clear();
+                m_conf.set("priority", "INFO");
+                m_conf.set("appenders[0].Ostream.layout", "Pattern");
+                m_numCategories = 0;
+            }
+
+
+            void enable(const std::string& category) {
+                {
+                    std::ostringstream oss;
+                    oss << "categories[" << m_numCategories << "].Category.name";
+                    m_conf.set(oss.str(), category);
+                }
+                {
+                    std::ostringstream oss;
+                    oss << "categories[" << m_numCategories << "].Category.priority";
+                    m_conf.set(oss.str(), "DEBUG");
+                }
+                m_numCategories++;
+            }
+
+            void disable(const std::string& category) {
+                {
+                    std::ostringstream oss;
+                    oss << "categories[" << m_numCategories << "].Category.name";
+                    m_conf.set(oss.str(), category);
+                }
+                {
+                    std::ostringstream oss;
+                    oss << "categories[" << m_numCategories << "].Category.priority";
+                    m_conf.set(oss.str(), "INFO");
+                }
+                m_numCategories++;
+            }
+
+            void reconfigure() {
+                karabo::log::Logger::configure(m_conf);
+            }
+
+
+        };
+
+    }
+}
+
+#endif	/* TRACER_HH */
+
