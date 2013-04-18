5e35efc3a5d2de6558e08dc552a27e3a7c0beb88
heisenb <burkard.heisen@xfel.eu>
2013-04-18 10:06:12 +0000

    Updated xms.hpp
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/branches/hash-v2@9407 a23ab9f9-1ea2-4934-960d-990259f9ea8c

---
+++ src/karabo/xms.hpp
@@ -0,0 +1,17 @@
+/*
+ * $Id$
+ *
+ * Author: <irina.kozlova@xfel.eu>
+ * 
+ * Created on December 21, 2011, 11:17 AM
+ *
+ * Copyright (C) European XFEL GmbH Hamburg. All rights reserved.
+ */
+
+#ifndef KARABO_XMS_HPP
+#define	KARABO_XMS_HPP
+
+#include "SignalSlotable.hh"
+
+#endif
+
