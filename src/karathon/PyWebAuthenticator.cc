daf99a5ef9ab143bbdeda3a1f97999fcd7554577
irinak <irinak@a23ab9f9-1ea2-4934-960d-990259f9ea8c>
2013-04-19 12:46:21 +0000

    updated binding for Authenticator
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/branches/hash-v2@9463 a23ab9f9-1ea2-4934-960d-990259f9ea8c

--- src/karathon/PyWebAuthenticator.cc
+++ src/karathon/PyWebAuthenticator.cc
@@ -21,11 +21,54 @@ void exportPyWebAuthenticator() {
     
     bp::class_<karabo::util::Timestamp>("Timestamp", bp::init<>());
     
-    bp::class_<Authenticator>( "Authenticator", bp::init<string const &, string const &, string const &, string const &, string const &, string const &, string const & >(( bp::arg("username"), bp::arg("password"), bp::arg("provider"), bp::arg("ipAddress"), bp::arg("hostname"), bp::arg("portNumber"), bp::arg("software") )) )
-        .def("login"
+    bp::class_<Authenticator> a( "Authenticator", bp::init<string const &, string const &, string const &, string const &, string const &, string const &, string const & >(( bp::arg("username"), bp::arg("password"), bp::arg("provider"), bp::arg("ipAddress"), bp::arg("hostname"), bp::arg("portNumber"), bp::arg("software") )) );
+        a.def("login"
             , (bool (Authenticator::*)(Timestamp const &))(&Authenticator::login)
-            , bp::arg("timestamp") = Timestamp())    
-        .def("logout"
-            , (bool (Authenticator::*)())(&Authenticator::logout))
-        ;
+            , bp::arg("timestamp") = Timestamp());    
+        a.def("logout"
+            , (bool (Authenticator::*)())(&Authenticator::logout));
+        
+        a.def("getSingleSignOn"
+            , (string (Authenticator::*)(const string) const)(&Authenticator::getSingleSignOn)
+            , bp::arg("ipAddress"));
+
+       a.def("getSessionToken"
+            , (string (Authenticator::*)() const)(&Authenticator::getSessionToken));
+     
+       a.def("getSoftware"
+            , (string (Authenticator::*)() const)(&Authenticator::getSoftware));
+       
+       a.def("getPortNumber"
+           , (string (Authenticator::*)() const)(&Authenticator::getPortNumber));
+       
+       a.def("getHostname"
+           , (string (Authenticator::*)() const)(&Authenticator::getHostname));
+       
+       a.def("getIpAddress"
+           , (string (Authenticator::*)() const)(&Authenticator::getIpAddress));
+       
+       a.def("getProvider"
+           , (string (Authenticator::*)() const)(&Authenticator::getProvider));
+       
+       a.def("getPassword"
+           , (string (Authenticator::*)() const)(&Authenticator::getPassword));
+
+       a.def("getUsername"
+           , (string (Authenticator::*)() const)(&Authenticator::getUsername));
+       
+       a.def("getRoleDesc"
+           , (string (Authenticator::*)() const)(&Authenticator::getRoleDesc));
+       
+       a.def("getWelcomeMessage"
+           , (string (Authenticator::*)() const)(&Authenticator::getWelcomeMessage));
+       
+       a.def("getRoleId"
+           , (unsigned long long (Authenticator::*)() const)(&Authenticator::getRoleId));
+       
+       a.def("getSoftwareId"
+           , (unsigned long long (Authenticator::*)() const)(&Authenticator::getSoftwareId));
+
+       a.def("getUserId"
+           , (unsigned long long (Authenticator::*)() const)(&Authenticator::getUserId));
+
 }
