9a3c71d51d08125ab3cd486400f246bc1b8c3ed2
esenov <sergey.esenov@xfel.eu>
2013-04-25 15:15:52 +0000

    Fix SchemaXsdSerializer and SchemaXmlSerializer; add default configurations for serializers (empty input configuration means the default one)
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/branches/hash-v2@9529 a23ab9f9-1ea2-4934-960d-990259f9ea8c

--- src/karathon/PyIoSerialization.cc
+++ src/karathon/PyIoSerialization.cc
@@ -21,20 +21,54 @@ namespace bp = boost::python;
 
 namespace karabo {
     namespace io {
-        
+
         class HashXmlSerializerWrap {
+
         public:
-            
+
+
             static bp::object save(HashXmlSerializer& s, const karabo::util::Hash& hash) {
                 std::string archive;
                 s.save(hash, archive);
                 return bp::object(archive);
             }
-            
+
+
             static bp::object load(HashXmlSerializer& s, const std::string& archive) {
                 Hash hash;
                 s.load(hash, archive);
                 return bp::object(hash);
+            }
+        };
+
+        class SchemaXsdSerializerWrap {
+
+        public:
+
+
+            static bp::object save(SchemaXsdSerializer& s, const karabo::util::Schema& schema) {
+                std::string archive;
+                s.save(schema, archive);
+                return bp::object(archive);
+            }
+        };
+
+        class SchemaXmlSerializerWrap {
+
+        public:
+
+
+            static bp::object save(SchemaXmlSerializer& s, const karabo::util::Schema& schema) {
+                std::string archive;
+                s.save(schema, archive);
+                return bp::object(archive);
+            }
+
+
+            static bp::object load(SchemaXmlSerializer& s, const std::string& archive) {
+                Schema schema;
+                s.load(schema, archive);
+                return bp::object(schema);
             }
         };
     }
@@ -44,30 +78,43 @@ namespace karabo {
 void exportPyIoSerialization() {
 
     {//exposing karabo::io::HashXmlSerializer
-
-        bp::class_< HashXmlSerializer > h("HashXmlSerializer", bp::init<Hash const &>((bp::arg("hash"))));
-            h.def("save"
+        Hash config("writeDataTypes", true,
+                    "readDataTypes", true,
+                    "indentation", -1,
+                    "xmlns", "http://xfel.eu/config",
+                    "prefix", "KRB_",
+                    "insertXmlNamespace", true);
+        bp::class_< HashXmlSerializer > h("HashXmlSerializer", bp::init<Hash const &>((bp::arg("configuration") = config)));
+        h.def("save"
               , &HashXmlSerializerWrap().save
               , (bp::arg("hash")));
-            h.def("load"
+        h.def("load"
               , &HashXmlSerializerWrap().load
               , (bp::arg("archive")));
     }
 
     {//exposing karabo::io::SchemaXsdSerializer
-
-        bp::class_< SchemaXsdSerializer > s("SchemaXsdSerializer", bp::init<Hash const &>((bp::arg("input"))));
-            s.def("save"
-              , (void (SchemaXsdSerializer::*)(Schema const &, string &))(&SchemaXsdSerializer::save)
-              , (bp::arg("object"), bp::arg("archive")));
+        Hash config("indentation", -1, "xmlns", "http://www.w3.org/2001/XMLSchema", "xmlnsa", "http://www.karabo.eu");
+        bp::class_< SchemaXsdSerializer > s("SchemaXsdSerializer", bp::init<Hash const &>((bp::arg("configuration") = config)));
+        s.def("save"
+              , &SchemaXsdSerializerWrap().save
+              , (bp::arg("schema")));
     }
 
     {//exposing karabo::io::SchemaXmlSerializer
-
-        bp::class_< SchemaXmlSerializer > s("SchemaXmlSerializer", bp::init<Hash const &>((bp::arg("hash"))));
-            s.def("save"
-              , (void (SchemaXmlSerializer::*)(Schema const &, string &))(&SchemaXmlSerializer::save)
-              , (bp::arg("object"), bp::arg("archive")));
+        Hash config("writeDataTypes", true,
+                    "readDataTypes", true,
+                    "indentation", -1,
+                    "xmlns", "http://xfel.eu/config",
+                    "prefix", "KRB_",
+                    "insertXmlNamespace", true);
+        bp::class_< SchemaXmlSerializer > s("SchemaXmlSerializer", bp::init<Hash const &>((bp::arg("configuration") = config)));
+        s.def("save"
+              , &SchemaXmlSerializerWrap().save
+              , (bp::arg("schema")));
+        s.def("load"
+              , &SchemaXmlSerializerWrap().load
+              , (bp::arg("archive")));
     }
 
 
