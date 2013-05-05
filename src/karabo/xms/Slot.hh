68c9042bb070e444f8e4dab6a0412d6130a0824c
heisenb <heisenb@a23ab9f9-1ea2-4934-960d-990259f9ea8c>
2013-05-05 12:53:11 +0000

    Preparing to send session information via Signal and Slots
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/branches/hash-v2@9599 a23ab9f9-1ea2-4934-960d-990259f9ea8c

--- src/karabo/xms/Slot.hh
+++ src/karabo/xms/Slot.hh
@@ -34,7 +34,7 @@ namespace karabo {
             std::string m_userIdOfSender;
             std::string m_roleOfSender;
             std::string m_sessionTokenOfSender;
-
+            
         public:
 
             const std::string& getInstanceIdOfSender() const;
