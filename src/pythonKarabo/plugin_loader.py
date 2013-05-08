98bb7c509faaf87e810c1312362cd9b1ef918965
esenov <esenov@a23ab9f9-1ea2-4934-960d-990259f9ea8c>
2013-05-08 16:29:42 +0000

    Revision of python device_server & python_device in progress.  Introduce plugin_loader as configurable class
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/trunk@9715 a23ab9f9-1ea2-4934-960d-990259f9ea8c

---
+++ src/pythonKarabo/plugin_loader.py
@@ -0,0 +1,43 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+__author__="Sergey Esenov <serguei.essenov at xfel.eu>"
+__date__ ="$May 8, 2013 12:55:50 PM$"
+
+from karabo_decorators import *
+
+
+@KARABO_CONFIGURATION_BASE_CLASS
+@KARABO_CLASSINFO("PythonPluginLoader", "1.0")
+class PluginLoader(object):
+
+    @staticmethod
+    def expectedParameters(expected):
+        
+        e = STRING_ELEMENT(expected).key("pluginDirectory")
+        e.displayedName("Plugin Directory").description("Directory to search for plugins")
+        e.assignmentOptional().defaultValue(os.environ['PWD'] + "/plugins")
+        e.isDirectory().advanced().commit()
+        
+    def __init__(self, input):
+        if "pluginDirectory" in input:
+            self.plugins = input["pluginDirectory"]
+        else:
+            self.plugins = os.environ['PWD'] + "/plugins"
+        sys.path.append(self.plugins)
+        self.pattern = re.compile(r'.py')
+
+    def getPluginDirectory(self):
+        return self.plugins
+
+    def update(self):
+        matches = filter(lambda m: not m is None, [self.pattern.search(x) for x in os.listdir(self.plugins)])
+        modules = list()
+        for m in matches:
+            name = m.string[0:m.start()]              # module name
+            path = self.plugins + "/" + m.string      # path to module source
+            if path.endswith(".pyc"):                 # skip compiled version
+                continue
+            modules.append((name, path,))
+        return modules
+
