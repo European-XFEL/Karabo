49ccd97a3fa280ed58976586adcbfb891a486701
wegerk <wegerk@a23ab9f9-1ea2-4934-960d-990259f9ea8c>
2013-05-14 16:58:39 +0000

    Added netbeans project for pythonGui2
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/trunk@9734 a23ab9f9-1ea2-4934-960d-990259f9ea8c

---
+++ build/netbeans/pythonGui2/build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+CWD=$(pwd)
+DIST=dist/$(uname -s)
+mkdir -p $DIST/lib
+mkdir -p $DIST/bin
+cd $DIST/bin
+cat > karabo-gui <<End-of-file
+#
+# This file was automatically generated. Do not edit.
+#
+# Author: <burkhard.heisen@xfel.eu>
+#
+CWD=\$(pwd)
+cd \$(dirname \$0)/../lib/pythonGui
+export PYTHONPATH=../
+export LD_LIBRARY_PATH=../../extern/lib
+export DYLD_LIBRARY_PATH=../../extern/lib
+export PATH=../../extern/bin
+python karabo-gui.py
+cd \$CWD
+
+End-of-file
+chmod u+x karabo-gui
+cd ../lib
+cp -rf ../../../../../../src/pythonGui .
+rm -f `find . -type f -name *.pyc`
+rm -rf `find . -type d -name .svn`
+cd $CWD
