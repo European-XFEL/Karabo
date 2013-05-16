8ea4c8213a01271c951387994592abaad3335049
esenov <sergey.esenov@xfel.eu>
2013-05-16 16:09:46 +0000

    Add __karabo_cpp_classid__ for Logger to get access to C++ schema
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/trunk@9752 a23ab9f9-1ea2-4934-960d-990259f9ea8c

--- src/karathon/PyLogLogger.cc
+++ src/karathon/PyLogLogger.cc
@@ -8,6 +8,7 @@
 
 #include "boost/python.hpp"
 #include <karabo/log/Logger.hh>
+#include "PythonFactoryMacros.hh"
 
 namespace bp = boost::python;
 using namespace karabo::log;
@@ -15,57 +16,61 @@ using namespace karabo::util;
 using namespace log4cpp;
 using namespace std;
 
+
 void exportPyLogLogger() {
-    
+
     {//log4cpp::Category
-        bp::class_< log4cpp::Category, boost::noncopyable > ct("Category", bp::no_init);
+        bp::class_< log4cpp::Category, boost::noncopyable > ct("Category", bp::no_init)
+                ;
 
         ct.def("getInstance"
-                , (log4cpp::Category & (*)(string const &))(&log4cpp::Category::getInstance)
-                , bp::arg("name")
-                , bp::return_internal_reference<> ()).staticmethod("getInstance");
- 
+               , (log4cpp::Category & (*)(string const &))(&log4cpp::Category::getInstance)
+               , bp::arg("name")
+               , bp::return_internal_reference<> ()).staticmethod("getInstance");
+
         ct.def("getName"
-                , (string const & (log4cpp::Category::*)()const) (&log4cpp::Category::getName)
-                , bp::return_value_policy<bp::copy_const_reference > ());
+               , (string const & (log4cpp::Category::*)()const) (&log4cpp::Category::getName)
+               , bp::return_value_policy<bp::copy_const_reference > ());
 
         ct.def("getAdditivity"
-                , (bool (log4cpp::Category::*)()const) (&log4cpp::Category::getAdditivity));
+               , (bool (log4cpp::Category::*)()const) (&log4cpp::Category::getAdditivity));
 
         ct.def("getChainedPriority"
-                , (int (log4cpp::Category::*)()const) (&log4cpp::Category::getChainedPriority));
+               , (int (log4cpp::Category::*)()const) (&log4cpp::Category::getChainedPriority));
 
         ct.def("WARN"
-                , (void (log4cpp::Category::*)(string const &))(&log4cpp::Category::warn)
-                , bp::arg("message"));
+               , (void (log4cpp::Category::*)(string const &))(&log4cpp::Category::warn)
+               , bp::arg("message"));
 
         ct.def("DEBUG"
-                , (void (log4cpp::Category::*)(string const &))(&log4cpp::Category::debug)
-                , bp::arg("message"));
+               , (void (log4cpp::Category::*)(string const &))(&log4cpp::Category::debug)
+               , bp::arg("message"));
 
         ct.def("INFO"
-                , (void (log4cpp::Category::*)(string const &))(&log4cpp::Category::info)
-                , bp::arg("message"));
+               , (void (log4cpp::Category::*)(string const &))(&log4cpp::Category::info)
+               , bp::arg("message"));
 
         ct.def("ERROR"
-                , (void (log4cpp::Category::*)(string const &))(&log4cpp::Category::error)
-                , bp::arg("message"));
+               , (void (log4cpp::Category::*)(string const &))(&log4cpp::Category::error)
+               , bp::arg("message"));
+
     }
 
     {//karabo::log::Logger
-        bp::class_< Logger > log("Logger", bp::init<Hash const &>((bp::arg("input"))));
-            log.def("configure"
-                , &Logger::configure
-                , (bp::arg("config") = Hash())).staticmethod("configure");
-            
-            log.def("reset", &Logger::reset).staticmethod("reset");
-            
-            log.def("getLogger"
-                , (log4cpp::Category& (*)(string const &))(&Logger::getLogger)
-                , (bp::arg("logCategorie"))
-                , bp::return_internal_reference<> ()).staticmethod("getLogger");
-                
+        bp::class_< Logger >("Logger", bp::init<Hash const &>((bp::arg("input"))))
+                KARABO_PYTHON_FACTORY_CONFIGURATOR(Logger)
+                .def("configure"
+                     , &Logger::configure
+                     , (bp::arg("config") = Hash())).staticmethod("configure")
+                .def("reset", &Logger::reset).staticmethod("reset")
+                .def("getLogger"
+                     , (log4cpp::Category & (*)(string const &))(&Logger::getLogger)
+                     , (bp::arg("logCategorie"))
+                     , bp::return_internal_reference<> ()).staticmethod("getLogger")
+                .attr("__karabo_cpp_classid__") = Logger::classInfo().getClassId()
+                ;
+
         bp::register_ptr_to_python< boost::shared_ptr< Logger > >();
     }
-    
+
 }
