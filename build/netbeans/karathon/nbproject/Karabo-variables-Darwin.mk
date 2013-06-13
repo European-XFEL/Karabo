cc0af8f2668c5dfdc4a617b97805dba24a00a241
heisenb <heisenb@a23ab9f9-1ea2-4934-960d-990259f9ea8c>
2013-06-13 17:37:31 +0000

    MacOSX Makefiles
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/trunk@10062 a23ab9f9-1ea2-4934-960d-990259f9ea8c

---
+++ build/netbeans/karathon/nbproject/Karabo-variables-Darwin.mk
@@ -0,0 +1,43 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-MacOSX
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-MacOSX
+CND_ARTIFACT_NAME_Debug=libkarathon.dylib
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-MacOSX/libkarathon.dylib
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-MacOSX/package
+CND_PACKAGE_NAME_Debug=libkarathon.dylib.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-MacOSX/package/libkarathon.dylib.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-MacOSX
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-MacOSX
+CND_ARTIFACT_NAME_Release=libkarathon.dylib
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-MacOSX/libkarathon.dylib
+CND_PACKAGE_DIR_Release=dist/Release/GNU-MacOSX/package
+CND_PACKAGE_NAME_Release=libkarathon.dylib.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-MacOSX/package/libkarathon.dylib.tar
+# Debug-MacOSX configuration
+CND_PLATFORM_Debug-MacOSX=GNU-MacOSX
+CND_ARTIFACT_DIR_Debug-MacOSX=dist/Debug-MacOSX/GNU-MacOSX
+CND_ARTIFACT_NAME_Debug-MacOSX=libkarathon.dylib
+CND_ARTIFACT_PATH_Debug-MacOSX=dist/Debug-MacOSX/GNU-MacOSX/libkarathon.dylib
+CND_PACKAGE_DIR_Debug-MacOSX=dist/Debug-MacOSX/GNU-MacOSX/package
+CND_PACKAGE_NAME_Debug-MacOSX=libkarathon.dylib.tar
+CND_PACKAGE_PATH_Debug-MacOSX=dist/Debug-MacOSX/GNU-MacOSX/package/libkarathon.dylib.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
