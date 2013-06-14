534eb830d4cd6a08dcbbb3532ce3d9820b0307aa
heisenb <burkard.heisen@xfel.eu>
2013-06-14 08:28:38 +0000

    Added number of core identification for MacOSX
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/trunk@10076 a23ab9f9-1ea2-4934-960d-990259f9ea8c

--- auto_build_all.sh
+++ auto_build_all.sh
@@ -50,7 +50,7 @@ else
     exit 1
 fi
 # Check whether to skip sys deps
-if [ $2 = "--skip" ]; then
+if [[ $2 = "--skip" ]]; then
     SKIP="y"
 else 
     SKIP="n"
@@ -63,12 +63,15 @@ if [ "$OS" = "Linux" ]; then
     DISTRO_ID=( $(lsb_release -is) )
     DISTRO_RELEASE=$(lsb_release -rs)
     NUM_CORES=`grep "processor" /proc/cpuinfo | wc -l`
-    if [ "$NUM_CORES" -gt "8" ]; then NUM_CORES=8; fi
 elif [ "$OS" = "Darwin" ]; then
     DISTRO_ID=MacOSX
     DISTRO_RELEASE=$(uname -r)
     CONF=${CONF}-MacOSX
+    NUM_CORES=`sysctl hw.ncpu | awk '{print $2}'`
 fi
+
+# Cut the total number to ensure memory fitness
+if [ "$NUM_CORES" -gt "8" ]; then NUM_CORES=8; fi
 
 if [ "$SKIP" = "n" ]; then
     echo 
