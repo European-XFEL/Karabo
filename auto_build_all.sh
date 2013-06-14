088082fd9389bc7231e466640a309c37c2870f6b
heisenb <heisenb@a23ab9f9-1ea2-4934-960d-990259f9ea8c>
2013-06-14 06:38:10 +0000

    Improvements to the auto_build_all.sh script
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/trunk@10073 a23ab9f9-1ea2-4934-960d-990259f9ea8c

--- auto_build_all.sh
+++ auto_build_all.sh
@@ -13,6 +13,19 @@ if [ $? -ne 0 ]; then
     exit 1;
 fi
 
+# Fetch configuration type (Release or Debug)
+if [ $# -gt 1 ]; then
+    if [ $1 = "Release" -o $1 = "Debug" ]
+	CONF=$1
+    else
+	echo
+	echo "Invalid option supplied. Allowed options: [Release|Debug]"
+	echo
+    fi
+else
+CONF="Debug" # default
+fi
+
 # Get some information about our system
 OS=$(uname -s)
 NUM_CORES=2
@@ -24,24 +37,31 @@ if [ "$OS" = "Linux" ]; then
 elif [ "$OS" = "Darwin" ]; then
     DISTRO_ID=MacOSX
     DISTRO_RELEASE=$(uname -r)
+    CONF=${CONF}-MacOSX
 fi
 
-echo -e "\n\n### Fetching dependencies from ${DISTRO_ID}'s package management system. ###\n\n"
+echo 
+echo "### Fetching dependencies from ${DISTRO_ID}'s package management system. ###"
+echo
+
 sleep 2
 
 # Platform specific sections here
 if [ "$DISTRO_ID" == "Ubuntu" ]; then
     sudo apt-get install subversion build-essential doxygen libqt4-dev libnss3-dev libnspr4-dev libreadline-dev libsqlite3-dev libX11-dev zlib1g-dev libXext-dev gfortran liblapack-dev
 elif [ "$DISTRO_ID" == "Scientific" ]; then
-    echo -e "\n\n### install necessary devel packages: ###\n yum install sqlite-devel sqlite \n yum --enablerepo=epel install qtwebkit-devel \n\n"
+    echo "### install necessary devel packages: ###\n yum install sqlite-devel sqlite \n yum --enablerepo=epel install qtwebkit-devel"
 fi
 
-echo -e "\n\n### Starting compilation (using $NUM_CORES threads) and packaging of the karaboFramework. ###\n\n"
+echo
+echo "### Starting compilation (using $NUM_CORES threads) and packaging of the karaboFramework. ###"
+echo
+
 sleep 2
 
 cd build/netbeans/karabo
-make -j$NUM_CORES 
-make package
+make CONF=$CONF -j$NUM_CORES 
+make CONF=$CONF package 
 
-echo -e "\n\n### Successfully finished building and packaging of karaboFramework. ###\n\n"
+echo "### Successfully finished building and packaging of karaboFramework. ###"
 
