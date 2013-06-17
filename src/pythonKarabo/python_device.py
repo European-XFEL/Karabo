95885fa187547a1422c84d31b57ed182671dbbeb
esenov <sergey.esenov@xfel.eu>
2013-06-17 08:28:34 +0000

    Refactoring...
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/trunk@10107 a23ab9f9-1ea2-4934-960d-990259f9ea8c

--- src/pythonKarabo/python_device.py
+++ src/pythonKarabo/python_device.py
@@ -51,7 +51,7 @@ class PythonDevice(BaseFsm):
     def __init__(self, configuration):
         if configuration is None:
             raise ValueError,"Configuration must be Hash object, not None"
-        print "PythonDevice constructor: Input configuration after being validated is ...\n", configuration
+        #print "PythonDevice constructor: Input configuration after being validated is ...\n", configuration
         super(PythonDevice, self).__init__(configuration)
 
         self.parameters = configuration
@@ -407,7 +407,7 @@ def launchPythonDevice():
         configuration = config[classid]
     else:
         configuration = Hash()
-    print "launchPythonDevice: input configuration before being validated is ...\n", configuration
+    #print "launchPythonDevice: input configuration before being validated is ...\n", configuration
     try:
         module = __import__(modname)
         device = PythonDevice.create(classid, configuration)
