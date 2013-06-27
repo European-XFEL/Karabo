6b0e26dfab3b860a88d2d935495aeaf0ddeff78d
irinak <irina.kozlova@xfel.eu>
2013-06-27 09:50:41 +0000

    new binding for Timestamp2
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/trunk@10203 a23ab9f9-1ea2-4934-960d-990259f9ea8c

---
+++ src/karathon/PyUtilTimestamp2.cc
@@ -0,0 +1,49 @@
+/*
+ * $Id$
+ *
+ * Author: <irina.kozlova@xfel.eu>
+ *
+ * Copyright (C) European XFEL GmbH Hamburg. All rights reserved.
+ */
+#include <boost/python.hpp>
+
+#include <karabo/util/Timestamp2.hh>
+#include <karabo/util/Epochstamp.hh>
+#include <karabo/util/Trainstamp.hh>
+
+namespace bp = boost::python;
+using namespace karabo::util;
+using namespace std;
+
+void exportPyUtilTimestamp2() {
+       
+bp::class_<Timestamp2> ts("Timestamp2", bp::init<Epochstamp const &, Trainstamp const &>((bp::arg("e"), bp::arg("t"))));
+    
+    ts.def("getSeconds"
+           , (unsigned long long const & (Timestamp2::*)() const)(&Timestamp2::getSeconds)
+           , bp::return_value_policy< bp::copy_const_reference >() );
+    
+    ts.def("getFractionalSeconds"
+           , (unsigned long long const & (Timestamp2::*)() const)(&Timestamp2::getFractionalSeconds)
+           , bp::return_value_policy< bp::copy_const_reference >() );
+    
+    ts.def("getTrainId"
+           , (unsigned long long const & (Timestamp2::*)() const)(&Timestamp2::getTrainId)
+           , bp::return_value_policy< bp::copy_const_reference >() );
+    
+    ts.def("fromHashAttributes"
+           , (Timestamp2 (*)(Hash::Attributes const) )(&Timestamp2::fromHashAttributes)
+           , bp::arg("attributes") );
+    ts.staticmethod("fromHashAttributes");
+    
+    ts.def("toIso8601"
+           , (string (Timestamp2::*)() const)(&Timestamp2::toIso8601));
+    
+    ts.def("toHashAttributes"
+           , (void (Timestamp2::*)(Hash::Attributes &) const)(&Timestamp2::toHashAttributes)
+           , bp::arg("attributes") );
+    
+    ts.def("toFormattedString"
+           , (string (Timestamp2::*)(const string &) const)(&Timestamp2::toFormattedString)
+           , bp::arg("format") );
+}
