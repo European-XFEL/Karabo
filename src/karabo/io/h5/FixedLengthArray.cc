3339bd81ea6dcb1dd7bd375aeea49a99a3f5cb48
wrona <wrona@a23ab9f9-1ea2-4934-960d-990259f9ea8c>
2013-06-28 13:34:37 +0000

    added factory registration for vector<char> which is now supported for h5
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/trunk@10228 a23ab9f9-1ea2-4934-960d-990259f9ea8c

--- src/karabo/io/h5/FixedLengthArray.cc
+++ src/karabo/io/h5/FixedLengthArray.cc
@@ -5,29 +5,30 @@
  *
  * Copyright (C) European XFEL GmbH Hamburg. All rights reserved.
  */
- 
- 
+
+
 #include "FixedLengthArray.hh"
 using namespace karabo::io;
-      
-namespace karabo {  
-  namespace io { 
-    namespace h5 {
-             
-   
-    KARABO_REGISTER_FOR_CONFIGURATION(Element, Dataset, Int8ArrayElement)
-    KARABO_REGISTER_FOR_CONFIGURATION(Element, Dataset, Int16ArrayElement)
-    KARABO_REGISTER_FOR_CONFIGURATION(Element, Dataset, Int32ArrayElement)
-    KARABO_REGISTER_FOR_CONFIGURATION(Element, Dataset, Int64ArrayElement)
-    KARABO_REGISTER_FOR_CONFIGURATION(Element, Dataset, UInt8ArrayElement)
-    KARABO_REGISTER_FOR_CONFIGURATION(Element, Dataset, UInt16ArrayElement)
-    KARABO_REGISTER_FOR_CONFIGURATION(Element, Dataset, UInt32ArrayElement)
-    KARABO_REGISTER_FOR_CONFIGURATION(Element, Dataset, UInt64ArrayElement)
-    KARABO_REGISTER_FOR_CONFIGURATION(Element, Dataset, BoolArrayElement)
-    KARABO_REGISTER_FOR_CONFIGURATION(Element, Dataset, StringArrayElement)
-    KARABO_REGISTER_FOR_CONFIGURATION(Element, Dataset, FloatArrayElement)
-    KARABO_REGISTER_FOR_CONFIGURATION(Element, Dataset, DoubleArrayElement)
-                   
+
+namespace karabo {
+    namespace io {
+        namespace h5 {
+
+
+            KARABO_REGISTER_FOR_CONFIGURATION(Element, Dataset, CharArrayElement)
+            KARABO_REGISTER_FOR_CONFIGURATION(Element, Dataset, Int8ArrayElement)
+            KARABO_REGISTER_FOR_CONFIGURATION(Element, Dataset, Int16ArrayElement)
+            KARABO_REGISTER_FOR_CONFIGURATION(Element, Dataset, Int32ArrayElement)
+            KARABO_REGISTER_FOR_CONFIGURATION(Element, Dataset, Int64ArrayElement)
+            KARABO_REGISTER_FOR_CONFIGURATION(Element, Dataset, UInt8ArrayElement)
+            KARABO_REGISTER_FOR_CONFIGURATION(Element, Dataset, UInt16ArrayElement)
+            KARABO_REGISTER_FOR_CONFIGURATION(Element, Dataset, UInt32ArrayElement)
+            KARABO_REGISTER_FOR_CONFIGURATION(Element, Dataset, UInt64ArrayElement)
+            KARABO_REGISTER_FOR_CONFIGURATION(Element, Dataset, BoolArrayElement)
+            KARABO_REGISTER_FOR_CONFIGURATION(Element, Dataset, StringArrayElement)
+            KARABO_REGISTER_FOR_CONFIGURATION(Element, Dataset, FloatArrayElement)
+            KARABO_REGISTER_FOR_CONFIGURATION(Element, Dataset, DoubleArrayElement)
+
+        }
     }
-  }
 }
