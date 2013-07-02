0741d1e3acc4254c80b57c5b1c78597be0f00f85
irinak <irina.kozlova@xfel.eu>
2013-07-02 16:14:26 +0000

    added binding for xip/Image and some Python unit tests
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/trunk@10267 a23ab9f9-1ea2-4934-960d-990259f9ea8c

---
+++ src/pythonKarabo/tests/image_test.py
@@ -0,0 +1,92 @@
+__author__="Irina Kozlova <irina.kozlova at xfel.eu>"
+__date__ ="$Jul 2, 2013$"
+
+import unittest
+from libkarathon import *
+
+class  Image_TestCase(unittest.TestCase):
+
+    def test_image_double(self):
+        try:
+            img = ImageDOUBLE(CPU)
+            self.assertTrue(img.isEmpty())
+            self.assertEqual(img.byteSize(), 0)
+        except Exception,e:
+            self.fail("test_image_double exception group 1: " + str(e))
+            
+        try:
+            img = ImageDOUBLE(CPU, 1024, 1024)
+            self.assertEqual(img.dimensionality(), 2)
+            self.assertEqual(img.dimX(), 1024)
+            self.assertEqual(img.dimY(), 1024)
+            self.assertEqual(img.dimZ(), 1)
+        except Exception,e:
+            self.fail("test_image_double exception group 2: " + str(e))
+            
+        try:
+            img = ImageDOUBLE(CPU, 10, 1, 1, 5.5)
+            self.assertEqual(img.dimensionality(), 1)
+            self.assertEqual(img.dimX(), 10)
+            self.assertEqual(img.dimY(), 1)
+            self.assertEqual(img.dimZ(), 1)
+            n = img.dimX()
+            for i in range(0,n):
+                self.assertEqual(img[i], 5.5)          
+        except Exception,e:
+            self.fail("test_image_double exception group 3: " + str(e))    
+        
+        try:
+            img = ImageDOUBLE(CPU, 4, 1, 1, "0,1,2,3", True)
+            self.assertEqual(img.dimensionality(), 1)
+            self.assertEqual(img.dimX(), 4)
+            self.assertEqual(img.dimY(), 1)
+            self.assertEqual(img.dimZ(), 1)
+            #n = img.dimX()
+            #for i in range(0,n):
+            #    self.assertEqual(img[i], i)
+        except Exception,e:
+            self.fail("test_image_double exception group 4: " + str(e))    
+        
+    def test_image_float(self):
+        try:
+            img = ImageFLOAT(CPU)
+            self.assertTrue(img.isEmpty())
+            self.assertEqual(img.byteSize(), 0)
+        except Exception,e:
+            self.fail("test_image_float exception group 1: " + str(e))
+            
+        try:
+            img = ImageFLOAT(CPU, 1024, 1024)
+            self.assertEqual(img.dimensionality(), 2)
+            self.assertEqual(img.dimX(), 1024)
+            self.assertEqual(img.dimY(), 1024)
+            self.assertEqual(img.dimZ(), 1)
+        except Exception,e:
+            self.fail("test_image_float exception group 2: " + str(e))
+            
+        try:
+            img = ImageFLOAT(CPU, 10, 1, 1, 5.5)
+            self.assertEqual(img.dimensionality(), 1)
+            self.assertEqual(img.dimX(), 10)
+            self.assertEqual(img.dimY(), 1)
+            self.assertEqual(img.dimZ(), 1)
+            n = img.dimX()
+            for i in range(0,n):
+                self.assertEqual(img[i], 5.5)          
+        except Exception,e:
+            self.fail("test_image_float exception group 3: " + str(e))    
+        
+        try:
+            img = ImageFLOAT(CPU, 4, 1, 1, "0,1,2,3", True)
+            self.assertEqual(img.dimensionality(), 1)
+            self.assertEqual(img.dimX(), 4)
+            self.assertEqual(img.dimY(), 1)
+            self.assertEqual(img.dimZ(), 1)
+            #n = img.dimX()
+            #for i in range(0,n):
+            #    self.assertEqual(img[i], i)
+        except Exception,e:
+            self.fail("test_image_float exception group 4: " + str(e))    
+    
+if __name__ == '__main__':
+    unittest.main()
