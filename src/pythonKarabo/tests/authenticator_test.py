0b1398ef08c2122124d906c9e5e257a9b4a6c4dd
maial <maial@a23ab9f9-1ea2-4934-960d-990259f9ea8c>
2013-07-04 16:52:03 +0000

    Correct Python tests according to new Classes.
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/trunk@10324 a23ab9f9-1ea2-4934-960d-990259f9ea8c

--- src/pythonKarabo/tests/authenticator_test.py
+++ src/pythonKarabo/tests/authenticator_test.py
@@ -17,15 +17,16 @@ class  Authenticator_TestCase(unittest.TestCase):
     def test_authenticator_correct_login(self):
         
         # Variables definition
-        username = "guest";
-        password = "guest";
+        username = "unitaryTests";
+        password = "karaboUnitaryTestsPass";
         provider = "LOCAL";
         ipAddress = "PythonUnitTestsIpAddress";
-        hostname = "127.0.0.1";
-        portNumber = "44444";
+        brokerHostname = "127.0.0.1";
+        brokerPortNumber = "4444";
+        brokerTopic = "topic";
         software = "Karabo";
         #
-        timeStr = "20130120T122059.259188123";
+        #timeStr = "20130120T122059.259188123";
         #karabo::util::Timestamp time = karabo::util::Timestamp(timeStr);        
         
         # Helper variables
@@ -34,22 +35,23 @@ class  Authenticator_TestCase(unittest.TestCase):
         
         # Check properties of empty Authenticator
         try:
-            a = Authenticator(username, password, provider, ipAddress, hostname, portNumber, software)
+            a = Authenticator(username, password, provider, ipAddress, brokerHostname, brokerPortNumber, brokerTopic)
             
             # Variables that should be correctly assigned
             self.assertEqual(a.getUsername(), username, "getUsername() don't match")
             self.assertEqual(a.getProvider(), provider, "getProvider() don't match")
             self.assertEqual(a.getIpAddress(), ipAddress, "getIpAddress() don't match")
-            self.assertEqual(a.getHostname(), hostname, "getHostname() don't match")
-            self.assertEqual(a.getPortNumber(), portNumber, "getPortNumber() don't match")
+            self.assertEqual(a.getBrokerHostname(), brokerHostname, "getBrokerHostname() don't match")
+            self.assertEqual(a.getBrokerPortNumber(), brokerPortNumber, "getBrokerPortNumber() don't match")
+            self.assertEqual(a.getBrokerTopic(), brokerTopic, "getBrokerTopic() don't match")
             self.assertEqual(a.getSoftware(), software, "getSoftware() don't match")
             
             # Variables that should be empty
-            self.assertEqual(a.getRoleDesc(), emptyString, "getRoleDesc() is not Empty")
+            self.assertEqual(a.getDefaultAccessLevelDesc(), emptyString, "getDefaultAccessLevelDesc() is not Empty")
             self.assertEqual(a.getWelcomeMessage(), emptyString, "getWelcomeMessage() is not Empty")
             self.assertEqual(a.getSessionToken(), emptyString, "getSessionToken() is not Empty")
             #
-            self.assertEqual(a.getRoleId(), -100, "getRoleId() is not undifined")
+            self.assertEqual(a.getDefaultAccessLevelId(), -100, "getDefaultAccessLevelId() is not undifined")
             self.assertEqual(a.getSoftwareId(), -100, "getSoftwareId() is not undifined")
             self.assertEqual(a.getUserId(), -100, "getUserId() is not undifined")
         except Exception, e:
@@ -63,18 +65,19 @@ class  Authenticator_TestCase(unittest.TestCase):
             self.assertEqual(a.getUsername(), username, "getUsername() don't match")
             self.assertEqual(a.getProvider(), provider, "getProvider() don't match")
             self.assertEqual(a.getIpAddress(), ipAddress, "getIpAddress() don't match")
-            self.assertEqual(a.getHostname(), hostname, "getHostname() don't match")
-            self.assertEqual(a.getPortNumber(), portNumber, "getPortNumber() don't match")
+            self.assertEqual(a.getBrokerHostname(), brokerHostname, "getBrokerHostname() don't match")
+            self.assertEqual(a.getBrokerPortNumber(), brokerPortNumber, "getBrokerPortNumber() don't match")
+            self.assertEqual(a.getBrokerTopic(), brokerTopic, "getBrokerTopic() don't match")
             self.assertEqual(a.getSoftware(), software, "getSoftware() don't match")
             
             # Variables that should be empty
-            self.assertEqual(a.getRoleDesc(), "GUEST_USER")
+            self.assertEqual(a.getDefaultAccessLevelDesc(), "USER")
             self.assertNotEqual(a.getWelcomeMessage(), emptyString, "getWelcomeMessage() is Empty")
             self.assertNotEqual(a.getSessionToken(), emptyString, "getSessionToken() is Empty")
             #
-            self.assertEqual(a.getRoleId(), 3, "getRoleId() is not undifined")
+            self.assertEqual(a.getDefaultAccessLevelId(), 1, "getDefaultAccessLevelId() is not undifined")
             self.assertEqual(a.getSoftwareId(), 1, "getSoftwareId() is not undifined")
-            self.assertEqual(a.getUserId(), -1, "getUserId() is not undifined")
+            self.assertEqual(a.getUserId(), -99, "getUserId() is not undifined")
         except Exception, e:
             self.fail(functionName + " exception after LOGIN: " + str(e))
         
@@ -86,16 +89,17 @@ class  Authenticator_TestCase(unittest.TestCase):
             self.assertEqual(a.getUsername(), username, "getUsername() don't match")
             self.assertEqual(a.getProvider(), provider, "getProvider() don't match")
             self.assertEqual(a.getIpAddress(), ipAddress, "getIpAddress() don't match")
-            self.assertEqual(a.getHostname(), hostname, "getHostname() don't match")
-            self.assertEqual(a.getPortNumber(), portNumber, "getPortNumber() don't match")
+            self.assertEqual(a.getBrokerHostname(), brokerHostname, "getBrokerHostname() don't match")
+            self.assertEqual(a.getBrokerPortNumber(), brokerPortNumber, "getBrokerPortNumber() don't match")
+            self.assertEqual(a.getBrokerTopic(), brokerTopic, "getBrokerTopic() don't match")
             self.assertEqual(a.getSoftware(), software, "getSoftware() don't match")
             
             # Variables that should be empty
-            self.assertEqual(a.getRoleDesc(), emptyString, "getRoleDesc() is not Empty")
+            self.assertEqual(a.getDefaultAccessLevelDesc(), emptyString, "getDefaultAccessLevelDesc() is not Empty")
             self.assertEqual(a.getWelcomeMessage(), emptyString, "getWelcomeMessage() is not Empty")
             self.assertEqual(a.getSessionToken(), emptyString, "getSessionToken() is not Empty")
             #
-            self.assertEqual(a.getRoleId(), -100, "getRoleId() is not undifined")
+            self.assertEqual(a.getDefaultAccessLevelId(), -100, "getDefaultAccessLevelId() is not undifined")
             self.assertEqual(a.getSoftwareId(), -100, "getSoftwareId() is not undifined")
             self.assertEqual(a.getUserId(), -100, "getUserId() is not undifined")
         except Exception, e:
@@ -105,15 +109,16 @@ class  Authenticator_TestCase(unittest.TestCase):
     def test_authenticator_incorrect_login(self):
         
         # Variables definition
-        username = "guest";
-        password = "guest2";
+        username = "unitaryTests";
+        password = "karaboUnitaryTestsPass222";
         provider = "LOCAL";
         ipAddress = "PythonUnitTestsIpAddress";
-        hostname = "127.0.0.1";
-        portNumber = "4444";
+        brokerHostname = "127.0.0.1";
+        brokerPortNumber = "4444";
+        brokerTopic = "topic";
         software = "Karabo";
         #
-        timeStr = "20130120T122059.259188123";
+        #timeStr = "20130120T122059.259188123";
         #karabo::util::Timestamp time = karabo::util::Timestamp(timeStr);        
         
         # Helper variables
@@ -122,22 +127,23 @@ class  Authenticator_TestCase(unittest.TestCase):
         
         # Check properties of empty Authenticator
         try:
-            a = Authenticator(username, password, provider, ipAddress, hostname, portNumber, software)
+            a = Authenticator(username, password, provider, ipAddress, brokerHostname, brokerPortNumber, brokerTopic)
             
             # Variables that should be correctly assigned
             self.assertEqual(a.getUsername(), username, "getUsername() don't match")
             self.assertEqual(a.getProvider(), provider, "getProvider() don't match")
             self.assertEqual(a.getIpAddress(), ipAddress, "getIpAddress() don't match")
-            self.assertEqual(a.getHostname(), hostname, "getHostname() don't match")
-            self.assertEqual(a.getPortNumber(), portNumber, "getPortNumber() don't match")
+            self.assertEqual(a.getBrokerHostname(), brokerHostname, "getBrokerHostname() don't match")
+            self.assertEqual(a.getBrokerPortNumber(), brokerPortNumber, "getBrokerPortNumber() don't match")
+            self.assertEqual(a.getBrokerTopic(), brokerTopic, "getBrokerTopic() don't match")
             self.assertEqual(a.getSoftware(), software, "getSoftware() don't match")
             
             # Variables that should be empty
-            self.assertEqual(a.getRoleDesc(), emptyString, "getRoleDesc() is not Empty")
+            self.assertEqual(a.getDefaultAccessLevelDesc(), emptyString, "getDefaultAccessLevelDesc() is not Empty")
             self.assertEqual(a.getWelcomeMessage(), emptyString, "getWelcomeMessage() is not Empty")
             self.assertEqual(a.getSessionToken(), emptyString, "getSessionToken() is not Empty")
             #
-            self.assertEqual(a.getRoleId(), -100, "getRoleId() is not undifined")
+            self.assertEqual(a.getDefaultAccessLevelId(), -100, "getDefaultAccessLevelId() is not undifined")
             self.assertEqual(a.getSoftwareId(), -100, "getSoftwareId() is not undifined")
             self.assertEqual(a.getUserId(), -100, "getUserId() is not undifined")
         except Exception, e:
@@ -151,16 +157,17 @@ class  Authenticator_TestCase(unittest.TestCase):
             self.assertEqual(a.getUsername(), username, "getUsername() don't match")
             self.assertEqual(a.getProvider(), provider, "getProvider() don't match")
             self.assertEqual(a.getIpAddress(), ipAddress, "getIpAddress() don't match")
-            self.assertEqual(a.getHostname(), hostname, "getHostname() don't match")
-            self.assertEqual(a.getPortNumber(), portNumber, "getPortNumber() don't match")
+            self.assertEqual(a.getBrokerHostname(), brokerHostname, "getBrokerHostname() don't match")
+            self.assertEqual(a.getBrokerPortNumber(), brokerPortNumber, "getBrokerPortNumber() don't match")
+            self.assertEqual(a.getBrokerTopic(), brokerTopic, "getBrokerTopic() don't match")
             self.assertEqual(a.getSoftware(), software, "getSoftware() don't match")
             
             # Variables that should be empty
-            self.assertEqual(a.getRoleDesc(), emptyString, "getRoleDesc() is not Empty")
+            self.assertEqual(a.getDefaultAccessLevelDesc(), emptyString, "getDefaultAccessLevelDesc() is not Empty")
             self.assertEqual(a.getWelcomeMessage(), emptyString, "getWelcomeMessage() is not Empty")
             self.assertEqual(a.getSessionToken(), emptyString, "getSessionToken() is not Empty")
             #
-            self.assertEqual(a.getRoleId(), -100, "getRoleId() is not undifined")
+            self.assertEqual(a.getDefaultAccessLevelId(), -100, "getDefaultAccessLevelId() is not undifined")
             self.assertEqual(a.getSoftwareId(), -100, "getSoftwareId() is not undifined")
             self.assertEqual(a.getUserId(), -100, "getUserId() is not undifined")
         except Exception, e:
@@ -170,8 +177,8 @@ class  Authenticator_TestCase(unittest.TestCase):
     def test_authenticator_single_sign_on(self):
         
         # Variables definition
-        username = "guest";
-        password = "guest";
+        username = "unitaryTests";
+        password = "karaboUnitaryTestsPass";
         provider = "LOCAL";
         ipAddress = "PythonUnitTestsIpAddress";
         ipAddressWrong = "PythonUnitTestsIpAddressXXXXXXXXXXX";
@@ -179,7 +186,7 @@ class  Authenticator_TestCase(unittest.TestCase):
         portNumber = "4444";
         software = "Karabo";
         #
-        timeStr = "20130120T122059.259188123";
+        #timeStr = "20130120T122059.259188123";
         #karabo::util::Timestamp time = karabo::util::Timestamp(timeStr);        
         
         # Helper variables
@@ -188,7 +195,7 @@ class  Authenticator_TestCase(unittest.TestCase):
         
         # Check properties of empty Authenticator (already validated in previous functions)
         try:
-            a = Authenticator(username, password, provider, ipAddress, hostname, portNumber, software)
+            a = Authenticator(username, password, provider, ipAddress, hostname, portNumber, brokerTopic)
         except Exception, e:
             self.fail(functionName + " creation exception before LOGIN: " + str(e))
         
