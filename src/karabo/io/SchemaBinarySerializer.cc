5d2ca68a623c9ef97e41be74e479a3fa88819828
boukhele <boukhele@a23ab9f9-1ea2-4934-960d-990259f9ea8c>
2013-07-10 13:58:00 +0000

    Schema serialization to binary
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/trunk@10419 a23ab9f9-1ea2-4934-960d-990259f9ea8c

---
+++ src/karabo/io/SchemaBinarySerializer.cc
@@ -0,0 +1,67 @@
+/* 
+ * File:   SchemaBinarySerializer.cc
+ * Author: <djelloul.boukhelef@xfel.eu>
+ * 
+ * Created on July 9, 2013, 8:59 AM
+ */
+
+#include <karabo/util/Configurator.hh>
+#include "HashBinarySerializer.hh"
+#include "SchemaBinarySerializer.hh"
+
+using namespace std;
+using namespace karabo::util;
+
+namespace karabo {
+    namespace io {
+
+
+        KARABO_REGISTER_FOR_CONFIGURATION(BinarySerializer<Schema>, SchemaBinarySerializer)
+
+        SchemaBinarySerializer::SchemaBinarySerializer(const Hash& input) {
+            m_serializer = BinarySerializer<Hash>::create("Bin", input);
+        }
+
+
+        SchemaBinarySerializer::~SchemaBinarySerializer() {
+        }
+
+
+        void SchemaBinarySerializer::expectedParameters(karabo::util::Schema& expected) {
+            HashBinarySerializer::expectedParameters(expected);
+        }
+
+        void SchemaBinarySerializer::save(const karabo::util::Schema& object, std::vector<char>& archive) try {
+            ostringstream os;
+            const string& rootName = object.getRootName();
+            const size_t size = rootName.size();
+            os.write((char*) &size, sizeof (size));
+            os.write(rootName.c_str(), size);
+
+            std::vector<char> v;
+            m_serializer->save(object.getParameterHash(), v);
+            string str = os.str();
+            archive.insert(archive.end(), str.c_str(), str.c_str() + str.length());
+            archive.insert(archive.end(), v.begin(), v.end());
+        } catch (...) {
+            std::clog << "Something wrong happend!!!" << std::endl;
+        }
+
+
+        void SchemaBinarySerializer::load(karabo::util::Schema& object, const char* archive, const size_t nBytes) {
+            std::stringstream is;
+            is.rdbuf()->pubsetbuf(const_cast<char*> (archive), nBytes);
+
+            size_t size;
+            is.read((char*) &size, sizeof (size));
+            char rootName[256];
+            is.read(rootName, size);
+            rootName[size] = 0;
+            object.setRootName(rootName);
+            Hash hash;
+            m_serializer->load(hash, archive + sizeof (size) + size, nBytes - sizeof (size) - size);
+            object.setParameterHash(hash);
+        }
+    }
+}
+
---
+++ src/karabo/io/SchemaBinarySerializer.hh
@@ -0,0 +1,39 @@
+/* 
+ * File:   SchemaBinarySerializer.hh
+ * Author: <djelloul.boukhelef@xfel.eu>
+ *
+ * Created on July 9, 2013, 8:58 AM
+ */
+
+#ifndef SCHEMABINARYSERIALIZER_HH
+#define	SCHEMABINARYSERIALIZER_HH
+
+#include <karabo/util/Schema.hh>
+
+#include "BinarySerializer.hh"
+
+namespace karabo {
+
+    namespace io {
+
+        class SchemaBinarySerializer : public BinarySerializer<karabo::util::Schema> {
+
+            BinarySerializer<karabo::util::Hash>::Pointer m_serializer;
+
+        public:
+
+            KARABO_CLASSINFO(SchemaBinarySerializer, "Bin", "1.0")
+
+            SchemaBinarySerializer(const karabo::util::Hash& hash);
+            virtual ~SchemaBinarySerializer();
+
+            static void expectedParameters(karabo::util::Schema& expected);
+
+            void save(const karabo::util::Schema& object, std::vector<char>& archive);
+            void load(karabo::util::Schema& object, const char* archive, const size_t nBytes);
+        };
+    }
+}
+
+#endif	/* SCHEMABINARYSERIALIZER_HH */
+
