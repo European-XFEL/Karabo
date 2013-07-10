c190fd399bcaf5787f5f559dc88a6a9659aefcd5
boukhele <boukhele@a23ab9f9-1ea2-4934-960d-990259f9ea8c>
2013-07-10 14:08:50 +0000

    - Add tests to Hash binary serializer - Add test for new Schema binary serializer - Add test for new Time stuff
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/trunk@10422 a23ab9f9-1ea2-4934-960d-990259f9ea8c

---
+++ src/karabo/tests/util/TimeClasses_Test.hh
@@ -0,0 +1,35 @@
+/* 
+ * File:   TimeClasses_Test.hh
+ * Author: boukhele
+ *
+ * Created on July 10, 2013, 2:35 PM
+ */
+
+#ifndef TIMECLASSES_TEST_HH
+#define	TIMECLASSES_TEST_HH
+
+#include <cppunit/extensions/HelperMacros.h>
+
+class TimeClasses_Test : public CPPUNIT_NS::TestFixture {
+    CPPUNIT_TEST_SUITE(TimeClasses_Test);
+    CPPUNIT_TEST(testEpochstamp);
+    CPPUNIT_TEST(testTimePeriod);
+    CPPUNIT_TEST(testTimeDuration);
+    CPPUNIT_TEST(testTimeProfiler);
+    CPPUNIT_TEST_SUITE_END();
+
+public:
+    TimeClasses_Test();
+    virtual ~TimeClasses_Test();
+    void setUp();
+    void tearDown();
+
+private:
+    void testEpochstamp();
+    void testTimePeriod();
+    void testTimeDuration();
+    void testTimeProfiler();
+};
+
+#endif	/* TIMECLASSES_TEST_HH */
+
