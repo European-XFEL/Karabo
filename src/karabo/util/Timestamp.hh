49cfb4e5e45fa68790608b875a6fa154b12cf0b1
irinak <irina.kozlova@xfel.eu>
2013-07-11 12:11:30 +0000

    class Timestamp updated  to a new version; improved layout of one message in PluginLoader.
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/trunk@10438 a23ab9f9-1ea2-4934-960d-990259f9ea8c

--- src/karabo/util/Timestamp.hh
+++ src/karabo/util/Timestamp.hh
@@ -1,78 +1,85 @@
-/*
+/* 
+ * File:   Timestamp.hh
+ * Author: WP76
  *
- * File:   Timestamp.cc
- * Author: <luis.maia@xfel.eu>
- *
- * Created on April 23, 2013, 11:53 AM
- *
- * Copyright (C) European XFEL GmbH Hamburg. All rights reserved.
+ * Created on June 19, 2013, 3:22 PM
  */
 
-#ifndef TIMESTAMP_HH
-#define	TIMESTAMP_HH
+#ifndef KARABO_UTIL_TIMESTAMP_HH
+#define	KARABO_UTIL_TIMESTAMP_HH
 
-#include <boost/date_time.hpp>
-#include <boost/date_time/c_local_time_adjustor.hpp>
+#include "Epochstamp.hh"
+#include "Trainstamp.hh"
+
 
 namespace karabo {
-
-    /**
-     * Namespace for package packageName
-     */
     namespace util {
 
         /**
-         * The Timestamp class.
-         * More detailed comments
+         * This class expresses a time point and holds it in form of an Epochstamp and Trainstamp
          */
         class Timestamp {
 
-            static boost::posix_time::ptime m_epoch;
-            boost::posix_time::ptime m_timePoint;
-            unsigned long long m_msSinceEpoch;
+            Epochstamp m_epochstamp;
+            Trainstamp m_trainstamp;
 
         public:
-            /**
-             * Constructors.
-             */
+
+            
             Timestamp();
-            Timestamp(const boost::posix_time::ptime pt);
-            Timestamp(const std::string& timeStr);
+                
+            Timestamp(const Epochstamp& e, const Trainstamp& t);
+            
+            inline const unsigned long long& getSeconds() const {
+                return m_epochstamp.getSeconds();
+            }
+            
+            inline const unsigned long long& getFractionalSeconds() const {
+                 return m_epochstamp.getFractionalSeconds();
+            }
+            
+            inline const unsigned long long& getTrainId() const {
+                return m_trainstamp.getTrainId();
+            }
+            
+            static bool hashAttributesContainTimeInformation(const Hash::Attributes attributes);
+           
+            /**
+             * Creates an Timestamp from three Hash attributes
+             * This function throws in case the attributes do no provide the correct information
+             * @param attributes Hash attributes
+             * @return Timestamp object
+             */
+            static Timestamp fromHashAttributes(const Hash::Attributes attributes);
+
+            /**
+             * Formats into ISO 8601
+             * @return ISO 8601 formatted string
+             */
+            std::string toIso8601() const;
+
+            /**
+             * Formats as Hash attributes
+             * @param attributes container to which the time point information is added
+             */
+            void toHashAttributes(Hash::Attributes& attributes) const;
+            
+            /**
+             * Formats to specified format
+             * @param format The format of the time point
+             * @return formated string
+             */
+            std::string toFormattedString(const std::string& format = "%Y-%b-%d %H:%M:%S") const;
+
+
             virtual ~Timestamp();
 
-            /*
-             * Getters and Setters
-             */
-            void setMsSinceEpoch(const unsigned long long msSinceEpoch);
-            void setTime(const boost::posix_time::ptime& timePoint);
-            void setTime(const std::string& timePoint);
-            //
-            boost::posix_time::ptime getTime() const;
-            unsigned long long getMsSinceEpoch() const;
-            unsigned long long getTrainId() const;
-
-            /*
-             * Class logic functions
-             */
-            std::string toString();
-            std::string toIsoString();
-            std::string toFormattedString(const std::string& format = "%Y-%b-%d %H:%M:%S");
-
-            static unsigned long long calculateAndReturnMsSinceEpoch(const boost::posix_time::ptime now);
-            //
-            boost::posix_time::ptime getStringFormated2PTime(const std::string& timestampStr, const boost::posix_time::time_input_facet* facet);
-            boost::posix_time::ptime getStringFormated2PTime(const std::string& timestampStr);
-            boost::posix_time::ptime getUniversalString2PTime(const std::string& timestampStr);
 
         private:
-            std::string toStandardString(const std::string& format);
-            std::string getPTime2String(const boost::posix_time::ptime pt, const boost::posix_time::time_facet* facet);
-
-            time_t ptime_to_time_t(const boost::posix_time::ptime t);
-            boost::posix_time::ptime local_ptime_from_utc_time_t(std::time_t const t);
+            
         };
+    }
+}
 
-    } // namespace packageName
-} // namespace karabo
+#endif
 
-#endif	/* TIMESTAMP_HH */
