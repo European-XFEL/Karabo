1746aec2726173f2ca32f60c50c394e19ede65f7
esenov <sergey.esenov@xfel.eu>
2013-07-13 19:49:21 +0000

    Replace absolute names of python interpreter path by '/usr/bin/env python'.
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/trunk@10501 a23ab9f9-1ea2-4934-960d-990259f9ea8c

--- build/netbeans/karabo/.bundle.sh
+++ build/netbeans/karabo/.bundle.sh
@@ -135,26 +135,47 @@ PYTHON_INTERPRETOR=`$PACKAGEDIR/extern/bin/python -c "import os,sys;print os.pat
 
 # Copying 'extern' has resulted in changing python interpreter path for scripts in 'bin' directory
  
-[ -f $PACKAGEDIR/extern/bin/ipython ]           && sed -i '1 s%^.*$%#!'${PYTHON_INTERPRETOR}'%g' $PACKAGEDIR/extern/bin/ipython  # <-- replace 1st line by proper interpreter path
-[ -f $PACKAGEDIR/extern/bin/ipython2.7-config ] && sed -i '1 s%^.*$%#!'${PYTHON_INTERPRETOR}'%g' $PACKAGEDIR/extern/bin/ipython2.7-config
-[ -f $PACKAGEDIR/extern/bin/2to3 ]              && sed -i '1 s%^.*$%#!'${PYTHON_INTERPRETOR}'%g' $PACKAGEDIR/extern/bin/2to3
-[ -f $PACKAGEDIR/extern/bin/cygdb ]             && sed -i '1 s%^.*$%#!'${PYTHON_INTERPRETOR}'%g' $PACKAGEDIR/extern/bin/cygdb
-[ -f $PACKAGEDIR/extern/bin/cython ]            && sed -i '1 s%^.*$%#!'${PYTHON_INTERPRETOR}'%g' $PACKAGEDIR/extern/bin/cython
-[ -f $PACKAGEDIR/extern/bin/guidata-tests ]     && sed -i '1 s%^.*$%#!'${PYTHON_INTERPRETOR}'%g' $PACKAGEDIR/extern/bin/guidata-tests
-[ -f $PACKAGEDIR/extern/bin/guiqwt-tests ]      && sed -i '1 s%^.*$%#!'${PYTHON_INTERPRETOR}'%g' $PACKAGEDIR/extern/bin/guiqwt-tests
-[ -f $PACKAGEDIR/extern/bin/idle ]              && sed -i '1 s%^.*$%#!'${PYTHON_INTERPRETOR}'%g' $PACKAGEDIR/extern/bin/idle
-[ -f $PACKAGEDIR/extern/bin/ipcluster ]         && sed -i '1 s%^.*$%#!'${PYTHON_INTERPRETOR}'%g' $PACKAGEDIR/extern/bin/ipcluster
-[ -f $PACKAGEDIR/extern/bin/ipcontroller ]      && sed -i '1 s%^.*$%#!'${PYTHON_INTERPRETOR}'%g' $PACKAGEDIR/extern/bin/ipcontroller
-[ -f $PACKAGEDIR/extern/bin/ipengine ]          && sed -i '1 s%^.*$%#!'${PYTHON_INTERPRETOR}'%g' $PACKAGEDIR/extern/bin/ipengine
-[ -f $PACKAGEDIR/extern/bin/iplogger ]          && sed -i '1 s%^.*$%#!'${PYTHON_INTERPRETOR}'%g' $PACKAGEDIR/extern/bin/iplogger
-[ -f $PACKAGEDIR/extern/bin/iptest ]            && sed -i '1 s%^.*$%#!'${PYTHON_INTERPRETOR}'%g' $PACKAGEDIR/extern/bin/iptest
-[ -f $PACKAGEDIR/extern/bin/ipython.py ]        && sed -i '1 s%^.*$%#!'${PYTHON_INTERPRETOR}'%g' $PACKAGEDIR/extern/bin/ipython.py
-[ -f $PACKAGEDIR/extern/bin/irunner ]           && sed -i '1 s%^.*$%#!'${PYTHON_INTERPRETOR}'%g' $PACKAGEDIR/extern/bin/irunner
-[ -f $PACKAGEDIR/extern/bin/nosetests ]         && sed -i '1 s%^.*$%#!'${PYTHON_INTERPRETOR}'%g' $PACKAGEDIR/extern/bin/nosetests
-[ -f $PACKAGEDIR/extern/bin/pycolor ]           && sed -i '1 s%^.*$%#!'${PYTHON_INTERPRETOR}'%g' $PACKAGEDIR/extern/bin/pycolor
-[ -f $PACKAGEDIR/extern/bin/pydoc ]             && sed -i '1 s%^.*$%#!'${PYTHON_INTERPRETOR}'%g' $PACKAGEDIR/extern/bin/pydoc
-[ -f $PACKAGEDIR/extern/bin/sift ]              && sed -i '1 s%^.*$%#!'${PYTHON_INTERPRETOR}'%g' $PACKAGEDIR/extern/bin/sift
-[ -f $PACKAGEDIR/extern/bin/smtpd.py ]          && sed -i '1 s%^.*$%#!'${PYTHON_INTERPRETOR}'%g' $PACKAGEDIR/extern/bin/smtpd.py
+[ -f $PACKAGEDIR/extern/bin/ipython ]           && sed -i '1 s%^.*$%#!/usr/bin/env python%g' $PACKAGEDIR/extern/bin/ipython  # <-- replace 1st line by proper interp path
+[ -f $PACKAGEDIR/extern/bin/ipython2.7-config ] && sed -i '1 s%^.*$%#!/usr/bin/env python%g' $PACKAGEDIR/extern/bin/ipython2.7-config
+[ -f $PACKAGEDIR/extern/bin/2to3 ]              && sed -i '1 s%^.*$%#!/usr/bin/env python%g' $PACKAGEDIR/extern/bin/2to3
+[ -f $PACKAGEDIR/extern/bin/cygdb ]             && sed -i '1 s%^.*$%#!/usr/bin/env python%g' $PACKAGEDIR/extern/bin/cygdb
+[ -f $PACKAGEDIR/extern/bin/cython ]            && sed -i '1 s%^.*$%#!/usr/bin/env python%g' $PACKAGEDIR/extern/bin/cython
+[ -f $PACKAGEDIR/extern/bin/guidata-tests ]     && sed -i '1 s%^.*$%#!/usr/bin/env python%g' $PACKAGEDIR/extern/bin/guidata-tests
+[ -f $PACKAGEDIR/extern/bin/guiqwt-tests ]      && sed -i '1 s%^.*$%#!/usr/bin/env python%g' $PACKAGEDIR/extern/bin/guiqwt-tests
+[ -f $PACKAGEDIR/extern/bin/idle ]              && sed -i '1 s%^.*$%#!/usr/bin/env python%g' $PACKAGEDIR/extern/bin/idle
+[ -f $PACKAGEDIR/extern/bin/ipcluster ]         && sed -i '1 s%^.*$%#!/usr/bin/env python%g' $PACKAGEDIR/extern/bin/ipcluster
+[ -f $PACKAGEDIR/extern/bin/ipcontroller ]      && sed -i '1 s%^.*$%#!/usr/bin/env python%g' $PACKAGEDIR/extern/bin/ipcontroller
+[ -f $PACKAGEDIR/extern/bin/ipengine ]          && sed -i '1 s%^.*$%#!/usr/bin/env python%g' $PACKAGEDIR/extern/bin/ipengine
+[ -f $PACKAGEDIR/extern/bin/iplogger ]          && sed -i '1 s%^.*$%#!/usr/bin/env python%g' $PACKAGEDIR/extern/bin/iplogger
+[ -f $PACKAGEDIR/extern/bin/iptest ]            && sed -i '1 s%^.*$%#!/usr/bin/env python%g' $PACKAGEDIR/extern/bin/iptest
+[ -f $PACKAGEDIR/extern/bin/ipython.py ]        && sed -i '1 s%^.*$%#!/usr/bin/env python%g' $PACKAGEDIR/extern/bin/ipython.py
+[ -f $PACKAGEDIR/extern/bin/irunner ]           && sed -i '1 s%^.*$%#!/usr/bin/env python%g' $PACKAGEDIR/extern/bin/irunner
+[ -f $PACKAGEDIR/extern/bin/nosetests ]         && sed -i '1 s%^.*$%#!/usr/bin/env python%g' $PACKAGEDIR/extern/bin/nosetests
+[ -f $PACKAGEDIR/extern/bin/pycolor ]           && sed -i '1 s%^.*$%#!/usr/bin/env python%g' $PACKAGEDIR/extern/bin/pycolor
+[ -f $PACKAGEDIR/extern/bin/pydoc ]             && sed -i '1 s%^.*$%#!/usr/bin/env python%g' $PACKAGEDIR/extern/bin/pydoc
+[ -f $PACKAGEDIR/extern/bin/sift ]              && sed -i '1 s%^.*$%#!/usr/bin/env python%g' $PACKAGEDIR/extern/bin/sift
+[ -f $PACKAGEDIR/extern/bin/smtpd.py ]          && sed -i '1 s%^.*$%#!/usr/bin/env python%g' $PACKAGEDIR/extern/bin/smtpd.py
+
+#[ -f $PACKAGEDIR/extern/bin/ipython ]           && sed -i '1 s%^.*$%#!'${PYTHON_INTERPRETOR}'%g' $PACKAGEDIR/extern/bin/ipython  # <-- replace 1st line by proper interp path
+#[ -f $PACKAGEDIR/extern/bin/ipython2.7-config ] && sed -i '1 s%^.*$%#!'${PYTHON_INTERPRETOR}'%g' $PACKAGEDIR/extern/bin/ipython2.7-config
+#[ -f $PACKAGEDIR/extern/bin/2to3 ]              && sed -i '1 s%^.*$%#!'${PYTHON_INTERPRETOR}'%g' $PACKAGEDIR/extern/bin/2to3
+#[ -f $PACKAGEDIR/extern/bin/cygdb ]             && sed -i '1 s%^.*$%#!'${PYTHON_INTERPRETOR}'%g' $PACKAGEDIR/extern/bin/cygdb
+#[ -f $PACKAGEDIR/extern/bin/cython ]            && sed -i '1 s%^.*$%#!'${PYTHON_INTERPRETOR}'%g' $PACKAGEDIR/extern/bin/cython
+#[ -f $PACKAGEDIR/extern/bin/guidata-tests ]     && sed -i '1 s%^.*$%#!'${PYTHON_INTERPRETOR}'%g' $PACKAGEDIR/extern/bin/guidata-tests
+#[ -f $PACKAGEDIR/extern/bin/guiqwt-tests ]      && sed -i '1 s%^.*$%#!'${PYTHON_INTERPRETOR}'%g' $PACKAGEDIR/extern/bin/guiqwt-tests
+#[ -f $PACKAGEDIR/extern/bin/idle ]              && sed -i '1 s%^.*$%#!'${PYTHON_INTERPRETOR}'%g' $PACKAGEDIR/extern/bin/idle
+#[ -f $PACKAGEDIR/extern/bin/ipcluster ]         && sed -i '1 s%^.*$%#!'${PYTHON_INTERPRETOR}'%g' $PACKAGEDIR/extern/bin/ipcluster
+#[ -f $PACKAGEDIR/extern/bin/ipcontroller ]      && sed -i '1 s%^.*$%#!'${PYTHON_INTERPRETOR}'%g' $PACKAGEDIR/extern/bin/ipcontroller
+#[ -f $PACKAGEDIR/extern/bin/ipengine ]          && sed -i '1 s%^.*$%#!'${PYTHON_INTERPRETOR}'%g' $PACKAGEDIR/extern/bin/ipengine
+#[ -f $PACKAGEDIR/extern/bin/iplogger ]          && sed -i '1 s%^.*$%#!'${PYTHON_INTERPRETOR}'%g' $PACKAGEDIR/extern/bin/iplogger
+#[ -f $PACKAGEDIR/extern/bin/iptest ]            && sed -i '1 s%^.*$%#!'${PYTHON_INTERPRETOR}'%g' $PACKAGEDIR/extern/bin/iptest
+#[ -f $PACKAGEDIR/extern/bin/ipython.py ]        && sed -i '1 s%^.*$%#!'${PYTHON_INTERPRETOR}'%g' $PACKAGEDIR/extern/bin/ipython.py
+#[ -f $PACKAGEDIR/extern/bin/irunner ]           && sed -i '1 s%^.*$%#!'${PYTHON_INTERPRETOR}'%g' $PACKAGEDIR/extern/bin/irunner
+#[ -f $PACKAGEDIR/extern/bin/nosetests ]         && sed -i '1 s%^.*$%#!'${PYTHON_INTERPRETOR}'%g' $PACKAGEDIR/extern/bin/nosetests
+#[ -f $PACKAGEDIR/extern/bin/pycolor ]           && sed -i '1 s%^.*$%#!'${PYTHON_INTERPRETOR}'%g' $PACKAGEDIR/extern/bin/pycolor
+#[ -f $PACKAGEDIR/extern/bin/pydoc ]             && sed -i '1 s%^.*$%#!'${PYTHON_INTERPRETOR}'%g' $PACKAGEDIR/extern/bin/pydoc
+#[ -f $PACKAGEDIR/extern/bin/sift ]              && sed -i '1 s%^.*$%#!'${PYTHON_INTERPRETOR}'%g' $PACKAGEDIR/extern/bin/sift
+#[ -f $PACKAGEDIR/extern/bin/smtpd.py ]          && sed -i '1 s%^.*$%#!'${PYTHON_INTERPRETOR}'%g' $PACKAGEDIR/extern/bin/smtpd.py
 
 # TODO: Some files from 'bin' are still not converted: pyuic4
 
