9c9528b97eed37b22cce010a6438c8d2ef9620ad
esenov <sergey.esenov@xfel.eu>
2013-07-13 17:11:28 +0000

    Rename 'karabo' package into 'karabo_package' to avoid bug in nosetests.  After installation the destination package name is 'karabo'
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/trunk@10499 a23ab9f9-1ea2-4934-960d-990259f9ea8c

---
+++ src/pythonKarabo/karabo_package/base_fsm.py
@@ -0,0 +1,61 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+__author__="Sergey Esenov <serguei.essenov at xfel.eu>"
+__date__ ="$May 10, 2013 2:17:13 PM$"
+
+import threading
+from abc import ABCMeta, abstractmethod
+from decorators import KARABO_CLASSINFO, KARABO_CONFIGURATION_BASE_CLASS
+
+@KARABO_CONFIGURATION_BASE_CLASS
+@KARABO_CLASSINFO("BaseFsm", "1.0")
+class BaseFsm(object):
+    
+    @staticmethod
+    def expectedParameters(expected):
+        pass
+    
+    def __init__(self, configuration):
+        super(BaseFsm, self).__init__()
+        self.fsm = None
+        self.processEventLock = threading.RLock()
+    
+    def getFsm(self):
+        return self.fsm
+    
+    def initFsmSlots(self, sigslot):
+        pass
+    
+    @abstractmethod
+    def errorFound(self, shortMessage, detailedMessage):
+        pass
+    
+    @abstractmethod
+    def errorFoundAction(self, shortMessage, detailedMessage):
+        pass
+    
+    @abstractmethod
+    def onStateUpdate(self, currentState):
+        pass
+    
+    @abstractmethod
+    def noStateTransition(self):
+        pass
+    
+    def startFsm(self):
+        """Start state machine"""
+        fsm = self.getFsm()
+        if fsm is not None:
+            fsm.start()
+            self.onStateUpdate(fsm.get_state())
+    
+    def processEvent(self, event):
+        """Process input event, i.e. drive state machine to the next state."""
+        with self.processEventLock:
+            fsm = self.getFsm()
+            if fsm is not None:
+                self.onStateUpdate("Changing...")
+                fsm.process_event(event)
+                self.onStateUpdate(fsm.get_state())
+    
