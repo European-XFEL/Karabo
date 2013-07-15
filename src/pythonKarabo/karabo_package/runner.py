b3c9c3688ae8e867d70c3dbc08e0e58c37ef5baa
esenov <sergey.esenov@xfel.eu>
2013-07-15 16:54:41 +0000

    Fix bug: no registration by hands needed - all imports should be always defined with full path: package.module. For example, karabo.decorators, karabo.device
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/trunk@10522 a23ab9f9-1ea2-4934-960d-990259f9ea8c

--- src/pythonKarabo/karabo_package/runner.py
+++ src/pythonKarabo/karabo_package/runner.py
@@ -4,9 +4,9 @@ __date__ ="$May 24, 2013 11:36:55 AM$"
 import os
 import sys
 import traceback
-from configurator import Configurator
+from karabo.configurator import Configurator
 from karabo.karathon import Hash, loadFromFile, saveToFile
-from decorators import KARABO_CLASSINFO, KARABO_CONFIGURATION_BASE_CLASS
+from karabo.decorators import KARABO_CLASSINFO, KARABO_CONFIGURATION_BASE_CLASS
 
 @KARABO_CONFIGURATION_BASE_CLASS
 @KARABO_CLASSINFO("Runner", "1.0")
