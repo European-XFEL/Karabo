4445e94cfca59cf1ff9a92ff5190d799d03b6fd5
maial <maial@a23ab9f9-1ea2-4934-960d-990259f9ea8c>
2013-08-01 11:18:03 +0000

    Update Timestamp and Epoch bindings. Conclude python tests to the Timestamp class
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/trunk@10886 a23ab9f9-1ea2-4934-960d-990259f9ea8c

--- src/karathon/PyUtilEpochstamp.cc
+++ src/karathon/PyUtilEpochstamp.cc
@@ -13,56 +13,57 @@ namespace bp = boost::python;
 using namespace karabo::util;
 using namespace std;
 
+
 void exportPyUtilEpochstamp() {
 
     bp::class_<Epochstamp> e("Epochstamp", bp::init<>());
     e.def(bp::init<const unsigned long long&, const unsigned long long&>((bp::arg("seconds"), bp::arg("fraction"))));
 
     e.def("getSeconds"
-          , (unsigned long long const & (Epochstamp::*)() const)(&Epochstamp::getSeconds)
+          , (unsigned long long const & (Epochstamp::*)() const) (&Epochstamp::getSeconds)
           , bp::return_value_policy< bp::copy_const_reference >());
 
     e.def("getFractionalSeconds"
-          , (unsigned long long const & (Epochstamp::*)() const)(&Epochstamp::getFractionalSeconds)
+          , (unsigned long long const & (Epochstamp::*)() const) (&Epochstamp::getFractionalSeconds)
           , bp::return_value_policy< bp::copy_const_reference >());
 
     e.def("fromIso8601"
           , &Epochstamp::toIso8601
           , bp::arg("timePoint"));
     e.staticmethod("fromIso8601");
-    
+
     e.def("fromIso8601Ext"
           , &Epochstamp::toIso8601Ext
           , bp::arg("timePoint"));
     e.staticmethod("fromIso8601Ext");
-    
+
     e.def("fromHashAttributes"
-           , &Epochstamp::fromHashAttributes
-           , bp::arg("attributes") );
+          , &Epochstamp::fromHashAttributes
+          , bp::arg("attributes"));
     e.staticmethod("fromHashAttributes");
-    
+
     e.def("toHashAttributes"
-           , &Epochstamp::toHashAttributes
-           , bp::arg("attributes") );
-    
+          , &Epochstamp::toHashAttributes
+          , bp::arg("attributes"));
+
     e.def("hashAttributesContainTimeInformation"
-           , &Epochstamp::hashAttributesContainTimeInformation
-           , bp::arg("attributes") );
+          , &Epochstamp::hashAttributesContainTimeInformation
+          , bp::arg("attributes"));
     e.staticmethod("hashAttributesContainTimeInformation");
-    
+
     e.def("toIso8601"
-           , (string (Epochstamp::*)() const)(&Epochstamp::toIso8601)
-           , bp::arg("precision")
-           , bp::arg("extended"));
-    
+          , (string(Epochstamp::*)() const) (&Epochstamp::toIso8601)
+          , bp::arg("precision")
+          , bp::arg("extended"));
+
     e.def("toIso8601Ext"
-           , (string (Epochstamp::*)() const)(&Epochstamp::toIso8601Ext)
-           , bp::arg("precision")
-           , bp::arg("extended"));
-    
+          , (string(Epochstamp::*)() const) (&Epochstamp::toIso8601Ext)
+          , bp::arg("precision")
+          , bp::arg("extended"));
+
     e.def("toFormattedString"
-           , (string (Epochstamp::*)(const string &) const)(&Epochstamp::toFormattedString)
-           , bp::arg("format") );
+          , (string(Epochstamp::*)(const string &) const) (&Epochstamp::toFormattedString)
+          , (bp::arg("format") = "%Y-%b-%d %H:%M:%S"));
 }
 
 
---
+++ src/pythonKarabo/tests/timestamp_test.py
@@ -0,0 +1,84 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+import unittest
+import time
+from karabo.karathon import *
+
+
+class  Timestamp_TestCase(unittest.TestCase):
+    #def setUp(self):
+    #    self.foo = Timestamp_()
+    #
+
+    #def tearDown(self):
+    #    self.foo.dispose()
+    #    self.foo = None
+
+    def test_timestamp_constructors(self):
+        
+        sleepDelay = 1
+        
+        try:
+            ts01 = Timestamp()
+        except Exception, e:
+            self.fail(functionName + " creating ts01 default constructor: " + str(e))
+        
+        time.sleep(sleepDelay)
+        
+        try:
+            ts02 = Timestamp()
+        except Exception, e:
+            self.fail(functionName + " creating ts02 default constructor: " + str(e))
+        
+        self.assertEqual(ts01.getSeconds() + sleepDelay, ts02.getSeconds(), "1st timestamp should have minus 'sleepDelay' second(s) than 2nd timestamp");
+        self.assertNotEqual(ts01.getFractionalSeconds(), ts02.getFractionalSeconds(), "1st and 2nd timestamp should have differents fractional seconds");
+        
+        # TODO: This test can be added after create the proper Constructor to Timestamp class
+        #try:
+        #    ts03 = Timestamp(ts02.getSeconds(), ts02.getFractionalSeconds())
+        #except Exception, e:
+        #    self.fail(functionName + " creating ts03 with seconds and fractionalSeconds constructor: " + str(e))
+        
+        #self.assertEqual(ts02.getSeconds(), ts03.getSeconds(), "2st and 3rd timestamp should have the same number of seconds");
+        #self.assertEqual(ts02.getFractionalSeconds(), ts03.getFractionalSeconds(), "2st and 3rd timestamp should have the same number of fractional seconds");
+        #
+        #self.assertEqual(ts01.getSeconds() + sleepDelay, ts03.getSeconds(), "1st timestamp should have minus 'sleepDelay' second(s) than 3rd timestamp");
+        #self.assertNotEqual(ts01.getFractionalSeconds(), ts03.getFractionalSeconds(), "1st and 3rd timestamp should have differents fractional seconds");
+        
+        
+    def test_timestamp_to_iso8601_string(self):
+        
+        # ISO8601 compact version
+        pTimeStr01 = "20121225T132536.789333123456789123";
+        try:
+            es01 = Epochstamp(1356441936, 789333123456789123) #(pTimeStr01)
+        except Exception, e:
+            self.fail(functionName + " creating Epochstamp01 using the String constructor: " + str(e))
+        
+        try:
+            ts01 = Timestamp(es01, Trainstamp())
+            
+            # Validate that "UNIVERSAL" format is corrected generated
+            pTimeConvertedStr01 = ts01.toFormattedString("%Y%m%dT%H%M%S");
+            self.assertEqual(pTimeConvertedStr01, "20121225T132536789333", "These strings must be equal");
+            
+            # Validate that default "user-friendly" format is corrected generated
+            pTimeConvertedStr02 = ts01.toFormattedString();
+            self.assertEqual(pTimeConvertedStr02, "2012-Dec-25 13:25:36", "These strings must be equal");
+            
+            # Validate that INPUT "user-friendly" format is corrected generated
+            pTimeConvertedStr03 = ts01.toFormattedString("%Y/%m/%d %H:%M:%S");
+            self.assertEqual(pTimeConvertedStr03, "2012/12/25 13:25:36", "These strings must be equal");
+            
+        except Exception, e:
+            self.fail(functionName + " creating Timestamp01 using the String constructor: " + str(e))
+        
+        # Validate time was correctly converted/stored with the correct information
+        self.assertEqual(ts01.getSeconds(), 1356441936, "1st timestamp number of seconds must be 1356441936");
+        self.assertEqual(ts01.getFractionalSeconds(), 789333123456789123, "1st timestamp number of fractional seconds must be 789333123456789123");
+
+
+if __name__ == '__main__':
+    unittest.main()
+
