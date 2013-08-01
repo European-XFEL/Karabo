4d641b72adf303dc43abd9420a4ffc94d2ac5f95
maial <luis.maia@xfel.eu>
2013-08-01 11:18:03 +0000

    Update Timestamp and Epoch bindings. Conclude python tests to the Timestamp class
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/trunk@10886 a23ab9f9-1ea2-4934-960d-990259f9ea8c

--- src/karathon/PyUtilTimestamp.cc
+++ src/karathon/PyUtilTimestamp.cc
@@ -13,59 +13,61 @@ namespace bp = boost::python;
 using namespace karabo::util;
 using namespace std;
 
+
 void exportPyUtilTimestamp() {
-       
-bp::class_<Timestamp> ts("Timestamp", bp::init<>());
+
+    bp::class_<Timestamp> ts("Timestamp", bp::init<>());
+
     ts.def(bp::init<Epochstamp const &, Trainstamp const &>((bp::arg("e"), bp::arg("t"))));
-    
+
     ts.def("getSeconds"
-           , (unsigned long long const & (Timestamp::*)() const)(&Timestamp::getSeconds)
-           , bp::return_value_policy< bp::copy_const_reference >() );
-    
+           , (unsigned long long const & (Timestamp::*)() const) (&Timestamp::getSeconds)
+           , bp::return_value_policy< bp::copy_const_reference >());
+
     ts.def("getFractionalSeconds"
-           , (unsigned long long const & (Timestamp::*)() const)(&Timestamp::getFractionalSeconds)
-           , bp::return_value_policy< bp::copy_const_reference >() );
-    
+           , (unsigned long long const & (Timestamp::*)() const) (&Timestamp::getFractionalSeconds)
+           , bp::return_value_policy< bp::copy_const_reference >());
+
     ts.def("getTrainId"
-           , (unsigned long long const & (Timestamp::*)() const)(&Timestamp::getTrainId)
-           , bp::return_value_policy< bp::copy_const_reference >() );
-    
+           , (unsigned long long const & (Timestamp::*)() const) (&Timestamp::getTrainId)
+           , bp::return_value_policy< bp::copy_const_reference >());
+
     ts.def("fromHashAttributes"
            , &Timestamp::fromHashAttributes
-           , bp::arg("attributes") );
+           , bp::arg("attributes"));
     ts.staticmethod("fromHashAttributes");
-    
+
     ts.def("hashAttributesContainTimeInformation"
            , &Timestamp::hashAttributesContainTimeInformation
-           , bp::arg("attributes") );
+           , bp::arg("attributes"));
     ts.staticmethod("hashAttributesContainTimeInformation");
-    
+
     ts.def("fromIso8601"
-          , &Timestamp::toIso8601
-          , bp::arg("timePoint"));
+           , &Timestamp::toIso8601
+           , bp::arg("timePoint"));
     ts.staticmethod("fromIso8601");
-    
+
     ts.def("fromIso8601Ext"
-          , &Timestamp::toIso8601Ext
-          , bp::arg("timePoint"));
+           , &Timestamp::toIso8601Ext
+           , bp::arg("timePoint"));
     ts.staticmethod("fromIso8601Ext");
-    
+
     ts.def("toIso8601"
-           , (string (Timestamp::*)() const)(&Timestamp::toIso8601)
+           , (string(Timestamp::*)() const) (&Timestamp::toIso8601)
            , bp::arg("precision")
            , bp::arg("extended"));
-    
+
     ts.def("toIso8601Ext"
-           , (string (Timestamp::*)() const)(&Timestamp::toIso8601Ext)
+           , (string(Timestamp::*)() const) (&Timestamp::toIso8601Ext)
            , bp::arg("precision")
            , bp::arg("extended"));
-    
+
     ts.def("toFormattedString"
-           , (string (Timestamp::*)(const string &) const)(&Timestamp::toFormattedString)
-           , bp::arg("format") );
-    
+           , (string(Timestamp::*)(const string &) const) (&Timestamp::toFormattedString)
+           , (bp::arg("format") = "%Y-%b-%d %H:%M:%S"));
+
     ts.def("toHashAttributes"
-           , (void (Timestamp::*)(Hash::Attributes &) const)(&Timestamp::toHashAttributes)
-           , bp::arg("attributes") );
-    
+           , (void (Timestamp::*)(Hash::Attributes &) const) (&Timestamp::toHashAttributes)
+           , bp::arg("attributes"));
+
 }
