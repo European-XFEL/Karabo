b5c13efe3efe42ebd9b80b43ac45abbd695f7447
esenov <esenov@a23ab9f9-1ea2-4934-960d-990259f9ea8c>
2013-09-18 17:20:35 +0000

    Updates following the changes in  karabo::io package
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/trunk@11338 a23ab9f9-1ea2-4934-960d-990259f9ea8c

---
+++ src/karathon/PythonOutputHandler.cc
@@ -0,0 +1,9 @@
+#include "PythonOutputHandler.hh"
+
+namespace karathon {
+    KARABO_REGISTER_IN_FACTORY(karabo::io::OutputHandler, PythonOutputHandler);
+    
+    KARABO_REGISTER_IN_FACTORY_1(karabo::io::OutputHandler, PythonOutputHandler, karabo::io::AbstractOutput::Pointer);
+}
+
+
---
+++ src/karathon/PythonOutputHandler.hh
@@ -0,0 +1,55 @@
+/* 
+ * File:   PythonOutputHandler.hh
+ * Author: esenov
+ *
+ * Created on September 17, 2013, 7:59 PM
+ */
+
+#ifndef KARATHON_PYTHONOUTPUTHANDLER_HH
+#define	KARATHON_PYTHONOUTPUTHANDLER_HH
+
+#include <boost/python.hpp>
+#include <boost/any.hpp>
+#include <karabo/util/Configurator.hh>
+#include <karabo/io/AbstractOutput.hh>
+#include <karabo/io/OutputHandler.hh>
+
+#include "Wrapper.hh"
+#include "ScopedGILAcquire.hh"
+
+namespace bp = boost::python;
+
+namespace karathon {
+ 
+    class PythonOutputHandler : public karabo::io::OutputHandler {
+    public:
+        
+        KARABO_CLASSINFO(PythonOutputHandler, "PythonOutputHandler", "1.0")
+                
+        PythonOutputHandler() {
+        }
+
+        PythonOutputHandler(const karabo::io::AbstractOutput::Pointer& output) : m_output(output) {
+        }
+
+        virtual ~PythonOutputHandler() {
+        }
+
+            void registerIOEventHandler(const boost::any& ioEventHandler) {
+                m_ioEventHandler = boost::any_cast < bp::object >(ioEventHandler);
+            }
+
+            void triggerIOEvent() {
+                if (m_ioEventHandler != bp::object())
+                    m_ioEventHandler(m_output);
+            }
+            
+        private:
+            karabo::io::AbstractOutput::Pointer m_output;
+            bp::object m_ioEventHandler;
+        
+    };
+}
+
+#endif	/* KARATHON_PYTHONOUTPUTHANDLER_HH */
+
