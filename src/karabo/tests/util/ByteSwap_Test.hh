d804605a58c7cebdd47298a1701fb331ba2cd5e9
parenti <parenti@a23ab9f9-1ea2-4934-960d-990259f9ea8c>
2013-10-04 14:57:26 +0000

    Added new byte swapping utility. Added unit tests for base64 and byte swapping utilities.
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/trunk@11587 a23ab9f9-1ea2-4934-960d-990259f9ea8c

---
+++ src/karabo/tests/util/ByteSwap_Test.hh
@@ -0,0 +1,34 @@
+/* 
+ * File:   ByteSwap_Test.hh
+ * Author: parenti
+ *
+ * Created on October 4, 2013, 3:51 PM
+ */
+
+#ifndef BYTESWAP_TEST_HH
+#define	BYTESWAP_TEST_HH
+
+#include <cppunit/extensions/HelperMacros.h>
+
+class ByteSwap_Test : public CPPUNIT_NS::TestFixture {
+
+    CPPUNIT_TEST_SUITE(ByteSwap_Test);
+    CPPUNIT_TEST(test16);
+    CPPUNIT_TEST(test32);
+    CPPUNIT_TEST(test64);
+    CPPUNIT_TEST_SUITE_END();
+
+public:
+    ByteSwap_Test();
+    virtual ~ByteSwap_Test();
+    void setUp();
+    void tearDown();
+
+private:
+    void test16();
+    void test32();
+    void test64();
+};
+
+#endif	/* BYTESWAP_TEST_HH */
+
