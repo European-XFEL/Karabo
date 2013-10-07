111b6c9c297b0787e058037509e8ef4476179f04
kumarm <kumarm@a23ab9f9-1ea2-4934-960d-990259f9ea8c>
2013-10-07 13:37:20 +0000

    Updated installExtern.sh to resume installing dependencies from the point it left off in the previous time. Previously it would always install from the first package. Fixed Makefile accordingly
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/trunk@11593 a23ab9f9-1ea2-4934-960d-990259f9ea8c

--- build/netbeans/karabo/.installExtern.sh
+++ build/netbeans/karabo/.installExtern.sh
@@ -16,7 +16,8 @@ fi
 
 # $1 EXTERN_DIR      Path to karabo extern folder
 # $2 INSTALL_PREFIX  Installation prefix
-# $3 WHAT            Specified libraries (optional)
+# $3 OPT_GUI	     With or without GUI
+# $4 WHAT            Specified libraries (optional)
 EXTERN_DIR=$1
 INSTALL_PREFIX=$2
 OPT_GUI=$3
@@ -30,9 +31,29 @@ if [ "$WHAT" != "" ]; then
     DEPENDENCIES=( $WHAT )
 fi
 
+#check if .marker.txt exists or not => if it does then read contents.
+#Otherwise create the file and input names of packages one by one as they are installed.
+if [ ! -f ./.marker.txt ]; then echo "" > ./.marker.txt; fi
+
+IFS=$'\r\n' MARKER=($(cat ./.marker.txt))
+
+#function to check existence of element in array
+elementIn() {
+    local e
+    for e in "${@:2}"; do [[ "$e" == "$1" ]] && return 0; done
+    return 1
+}
+
+#install packages one by one.
 for i in "${DEPENDENCIES[@]}"
 do
     :
+    elementIn "$i" "${MARKER[@]}"
+    vin=$?
+    if [ $vin -ne 1 ]
+    then
+	continue;
+    fi
     $EXTERN_DIR/install.sh $i $INSTALL_PREFIX
     rv=$?
     if [ $rv -ne 0 ]; then 
@@ -41,6 +62,7 @@ do
         echo
         exit $rv
     fi
+    echo $i >> ./.marker.txt
 done
 
 cd $CWD
