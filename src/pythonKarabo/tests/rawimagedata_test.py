43618419c428ffaa66b04a553ba3707c2fd15d89
irinak <irinak@a23ab9f9-1ea2-4934-960d-990259f9ea8c>
2013-10-10 12:20:02 +0000

    added python test for image processing
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/trunk@11606 a23ab9f9-1ea2-4934-960d-990259f9ea8c

--- src/pythonKarabo/tests/rawimagedata_test.py
+++ src/pythonKarabo/tests/rawimagedata_test.py
@@ -11,7 +11,7 @@ class  RawImageData_TestCase(unittest.TestCase):
     def setUp(self):
         self.resourcesdir = "../../../src/pythonKarabo/tests/resources/"
     
-    def test_rawimagedata_to_png(self):
+    def test_rawimage_to_png(self):
         try:
             imgArr = np.fromfile(self.resourcesdir+"image_0001.raw", dtype=np.uint32)
             self.assertEqual(imgArr.size, 1048576)
@@ -19,7 +19,26 @@ class  RawImageData_TestCase(unittest.TestCase):
             imgArr.shape = (1024, 1024)
             
             plt.imshow(imgArr)
-            plt.savefig(self.resourcesdir+"image_0001.png")
+            plt.savefig(self.resourcesdir+"image_0001A.png")
             
         except Exception,e:
-            self.fail("test_rawimagedata_to_png exception: " + str(e))
+            self.fail("test_rawimage_to_png exception group 1: " + str(e))
+
+        try:
+            f = open(self.resourcesdir+"image_0001.raw")
+            imgStr = f.read()
+            f.close()
+            
+            imgArr = np.array(bytearray(imgStr), dtype=np.uint32)
+            self.assertEqual(imgArr.size, 4194304)
+             
+            imgArr.shape = (1024, 1024, 4)
+            
+            plt.imshow(imgArr)
+            plt.savefig(self.resourcesdir+"image_0001B.png")
+        
+        except Exception,e:
+            self.fail("test_rawimage_to_png exception group 2: " + str(e))
+
+if __name__ == '__main__':
+    unittest.main()   
