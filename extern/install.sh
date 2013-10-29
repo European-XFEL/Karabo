9c53675712bc65ca252d6339d677572bde941b84
jszuba <janusz.szuba@xfel.eu>
2013-10-29 14:23:00 +0000

    uncommenting exit
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/trunk@11734 a23ab9f9-1ea2-4934-960d-990259f9ea8c

--- extern/install.sh
+++ extern/install.sh
@@ -18,7 +18,7 @@ safeRunCommand() {
     ret_code=$?
     if [ $ret_code != 0 ]; then
 	printf "Error : [%d] when executing command: '$cmnd'" $ret_code
-	#exit $ret_code
+	exit $ret_code
     fi
 }
 
