4efc8fbdf5207e2d34df4316cb5ad559391aa9b0
irinak <irinak@a23ab9f9-1ea2-4934-960d-990259f9ea8c>
2013-11-13 09:51:56 +0000

    corrections
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/trunk@11948 a23ab9f9-1ea2-4934-960d-990259f9ea8c

--- src/pythonKarabo/karabo_package/device.py
+++ src/pythonKarabo/karabo_package/device.py
@@ -205,10 +205,10 @@ class PythonDevice(BaseFsm):
                 if type(stamp) is not Timestamp:
                     raise TypeError,"The 3rd argument should be Timestamp"
                 if isCpuImage(value):
-                    _setImage(key, value)
+                    self._setImage(key, value)
                     return;
                 elif type(value) is RawImageData:
-                    _setRawImageData(key, value)
+                    self._setRawImageData(key, value)
                     return
                 pars = tuple([Hash(key, value), stamp])
             
@@ -224,10 +224,10 @@ class PythonDevice(BaseFsm):
                 if type(pars[0]) is not Hash:
                     key, value = pars
                     if isCpuImage(value):
-                        _setImage(key, value)
+                        self._setImage(key, value)
                         return
                     elif type(value) is RawImageData:
-                        _setRawImageData(key, value)
+                        self._setRawImageData(key, value)
                         return
                     pars = tuple([Hash(key,value), Timestamp()])
                 hash, stamp = pars
@@ -236,10 +236,10 @@ class PythonDevice(BaseFsm):
                 for key in paths:
                     value = hash[key]
                     if isCpuImage(value):
-                        _setImage(key, value)    # process images individually
+                        self._setImage(key, value)    # process images individually
                         hash.erasePath(key)      # clear hash from images 
                     elif type(value) is RawImageData:
-                        _setRawImageData(key, value)
+                        self._setRawImageData(key, value)
                         hash.erasePath(key)
         
                 try:
@@ -258,7 +258,7 @@ class PythonDevice(BaseFsm):
                 if not validated.empty():
                     self.parameters.merge(validated, HashMergePolicy.REPLACE_ATTRIBUTES)
                     self._ss.emit("signalChanged", validated, self.deviceid)
-
+       
     def __setitem__(self, key, value):
         self.set(key, value, Timestamp())
         
