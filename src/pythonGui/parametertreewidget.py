bee3e552c89d7cc5f61e238be2d1bac65b481f79
Kerstin Weger <kerstin.weger@xfel.eu>
2014-01-22 09:15:04 +0100

    Do not use QMimeData for Drag/Drop anymore - use source

--- src/pythonGui/parametertreewidget.py
+++ src/pythonGui/parametertreewidget.py
@@ -11,16 +11,15 @@
 
 __all__ = ["ParameterTreeWidget"]
 
-
 import treewidgetitems.attributetreewidgetitem
 from editableapplylatercomponent import EditableApplyLaterComponent
-from enums import *
+from enums import NavigationItemTypes
 import globals
 from karabo.karathon import *
 from manager import Manager
 
-from PyQt4.QtCore import *
-from PyQt4.QtGui import *
+from PyQt4.QtCore import QByteArray, QMimeData, QRect, Qt
+from PyQt4.QtGui import QAbstractItemView, QMenu, QTreeWidget
 
 
 class ParameterTreeWidget(QTreeWidget):
@@ -88,62 +87,7 @@ class ParameterTreeWidget(QTreeWidget):
 
 
     def mimeData(self, items):
-        item = items[0]
-
-        # Attributes can not be dropped
-        if isinstance(item, treewidgetitems.attributetreewidgetitem.AttributeTreeWidgetItem):
-            return
-        
-        mimeData = QMimeData()        
-
-        # Put necessary data in MimeData:
-        # Source type
-        mimeData.setData("sourceType", "ParameterTreeWidget")
-        # Internal key
-        mimeData.setData("internalKey", item.internalKey)
-        # Display name
-        displayName = item.text(0)
-        # Use DeviceClass/DeviceInstance-Key if no displayName is set
-        if len(item.text(0)) == 0:
-            keys = item.internalKey.split('.')
-            displayName = keys[1]
-        mimeData.setData("displayName", displayName)
-        
-        # Get NavigationItemType
-        navigationItemType = self.__configPanel.getNavigationItemType()
-        
-        # Display component?
-        hasDisplayComponent = navigationItemType == NavigationItemTypes.DEVICE
-        mimeData.setData("hasDisplayComponent", "{}".format(hasDisplayComponent))
-        # Editable component?
-        hasEditableComponent = item.editableComponent is not None
-        mimeData.setData("hasEditableComponent", "{}".format(hasEditableComponent))
-        
-        # TODO: HACK to get apply button disabled
-        if hasEditableComponent:
-            mimeData.setData("currentValue", "{}".format(item.editableComponent.value))
-        
-        if item.unitSymbol:
-            mimeData.setData("unitSymbol", "{}".format(item.unitSymbol))
-        if item.metricPrefixSymbol:
-            mimeData.setData("metricPrefixSymbol", "{}".format(item.metricPrefixSymbol))
-
-        if item.enumeration:
-            enumerationString = str()
-            nbEnums = len(item.enumeration)
-            for i in xrange(nbEnums):
-                enumerationString += item.enumeration[i]
-                if i != (nbEnums-1):
-                    enumerationString += ","
-            mimeData.setData("enumeration", enumerationString)
-            
-        # Navigation item type
-        mimeData.setData("navigationItemType", "{}".format(navigationItemType))
-        # Class alias
-        if item.classAlias:
-            mimeData.setData("classAlias", "{}".format(item.classAlias))
-
-        return mimeData
+        return QMimeData()
 
 
 ### getter & setter functions ###
