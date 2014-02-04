db8ba66f9eaf627325b2e3076e9e6c2118ee8876
Kerstin Weger <kerstin.weger@xfel.eu>
2014-02-04 11:09:18 +0100

    - Removed redundant EditablePathApplyComponent and EditableNoApplyComponent, DisplayFilePath/EditableFilePath - Added new classes to handle PATH_ELEMENTs of type "directory", "filein", "fileout" - Bugfix: change widget of editablewidgets in custompanel

---
+++ src/pythonGui/displaywidgets/displaydirectory.py
@@ -0,0 +1,63 @@
+#############################################################################
+# Author: <kerstin.weger@xfel.eu>
+# Created on February 4, 2014
+# Copyright (C) European XFEL GmbH Hamburg. All rights reserved.
+#############################################################################
+
+
+"""This module contains a class which represents a widget plugin for parameters
+   and is created by the factory class DisplayWidget.
+"""
+
+__all__ = ["DisplayDirectory"]
+
+
+from widget import DisplayWidget
+
+from PyQt4.QtGui import (QHBoxLayout, QIcon, QLineEdit, QToolButton, QWidget)
+
+
+class DisplayDirectory(DisplayWidget):
+    category = "String"
+    alias = "Directory"
+
+    def __init__(self, **params):
+        super(DisplayDirectory, self).__init__(**params)
+
+        self.__compositeWidget = QWidget()
+        hLayout = QHBoxLayout(self.__compositeWidget)
+        hLayout.setContentsMargins(0,0,0,0)
+
+        self.__lePath = QLineEdit()
+        self.__lePath.setReadOnly(True)
+        hLayout.addWidget(self.__lePath)
+
+        text = "Select directory"
+        self.__tbPath = QToolButton()
+        self.__tbPath.setStatusTip(text)
+        self.__tbPath.setToolTip(text)
+        self.__tbPath.setIcon(QIcon(":open"))
+        self.__tbPath.setEnabled(False)
+        self.__tbPath.setMaximumSize(25,25)
+        hLayout.addWidget(self.__tbPath)
+
+
+    @property
+    def widget(self):
+        return self.__compositeWidget
+
+
+    @property
+    def value(self):
+        return self.__lePath.text()
+
+
+    def valueChanged(self, key, value, timestamp=None):
+        if value is None:
+            return
+
+        if value != self.value:
+            self.__lePath.blockSignals(True)
+            self.__lePath.setText(value)
+            self.__lePath.blockSignals(False)
+
