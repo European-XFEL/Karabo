2ab6e3eeb72e1608a2d753b1ab56dbba6fc88e99
esenov <sergey.esenov@xfel.eu>
2014-02-13 17:52:01 +0000

    Add explicit template instantiations for Hash/Schema-Binary/Text-Input/Output classes
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/trunk@12784 a23ab9f9-1ea2-4934-960d-990259f9ea8c

---
+++ src/karabo/io/HashBinaryFileInput.cc
@@ -0,0 +1,5 @@
+#include <karabo/util/Configurator.hh>
+#include <karabo/util/Hash.hh>
+#include "karabo/io/BinaryFileInput.hh"
+
+KARABO_EXPLICIT_TEMPLATE(karabo::io::BinaryFileInput<karabo::util::Hash>)
