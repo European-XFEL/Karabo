9584affd7cdf64bfa4b4ee3a7b72e178600142d2
Martin Teichmann <martin.teichmann@xfel.eu>
2014-02-13 09:54:28 +0100

    Revert "- Bugfix: show correct values in dropped component"
    better idea for bugfix available

--- src/pythonGui/manager.py
+++ src/pythonGui/manager.py
@@ -39,16 +39,19 @@ class DataNotifier(QObject):
     def __init__(self, key, component):
         super(DataNotifier, self).__init__()
 
+        self.signalUpdateComponent.connect(self.onValueChanged)
         self.addComponent(key, component)
-        # Store first added component for later value update
-        self.firstComponent = component
+
+
+    def onValueChanged(self, key, value):
+        self.value = value
 
 
     def addComponent(self, key, component):
         self.signalUpdateComponent.connect(component.onValueChanged)
         self.signalUpdateDisplayValue.connect(component.onDisplayValueChanged)
-        if hasattr(self, "firstComponent"):
-            self.signalUpdateComponent.emit(key, self.firstComponent.value)
+        if hasattr(self, "value"):
+            self.signalUpdateComponent.emit(key, self.value)
 
 
     def updateDisplayValue(self, key, value):
