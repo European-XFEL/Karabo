6aff6cf47a236ec98c48f0ca14dd85c6b18fad2b
irinak <irina.kozlova@xfel.eu>
2014-02-27 18:05:53 +0000

    added binding for TimeDuration
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/trunk@12907 a23ab9f9-1ea2-4934-960d-990259f9ea8c

---
+++ src/karathon/PyUtilTimeDuration.cc
@@ -0,0 +1,85 @@
+/*
+ * $Id$
+ *
+ * Author: <irina.kozlova@xfel.eu>
+ *
+ * Copyright (C) European XFEL GmbH Hamburg. All rights reserved.
+ */
+#include "boost/python.hpp"
+
+#include <karabo/util/TimeDuration.hh>
+
+namespace bp = boost::python;
+using namespace karabo::util;
+using namespace std;
+
+
+void exportPyUtilTimeDuration() {
+
+    bp::class_<TimeDuration> t("TimeDuration", bp::init<>());
+    t.def(bp::init<Hash const & >((bp::arg("hash"))));
+    bp::implicitly_convertible< Hash const &, TimeDuration >();
+    t.def(bp::init< long long unsigned int, long long unsigned int >((bp::arg("seconds"), bp::arg("fractions"))));
+    t.def(bp::init< int, int, int, long long unsigned int, long long unsigned int >((bp::arg("days"), bp::arg("hours"), bp::arg("minutes"), bp::arg("seconds"), bp::arg("fractions"))));
+    t.def(bp::self_ns::str(bp::self));
+
+    t.def("format"
+          , (string(TimeDuration::*)(string const &) const) (&TimeDuration::format)
+          , (bp::arg("fmt")));
+
+    t.def("fromHash"
+          , (void (TimeDuration::*)(Hash const &))(&TimeDuration::fromHash)
+          , (bp::arg("hash")));
+
+    t.def("getDays"
+          , (unsigned long int (TimeDuration::*)()const) (&TimeDuration::getDays));
+
+    t.def("getFractions"
+          , (TimeValue(TimeDuration::*)(karabo::util::TIME_UNITS const) const) (&TimeDuration::getFractions)
+          , (bp::arg("unit") = karabo::util::NANOSEC));
+
+    t.def("getHours"
+          , (unsigned long int (TimeDuration::*)()const) (&TimeDuration::getHours));
+    
+    t.def("getMinutes"
+          , (unsigned long int (TimeDuration::*)()const) (&TimeDuration::getMinutes));
+
+    t.def("getSeconds"
+          , (TimeValue(TimeDuration::*)()const) (&TimeDuration::getSeconds));
+
+    t.def("getTotalHours"
+          , (TimeValue(TimeDuration::*)()const) (&TimeDuration::getTotalHours));
+
+    t.def("getTotalMinutes"
+          , (TimeValue(TimeDuration::*)()const) (&TimeDuration::getTotalMinutes));
+
+    t.def("getTotalSeconds"
+          , (TimeValue(TimeDuration::*)()const) (&TimeDuration::getTotalSeconds));
+
+    t.def("isNull"
+          , (bool (TimeDuration::*)() const) (&TimeDuration::isNull));
+
+    t.def("setDefaultFormat"
+          , (void (*)(string const &))(&TimeDuration::setDefaultFormat)
+          , (bp::arg("fmt")));
+    t.staticmethod("setDefaultFormat");
+
+    t.def("toHash"
+          , (void (TimeDuration::*)(Hash &))(&TimeDuration::toHash)
+          , (bp::arg("hash")));
+    
+    t.def(bp::self != bp::self);
+    t.def(bp::self + bp::self);
+    t.def(bp::self += bp::self);
+    t.def(bp::self - bp::self);
+    t.def(bp::self -= bp::self);
+    t.def(bp::self / bp::self);
+    t.def(bp::self < bp::self);
+    t.def(bp::self <= bp::self);
+    t.def(bp::self == bp::self);
+    t.def(bp::self > bp::self);
+    t.def(bp::self >= bp::self);
+
+}
+
+
