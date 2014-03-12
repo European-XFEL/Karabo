fa8c8b4236ed8b9b3628cfc8708a4a575fd83e33
Martin Teichmann <martin.teichmann@xfel.eu>
2014-03-12 13:53:11 +0100

    work on unittests for python hash

--- src/karathonTest/tests/constructors.py
+++ src/karathonTest/tests/constructors.py
@@ -1,82 +1,71 @@
 import unittest
-from libkarathon import *
+from hash import Hash
 
 class  ConstructorsTestCase(unittest.TestCase):
 
-    def test_constructors(self):
-        
-        # Check properties of empty Hash
-        try:
-            h = Hash()
-            self.assertEqual(len(h), 0)
-            self.assertEqual(h.empty(), True)
-        except Exception, e:
-            self.fail("test_constructors exception group 1: " + str(e))
+    def test_empty(self):
+        """ Check properties of empty Hash """
+        h = Hash()
+        self.assertEqual(len(h), 0)
+        self.assertEqual(h.empty(), True)
 
-        # Check Hash with one property
-        try:
-            h = Hash('a', 1)
-            self.assertEqual(len(h), 1)
-            self.assertEqual(h.empty(), False)
-            self.assertEqual(h.get('a'), 1)
-            self.assertEqual(h['a'], 1)
-        except Exception,e:
-            self.fail("test_constructors exception group 2: " + str(e))
+    def test_one_property(self):
+        """ Check Hash with one property """
+        h = Hash('a', 1)
+        self.assertEqual(len(h), 1)
+        self.assertEqual(h.empty(), False)
+        self.assertEqual(h.get('a'), 1)
+        self.assertEqual(h['a'], 1)
             
-        # Check Hash with 2 properties
-        try:
-            h = Hash('a', 1, 'b', 2.0)
-            self.assertEqual(h.empty(), False)
-            self.assertEqual(len(h), 2)
-            self.assertEqual(h['a'], 1)
-            self.assertEqual(h['b'], 2.0)
-        except Exception,e:
-            self.fail("test_constructors exception group 3: " + str(e))
+    def test_two_properties(self):
+        """ Check Hash with 2 properties """
+        h = Hash('a', 1, 'b', 2.0)
+        self.assertEqual(h.empty(), False)
+        self.assertEqual(len(h), 2)
+        self.assertEqual(h['a'], 1)
+        self.assertEqual(h['b'], 2.0)
         
-        # Check Hash with 6 properties of different types
-        try:
-            h = Hash("a.b.c", 1, "b.c", 2.0, "c", 3.7, "d.e", "4",
-                    "e.f.g.h", [5,5,5,5,5], "F.f.f.f.f", Hash("x.y.z", 99))
-            self.assertEqual(h.empty(), False)
-            self.assertEqual(len(h), 6)
-            self.assertEqual(h['a.b.c'], 1)
-            self.assertEqual(h['b.c'], 2.0)
-            self.assertEqual(h['c'], 3.7)
-            self.assertEqual(h['d.e'], "4")
-            self.assertEqual(h['e.f.g.h'][0], 5)
-            self.assertEqual(len(h['e.f.g.h']), 5)
-            self.assertEqual(h['F.f.f.f.f']['x.y.z'], 99)
-            self.assertEqual(h['F.f.f.f.f.x.y.z'], 99)
-            # Make Hash flat
-            flat = Hash()
-            Hash.flatten(h, flat)
-            self.assertEqual(flat.empty(), False)
-            self.assertEqual(len(flat), 6)
-            self.assertEqual(flat.get('a.b.c', ' '), 1)
-            self.assertEqual(flat.get('b.c', ' '), 2.0)
-            self.assertEqual(flat.get('c', ' '), 3.7)
-            self.assertEqual(flat.get('d.e', ' '), "4")
-            self.assertEqual(flat.get('e.f.g.h', ' ')[0], 5)
-            self.assertEqual(len(flat.get('e.f.g.h', ' ')), 5)
-            self.assertEqual(flat.get('F.f.f.f.f.x.y.z', ' '), 99)
+    def test_six_properties(self):
+        """ Check Hash with 6 properties of different types """
+        h = Hash("a.b.c", 1, "b.c", 2.0, "c", 3.7, "d.e", "4",
+                "e.f.g.h", [5,5,5,5,5], "F.f.f.f.f", Hash("x.y.z", 99))
+        self.assertEqual(h.empty(), False)
+        self.assertEqual(len(h), 6)
+        self.assertEqual(h['a.b.c'], 1)
+        self.assertEqual(h['b.c'], 2.0)
+        self.assertEqual(h['c'], 3.7)
+        self.assertEqual(h['d.e'], "4")
+        self.assertEqual(h['e.f.g.h'][0], 5)
+        self.assertEqual(len(h['e.f.g.h']), 5)
+        self.assertEqual(h['F.f.f.f.f']['x.y.z'], 99)
+        self.assertEqual(h['F.f.f.f.f.x.y.z'], 99)
+        # Make Hash flat
+        flat = Hash()
+        Hash.flatten(h, flat)
+        self.assertEqual(flat.empty(), False)
+        self.assertEqual(len(flat), 6)
+        self.assertEqual(flat.get('a.b.c', ' '), 1)
+        self.assertEqual(flat.get('b.c', ' '), 2.0)
+        self.assertEqual(flat.get('c', ' '), 3.7)
+        self.assertEqual(flat.get('d.e', ' '), "4")
+        self.assertEqual(flat.get('e.f.g.h', ' ')[0], 5)
+        self.assertEqual(len(flat.get('e.f.g.h', ' ')), 5)
+        self.assertEqual(flat.get('F.f.f.f.f.x.y.z', ' '), 99)
             
-            # Make flat Hash unflatten again
-            tree = Hash()
-            flat.unflatten(tree)
-            self.assertEqual(tree.empty(), False)
-            self.assertEqual(len(tree), 6)
-            self.assertEqual(tree['a.b.c'], 1)
-            self.assertEqual(tree['b.c'], 2.0)
-            self.assertEqual(tree['c'], 3.7)
-            self.assertEqual(tree['d.e'], "4")
-            self.assertEqual(tree['e.f.g.h'][0], 5)
-            self.assertEqual(len(tree['e.f.g.h']), 5)
-            self.assertEqual(tree['F.f.f.f.f']['x.y.z'], 99)
-            self.assertEqual(tree['F.f.f.f.f.x.y.z'], 99)
-        except Exception,e:
-            self.fail("test_constructors exception group 4: " + str(e))
+        # Make flat Hash unflatten again
+        tree = Hash()
+        flat.unflatten(tree)
+        self.assertEqual(tree.empty(), False)
+        self.assertEqual(len(tree), 6)
+        self.assertEqual(tree['a.b.c'], 1)
+        self.assertEqual(tree['b.c'], 2.0)
+        self.assertEqual(tree['c'], 3.7)
+        self.assertEqual(tree['d.e'], "4")
+        self.assertEqual(tree['e.f.g.h'][0], 5)
+        self.assertEqual(len(tree['e.f.g.h']), 5)
+        self.assertEqual(tree['F.f.f.f.f']['x.y.z'], 99)
+        self.assertEqual(tree['F.f.f.f.f.x.y.z'], 99)
         
-
 if __name__ == '__main__':
     unittest.main()
 
--- src/karathonTest/tests/iteration.py
+++ src/karathonTest/tests/iteration.py
@@ -1,160 +1,138 @@
 import unittest
-from libkarathon import *
+from hash import Hash
 
 
 class  IterationTestCase(unittest.TestCase):
-    #def setUp(self):
-    #    self.foo = Iteration()
-    #
+    def setUp(self):
+        self.h = Hash("should", 1, "be", 2, "iterated", 3, "in", 4,
+                      "correct", 5, "order", 6)
+        #self.a = HashAttributes("should", 1, "be", 2, "iterated", 3, "in", 4,
+        #                       "correct", 5, "order", 6)
 
-    #def tearDown(self):
-    #    self.foo.dispose()
-    #    self.foo = None
 
-    def test_iteration(self):
-        h = Hash("should", 1, "be", 2, "iterated", 3, "in", 4, "correct", 5, "order", 6)
-        a = HashAttributes("should", 1, "be", 2, "iterated", 3, "in", 4, "correct", 5, "order", 6)
-        try:
-            insertionOrder = list()
-            for k in h:
-                insertionOrder.append(str(k))
-            self.assertEqual(insertionOrder[0], "should");
-            self.assertEqual(insertionOrder[1], "be");
-            self.assertEqual(insertionOrder[2], "iterated");
-            self.assertEqual(insertionOrder[3], "in");
-            self.assertEqual(insertionOrder[4], "correct");
-            self.assertEqual(insertionOrder[5], "order");
+    def test_iteration_1(self):
+        insertionOrder = [ ]
+        for k in self.h:
+            insertionOrder.append(str(k))
+        self.assertEqual(insertionOrder[0], "should");
+        self.assertEqual(insertionOrder[1], "be");
+        self.assertEqual(insertionOrder[2], "iterated");
+        self.assertEqual(insertionOrder[3], "in");
+        self.assertEqual(insertionOrder[4], "correct");
+        self.assertEqual(insertionOrder[5], "order");
             
-        except Exception,e:
-            self.fail("test_iteration exception group 1: " + str(e))
 
-        try:
-            alphaNumericOrder = list()
-            for k in h:
-                alphaNumericOrder.append(k.getKey())
-            alphaNumericOrder.sort()
-            self.assertEqual(alphaNumericOrder[0], "be")
-            self.assertEqual(alphaNumericOrder[1], "correct")
-            self.assertEqual(alphaNumericOrder[2], "in")
-            self.assertEqual(alphaNumericOrder[3], "iterated")
-            self.assertEqual(alphaNumericOrder[4], "order")
-            self.assertEqual(alphaNumericOrder[5], "should")
-        except Exception,e:
-            self.fail("test_iteration exception group 2: " + str(e))
+    def test_iteration_2(self):
+        alphaNumericOrder = list()
+        for k in self.h:
+            alphaNumericOrder.append(k.getKey())
+        alphaNumericOrder.sort()
+        self.assertEqual(alphaNumericOrder[0], "be")
+        self.assertEqual(alphaNumericOrder[1], "correct")
+        self.assertEqual(alphaNumericOrder[2], "in")
+        self.assertEqual(alphaNumericOrder[3], "iterated")
+        self.assertEqual(alphaNumericOrder[4], "order")
+        self.assertEqual(alphaNumericOrder[5], "should")
 
+    def test_iteration_3(self):
+        h = Hash(self.h)
         h.set("be", "2") # Has no effect on order
 
-        try:
-            insertionOrder = list()
-            for k in h:
-                insertionOrder.append(str(k.getKey()))
-        
-            self.assertEqual(insertionOrder[0], "should")
-            self.assertEqual(insertionOrder[1], "be")
-            self.assertEqual(insertionOrder[2], "iterated")
-            self.assertEqual(insertionOrder[3], "in")
-            self.assertEqual(insertionOrder[4], "correct")
-            self.assertEqual(insertionOrder[5], "order")
-        except Exception,e:
-            self.fail("test_iteration exception group 3: " + str(e))
+        insertionOrder = list()
+        for k in h:
+            insertionOrder.append(str(k.getKey()))
 
-        try:
-            alphaNumericOrder = list()
-            for k in h:
-                alphaNumericOrder.append(str(k.getKey()))
-            alphaNumericOrder.sort()
-            self.assertEqual(alphaNumericOrder[0], "be")
-            self.assertEqual(alphaNumericOrder[1], "correct")
-            self.assertEqual(alphaNumericOrder[2], "in")
-            self.assertEqual(alphaNumericOrder[3], "iterated")
-            self.assertEqual(alphaNumericOrder[4], "order")
-            self.assertEqual(alphaNumericOrder[5], "should")
-        except Exception,e:
-            self.fail("test_iteration exception group 4: " + str(e))
+        self.assertEqual(insertionOrder[0], "should")
+        self.assertEqual(insertionOrder[1], "be")
+        self.assertEqual(insertionOrder[2], "iterated")
+        self.assertEqual(insertionOrder[3], "in")
+        self.assertEqual(insertionOrder[4], "correct")
+        self.assertEqual(insertionOrder[5], "order")
 
+    def test_iteration_3(self):
+        alphaNumericOrder = list()
+        for k in self.h:
+            alphaNumericOrder.append(str(k.getKey()))
+        alphaNumericOrder.sort()
+        self.assertEqual(alphaNumericOrder[0], "be")
+        self.assertEqual(alphaNumericOrder[1], "correct")
+        self.assertEqual(alphaNumericOrder[2], "in")
+        self.assertEqual(alphaNumericOrder[3], "iterated")
+        self.assertEqual(alphaNumericOrder[4], "order")
+        self.assertEqual(alphaNumericOrder[5], "should")
+
+
+    def test_iteration_4(self):
+        h = Hash(self.h)
         h.erase("be")  # Remove
         h.set("be", "2")   # Must be last element in sequence now
 
-        try:
-            insertionOrder = list()
-            for k in h:
-                insertionOrder.append(str(k.getKey()))
+        insertionOrder = list()
+        for k in h:
+            insertionOrder.append(str(k.getKey()))
+
+        self.assertEqual(insertionOrder[0], "should")
+        self.assertEqual(insertionOrder[1], "iterated")
+        self.assertEqual(insertionOrder[2], "in")
+        self.assertEqual(insertionOrder[3], "correct")
+        self.assertEqual(insertionOrder[4], "order")
+        self.assertEqual(insertionOrder[5], "be")
+
+    def test_iteration_5(self):
+        alphaNumericOrder = list()
+        for k in self.h:
+            alphaNumericOrder.append(str(k.getKey()))
+        alphaNumericOrder.sort()
+        self.assertEqual(alphaNumericOrder[0], "be")
+        self.assertEqual(alphaNumericOrder[1], "correct")
+        self.assertEqual(alphaNumericOrder[2], "in")
+        self.assertEqual(alphaNumericOrder[3], "iterated")
+        self.assertEqual(alphaNumericOrder[4], "order")
+        self.assertEqual(alphaNumericOrder[5], "should")
+
+    def test_iteration_6(self):
+        tmp = list()           # create empty set
+        self.h.getKeys(tmp)    # fill set by keys
+        tmp.sort()
+        it = iter(tmp)
+        self.assertEqual(str(it.next()), "be")
+        self.assertEqual(str(it.next()), "correct")
+        self.assertEqual(str(it.next()), "in")
+        self.assertEqual(str(it.next()), "iterated")
+        self.assertEqual(str(it.next()), "order")
+        self.assertEqual(str(it.next()), "should")
+
+    def test_iteration_7(self):
+        tmp = list()           # create empty vector
+        self.h.getKeys(tmp)    # fill vector by keys
+        it = iter(tmp)
+        self.assertEqual(str(it.next()), "should")
+        self.assertEqual(str(it.next()), "be")
+        self.assertEqual(str(it.next()), "iterated")
+        self.assertEqual(str(it.next()), "in")
+        self.assertEqual(str(it.next()), "correct")
+        self.assertEqual(str(it.next()), "order")
+
+    def test_iteration_8(self):
+        h = Hash("b", "bla-la-la", "a.b.c", 1, "abc.2", 2.2222222, "a.b.c1.d", "abc1d", "abc.1", 1.11111111)
         
-            self.assertEqual(insertionOrder[0], "should")
-            self.assertEqual(insertionOrder[1], "iterated")
-            self.assertEqual(insertionOrder[2], "in")
-            self.assertEqual(insertionOrder[3], "correct")
-            self.assertEqual(insertionOrder[4], "order")
-            self.assertEqual(insertionOrder[5], "be")
-        except Exception,e:
-            self.fail("test_iteration exception group 5: " + str(e))
+        l = [];
+        h.getKeys(l)    # use top level keys
+        self.assertEqual(len(l), 3)
+        i = iter(l)                         # "canonical" order: on every level insertion order
+        self.assertEqual(str(i.next()), "b")
+        self.assertEqual(str(i.next()), "a")
+        self.assertEqual(str(i.next()), "abc")
 
-        try:
-            alphaNumericOrder = list()
-            for k in h:
-                alphaNumericOrder.append(str(k.getKey()))
-            alphaNumericOrder.sort()
-            self.assertEqual(alphaNumericOrder[0], "be")
-            self.assertEqual(alphaNumericOrder[1], "correct")
-            self.assertEqual(alphaNumericOrder[2], "in")
-            self.assertEqual(alphaNumericOrder[3], "iterated")
-            self.assertEqual(alphaNumericOrder[4], "order")
-            self.assertEqual(alphaNumericOrder[5], "should")
-        except Exception,e:
-            self.fail("test_iteration exception group 6: " + str(e))
-
-        #  getKeys(...) to ...
-        #         "list" and sort it  ... like C++ set
-        try:
-            tmp = list()           # create empty set
-            h.getKeys(tmp)         # fill set by keys
-            tmp.sort()
-            it = iter(tmp)
-            self.assertEqual(str(it.next()), "be")
-            self.assertEqual(str(it.next()), "correct")
-            self.assertEqual(str(it.next()), "in")
-            self.assertEqual(str(it.next()), "iterated")
-            self.assertEqual(str(it.next()), "order")
-            self.assertEqual(str(it.next()), "should")
-        except Exception,e:
-            self.fail("test_iteration exception group 7: " + str(e))
-
-        #        "list" ... like C++ vector
-        try:
-            tmp = list()           # create empty vector
-            h.getKeys(tmp)         # fill vector by keys
-            it = iter(tmp)
-            self.assertEqual(str(it.next()), "should")
-            self.assertEqual(str(it.next()), "iterated")
-            self.assertEqual(str(it.next()), "in")
-            self.assertEqual(str(it.next()), "correct")
-            self.assertEqual(str(it.next()), "order")
-            self.assertEqual(str(it.next()), "be")
-        except Exception,e:
-            self.fail("test_iteration exception group 8: " + str(e))
-
-        try:
-            h = Hash("b", "bla-la-la", "a.b.c", 1, "abc.2", 2.2222222, "a.b.c1.d", "abc1d", "abc.1", 1.11111111)
-            
-            l = [];
-            h.getKeys(l)    # use top level keys
-            self.assertEqual(len(l), 3)
-            i = iter(l)                         # "canonical" order: on every level insertion order
-            self.assertEqual(str(i.next()), "b")
-            self.assertEqual(str(i.next()), "a")
-            self.assertEqual(str(i.next()), "abc")
-            
-            l = [];
-            h.getPaths(l)   # use full keys 
-            self.assertEqual(len(l), 5)
-            i = iter(l)
-            self.assertEqual(str(i.next()), "b")
-            self.assertEqual(str(i.next()), "a.b.c")
-            self.assertEqual(str(i.next()), "a.b.c1.d")
-            self.assertEqual(str(i.next()), "abc.2")
-            self.assertEqual(str(i.next()), "abc.1")
-        except Exception,e:
-            self.fail("test_iteration exception group 9: " + str(e))
+        l = [];
+        h.getPaths(l)   # use full keys
+        self.assertEqual(len(l), 5)
+        i = iter(l)
+        self.assertEqual(str(i.next()), "b")
+        self.assertEqual(str(i.next()), "a.b.c")
+        self.assertEqual(str(i.next()), "a.b.c1.d")
+        self.assertEqual(str(i.next()), "abc.2")
+        self.assertEqual(str(i.next()), "abc.1")
 
 if __name__ == '__main__':
     unittest.main()
