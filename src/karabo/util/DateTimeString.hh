31750e73cb5dfeb251ee7e44b71ea6a6aaab7106
maial <maial@a23ab9f9-1ea2-4934-960d-990259f9ea8c>
2014-03-19 08:43:33 +0000

    Correct files to allow trunk compilation (XFEL #330)
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/trunk@13249 a23ab9f9-1ea2-4934-960d-990259f9ea8c

--- src/karabo/util/DateTimeString.hh
+++ src/karabo/util/DateTimeString.hh
@@ -17,6 +17,7 @@ namespace karabo {
     namespace util {
 
         class DateTimeString {
+
             // Considering the following example: 2013-01-20T20:30:00.123456Z
             // each string should contain the following values:
             std::string m_dateString; //2013-01-20
@@ -43,42 +44,45 @@ namespace karabo {
              * @param inputTimeZoneStr String that represents the time zone part of the Karabo agreed ISO-8601 subset API
              */
             DateTimeString(const std::string& inputDateStr, const std::string& inputTimeStr,
-                    const std::string& inputFractionSecondStr, const std::string& inputTimeZoneStr);
-            
+                           const std::string& inputFractionSecondStr, const std::string& inputTimeZoneStr);
 
-//            DateTimeString(const DateTimeString& orig);
-            
+
+            //            DateTimeString(const DateTimeString& orig);
+
             virtual ~DateTimeString();
-            
+
+            /**
+             * Get the second (resp. fractional of a second) part, 
+             * @return unsigned int 64bits
+             */
+            inline const std::string& getDateString() const {
+                return m_dateString;
+            }
+
+            inline const std::string& getDateTimeString() const {
+                return m_dateTimeString;
+            }
+
+            inline const std::string& getFractionalSecondString() const {
+                return m_fractionalSecondString;
+            }
+
+            inline const std::string& getTimeString() const {
+                return m_timeString;
+            }
+
+            inline const std::string& getTimeZoneString() const {
+                return m_timeZoneString;
+            }
+
             const bool isStringValidIso8601(const std::string& timePoint);
-            
+
             const bool isStringKaraboValidIso8601(const std::string& timePoint);
-            
+
         private:
 
         };
     }
-
-    inline const std::string& GetDateString() const {
-        return m_dateString;
-    }
-
-    inline const std::string& GetDateTimeString() const {
-        return m_dateTimeString;
-    }
-
-    inline const std::string& GetFractionalSecondString() const {
-        return m_fractionalSecondString;
-    }
-
-    inline const std::string& GetTimeString() const {
-        return m_timeString;
-    }
-
-    inline const std::string& GetTimeZoneString() const {
-        return m_timeZoneString;
-    }
-
 }
 
 #endif	/* DATETIMESTRING_HH */
