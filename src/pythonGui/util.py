404404b3b4d0c0fad9f99a51f3399cb391915bdb
Martin Teichmann <martin.teichmann@xfel.eu>
2014-03-27 10:31:33 +0100

    introduce a SignalBlocker
    this way we can block signals with a context manager

--- src/pythonGui/util.py
+++ src/pythonGui/util.py
@@ -11,3 +11,16 @@ class Weak(object):
 
     def __delete__(self, instance):
         del instance.__dict__[self]
+
+
+class SignalBlocker(object):
+    def __init__(self, object):
+        self.object = object
+
+
+    def __enter__(self):
+        self.state = self.object.blockSignals(True)
+
+
+    def __exit__(self, a, b, c):
+        self.object.blockSignals(self.state)
