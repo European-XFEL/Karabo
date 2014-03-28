31034d4d75ca8f6de84c7953a557a686f11b8c56
jszuba <janusz.szuba@xfel.eu>
2014-03-28 14:21:03 +0000

    updating makefiles for Mac OS X with GNU_MacPorts toolchain
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/trunk@13363 a23ab9f9-1ea2-4934-960d-990259f9ea8c

--- build/netbeans/brokerMessageLogger/nbproject/Karabo-variables-Darwin.mk
+++ build/netbeans/brokerMessageLogger/nbproject/Karabo-variables-Darwin.mk
@@ -7,21 +7,21 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=GNU-MacOSX
-CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-MacOSX/bin
+CND_PLATFORM_Debug=GNU_MacPorts-MacOSX
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU_MacPorts-MacOSX/bin
 CND_ARTIFACT_NAME_Debug=karabo-brokermessagelogger
-CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-MacOSX/bin/karabo-brokermessagelogger
-CND_PACKAGE_DIR_Debug=dist/Debug/GNU-MacOSX/package
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU_MacPorts-MacOSX/bin/karabo-brokermessagelogger
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU_MacPorts-MacOSX/package
 CND_PACKAGE_NAME_Debug=brokermessagelogger.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/GNU-MacOSX/package/brokermessagelogger.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU_MacPorts-MacOSX/package/brokermessagelogger.tar
 # Release configuration
-CND_PLATFORM_Release=GNU-MacOSX
-CND_ARTIFACT_DIR_Release=dist/Release/GNU-MacOSX/bin
+CND_PLATFORM_Release=GNU_MacPorts-MacOSX
+CND_ARTIFACT_DIR_Release=dist/Release/GNU_MacPorts-MacOSX/bin
 CND_ARTIFACT_NAME_Release=karabo-brokermessagelogger
-CND_ARTIFACT_PATH_Release=dist/Release/GNU-MacOSX/bin/karabo-brokermessagelogger
-CND_PACKAGE_DIR_Release=dist/Release/GNU-MacOSX/package
+CND_ARTIFACT_PATH_Release=dist/Release/GNU_MacPorts-MacOSX/bin/karabo-brokermessagelogger
+CND_PACKAGE_DIR_Release=dist/Release/GNU_MacPorts-MacOSX/package
 CND_PACKAGE_NAME_Release=brokermessagelogger.tar
-CND_PACKAGE_PATH_Release=dist/Release/GNU-MacOSX/package/brokermessagelogger.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU_MacPorts-MacOSX/package/brokermessagelogger.tar
 #
 # include compiler specific variables
 #
