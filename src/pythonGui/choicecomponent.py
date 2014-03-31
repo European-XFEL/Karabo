c872efd45d9e9008e402ace87a72dcd6cb0f5e4f
Martin Teichmann <martin.teichmann@xfel.eu>
2014-03-31 13:13:41 +0200

    unifiy __init__ parameters of all components
    makes calling them generically easier.

--- src/pythonGui/choicecomponent.py
+++ src/pythonGui/choicecomponent.py
@@ -23,7 +23,7 @@ from PyQt4.QtGui import *
 class ChoiceComponent(BaseComponent):
 
 
-    def __init__(self, classAlias, box, parent):
+    def __init__(self, classAlias, box, parent, widgetFactory=None):
         super(ChoiceComponent, self).__init__(classAlias)
         
         self.__choiceWidget = EditableWidget.get_class(classAlias)(box, parent)
