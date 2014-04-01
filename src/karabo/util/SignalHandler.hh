deb28ba2c04ef93f90be020255c2da87dfd11fbc
boukhele <djelloul.boukhelef@xfel.eu>
2014-04-01 07:39:57 +0000

    XFEL-CLOSED #546 Feature: Implement signal handler support (for Linux systems)
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/trunk@13373 a23ab9f9-1ea2-4934-960d-990259f9ea8c

---
+++ src/karabo/util/SignalHandler.hh
@@ -0,0 +1,248 @@
+/* 
+ * File:   SignalHandler.hh
+ * Author: boukhelef
+ *
+ * Created on March 31, 2014, 3:52 PM
+ */
+
+#ifndef SIGNALHANDLER_HH
+#define	SIGNALHANDLER_HH
+
+
+#ifdef __linux__
+#include <limits.h>
+#include <setjmp.h>
+#include <pthread.h>
+#include <signal.h>
+
+#if defined(__GNUC__)
+#if defined(__GLIBC__)
+#ifndef __UCLIBC__
+#define _HAVE_EXECINFO_H_ 1
+#endif
+#endif
+#endif
+
+#ifdef _HAVE_EXECINFO_H_
+#include <execinfo.h>
+#include <cxxabi.h>
+#endif
+
+#endif
+
+#include <exception>
+#include <boost/thread/pthread/mutex.hpp>
+
+namespace karabo {
+    namespace util {
+
+        #ifdef __linux__
+
+        jmp_buf context;
+
+        /*************************************************************
+         * 
+         * StackTrace prints out the exception stack, 
+         * Symbols are, in the best case, C++ demangled.
+         * 
+         *-----------------------------------------------------------*/
+
+        class StackTrace {
+
+        public:
+            static std::string demangle(const char* symbol);
+            static void print(std::ostream& os);
+        };
+
+        std::ostream& operator <<(std::ostream& os, const StackTrace& trace) {
+            trace.print(os);
+            return os;
+        }
+
+        /*************************************************************
+         * 
+         * SignalHandler class install signal handler provided at the template argument
+         * 
+         *-----------------------------------------------------------*/
+
+
+        template <class SignalExceptionClass> class SignalHandler {
+
+        private:
+
+            class Singleton {
+
+            public:
+
+                Singleton() {
+                    // TODO: save/restore previous signal handler
+                    struct sigaction sa;
+                    memset(&sa, 0, sizeof (struct sigaction));
+                    sigemptyset(&sa.sa_mask);
+                    sa.sa_sigaction = HandleSignal;
+                    sa.sa_flags = SA_SIGINFO | SA_ONSTACK; // | SA_RESTART | SA_RESETHAND
+
+                    sigaction(SignalExceptionClass::GetSignalNumber(), &sa, NULL);
+                }
+
+                static void HandleSignal(int signum, siginfo_t *siginfo, void *ctx) {
+
+                    switch (SignalExceptionClass::PreProcessing()) {
+                        case 0: throw SignalExceptionClass();
+                            break;
+                        case 1:
+                            // Jump to the cleaning section
+                            longjmp(context, signum);
+                            break;
+                        default:;
+                    }
+                }
+            };
+
+        public:
+
+            SignalHandler() {
+                static Singleton __SignalHandler;
+            }
+        };
+
+        // Handler for SIGSEGV
+
+        class SegmentationViolation : public std::exception {
+
+        public:
+
+            static int GetSignalNumber() {
+                return SIGSEGV;
+            }
+
+            static int PreProcessing() {
+                return 0;
+            }
+        };
+        SignalHandler<SegmentationViolation> __SegmentationFaultHandler;
+
+        // Handler for generic exception
+
+        class GenericException : public std::exception {
+
+        public:
+
+            static int GetSignalNumber() {
+                return _NSIG;
+            }
+
+            static int PreProcessing() {
+                return 0;
+            }
+        };
+        SignalHandler<GenericException> __GenericExceptionHandler;
+
+        // Handler for SIGFPE
+
+        class FloatingPointException : public std::exception {
+
+        public:
+
+            static int GetSignalNumber() {
+                return SIGFPE;
+            }
+
+            static int PreProcessing() {
+                return 0;
+            }
+        };
+        SignalHandler<FloatingPointException> __FloatingPointExceptionHandler;
+
+        // Handler for SIGINT
+
+        class InterruptSignal : public std::exception {
+
+        public:
+
+            static int GetSignalNumber() {
+                return SIGINT;
+            }
+
+            static int PreProcessing() {
+                return 1;
+            }
+        };
+        SignalHandler<InterruptSignal> __InterruptSignalHandler;
+
+        // Handler for SIGQUIT
+
+        class QuitSignal : public std::exception {
+
+        public:
+
+            static int GetSignalNumber() {
+                return SIGQUIT;
+            }
+
+            static int PreProcessing();
+        };
+        SignalHandler<QuitSignal> __QuitSignalHandler;
+
+        // Handler for SIGTERM
+
+        class TerminateSignal : public std::exception {
+
+        public:
+
+            static int GetSignalNumber() {
+                return SIGTERM;
+            }
+
+            static int PreProcessing() {
+                return 1;
+            }
+        };
+        SignalHandler<TerminateSignal> __TerminateSignalHandler;
+
+        /*************************************************************
+         * 
+         * Generic exceptions and signals handler
+         * This class handles signals using two methods:
+         * TerminateCallback: handles signals (eg. SIGSEGV,...) that were caught by other thread 
+         * and were translated into exceptions using the SignalHandler translator
+         * SignalThread: Handles other signals (eg. SIGINT, SIGTERM, ...)
+         * This trick improves the robustness of the application by delegating one thread to handle all async. signals.
+         * In the best case, the other worker threads continue running without being disturbed.
+         * 
+         *-----------------------------------------------------------*/
+
+        class GlobalExceptionHandler {
+
+        private:
+
+            class Singleton {
+
+                static boost::mutex m_mutex;
+            public:
+
+                Singleton() {
+                    std::set_terminate(TerminateCallback);
+                }
+
+                static void TerminateCallback();
+            };
+
+        public:
+            static void *SignalThread(void *arg);
+
+            GlobalExceptionHandler() {
+                static Singleton __GlobalExceptionHandler;
+            }
+        };
+
+        // Global instance of ExceptionHandler
+        GlobalExceptionHandler __GlobalExceptionHandler;
+
+        #endif
+
+    }
+}
+
+#endif	/* SIGNALHANDLER_HH */
+
