4f824589ca48b3b61c0c9230a0751a8c16a61aad
esenov <sergey.esenov@xfel.eu>
2014-04-08 09:43:21 +0000

    Refactoring...
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/trunk@13444 a23ab9f9-1ea2-4934-960d-990259f9ea8c

--- src/pythonKarabo/tests/__init__.py
+++ src/pythonKarabo/tests/__init__.py
@@ -1,9 +1,2 @@
 __author__="Sergey Esenov <serguei.essenov at xfel.eu>"
 __date__ ="$Oct 16, 2012 3:22:47 PM$"
-
-import sys
-from nose import main
-
-if __name__ == '__main__':
-    sys.argv.append("-sv")
-    main()
