fcf084f606180ce8bffb40578511085ca3f05857
Kerstin Weger <kerstin.weger@xfel.eu>
2014-05-19 15:55:30 +0200

    - set descriptor correctly

--- src/pythonGui/configuration.py
+++ src/pythonGui/configuration.py
@@ -46,7 +46,6 @@ class Configuration(QObject):
 
     def setSchema(self, schema):
         self._box.descriptor = Schema.parse(schema.name, schema.hash, {})
-        print "emit.setSchema", self.path
         self.signalConfigurationNewDescriptor.emit(self)
 
 
