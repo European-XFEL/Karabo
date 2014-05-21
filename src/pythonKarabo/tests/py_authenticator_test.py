f878aeca03b14bfab9e6844169d7f51f633e132f
Martin Teichmann <martin.teichmann@xfel.eu>
2014-05-21 13:40:49 +0200

    updated unit test for python authenticator

--- src/pythonKarabo/tests/py_authenticator_test.py
+++ src/pythonKarabo/tests/py_authenticator_test.py
@@ -2,176 +2,176 @@
 # and open the template in the editor.
 
 import unittest
-from karabo.karathon import * 
-from karabo.py_authenticator import PyAuthenticator
+from karabo.authenticator import Authenticator
 
+from datetime import datetime
 
-class  PyAuthenticator_TestCase(unittest.TestCase):
-    #def setUp(self):
-    #    self.foo = Authenticator_()
-    #
+class  Authenticator_TestCase(unittest.TestCase):
+    username = 'unitaryTests'
+    provider = 'LOCAL'
+    brokerHostname = '127.0.0.1'
+    brokerPortNumber = 4444
+    brokerTopic = 'topic'
+    software = 'Karabo'
+    ipAddress = 'PythonUnitTestsIpAddress' + datetime.utcnow().isoformat()
 
-    #def tearDown(self):
-    #    self.foo.dispose()
-    #    self.foo = None
 
     def test_py_authenticator_correct_login(self):
-        
-        # Variables definition
-        username = 'unitaryTests'
         password = 'karaboUnitaryTestsPass'
-        provider = 'LOCAL'
-        brokerHostname = '127.0.0.1'
-        brokerPortNumber = 4444
-        brokerTopic = 'topic'
-        software = 'Karabo'
-        #
-        current_epo = Epochstamp()
-        ipAddress = 'PythonUnitTestsIpAddress' + current_epo.toIso8601Ext(TIME_UNITS.ATTOSEC)
-        
+
         # Helper variables
-        emptyString = ''
         functionName = 'test_authenticator_correct_login'
-        
-        # Check properties of empty Authenticator
-        try:
-            a = PyAuthenticator(username, password, provider, ipAddress, brokerHostname, brokerPortNumber, brokerTopic)
-            
-            # Variables that should be correctly assigned
-            self.assertEqual(a.getUsername(), username, "getUsername() don't match")
-            self.assertEqual(a.getProvider(), provider, "getProvider() don't match")
-            self.assertEqual(a.getIpAddress(), ipAddress, "getIpAddress() don't match")
-            self.assertEqual(a.getBrokerHostname(), brokerHostname, "getBrokerHostname() don't match")
-            self.assertEqual(int(a.getBrokerPortNumber()), brokerPortNumber, "getBrokerPortNumber() don't match")
-            self.assertEqual(a.getBrokerTopic(), brokerTopic, "getBrokerTopic() don't match")
-            self.assertEqual(a.getSoftware(), software, "getSoftware() don't match")
-            
-            
-            # Variables that should be empty
-            self.assertEqual(a.getDefaultAccessLevelDesc(), emptyString, "getDefaultAccessLevelDesc() is not Empty")
-            self.assertEqual(a.getWelcomeMessage(), emptyString, "getWelcomeMessage() is not Empty")
-            self.assertEqual(a.getSessionToken(), emptyString, "getSessionToken() is not Empty")
-            #
-            self.assertEqual(a.getDefaultAccessLevelId(), -100, "getDefaultAccessLevelId() is not undifined")
-            self.assertEqual(a.getSoftwareId(), -100, "getSoftwareId() is not undifined")
-            self.assertEqual(a.getUserId(), -100, "getUserId() is not undifined")
-        except Exception as e:
-            self.fail(functionName + " creation exception before LOGIN: " + str(e))
-        
-        # Execute Login
-        try:
-            self.assertTrue(a.login())
-            
-            # Variables that should be correctly assigned
-            self.assertEqual(a.getUsername(), username, "getUsername() don't match")
-            self.assertEqual(a.getProvider(), provider, "getProvider() don't match")
-            self.assertEqual(a.getIpAddress(), ipAddress, "getIpAddress() don't match")
-            self.assertEqual(a.getBrokerHostname(), brokerHostname, "getBrokerHostname() don't match")
-            self.assertEqual(int(a.getBrokerPortNumber()), brokerPortNumber, "getBrokerPortNumber() don't match")
-            self.assertEqual(a.getBrokerTopic(), brokerTopic, "getBrokerTopic() don't match")
-            self.assertEqual(a.getSoftware(), software, "getSoftware() don't match")
-            
-            # Variables that should be empty
-            self.assertEqual(a.getDefaultAccessLevelDesc(), "USER")
-            self.assertNotEqual(a.getWelcomeMessage(), emptyString, "getWelcomeMessage() is Empty")
-            self.assertNotEqual(a.getSessionToken(), emptyString, "getSessionToken() is Empty")
-            #
-            self.assertEqual(a.getDefaultAccessLevelId(), 1, "getDefaultAccessLevelId() is not undifined")
-            self.assertEqual(a.getSoftwareId(), 1, "getSoftwareId() is not undifined")
-            self.assertEqual(a.getUserId(), -99, "getUserId() is not undifined")
-        except Exception as e:
-            self.fail(functionName + " exception after LOGIN: " + str(e))
-        
+
+        a = Authenticator(self.username, password, self.provider,
+                          self.ipAddress, self.brokerHostname,
+                          self.brokerPortNumber, self.brokerTopic)
+
+        # Variables that should be correctly assigned
+        self.assertEqual(a.username, self.username)
+        self.assertEqual(a.provider, self.provider)
+        self.assertEqual(a.currentIpAddress, self.ipAddress)
+        self.assertEqual(a.brokerHostname, self.brokerHostname)
+        self.assertEqual(int(a.brokerPortNumber), self.brokerPortNumber)
+        self.assertEqual(a.brokerTopic, self.brokerTopic)
+        self.assertEqual(a.softwareDesc, self.software)
+
+
+        # Variables that should be empty
+        self.assertEqual(a.defaultAccessLevelDesc, "")
+        self.assertEqual(a.welcomeMessage, "")
+        self.assertEqual(a.sessionToken, "")
+        #
+        self.assertEqual(a.defaultAccessLevelId, -100)
+        self.assertEqual(a.softwareId, -100)
+        self.assertEqual(a.userId, -100)
+
+        self.assertTrue(a.login())
+
+        # Variables that should be correctly assigned
+        self.assertEqual(a.username, self.username)
+        self.assertEqual(a.provider, self.provider)
+        self.assertEqual(a.currentIpAddress, self.ipAddress)
+        self.assertEqual(a.brokerHostname, self.brokerHostname)
+        self.assertEqual(int(a.brokerPortNumber), self.brokerPortNumber)
+        self.assertEqual(a.brokerTopic, self.brokerTopic)
+        self.assertEqual(a.softwareDesc, self.software)
+
+        # Variables that should be empty
+        self.assertEqual(a.defaultAccessLevelDesc, "USER")
+        self.assertNotEqual(a.welcomeMessage, "")
+        self.assertNotEqual(a.sessionToken, "")
+        #
+        self.assertEqual(a.defaultAccessLevelId, 1)
+        self.assertEqual(a.softwareId, 1)
+        self.assertEqual(a.userId, -99)
+
         # Execute Logout
-        try:
-            self.assertTrue(a.logout())
-            
-            # Variables that should be correctly assigned
-            self.assertEqual(a.getUsername(), username, "getUsername() don't match")
-            self.assertEqual(a.getProvider(), provider, "getProvider() don't match")
-            self.assertEqual(a.getIpAddress(), ipAddress, "getIpAddress() don't match")
-            self.assertEqual(a.getBrokerHostname(), brokerHostname, "getBrokerHostname() don't match")
-            self.assertEqual(int(a.getBrokerPortNumber()), brokerPortNumber, "getBrokerPortNumber() don't match")
-            self.assertEqual(a.getBrokerTopic(), brokerTopic, "getBrokerTopic() don't match")
-            self.assertEqual(a.getSoftware(), software, "getSoftware() don't match")
-            
-            # Variables that should be empty
-            self.assertEqual(a.getDefaultAccessLevelDesc(), emptyString, "getDefaultAccessLevelDesc() is not Empty")
-            self.assertEqual(a.getWelcomeMessage(), emptyString, "getWelcomeMessage() is not Empty")
-            self.assertEqual(a.getSessionToken(), emptyString, "getSessionToken() is not Empty")
-            #
-            self.assertEqual(a.getDefaultAccessLevelId(), -100, "getDefaultAccessLevelId() is not undifined")
-            self.assertEqual(a.getSoftwareId(), -100, "getSoftwareId() is not undifined")
-            self.assertEqual(a.getUserId(), -100, "getUserId() is not undifined")
-        except Exception as e:
-            self.fail(functionName + " exception after LOGOUT: " + str(e))
+        self.assertTrue(a.logout())
+
+        # Variables that should be correctly assigned
+        self.assertEqual(a.username, self.username, "username don't match")
+        self.assertEqual(a.provider, self.provider, "provider don't match")
+        self.assertEqual(a.currentIpAddress, self.ipAddress,
+                         "ipAddress don't match")
+        self.assertEqual(a.brokerHostname, self.brokerHostname,
+                         "brokerHostname don't match")
+        self.assertEqual(int(a.brokerPortNumber), self.brokerPortNumber,
+                         "brokerPortNumber don't match")
+        self.assertEqual(a.brokerTopic, self.brokerTopic,
+                         "brokerTopic don't match")
+        self.assertEqual(a.softwareDesc, self.software, "software don't match")
+
+
+        # Variables that should be empty
+        self.assertEqual(a.defaultAccessLevelDesc, "",
+                         "defaultAccessLevelDesc is not Empty")
+        self.assertEqual(a.welcomeMessage, "",
+                         "welcomeMessage is not Empty")
+        self.assertEqual(a.sessionToken, "", "sessionToken() is not Empty")
+        #
+        self.assertEqual(a.defaultAccessLevelId, -100,
+                         "defaultAccessLevelId is not undefined")
+        self.assertEqual(a.softwareId, -100, "softwareId is not undefined")
+        self.assertEqual(a.userId, -100, "userId is not undifined")
 
 
     def test_py_authenticator_incorrect_login(self):
-        
-        # Variables definition
-        username = 'unitaryTests'
-        password = 'karaboUnitaryTestsPass222'
-        provider = 'LOCAL'
-        brokerHostname = '127.0.0.1'
-        brokerPortNumber = 4444
-        brokerTopic = 'topic'
-        software = 'Karabo'
-        #
-        current_epo = Epochstamp()
-        ipAddress = 'PythonUnitTestsIpAddress' + current_epo.toIso8601Ext(TIME_UNITS.ATTOSEC)
-        
+        password = 'karaboUnitaryTestsNotPass'
+
         # Helper variables
-        emptyString = ''
-        functionName = 'test_authenticator_incorrect_login'
-        
-        # Check properties of empty Authenticator
-        try:
-            a = PyAuthenticator(username, password, provider, ipAddress, brokerHostname, brokerPortNumber, brokerTopic)
-            
-            # Variables that should be correctly assigned
-            self.assertEqual(a.getUsername(), username, "getUsername() don't match")
-            self.assertEqual(a.getProvider(), provider, "getProvider() don't match")
-            self.assertEqual(a.getIpAddress(), ipAddress, "getIpAddress() don't match")
-            self.assertEqual(a.getBrokerHostname(), brokerHostname, "getBrokerHostname() don't match")
-            self.assertEqual(int(a.getBrokerPortNumber()), brokerPortNumber, "getBrokerPortNumber() don't match")
-            self.assertEqual(a.getBrokerTopic(), brokerTopic, "getBrokerTopic() don't match")
-            self.assertEqual(a.getSoftware(), software, "getSoftware() don't match")
-            
-            # Variables that should be empty
-            self.assertEqual(a.getDefaultAccessLevelDesc(), emptyString, "getDefaultAccessLevelDesc() is not Empty")
-            self.assertEqual(a.getWelcomeMessage(), emptyString, "getWelcomeMessage() is not Empty")
-            self.assertEqual(a.getSessionToken(), emptyString, "getSessionToken() is not Empty")
-            #
-            self.assertEqual(a.getDefaultAccessLevelId(), -100, "getDefaultAccessLevelId() is not undifined")
-            self.assertEqual(a.getSoftwareId(), -100, "getSoftwareId() is not undifined")
-            self.assertEqual(a.getUserId(), -100, "getUserId() is not undifined")
-        except Exception as e:
-            self.fail(functionName + " creation exception before LOGIN: " + str(e))
-        
-        # Execute Login
-        try:
-            self.assertFalse(a.login())
-            
-            # Variables that should be correctly assigned
-            self.assertEqual(a.getUsername(), username, "getUsername() don't match")
-            self.assertEqual(a.getProvider(), provider, "getProvider() don't match")
-            self.assertEqual(a.getIpAddress(), ipAddress, "getIpAddress() don't match")
-            self.assertEqual(a.getBrokerHostname(), brokerHostname, "getBrokerHostname() don't match")
-            self.assertEqual(int(a.getBrokerPortNumber()), brokerPortNumber, "getBrokerPortNumber() don't match")
-            self.assertEqual(a.getBrokerTopic(), brokerTopic, "getBrokerTopic() don't match")
-            self.assertEqual(a.getSoftware(), software, "getSoftware() don't match")
-            
-            # Variables that should be empty
-            self.assertEqual(a.getDefaultAccessLevelDesc(), emptyString, "getDefaultAccessLevelDesc() is not Empty")
-            self.assertEqual(a.getWelcomeMessage(), emptyString, "getWelcomeMessage() is not Empty")
-            self.assertEqual(a.getSessionToken(), emptyString, "getSessionToken() is not Empty")
-            #
-            self.assertEqual(a.getDefaultAccessLevelId(), -100, "getDefaultAccessLevelId() is not undifined")
-            self.assertEqual(a.getSoftwareId(), -100, "getSoftwareId() is not undifined")
-            self.assertEqual(a.getUserId(), -100, "getUserId() is not undifined")
-        except Exception as e:
-            self.fail(functionName + " exception after LOGIN: " + str(e))
+        functionName = 'test_authenticator_correct_login'
+
+        a = Authenticator(self.username, password, self.provider,
+                          self.ipAddress, self.brokerHostname,
+                          self.brokerPortNumber, self.brokerTopic)
+
+        # Variables that should be correctly assigned
+        self.assertEqual(a.username, self.username)
+        self.assertEqual(a.provider, self.provider)
+        self.assertEqual(a.currentIpAddress, self.ipAddress)
+        self.assertEqual(a.brokerHostname, self.brokerHostname)
+        self.assertEqual(int(a.brokerPortNumber), self.brokerPortNumber)
+        self.assertEqual(a.brokerTopic, self.brokerTopic)
+        self.assertEqual(a.softwareDesc, self.software)
+
+
+        # Variables that should be empty
+        self.assertEqual(a.defaultAccessLevelDesc, "")
+        self.assertEqual(a.welcomeMessage, "")
+        self.assertEqual(a.sessionToken, "")
+        #
+        self.assertEqual(a.defaultAccessLevelId, -100)
+        self.assertEqual(a.softwareId, -100)
+        self.assertEqual(a.userId, -100)
+
+        self.assertFalse(a.login())
+
+        # Variables that should be correctly assigned
+        self.assertEqual(a.username, self.username)
+        self.assertEqual(a.provider, self.provider)
+        self.assertEqual(a.currentIpAddress, self.ipAddress)
+        self.assertEqual(a.brokerHostname, self.brokerHostname)
+        self.assertEqual(int(a.brokerPortNumber), self.brokerPortNumber)
+        self.assertEqual(a.brokerTopic, self.brokerTopic)
+        self.assertEqual(a.softwareDesc, self.software)
+
+        # Variables that should be empty
+        self.assertEqual(a.defaultAccessLevelDesc, "")
+        self.assertEqual(a.welcomeMessage, "")
+        self.assertEqual(a.sessionToken, "")
+        #
+        self.assertEqual(a.defaultAccessLevelId, -100)
+        self.assertEqual(a.softwareId, -100)
+        self.assertEqual(a.userId, -100)
+
+        # Execute Logout
+        self.assertFalse(a.logout())
+
+        # Variables that should be correctly assigned
+        self.assertEqual(a.username, self.username, "username don't match")
+        self.assertEqual(a.provider, self.provider, "provider don't match")
+        self.assertEqual(a.currentIpAddress, self.ipAddress,
+                         "ipAddress don't match")
+        self.assertEqual(a.brokerHostname, self.brokerHostname,
+                         "brokerHostname don't match")
+        self.assertEqual(int(a.brokerPortNumber), self.brokerPortNumber,
+                         "brokerPortNumber don't match")
+        self.assertEqual(a.brokerTopic, self.brokerTopic,
+                         "brokerTopic don't match")
+        self.assertEqual(a.softwareDesc, self.software, "software don't match")
+
+
+        # Variables that should be empty
+        self.assertEqual(a.defaultAccessLevelDesc, "",
+                         "defaultAccessLevelDesc is not Empty")
+        self.assertEqual(a.welcomeMessage, "",
+                         "welcomeMessage is not Empty")
+        self.assertEqual(a.sessionToken, "", "sessionToken() is not Empty")
+        #
+        self.assertEqual(a.defaultAccessLevelId, -100,
+                         "defaultAccessLevelId is not undefined")
+        self.assertEqual(a.softwareId, -100, "softwareId is not undefined")
+        self.assertEqual(a.userId, -100, "userId is not undifined")
 
 
 if __name__ == '__main__':
