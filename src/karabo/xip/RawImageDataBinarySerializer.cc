bb1cbad8ab0c41f9cccd49c41cb50c3f91ae3658
heisenb <burkard.heisen@xfel.eu>
2014-06-06 14:50:03 +0000

    For the RawImageData object to be shipped across the p2p connections it must be registered to all involved factories. This commit add the forgotten registration to the CppInputHandler
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/trunk@13749 a23ab9f9-1ea2-4934-960d-990259f9ea8c

--- src/karabo/xip/RawImageDataBinarySerializer.cc
+++ src/karabo/xip/RawImageDataBinarySerializer.cc
@@ -8,6 +8,7 @@
 
 #include "RawImageDataBinarySerializer.hh"
 #include "karabo/io/BinaryFileOutput.hh"
+#include <karabo/io/CppInputHandler.hh>
 #include <karabo/xms/NetworkOutput.hh>
 #include <karabo/xms/NetworkInput.hh>
 
@@ -21,6 +22,9 @@ namespace karabo {
                 
         KARABO_REGISTER_FOR_CONFIGURATION(karabo::io::Output<RawImageData >, karabo::xms::NetworkOutput<RawImageData >)
         KARABO_REGISTER_FOR_CONFIGURATION(karabo::io::Input<RawImageData >, karabo::xms::NetworkInput<RawImageData >)
+
+        KARABO_REGISTER_IN_FACTORY_1(karabo::io::InputHandler, karabo::io::CppInputHandler<karabo::io::Input<RawImageData> > , karabo::io::AbstractInput::Pointer);
+
     }
 }
 
