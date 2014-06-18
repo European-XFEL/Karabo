f1025a2a3f55b5c50c4c08967e15565f5b39c48b
Martin Teichmann <martin.teichmann@xfel.eu>
2014-06-18 17:07:03 +0200

    show whether value is to high or low for doubles

--- src/pythonGui/editablewidgets/editabledoublespinbox.py
+++ src/pythonGui/editablewidgets/editabledoublespinbox.py
@@ -37,10 +37,20 @@ class EditableDoubleSpinBox(EditableWidget):
         self.__leDblValue = QLineEdit(parent)
         self.__validator = QDoubleValidator(self.__leDblValue)
         self.__leDblValue.setValidator(self.__validator)
-        self.__leDblValue.textChanged.connect(self.onEditingFinished)
+        self.__leDblValue.editingFinished.connect(self.onEditingFinished)
+        self.__leDblValue.textChanged.connect(self.onTextChanged)
 
         # Needed for updates during input, otherwise cursor jumps to end of input
         self.__lastCursorPos = 0
+        self.normalPalette = self.__leDblValue.palette()
+        self.errorPalette = QPalette(self.normalPalette)
+        self.errorPalette.setColor(QPalette.Text, Qt.red)
+
+
+    def onTextChanged(self, text):
+        self.__leDblValue.setPalette(self.normalPalette
+                                     if self.__leDblValue.hasAcceptableInput()
+                                     else self.errorPalette)
 
 
     @property
@@ -48,12 +58,12 @@ class EditableDoubleSpinBox(EditableWidget):
         return self.__leDblValue
 
 
-    def addParameters(self,minInc=None, maxInc=None, **params):
-        if minInc is not None:
-            self.__validator.setBottom(minInc)
-
-        if maxInc is not None:
-            self.__validator.setTop(maxInc)
+    def typeChanged(self, box):
+        min, max = box.descriptor.getMinMax()
+        if min is not None:
+            self.__validator.setBottom(min)
+        if max is not None:
+            self.__validator.setTop(max)
 
 
     @property
@@ -76,7 +86,6 @@ class EditableDoubleSpinBox(EditableWidget):
             self.onEditingFinished(value)
 
 
-### slots ###
-    def onEditingFinished(self, value):
+    def onEditingFinished(self):
         self.__lastCursorPos = self.__leDblValue.cursorPosition()
-        self.signalEditingFinished.emit(self.boxes[0], float(value))
+        self.signalEditingFinished.emit(self.boxes[0], self.value)
