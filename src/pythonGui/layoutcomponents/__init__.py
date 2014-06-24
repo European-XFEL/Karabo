531dbd74bc1f4e6c48d912b18f5712944637951d
Martin Teichmann <martin.teichmann@xfel.eu>
2014-06-24 14:18:06 +0200

    remove old code

--- src/pythonGui/layoutcomponents/__init__.py
+++
@@ -1,8 +0,0 @@
-#############################################################################
-# Author: <kerstin.weger@xfel.eu>
-# Created on September 25, 2012
-# Copyright (C) European XFEL GmbH Hamburg. All rights reserved.
-#############################################################################
-
-# define package layoutcomponents
-
