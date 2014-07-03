a3079ee65e3e1a307117819f63632124fe3c40d4
Martin Teichmann <martin.teichmann@xfel.eu>
2014-07-03 14:02:48 +0200

    make line edit the default for integers
    as Qt's spin boxes only work for 32 bit signed integers at max,
    we now simply use line edits with a validator, that works fine.

--- src/pythonGui/editablewidgets/editabledoublespinbox.py
+++ src/pythonGui/editablewidgets/editabledoublespinbox.py
@@ -9,29 +9,31 @@ __all__ = ["EditableDoubleSpinBox"]
 
 
 from util import SignalBlocker
-from widget import EditableWidget
+from widget import DisplayWidget, EditableWidget
 
 from PyQt4.QtCore import Qt
-from PyQt4.QtGui import QLineEdit, QDoubleValidator, QPalette
+from PyQt4.QtGui import QLineEdit, QDoubleValidator, QPalette, QValidator
 
 
-class EditableDoubleSpinBox(EditableWidget):
+class NumberLineEdit(EditableWidget, DisplayWidget):
     category = "Digit"
-    alias = "Float Field"
 
     def __init__(self, box, parent):
-        super(EditableDoubleSpinBox, self).__init__(box)
-        
+        super(NumberLineEdit, self).__init__(box)
         self.widget = QLineEdit(parent)
-        self.validator = QDoubleValidator(self.widget)
         self.widget.setValidator(self.validator)
-        self.widget.textChanged.connect(self.onTextChanged)
 
-        # Needed for updates during input, otherwise cursor jumps to end of input
+        # for updates during input, otherwise cursor jumps to end of input
         self.lastCursorPos = 0
         self.normalPalette = self.widget.palette()
         self.errorPalette = QPalette(self.normalPalette)
         self.errorPalette.setColor(QPalette.Text, Qt.red)
+
+
+    def setReadOnly(self, ro):
+        self.widget.setReadOnly(ro)
+        if not ro:
+            self.widget.textChanged.connect(self.onTextChanged)
 
 
     def onTextChanged(self, text):
@@ -49,14 +51,9 @@ class EditableDoubleSpinBox(EditableWidget):
         self.validator.setTop(max)
 
 
-    @property
-    def value(self):
-        return float(self.widget.text())
-
-
     def valueChanged(self, box, value, timestamp=None, forceRefresh=False):
         if value is None:
-            value = 0.0
+            value = 0
 
         with SignalBlocker(self.widget):
             self.widget.setText("{}".format(value))
@@ -66,3 +63,63 @@ class EditableDoubleSpinBox(EditableWidget):
         if forceRefresh:
             # Needs to be called to update possible apply buttons
             self.onEditingFinished(value)
+
+
+class DoubleLineEdit(NumberLineEdit):
+    alias = "Float Field"
+
+    def __init__(self, box, parent):
+        self.validator = QDoubleValidator()
+        NumberLineEdit.__init__(self, box, parent)
+
+
+    @property
+    def value(self):
+        return float(self.widget.text())
+
+
+class IntValidator(QValidator):
+    def __init__(self, min=None, max=None, parent=None):
+        QValidator.__init__(self, parent)
+        self.min = min
+        self.max = max
+
+
+    def validate(self, input, pos):
+        if input in ('+', '-', ''):
+            return self.Intermediate, input, pos
+
+        if not (input.isdigit() or input[0] in '+-' and input[1:].isdigit()):
+            return self.Invalid, input, pos
+
+        if self.min >= 0 and input.startswith('-'):
+            return self.Invalid, input, pos
+
+        if self.max < 0 and input.startswith('+'):
+            return self.Invalid, input, pos
+
+        if self.min <= int(input) <= self.max:
+            return self.Acceptable, input, pos
+        else:
+            return self.Intermediate, input, pos
+
+
+    def setBottom(self, min):
+        self.min = min
+
+
+    def setTop(self, max):
+        self.max = max
+
+
+class IntLineEdit(NumberLineEdit):
+    alias = "Integer Field"
+
+    def __init__(self, box, parent):
+        self.validator = IntValidator()
+        NumberLineEdit.__init__(self, box, parent)
+
+
+    @property
+    def value(self):
+        return int(self.widget.text())
