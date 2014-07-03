f0f12ad329182199f036a320c05d4fd0569f61d7
Martin Teichmann <martin.teichmann@xfel.eu>
2014-07-03 15:01:03 +0200

    add some enums from c++ karabo to python karabo
    so that we can access it in the GUI without karathon

---
+++ src/pythonKarabo/karabo_package/enums.py
@@ -0,0 +1,9 @@
+EncodingType = ("GRAY", "RGB", "RGBA", "BGR", "BGRA", "CMYK", "YUV", "BAYER",
+                "JPEG", "PNG", "RAW") # from karabo.xip.Encoding.EncodingType
+
+ChannelSpaceType = (
+    "u_8_1", "s_8_1", "u_10_2", "s_10_2", "u_12_2", "s_12_2", "u_12_1p5",
+    "s_12_1p5", "u_16_2", "s_16_2", "f_16_2", "u_32_4", "s_32_4", "f_32_4",
+    "u_64_8", "s_64_8", "f_64_8", "u_16_1", "s_16_1", "u_32_1", "s_32_1",
+    "f_32_1", "u_64_1", "s_64_1", "f_64_1")
+
