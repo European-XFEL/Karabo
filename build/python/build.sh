f1812b496f9cf4e17d276db23d1c417faf431757
jszuba <janusz.szuba@xfel.eu>
2014-07-08 13:47:49 +0000

    adding build script and version according to svn tag for tags and branches or revision in case of trunk
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/branches/1.2@14058 a23ab9f9-1ea2-4934-960d-990259f9ea8c

---
+++ build/python/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+tmp=$(svn info ../../ | grep URL)
+VERSION=${tmp##*/}
+if [ "$VERSION" = "trunk" ]; then
+    tmp=$(svn info ../../ | grep Revision)
+    VERSION=r${tmp##*: }
+fi
+
+
+#sed -i "/version/c\    version=\"$VERSION\"," setup.py 
+echo $VERSION > VERSION
+
+( cd ../../extern/resources/suds; unzip -q suds-jurko-0.5.zip )
+
+python setup.py  bdist_wininst
+
+
