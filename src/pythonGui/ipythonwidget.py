cad9f750ec02a0e30f409c1c54f52b2ce163f11d
Kerstin Weger <kerstin.weger@xfel.eu>
2014-07-09 17:25:32 +0200

    - make iPython console work again

---
+++ src/pythonGui/ipythonwidget.py
@@ -0,0 +1,66 @@
+#############################################################################
+# Author: <kerstin.weger@xfel.eu>
+# Created on July 9, 2011
+# Copyright (C) European XFEL GmbH Hamburg. All rights reserved.
+#############################################################################
+
+
+__all__ = ["IPythonWidget"]
+
+# Import the console machinery from ipython
+from IPython.qt.console.rich_ipython_widget import RichIPythonWidget
+from IPython.qt.inprocess import QtInProcessKernelManager
+from IPython.lib import guisupport
+
+
+class IPythonWidget(RichIPythonWidget):
+        
+    """
+    A convenience class for a live IPython console widget.
+    """
+    def __init__(self,customBanner=None, *args, **kwargs):
+        if customBanner is not None:
+            self.banner = customBanner
+        
+        super(IPythonWidget, self).__init__(*args,**kwargs)
+        
+        self.kernel_manager = kernel_manager = QtInProcessKernelManager()
+        kernel_manager.start_kernel()
+        kernel_manager.kernel.gui = 'qt4'
+        
+        self.kernel_client = kernel_client = self._kernel_manager.client()
+        kernel_client.start_channels()
+
+        def stop():
+            kernel_client.stop_channels()
+            kernel_manager.shutdown_kernel()
+            guisupport.get_app_qt4().exit()
+            
+        self.exit_requested.connect(stop)
+
+
+    def pushVariables(self,variableDict):
+        """ Given a dictionary containing name / value pairs, push those variables to the IPython console widget """
+        self.kernel_manager.kernel.shell.push(variableDict)
+
+
+    def clearConsole(self):
+        """
+        The terminal gets cleared.
+        """
+        self._control.clear()
+
+
+    def printText(self, text):
+        """
+        The given \text is printed.
+        """
+        self._append_plain_text(text)
+
+
+    def executeCommand(self, command, inFrame=False):
+        """
+        The given \command is executed in the frame of the console or not.
+        """
+        self._execute(command, inFrame)
+
