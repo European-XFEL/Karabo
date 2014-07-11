c21aafeee9741b5e0f7f9a7cb7fff5df01d9e3b3
Kerstin Weger <kerstin.weger@xfel.eu>
2014-07-11 14:31:24 +0200

    - Bugfix while closing scene tabs - if statement for visibility was not correct

--- src/pythonGui/projectmodel.py
+++ src/pythonGui/projectmodel.py
@@ -333,9 +333,8 @@ class ProjectModel(QStandardItemModel):
         self.removeProject(project)
         
         for scene in project.scenes:
-            if scene.isVisible():
-                # Only send signal, if scene is currently visible
-                self.signalRemoveScene.emit(scene)
+            # Only send signal, if scene is currently visible
+            self.signalRemoveScene.emit(scene)
 
 
     def projectNew(self, filename):
@@ -716,8 +715,7 @@ class ProjectModel(QStandardItemModel):
         project.remove(object)
         
         if isinstance(object, Scene):
-            if object.isVisible():
-                self.signalRemoveScene.emit(object)
+            self.signalRemoveScene.emit(object)
         
         self.updateData()
         
