c116a22dd884d929421be3d71401e64db0648c28
parenti <andrea.parenti@xfel.eu>
2014-08-15 13:55:58 +0000

    Merging from branch 1.2 change 14402.
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/trunk@14403 a23ab9f9-1ea2-4934-960d-990259f9ea8c

--- src/karabo/core/CameraFsm.hh
+++ src/karabo/core/CameraFsm.hh
@@ -57,6 +57,7 @@ namespace karabo {
                 SLOT0(trigger);
                 SLOT0(stop);
                 SLOT0(reset);
+                SLOT2(errorFound, std::string, std::string);
             }
 
 
