1ee82501fb0c79a4cd53c97186768b28293d0349
Kerstin Weger <kerstin.weger@xfel.eu>
2014-09-04 14:48:44 +0200

    Introduce new scene item "WorkflowItem" and paint in scene in dropEvent

---
+++ src/pythonGui/sceneitems/__init__.py
@@ -0,0 +1,7 @@
+#############################################################################
+# Author: <kerstin.weger@xfel.eu>
+# Created on September 4, 2014
+# Copyright (C) European XFEL GmbH Hamburg. All rights reserved.
+#############################################################################
+
+# define package sceneitems
