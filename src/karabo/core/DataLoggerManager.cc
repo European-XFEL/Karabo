60c95f2cfd54d2e149f0793a09479b4b88f50d86
heisenb <burkard.heisen@xfel.eu>
2014-09-04 09:45:40 +0000

    Added skeleton classes for the prototype ASCII based data-logger. Removed two obsolete files (not even present in the project)
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/trunk@14691 a23ab9f9-1ea2-4934-960d-990259f9ea8c

---
+++ src/karabo/core/DataLoggerManager.cc
@@ -0,0 +1,139 @@
+/*
+ * $Id$
+ *
+ * Author: <burkhard.heisen@xfel.eu>
+ *
+ * Copyright (C) European XFEL GmbH Hamburg. All rights reserved.
+ */
+
+
+#include <karabo/io/Input.hh>
+#include "DataLoggerManager.hh"
+#include "karabo/io/FileTools.hh"
+
+namespace karabo {
+    namespace core {
+
+        using namespace krb_log4cpp;
+        using namespace std;
+        using namespace karabo::util;
+        using namespace karabo::io;
+
+
+        KARABO_REGISTER_FOR_CONFIGURATION(BaseDevice, Device<OkErrorFsm>, DataLoggerManager)
+
+        void DataLoggerManager::expectedParameters(Schema& expected) {
+
+            INT32_ELEMENT(expected).key("flushInterval")
+                    .displayedName("Flush interval")
+                    .description("The interval after which the memory accumulated data is made persistent")
+                    .unit(Unit::SECOND)
+                    .assignmentOptional().defaultValue(40)
+                    .reconfigurable()
+                    .commit();
+
+            STRING_ELEMENT(expected).key("fileFormat")
+                    .displayedName("File format")
+                    .description("The file format to use for logging")
+                    .options("xml, bin, hdf5")
+                    .assignmentOptional().defaultValue("bin")
+                    .commit();
+
+            INT32_ELEMENT(expected).key("maximumFileSize")
+                    .displayedName("Maximum file size")
+                    .description("After any archived file has reached this size it will be time-stamped and not appended anymore")
+                    .unit(Unit::BYTE)
+                    .metricPrefix(MetricPrefix::MEGA)
+                    .reconfigurable()
+                    .assignmentOptional().defaultValue(100)
+                    .commit();
+
+            FLOAT_ELEMENT(expected).key("lastFlushDuration")
+                    .displayedName("Last flush duration")
+                    .description("Time needed for the last flush")
+                    .unit(Unit::SECOND)
+                    .readOnly()
+                    .warnHigh(20)
+                    .alarmHigh(40)
+                    .commit();
+
+            OVERWRITE_ELEMENT(expected).key("visibility")
+                    .setNewDefaultValue(5)
+                    .commit();
+
+            OVERWRITE_ELEMENT(expected).key("deviceId")
+                    .setNewDefaultValue("Karabo_DataLoggerManager_0")
+                    .commit();
+
+            // Slow beats
+            OVERWRITE_ELEMENT(expected).key("heartbeatInterval")
+                    .setNewDefaultValue(60)
+                    .commit();
+        }
+
+
+        DataLoggerManager::DataLoggerManager(const Hash& input) : Device<OkErrorFsm>(input) {
+        }
+
+
+        DataLoggerManager::~DataLoggerManager() {
+        }
+
+
+        void DataLoggerManager::okStateOnEntry() {
+
+            // Register handlers
+            remote().registerInstanceNewMonitor(boost::bind(&karabo::core::DataLoggerManager::instanceNewHandler, this, _1));
+            remote().registerInstanceGoneMonitor(boost::bind(&karabo::core::DataLoggerManager::instanceGoneHandler, this, _1, _2));
+            
+            // Prepare backend to persist data (later we should use brokerhost/brokerport/brokertopic)
+            if (!boost::filesystem::exists("karaboHistory")) {
+                boost::filesystem::create_directory("karaboHistory");
+            }
+
+            // Get all current instances in the system
+            const Hash& systemTopology = remote().getSystemTopology();
+            boost::optional<const Hash::Node&> node = systemTopology.find("device");
+            if (node) {
+                const Hash& devices = node->getValue<Hash>();
+                for (Hash::const_iterator it = devices.begin(); it != devices.end(); ++it) { // Loop all devices
+                    if (it->hasAttribute("archive") && (it->getAttribute<bool>("archive") == false)) continue;
+                    const std::string& deviceId = it->getKey();
+                    if (deviceId == m_instanceId) continue; // Skip myself
+                    
+                    // Check whether according logger devices exists and if not instantiate
+                    
+                }
+            }
+        }
+
+
+        void DataLoggerManager::instanceNewHandler(const karabo::util::Hash& topologyEntry) {
+
+            try {
+                KARABO_LOG_FRAMEWORK_DEBUG << "instanceNewHandler";
+                const std::string& type = topologyEntry.begin()->getKey();
+
+                if (type == "device") { // Take out only devices for the time being
+                    const Hash& entry = topologyEntry.begin()->getValue<Hash>();
+                    const string& deviceId = entry.begin()->getKey();
+
+                    // Skip devices that are marked to globally prevent archiving
+                    if (entry.hasAttribute(deviceId, "archive") && (entry.getAttribute<bool>(deviceId, "archive") == false)) return;
+
+                    // Check whether according logger device exists (it should not) and instantiate
+                    
+                }
+
+            } catch (const Exception& e) {
+                KARABO_LOG_ERROR << e;
+            }
+        }
+
+        void DataLoggerManager::instanceGoneHandler(const std::string& instanceId, const karabo::util::Hash& instanceInfo) {
+            
+            // Call slotTagDeviceToBeDiscontinued and than kill the logger device
+            
+        }        
+    }
+}
