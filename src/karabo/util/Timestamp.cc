3795df06fe753be2cb6c2431b442b7c46d195554
Luis Maia <luis.maia@xfel.eu>
2014-09-06 00:47:03 +0200

    Add locale notion (en_US.UTF-8) to Python tests (#987)

--- src/karabo/util/Timestamp.cc
+++ src/karabo/util/Timestamp.cc
@@ -10,48 +10,42 @@
 namespace karabo {
     namespace util {
 
-
         Timestamp::Timestamp() : m_epochstamp(Epochstamp()), m_trainstamp(Trainstamp()) {
         }
-
 
         Timestamp::Timestamp(const Epochstamp& e, const Trainstamp& t) : m_epochstamp(e), m_trainstamp(t) {
         }
 
-
         Timestamp::~Timestamp() {
         }
-
 
         bool Timestamp::hashAttributesContainTimeInformation(const Hash::Attributes attributes) {
             return (Epochstamp::hashAttributesContainTimeInformation(attributes) && Trainstamp::hashAttributesContainTimeInformation(attributes));
         }
 
-
         Timestamp Timestamp::fromHashAttributes(const Hash::Attributes attributes) {
             return Timestamp(Epochstamp::fromHashAttributes(attributes), Trainstamp::fromHashAttributes(attributes));
         }
-
 
         std::string Timestamp::toIso8601(TIME_UNITS precision, bool extended) const {
             return m_epochstamp.toIso8601(precision, extended);
         }
 
-
         std::string Timestamp::toIso8601Ext(TIME_UNITS precision, bool extended) const {
             return m_epochstamp.toIso8601Ext(precision, extended);
         }
-
 
         std::string Timestamp::toFormattedString(const std::string& format, const std::string& localTimeZone) const {
             return m_epochstamp.toFormattedString(format, localTimeZone);
         }
 
+        std::string Timestamp::toFormattedStringLocale(const std::string& localeName, const std::string& format, const std::string& localTimeZone) const {
+            return m_epochstamp.toFormattedStringLocale(localeName, format, localTimeZone);
+        }
 
         double Timestamp::toTimestamp() const {
             return m_epochstamp.toTimestamp();
         }
-
 
         void Timestamp::toHashAttributes(Hash::Attributes& attributes) const {
             m_epochstamp.toHashAttributes(attributes);
--- src/karathon/PyUtilTimestamp.cc
+++ src/karathon/PyUtilTimestamp.cc
@@ -13,7 +13,6 @@ namespace bp = boost::python;
 using namespace karabo::util;
 using namespace std;
 
-
 void exportPyUtilTimestamp() {
 
     bp::class_<Timestamp> ts("Timestamp", bp::init<>());
@@ -21,44 +20,48 @@ void exportPyUtilTimestamp() {
     ts.def(bp::init<Epochstamp const &, Trainstamp const &>((bp::arg("e"), bp::arg("t"))));
 
     ts.def("getSeconds"
-           , (unsigned long long const & (Timestamp::*)() const) (&Timestamp::getSeconds)
-           , bp::return_value_policy< bp::copy_const_reference >());
+            , (unsigned long long const & (Timestamp::*)() const) (&Timestamp::getSeconds)
+            , bp::return_value_policy< bp::copy_const_reference >());
 
     ts.def("getFractionalSeconds"
-           , (unsigned long long const & (Timestamp::*)() const) (&Timestamp::getFractionalSeconds)
-           , bp::return_value_policy< bp::copy_const_reference >());
+            , (unsigned long long const & (Timestamp::*)() const) (&Timestamp::getFractionalSeconds)
+            , bp::return_value_policy< bp::copy_const_reference >());
 
     ts.def("getTrainId"
-           , (unsigned long long const & (Timestamp::*)() const) (&Timestamp::getTrainId)
-           , bp::return_value_policy< bp::copy_const_reference >());
+            , (unsigned long long const & (Timestamp::*)() const) (&Timestamp::getTrainId)
+            , bp::return_value_policy< bp::copy_const_reference >());
 
     ts.def("fromHashAttributes"
-           , &Timestamp::fromHashAttributes
-           , bp::arg("attributes"));
+            , &Timestamp::fromHashAttributes
+            , bp::arg("attributes"));
     ts.staticmethod("fromHashAttributes");
 
     ts.def("hashAttributesContainTimeInformation"
-           , &Timestamp::hashAttributesContainTimeInformation
-           , bp::arg("attributes"));
+            , &Timestamp::hashAttributesContainTimeInformation
+            , bp::arg("attributes"));
     ts.staticmethod("hashAttributesContainTimeInformation");
 
     ts.def("toIso8601"
-           , (string(Timestamp::*)(karabo::util::TIME_UNITS, bool) const) (&Timestamp::toIso8601)
-           , (bp::arg("precision") = karabo::util::MICROSEC, bp::arg("extended") = (bool)(false)));
+            , (string(Timestamp::*)(karabo::util::TIME_UNITS, bool) const) (&Timestamp::toIso8601)
+            , (bp::arg("precision") = karabo::util::MICROSEC, bp::arg("extended") = (bool)(false)));
 
     ts.def("toIso8601Ext"
-           , (string(Timestamp::*)(karabo::util::TIME_UNITS, bool) const) (&Timestamp::toIso8601Ext)
-           , (bp::arg("precision") = karabo::util::MICROSEC, bp::arg("extended") = (bool)(false)));
+            , (string(Timestamp::*)(karabo::util::TIME_UNITS, bool) const) (&Timestamp::toIso8601Ext)
+            , (bp::arg("precision") = karabo::util::MICROSEC, bp::arg("extended") = (bool)(false)));
+
+    ts.def("toFormattedStringLocale"
+            , (string(Timestamp::*)(const string &, const string &, const string &) const) (&Timestamp::toFormattedStringLocale)
+            , (bp::arg("localeName") = "", bp::arg("format") = "%Y-%b-%d %H:%M:%S", bp::arg("localTimeZone") = "Z"));
 
     ts.def("toFormattedString"
-           , (string(Timestamp::*)(const string &, const string &) const) (&Timestamp::toFormattedString)
-           , (bp::arg("format") = "%Y-%b-%d %H:%M:%S", bp::arg("localTimeZone") = "Z"));
+            , (string(Timestamp::*)(const string &, const string &) const) (&Timestamp::toFormattedString)
+            , (bp::arg("format") = "%Y-%b-%d %H:%M:%S", bp::arg("localTimeZone") = "Z"));
 
     ts.def("toTimestamp"
-           , (double (Timestamp::*)() const) (&Timestamp::toTimestamp));
+            , (double (Timestamp::*)() const) (&Timestamp::toTimestamp));
 
     ts.def("toHashAttributes"
-           , (void (Timestamp::*)(Hash::Attributes &) const) (&Timestamp::toHashAttributes)
-           , bp::arg("attributes"));
+            , (void (Timestamp::*)(Hash::Attributes &) const) (&Timestamp::toHashAttributes)
+            , bp::arg("attributes"));
 
 }
