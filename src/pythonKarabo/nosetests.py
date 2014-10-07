d12c846706b996f1f495b342c58eb22adb4263ed
esenov <sergey.esenov@xfel.eu>
2014-10-07 15:44:23 +0000

    Fix for running command line script in python3.  Script should be launched from ..../karaboFramework/src/pythonKarabo directory as 'python3 nosetests.py'.
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/trunk@15057 a23ab9f9-1ea2-4934-960d-990259f9ea8c

--- src/pythonKarabo/nosetests.py
+++ src/pythonKarabo/nosetests.py
@@ -1,12 +1,14 @@
-#! /usr/bin/python
+#!/usr/bin/env python3
 
 __author__="Sergey Esenov <serguei.essenov@xfel.eu>"
 __date__ ="$Apr 8, 2014 11:32:06 AM$"
 
 import os
-from nose import main
+import nose
+import sys
 
 if __name__ == "__main__":
     os.chdir("tests")
-    main()
+    sys.path.append(os.getcwd())
+    nose.run()
     os.chdir("..")
