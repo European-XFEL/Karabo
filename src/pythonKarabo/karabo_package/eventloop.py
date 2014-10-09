559dfb5051253800cb4c0824f4d2262cd0572673
Martin Teichmann <martin.teichmann@xfel.eu>
2014-10-09 11:05:59 +0200

    add new files for python only karabo

---
+++ src/pythonKarabo/karabo_package/eventloop.py
@@ -0,0 +1,155 @@
+from __future__ import absolute_import, unicode_literals
+from karabo.hashtypes import Type, Slot
+from karabo.hash import Hash, BinaryWriter
+from karabo import openmq
+
+from asyncio import (coroutine, Future, get_event_loop, set_event_loop,
+                     SelectorEventLoop, Task)
+from copy import copy
+from concurrent.futures import ThreadPoolExecutor
+from itertools import chain
+import sys
+
+
+class Broker:
+    def __init__(self, loop, deviceId, classId):
+        self.loop = loop
+        self.connection = loop.connection
+        self.session = openmq.Session(self.connection, False, 1, 0)
+        self.destination = openmq.Destination(self.session, "teichman", 1)
+        self.producer = openmq.Producer(self.session, self.destination)
+        self.deviceId = deviceId
+        self.classId = classId
+
+    def call(self, signal, slots, targets, reply, args):
+        hash = Hash()
+        for i, a in enumerate(args):
+            hash['a{}'.format(i + 1)] = a
+        p = openmq.Properties()
+        p['signalFunction'] = signal
+        p['signalInstanceId'] = self.deviceId
+        p['slotFunction'] = (b'|' + b'||'.join(
+            s if isinstance(s, bytes) else s.encode("utf8")
+            for s in slots) + b'|') if slots else b'__none__'
+        p['slotInstanceId'] = (b'|' + b'||'.join(
+            t if isinstance(t, bytes) else t.encode("utf8")
+            for t in targets) + b'|') if targets else b'__none__'
+        if reply is not None:
+            p['replyTo'] = reply
+        p['hostname'] = "exflpcx18981"
+        p['classId'] = self.classId
+        p['__format'] = 'Bin'
+        writer = BinaryWriter()
+        m = openmq.BytesMessage()
+        m.data = writer.write(hash)
+        m.properties = p
+        self.producer.send(m, 1, 4, 1000)
+
+    def emit(self, signal, slots, targets, *args):
+        self.call(signal, slots, targets, None, args)
+
+    def reply(self, whom, *args):
+        hash = Hash()
+        for i, a in enumerate(args):
+            hash['a{}'.format(i + 1)] = a
+        p = openmq.Properties()
+        p['replyFrom'] = whom
+        p['__format'] = 'Bin'
+        writer = BinaryWriter()
+        m = openmq.BytesMessage()
+        m.data = writer.write(hash)
+        m.properties = p
+        self.producer.send(m, 1, 4, 1000)
+
+    def connect(self, deviceId, signal, slot):
+        self.emit("call", ["slotConnectToSignal"], [deviceId], signal,
+                  slot.__self__.deviceId, slot.__name__, 0)
+
+    def registerSlot(self, x):
+        """ legacy code. slots don't need to be registered """
+        return
+
+
+class Client(object):
+    def onChanged(self, hash):
+        for k, v in hash.iteritems():
+            a = getattr(type(self), k, None)
+            if a is not None:
+                self.__dict__[a] = v
+                for f in self._futures.get(a, []):
+                    f.set_result(v)
+
+    def setValue(self, attr, value):
+        self._device._ss.emit("call", ["slotReconfigure"], [self.deviceId],
+                              Hash(attr.key, value))
+
+
+class EventLoop(SelectorEventLoop):
+    def __init__(self):
+        super().__init__()
+        self.connection = None
+        self.changedFuture = Future()  # call if some property changes
+        self.set_default_executor(ThreadPoolExecutor(10000))
+
+    def getBroker(self, deviceId, classId):
+        if self.connection is None:
+            p = openmq.Properties()
+            p["MQBrokerHostName"] = "exfl-broker.desy.de"
+            p["MQBrokerHostPort"] = 7777
+            p["MQConnectionType"] = "TCP"
+            p["MQPingInterval"] = 20
+            p["MQSSLIsHostTrusted"] = True
+            p["MQAckOnProduce"] = False
+            p["MQAckTimeout"] = 0
+
+            self.connection = openmq.Connection(p, "guest", "guest")
+            self.connection.start()
+
+        return Broker(self, deviceId, classId)
+
+    @coroutine
+    def get_device(self, deviceId):
+        f = self.device.slotSchemaUpdated[self.deviceId]
+        self.device._ss.emit("call", ["slotGetSchema"], [self.deviceId], False)
+        schema = yield from f
+        f = self.device.slotChanged[self.deviceId]
+        self.device._ss.emit("call", ["slotGetConfiguration"], [self.deviceId])
+        self.device._ss.connect(self.deviceId, "signalChanged",
+                                self.device.slotChanged)
+        hash = yield from f
+
+        dict = {}
+        for k, v, a in self.schema.hash.iterall():
+            if a["nodeType"] == 0:
+                d = Type.fromname[a["valueType"]]()
+                d.key = k
+                dict[k] = d
+            elif a["nodeType"] == 1 and a["displayType"] == "Slot":
+                s = Slot()
+                s.method = lambda self, name=k: self._device._ss.emit(
+                    "call", [name], [self.deviceId])
+                dict[k] = s
+        cls = type(self.schema.name.encode("ascii"), (Client,), dict)
+        o = cls()
+        o._device = self.device
+        o._futures = {}
+        self.device.clients[self.deviceId] = o
+        o.onChanged(hash)
+        return o
+
+    @coroutine
+    def getValueFuture(self, device, attr):
+        return device._futures.setdefault(attr, Future(loop=self))
+
+    @coroutine
+    def waitForChanges(self):
+        yield from self.changedFuture
+
+    def close(self):
+        for t in Task.all_tasks(self):
+            t.cancel()
+        self._ready.extend(self._scheduled)
+        self._scheduled.clear()
+        while self._ready:
+            self._run_once()
+        self.connection.close()
---
+++ src/pythonKarabo/karabo_package/legacy.py
@@ -0,0 +1,168 @@
+"""Support for legacy features
+
+Historically, all devices just did a `from karabo.device import *`.
+This is really weird, why should everything be defined in karabo.device?
+So now karabo.device star-imports this module, which does the old stuff. """
+
+from karabo.enums import MetricPrefix, Unit, EncodingType
+from karabo.decorators import KARABO_CLASSINFO
+from karabo.hash import Hash
+from karabo.hashtypes import Type
+from karabo.schema import *
+from karabo.base_fsm import BaseFsm
+import karabo.schema
+
+from enum import Enum
+
+__all__ = ["MetricPrefix", "Unit", "EncodingType", "KARABO_CLASSINFO",
+           "Schema", "PATH_ELEMENT", "NODE_ELEMENT", "SLOT_ELEMENT",
+           "IMAGE_ELEMENT", "OVERWRITE_ELEMENT", "CHOICE_ELEMENT",
+           "LIST_ELEMENT", "RawImageData", "BaseFsm"]
+
+
+def publish(enum):
+    globals().update((e.name, e) for e in enum)
+    __all__.extend(e.name for e in enum)
+
+
+publish(MetricPrefix)
+publish(Unit)
+
+
+def RawImageData(image, type):
+    return Hash("dims", list(image.shape),
+                "type", Type.strs[image.dtype.str].hashname(),
+                "data", image.tostring())
+
+
+def parameter(name, value):
+    def f(self):
+        self.attributes[name] = value.value
+        return self
+    return f
+
+
+class Dotter(object):
+    def __init__(self, schema):
+        self.schema = schema
+        self.attributes = {}
+        self.value = karabo.hash.Hash()
+
+    def __getattr__(self, name):
+        return partial(self.dotter, name)
+
+    def key(self, name):
+        self.key = name
+        return self
+
+    expertAccess = parameter('requiredAccessLevel', AccessLevel.EXPERT)
+    advanced = parameter('requiredAccessLevel', AccessLevel.EXPERT)
+    reconfigurable = parameter('accessMode', AccessMode.RECONFIGURABLE)
+    init = parameter('accessMode', AccessMode.INITONLY)
+    readOnly = parameter('accessMode', AccessMode.READONLY)
+    assignmentOptional = parameter('assignment', Assignment.OPTIONAL)
+    assignmentInternal = parameter('assignment', Assignment.INTERNAL)
+
+    def noDefaultValue(self):
+        return self
+
+    def initialValue(self, value):
+        self.attributes["defaultValue"] = value
+        return self
+
+    def dotter(self, name, *args):
+        if len(args) == 1:
+            args = args[0]
+            if isinstance(args, Enum):
+                args = args.value
+            self.attributes[name] = args
+        else:
+            print('bad:', name, args)
+        return self
+
+    def commit(self):
+        self.schema.hash[self.key] = self.value
+        self.schema.hash[self.key, ...] = self.attributes
+
+
+class Leaf(Dotter):
+    def commit(self):
+        self.attributes['nodeType'] = 0
+        n = str(type(self).__name__)
+        self.attributes['valueType'] = self.hashname
+        super(Leaf, self).commit()
+
+
+for t in hashtypes.Type.types:
+    if t is not None:
+        name = "{}_ELEMENT".format(t.hashname())
+        globals()[name] = type(name, (Leaf,), dict(hashname=t.hashname()))
+        __all__.append(name)
+
+
+class PATH_ELEMENT(STRING_ELEMENT):
+    isDirectory = parameter('displayType', 'directory')
+
+
+class NODE_ELEMENT(Dotter):
+    def appendParametersOfConfigurableClass(self, cls, classId):
+        self.value = cls.getSchema(classId).hash
+        self.attributes['classId'] = classId
+        self.attributes['displayType'] = cls.__name__
+        return self
+
+    def commit(self):
+        self.attributes['nodeType'] = 1
+        super(NODE_ELEMENT, self).commit()
+
+
+class IMAGE_ELEMENT(NODE_ELEMENT):
+    def commit(self):
+        self.value = Image.getClassSchema().hash
+        self.attributes['displayType'] = 'Image'
+        super().commit()
+
+
+class SLOT_ELEMENT(NODE_ELEMENT):
+    def commit(self):
+        self.attributes['displayType'] = 'Slot'
+        super(SLOT_ELEMENT, self).commit()
+
+
+class OVERWRITE_ELEMENT(Dotter):
+    def key(self, name):
+        self.key = name
+        assert name in self.schema.hash
+        return self
+
+    def dotter(self, name, *args):
+        assert name.startswith('setNew')
+        name = name[6].lower() + name[7:]
+        return Dotter.dotter(self, name, *args)
+
+    def commit(self):
+        self.schema.hash[self.key, ...].update(self.attributes)
+
+
+class CHOICE_ELEMENT(Dotter):
+    def appendNodesOfConfigurationBase(self, base):
+        return self
+
+    def commit(self):
+        self.attributes['nodeType'] = 2
+        super(CHOICE_ELEMENT, self).commit()
+
+
+class LIST_ELEMENT(Dotter):
+    def appendNodesOfConfigurationBase(self, base):
+        if self.value is None:
+            self.value = karabo.hash.Hash()
+        for n in base.getRegisteredClasses():
+            self.value[n] = base.getSchema(n).hash
+            d = dict(classId=n, displayType=n, nodeType=1, accessMode=7)
+            self.value[n, ...] = d
+        return self
+
+    def commit(self):
+        self.attributes['nodeType'] = 3
+        super(LIST_ELEMENT, self).commit()
