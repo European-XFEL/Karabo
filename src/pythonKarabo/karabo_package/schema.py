fd431a8929bdf4f8d62537e9367fa3edeb885d4d
Martin Teichmann <martin.teichmann@xfel.eu>
2014-10-09 11:05:59 +0200

    add new files for python only karabo

---
+++ src/pythonKarabo/karabo_package/schema.py
@@ -0,0 +1,220 @@
+
+import karabo.hash
+from karabo import hashtypes
+from karabo.enums import AccessLevel, AccessMode, Assignment
+from karabo.hashtypes import Descriptor, Schema_ as Schema  # this is not nice
+from karabo.registry import Registry
+
+from enum import Enum
+from functools import partial
+from collections import OrderedDict
+
+
+class MetaConfigurable(type(Registry)):
+    @staticmethod
+    def __prepare__(name, bases):
+        return OrderedDict()
+
+
+class Configurable(Registry, metaclass=MetaConfigurable):
+    _subclasses = []
+
+    def __init__(self, configuration={}):
+        for k in self._allattrs:
+            t = getattr(type(self), k)
+            if k in configuration:
+                v = configuration[k]
+                if t.enum is not None:
+                    v = t.enum(v)
+                setattr(self, k, v)
+                del configuration[k]
+            else:
+                if t.defaultValue is not None:
+                    setattr(self, k, t.defaultValue)
+
+    @classmethod
+    def register(cls, name, dict):
+        super(Configurable, cls).register(name, dict)
+        for k, v in dict.items():
+            if isinstance(v, Descriptor):
+                v.key = k
+        cls._attrs = [k for k in dict
+                      if isinstance(getattr(cls, k), Descriptor)]
+        cls._allattrs = set.union(*(set(c._attrs) for c in cls.__mro__
+                                    if hasattr(c, "_attrs")))
+        cls.xsd = cls.getClassSchema()
+        cls._subclasses = []
+        for b in cls.__bases__:
+            if isinstance(b, Configurable):
+                b._subclasses.append(cls)
+
+    @classmethod
+    def getClassSchema(cls, rules=None):
+        schema = karabo.schema.Schema(cls.__name__, rules)
+        for c in cls.__mro__[::-1]:
+            if hasattr(c, "expectedParameters"):
+                c.expectedParameters(schema)
+            if hasattr(c, "_attrs"):
+                for k in c._attrs:
+                    v = getattr(c, k)
+                    schema.hash[k] = v.subschema()
+                    schema.hash[k, ...] = {
+                        k: v.value if isinstance(v, Enum) else v
+                        for k, v in v.parameters().items()}
+        return schema
+
+    def __dir__(self):
+        return list(self._allattrs)
+
+    @classmethod
+    def node(cls, **kwargs):
+        return Node(cls, **kwargs)
+
+    @classmethod
+    def choiceOfNodes(cls, **kwargs):
+        return ChoiceOfNodes(cls, **kwargs)
+
+    @classmethod
+    def listOfNodes(cls, **kwargs):
+        return ListOfNodes(cls, **kwargs)
+
+    def setValue(self, key, value):
+        self.__dict__[key] = value
+
+    def run(self):  # endpoint for multiple inheritance
+        self.running = True
+
+
+class Node(Descriptor):
+    def __init__(self, cls, **kwargs):
+        self.cls = cls
+        Descriptor.__init__(self, **kwargs)
+
+    def parameters(self):
+        ret = super(Node, self).parameters()
+        ret["nodeType"] = 1
+        return ret
+
+    def subschema(self):
+        return self.cls.getClassSchema().hash
+
+    def __set__(self, instance, value):
+        instance.setValue(self, self.cls(value))
+
+    def asHash(self, instance):
+        r = karabo.hash.Hash()
+        for k in self.cls._allattrs:
+            a = getattr(instance, k, None)
+            if a is not None:
+                r[k] = getattr(self.cls, k).asHash(a)
+        return r
+
+
+class ChoiceOfNodes(Node):
+    def parameters(self):
+        ret = super(ChoiceOfNodes, self).parameters()
+        ret["nodeType"] = 2
+        return ret
+
+    def subschema(self):
+        h = karabo.hash.Hash()
+        for c in self.cls._subclasses:
+            h[c.__name__] = c.getClassSchema().hash
+
+    def __set__(self, instance, value):
+        for k, v in value.items():
+            for c in self.cls._subclasses:
+                if c.__name__ == k:
+                    instance.setValue(self, c(value))
+                    break
+            break  # there should be only one entry
+
+    def asHash(self, instance):
+        r = karabo.hash.Hash()
+        t = type(instance)
+        for k in t._allattrs:
+            a = getattr(instance, k, None)
+            if a is not None:
+                r[k] = getattr(t, k).asHash(a)
+        return karabo.hash.Hash(t.__name__, r)
+
+
+class ListOfNodes(Node):
+    def parameters(self):
+        ret = super(ListOfNodes, self).parameters()
+        ret["nodeType"] = 3
+        return ret
+
+    def subschema(self):
+        h = karabo.hash.Hash()
+        for c in self.cls._subclasses:
+            h[c.__name__] = c.getClassSchema().hash
+        return h
+
+    def __set__(self, instance, value):
+        l = []
+        for k, v in value.items():
+            for c in self.cls._subclasses:
+                if c.__name__ == k:
+                    l.append(c(value))
+                    break
+        instance.setValue(self, l)
+
+    def asHash(self, instance):
+        l = []
+        for v in instance:
+            r = karabo.hash.Hash()
+            t = type(v)
+            for k in t._allattrs:
+                r[k] = getattr(t, k).asHash(getattr(v, k))
+            l.append(r)
+        return karabo.hash.Hash(t.__name__, l)
+
+
+class Validator(object):
+    def __init__(self, injectDefaults=True):
+        self.injectDefaults = injectDefaults
+
+    def validate(self, schema, hash, stamp=None):
+        """validate the hash to fit schema and return the validated hash
+
+        most importantly, if hash does not contain some parameters, those
+        are inserted with their default value"""
+        return self.r_validate(schema.hash, hash)
+
+    def r_validate(self, subject, object):
+        for k, v, a in subject.iterall():
+            assert 'nodeType' in a
+            if a['nodeType'] == 3:
+                if k in object:
+                    object[k] = [[karabo.hash.Hash(kk,
+                                                   self.r_validate(v[kk], vv))
+                                  for kk, vv in vvv.items()][0]
+                                 for vvv in object[k]]
+                elif self.injectDefaults:
+                    dv = a.get('defaultValue', [])
+                    if isinstance(dv, str):
+                        dv = dv.split()
+                    object[k] = [karabo.hash.Hash(
+                                 vv, self.r_validate(v[vv],
+                                                     karabo.hash.Hash()))
+                                 for vv in dv]
+            elif a['nodeType'] == 2:
+                if k in object:
+                    object[k] = [karabo.hash.Hash(kk,
+                                                  self.r_validate(v[kk], vv))
+                                 for kk, vv in object[k].items()][0]
+                elif self.injectDefaults and "defaultValue" in a:
+                    dv = a['defaultValue']
+                    object[k] = karabo.hash.Hash(
+                        dv, self.r_validate(v[dv], karabo.hash.Hash()))
+            elif k in object:
+                object[k] = self.r_validate(v, object[k])
+            elif self.injectDefaults and 'defaultValue' in a:
+                object[k] = a['defaultValue']
+            elif a['nodeType'] == 1:
+                object[k] = self.r_validate(v, karabo.hash.Hash())
+            else:
+                pass
+                #print 'not validated:', k
+        return object
---
+++ src/pythonKarabo/karabo_package/signalslot.py
@@ -0,0 +1,332 @@
+from __future__ import unicode_literals
+from karabo.decorators import KARABO_CLASSINFO, KARABO_CONFIGURATION_BASE_CLASS
+from karabo import openmq, hashtypes
+from karabo.hash import BinaryParser, Hash
+from karabo.hashtypes import String, Int32, Type, Slot
+from karabo.enums import AccessLevel, Assignment, AccessMode
+from karabo.schema import Schema, Configurable
+from karabo.timestamp import Timestamp
+from karabo.registry import Registry
+
+from asyncio import async, coroutine, Future, get_event_loop, sleep
+from itertools import count
+import sys
+import time
+
+
+class Signal(object):
+    def __init__(self, *args):
+        self.args = args
+
+
+class Device(Registry):
+    @classmethod
+    def schematize(cls):
+        schema = Schema(cls.__name__, hash)
+        for k, v in cls.__dict__.items():
+            if isinstance(v, Type):
+                h = Hash()
+                hash[k] = h
+                hash[k, 'nodeType'] = 0
+                hash[k, 'valueType'] = v.hashname()
+        return schema
+
+    @classmethod
+    def register(cls, name, dict):
+        super(Device, cls).register(name, dict)
+        #globalDevice.slotNewClassAvailable('bla', name, cls.schematize())
+
+
+def replySlot(slot):
+    """ using this decorator you can define a reply slot, everything the
+    slot returns is not only sent as a normal reply, but also emitted as
+    a signal to the defined slot, back to the sender. """
+    def f(g):
+        g.replySlot = slot
+        return g
+    return f
+
+
+class DeviceServer(Device):
+    def __init__(self, deviceId):
+        Device.__init__(self, deviceId, type="server", serverId=deviceId,
+                        version=23, host="exflpcx18981", visibility=0)
+
+
+class ConnectionType(object):
+    NO_TRACK = 0
+
+
+class BoundSignal(object):
+    def __init__(self, device, name, signal):
+        self.device = device
+        self.name = name
+        self.connected = []
+        self.args = signal.args
+
+    def connect(self, target, slot, dunno):
+        self.connected.append((target, slot, dunno))
+
+    def disconnect(self, target, slot):
+        self.connected = [(t, s, d) for t, s, d in self.connected
+                          if t != target or s != slot]
+
+    def __call__(self, *args):
+        args = [d.cast(v) for d, v in zip(self.args, args)]
+        self.device._ss.emit(self.name, [s for t, s, d in self.connected],
+                             [t for t, s, d in self.connected], *args)
+
+
+class Client(object):
+    def __init__(self, device):
+        self._device = device
+        self._futures = {}
+
+    @classmethod
+    def __dir__(cls):
+        return dir(cls)
+
+    def onChanged(self, hash):
+        for k, v, a in hash.iterall():
+            d = getattr(type(self), k, None)
+            if d is not None:
+                self.__dict__[d] = v
+                f = self._futures.pop(k, None)
+                if f is not None:
+                    f.set_result((k, v, Timestamp.fromHashAttributes(a)))
+        f = self._futures.pop(None, None)
+        if f is not None:
+            f.set_result(hash)
+
+    def setValue(self, attr, value):
+        self._device._ss.emit("call", ["slotReconfigure"], [self.deviceId],
+                              Hash(attr.key, value))
+
+
+@KARABO_CONFIGURATION_BASE_CLASS
+@KARABO_CLASSINFO('SignalSlotable', '1.0')
+class SignalSlotable(Configurable):
+    signalHeartbeat = Signal(hashtypes.String(), hashtypes.Int32(),
+                             hashtypes.Hash())
+    signalChanged = Signal(hashtypes.Hash(), hashtypes.String())
+
+    _deviceId_ = String(
+        displayedName="_DeviceID_",
+        description="Do not set this property, it will be set by the "
+                    "device-server",
+        requiredAccessLevel=AccessLevel.EXPERT,
+        assignment=Assignment.INTERNAL, accessMode=AccessMode.INITONLY,
+        defaultValue="__none__")
+
+    deviceId = String(
+        displayedName="DeviceID",
+        description="The device instance ID uniquely identifies a device "
+                    "instance in the distributed system",
+        accessMode=AccessMode.READONLY)
+
+    heartbeatInterval = Int32(
+        displayedName="Heartbeat interval",
+        description="The heartbeat interval",
+        assignment=Assignment.OPTIONAL, defaultValue=20,
+        requiredAccessLevel=AccessLevel.ADMIN)
+
+    def __init__(self, configuration):
+        self.notify_changes = False
+        for k in dir(type(self)):
+            if isinstance(getattr(self, k, None), Signal):
+                setattr(self, k, BoundSignal(self, k, getattr(self, k)))
+        super().__init__(configuration)
+        self.deviceId = self._deviceId_
+        self._ss = get_event_loop().getBroker(self.deviceId,
+                                              type(self).__name__)
+        self.notify_changes = True
+        self.info = Hash("heartbeatInterval", self.heartbeatInterval)
+
+        self.__schemaFutures = {}
+        self.__devices = {}
+        self.__repliers = {}
+
+    @coroutine
+    def slotPing(self, instanceId, replyIfMe, track=None):
+        if replyIfMe:
+            if instanceId == self.deviceId:
+                return self.info
+        else:
+            self._ss.emit("call", ["slotPingAnswer"], self.deviceId, self.info)
+        if track and instanceId != self.deviceId:
+            self.signalHeartbeat.connect(instanceId, "slotHeartbeat", None)
+
+    @coroutine
+    def slotHeartbeat(self, networkId, heartbeatInterval, info):
+        pass
+
+    @coroutine
+    def slotStopTrackingExistenceOfConnection(self, *args):
+        print('receivet stopTracking...', args)
+
+    @coroutine
+    def slotDisconnectFromSlot(self, *args):
+        print("SDFS", args)
+
+    @coroutine
+    def heartbeats(self):
+        while True:
+            self.signalHeartbeat(self.deviceId, self.heartbeatInterval,
+                                 self.info)
+            yield from sleep(self.heartbeatInterval)
+
+    @coroutine
+    def consume(self):
+        self._ss.consumer = openmq.Consumer(
+            self._ss.session, self._ss.destination,
+            "slotInstanceId LIKE '%|{}|%' OR replyFrom LIKE '{}-%'"
+            "OR slotInstanceId LIKE '%|*|%'".format(
+                self.deviceId, self.deviceId), False)
+        self._ss.emit('call', ['slotInstanceNew'], ['*'],
+                      self.deviceId, self.info)
+        try:
+            while self.running:
+                async(self.handleMessage(
+                    (yield from get_event_loop().run_in_executor(
+                        None, self._ss.consumer.receiveMessage))))
+        finally:
+            self._ss.emit('call', ['slotInstanceGone'], ['*'],
+                          self.deviceId, self.info)
+
+    def run(self):
+        async(self.heartbeats())
+        async(self.consume())
+        super().run()
+
+    def call(self, slot, target, *args):
+        reply = "{}-{}".format(self.deviceId, time.monotonic().hex())
+        self._ss.call("call", [slot], [target], reply, args)
+        future = Future(loop=self._ss.loop)
+        self.__repliers[reply] = future
+        return future
+
+    @coroutine
+    def handleMessage(self, message):
+        parser = BinaryParser()
+        try:
+            hash = parser.read(message.data)
+        except:
+            hash = Hash()
+        params = []
+        for i in count(1):
+            try:
+                params.append(hash['a{}'.format(i)])
+            except KeyError:
+                break
+        try:
+            replyFrom = message.properties['replyFrom'].decode("ascii")
+        except openmq.Error:
+            pass
+        else:
+            f = self.__repliers.get(replyFrom)
+            if f is not None:
+                f.set_result(params)
+            return
+
+        slots = message.properties['slotFunction']
+        #for k in message.properties:
+        #    print k, message.properties[k]
+        try:
+            replyTo = message.properties['replyTo']
+        except openmq.Error:
+            replyTo = None
+
+        sender = message.properties['signalInstanceId']
+        for s in slots[1:-1].split(b'||'):
+            try:
+                slot = getattr(self, s.decode("utf8"), None)
+                if slot is None:
+                    if ("|{}|".format(self.deviceId).encode("ascii") in
+                            message.properties['slotInstanceId']):
+                        raise AttributeError
+                    else:
+                        return
+                reply = yield from slot(*params)
+                if not isinstance(reply, tuple):
+                    reply = reply,
+                if hasattr(slot, "replySlot"):
+                    self._ss.emit('call', [slot.replySlot],
+                                  [message.properties['signalInstanceId']],
+                                  *reply)
+                if replyTo is not None:
+                    self._ss.reply(replyTo, *reply)
+            except Exception as e:
+                sys.excepthook(*sys.exc_info())
+                print("Slot={}".format(s.decode("utf8")))
+
+    def stopEventLoop(self):
+        self.running = False
+
+    @coroutine
+    def slotConnectToSignal(self, signal, target, slot, dunno):
+        getattr(self, signal).connect(target, slot, dunno)
+        return True
+
+    @coroutine
+    def slotDisconnectFromSignal(self, signal, target, slot):
+        getattr(self, signal).disconnect(target, slot)
+        return True
+
+    @staticmethod
+    def create(serverid):
+        return SignalSlotable(serverid)
+
+    def updateInstanceInfo(self, info):
+        self.info.merge(info)
+        self._ss.emit('call', ['slotInstanceUpdated'], ['*'],
+                      self.deviceId, self.info)
+
+    def setValue(self, attr, value):
+        self.__dict__[attr] = value
+        if self.notify_changes:
+            self.signalChanged(Hash(attr.key, value), self.deviceId)
+
+    @coroutine
+    def slotSchemaUpdated(self, schema, deviceId):
+        future = self.__schemaFutures.pop(deviceId, None)
+        if future is not None:
+            future.set_result(schema)
+
+    @coroutine
+    def slotChanged(self, configuration, deviceId):
+        d = self.__devices.get(deviceId)
+        if d is not None:
+            d.onChanged(configuration)
+        loop = get_event_loop()
+        loop.changedFuture.set_result(None)
+        loop.changedFuture = Future()
+
+    @coroutine
+    def getDevice(self, deviceId):
+        ret = self.__devices.get(deviceId)
+        if ret is not None:
+            return ret
+
+        self._ss.emit("call", ["slotGetSchema"], [deviceId], False)
+        schema = yield from self.__schemaFutures.setdefault(
+            deviceId, Future(loop=self._ss.loop))
+
+        dict = {}
+        for k, v, a in schema.hash.iterall():
+            if a["nodeType"] == 0:
+                d = Type.fromname[a["valueType"]]()
+                d.key = k
+                dict[k] = d
+            elif a["nodeType"] == 1 and a.get("displayType") == "Slot":
+                s = Slot()
+                s.method = lambda self, name=k: self._device._ss.emit(
+                    "call", [name], [self.deviceId])
+                dict[k] = s
+        cls = type(schema.name, (Client,), dict)
+
+        self._ss.emit("call", ["slotGetConfiguration"], [deviceId])
+        self._ss.connect(deviceId, "signalChanged", self.slotChanged)
+        ret = cls(self)
+        self.__devices[deviceId] = ret
+        yield from ret._futures.setdefault(None, Future(loop=self._ss.loop))
+        return ret
