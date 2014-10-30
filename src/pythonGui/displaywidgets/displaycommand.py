07e771116b21fa61581f2cc9dcee3a399a62f2cb
Martin Teichmann <martin.teichmann@xfel.eu>
2014-10-30 18:19:23 +0100

    put icons onto push buttons in GUI

--- src/pythonGui/displaywidgets/displaycommand.py
+++ src/pythonGui/displaywidgets/displaycommand.py
@@ -20,10 +20,14 @@
 
 __all__ = ["DisplayCommand"]
 
-
+from const import ns_karabo
+from displaywidgets.icons import Item, SelectionDialog
 from widget import DisplayWidget
 
-from PyQt4.QtGui import QToolButton, QAction
+from PyQt4.QtCore import QSize
+from PyQt4.QtGui import QToolButton, QWidget, QAction, QStackedLayout, QIcon
+
+from xml.etree.ElementTree import Element
 
 
 class DisplayCommand(DisplayWidget):
@@ -32,15 +36,37 @@ class DisplayCommand(DisplayWidget):
 
     def __init__(self, box, parent):
         super(DisplayCommand, self).__init__(None)
-        self.widget = QToolButton(parent)
+        self.widget = QWidget(parent)
+        layout = QStackedLayout(self.widget)
+        self.button = QToolButton(self.widget)
+        self.button.setIconSize(QSize(1000, 1000))
+        layout.addWidget(self.button)
+        action = QAction("Change Icons...", self.widget)
+        self.widget.addAction(action)
+        action.triggered.connect(self.showDialog)
         self.current = None
+        self.actions = []
         self.addBox(box)
 
 
+    def showDialog(self):
+        dialog = SelectionDialog(self.project, self.actions, None)
+        self.actions = dialog.exec()
+        self.updateIcons()
+
+    def updateIcons(self):
+        for a in self.actions:
+            if a.pixmap is not None:
+                a.action.setIcon(QIcon(a.pixmap))
+
     def addBox(self, box):
-        action = QAction("NO TEXT", self.widget)
-        action.box = box
-        self.widget.addAction(action)
+        action = QAction("NO TEXT", self.button)
+        self.button.addAction(action)
+        item = Item()
+        item.box = box
+        item.value = None
+        item.action = action
+        self.actions.append(item)
         box.configuration.boxvalue.state.signalUpdateComponent.connect(
             self.update)
         self.update()
@@ -49,27 +75,46 @@ class DisplayCommand(DisplayWidget):
 
     @property
     def boxes(self):
-        return [a.box for a in self.widget.actions()]
+        return [a.box for a in self.actions]
 
 
     def typeChanged(self, box):
-        for a in self.widget.actions():
-            if a.box is box and a.text() == "NO TEXT":
-                a.triggered.disconnect()
-                a.triggered.connect(box.execute)
-                if a.text() == "NO TEXT":
-                    a.setText(box.descriptor.displayedName)
+        for item in self.actions:
+            if item.box is box and item.value is None:
+                item.action.triggered.disconnect()
+                item.action.triggered.connect(box.execute)
+                item.action.setText(box.descriptor.displayedName)
+                item.value = box.descriptor.displayedName
 
 
     def update(self):
-        for a in self.widget.actions():
-            descr = a.box.descriptor
+        for item in self.actions:
+            descr = item.box.descriptor
             if descr is None or descr.allowedStates is None:
                 continue
-            a.setEnabled(a.box.configuration.value.state in descr.allowedStates)
-        for a in self.widget.actions():
+            item.action.setEnabled(item.box.configuration.value.state in
+                                   descr.allowedStates)
+        for a in self.button.actions():
             if a.isEnabled():
-                self.widget.setDefaultAction(a)
+                self.button.setDefaultAction(a)
                 break
         else:
-            self.widget.setDefaultAction(self.widget.actions()[0])
+            self.button.setDefaultAction(self.button.actions()[0])
+
+
+    def save(self, e):
+        for item in self.actions:
+            if item.url is not None:
+                ee = Element(ns_karabo + "action")
+                ee.set("key", item.box.key())
+                ee.set("image", item.url)
+                e.append(ee)
+
+    def load(self, e):
+        for ee in e:
+            key = ee.get("key")
+            for item in self.actions:
+                if item.box.key() == key:
+                    item.__init__(ee, self.project)
+                    break
+        self.updateIcons()
