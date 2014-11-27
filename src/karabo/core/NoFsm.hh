bbf1961a58c14c7d20f447ce830c269516680a50
esenov <sergey.esenov@xfel.eu>
2014-11-27 16:22:35 +0000

    Clean BaseFsm, NoFsm and make NoFsm to be just another base class
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/trunk@15727 a23ab9f9-1ea2-4934-960d-990259f9ea8c

--- src/karabo/core/NoFsm.hh
+++ src/karabo/core/NoFsm.hh
@@ -15,7 +15,7 @@
 namespace karabo {
     namespace core {
 
-        class NoFsm : public BaseFsm {
+        class NoFsm {
         public:
 
             KARABO_CLASSINFO(NoFsm, "NoFsm", "1.0")
@@ -27,30 +27,26 @@ namespace karabo {
             void initFsmSlots() {
             }
 
+            virtual void stopWorkers() {}
+            
         public:
 
             virtual ~NoFsm() {
-            }
+            }                      
 
-            virtual void errorFound(const std::string&, const std::string&) = 0;
-
-            void onNoStateTransition(const std::string& typeId, int state) {
-            }
-
+            
+            
             void startFsm() {
                 if (!m_initialFunc)
                     throw KARABO_PARAMETER_EXCEPTION("No initial function defined. Please call 'initialFunc' function in the constructor");
                 m_initialFunc();
             }
 
-            void initialFunc(const boost::function<void()>& func) {
+            void registerInitialFunction(const boost::function<void()>& func) {
                 m_initialFunc = func;
             }
 
         private:
-
-            void exceptionFound(const std::string&, const std::string&) const {
-            }
 
             boost::function<void() > m_initialFunc;
 
