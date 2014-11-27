6b953eb6049e952dbcb1c46ce787ba2a4bfe132d
esenov <esenov@a23ab9f9-1ea2-4934-960d-990259f9ea8c>
2014-11-27 18:53:40 +0000

    Cleaning and fix for C++ device synchronization (regex related)
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/trunk@15734 a23ab9f9-1ea2-4934-960d-990259f9ea8c

--- src/pythonKarabo/karabo_package/no_fsm.py
+++ src/pythonKarabo/karabo_package/no_fsm.py
@@ -8,7 +8,7 @@ __date__ ="$Nov 26, 2014 3:18:24 PM$"
 from karabo.decorators import KARABO_CLASSINFO
 import karabo.base_fsm as base
 
-@KARABO_CLASSINFO("NoFsm", "1.2 1.3")
+@KARABO_CLASSINFO("NoFsm", "1.3")
 class NoFsm(base.BaseFsm):
     
     @staticmethod
@@ -22,9 +22,6 @@ class NoFsm(base.BaseFsm):
     def getFsm(self):
         return None
     
-    def initFsmSlots(self, sigslot):
-        pass
-    
     def startFsm(self):
         """Start state machine"""
         self.updateState("Changing...")
@@ -32,8 +29,5 @@ class NoFsm(base.BaseFsm):
             raise RuntimeError("No initial function defined. Please call 'initialFunc' method in the device constructor")
         self.func()    # call initial function registered in the device constructor
         
-    def processEvent(self, event):
-        pass
-
-    def initialFunc(self, func):
+    def registerInitialFunction(self, func):
         self.func = func
