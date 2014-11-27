bcd792360031cc5c4b3aae89de3e81b1a80e2d4e
Martin Teichmann <martin.teichmann@xfel.eu>
2014-11-27 15:45:23 +0100

    declare old API default

--- src/pythonKarabo/karabo_package/__init__.py
+++ src/pythonKarabo/karabo_package/__init__.py
@@ -4,11 +4,11 @@ import os.path
 __all__ = ["AccessLevel", "AccessMode", "Assignment", "String", "Integer",
            "waitUntil", "sleep", "Slot"]
 
-if hasattr(sys, "karabo_api") and sys.karabo_api == 1:
-    __path__ = [os.path.join(__path__[0], "legacy"), __path__[0]]
-else:
+if hasattr(sys, "karabo_api") and sys.karabo_api == 2:
     from karabo.device import Device
     __all__.append("Device")
+else:
+    __path__ = [os.path.join(__path__[0], "legacy"), __path__[0]]
 
 import karabo.hash
 from karabo.hashtypes import String, Int32 as Integer, Double as Float, Slot
