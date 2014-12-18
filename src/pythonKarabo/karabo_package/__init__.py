a3ba523347031e944026a52db0e93536da677fd5
Martin Teichmann <martin.teichmann@xfel.eu>
2014-12-18 13:33:59 +0100

    make old api work again

--- src/pythonKarabo/karabo_package/__init__.py
+++ src/pythonKarabo/karabo_package/__init__.py
@@ -7,13 +7,13 @@ __all__ = ["AccessLevel", "AccessMode", "Assignment", "String", "Integer",
 if hasattr(sys, "karabo_api") and sys.karabo_api == 2:
     from karabo.device import Device
     __all__.append("Device")
+    import karabo.hash
+    from karabo.hashtypes import (String, Int32 as Integer, Double as Float,
+                                  Slot, Bool)
+    from karabo.async import waitUntil
+    from asyncio import sleep
+    from karabo.enums import *
 else:
     __path__ = [os.path.join(__path__[0], "legacy"), __path__[0]]
 
-import karabo.hash
-from karabo.hashtypes import (String, Int32 as Integer, Double as Float, Slot,
-                              Bool)
-from karabo.async import waitUntil
-from asyncio import sleep
-from karabo.enums import *
 
