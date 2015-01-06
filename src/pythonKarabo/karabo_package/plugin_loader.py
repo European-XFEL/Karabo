35e73d421a4e60269b2a37e5d5a5cef78ab76148
Martin Teichmann <martin.teichmann@xfel.eu>
2015-01-06 14:16:58 +0100

    remove plugins3 folder

--- src/pythonKarabo/karabo_package/plugin_loader.py
+++ src/pythonKarabo/karabo_package/plugin_loader.py
@@ -24,29 +24,15 @@ class PluginLoader(object):
         e.assignmentOptional().defaultValue("plugins")
         e.isDirectory().expertAccess().commit()
 
-        e = PATH_ELEMENT(expected).key("plugin3Directory")
-        e.displayedName("Python 3 Plugin Directory")
-        e.description("Directory to search and put plugins for python 3")
-        e.assignmentOptional().defaultValue("plugins3")
-        e.isDirectory().expertAccess().commit()
-
     def __init__(self, input):
         if "pluginDirectory" in input:
             self.plugins = input["pluginDirectory"]
-            self.plugins3 = input["plugin3Directory"]
         else:
             self.plugins = os.environ['PWD'] + "/plugins"
-            self.plugins3 = os.environ['PWD'] + "/plugins3"
-        sys.path.append(self.plugins3)
-        self.pattern = re.compile(r'\.py$')
+        sys.path.append(self.plugins)
 
     def getPluginDirectory(self):
-        return self.plugins3
+        return self.plugins
 
     def update(self):
-        for n in os.listdir(self.plugins):
-            if (n.endswith(".py") and
-                not os.path.exists(os.path.join(self.plugins3, n))):
-                    call(["2to3", "-Wno", self.plugins3,
-                          os.path.join(self.plugins, n)])
-        return [n[:-3] for n in os.listdir(self.plugins3) if n.endswith(".py")]
+        return [n[:-3] for n in os.listdir(self.plugins) if n.endswith(".py")]
