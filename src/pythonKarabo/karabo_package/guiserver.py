c458a25d528546cdbd7b980aa884ec0a9efd3135
Martin Teichmann <martin.teichmann@xfel.eu>
2015-01-14 10:45:29 +0100

    properly use coroutines in guiserver
    The guiserver did not use the coroutines/yield from properly,
    it did unnecessary locking.
    
    This patch also reduces the amount of coroutines necessary,
    by not letting everything run in parallel. Is that an option
    for all of python-karabo as well?

--- src/pythonKarabo/karabo_package/guiserver.py
+++ src/pythonKarabo/karabo_package/guiserver.py
@@ -1,13 +1,22 @@
 import sys
 sys.karabo_api = 2
 
-from asyncio import (async, coroutine, start_server, Lock, get_event_loop,
-                     sleep, gather, IncompleteReadError)
+from asyncio import (coroutine, start_server, get_event_loop,
+                     IncompleteReadError)
+from functools import wraps
 
 from karabo import Device, Integer, Assignment
 from karabo.hash import Hash
 from karabo import openmq
 from karabo.p2p import Channel
+
+
+def parallel(f):
+    f = coroutine(f)
+    @wraps(f)
+    def wrapper(self, *args, **kwargs):
+        self.async(f(self, *args, **kwargs))
+    return wrapper
 
 
 class GuiServer(Device):
@@ -24,9 +33,9 @@ class GuiServer(Device):
         self.systemTopology = Hash("device", Hash(), "server", Hash())
 
     def run(self):
+        self.async(self.log_handler())
         super().run()
-        async(self.run_server())
-        async(self.log_handler())
+        self.async(self.run_server())
 
     @coroutine
     def run_server(self):
@@ -36,7 +45,6 @@ class GuiServer(Device):
     @coroutine
     def connect_client(self, reader, writer):
         channel = Channel(reader, writer)
-        channel.lock = Lock()
         props = get_event_loop().connection.properties
         brokerInfo = Hash("type", "brokerInformation",
                           "host", props["MQBrokerHostName"],
@@ -48,60 +56,51 @@ class GuiServer(Device):
         try:
             while True:
                 info = yield from channel.readHash("Bin")
-                yield from getattr(self, "handle_" + info["type"])(
+                getattr(self, "handle_" + info["type"])(
                     channel, **{k: v for k, v in info.items() if k != "type"})
         except IncompleteReadError:
             pass
         finally:
             for k in list(self.deviceChannels):
-                yield from self.handle_stopMonitoringDevice(channel, k)
+                self.handle_stopMonitoringDevice(channel, k)
             self.channels.remove(channel)
 
-    @coroutine
     def handle_error(self, channel, **kwargs):
         print("ERROR", kwargs)
 
-    @coroutine
     def handle_login(self, channel, username, provider, password,
                      sessionToken):
-        yield from self.respond(channel, "systemTopology",
-                                systemTopology=self.systemTopology)
+        self.respond(channel, "systemTopology",
+                     systemTopology=self.systemTopology)
 
-    @coroutine
     def handle_reconfigure(self, channel, deviceId, configuration):
         self._ss.emit("call", {deviceId: ["slotReconfigure"]}, configuration)
 
-    @coroutine
     def handle_execute(self, channel, deviceId, command):
         self._ss.emit("call", {deviceId: [command]})
 
-    @coroutine
     def handle_initDevice(self, channel, serverId, **kwargs):
         self._ss.emit("call", {serverId: ["slotStartDevice"]}, Hash(kwargs))
 
-    @coroutine
+    @parallel
     def handle_getDeviceConfiguration(self, channel, deviceId):
         conf, deviceId = yield from self.call(deviceId, "slotGetConfiguration")
-        yield from self.respond(channel, "deviceConfiguration",
-                                deviceId=deviceId, configuration=conf)
+        self.respond(channel, "deviceConfiguration",
+                     deviceId=deviceId, configuration=conf)
 
-    @coroutine
     def handle_killServer(self, channel, serverId):
         self._ss.emit("call", {serverId: ["slotKillServer"]})
 
-    @coroutine
     def handle_killDevice(self, channel, deviceId):
         self._ss.emit("call", {deviceId: ["slotKillDevice"]})
 
-    @coroutine
     def handle_startMonitoringDevice(self, channel, deviceId):
         if deviceId not in self.deviceChannels:
             self.deviceChannels[deviceId] = set()
             self.registerDevice(deviceId)
         self.deviceChannels[deviceId].add(channel)
-        yield from self.handle_getDeviceConfiguration(channel, deviceId)
+        self.handle_getDeviceConfiguration(channel, deviceId)
 
-    @coroutine
     def handle_stopMonitoringDevice(self, channel, deviceId):
         channels = self.deviceChannels[deviceId]
         channels.discard(channel)
@@ -111,20 +110,18 @@ class GuiServer(Device):
             self._ss.disconnect(deviceId, "signalSchemaUpdated",
                                 self.slotSchemaUpdated)
 
-    @coroutine
+    @parallel
     def handle_getClassSchema(self, channel, serverId, classId):
         schema, _, _ = yield from self.call(serverId, "slotGetClassSchema",
                                             classId)
-        yield from self.respond(channel, "classSchema", serverId=serverId,
-                                classId=classId, schema=schema)
+        self.respond(channel, "classSchema", serverId=serverId,
+                     classId=classId, schema=schema)
 
-    @coroutine
+    @parallel
     def handle_getDeviceSchema(self, channel, deviceId):
         schema, id = yield from self.call(deviceId, "slotGetSchema", False)
-        yield from self.respond(channel, "deviceSchema", deviceId=id,
-                                schema=schema)
+        self.respond(channel, "deviceSchema", deviceId=id, schema=schema)
 
-    @coroutine
     def handle_getPropertyHistory(self, channel, deviceId, property, t0, t1,
                                   maxNumData=0):
         self._ss.emit("call", {"Karabo_DataLoggerManager_0":
@@ -133,27 +130,22 @@ class GuiServer(Device):
                           "from", t0, "to", t1, "maxNumData", maxNumData))
         self.histories[deviceId, property] = channel
 
-    @coroutine
     def slotPropertyHistory(self, deviceId, property, data):
         channel = self.histories.pop((deviceId, property), None)
         if channel is None:
             return
-        yield from self.respond(channel, "propertyHistory", deviceId=deviceId,
-                                property=property, data=data)
+        self.respond(channel, "propertyHistory", deviceId=deviceId,
+                     property=property, data=data)
 
-    @coroutine
     def slotSchemaUpdated(self, schema, deviceId):
-        yield from gather(
-            *(self.respond(c, "deviceSchema", deviceId=deviceId, schema=schema)
-              for c in self.channels))
+        for c in self.channels:
+            self.respond(c, "deviceSchema", deviceId=deviceId, schema=schema)
 
-    @coroutine
     def slotNotification(self, type, shortMessage, detailedMessage, deviceId):
-        yield from gather(
-            *(self.respond(c, "notification", deviceId=deviceId,
-                           messageType=type, shortMsg=shortMessage,
-                           detailedMsg=detailedMessage)
-              for c in self.channels))
+        for c in self.channels:
+            self.respond(c, "notification", deviceId=deviceId,
+                         messageType=type, shortMsg=shortMessage,
+                         detailedMsg=detailedMessage)
 
     @coroutine
     def log_handler(self):
@@ -167,15 +159,14 @@ class GuiServer(Device):
                 if e.status != 2103:
                     raise
             else:
-                yield from gather(
-                    *(self.respond(c, "log", message=message.data)
-                      for c in self.channels))
+                for c in self.channels:
+                    self.respond(c, "log", message=message.data)
 
     @coroutine
     def slotInstanceNew(self, instanceId, info):
         entry = self.updateSystemTopology(instanceId, info)
         for c in self.channels:
-            yield from self.respond(c, "instanceNew", topologyEntry=entry)
+            self.respond(c, "instanceNew", topologyEntry=entry)
 
         if "device" not in info:
             return
@@ -187,24 +178,22 @@ class GuiServer(Device):
     @coroutine
     def slotInstanceUpdated(self, instanceId, info):
         entry = self.updateSystemTopology(instanceId, info)
-        yield from gather(*(self.respond(c, "instanceUpdated",
-                                         topologyEntry=entry)
-                            for c in self.channels))
+        for c in self.channels:
+            self.respond(c, "instanceUpdated", topologyEntry=entry)
 
     @coroutine
     def slotInstanceGone(self, instanceId, info):
         type = info["type"]
-        yield from gather(
-            *(self.respond(c, "instanceGone", instanceId=instanceId,
-                           instanceType=type) for c in self.channels))
+        for c in self.channels:
+            self.respond(c, "instanceGone", instanceId=instanceId,
+                         instanceType=type)
         self.systemTopology[type].pop(instanceId, None)
 
     @coroutine
     def slotChanged(self, configuration, deviceId):
-        yield from gather(
-            *(self.respond(c, "deviceConfiguration",
-                           deviceId=deviceId, configuration=configuration)
-              for c in self.deviceChannels.get(deviceId, [])))
+        for c in self.deviceChannels.get(deviceId, []):
+            self.respond(c, "deviceConfiguration", deviceId=deviceId,
+                         configuration=configuration)
 
     @coroutine
     def slotPingAnswer(self, deviceId, info):
@@ -223,13 +212,11 @@ class GuiServer(Device):
         self._ss.connect(deviceId, "signalSchemaUpdated",
                          self.slotSchemaUpdated)
 
-    @coroutine
     def respond(self, channel, type, **kwargs):
-        with (yield from channel.lock):
-            h = Hash("type", type)
-            for k, v in kwargs.items():
-                h[k] = v
-            channel.writeHash(h, "Bin")
+        h = Hash("type", type)
+        for k, v in kwargs.items():
+            h[k] = v
+        channel.writeHash(h, "Bin")
 
 if __name__ == "__main__":
     from asyncio import set_event_loop
