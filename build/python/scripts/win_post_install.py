d0e531c49d756ea09bfdd00b28298ffe2d57761d
jszuba <janusz.szuba@xfel.eu>
2015-01-16 09:42:36 +0000

    adapting to python3.4; still issues with running postinstall script which creates menu entry and shortcuts
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/trunk@16019 a23ab9f9-1ea2-4934-960d-990259f9ea8c

--- build/python/scripts/win_post_install.py
+++ build/python/scripts/win_post_install.py
@@ -1,3 +1,4 @@
+#!python
 """Windows-specific part of the installation"""
 from __future__ import print_function
 import os, sys
@@ -10,32 +11,36 @@ def create_shortcut_safe(target, description, link_file, args, workdir):
 
 def install():
     prefix = sys.prefix
-    python = prefix + r'\pythonw.exe'
+    python = os.path.join(prefix, 'pythonw.exe')
+    title = 'KaraboGUI-VERSION'
 
     # Lookup path to common startmenu ...
     mv_dir = get_special_folder_path('CSIDL_COMMON_PROGRAMS') 
-    mv_dir += r'\Karabo'
+    mv_dir = os.path.join(mv_dir, title)
 
     if not os.path.isdir(mv_dir):
         os.mkdir(mv_dir)
         directory_created(mv_dir)
 
-    karaboGuiPath = get_python_lib() + r'\karaboGui'
-    guiScript = karaboGuiPath + r'\karabo-gui.py'
+    karaboGuiPath = os.path.join(get_python_lib(), 'karaboGui')
+    guiScript = os.path.join(karaboGuiPath, 'karabo-gui.py')
     workDir=get_special_folder_path('CSIDL_DESKTOPDIRECTORY')
 
     # Create program shortcuts ...
-    shortcut = mv_dir + r'\Karabo.lnk'
-    create_shortcut_safe(python, 'Karabo', shortcut, guiScript, workdir=workDir)
+    shortcut = os.path.join(mv_dir, "{}.lnk".format(title))
+    create_shortcut_safe(python, title, shortcut, guiScript, workdir=workDir)
 
 def remove():
     pass
 
 if len(sys.argv) > 1:
     if sys.argv[1] == '-install':
+        f = open(r"D:\test.txt",'w')
+        print('Creating Shortcut', file=f)
         install()
     elif sys.argv[1] == '-remove':
         remove()
     else:
         pass
         #print "Script was called with option %s" % sys.argv[1]"
+
