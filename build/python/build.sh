8a26b73dffddfd3bdaf8ffb95958268f69a3bc68
jszuba <janusz.szuba@xfel.eu>
2015-01-16 09:42:36 +0000

    adapting to python3.4; still issues with running postinstall script which creates menu entry and shortcuts
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/trunk@16019 a23ab9f9-1ea2-4934-960d-990259f9ea8c

--- build/python/build.sh
+++ build/python/build.sh
@@ -8,12 +8,25 @@ if [ "$VERSION" = "trunk" ]; then
     VERSION=r${tmp##*: }
 fi
 
+if [ -z $KARABO ]; then
+    if [ -e $HOME/.karabo/karaboFramework ]; then
+        KARABO=$(cat $HOME/.karabo/karaboFramework)
+    else
+      echo "ERROR Could not find karaboFramework. Make sure you have installed the karaboFramework."
+      exit 1
+    fi
+fi
+
 
 #sed -i "/version/c\    version=\"$VERSION\"," setup.py 
 echo $VERSION > VERSION
+sed -i s/VERSION/$VERSION/ scripts/win_post_install.py
+sed -i s/VERSION/$VERSION/ setup.cfg
 
-( cd ../../extern/resources/suds; unzip -q suds-jurko-0.6.zip )
+#( cd ../../extern/resources/suds; unzip -q suds-jurko-0.6.zip )
 
-python setup.py  bdist_wininst
+$KARABO/extern/bin/python3 setup.py  bdist_wininst
 
+sed -i s/$VERSION/VERSION/ scripts/win_post_install.py
+sed -i s/$VERSION/VERSION/ setup.cfg
 
