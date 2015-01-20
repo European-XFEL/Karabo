9f8c7567774d936fd0df38ad8f1f505ae3674ebe
esenov <sergey.esenov@xfel.eu>
2015-01-20 13:00:55 +0000

    Make direct slot call when "heartbeat" message arriving (no queuing)
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/trunk@16042 a23ab9f9-1ea2-4934-960d-990259f9ea8c

--- src/karabo/xms/SignalSlotable.hh
+++ src/karabo/xms/SignalSlotable.hh
@@ -106,9 +106,6 @@ namespace karabo {
             boost::mutex m_waitMutex;
             boost::condition_variable m_hasNewEvent;
 
-            boost::mutex m_waitHeartbeatMutex;
-            boost::condition_variable m_hasNewHeartbeat;
-
             typedef std::pair<karabo::util::Hash::Pointer /*header*/, karabo::util::Hash::Pointer /*body*/> Event;
             struct CompareEventPriority {
                 bool operator()(const Event& lhs, const Event& rhs) const {
@@ -120,11 +117,6 @@ namespace karabo {
             boost::mutex m_eventQueueMutex;
             bool m_runEventLoop;
             boost::thread_group m_eventLoopThreads;
-
-            std::deque<Event> m_heartbeatQueue;
-            boost::mutex m_heartbeatQueueMutex;
-            bool m_runHeartbeatLoop;
-            boost::thread_group m_heartbeatLoopThreads;
 
             bool m_isPingable;
 
