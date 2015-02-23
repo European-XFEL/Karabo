5b8eec30d083014c618e2a089849d373cc47095f
esenov <sergey.esenov@xfel.eu>
2015-02-23 12:07:37 +0000

    Rename 'async_test' function into 'async_tst' to avoid running 'async_test' auxiliary function as a unit test by nosetests tool.
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/trunk@16320 a23ab9f9-1ea2-4934-960d-990259f9ea8c

--- src/pythonKarabo/tests/remote_test.py
+++ src/pythonKarabo/tests/remote_test.py
@@ -10,7 +10,7 @@ from karabo.eventloop import EventLoop
 from karabo.python_device import Device
 from karabo import Slot, Integer
 
-def async_test(f):
+def async_tst(f):
     @wraps(f)
     def wrapper(self, *args, **kwargs):
         coro = coroutine(f)
@@ -128,21 +128,21 @@ class Local(Device):
 
 
 class Tests(TestCase):
-    @async_test
+    @async_tst
     def test_execute(self):
         self.assertFalse(remote.done)
         yield from local.letitdo()
         yield from sleep(0.1)
         self.assertTrue(remote.done)
 
-    @async_test
+    @async_tst
     def test_change(self):
         remote.value = 7
         yield from local.letitchange()
         yield from sleep(0.1)
         self.assertEqual(remote.value, 3)
 
-    @async_test
+    @async_tst
     def test_disconnect(self):
         yield from local.disconnect()
         self.assertEqual(local.f1, -1)
@@ -151,7 +151,7 @@ class Tests(TestCase):
         self.assertEqual(local.f4, 29)
 
 
-    @async_test
+    @async_tst
     def test_set(self):
         remote.value = 7
         yield from local.letset()
@@ -160,26 +160,26 @@ class Tests(TestCase):
         self.assertEqual(local.f3, 6)
 
 
-    @async_test
+    @async_tst
     def test_generic(self):
         remote.value = 7
         yield from local.dogeneric()
         yield from sleep(0.1)
         self.assertEqual(remote.value, 22)
 
-    @async_test
+    @async_tst
     def test_other(self):
         yield from local.other()
         yield from sleep(0.1)
         self.assertEqual(remote.value, 102)
 
-    @async_test
+    @async_tst
     def test_setwait(self):
         yield from local.setwait()
         self.assertEqual(remote.value, 200)
         self.assertEqual(remote.counter, 300)
 
-    @async_test
+    @async_tst
     def test_setnowait(self):
         remote.value = 0
         remote.counter = 0
@@ -191,7 +191,7 @@ class Tests(TestCase):
         self.assertEqual(remote.counter, 300)
 
 
-    @async_test
+    @async_tst
     def test_waituntil(self):
         yield from local.waituntil()
         self.assertEqual(local.f1, 0)
