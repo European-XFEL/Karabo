64e895354be7e791b674e878f735d39dd8f093c9
Martin Teichmann <martin.teichmann@xfel.eu>
2015-02-23 17:08:45 +0100

    make macros print output to GUI

---
+++ src/pythonKarabo/karabo/output.py
@@ -0,0 +1,29 @@
+""" This module redirects output """
+
+from asyncio import coroutine, Task
+import threading
+
+threadData = threading.local()
+
+class StdOut:
+    """ An output stream that redirects output to the karabo network """
+    def __init__(self, base):
+        self.base = base
+
+    def write(self, data):
+        try:
+            task = Task.current_task()
+            task.instance.printToConsole(data)
+        except (AttributeError, AssertionError):
+            try:
+                instance = threadData.instance
+                func = instance.printToConsole
+                @coroutine
+                def print():
+                    func(data)
+                instance.async(print())
+            except AttributeError:
+                self.base.write(data)
+
+
+
