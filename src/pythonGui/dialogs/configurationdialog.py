8d6ccc50003ce1d4e287e939c45654b8d79d83d3
Kerstin Weger <kerstin.weger@xfel.eu>
2015-02-25 13:53:40 +0100

    Add "Apply configurations" to context menu of Configurations

--- src/pythonGui/dialogs/configurationdialog.py
+++ src/pythonGui/dialogs/configurationdialog.py
@@ -8,12 +8,13 @@
 """This module contains dialog classes which allow the loading and saving of
 configurations."""
 
-__all__ = ["SelectProjectDialog", "SelectProjectConfigurationDialog"]
+__all__ = ["SelectProjectDialog", "SelectProjectConfigurationDialog",
+           "SelectMultipleProjectConfigurationDialog"]
 
 
 from karabo.project import ProjectConfiguration
 
-from PyQt4.QtCore import Qt
+from PyQt4.QtCore import pyqtSignal, Qt
 from PyQt4.QtGui import (QDialog, QDialogButtonBox, QFormLayout, QLineEdit,
                          QListWidget, QListWidgetItem, QTreeWidget,
                          QTreeWidgetItem, QVBoxLayout)
@@ -117,7 +118,7 @@ class SelectProjectConfigurationDialog(QDialog):
 
     def _populate(self, projects):
         for p in projects:
-            item = QTreeWidgetItem([p.name])
+            item = QTreeWidgetItem(self.projConfWidget, [p.name])
             item.setData(0, Qt.UserRole, p)
             self.projConfWidget.addTopLevelItem(item)
             item.setExpanded(True)
@@ -142,3 +143,119 @@ class SelectProjectConfigurationDialog(QDialog):
         self.buttonBox.button(QDialogButtonBox.Ok).setEnabled((item is not None) \
                 and isinstance(item.data(0, Qt.UserRole), ProjectConfiguration))
 
+
+class SelectMultipleProjectConfigurationDialog(QDialog):
+    """
+    Select configurations of a project and apply them to the specific devices.
+    """
+    
+    def __init__(self, project):
+        super(SelectMultipleProjectConfigurationDialog, self).__init__()
+
+        self.setWindowTitle("Select configurations")
+        
+        self.projConfWidget = CheckableTreeWidget(self)
+        self.projConfWidget.headerItem().setHidden(True)
+        self._populate(project)
+        self.projConfWidget.itemChecked.connect(self.onHandleItemChecked)
+        
+        vLayout = QVBoxLayout(self)
+        vLayout.addWidget(self.projConfWidget)
+        
+        self.buttonBox = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
+        self.buttonBox.accepted.connect(self.accept)
+        self.buttonBox.rejected.connect(self.reject)
+        vLayout.addWidget(self.buttonBox)
+
+
+    def _populate(self, project):
+        for deviceId, configList in project.configurations.items():
+            deviceItem = QTreeWidgetItem(self.projConfWidget, [deviceId])
+            self.projConfWidget.addTopLevelItem(deviceItem)
+            deviceItem.setExpanded(True)
+
+            for index, c in enumerate(configList):
+                confItem = CheckableTreeWidgetItem([c.filename])
+                confItem.setData(0, Qt.UserRole, c)
+                deviceItem.addChild(confItem)
+                confItem.setExpanded(True)
+                
+                if index == 0:
+                    # First element selected
+                    confItem.setCheckState(0, Qt.Checked)
+                else:
+                    confItem.setCheckState(0, Qt.Unchecked)
+
+
+    def projectConfigurations(self):
+        """
+        This function returns all selected project configurations associated to
+        a deviceId.
+        """
+        configurations = {}
+        for i in range(self.projConfWidget.topLevelItemCount()):
+            item = self.projConfWidget.topLevelItem(i)
+            
+            for j in range(item.childCount()):
+                childItem = item.child(j)
+                if childItem.checkState(0) == Qt.Checked:
+                    configurations[item.text(0)] = childItem.data(0, Qt.UserRole)
+
+        return configurations
+
+
+    def onHandleItemChecked(self, item, column):
+        siblings = item.siblings()
+        if item.checkState(column) == Qt.Checked:
+            # Uncheck all siblings
+            for s in siblings:
+                self.projConfWidget.blockSignals(True)
+                s.setCheckState(0, Qt.Unchecked)
+                self.projConfWidget.blockSignals(False)
+        else:
+            if siblings: item.setCheckState(0, Qt.Checked)
+
+
+class CheckableTreeWidget(QTreeWidget):
+    itemChecked = pyqtSignal(object, int)
+
+    def __init__(self, parent):
+        QTreeWidget.__init__(self, parent)
+
+
+class CheckableTreeWidgetItem(QTreeWidgetItem):
+    
+    def __init__(self, strings):
+        QTreeWidgetItem.__init__(self, strings)
+        self.setFlags(self.flags() | Qt.ItemIsUserCheckable)
+
+
+    def setData(self, column, role, value):
+        state = self.checkState(column)
+        QTreeWidgetItem.setData(self, column, role, value)
+        
+        if role == Qt.CheckStateRole and state != self.checkState(column):
+            treewidget = self.treeWidget()
+            if treewidget is not None:
+                treewidget.itemChecked.emit(self, column)
+
+
+    def siblings(self):
+        """
+        This function returns a list of all siblings of this item.
+        """
+        siblings = []
+        parent = self.parent()
+        if parent is not None:
+            for i in range(parent.childCount()):
+                childItem = parent.child(i)
+                if childItem != self:
+                    siblings.append(childItem)
+        else:
+            parent = self.treeWidget()
+            for i in range(parent.topLevelItemCount()):
+                childItem = parent.topLevelItem(i)
+                if childItem != self:
+                    siblings.append(childItem)
+        
+        return siblings        
