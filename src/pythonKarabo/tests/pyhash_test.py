c8289bff0034ac636f45292b16a949f8c155c8af
esenov <esenov@a23ab9f9-1ea2-4934-960d-990259f9ea8c>
2015-02-28 11:52:45 +0000

    Withdraw temporary workaround in pyhash_test unit test, because support of 'bytes' type objects added to karathon
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/trunk@16400 a23ab9f9-1ea2-4934-960d-990259f9ea8c

--- src/pythonKarabo/tests/pyhash_test.py
+++ src/pythonKarabo/tests/pyhash_test.py
@@ -202,7 +202,8 @@ class Hash_TestCase(unittest.TestCase):
         r = BinaryParser()
         s = w.write(self.create_hash())
         ser = BinarySerializerHash.create("Bin")
-        h = ser.load(bytearray(s))
+        print(s)
+        h = ser.load(s)
         ret = Hash.decode(ser.save(h), "Bin")
         self.check_hash(ret)
 
