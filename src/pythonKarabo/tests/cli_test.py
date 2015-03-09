9a4d046cd18405774aaaa6c34763a17395babd7e
Martin Teichmann <martin.teichmann@xfel.eu>
2015-03-09 14:19:18 +0100

    make sure an event loop can be garbage collected

---
+++ src/pythonKarabo/tests/cli_test.py
@@ -0,0 +1,35 @@
+import karabo
+karabo.api_version = 2
+
+import gc
+from unittest import TestCase, main
+import time
+import weakref
+
+from karabo.macro import Macro
+from karabo import Integer, Slot
+
+
+class Remote(Macro):
+    counter = Integer(defaultValue=-1)
+
+    @Slot()
+    def count(self):
+        for i in range(30):
+            self.counter = i
+            time.sleep(0.1)
+
+
+class Tests(TestCase):
+    def test_delete(self):
+        remote = Remote()
+        remote.count()
+        self.assertEqual(remote.counter, 29)
+        r = weakref.ref(remote)
+        del remote
+        gc.collect()
+        self.assertIsNone(r())
+
+
+if __name__ == "__main__":
+    main()
