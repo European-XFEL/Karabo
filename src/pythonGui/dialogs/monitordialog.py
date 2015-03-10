2649ccb97767cf1b9c31f5cb090f881a029f690e
Kerstin Weger <kerstin.weger@xfel.eu>
2015-03-10 10:28:43 +0100

    Use combobox to select from project devices when defining a monitor object

--- src/pythonGui/dialogs/monitordialog.py
+++ src/pythonGui/dialogs/monitordialog.py
@@ -10,8 +10,7 @@ __all__ = ["MonitorDialog"]
 import icons
 
 from PyQt4 import uic
-#from PyQt4.QtCore import pyqtSlot, QRegExp, Qt, QSize
-from PyQt4.QtGui import (QDialog, QDialogButtonBox)
+from PyQt4.QtGui import (QDialog, QDialogButtonBox, QLineEdit)
 
 import os.path
 
@@ -19,25 +18,42 @@ import os.path
 class MonitorDialog(QDialog):
 
 
-    def __init__(self, monitor):
+    def __init__(self, project, monitor):
+        """
+        Create a dialog to edit a \monitor object which belongs to a certain
+        \project.
+        """
         QDialog.__init__(self)
         uic.loadUi(os.path.join(os.path.dirname(__file__), 'monitordialog.ui'), self)
         
+        # Lineedit to add a user-defined not yet available device of the project
+        self.leDeviceId = QLineEdit()
+        self.cbDeviceId.setLineEdit(self.leDeviceId)
+        
+        # Add all available devices of project
+        for d in project.devices:
+            self.cbDeviceId.addItem(d.id, d)
+        
         self.leName.textChanged.connect(self.onChanged)
-        self.leDeviceId.textChanged.connect(self.onChanged)
+        self.cbDeviceId.currentIndexChanged.connect(self.onSelectDeviceId)
+        self.leDeviceId.textChanged.connect(self.onSelectDeviceId)
         self.leDeviceProperty.textChanged.connect(self.onChanged)
         
         if monitor is None:
             title = "Add monitor"
-            
-            self.buttonBox.button(QDialogButtonBox.Ok).setEnabled(False)
         else:
             title = "Edit monitor"
             
             # Set monitor data to dialog
             self.leName.setText(monitor.name)
             config = monitor.config
-            self.leDeviceId.setText(config.get("deviceId"))
+            
+            deviceId = config.get("deviceId")
+            index = self.cbDeviceId.findText(deviceId)
+            if index > 0:
+                self.cbDeviceId.setCurrentIndex(index)
+            else:
+                self.leDeviceId.setText(deviceId)
             self.leDeviceProperty.setText(config.get("deviceProperty")) 
             self.leMetricPrefixSymbol.setText(config.get("metricPrefixSymbol"))
             self.leUnitSymbol.setText(config.get("unitSymbol"))
@@ -45,12 +61,9 @@ class MonitorDialog(QDialog):
         
         self.setWindowTitle(title)
         
-        self.pbSelectDeviceId.setIcon(icons.deviceInstance)
-        self.pbSelectDeviceId.clicked.connect(self.onSelectDeviceId)
-        
-        self.pbSelectProperty.setIcon(icons.editFind)
-        self.pbSelectProperty.clicked.connect(self.onSelectDeviceProperty)
-        self.pbSelectProperty.setDisabled(not self.leDeviceId.text())
+        self.tbDeviceProperty.setIcon(icons.editFind)
+        self.tbDeviceProperty.clicked.connect(self.onSelectDeviceProperty)
+        self.onSelectDeviceId()
 
 
     @property
@@ -60,7 +73,7 @@ class MonitorDialog(QDialog):
 
     @property
     def deviceId(self):
-        return self.leDeviceId.text()
+        return self.cbDeviceId.currentText()
 
 
     @property
@@ -84,13 +97,14 @@ class MonitorDialog(QDialog):
 
 
     def onChanged(self, text):
-        disable = not self.leName.text() or not self.leDeviceId.text() or \
-                 not self.leDeviceProperty.text()
+        disable = not self.name or not self.deviceId or not self.deviceProperty
         self.buttonBox.button(QDialogButtonBox.Ok).setDisabled(disable)
 
 
     def onSelectDeviceId(self):
-        print("onSelectDeviceId")
+        self.tbDeviceProperty.setDisabled(not self.leDeviceId.text() or \
+                                          self.cbDeviceId.currentIndex() < 0)
+        self.onChanged(self.deviceId)
 
 
     def onSelectDeviceProperty(self):
