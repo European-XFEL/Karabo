abe6be57aade8085b48dd674440d6d9138fbbee8
Martin Teichmann <martin.teichmann@xfel.eu>
2015-04-09 11:52:49 +0200

    more commands for ikarabo
    this adds commands to enquire currently existing servers and devices,
    as well as starting devices.

--- src/pythonKarabo/tests/tests_api_2/cli_test.py
+++ src/pythonKarabo/tests/tests_api_2/cli_test.py
@@ -11,7 +11,9 @@ from karabo import Integer, Slot
 from karabo.eventloop import EventLoop
 from karabo.hash import Hash
 from karabo.python_server import DeviceServer
-from karabo.device_client import getDevice
+from karabo.device_client import (
+    getDevice, instantiate, DeviceClientBase, getDevices)
+from karabo.python_device import Device
 
 from .eventloop import setEventLoop
 
@@ -24,6 +26,18 @@ class Remote(Macro):
         for i in range(30):
             self.counter = i
             time.sleep(0.1)
+
+    @Slot()
+    def instantiate(self):
+        instantiate("testServer", "Other", "other")
+
+
+class Other(Device):
+    something = Integer(defaultValue=333)
+
+
+class DeviceClient(Macro, DeviceClientBase):
+    pass
 
 
 class Tests(TestCase):
@@ -84,6 +98,41 @@ class Tests(TestCase):
         loop.run_forever()
         loop.close()
 
+    @coroutine
+    def init_server(self, server):
+        yield from server.startInstance()
+        remote = Remote(_deviceId_="remote")
+        yield from remote.startInstance()
+        proxy = yield from getDevice("remote")
+        yield from proxy.instantiate()
+        yield from sleep(4)
+        return (yield from getDevice("other"))
+
+    def test_server(self):
+        loop = setEventLoop()
+        server = DeviceServer(dict(serverId="testServer"))
+        task = loop.create_task(self.init_server(server), server)
+        proxy = loop.run_until_complete(task)
+        self.assertEqual(proxy.something, 333)
+        async(server.slotKillServer())
+        loop.run_forever()
+        loop.close()
+
+    @coroutine
+    def init_topo(self, dc):
+        yield from dc.startInstance()
+        other = Other(dict(_deviceId_="other"))
+        self.assertNotIn("other", getDevices())
+        yield from other.startInstance()
+        yield from sleep(1)
+        self.assertIn("other", getDevices())
+
+    def test_topology(self):
+        loop = setEventLoop()
+        dc = DeviceClient(dict(_deviceId_="dc"))
+        task = loop.create_task(self.init_topo(dc), dc)
+        loop.run_until_complete(task)
+        loop.close()
 
 if __name__ == "__main__":
     main()
