d1a987045d29b2a9cb8af2ccf5590f003f876958
heisenb <burkard.heisen@xfel.eu>
2015-04-15 14:01:20 +0000

    Added some initial idea for an interface class (c++), should not harm anything existing. The commit is to not forget, but the feature will be implemented in later release.
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/trunk@17084 a23ab9f9-1ea2-4934-960d-990259f9ea8c

--- src/karabo/core.hpp
+++ src/karabo/core.hpp
@@ -17,6 +17,7 @@
 #include "core/StartStopFsm.hh"
 #include "core/CameraFsm.hh"
 #include "core/NoFsm.hh"
+#include "core/MotorInterface.hh"
 #include "core/DeviceServer.hh"
 #include "core/Runner.hh"
 
