ef3e525dc93800791bd285c3d54746ec582f6f2d
Martin Teichmann <martin.teichmann@xfel.eu>
2015-04-22 16:49:46 +0200

    add documentation
    this also gives a minor improvement to assure all code is called
    in case of error.

--- src/pythonKarabo/karabo/metamacro.py
+++ src/pythonKarabo/karabo/metamacro.py
@@ -1,29 +1,47 @@
 from asyncio import coroutine, gather
 
+from karabo.enums import AccessMode
 from karabo.python_device import Device
 from karabo.hashtypes import String
 from karabo.macro import Macro
 
 
 class MetaMacro(Device):
-    code = String()
-    project = String()
-    module = String()
+    """This is the device that starts macros.
 
-    def __init__(self, configuration):
-        super().__init__(configuration)
+    It doesn't actually get instantiated itself, but when it is started will
+    execute the code it is given and start the macros therein."""
+    code = String(
+        displayedName="Python Code",
+        description="The code to be executed. It typically defines Macros",
+        accessMode=AccessMode.INITONLY)
+    project = String(
+        displayedName="Containing Project",
+        description="The project this code is contained in",
+        accessMode=AccessMode.INITONLY)
+    module = String(
+        displayedName="Name of Module",
+        description="The name of the module within the project",
+        accessMode=AccessMode.INITONLY)
+
+    def __init__(self, config):
+        super().__init__(config)
         Macro.subclasses = []
-        code = compile(self.code, self.module, "exec")
-        exec(code, globals())
-        self.classes = Macro.subclasses
-        Macro.subclasses = []
+        try:
+            code = compile(self.code, self.module, "exec")
+            exec(code, globals())
+            self.classes = Macro.subclasses
+        finally:
+            Macro.subclasses = []
 
     @coroutine
     def run_async(self):
+        # this does not call super, as we don't want to run MetaMacro itself,
+        # but only the macros in the supplied code
         p = dict(_serverId_=self.serverId, project=self.project,
                  module=self.module)
         objs = []
         for c in self.classes:
-            p["_deviceId_"] ="{}-{}".format(self.deviceId, c.__name__)
+            p["_deviceId_"] = "{}-{}".format(self.deviceId, c.__name__)
             objs.append(c(p))
         yield from gather(*[o.startInstance() for o in objs])
