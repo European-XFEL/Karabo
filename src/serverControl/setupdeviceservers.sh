463c9809acd06d7f9974f54bf6c15cd9d09f2b19
kumarm <mayank.kumar@xfel.eu>
2015-04-24 14:18:17 +0000

    works: server control tool now sets up different kinds of device servers differently
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/branches/1.3@17194 a23ab9f9-1ea2-4934-960d-990259f9ea8c

--- src/serverControl/setupdeviceservers.sh
+++ src/serverControl/setupdeviceservers.sh
@@ -23,21 +23,88 @@ INSTALL_PREFIX=$( grep install_prefix ${HOST_INFO_FILE} | cut -d'=' -f2)
 KARABO_USER=$( grep karabo_user ${HOST_INFO_FILE} | cut -d'=' -f2)
 DEVICE_SERVER_IDS_LIST=$( grep ds_ids ${HOST_INFO_FILE} | cut -d'=' -f2)
 
-BROKER_HOSTNAME=$(echo ${BROKER_HOSTS} | cut -d':' -f1 )
-BROKER_PORT=$(echo ${BROKER_HOSTS} | cut -d':' -f2)
-
 ###### copy all scripts in bin directory from karaboRun to inside run directory
 ANY_KARABO=$( grep karabo_fws ${HOST_INFO_FILE} | cut -d'=' -f2 | cut -d',' -f1)
 if [[ ! -d ${RUN_PREFIX}/${INSTALLATION_NAME}/bin ]]; then mkdir ${RUN_PREFIX}/${INSTALLATION_NAME}/bin; fi
 cp ${INSTALL_PREFIX}/${ANY_KARABO}/${KARABO_RUN}/bin/. ${RUN_PREFIX}/${INSTALLATION_NAME}/bin -R
 
+# adjust allInfo file
+cat > ${RUN_PREFIX}/${INSTALLATION_NAME}/bin/allInfo <<End-of-file
+
+##########################################################################################
+# allInfo contains configuration definitions used to manage master, single or multi device
+# server, gui and other applications used in a localhost device test system.
+#
+# management scripts:
+#   1. allStart - to start karabo processes defined by the ALLINFO environmental
+#                 allStart has a single optional parameter "-nox" which controls whether
+#                 the processes are started as individual xterms or accumulated as background jobs
+#   2. allStop  - to stop started processes
+#   3. allCheck - to check the definitions used by test system scripts
+#                 this script is the only check made of the definitions used
+#   5. allRestart - to restart any "missing" applicationsd
+#                 a candidate to a missing application is the gui which was closed at
+#                 the end of the last session with all other applications left running.
+#
+# author: CY 18.12.2012, largely modified: BH 09.04.2015
+
+
+##################
+#  Broker Hosts  #
+##################
+
+# This setting is the default, it will try the XFEL broker first and failover to a local if unsuccessful
+export KARABO_BROKER_HOSTS=${BROKER_HOSTS}
+
+
+#################
+# Broker Topic  #
+#################
+
+# By default your username will become your topic name
+
+# You may want to fix it using:
+
+#export KARABO_BROKER_TOPIC=xbeam
+
+
+##################################
+# Kill Signal (used by allStop)  #
+##################################
+
+KARABO_KILL_SIGNAL=9
+
+
+################
+# Applications #
+################
+
+# Component definitions, currently 6 tokens per process
+# 1. relative path of the starter filename (including the starter)
+# 2. blank separated parameters to starter
+# 3. delay, in seconds, before starting next process
+# 4. number of scroll lines of xterm
+# 5. if "yes" use the x11 display specified in XUSE environmental. To start applications on the
+#    control host define XUSE to the main console ($ export XUSE=":0") and, provided your login account
+#    is identical to the console's owner account, all applications with 9="yes" will be displayed there.
+#    Applications started in this way will remain alive when the session is closed.
+# Note: additional servers can be added by duplicating a server line and performing
+# the necessary directory [definition 2] renaming (servers have unique directories)
+# and providing a unique ALLINFO array index
+
+KARABO_SERVERS="../"
+
+End-of-file
+
 IFS=',' read -a ds_ids_array <<< "${DEVICE_SERVER_IDS_LIST}"
+counter=0
 for one_ds_id in ${ds_ids_array[@]}
     do
         :
         one_ds_id_dir="${RUN_PREFIX}/${INSTALLATION_NAME}/${one_ds_id}"
         if [[ ! -d ${one_ds_id_dir} ]]; then mkdir -p ${one_ds_id_dir}; fi
 
+        # create serverId.xml
         serverid_xml_file="${one_ds_id_dir}/serverId.xml"
         if [[ ! -e ${serverid_xml_file} ]]; then touch ${serverid_xml_file}; fi
         echo ${one_ds_id} > ${serverid_xml_file}
@@ -68,7 +135,6 @@ for one_ds_id in ${ds_ids_array[@]}
   <Logger><priority>INFO</priority></Logger>
 </DeviceServer>
 End-of-file
-
             #create startup script
             cat > ${one_ds_id_dir}/startCppServer <<End-of-file
 #!/bin/bash
@@ -103,16 +169,24 @@ until \${KARABO}/bin/karabo-deviceserver "$@"; do
 done
 
 End-of-file
+            chmod +x ${one_ds_id_dir}/startCppServer
+            echo "ALLINFO[${counter}]=\${KARABO_SERVERS}/dataLoggerServer/startCppServer::0:100:yes" >> ${RUN_PREFIX}/${INSTALLATION_NAME}/bin/allInfo
 
         elif [[ ${one_ds_id} = "guiServer" ]]; then
             echo "guiServer found"
         elif [[ ${one_ds_id} = "macroServer" ]]; then
             echo "macroServer found"
         else
-            echo "some other device server"
-
+            echo "some other device server found"
             # create autoload.xml
-
+            cat > ${one_ds_id_dir}/autoload.xml <<End-of-file
+<?xml version="1.0"?>
+<DeviceServer>
+  <Logger>
+    <priority>INFO</priority>
+  </Logger>
+</DeviceServer>
+End-of-file
             # setup device server
             one_ds_plugins_dir="${one_ds_id_dir}/plugins"
             if [[ ! -d ${one_ds_plugins_dir} ]]; then mkdir ${one_ds_plugins_dir}; fi
@@ -134,10 +208,16 @@ End-of-file
             python_found=$(find ${one_ds_plugins_dir} -name \*.py)
             cpp_found=$(find ${one_ds_plugins_dir} -name \*.so)
             server_type=""
+            device_server_name=""
+            special_argument=""
             if [[ -n ${python_found} && -z ${cpp_found} ]]; then
                 server_type="python"
+                device_server_name="PythonServerApi1"
+                special_argument="DeviceServer"
             elif [[ -z ${python_found} && -n ${cpp_found} ]]; then
                 server_type=""
+                device_server_name="CppServer"
+                special_argument=""
             elif [[ -n ${python_found} && -n ${cpp_found} ]]; then
                 echo "mixing python and cpp plugins in the same device server ${one_ds_id} is not permitted. Exiting."
                 exit 2
@@ -146,20 +226,21 @@ End-of-file
                 exit 2
             fi
 
+            configuration=$(grep configuration ${DS_ID_INFO_FILE} | cut -d'=' -f2)
+
             # create startup script
-            cat > ${one_ds_id_dir}/start${server_type^}DeviceServer <<End-of-file
+            cat > ${one_ds_id_dir}/start${device_server_name} <<End-of-file
 #!/bin/bash
 #
 # This file was automatically generated. Do not edit.
 #
 
-export KARABO_BROKER_HOST=${BROKER_HOSTNAME}
-export KARABO_BROKER_PORT=${BROKER_PORT}
+export KARABO_BROKER_HOSTS=${BROKER_HOSTS}
 export KARABO_BROKER_TOPIC=${INSTALLATION_NAME}
 KARABO=${INSTALL_PREFIX}/${karabo_fw}
 SERVER_ID=${one_ds_id}
 # should take config from json file
-ARGS=""
+ARGS=${configuration}
 
 failed=0
 failedmod10=0
@@ -186,7 +267,9 @@ done
 
 End-of-file
             chmod +x ${one_ds_id_dir}/start${server_type^}DeviceServer
+            echo "ALLINFO[${counter}]=\${KARABO_SERVERS}/${one_ds_id}/start${device_server_name}:${special_argument}:0:100:yes" >> ${RUN_PREFIX}/${INSTALLATION_NAME}/bin/allInfo
         fi
+        counter=$((counter+1))
     done
 
 chown  -R ${KARABO_USER}:exfel "${RUN_PREFIX}"/"${INSTALLATION_NAME}"
