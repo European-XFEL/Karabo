b31942d6d9b46441942212953deb57011df1f34e
Martin Teichmann <martin.teichmann@xfel.eu>
2015-04-30 16:36:06 +0200

    add NoWait versions of shutdown and instantiate

--- src/pythonKarabo/karabo/cli.py
+++ src/pythonKarabo/karabo/cli.py
@@ -10,7 +10,7 @@ import IPython
 from karabo.device_client import (
     getDevice, waitUntil, waitUntilNew, setWait, setNoWait, execute,
     executeNoWait, DeviceClientBase, getDevices, getClasses, getServers,
-    instantiate, connectDevice, shutdown)
+    instantiate, connectDevice, shutdown, shutdownNoWait, instantiateNoWait)
 from karabo.eventloop import NoEventLoop
 from karabo.macro import Macro
 
@@ -37,32 +37,27 @@ ip = IPython.get_ipython()
 
 
 def device_completer(self, line):
+    #print("device")
     return list(devices.systemTopology["device"])
 
-
-def server_completer(self, line):
-    return list(devices.systemTopology["server"])
-
-
-first_param = re.compile("\"[^\"]*\"|'[^']*'")
 
 def class_completer(self, line):
     param = first_param.search(line.line)
     if param:
         return devices.systemTopology["server"][param.group(0)[1:-1],
                                                 "deviceClasses"]
-    return []
+    return list(devices.systemTopology["server"])
+first_param = re.compile("\"[^\"]*\"|'[^']*'")
 
 
 ip.set_hook("complete_command", device_completer,
             re_key=".*((get|connect)Device|execute(NoWait)?|"
-                   "set(No)?Wait|shutdown)")
-ip.set_hook("complete_command", server_completer,
-            re_key=".*instantiate")
+                   "set(No)?Wait|shutdown(NoWait)?)\(")
 ip.set_hook("complete_command", class_completer,
-            re_key=".*instantiate\([\"'].*[\"'],")
+            re_key=".*instantiate(NoWait)?\(")
 
 
 __all__ = ["getDevice", "waitUntil", "waitUntilNew", "setWait", "setNoWait",
-       "execute", "executeNoWait", "getDevices", "getClasses",
-       "getServers", "instantiate", "connectDevice", "shutdown"]
+           "execute", "executeNoWait", "getDevices", "getClasses",
+           "getServers", "instantiate", "connectDevice", "shutdown",
+           "shutdownNoWait", "instantiateNoWait"]
