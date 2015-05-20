f7272161e86f222f8ff8a6d2f99db42e9545200b
jszuba <janusz.szuba@xfel.eu>
2015-05-20 12:40:25 +0000

    modifying cppDep - now it is blank cpp project configured with includes and linking of karabo; this needs to be anyway modified by someone who creates a new dependency
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/branches/1.3@17435 a23ab9f9-1ea2-4934-960d-990259f9ea8c

--- run/etc/templates/dependency/cppDep/nbproject/Karabo-variables-Linux.mk
+++ run/etc/templates/dependency/cppDep/nbproject/Karabo-variables-Linux.mk
@@ -9,19 +9,19 @@ CND_DISTDIR=dist
 # Debug configuration
 CND_PLATFORM_Debug=GNU-Linux-x86
 CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux-x86
-CND_ARTIFACT_NAME_Debug=libminimal.so
-CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux-x86/libminimal.so
+CND_ARTIFACT_NAME_Debug=libcppDep.so
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux-x86/libcppDep.so
 CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux-x86/package
-CND_PACKAGE_NAME_Debug=libminimal.so.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux-x86/package/libminimal.so.tar
+CND_PACKAGE_NAME_Debug=libcppDep.so.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux-x86/package/libcppDep.so.tar
 # Release configuration
 CND_PLATFORM_Release=GNU-Linux-x86
 CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux-x86
-CND_ARTIFACT_NAME_Release=libminimal.so
-CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux-x86/libminimal.so
+CND_ARTIFACT_NAME_Release=libcppDep.so
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux-x86/libcppDep.so
 CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux-x86/package
-CND_PACKAGE_NAME_Release=libminimal.so.tar
-CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux-x86/package/libminimal.so.tar
+CND_PACKAGE_NAME_Release=libcppDep.so.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux-x86/package/libcppDep.so.tar
 #
 # include compiler specific variables
 #
