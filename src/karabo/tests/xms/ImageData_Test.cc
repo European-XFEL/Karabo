3cdaa957c4f7da9b63a38e8067a11b63fd1a065b
parenti <andrea.parenti@xfel.eu>
2015-05-21 14:32:55 +0000

    Added tests for ImageData
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/branches/1.3@17452 a23ab9f9-1ea2-4934-960d-990259f9ea8c

---
+++ src/karabo/tests/xms/ImageData_Test.cc
@@ -0,0 +1,89 @@
+/*
+ * File:   ImageData_Test.cc
+ * Author: parenti
+ *
+ * Created on May 21, 2015, 3:05:22 PM
+ */
+
+#include <boost/thread/pthread/thread_data.hpp>
+
+#include "ImageData_Test.hh"
+
+using namespace std;
+using namespace karabo::util;
+using namespace karabo::io;
+using namespace karabo::net;
+using namespace karabo::xms;
+
+CPPUNIT_TEST_SUITE_REGISTRATION(ImageData_Test);
+
+
+ImageData_Test::ImageData_Test() {
+}
+
+
+ImageData_Test::~ImageData_Test() {
+}
+
+
+void ImageData_Test::setUp() {
+}
+
+
+void ImageData_Test::tearDown() {
+}
+
+void ImageData_Test::testConstructor() {
+    
+    Dims dims(200, 100);
+    std::vector<int> someData(dims.size(), 2);
+    
+    {
+        ImageData image(&someData[0], someData.size(), true, dims);
+        
+        Dims imageDims(image.getDimensions());
+        Dims imageOffsets(image.getROIOffsets());
+        
+        CPPUNIT_ASSERT(image.getByteSize() == 20000 * sizeof (int));
+        CPPUNIT_ASSERT(image.getChannelSpace() == ChannelSpace::s_32_4);
+        CPPUNIT_ASSERT(image.getEncoding() == Encoding::GRAY);
+        
+        CPPUNIT_ASSERT(imageDims.size() == 20000);
+        CPPUNIT_ASSERT(imageDims.rank() == 2);
+        CPPUNIT_ASSERT(imageDims.x1() == 200);
+        CPPUNIT_ASSERT(imageDims.x2() == 100);
+        
+        CPPUNIT_ASSERT(imageOffsets.rank() == 2);
+        CPPUNIT_ASSERT(imageOffsets.x1() == 0);
+        CPPUNIT_ASSERT(imageOffsets.x2() == 0);
+        
+        CPPUNIT_ASSERT(image.getDataType() == "INT32");
+    }
+    
+    {
+        karabo::xip::CpuImage<unsigned char> cpuImage(200, 100, 5);
+        ImageData image(cpuImage);
+        
+        Dims imageDims(image.getDimensions());
+        Dims imageOffsets(image.getROIOffsets());
+        
+        CPPUNIT_ASSERT(image.getByteSize() == 200 * 100 * 5 * sizeof (unsigned char));
+        CPPUNIT_ASSERT(image.getChannelSpace() == ChannelSpace::u_8_1);
+        CPPUNIT_ASSERT(image.getEncoding() == Encoding::GRAY);
+        
+        CPPUNIT_ASSERT(imageDims.size() == 100000);
+        CPPUNIT_ASSERT(imageDims.rank() == 3);
+        CPPUNIT_ASSERT(imageDims.x1() == 200);
+        CPPUNIT_ASSERT(imageDims.x2() == 100);
+        CPPUNIT_ASSERT(imageDims.x3() == 5);
+        
+        CPPUNIT_ASSERT(imageOffsets.rank() == 3);
+        CPPUNIT_ASSERT(imageOffsets.x1() == 0);
+        CPPUNIT_ASSERT(imageOffsets.x2() == 0);
+        CPPUNIT_ASSERT(imageOffsets.x3() == 0);
+        
+        CPPUNIT_ASSERT(image.getDataType() == "UINT8");
+    }
+    
+    // TODO
+}
---
+++ src/karabo/tests/xms/ImageData_Test.hh
@@ -0,0 +1,35 @@
+/*
+ * File:   ImageData_Test.hh
+ * Author: parenti
+ *
+ * Created on May 21, 2015, 3:06:21 PM
+ */
+
+#ifndef IMAGEDATA_TEST_HH
+#define	IMAGEDATA_TEST_HH
+
+#include <karabo/xms.hpp>
+#include <cppunit/extensions/HelperMacros.h>
+
+class ImageData_Test : public CPPUNIT_NS::TestFixture {
+
+    CPPUNIT_TEST_SUITE(ImageData_Test);
+
+    CPPUNIT_TEST(testConstructor);
+   
+    CPPUNIT_TEST_SUITE_END();
+
+public:
+    ImageData_Test();
+    virtual ~ImageData_Test();
+    void setUp();
+    void tearDown();
+
+private:
+    
+    void testConstructor();
+    
+};
+
+#endif	/* IMAGEDATA_TEST_HH */
+
