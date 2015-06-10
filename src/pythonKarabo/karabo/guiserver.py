68de8f7e1458d3d2d44dc6d7d0f2a3f3b464f02d
Martin Teichmann <martin.teichmann@xfel.eu>
2015-06-10 15:30:17 +0200

    handle projects in python gui server

--- src/pythonKarabo/karabo/guiserver.py
+++ src/pythonKarabo/karabo/guiserver.py
@@ -129,6 +129,40 @@ class GuiServer(DeviceClientBase):
                           "from", t0, "to", t1, "maxNumData", maxNumData))
         self.histories[deviceId, property] = channel
 
+    @parallel
+    def handle_getAvailableProjects(self, channel):
+        projects = yield from self.call("Karabo_ProjectManager",
+                                        "slotGetAvailableProjects")
+        self.respond(channel, "availableProjects", availableProjects=projects)
+
+    @parallel
+    def handle_newProject(self, channel, author, name, data):
+        name, success, data = yield from self.call(
+            "Karabo_ProjectManager", "slotNewProject", author, name, data)
+        self.respond(channel, "projectNew", name=name, success=success,
+                     data=data)
+
+    @parallel
+    def handle_loadProject(self, channel, user, name):
+        name, metaData, data = yield from self.call(
+            "Karabo_ProjectManager", "slotLoadProject", user, name)
+        self.respond(channel, "projectLoaded", name=name, metaData=metaData,
+                     buffer=data)
+
+    @parallel
+    def handle_saveProject(self, channel, user, name, data):
+        name, success, data = yield from self.call(
+            "Karabo_ProjectManager", "slotSaveProject", user, name)
+        self.respond(channel, "projectSaved", name=name, success=success,
+                     data=data)
+
+    @parallel
+    def handle_closeProject(self, channel, user, name):
+        name, success, data = yield from self.call(
+            "Karabo_ProjectManager", "slotCloseProject", user, name)
+        self.respond(channel, "projectLoaded", name=name, success=success,
+                     data=data)
+
     @slot
     def slotPropertyHistory(self, deviceId, property, data):
         channel = self.histories.pop((deviceId, property), None)
