4cbdcfb67cdedfbc221017ef5e517258fe2775b2
maial <maial@a23ab9f9-1ea2-4934-960d-990259f9ea8c>
2015-06-15 13:56:29 +0000

    improve MacOSX installation
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/branches/1.3@17689 a23ab9f9-1ea2-4934-960d-990259f9ea8c

--- extern/resources/log4cpp/replace_namespace.sh
+++ extern/resources/log4cpp/replace_namespace.sh
@@ -19,7 +19,28 @@ if [ -d include/${OLD} ]; then
     mv include/${OLD} include/${NEW}
 fi
 
+
+if [ "$(uname -s)" == "Darwin" ]; then
+
 # Update makefiles
+find . -type f -name Makefile -exec sed -i "" -E "s|\(includedir\)/${OLD}|\(includedir\)/${NEW}|" {} \;
+sed -i "" -E "s|SUBDIRS = ${OLD}|SUBDIRS = ${NEW}|" include/Makefile
+
+# Update source files
+for ext in $EXTS; do
+    
+    # Update name of header file subdir
+    find . -type f -name "*.${ext}" -exec sed -i "" -E "s|include +<${OLD}|include <${NEW}|g; s|include +\"${OLD}|include \"${NEW}|g" {} \;
+
+    # Update namespace
+    find . -type f -name "*.${ext}" -exec sed -i "" -E "s|namespace +${OLD}|namespace ${NEW}|g; s|${OLD}::|${NEW}::|g" {} \;
+
+    # Rename macros
+    find . -type f -name "*.${ext}" -exec sed -i "" -E "s|${OLD_UP}|${NEW_UP}|g" {} \;
+done
+
+else
+
 find . -type f -name Makefile -exec sed -i -r "s|\(includedir\)/${OLD}|\(includedir\)/${NEW}|" {} \;
 sed -i -r "s|SUBDIRS = ${OLD}|SUBDIRS = ${NEW}|" include/Makefile
 
@@ -35,3 +56,5 @@ for ext in $EXTS; do
     # Rename macros
     find . -type f -name "*.${ext}" -exec sed -i -r "s|${OLD_UP}|${NEW_UP}|g" {} \;
 done
+
+fi
