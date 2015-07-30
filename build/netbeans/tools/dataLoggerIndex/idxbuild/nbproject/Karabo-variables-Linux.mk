919dfebb3c80f86c47925d0560b844b5fb8a22a9
esenov <sergey.esenov@xfel.eu>
2015-07-30 14:58:02 +0000

    Fix release configuration for 'idxbuild'
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/branches/1.3@17979 a23ab9f9-1ea2-4934-960d-990259f9ea8c

--- build/netbeans/tools/dataLoggerIndex/idxbuild/nbproject/Karabo-variables-Linux.mk
+++ build/netbeans/tools/dataLoggerIndex/idxbuild/nbproject/Karabo-variables-Linux.mk
@@ -16,9 +16,9 @@ CND_PACKAGE_NAME_Debug=idxbuild.tar
 CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux-x86/package/idxbuild.tar
 # Release configuration
 CND_PLATFORM_Release=GNU-Linux-x86
-CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux-x86
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux-x86/bin
 CND_ARTIFACT_NAME_Release=idxbuild
-CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux-x86/idxbuild
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux-x86/bin/idxbuild
 CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux-x86/package
 CND_PACKAGE_NAME_Release=idxbuild.tar
 CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux-x86/package/idxbuild.tar
