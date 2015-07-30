3a73d6303f4ad373a973299d8af46223f8522fb1
esenov <esenov@a23ab9f9-1ea2-4934-960d-990259f9ea8c>
2015-07-30 14:58:33 +0000

    Fix release configuration for 'idxview'
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/branches/1.3@17980 a23ab9f9-1ea2-4934-960d-990259f9ea8c

--- build/netbeans/tools/dataLoggerIndex/idxview/nbproject/Karabo-variables-Linux.mk
+++ build/netbeans/tools/dataLoggerIndex/idxview/nbproject/Karabo-variables-Linux.mk
@@ -16,9 +16,9 @@ CND_PACKAGE_NAME_Debug=idxview.tar
 CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux-x86/package/idxview.tar
 # Release configuration
 CND_PLATFORM_Release=GNU-Linux-x86
-CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux-x86
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux-x86/bin
 CND_ARTIFACT_NAME_Release=idxview
-CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux-x86/idxview
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux-x86/bin/idxview
 CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux-x86/package
 CND_PACKAGE_NAME_Release=idxview.tar
 CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux-x86/package/idxview.tar
