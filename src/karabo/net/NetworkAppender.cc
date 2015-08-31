509ee84444125dfcb08ef33d8c241a93ce080563
Gero Flucke <gero.flucke@xfel.eu>
2015-08-31 16:19:28 +0200

    NetworkAppender now sends a Hash and not a string.
    This avoids the need of delimiters whose appearence in the message should be
    (but was not) checked and treated.
    The Hash contains a vector<Hash>, i.e. a Hash for each message. Its keys are
    "timestamp", "type", "category" and "message".
    Also the format for the underlying krb_log4cpp logging classes is now
    hardcoded here (and not in the device server).

--- src/karabo/net/NetworkAppender.cc
+++ src/karabo/net/NetworkAppender.cc
@@ -5,8 +5,11 @@
  * Created on September 21, 2011, 4:39 PM
  */
 
+#include <iostream>
+
 #include <krb_log4cpp/LoggingEvent.hh>
 #include <karabo/util/Hash.hh>
+
 #include "NetworkAppender.hh"
 
 using namespace krb_log4cpp;
@@ -17,7 +20,14 @@ namespace karabo {
 
 
         NetworkAppender::NetworkAppender(const std::string& name, const karabo::net::BrokerChannel::Pointer& channel) :
-        LayoutAppender(name), m_channel(channel), m_ok(true) {
+        LayoutAppender(name), m_channel(channel), m_ok(true)
+        {
+            // Time format should match "yyyy-MM-dd hh:mm:ss" as GUI expects in
+            // logwidget.py:
+            m_timeLayout.setConversionPattern("%d{%F %H:%M:%S}");
+            m_priorityLayout.setConversionPattern("%p");
+            m_categoryLayout.setConversionPattern("%c");
+            m_messageLayout.setConversionPattern("%m");
 
             // Start thread
             m_thread = boost::thread(boost::bind(&karabo::net::NetworkAppender::checkLogCache, this));
@@ -39,7 +49,16 @@ namespace karabo {
 
         void NetworkAppender::_append(const LoggingEvent& event) {
             boost::mutex::scoped_lock lock(m_mutex);
-            m_logCache += _getLayout().format(event) + "#";
+            // Add message as Hash to cache - since emplace_back is C++11,
+            // we first push_back an empty one and fill it in-place instead
+            // of creating one and push_back (i.e. copy) it...
+            m_logCache.push_back(util::Hash());
+            util::Hash& message = m_logCache.back();
+            // The keys here are expected by the GUI in logwidget.py:
+            message.set("timestamp", m_timeLayout.format(event));
+            message.set("type", m_priorityLayout.format(event));
+            message.set("category", m_categoryLayout.format(event));
+            message.set("message", m_messageLayout.format(event));
         }
 
 
@@ -51,7 +70,11 @@ namespace karabo {
                 }
 
             } catch (const karabo::util::Exception& e) {
-                std::cout << e;
+                // Would be better to use the macro to the message into the
+                // log file - would need KARABO_CLASSINFO in .hh ?
+                // GF FIXME
+                // KARABO_LOG_FRAMEWORK_ERROR << "Writing failed: " << e;
+                std::cerr << "NetworkAppender: Writing failed: " << e << std::endl;
             }
         }
 
@@ -61,8 +84,9 @@ namespace karabo {
             boost::mutex::scoped_lock lock(m_mutex);
             if (m_logCache.empty()) return;
 
-            Hash header("target", "log");
-            m_channel->write(header, m_logCache);
+            util::Hash header("target", "log");
+            util::Hash data("messages", m_logCache);
+            m_channel->write(header, data);
             m_logCache.clear();
         }
 
