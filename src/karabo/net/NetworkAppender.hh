fc0d302edb1336d704eaa418ce7913a0526caefa
Gero Flucke <gero.flucke@xfel.eu>
2015-08-31 16:19:28 +0200

    NetworkAppender now sends a Hash and not a string.
    This avoids the need of delimiters whose appearence in the message should be
    (but was not) checked and treated.
    The Hash contains a vector<Hash>, i.e. a Hash for each message. Its keys are
    "timestamp", "type", "category" and "message".
    Also the format for the underlying krb_log4cpp logging classes is now
    hardcoded here (and not in the device server).

--- src/karabo/net/NetworkAppender.hh
+++ src/karabo/net/NetworkAppender.hh
@@ -3,13 +3,24 @@
  * Author: irinak
  *
  * Created on September 21, 2011, 4:39 PM
+ *
+ * This class is used to propagate log messages through the network to the GUI.
+ *
+ * It sends via the broker with a header (Hash("target", "log")) and a body.
+ * The latter is a hash with a single key ("messages") The corresponding value
+ * is a std::vector<Hash> containing one Hash for each message. Its keys are
+ * "timestamp", "type", "category" and "message".
  */
 
 #ifndef KARABO_LOGCONFIG_NETWORKAPPENDER_HH
 #define	KARABO_LOGCONFIG_NETWORKAPPENDER_HH
 
+#include <vector>
+
 #include <karabo/net/BrokerChannel.hh>
+
 #include <krb_log4cpp/LayoutAppender.hh>
+#include <krb_log4cpp/PatternLayout.hh>
 #include <krb_log4cpp/Portability.hh>
 
 namespace karabo {
@@ -43,7 +54,13 @@ namespace karabo {
             boost::thread m_thread;
             boost::mutex m_mutex;
 
-            std::string m_logCache;
+            /// layouts for each component
+            krb_log4cpp::PatternLayout m_timeLayout;
+            krb_log4cpp::PatternLayout m_priorityLayout;
+            krb_log4cpp::PatternLayout m_categoryLayout;
+            krb_log4cpp::PatternLayout m_messageLayout;
+            /// cash for messages
+            std::vector<util::Hash>  m_logCache;
             bool m_ok;
         };
 
