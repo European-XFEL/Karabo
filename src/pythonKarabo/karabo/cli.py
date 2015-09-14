099cba715b2a275580b22908dbea0a9fa048e870
Martin Teichmann <martin.teichmann@xfel.eu>
2015-09-14 10:56:44 +0200

    make cli.py importable
    cli.py is actually not a module, but a script. But like with
    every script, it should still be importable, with an if statement
    that is only run if the module is run as a script.
    
    The if statement is non-standard, as the script is to be run from
    within ipython, not on the command line.

--- src/pythonKarabo/karabo/cli.py
+++ src/pythonKarabo/karabo/cli.py
@@ -2,7 +2,10 @@
 # it has side-effects while importing (like showing a banner)
 # so don't use it for anything else
 
-import karabo
+# when expanding this script, assure that afterwards it is still
+# easily importable, which is important for the tests. No side effects
+# should happend if this script is just imported as a module, so
+# all side effects should be added to the if statement below.
 
 from asyncio import set_event_loop
 import os
@@ -13,20 +16,13 @@ import sys
 
 import IPython
 
+import karabo
 from karabo.device_client import (
     getDevice, waitUntil, waitUntilNew, setWait, setNoWait, execute,
     executeNoWait, DeviceClientBase, getDevices, getClasses, getServers,
     instantiate, connectDevice, shutdown, shutdownNoWait, instantiateNoWait)
 from karabo.eventloop import NoEventLoop
 from karabo.macro import Macro
-
-
-with open(osp.join(osp.dirname(sys.base_prefix), "VERSION"), "r") as fin:
-    version = fin.read()
-
-print("""IKarabo Version {}
-Type karabo? for help
-""".format(version))
 
 
 class DeviceClient(Macro, DeviceClientBase):
@@ -44,15 +40,7 @@ class DeviceClient(Macro, DeviceClientBase):
                 raise AttributeError('Unknown device "{}"'.format(name))
 
 
-devices = DeviceClient(_deviceId_="ikarabo-{}-{}".format(socket.gethostname(),
-                                                         os.getpid()))
-set_event_loop(NoEventLoop(devices))
-
-ip = IPython.get_ipython()
-
-
 def device_completer(self, line):
-    #print("device")
     return list(devices.systemTopology["device"])
 
 
@@ -62,17 +50,38 @@ def class_completer(self, line):
         return devices.systemTopology["server"][param.group(0)[1:-1],
                                                 "deviceClasses"]
     return list(devices.systemTopology["server"])
+
+
 first_param = re.compile("\"[^\"]*\"|'[^']*'")
 
 
-ip.set_hook("complete_command", device_completer,
-            re_key=".*((get|connect)Device|execute(NoWait)?|"
-                   "set(No)?Wait|shutdown(NoWait)?)\(")
-ip.set_hook("complete_command", class_completer,
-            re_key=".*instantiate(NoWait)?\(")
+# here starts the main part of this script. It does not use the
+# standard if __name__ == "__main__" idiom, instead it checks whether
+# this script is started from within ipython, in which case it installs
+# everything needed into the ipython kernel.
 
 
-__all__ = ["getDevice", "waitUntil", "waitUntilNew", "setWait", "setNoWait",
-           "execute", "executeNoWait", "getDevices", "getClasses",
-           "getServers", "instantiate", "connectDevice", "shutdown",
-           "shutdownNoWait", "instantiateNoWait", "karabo"]
+ip = IPython.get_ipython()
+if ip is not None:
+    with open(osp.join(osp.dirname(sys.base_prefix), "VERSION"), "r") as fin:
+        version = fin.read()
+
+    print("""IKarabo Version {}
+    Type karabo? for help
+    """.format(version))
+
+    devices = DeviceClient(_deviceId_="ikarabo-{}-{}".format(
+        socket.gethostname(), os.getpid()))
+    set_event_loop(NoEventLoop(devices))
+
+    ip.set_hook("complete_command", device_completer,
+                re_key=".*((get|connect)Device|execute(NoWait)?|"
+                       "set(No)?Wait|shutdown(NoWait)?)\(")
+    ip.set_hook("complete_command", class_completer,
+                re_key=".*instantiate(NoWait)?\(")
+
+
+    __all__ = ["getDevice", "waitUntil", "waitUntilNew", "setWait", "setNoWait",
+               "execute", "executeNoWait", "getDevices", "getClasses",
+               "getServers", "instantiate", "connectDevice", "shutdown",
+               "shutdownNoWait", "instantiateNoWait", "karabo"]
