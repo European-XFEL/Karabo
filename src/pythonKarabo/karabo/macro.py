21bfbc080ffdda764022ef6db9afeeb9e6ef3f31
Martin Teichmann <martin.teichmann@xfel.eu>
2015-09-23 10:33:48 +0200

    properly log errors in monitors

--- src/pythonKarabo/karabo/macro.py
+++ src/pythonKarabo/karabo/macro.py
@@ -1,5 +1,5 @@
-from asyncio import (async, coroutine, Future, get_event_loop, set_event_loop,
-                     Task, TimeoutError)
+from asyncio import (async, CancelledError, coroutine, get_event_loop,
+                     set_event_loop)
 import atexit
 from functools import wraps
 import sys
@@ -172,7 +172,14 @@ class Macro(Device):
             while True:
                 yield from waitUntilNew(d)
                 for m in self._monitors:
-                    setattr(self, m.key, m.monitor(self))
+                    try:
+                        setattr(self, m.key, m.monitor(self))
+                    except CancelledError:
+                        raise
+                    except:
+                        self.logger.exception(
+                            'exception in monitor "{}" of device "{}"'.
+                            format(m.key, self.deviceId))
 
     def printToConsole(self, data):
         self.print = data
