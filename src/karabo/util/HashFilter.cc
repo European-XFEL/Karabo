c2fc5ec5186d8ba99836225e65cee1a14ac622af
esenov <sergey.esenov@xfel.eu>
2015-09-24 10:31:04 +0000

    Add to HashFilter class 'byAccessMode' method
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/branches/1.3@18396 a23ab9f9-1ea2-4934-960d-990259f9ea8c

--- src/karabo/util/HashFilter.cc
+++ src/karabo/util/HashFilter.cc
@@ -58,16 +58,16 @@ namespace karabo {
                 // For vector<Hash> the following policy is implemented
                 // The size of the vector is preserved unless all Hashes in the vector are empty
                 const vector<Hash>& inputVector = inputNode.getValue<vector<Hash> >();
-                
+
                 // At the moment we copy the vector<Hash> to the result in all cases except 
                 // if every Hash is empty after running a filter.
                 // One can optimize it
-                
+
                 //vector<Hash>& outputVector = result.bindReference<vector<Hash> > (path);
                 //outputVector.resize(inputVector.size());                
                 //vector<Hash>& outputVector(inputVector.size());
                 vector<Hash> outputVector(inputVector.size());
-                
+
                 for (size_t i = 0; i < inputVector.size(); ++i) {
                     //clog << "index i=" << i << endl;
                     const Hash& input = inputVector[i];
@@ -79,12 +79,12 @@ namespace karabo {
                 }
                 for (size_t i = 0; i < outputVector.size(); ++i) {
                     const Hash& output = outputVector[i];
-                    if(output.size() > 0 ){
-                        result.set(path,outputVector);
+                    if (output.size() > 0) {
+                        result.set(path, outputVector);
                         return;
                     }
                 }
-                
+
 
             } else {
                 processNode(master, inputNode, result, path, tags);
@@ -108,6 +108,93 @@ namespace karabo {
                         //clog << "tag " << t[i] << " not requested for: " << inputNode.getKey() << endl;
                     }
                 }
+            }
+            return false;
+
+
+        }
+
+
+        void HashFilter::byAccessMode(const Schema& schema, const Hash& input, Hash& result, const AccessType& value) {
+
+            const Hash& master = schema.getParameterHash();
+            for (Hash::const_iterator it = input.begin(); it != input.end(); ++it) {
+                r_byAccessMode(master, *it, result, it->getKey(), value);
+            }
+        }
+
+
+        void HashFilter::r_byAccessMode(const Hash& master, const Hash::Node& inputNode, Hash& result, const std::string& path, const AccessType& value) {
+
+            if (!master.has(path)) {
+                //clog << "Path: " << path << " does not exist in schema" << endl;
+                return;
+            }
+
+            if (inputNode.is<Hash>()) {
+
+                // if the tag was found in the HASH copy complete Hash and return
+                // otherwise process the Hash further
+                if (processNodeForAccessMode(master, inputNode, result, path, value) == true) return;
+
+                const Hash& input = inputNode.getValue<Hash>();
+                for (Hash::const_iterator it = input.begin(); it != input.end(); ++it) {
+                    string newPath = path + "." + it->getKey();
+                    r_byAccessMode(master, *it, result, newPath, value);
+                }
+            } else if (inputNode.is<vector<Hash> >()) {
+
+                // if the tag was found in the vector<HASH> (defined for LIST_ELEMENT) copy complete vector<Hash> and return
+                // otherwise process the Hash further
+                if (processNodeForAccessMode(master, inputNode, result, path, value) == true) {
+                    //clog << "Copy entire vector<Hash> " << path << endl;
+                    return;
+                }
+                //clog << "Further processing vector<Hash>  " << path << endl;
+
+                // For vector<Hash> the following policy is implemented
+                // The size of the vector is preserved unless all Hashes in the vector are empty
+                const vector<Hash>& inputVector = inputNode.getValue<vector<Hash> >();
+
+                // At the moment we copy the vector<Hash> to the result in all cases except 
+                // if every Hash is empty after running a filter.
+                // One can optimize it
+
+                //vector<Hash>& outputVector = result.bindReference<vector<Hash> > (path);
+                //outputVector.resize(inputVector.size());                
+                //vector<Hash>& outputVector(inputVector.size());
+                vector<Hash> outputVector(inputVector.size());
+
+                for (size_t i = 0; i < inputVector.size(); ++i) {
+                    //clog << "index i=" << i << endl;
+                    const Hash& input = inputVector[i];
+                    Hash& output = outputVector[i];
+                    for (Hash::const_iterator it = input.begin(); it != input.end(); ++it) {
+                        //clog << "Hash in vector path=" << path << endl;
+                        r_byAccessMode(master.get<Hash>(path), *it, output, it->getKey(), value);
+                    }
+                }
+                for (size_t i = 0; i < outputVector.size(); ++i) {
+                    const Hash& output = outputVector[i];
+                    if (output.size() > 0) {
+                        result.set(path, outputVector);
+                        return;
+                    }
+                }
+
+
+            } else {
+                processNodeForAccessMode(master, inputNode, result, path, value);
+            }
+        }
+
+
+        bool HashFilter::processNodeForAccessMode(const Hash& master, const Hash::Node& inputNode, Hash& result,
+                                                  const std::string& path, const AccessType& value) {
+
+            if (master.hasAttribute(path, KARABO_SCHEMA_ACCESS_MODE)) {
+                int t = master.getAttribute<int>(path, KARABO_SCHEMA_ACCESS_MODE);
+                return (t & int(value)) == int(value);
             }
             return false;
 
