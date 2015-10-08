cb4e37499973cdf946c82c07501bb6ba3a293730
John Wiggins <john.wiggins@xfel.eu>
2015-10-08 14:46:14 +0200

    Create a template for Python dependencies.
    The dependencies will be built and installed as wheels. This required adding
    pip and wheel to the external dependencies of Karabo.
    
    Additionally, the netbeans project was removed from the pythonDevice template.

---
+++ run/etc/templates/dependency/pythonDep/src/setup.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='__CLASS_NAME__',
+      version='',
+      author='',
+      author_email='__EMAIL__',
+      description='',
+      long_description='',
+      url='',
+      packages=find_packages(),
+      package_data={},
+      requires=[],
+      )
+
