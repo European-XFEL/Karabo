a3b114cee395e9d570368b50612300a78d3fc379
John Wiggins <john.wiggins@xfel.eu>
2015-10-08 14:46:14 +0200

    Create a template for Python dependencies.
    The dependencies will be built and installed as wheels. This required adding
    pip and wheel to the external dependencies of Karabo.
    
    Additionally, the netbeans project was removed from the pythonDevice template.

---
+++ run/etc/templates/dependency/pythonDep/.install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ -z $KARABO ]; then
+    if [ -e $HOME/.karabo/karaboFramework ]; then
+        KARABO=$(cat $HOME/.karabo/karaboFramework)
+    else
+      echo "ERROR Could not find karaboFramework. Make sure you have installed the karaboFramework."
+      exit 1
+    fi
+fi
+
+$KARABO/bin/.bundle-pythondependency.sh $1 $2 $3
