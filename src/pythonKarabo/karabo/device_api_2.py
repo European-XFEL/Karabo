5b1227be4f86d67df9bace39488a80e5823ac948
John Wiggins <john.wiggins@xfel.eu>
2015-11-19 10:47:12 +0100

    Add device API modules for APIs 1 & 2.
    These modules help us and our users maintain a consistent API for device
    development. The included unit tests are designed to break when certain
    changes are made so that we don't accidentally change the API.

---
+++ src/pythonKarabo/karabo/device_api_2.py
@@ -0,0 +1,31 @@
+""" This module provides all the API 2 names which a Device might need.
+"""
+
+from .device_client import (
+    DeviceClientBase, Proxy, ProxySlot, ProxyNode, AutoDisconnectProxy,
+    SubProxy, OneShotQueue, getHistory, waitUntilNew, waitUntil, setWait,
+    setNoWait, getDevice, executeNoWait, updateDevice, sleep
+)
+from .enums import AccessLevel, AccessMode, Assignment, MetricPrefix, Unit
+from .eventloop import Broker, Client, EventLoop, NoEventLoop
+from .exceptions import KaraboError
+from .hash import (
+    Attribute, BinaryParser, BinaryWriter, Bool, Byte, Char, ComplexDouble,
+    ComplexFloat, Descriptor, Double, Element, Enumable, Float, Hash,
+    HashElement, HashMergePolicy, HashType, Int16, Int32, Int64, Int8, Integer,
+    ListElement, Number, NumpyVector, Schema, SchemaHashType, Simple,
+    SimpleElement, Slot, Special, String, StringList, TableElement, Type,
+    UInt16, UInt32, UInt64, UInt8, Vector, VectorBool, VectorChar,
+    VectorComplexDouble, VectorComplexFloat, VectorDouble, VectorFloat,
+    VectorHash, VectorInt16, VectorInt32, VectorInt64, VectorInt8,
+    VectorString, VectorUInt16, VectorUInt32, VectorUInt64, VectorUInt8,
+    Writer, XMLParser, XMLWriter, saveToFile
+)
+from .macro import Macro, Monitor
+from .python_device import (
+    Device, DeviceClient, PythonDevice, launchPythonDevice
+)
+from .registry import MetaRegistry, Registry
+from .schema import Configurable, Node, ChoiceOfNodes, ListOfNodes, Validator
+from .signalslot import Signal, SignalSlotable, slot, coslot, replySlot
+from .timestamp import Timestamp
