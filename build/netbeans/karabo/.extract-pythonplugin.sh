016c9644d43da460059253c7c5dc5b380b38588e
John Wiggins <john.wiggins@xfel.eu>
2015-11-30 18:18:49 +0100

    Do away with the tarfile wrapper for Python device bundles.
    The wheel file is already a binary, so add it to the end of the self-extracting
    shell script instead.

--- build/netbeans/karabo/.extract-pythonplugin.sh
+++ build/netbeans/karabo/.extract-pythonplugin.sh
@@ -97,27 +97,27 @@ then
 fi
 
 echo -n " Extracting files, please wait..."
-# searches for the line number where finish the script and start the tar.gz
-SKIP=`awk '/^__TARFILE_FOLLOWS__/ { print NR + 1; exit 0; }' $0`
-tail -n +$SKIP $0 | (cd  $installDir && tar xzf -) || echo_exit "Problem unpacking the file $0"
+# searches for the line number where finish the script and start the .whl
+SKIP=`awk '/^__WHEELFILE_FOLLOWS__/ { print NR + 1; exit 0; }' $0`
+tail -n +$SKIP $0 | (cd  $installDir && cat - > $WHEELNAME) || echo_exit "Problem unpacking the file $0"
 echo  " unpacking finished successfully"
-# Any script here will happen after the tar file extract.
+# Any script here will happen after the wheel file is extracted.
 echo
-echo -n "Running post install script..."
+echo -n "Running wheel installation..."
 if [ ! -z $KARABO ]; then
     if [ $(uname -s) == "Darwin" ]; then
         PIP=/opt/local/bin/pip
     else
         PIP=$KARABO/extern/bin/pip
     fi
-    destDir=$installDir/$PLUGINNAME-$VERSION-$KARABOVERSION
-    $PIP install -U --no-index --target $installDir $destDir/*.whl
+    destPath=$installDir/$WHEELNAME
+    $PIP install -U --no-index --target $installDir $destPath
 fi
 echo " done."
 echo
-echo " Package was successfully installed to: $installDir/$PLUGINNAME-$VERSION-$KARABOVERSION"
+echo " Package was successfully installed to: $installDir"
 echo
 echo
 exit 0
 # NOTE: Don't place any newline characters after the last line below.
-__TARFILE_FOLLOWS__
+__WHEELFILE_FOLLOWS__
