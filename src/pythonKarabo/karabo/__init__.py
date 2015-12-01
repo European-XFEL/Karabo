2799e02f234ac39471224564d5618c246bd6a92c
John Wiggins <john.wiggins@xfel.eu>
2015-12-01 15:23:55 +0100

    Generate the package version for pythonKarabo when running setup.py

--- src/pythonKarabo/karabo/__init__.py
+++ src/pythonKarabo/karabo/__init__.py
@@ -1,2 +1,8 @@
+try:
+    from karabo._version import full_version as __version__
+except ImportError:
+    # Don't cause a failure when running setup.py
+    __version__ = ''
+
 __author__="Sergey Esenov <serguei.essenov at xfel.eu>"
 __date__ ="$Jul 11, 2013 5:20:23 PM$"
