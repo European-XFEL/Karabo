6b3e61d9a7672ccfae65e4d646be8e8e1a173cb3
John Wiggins <john.wiggins@xfel.eu>
2015-12-02 15:34:48 +0100

    Add setup.py version bits to the convert-karabo-device-project script

--- src/pythonKarabo/karabo/interactive/convert_device_project.py
+++ src/pythonKarabo/karabo/interactive/convert_device_project.py
@@ -26,10 +26,22 @@ ENTRY_POINT_TEMPLATE = "'{cname} = {package_name}.{fname}:{cname}',"
 SETUP_TEMPLATE = """\
 #!/usr/bin/env python
 
+from os.path import dirname
+
 from setuptools import setup, find_packages
 
+
+def _get_version_string():
+    try:
+        from karabo.packaging.versioning import get_package_version
+    except ImportError:
+        print("WARNING: Karabo framework not found! Version will be blank!")
+        return ''
+
+    return get_package_version(dirname(__file__))
+
 setup(name='{package_name}',
-      version='',
+      version=_get_version_string(),
       author='',
       author_email='',
       description='',
