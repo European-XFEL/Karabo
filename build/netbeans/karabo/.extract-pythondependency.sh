81277f456b98e8d00263b696d87323ece5aa3cb9
John Wiggins <john.wiggins@xfel.eu>
2015-12-10 10:29:23 +0100

    Package Python dependencies the same way as Python devices.

---
+++ build/netbeans/karabo/.extract-pythondependency.sh
@@ -0,0 +1,75 @@
+usage()
+{
+  cat <<EOF
+Usage: $0 [options]
+Options:
+  --help       print this message
+  --prefix=dir install directory
+EOF
+  exit 1
+}
+
+echo_exit()
+{
+  echo $1
+  exit 1
+}
+
+if [ -z $KARABO ]; then
+    if [ -e $HOME/.karabo/karaboFramework ]; then
+        KARABO=$(cat $HOME/.karabo/karaboFramework)
+    else
+        echo "ERROR Could not find karaboFramework. Make sure you have installed the karaboFramework"
+        exit 1
+    fi
+fi
+
+install_prefix_dir=""
+for a in $@; do
+  if echo $a | grep "^--prefix=" > /dev/null 2> /dev/null; then
+     install_prefix_dir=`echo $a | sed "s/^--prefix=//"`
+     install_prefix_dir=${install_prefix_dir/#\~/$HOME}
+     if [ ! -d ${install_prefix_dir} ]; then
+       mkdir -p  ${install_prefix_dir} ||  echo_exit "Cannot create directory ${install_prefix_dir}"
+     fi
+     install_prefix_dir=`cd "${install_prefix_dir}"; pwd`
+  fi
+  if echo $a | grep "^--help" > /dev/null 2> /dev/null; then
+    usage
+  fi
+done
+if [ "x${install_prefix_dir}x" = "xx" ]; then
+    usage
+fi
+
+installDir=$install_prefix_dir
+if [ ! -d $installDir ]; then
+    mkdir -p $installDir || echo_exit "Cannot create directory $installDir"
+fi
+
+echo "This is a self-extracting archive."
+echo -n " Extracting files, please wait..."
+# searches for the line number where finish the script and start the .whl
+SKIP=`awk '/^__WHEELFILE_FOLLOWS__/ { print NR + 1; exit 0; }' $0`
+tail -n +$SKIP $0 | (cd  $installDir && cat - > $WHEELNAME) || echo_exit "Problem unpacking the file $0"
+echo  " unpacking finished successfully"
+# Any script here will happen after the wheel file extract.
+echo
+echo -n "Running wheel installation..."
+if [ ! -z $KARABO ]; then
+    if [ $(uname -s) == "Darwin" ]; then
+        PIP=/opt/local/bin/pip
+    else
+        PIP=$KARABO/extern/bin/pip
+    fi
+    destPath=$installDir/$WHEELNAME
+    $PIP install -U --no-index --target $installDir $destPath
+fi
+echo " done."
+echo
+echo " Package was successfully installed to: $installDir"
+echo
+echo
+exit 0
+# NOTE: Don't place any newline characters after the last line below.
+__WHEELFILE_FOLLOWS__
