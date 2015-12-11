a3abe74a891a67d7931ff60c2e71d7aa2f11f457
John Wiggins <john.wiggins@xfel.eu>
2015-12-11 15:27:27 +0100

    Move the project keys into a constants module

--- src/pythonKarabo/karabo/api2/_project/model.py
+++ src/pythonKarabo/karabo/api2/_project/model.py
@@ -19,25 +19,6 @@ class ProjectAccess(Enum):
     CLOUD_READONLY = 2  # readonly access
 
 
-class ProjectKeys(object):
-    DEVICES_LABEL = "Devices"
-    SCENES_LABEL = "Scenes"
-    CONFIGURATIONS_LABEL = "Configurations"
-    MACROS_LABEL = "Macros"
-    MONITORS_LABEL = "Monitors"
-    RESOURCES_LABEL = "Resources"
-
-    PROJECT_KEY = "project"
-    DEVICES_KEY = "devices"
-    SCENES_KEY = "scenes"
-    CONFIGURATIONS_KEY = "configurations"
-    MACROS_KEY = "macros"
-    MONITORS_KEY = "monitors"
-    RESOURCES_KEY = "resources"
-
-    PROJECT_SUFFIX = "krb"
-
-
 class ProjectData(object):
     def __init__(self, filename, config=None):
         super(ProjectData, self).__init__()
