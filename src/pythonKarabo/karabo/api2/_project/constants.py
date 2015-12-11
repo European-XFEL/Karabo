d2514a998d091751cb72ae7c52f01f3121839ab6
John Wiggins <john.wiggins@xfel.eu>
2015-12-11 15:27:27 +0100

    Move the project keys into a constants module

---
+++ src/pythonKarabo/karabo/api2/_project/constants.py
@@ -0,0 +1,19 @@
+""" This is a set of constants for use by the project code.
+"""
+
+DEVICES_LABEL = "Devices"
+SCENES_LABEL = "Scenes"
+CONFIGURATIONS_LABEL = "Configurations"
+MACROS_LABEL = "Macros"
+MONITORS_LABEL = "Monitors"
+RESOURCES_LABEL = "Resources"
+
+PROJECT_KEY = "project"
+DEVICES_KEY = "devices"
+SCENES_KEY = "scenes"
+CONFIGURATIONS_KEY = "configurations"
+MACROS_KEY = "macros"
+MONITORS_KEY = "monitors"
+RESOURCES_KEY = "resources"
+
+PROJECT_SUFFIX = "krb"
