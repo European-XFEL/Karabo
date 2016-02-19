c16ee150f7f8b87f6aa08c6a777f6b7a65863815
John Wiggins <john.wiggins@xfel.eu>
2016-02-19 11:34:36 +0100

    Clean up the Python dependency bundle/extract scripts.

--- build/netbeans/karabo/.extract-pythondependency.sh
+++ build/netbeans/karabo/.extract-pythondependency.sh
@@ -1,75 +1,53 @@
-usage()
-{
-  cat <<EOF
-Usage: $0 [options]
-Options:
-  --help       print this message
-  --prefix=dir install directory
-EOF
-  exit 1
-}
 
 echo_exit()
 {
-  echo $1
-  exit 1
+    echo $1
+    exit 1
 }
 
 if [ -z $KARABO ]; then
-    if [ -e $HOME/.karabo/karaboFramework ]; then
-        KARABO=$(cat $HOME/.karabo/karaboFramework)
-    else
-        echo "ERROR Could not find karaboFramework. Make sure you have installed the karaboFramework"
-        exit 1
+    echo_exit "\$KARABO is not defined. Make sure you have sourced the activate script for the Karabo Framework which you would like to target."
+else
+    LOCALKARABOVERSION=$(cat $KARABO/VERSION)
+    if [ "$LOCALKARABOVERSION" != "$KARABOVERSION" ]; then
+        echo "Plugin was compiled with different karaboFramework version"
+        echo "than installed one:"
+        echo "Compiled: $KARABOVERSION vs. Installed: $LOCALKARABOVERSION"
+        echo " "
+
+        read -e -p " Continue installation? [Y/n] " RESPONSE
+        if [ "$RESPONSE" == "N" || "$RESPONSE" == "n" ]; then
+            exit 1
+        fi
     fi
-fi
-
-install_prefix_dir=""
-for a in $@; do
-  if echo $a | grep "^--prefix=" > /dev/null 2> /dev/null; then
-     install_prefix_dir=`echo $a | sed "s/^--prefix=//"`
-     install_prefix_dir=${install_prefix_dir/#\~/$HOME}
-     if [ ! -d ${install_prefix_dir} ]; then
-       mkdir -p  ${install_prefix_dir} ||  echo_exit "Cannot create directory ${install_prefix_dir}"
-     fi
-     install_prefix_dir=`cd "${install_prefix_dir}"; pwd`
-  fi
-  if echo $a | grep "^--help" > /dev/null 2> /dev/null; then
-    usage
-  fi
-done
-if [ "x${install_prefix_dir}x" = "xx" ]; then
-    usage
-fi
-
-installDir=$install_prefix_dir
-if [ ! -d $installDir ]; then
-    mkdir -p $installDir || echo_exit "Cannot create directory $installDir"
-fi
 
 echo "This is a self-extracting archive."
 echo -n " Extracting files, please wait..."
 # searches for the line number where finish the script and start the .whl
 SKIP=`awk '/^__WHEELFILE_FOLLOWS__/ { print NR + 1; exit 0; }' $0`
-tail -n +$SKIP $0 | (cd  $installDir && cat - > $WHEELNAME) || echo_exit "Problem unpacking the file $0"
+tail -n +$SKIP $0 | cat - > $WHEELNAME || echo_exit "Problem unpacking the file $0"
 echo  " unpacking finished successfully"
-# Any script here will happen after the wheel file extract.
+
 echo
 echo -n "Running wheel installation..."
-if [ ! -z $KARABO ]; then
-    if [ $(uname -s) == "Darwin" ]; then
-        PIP=/opt/local/bin/pip
-    else
-        PIP=$KARABO/extern/bin/pip
-    fi
-    destPath=$installDir/$WHEELNAME
-    $PIP install -U --no-index --target $installDir $destPath
+
+if [ $(uname -s) == "Darwin" ]; then
+    PIP=/opt/local/bin/pip
+else
+    PIP=$KARABO/extern/bin/pip
 fi
+
+$PIP install -U --no-index $WHEELNAME
+
 echo " done."
 echo
-echo " Package was successfully installed to: $installDir"
+echo " Package was successfully installed to: $KARABO"
 echo
 echo
+
+# Clean up
+rm $WHEELNAME
+
 exit 0
 # NOTE: Don't place any newline characters after the last line below.
 __WHEELFILE_FOLLOWS__
