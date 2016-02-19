1a4e2f13093d13361e235ed206d3691111413568
John Wiggins <john.wiggins@xfel.eu>
2016-02-19 17:27:38 +0100

    Add a console_script entrypoint to the Python device template.
    This makes it possible to launch an independent device server which only
    has one device type which it can instantiate.

---
+++ run/etc/templates/pythonDevice/minimal/src/__PACKAGE_NAME__/server/__CLASS_NAME__.py
@@ -0,0 +1,9 @@
+__author__ = "__EMAIL__"
+__date__ = "__DATE__"
+__copyright__ = ("Copyright (c) 2010-2015 European XFEL GmbH Hamburg. "
+                 "All rights reserved.")
+
+from karabo.api_1 import runSingleDeviceServer
+
+def main():
+    runSingleDeviceServer('__CLASS_NAME__')
