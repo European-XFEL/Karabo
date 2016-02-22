d4a31306f175963202cc80c79db04764d71ff9ce
John Wiggins <john.wiggins@xfel.eu>
2016-02-22 13:43:48 +0100

    Simplify the Hash code a bit by removing the non-Pythonic bits

--- src/pythonGui/krbgui/data/tests/test_hash.py
+++ src/pythonGui/krbgui/data/tests/test_hash.py
@@ -8,7 +8,6 @@ from .utils import create_refactor_hash, check_hash
 def test_constructors():
     h = Hash()
     assert len(h) == 0
-    assert h.empty()
 
     h = Hash('a', 1)
     assert len(h) == 1
@@ -32,55 +31,12 @@ def test_constructors():
     assert h['F.f.f.f.f.x.y.z'] == 99
     assert h['F']['f']['f']['f']['f']['x']['y']['z'] == 99
 
+    del h['a.b.c']
+    assert not ('a.b.c' in h)
+
     h = Hash({'foo': 42, 'bar': np.pi})
     assert h['foo'] == 42
     assert h['bar'] == np.pi
-
-
-def test_getSet():
-    h = Hash()
-    h.set("a.b.c1.d", 1)
-    assert h.get("a").has("b")
-    assert h.get("a.b").has("c1")
-    assert h.get("a.b.c1").has("d")
-    assert h.has("a.b.c1.d")
-    assert "a.b.c1.d" in h
-    assert h.get("a").has("b.c1")
-    assert "b.c1" in h["a"]
-
-    h.set("a.b.c2.d", 2.0)
-    assert h.get("a.b").has("c2.d")
-    assert h.get("a.b").has("c1.d")
-    assert h.get("a.b.c1.d") == 1
-    assert h.get("a.b.c2.d") == 2.0
-
-    h = Hash()
-    h["a.b.c"] = 1  # statement is equivalent to h.set("a.b.c", 1)
-    h["a.b.c"] = 2
-    assert h["a.b.c"] == 2
-    assert h.has("a.b")
-    assert not h.has("a.b.c.d")
-    assert h["a"]["b"]["c"] == 2
-    h["a"]["b"]["c"] = 77
-    assert h["a"]["b"]["c"] == 77
-    assert h["a.b.c"] == 77
-
-    assert h.get("laber") is None
-    assert h.get("laber", "whatever") == "whatever"
-
-    del h["a.b.c"]
-    assert "a.b.c" not in h
-
-
-def test_getSetVectorHash():
-    h = Hash('a', [])
-    g = [Hash('b', 1), Hash('b', 2)]  # python list of Hashes
-    vh = h['a']  # get the reference because value is VectorHash
-    vh.extend(g)
-    g1 = (Hash('c', 10), Hash('c', 20),)  # python tuple of Hashes
-    vh.extend(g1)  # "extend" lists, tuples, VectorHash objects
-    vh.append(Hash('d', 100))  # "append" Hash object
-    assert len(vh) == 5
 
 
 def test_iteration():
@@ -89,14 +45,14 @@ def test_iteration():
     insertionOrder = [k for k in h]
     assert insertionOrder == ["should", "be", "iterated", "in", "correct",
                               "order"]
-    h.set("be", "2")  # Has no effect on order
+    h["be"] = "2"  # Has no effect on order
 
     insertionOrder = [k for k in h]
     assert insertionOrder == ["should", "be", "iterated", "in", "correct",
                               "order"]
 
-    h.erase("be")  # Remove
-    h.set("be", "2")   # Must be last element in sequence now
+    del h["be"]  # Remove
+    h["be"] = "2"   # Must be last element in sequence now
     insertionOrder = [k for k in h]
     assert insertionOrder == ["should", "iterated", "in", "correct", "order",
                               "be"]
@@ -104,26 +60,26 @@ def test_iteration():
 
 def test_attributes():
     h = Hash("a.b.a.b", 42)
-    h.setAttribute("a.b.a.b", "attr1", "someValue")
-    assert h.hasAttribute("a.b.a.b", "attr1")
-    assert h.getAttribute("a.b.a.b", "attr1") == "someValue"
+    h["a.b.a.b", "attr1"] = "someValue"
+    assert "attr1" in h["a.b.a.b", ...]
+    assert h["a.b.a.b", "attr1"] == "someValue"
 
     h = Hash("a.b.a.b", 42)
-    h.setAttribute("a.b.a.b", "attr1", "someValue")
-    assert h.getAttribute("a.b.a.b", "attr1") == "someValue"
+    h["a.b.a.b", "attr1"] = "someValue"
+    assert h["a.b.a.b", "attr1"] == "someValue"
 
-    h.setAttribute("a.b.a.b", "attr2", 42)
-    assert h.getAttribute("a.b.a.b", "attr1") == "someValue"
-    assert h.getAttribute("a.b.a.b", "attr2"), 42
+    h["a.b.a.b", "attr2"] = 42
+    assert h["a.b.a.b", "attr1"] == "someValue"
+    assert h["a.b.a.b", "attr2"] == 42
 
-    h.setAttribute("a.b.a.b", "attr2", 43)
-    assert h.getAttribute("a.b.a.b", "attr1") == "someValue"
-    assert h.getAttribute("a.b.a.b", "attr2") == 43
+    h["a.b.a.b", "attr2"] = 43
+    assert h["a.b.a.b", "attr1"] == "someValue"
+    assert h["a.b.a.b", "attr2"] == 43
 
-    h.setAttribute("a.b.a.b", "attr1", True)
-    assert h.getAttribute("a.b.a.b", "attr1") is True
-    assert h.getAttribute("a.b.a.b", "attr2") == 43
-    attrs = h.getAttributes("a.b.a.b")
+    h["a.b.a.b", "attr1"] = True
+    assert h["a.b.a.b", "attr1"] is True
+    assert h["a.b.a.b", "attr2"] == 43
+    attrs = h["a.b.a.b", ...]
     assert attrs.get("attr1") is True
     assert attrs["attr1"] is True
     assert attrs.get("attr2") == 43
@@ -179,14 +135,9 @@ def test_paths_and_keys():
     paths = ['bool', 'int', 'string', 'stringlist', 'chars', 'vector',
              'emptyvector', 'hash.a', 'hash.b', 'hash', 'hashlist',
              'emptystringlist', 'nada', 'schema']
-    nonkeys = ['hash.a', 'hash.b']
-    keys = [k for k in paths if k not in nonkeys]
 
     h = create_refactor_hash()
-
     assert h.paths() == paths
-    assert h.getKeys() == keys
-    assert h.getKeys(keys=['extra']) == (['extra'] + keys)
 
 
 @raises(TypeError)
@@ -197,4 +148,4 @@ def test_unknown_value_type_raises():
 
 def test_schema_simple():
     s = Schema('a_schema')
-    assert s.hash.empty()
+    assert len(s.hash) == 0
