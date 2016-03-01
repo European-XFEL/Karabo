2507d3c5b461dd2c66d259a507516ec6accbea27
heisenb <burkard.heisen@xfel.eu>
2016-03-01 18:36:07 +0000

    First prototype of a device-server/plugin template for c++, needs discussion and finalization
    git-svn-id: https://svnsrv.desy.de/desy/EuXFEL/WP76/karabo/karaboFramework/trunk@19248 a23ab9f9-1ea2-4934-960d-990259f9ea8c

---
+++ run/etc/templates/cppServer/minimal/src/tests/__CLASS_NAME___Test.cc
@@ -0,0 +1,27 @@
+/*
+ * $Id$
+ *
+ * Author: <__EMAIL__>
+ * 
+ * Created on __DATE__
+ *
+ * Copyright (c) 2010-2013 European XFEL GmbH Hamburg. All rights reserved.
+ */
+
+#include <karabo/karabo.hpp>
+
+using namespace std;
+
+KARABO_NAMESPACES
+
+int main(int argc, char** argv) {
+
+     
+    } catch (const Exception& e) {
+        std::cerr << e;
+    } catch (...) {
+        KARABO_RETHROW;
+    }
+    return (EXIT_SUCCESS);
+}
+
