75ca767bb4a6cee81823d63c36804ba233d86752
Martin Teichmann <martin.teichmann@xfel.eu>
2016-03-03 09:31:29 +0100

    implement a bit more of the timestamp class
    now we can parse string timetamps

---
+++ src/pythonKarabo/karabo/api2/tests/timestamp_test.py
@@ -0,0 +1,26 @@
+from unittest import TestCase, main
+import time
+
+from karabo.api2.timestamp import Timestamp
+
+
+class Tests(TestCase):
+    def test_init(self):
+        t = Timestamp()
+        self.assertLess(abs(t.toTimestamp() - time.time()), 0.001)
+
+        t2 = Timestamp(t)
+        self.assertLess(abs(t2.toTimestamp() - time.time()), 0.001)
+
+        t3 = Timestamp(t.toLocal())
+        self.assertLess(abs(t3.toTimestamp() - time.time()), 0.001)
+
+    def test_hash(self):
+        self.assertIsNone(Timestamp.fromHashAttributes(dict()))
+        t = Timestamp.fromHashAttributes(dict(sec=1234, frac=5678, tid=22))
+        self.assertEqual(t.time, 1234000000000000005678)
+        self.assertEqual(t.tid, 22)
+
+
+if __name__ == "__main__":
+    main()
