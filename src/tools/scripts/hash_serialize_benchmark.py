b701c31dd50ddae5ae6fae262ca4abc7c9f78028
John Wiggins <john.wiggins@xfel.eu>
2016-03-03 12:05:54 +0100

    Make the benchmark work with all the APIs.
    Now we can compare!

--- src/tools/scripts/hash_serialize_benchmark.py
+++ src/tools/scripts/hash_serialize_benchmark.py
@@ -3,8 +3,6 @@ import time
 
 import numpy as np
 
-from krbgui.data.api import Hash, read_binary_hash, write_binary_hash
-
 
 def benchmark_read_func(read_func, hash_buffer, num_iters=10):
     """ Runs `read_func` `num_iters` times on the data in `read_buffer` and
@@ -32,8 +30,8 @@ def benchmark_wrapper(func, args):
     return perf_time, count
 
 
-def create_array_hash():
-    return Hash(
+def create_array_hash(hash_factory):
+    return hash_factory(
         "data", np.zeros(200000),
         "width", 400,
         "height", 500,
@@ -41,39 +39,77 @@ def create_array_hash():
     )
 
 
-def create_flat_hash():
-    return Hash(
+def create_flat_hash(hash_factory):
+    return hash_factory(
         "bool", True,
         "bytes", b"deadbeef",
         "float", 3.14,
         "integer", 42,
-        "bigint", 1 << 60,
-        "complex", 1+2j,
+        "bigint", 1 << 20,
         "stringlist", ["one", "two", "three"],
-        "hashlist", [Hash("foo", 123), Hash("bar", False)]
+        "hashlist", [hash_factory("foo", 123), hash_factory("bar", False)]
     )
 
 
-def create_deep_hash():
-    return Hash(
+def create_deep_hash(hash_factory):
+    return hash_factory(
         "a.b.c", True,
         "foo.bar.baz", b"deadbeef",
         "foo.bar.qux", 3.14,
         "a.b.z", 42,
-        "x.y.z", 1 << 60,
-        "one.two.three", 1+2j,
+        "x.y.z", 1 << 20,
         "three.two.one", ["one", "two", "three"],
-        "hashlist", [Hash("foo", 123), Hash("bar", False)]
+        "hashlist", [hash_factory("foo", 123), hash_factory("bar", False)]
     )
 
 
-def get_hash_buffer(hash_create_func, write_func):
-    return write_func(hash_create_func())
+def get_api_1_funcs():
+    from karabo.api_1 import Hash, BinarySerializerHash
+
+    serializer = BinarySerializerHash.create("Bin")
+
+    def create_hash(*args):
+        h = Hash()
+        for k, v in zip(args[::2], args[1::2]):
+            h.set(k, v)
+        return h
+
+    def read_func(buffer):
+        return serializer.load(buffer)
+
+    def write_func(hsh):
+        return serializer.save(hsh)
+
+    return (read_func, write_func, create_hash)
+
+
+def get_api_2_funcs():
+    from karabo.api_2 import BinaryParser, Hash
+
+    parser = BinaryParser()
+
+    def read_func(buffer):
+        return parser.read(buffer)
+
+    def write_func(hsh):
+        return hsh.encode("Bin")
+
+    return (read_func, write_func, Hash)
+
+
+def get_api_refactor_funcs():
+    from krbgui.data.api import Hash, read_binary_hash, write_binary_hash
+
+    return (read_binary_hash, write_binary_hash, Hash)
+
+
+def get_hash_buffer(hash_create_func, write_func, hash_factory):
+    return write_func(hash_create_func(hash_factory))
 
 
 def run_benchmark(args):
     API_FUNCS = [
-        (read_binary_hash, write_binary_hash),
+        get_api_1_funcs(), get_api_2_funcs(), get_api_refactor_funcs()
     ]
     RUNS = [
         ('Array', create_array_hash),
@@ -81,9 +117,9 @@ def run_benchmark(args):
         ('Deep', create_deep_hash),
     ]
 
-    read_func, write_func = API_FUNCS[args.api - 1]
+    read_func, write_func, hash_factory = API_FUNCS[args.api - 1]
     for name, create_func in RUNS:
-        buffer = get_hash_buffer(create_func, write_func)
+        buffer = get_hash_buffer(create_func, write_func, hash_factory)
         f_args = (read_func, buffer)
         avg_time, iterations = benchmark_wrapper(benchmark_read_func, f_args)
         print('{}: API {} ran in {} secs ({} iterations)'.format(
