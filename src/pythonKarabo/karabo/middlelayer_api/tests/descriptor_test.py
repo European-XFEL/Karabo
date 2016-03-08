84f1499b379b129ca28b863d9c79292ea2d9a0cf
Martin Teichmann <martin.teichmann@xfel.eu>
2016-03-08 18:34:59 +0100

    add tests for the new converters

---
+++ src/pythonKarabo/karabo/middlelayer_api/tests/descriptor_test.py
@@ -0,0 +1,179 @@
+from unittest import TestCase, main
+
+from karabo.middlelayer_api import hash as hashmod
+from karabo.middlelayer_api.basetypes import QuantityValue
+from karabo.middlelayer_api.enums import Unit, MetricPrefix
+
+
+class Tests(TestCase):
+    def check_timestamp(self, desc, value):
+        self.assertEqual(value.descriptor, desc)
+        value.timestamp = 7
+        c = desc.toKaraboValue(value)
+        self.assertEqual(c.timestamp, 7)
+        if isinstance(desc, hashmod.NumpyVector):
+            self.assertTrue((c == value).all())
+        else:
+            self.assertTrue(c == value)
+
+    def test_bool(self):
+        d = hashmod.Bool()
+        v = d.toKaraboValue(3)
+        self.assertTrue(v)
+        self.assertIs(v.value, True)
+        self.check_timestamp(d, v)
+
+    def test_vector_bool(self):
+        d = hashmod.VectorBool()
+        v = d.toKaraboValue([True, False, True])
+        self.assertTrue(v[0])
+        self.assertFalse(v[1])
+        self.assertEqual(v.ndim, 1)
+        self.assertEqual(v.dtype, bool)
+        self.check_timestamp(d, v)
+
+    def test_char(self):
+        d = hashmod.Char()
+        v = d.toKaraboValue(b"3")
+        self.assertEqual(v, 51)
+        self.check_timestamp(d, v)
+
+    def test_vector_char(self):
+        d = hashmod.VectorChar()
+        v = d.toKaraboValue(b"asdf")
+        self.assertTrue(v == b"asdf")
+        self.assertEqual(repr(v), "b'asdf'")
+        self.check_timestamp(d, v)
+
+    def test_ints(self):
+        d = hashmod.Int8()
+        v = d.toKaraboValue(3)
+        self.assertEqual(v, 3)
+        self.check_timestamp(d, v)
+
+        d = hashmod.Int8(unitSymbol=Unit.METER,
+                         metricPrefixSymbol=MetricPrefix.MILLI,
+                         minExc=3, maxInc=6000)
+        v = d.toKaraboValue(5)
+        self.assertEqual(v, QuantityValue("5 mm"))
+        v = d.toKaraboValue("5 m")
+        self.assertEqual(v.magnitude, 5000)
+        v = d.toKaraboValue("3.71111 m")
+        self.assertEqual(v.magnitude, 3711)
+
+        with self.assertRaises(ValueError):
+            v = d.toKaraboValue(2)
+        with self.assertRaises(ValueError):
+            v = d.toKaraboValue("1000 nm")
+        with self.assertRaises(ValueError):
+            v = d.toKaraboValue("7 m")
+
+    def test_vector_ints(self):
+        d = hashmod.VectorInt8()
+        v = d.toKaraboValue([1, 2, 3])
+        self.check_timestamp(d, v)
+        self.assertEqual(v[1], 2)
+
+        d = hashmod.VectorInt8(unitSymbol=Unit.METER,
+                               metricPrefixSymbol=MetricPrefix.MILLI)
+        v = d.toKaraboValue([2, 3, 4])
+        self.assertEqual(v[1], QuantityValue("3 mm"))
+        self.assertNotEqual(v[2], 4)
+
+        d = hashmod.VectorInt8(unitSymbol=Unit.METER,
+                               metricPrefixSymbol=MetricPrefix.MICRO)
+        v = d.toKaraboValue(v)
+        self.assertAlmostEqual(v[1].magnitude, 3000)
+
+    def test_floats(self):
+        d = hashmod.Float()
+        v = d.toKaraboValue(3)
+        self.assertEqual(v, 3)
+        self.check_timestamp(d, v)
+
+        d = hashmod.Float(unitSymbol=Unit.METER,
+                          metricPrefixSymbol=MetricPrefix.MILLI,
+                          minExc=3, maxInc=6000)
+        v = d.toKaraboValue(5)
+        self.assertEqual(v, QuantityValue("5 mm"))
+        v = d.toKaraboValue("5 m")
+        self.assertEqual(v.magnitude, 5000)
+        v = d.toKaraboValue("3.71111 m")
+        self.assertEqual(v.magnitude, 3711.11)
+
+        with self.assertRaises(ValueError):
+            v = d.toKaraboValue(2)
+        with self.assertRaises(ValueError):
+            v = d.toKaraboValue("1000 nm")
+        with self.assertRaises(ValueError):
+            v = d.toKaraboValue("7 m")
+
+    def test_vector_floats(self):
+        d = hashmod.VectorFloat()
+        v = d.toKaraboValue([1, 2, 3])
+        self.check_timestamp(d, v)
+        self.assertEqual(v[1], 2)
+
+        d = hashmod.VectorFloat(unitSymbol=Unit.METER,
+                                metricPrefixSymbol=MetricPrefix.MILLI)
+        v = d.toKaraboValue([2, 3, 4])
+        self.assertEqual(v[1], QuantityValue("3 mm"))
+        self.assertNotEqual(v[2], 4)
+
+        d = hashmod.VectorFloat(unitSymbol=Unit.METER,
+                                metricPrefixSymbol=MetricPrefix.KILO)
+        v = d.toKaraboValue(v)
+        self.assertAlmostEqual(v[1].magnitude, 3e-6)
+
+    def test_complex(self):
+        d = hashmod.ComplexFloat()
+        v = d.toKaraboValue(3+4j)
+        self.assertEqual(v, 3+4j)
+        self.check_timestamp(d, v)
+
+        d = hashmod.ComplexFloat(unitSymbol=Unit.METER,
+                          metricPrefixSymbol=MetricPrefix.MILLI)
+        v = d.toKaraboValue(5+3j)
+        self.assertEqual(v.real, QuantityValue("5 mm"))
+        self.assertEqual(v.imag, QuantityValue("3 mm"))
+        v = d.toKaraboValue("5 m")
+        self.assertEqual(v.magnitude, 5000)
+        v = d.toKaraboValue("3.71111 m")
+        self.assertEqual(v.magnitude, 3711.11)
+
+    def test_vector_complex(self):
+        d = hashmod.VectorComplexFloat()
+        v = d.toKaraboValue([1+2j, 2+3j, 3])
+        self.check_timestamp(d, v)
+        self.assertEqual(v[1], 2+3j)
+
+        d = hashmod.VectorComplexFloat(unitSymbol=Unit.METER,
+                                metricPrefixSymbol=MetricPrefix.MILLI)
+        v = d.toKaraboValue([2+3j, 3+4j, 4])
+        self.assertEqual(v[1].real, QuantityValue("3 mm"))
+        self.assertEqual(v[1].imag, QuantityValue("4 mm"))
+        self.assertNotEqual(v[2], 4)
+
+        d = hashmod.VectorComplexFloat(unitSymbol=Unit.METER,
+                                       metricPrefixSymbol=MetricPrefix.KILO)
+        v = d.toKaraboValue(v)
+        self.assertAlmostEqual(v[1].magnitude, 3e-6+4e-6j)
+
+    def test_string(self):
+        d = hashmod.String()
+        v = d.toKaraboValue("bla")
+        self.check_timestamp(d, v)
+        self.assertEqual(v, "bla")
+        self.assertEqual(repr(v), "'bla'")
+
+    def test_vector_string(self):
+        d = hashmod.VectorString()
+        v = d.toKaraboValue(["a", "b", "c"])
+        self.check_timestamp(d, v)
+        self.assertEqual(v, ["a", "b", "c"])
+        self.assertEqual(v[1], "b")
+        self.assertEqual(len(v), 3)
+
+
+if __name__ == "__main__":
+    main()
