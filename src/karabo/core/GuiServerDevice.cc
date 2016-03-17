bd167be59e5471b601f997fcfab6b0ca79015de6
Gero Flucke <gero.flucke@xfel.eu>
2016-03-17 11:37:58 +0100

    Less map look-up and more robust unregistering in onStopMonitoringDevice.

--- src/karabo/core/GuiServerDevice.cc
+++ src/karabo/core/GuiServerDevice.cc
@@ -455,7 +455,7 @@ namespace karabo {
 
             try {
                 bool unregisterFlag = false;
-                string deviceId = info.get<string > ("deviceId");
+                const string& deviceId = info.get<string > ("deviceId");
 
                 KARABO_LOG_FRAMEWORK_DEBUG << "onStopMonitoringDevice : \"" << deviceId << "\"";
                 
@@ -467,8 +467,7 @@ namespace karabo {
 
                 {
                     boost::mutex::scoped_lock lock(m_monitoredDevicesMutex);
-                    m_monitoredDevices[deviceId]--;
-                    if (m_monitoredDevices[deviceId] == 0) unregisterFlag = true;
+                    if (--m_monitoredDevices[deviceId] <= 0) unregisterFlag = true; // prefix decrement!
                     KARABO_LOG_FRAMEWORK_DEBUG << "onStopMonitoringDevice " << deviceId << " (" << m_monitoredDevices[deviceId] << ")";
                 }
 
