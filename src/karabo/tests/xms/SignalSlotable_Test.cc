3bd614c0388e53bda4dab216f640bdd475248f27
Gero Flucke <gero.flucke@xfel.eu>
2016-03-21 10:13:28 +0100

    Polish SignalSlotable_Test.
    - Take care that receiveAsync test is taken into account in test result.
    - Move test class from header to .cc file. (Looks as if netbeans does not
      always recompile the test if only the header changed!)
    - CPPUNIT_ASSERT after joining thread (better diagnosis of failures).

--- src/karabo/tests/xms/SignalSlotable_Test.cc
+++ src/karabo/tests/xms/SignalSlotable_Test.cc
@@ -15,6 +15,71 @@ using namespace karabo::io;
 using namespace karabo::net;
 using namespace karabo::xms;
 
+class SignalSlotDemo : public karabo::xms::SignalSlotable {
+    int m_messageCount;
+    bool m_allOk;
+    boost::mutex m_mutex;
+
+public:
+
+    KARABO_CLASSINFO(SignalSlotDemo, "SignalSlotDemo", "1.0")
+
+    SignalSlotDemo(const std::string& instanceId, const karabo::net::BrokerConnection::Pointer connection) :
+    karabo::xms::SignalSlotable(instanceId, connection), m_messageCount(0), m_allOk(true) {
+
+        KARABO_SIGNAL("signalA", std::string);
+
+        KARABO_SLOT(slotA, std::string);
+
+        KARABO_SLOT(slotB, int, karabo::util::Hash);
+
+        KARABO_SLOT(slotC, int);
+
+    }
+
+    void slotA(const std::string& msg) {
+        boost::mutex::scoped_lock lock(m_mutex);
+        // Assertions
+        m_messageCount++;
+        if (msg != "Hello World!") m_allOk = false;
+        if (getSenderInfo("slotA")->getInstanceIdOfSender() != "SignalSlotDemo") {
+            m_messageCount += 1000; // Invalidate message count will let the test fail!
+        }
+        SIGNAL2("signalB", int, karabo::util::Hash);
+        connect("signalB", "slotB");
+        emit("signalB", 42, karabo::util::Hash("Was.soll.das.bedeuten", "nix"));
+    }
+
+    void slotB(int someInteger, const karabo::util::Hash& someConfig) {
+        boost::mutex::scoped_lock lock(m_mutex);
+        // Assertions
+        m_messageCount++;
+        if (someInteger != 42) m_allOk = false;
+        if (someConfig.get<std::string>("Was.soll.das.bedeuten") != "nix") m_allOk = false;
+    }
+
+    void slotC(int number) {
+        boost::mutex::scoped_lock lock(m_mutex);
+        // Assertions
+        m_messageCount++;
+        if (number != 1) m_allOk = false;
+        reply(number + number);
+    }
+
+    bool wasOk() {
+        std::cout << "\nwasOk : m_messageCount=" << m_messageCount << ", m_allOk=" << m_allOk << std::endl;
+        return ((m_messageCount == 6) && m_allOk);
+    }
+
+    void myCallBack(const std::string& someData, int number) {
+        m_messageCount++;
+        std::clog << "Got called with: " << someData << " and " << number << std::endl;
+    }
+
+
+};
+
+
 CPPUNIT_TEST_SUITE_REGISTRATION(SignalSlotable_Test);
 
 
@@ -55,7 +120,7 @@ void SignalSlotable_Test::testMethod() {
         SignalSlotDemo ssDemo("SignalSlotDemo", connection);
         ssDemo.setNumberOfThreads(2);
         boost::thread t(boost::bind(&SignalSlotable::runEventLoop, &ssDemo, 10, Hash()));                
-        
+
         // Give thread some time to come up
         boost::this_thread::sleep(boost::posix_time::milliseconds(100));
 
@@ -64,28 +129,34 @@ void SignalSlotable_Test::testMethod() {
             t.join(); // Blocks
             return;
         }
-        
+
         ssDemo.connect("signalA", "slotA");
 
         ssDemo.emit("signalA", "Hello World!");
+
+        bool timeout = false;
 
         int reply;
         try {
             ssDemo.request("SignalSlotDemo", "slotC", 1).timeout(500).receive(reply);
         } catch (karabo::util::TimeoutException&) {
-            CPPUNIT_ASSERT(false == true);
+            timeout = true;
         }
-        CPPUNIT_ASSERT(reply == 2);
-        
+
         string someData("myPrivateStuff");
         ssDemo.request("SignalSlotDemo", "slotC", 1).receiveAsync<int>(boost::bind(&SignalSlotDemo::myCallBack, &ssDemo, someData, _1));
-            
+
         ssDemo.call("SignalSlotDemo", "slotC", 1);
-                
+
         boost::this_thread::sleep(boost::posix_time::seconds(1));
         ssDemo.stopEventLoop();
         t.join();
 
+        // Looks to be better to do this after joining the thread...
+        CPPUNIT_ASSERT(timeout == false);
+        CPPUNIT_ASSERT(reply == 2);
+        // Give thread some time to receiveAsync from above
+        boost::this_thread::sleep(boost::posix_time::milliseconds(250));
         CPPUNIT_ASSERT(ssDemo.wasOk() == true);
     } catch (const karabo::util::Exception& e) {
         cout << e;
