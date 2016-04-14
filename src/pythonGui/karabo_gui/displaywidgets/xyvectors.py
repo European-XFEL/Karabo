5f292bbadb24d8301e830636d6a9557aaa8b7000
Martin Teichmann <martin.teichmann@xfel.eu>
2016-04-14 15:48:28 +0200

    add an XY-Plot widget
    after strong and long-lasting request, we finally have an XY-Plot
    widget which plots one vector against the other.
    
    Upon special request, this should be back-ported to 1.4.

---
+++ src/pythonGui/karabo_gui/displaywidgets/xyvectors.py
@@ -0,0 +1,81 @@
+import base64
+import pickle
+from xml.etree.ElementTree import Element
+
+from PyQt4.Qwt5.Qwt import QwtPlot
+
+from guiqwt.plot import CurveDialog, PlotManager
+from guiqwt.builder import make
+
+from karabo.api_2 import NumpyVector
+
+from karabo_gui.const import ns_karabo
+from karabo_gui.widget import DisplayWidget
+from manager import getDevice
+
+
+class XYVector(DisplayWidget):
+    category = NumpyVector
+    alias = "XY-Plot"
+
+    def __init__(self, box, parent):
+        super().__init__(None)
+        self.widget = CurveDialog(edit=False, toolbar=True,
+                                  wintitle="XY-Plot")
+        self.plot = self.widget.get_plot()
+        self.plot.set_antialiasing(True)
+
+        self.manager = PlotManager(self)
+        self.manager.add_plot(self.plot)
+
+        self.manager.register_all_curve_tools()
+        self.manager.register_other_tools()
+
+        self.plot.setAxisAutoScale(QwtPlot.yLeft)
+        self.plot.setAxisAutoScale(QwtPlot.xBottom)
+        self.xbox = box
+        self.curves = {}
+        self.active = True
+
+    def typeChanged(self, box):
+        pos = QwtPlot.xBottom if box is self.xbox else QwtPlot.yLeft
+        self.plot.setAxisTitle(pos, box.descriptor.displayedName)
+
+    def addBox(self, box):
+        if box.descriptor is not None:
+            name = box.descriptor.displayedName
+        else:
+            name = box.path[-1]
+        curve = make.curve([], [], name, "r")
+        self.curves[box] = curve
+        self.plot.add_item(curve)
+        return True
+
+    @property
+    def boxes(self):
+        return [self.xbox] + list(self.curves.keys())
+
+    def valueChanged(self, box, value, timestamp=None):
+        if box is self.xbox:
+            for b, c in self.curves.items():
+                c.set_data(value, b.value)
+        else:
+            self.curves[box].set_data(self.xbox.value, value)
+        self.plot.replot()
+
+    def save(self, e):
+        for box, curve in self.curves.items():
+            ee = Element(ns_karabo + "box")
+            ee.set("device", box.configuration.id)
+            ee.set("path", ".".join(box.path))
+            ee.text = base64.b64encode(pickle.dumps(curve)).decode("ascii")
+            e.append(ee)
+
+    def load(self, e):
+        for ee in e:
+            box = getDevice(ee.get("device")).getBox(ee.get("path").split("."))
+            curve = self.curves[box]
+            self.plot.del_item(curve)
+            curve = pickle.loads(base64.b64decode(ee.text))
+            self.plot.add_item(curve)
+            self.curves[box] = curve
