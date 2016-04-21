ec23ad13153b6f545b417357a35b257745e36301
Gero Flucke <gero.flucke@xfel.eu>
2016-04-21 11:56:07 +0200

    Use smarter bash syntax (and remove a remnant "$ID").

--- src/tools/scripts/inspectBrokerTopic.sh
+++ src/tools/scripts/inspectBrokerTopic.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BROKER=exfl-broker:7777
+BROKER_DEFAULT=exfl-broker:7777
 
 if [ ${#@} -le 0 ] || [ $1 == "-h" ] || [ $1 == "--help" ]; then
     # print help message if first argument asks for help
@@ -9,17 +9,19 @@ if [ ${#@} -le 0 ] || [ $1 == "-h" ] || [ $1 == "--help" ]; then
     echo 
     echo "  $0 [-h|--help] topic [broker:port [firstMessageIdx [numMessages]]]"
     echo
-    echo "Default broker:port is '$BROKER'."
+    echo "Default broker:port is '$BROKER_DEFAULT'."
     echo "Must be called from a directory where './queryBroker' is available."
     echo
     exit 0
 fi
 
 TOPIC=$1
+# Broker from argument or from default if argument not set (or null).
+BROKER=${2:-$BROKER_DEFAULT}
 
-if [ ${#@} -ge 2 ]; then
-    BROKER=$2
-fi
+COMMAND="./queryBroker -b $BROKER list msg -t t -n $TOPIC -nocheck"
+# If more arguments are given, use them (otherwise ${3:+<...>} stays empty):
+COMMAND="$COMMAND ${3:+-startMsgIndex $3} ${4:+-maxMsgsRet $4}"
 
 # Command gives something like:
 # ...
@@ -29,19 +31,11 @@ fi
 #0           ID:15-192.168.140.159-59611-1459519722221       4          BytesMessage
 #1           ID:8-192.168.140.159-59620-1459519722615        4          BytesMessage
 # ...
-COMMAND="./queryBroker -b $BROKER list msg -t t -n $TOPIC -nocheck"
-
-if [ ${#@} -ge 3 ]; then
-    COMMAND="$COMMAND -startMsgIndex $3"
-    if [ ${#@} -ge 4 ]; then
-        COMMAND="$COMMAND -maxMsgsRet $4"
-    fi
-fi
 
 declare -i counter=0
 
 for msgID in `$COMMAND | grep 'ID:' | awk '{print $2}' `; do
-    echo "message query no. ${counter}: " $ID
+    echo "message query no. ${counter}: "
     ./queryBroker -b $BROKER query msg -t t -n $TOPIC -nocheck -msgID $msgID
     echo
     echo "=================================================================="
