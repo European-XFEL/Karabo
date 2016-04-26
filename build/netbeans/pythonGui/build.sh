4e4c0e90d0d4e715ceae6b7acdf21b1f5a901bb0
John Wiggins <john.wiggins@xfel.eu>
2016-04-26 16:10:56 +0200

    Fix up the build scripts to build the GUI as a wheel

--- build/netbeans/pythonGui/build.sh
+++ build/netbeans/pythonGui/build.sh
@@ -1,33 +1,28 @@
 #!/bin/bash
 CWD=$(pwd)
-DIST=dist/$(uname -s)
-# clean previous dist folder - in case of file/folders deletion/creation in oryginal source folder
+OS=$(uname -s)
+DIST=dist/$OS
+
+if [ "$OS" = "Darwin" ]; then
+    PYTHON=/opt/local/bin/python
+    KARABO=$($PYTHON -c 'import os,sys;print(os.path.realpath(sys.argv[1]))' "$1")
+    PIP=/opt/local/bin/pip
+    PIP_EXTRA_ARGS="--user"
+else
+    KARABO=$(readlink -f "$1")
+    PYTHON=$KARABO/extern/bin/python
+    PIP=$KARABO/extern/bin/pip
+    PIP_EXTRA_ARGS=
+fi
+
+# clean previous dist folder - in case of file/folders deletion/creation in original source folder
 rm -rf dist
 mkdir -p $DIST/lib
 mkdir -p $DIST/bin
-cd $DIST/bin
-cat > karabo-gui <<End-of-file
-#!/bin/bash
-#
-# This file was automatically generated. Do not edit.
-#
-SCRIPT_DIR=\$(dirname \`[[ \$0 = /* ]] && echo "\$0" || echo "\$PWD/\${0#./}"\`)
-KARABO_DIR=\$SCRIPT_DIR/..
-OS=\$(uname -s)
-export PATH=\$KARABO_DIR/extern/bin:\$PATH
-export PYTHONPATH=\$KARABO_DIR/extern/lib/karabo_python
-if [ "\$OS" = "Darwin" ]; then
-    export PATH=/opt/local/bin:\$PATH
-    export PYTHONPATH=\$KARABO_DIR/lib:\$PYTHONPATH
-    export DYLD_LIBRARY_PATH=\$KARABO_DIR/lib:\$KARABO_DIR/extern/lib:\$DYLD_LIBRARY_PATH
-fi
 
-cd \$KARABO_DIR/lib/pythonGui
-python3 karabo-gui.py "\$@"
-cd -
-End-of-file
-chmod a+x karabo-gui
-cd ../lib
-cp -rf ../../../../../../src/pythonGui .
-rm -f `find . -type f -name *.pyc`
+
+cd ../../../src/pythonGui
+rm -rf dist/ build/
+$PYTHON setup.py bdist_wheel
+$PIP install -U $PIP_EXTRA_ARGS dist/*.whl
 cd $CWD
