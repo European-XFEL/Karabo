8180eda247d27945e3e9221e924dbe90da4ff8ce
John Wiggins <john.wiggins@xfel.eu>
2016-05-02 10:58:01 +0200

    Improve the invocation of sphinx-quickstart in python devices
    - Don't create the make.bat file
    - Force the 'default' sphinx theme

--- run/etc/templates/pythonDevice/minimal/.configure.sh
+++ run/etc/templates/pythonDevice/minimal/.configure.sh
@@ -8,6 +8,10 @@ echo
 mkdir doc
 pushd doc
 
-sphinx-quickstart -q --sep -p "__PACKAGE_NAME__" -a "__EMAIL__" -v 0.1.0 .
+sphinx-quickstart -q --sep --no-batchfile -p "__PACKAGE_NAME__" -a "__EMAIL__" -v 0.1.0 .
+
+# Replace the theme with one of our choosing
+# In sphinx 1.4+, this can be done easily with the -D option...
+sed -i "s%^html_theme\ \=\ '\([a-zA-Z]*\)'%html_theme\ \=\ 'default'%" source/conf.py
 
 popd
