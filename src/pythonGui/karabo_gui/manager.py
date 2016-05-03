6807936c1f32e9bcd1620b0876fce5b8b294ab7f
John Wiggins <john.wiggins@xfel.eu>
2016-05-03 13:22:31 +0200

    Remove some code duplication

--- src/pythonGui/karabo_gui/manager.py
+++ src/pythonGui/karabo_gui/manager.py
@@ -288,6 +288,12 @@ class _Manager(QObject):
         else:
             return None
 
+    @staticmethod
+    def _loadClassConfiguration(classConfig, configHash, classId):
+        if classId not in configHash:
+            MessageBox.showError("Configuration load failed")
+            return
+        classConfig.fromHash(configHash[classId])
 
     def onOpenFromFile(self):
         filename = QFileDialog.getOpenFileName(None, "Open configuration", \
@@ -301,12 +307,8 @@ class _Manager(QObject):
         r = XMLParser()
         with open(filename, 'rb') as file:
             config = r.read(file.read())
-        
-        if not config.has(classId):
-            MessageBox.showError("Configuration open failed")
-            return
 
-        conf.fromHash(config[classId])
+        self._loadClassConfiguration(conf, config, classId)
 
     def onOpenFromProject(self):
         # Open dialog to select project and configuration
@@ -317,11 +319,7 @@ class _Manager(QObject):
         conf, classId = self.currentConfigurationAndClassId()
 
         config = dialog.projectConfiguration().hash
-        if not config.has(classId):
-            MessageBox.showError("Configuration open failed")
-            return
-
-        conf.fromHash(config[classId])
+        self._loadClassConfiguration(conf, config, classId)
 
     def onSaveToFile(self):
         """
