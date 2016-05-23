09f7c032bd17e37986c43866f6b533edb6931fd8
Kerstin Weger <kerstin.weger@xfel.eu>
2016-05-23 13:33:25 +0200

    Forward port documentation changes from 1.4. into master branch

---
+++ doc/howto/examples/myeditwidget.py
@@ -0,0 +1,30 @@
+from PyQt4.QtGui import QDoubleSpinBox
+
+from util import SignalBlocker
+from widget import EditableWidget
+from karabo.hashtypes import Simple
+
+
+class MyEditWidget(EditableWidget):
+    category = Simple # this widget will show numbers
+    alias = "My cool widget" # this is shown in the context menu
+
+    def __init__(self, box, parent):
+        super(MyEditWidget, self).__init__(box)
+        
+        self.widget = QDoubleSpinBox(parent)
+
+    @property
+    def value(self):
+        """ The current value of the widget is returned.
+        """
+        return self.widget.value()
+
+    def typeChanged(self, box):
+        # Do some initialization for a new value type
+        pass
+
+    def valueChanged(self, box, value, timestamp=None):
+        # Typically something like:
+        with SignalBlocker(self.widget):
+            self.widget.setValue(value)
