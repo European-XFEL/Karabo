d72d4b86b7e7f60236dd27c2eb3e61ff8e9976bc
John Wiggins <john.wiggins@xfel.eu>
2016-05-25 11:52:09 +0200

    Remove the karabo.api module.
    karabo.api is not the preferred nomenclature. karabo.middlelayer, please.

--- src/pythonKarabo/karabo/api.py
+++
@@ -1,8 +0,0 @@
-from karabo.middlelayer_api.enums import (
-    AccessLevel, AccessMode, Assignment, MetricPrefix, Unit)
-from karabo.middlelayer_api.hash import (
-    String, Int32 as Int, Slot, Double as Float, Bool)
-from karabo.middlelayer_api.macro import Macro
-from karabo.middlelayer_api.device_client import (
-    waitUntilNew, waitUntil, setWait, setNoWait, getDevice, executeNoWait,
-    updateDevice, sleep)
