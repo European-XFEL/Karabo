61eb2cb0c9b295956f798175b48d8ae9e5c821c6
Martin Teichmann <martin.teichmann@xfel.eu>
2016-05-30 15:27:27 +0200

    minor spelling fixes

--- src/pythonKarabo/karabo/middlelayer_api/exceptions.py
+++ src/pythonKarabo/karabo/middlelayer_api/exceptions.py
@@ -2,5 +2,5 @@
 class KaraboError(Exception):
     """A :class:`KaraboError` is raised if an error occurs which is
     specific to Karabo. This is mostly because things went wrong on
-    the other side of the network.
+    the other end of a network connection.
     """
