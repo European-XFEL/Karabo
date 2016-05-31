da5eb57ac57319b88bd646ac08d42c85c7cbe342
Gero Flucke <gero.flucke@xfel.eu>
2016-05-31 09:51:23 +0200

    Remove code duplication for running asio::io_service in a protected way.
    Also add some delay before rerunning to avoid logging errors at full speed in
    case of recurring problems.

--- src/karabo/net/utils.hh
+++ src/karabo/net/utils.hh
@@ -12,10 +12,25 @@
 
 #include <string>
 
+namespace boost {
+    namespace asio {
+        class io_service;
+    }
+}
+
 namespace karabo {
     namespace net {
         /// Bare host name after stripping domain (exflxxx12345.desy.de => exflxxx12345)
         std::string bareHostName();
+
+        /// Wrapper around boost::asio::io_service::run that catches exceptions,
+        /// logs them as errors and continues after some delay.
+        /// @param service valid (raw) pointer to the io_service
+        /// @param category the category used for logging
+        /// @param errorMessage will be part of the logged error
+        /// @param delayInMilliSec is the delay after each catch
+        void runProtected(boost::asio::io_service* service, const std::string& category,
+                          const std::string& errorMessage, unsigned int delayInMilliSec = 100);
     }
 }
 
