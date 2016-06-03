86dee98623b3b379d96212134e2dc3ec73c40d55
John Wiggins <john.wiggins@xfel.eu>
2016-06-03 11:32:55 +0200

    Add the widget classes to the scene model

--- src/pythonGui/karabo_gui/scenemodel/api.py
+++ src/pythonGui/karabo_gui/scenemodel/api.py
@@ -7,3 +7,17 @@ from .model import SceneModel  # noqa
 from .registry import register_scene_reader, register_scene_writer  # noqa
 from .shapes import LineModel, PathModel, RectangleModel  # noqa
 from .simple_widgets import LabelModel, SceneLinkModel, WorkflowItemModel  # noqa
+from .widgets import (BitfieldModel, CheckBoxModel, ChoiceElementModel,  # noqa
+                      ComboBoxModel, DirectoryModel, DisplayAlignedImageModel,  # noqa
+                      DisplayCommandModel, DisplayIconsetModel,  # noqa
+                      DisplayImageModel, DisplayImageElementModel,  # noqa
+                      DisplayLabelModel, DisplayPlotModel,  # noqa
+                      DisplayStateColorModel, DoubleLineEditModel,  # noqa
+                      EditableListModel, EditableListElementModel,  # noqa
+                      EditableSpinBoxModel, EvaluatorModel, FileInModel,  # noqa
+                      FileOutModel, FloatSpinBoxModel, HexadecimalModel,  # noqa
+                      IconData, BaseIconsModel, DigitIconsModel,  # noqa
+                      SelectionIconsModel, TextIconsModel, IntLineEditModel,  # noqa
+                      KnobModel, LineEditModel, PlotCurveModel, LinePlotModel,  # noqa
+                      MonitorModel, SingleBitModel, SliderModel,  # noqa
+                      TableElementModel, VacuumWidgetModel, XYPlotModel)  # noqa
