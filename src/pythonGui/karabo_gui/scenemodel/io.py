ddd727286551d389cc8ec7a73c9b30fbb607a870
John Wiggins <john.wiggins@xfel.eu>
2016-06-03 11:30:56 +0200

    Add the read_scene/write_scene methods to the scene model + tests

---
+++ src/pythonGui/karabo_gui/scenemodel/io.py
@@ -0,0 +1,25 @@
+from xml.etree.ElementTree import Element, parse, tostring
+
+from .const import NS_KARABO
+from .registry import get_reader, get_writer
+
+
+def read_scene(filename):
+    """ Read a scene from the file name `filename` and return it.
+    """
+    tree = parse(filename)
+    root = tree.getroot()
+
+    version = int(root.get(NS_KARABO + 'version', '1'))
+    reader = get_reader(version)
+    return reader(root)
+
+
+def write_scene(scene):
+    """ Write Scene object `scene` to a string.
+    """
+    root = Element('svg', version='1')
+
+    writer = get_writer()
+    writer(scene, root)
+    return tostring(root)
