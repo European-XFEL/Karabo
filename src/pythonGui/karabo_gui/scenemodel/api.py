f806c4b431822c1bbed64b8521b1a0e54c0f31ac
John Wiggins <john.wiggins@xfel.eu>
2016-06-03 11:22:17 +0200

    Add the SceneModel class and the bases for the rest of the model

--- src/pythonGui/karabo_gui/scenemodel/api.py
+++ src/pythonGui/karabo_gui/scenemodel/api.py
@@ -1,2 +1,5 @@
+from .bases import (BaseLayoutData, BaseLayoutModel, BaseSceneObjectData,  # noqa
+                    BaseShapeObjectData,  BaseWidgetObjectData)  # noqa
 from .const import NS_KARABO, NS_SVG, SCENE_MIN_WIDTH, SCENE_MIN_HEIGHT  # noqa
+from .model import SceneModel  # noqa
 from .registry import register_scene_reader, register_scene_writer  # noqa
