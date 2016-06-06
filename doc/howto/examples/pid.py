4c9f4fb17897339dfe72d905526de3eb6b9f77c0
Martin Teichmann <martin.teichmann@xfel.eu>
2016-06-06 10:43:26 +0200

    improve pid device accorting to suggestions

--- doc/howto/examples/pid.py
+++ doc/howto/examples/pid.py
@@ -1,5 +1,5 @@
 from karabo.middlelayer import (
-    AccessMode, background, Device, Float,
+    AccessMode, Assignment, background, Device, Float,
     getDevice, Slot, State, String, Unit, waitUntilNew)
 
 
@@ -18,12 +18,14 @@ class PID(Device):
         displayedName="Proportional Term",
         description="Feed back the error proportionally with this factor",
         accessMode=AccessMode.RECONFIGURABLE,
+        assignment=Assignment.MANDATORY,
         unitSymbol=Unit.NUMBER)
 
     integral = Float(
         displayedName="Integral Term",
         description="Feed back the integral of the error with this factor",
         accessMode=AccessMode.RECONFIGURABLE,
+        assignment=Assignment.MANDATORY,
         unitSymbol=Unit.HERTZ)
 
     current_integral = Float(
@@ -36,6 +38,7 @@ class PID(Device):
         displayedName="Derivative Term",
         description="Feed back the derivative of the error with this factor",
         accessMode=AccessMode.RECONFIGURABLE,
+        assignment=Assignment.MANDATORY,
         unitSymbol=Unit.SECOND)
 
     setpoint = Float(
@@ -52,12 +55,14 @@ class PID(Device):
         displayedName="Process Variable",
         description="The device and property name of the process variable",
         accessMode=AccessMode.INITONLY,
+        assignment=Assignment.MANDATORY,
         displayType="property")
 
     control = String(
         displayedName="Control Variable",
         description="The device and property name of the control variable",
         accessMode=AccessMode.INITONLY,
+        assignment=Assignment.MANDATORY,
         displayType="property")
 
     command = String(
@@ -65,6 +70,7 @@ class PID(Device):
         description="The device and property name of the slot to execute on "
                     "the control device",
         accessMode=AccessMode.INITONLY,
+        assignment=Assignment.MANDATORY,
         displayType="remoteSlot")
 
     @Slot(displayedName="Go")
@@ -84,11 +90,16 @@ class PID(Device):
     def runner(self):
         process_name, process_property = self.process.split(".", 2)
         control_name, control_property = self.control.split(".", 2)
+        command_name, command_slot = self.command.split(".", 2)
+        assert command_name == control_name
 
         with getDevice(control_name) as control_device, \
                 getDevice(process_name) as process_device:
+            command = getattr(control_device, command_slot)
             last_error = getattr(process_device, process_name) - self.setpoint
             while True:
+                waitUntilNew(getattr(process_device, process_name))
+
                 error = getattr(process_device, process_name) - self.setpoint
                 self.current_integral += error * (error.timestamp -
                                                   last_error.timestamp)
@@ -98,7 +109,6 @@ class PID(Device):
                            self.integral * self.current_integral +
                            self.derivative * derivative)
                 setattr(control_device, control_name, control)
+                command()
 
                 last_error = error
-
-                waitUntilNew(getattr(process_device, process_name))
