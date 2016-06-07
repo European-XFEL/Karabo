4a50eed50c73a0ba4836a5e66c896248e45987f1
Sergey Esenov <serguei.essenov@xfel.eu>
2016-06-07 10:54:32 +0200

    Experimental unified states for NoFsm case only.

--- src/karabo/core/NoFsm.hh
+++ src/karabo/core/NoFsm.hh
@@ -8,9 +8,14 @@
 #ifndef KARABO_CORE_NOFSM_HH
 #define	KARABO_CORE_NOFSM_HH
 
-
+#include <vector>
+#include <boost/function.hpp>
+#include <boost/bind.hpp>
 #include <karabo/xms/SlotElement.hh>
-#include "Device.hh"
+#include <karabo/util/ClassInfo.hh>
+#include <karabo/util/Schema.hh>
+//#include "Device.hh"
+#include "States.hh"
 
 namespace karabo {
     namespace core {
@@ -27,22 +32,22 @@ namespace karabo {
             }
 
             virtual ~NoFsm() {
-            }                      
-            
+            }
+
             void startFsm() {
                 //if (m_initialFunc.empty())
                 //    throw KARABO_LOGIC_EXCEPTION("No initial function defined. Please use  \"KARABO_INITIAL_FUNCTION(your_initial_function)\" macro in the device constructor");
                 // Call second constructors in the same order as first constructors were called
                 for (size_t i = 0; i < m_initialFunc.size(); ++i) m_initialFunc[i]();
             }
-            
+
             virtual void stopFsm() {}
 
             void registerInitialFunction(const boost::function<void()>& func) {
                 m_initialFunc.push_back(func);
             }
-            
-            #define KARABO_INITIAL_FUNCTION(function) this->registerInitialFunction(boost::bind(&Self::function, this));
+
+#define KARABO_INITIAL_FUNCTION(function) this->registerInitialFunction(boost::bind(&Self::function, this));
 
         private:
 
