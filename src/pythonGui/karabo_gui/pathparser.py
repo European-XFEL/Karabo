55f362b358f9937ffaf4596214158099f98b6424
Kerstin Weger <kerstin.weger@xfel.eu>
2016-06-09 10:12:21 +0200

    PEP8 corrections

--- src/pythonGui/karabo_gui/pathparser.py
+++ src/pythonGui/karabo_gui/pathparser.py
@@ -1,14 +1,13 @@
+import re
+from math import sqrt
+
 from PyQt4.QtCore import QPointF
 from PyQt4.QtGui import QPainterPath
 
 
-import re
-from math import sqrt
-
-
 class Parser(object):
     re = re.compile("([MmZzLlHhVvCcSsQqTtAa])|"
-        "(-?(([0-9]*[.][0-9]*)|[0-9]+)([eE][+-]?[0-9]*)?)")
+                    "(-?(([0-9]*[.][0-9]*)|[0-9]+)([eE][+-]?[0-9]*)?)")
 
     def next(self):
         if self.iter is None:
@@ -21,15 +20,12 @@ class Parser(object):
             self.iter = None
         return ret
 
-
     def __init__(self, s):
         self.iter = self.re.finditer(s)
-
 
     def __iter__(self):
         while self.token is None:
             yield self.next()
-
 
     def parse(self):
         self.path = QPainterPath()
@@ -44,7 +40,6 @@ class Parser(object):
         except StopIteration:
             return self.path
 
-
     def points(self, relative):
         for x, y in zip(self, self):
             if relative:
@@ -52,21 +47,17 @@ class Parser(object):
             else:
                 yield QPointF(x, y)
 
-
     def m(self, relative):
         p = next(self.points(relative))
         self.path.moveTo(p)
         self.l(relative)
 
-
     def z(self, relative):
         self.path.closeSubpath()
-
 
     def l(self, relative):
         for p in self.points(relative):
             self.path.lineTo(p)
-
 
     def h(self, relative):
         for x in self:
@@ -77,7 +68,6 @@ class Parser(object):
                 pos.setX(x)
             self.lineTo(pos)
 
-
     def v(self, relative):
         for y in self:
             pos = self.path.currentPosition()
@@ -87,13 +77,11 @@ class Parser(object):
                 pos.setY(y)
             self.lineTo(pos)
 
-
     def c(self, relative):
         p = self.points(relative)
         for a, b, c in zip(p, p, p):
             self.path.cubicTo(a, b, c)
         self.lastcontrol = b
-
 
     def s(self, relative):
         p = self.points(relative)
@@ -105,13 +93,11 @@ class Parser(object):
             a = 2 * c - b
         self.lastcontrol = b
 
-
     def q(self, relative):
         p = self.points(relative)
         for a, b in zip(p, p):
             self.path.quadTo(a, b)
         self.lastcontrol = a
-
 
     def t(self, relative):
         if self.lasttoken not in 'QqTt':
@@ -122,13 +108,12 @@ class Parser(object):
             a = 2 * c - b
         self.lastcontrol = a
 
-
     def a(self, relative):
         for rx, ry, phi, fa, fs, x, y in zip(*(self,) * 7):
             pp = (self.path.currentPosition() - QPointF(x, y)) / 2
             cp = (-1) ** (fa == fs) * sqrt(
                  ((rx * ry) ** 2 - (rx * pp.y()) ** 2 - (ry * pp.x()) ** 2) /
-                  ((rx * pp.y()) ** 2 + (ry * pp.x()) ** 2)) * (
+                 ((rx * pp.y()) ** 2 + (ry * pp.x()) ** 2)) * (
                  QPointF(rx * pp.y() / ry, -ry * pp.x() / rx))
             c = cp + (self.path.currentPosition() + QPointF(x, y)) / 2
 
