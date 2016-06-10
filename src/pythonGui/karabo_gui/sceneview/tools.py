05fdc8df98740cc9cacc5fed7329be8a0800c0d7
Kerstin Weger <kerstin.weger@xfel.eu>
2016-06-10 14:10:09 +0200

    Add first actions (design/control mode switch, line)

--- src/pythonGui/karabo_gui/sceneview/tools.py
+++ src/pythonGui/karabo_gui/sceneview/tools.py
@@ -1,6 +1,6 @@
 from traits.api import Callable
 
-from .bases import BaseSceneAction
+from .bases import BaseSceneAction, BaseSceneTool
 
 
 class CreateToolAction(BaseSceneAction):
@@ -15,3 +15,28 @@ class CreateToolAction(BaseSceneAction):
         """
         tool = self.tool_factory()
         scene_view.set_tool(tool)
+
+
+class LineSceneTool(BaseSceneTool):
+    def draw(self, painter):
+        """ The method which is responsible for drawing this tool.
+        The tool for a SceneView will be drawn after everything else in
+        the view has been drawn.
+
+        This method is optional.
+        """
+
+    def mouse_down(self, scene_view, event):
+        """ A callback which is fired whenever the user clicks in the
+        SceneView.
+        """
+
+    def mouse_move(self, scene_view, event):
+        """ A callback which is fired whenever the user moves the mouse
+        in the SceneView.
+        """
+
+    def mouse_up(self, scene_view, event):
+        """ A callback which is fired whenever the user ends a mouse click
+        in the SceneView.
+        """
