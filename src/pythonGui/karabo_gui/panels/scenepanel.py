94fa17476c4b9f8e83bf947f08ca4d540882db21
Kerstin Weger <kerstin.weger@xfel.eu>
2016-06-10 14:10:09 +0200

    Add first actions (design/control mode switch, line)

--- src/pythonGui/karabo_gui/panels/scenepanel.py
+++ src/pythonGui/karabo_gui/panels/scenepanel.py
@@ -4,22 +4,79 @@
 # Copyright (C) European XFEL GmbH Hamburg. All rights reserved.
 #############################################################################
 
-from PyQt4.QtCore import pyqtSignal, Qt
-from PyQt4.QtGui import (QPalette, QScrollArea)
+from PyQt4.QtCore import pyqtSignal, pyqtSlot, Qt
+from PyQt4.QtGui import (QAction, QPalette, QScrollArea, QSizePolicy, QWidget)
 
 from karabo_gui.docktabwindow import Dockable
+import karabo_gui.icons as icons
+from karabo_gui.sceneview.tools import CreateToolAction, LineSceneTool
+from karabo_gui.toolbar import ToolBar
 
 
 class ScenePanel(Dockable, QScrollArea):
     signalClosed = pyqtSignal()
 
-    def __init__(self, scene):
+    def __init__(self, scene, connected_to_server):
         super(ScenePanel, self).__init__()
 
         # Reference to underlying scene view
         self.scene = scene
         self.setWidget(self.scene)
 
+        self.setupActions(connected_to_server)
+
         self.setBackgroundRole(QPalette.Dark)
         self.setHorizontalScrollBarPolicy(Qt.ScrollBarAsNeeded)
         self.setVerticalScrollBarPolicy(Qt.ScrollBarAsNeeded)
+
+    def design_mode_text(self, is_design_mode):
+        if is_design_mode:
+            return "Change to control mode"
+
+        return "Change to design mode"
+
+    def setupActions(self, connected_to_server):
+        # Switch for design and control mode
+        text = self.design_mode_text(self.scene.design_mode)
+        self.ac_design_mode = QAction(icons.transform, text, self)
+        self.ac_design_mode.setToolTip(text)
+        self.ac_design_mode.setStatusTip(text)
+        self.ac_design_mode.setCheckable(True)
+        self.ac_design_mode.setChecked(self.scene.design_mode)
+        self.ac_design_mode.setEnabled(connected_to_server)
+        self.ac_design_mode.toggled.connect(self.design_mode_changed)
+
+        action = CreateToolAction(tool_factory=LineSceneTool,
+                                  icon=icons.line, text="Add line",
+                                  tooltip="Add line to scene")
+        self.ac_line = QAction(action.icon, action.text, self)
+        self.ac_line.setToolTip(action.text)
+        self.ac_line.setStatusTip(action.tooltip)
+        self.ac_line.triggered.connect(action.perform)
+
+    def setupToolBars(self, standardToolBar, parent):
+        standardToolBar.addAction(self.ac_design_mode)
+
+        tool_bar = ToolBar("Drawing")
+        tool_bar.setVisible(self.scene.design_mode)
+        parent.addToolBar(tool_bar)
+        self.scene.setFocusProxy(tool_bar)
+        tool_bar.setFocusPolicy(Qt.StrongFocus)
+
+        tool_bar.addSeparator()
+        tool_bar.addAction(self.ac_line)
+
+        self.drawing_tool_bar = tool_bar
+
+        # Add placeholder widget to toolbar
+        widget = QWidget()
+        widget.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+        self.drawing_tool_bar.addWidget(widget)
+
+    @pyqtSlot(bool)
+    def design_mode_changed(self, is_checked):
+        self.drawing_tool_bar.setVisible(is_checked)
+        text = self.design_mode_text(is_checked)
+        self.ac_design_mode.setToolTip(text)
+        self.ac_design_mode.setStatusTip(text)
+        self.scene.design_mode = is_checked
