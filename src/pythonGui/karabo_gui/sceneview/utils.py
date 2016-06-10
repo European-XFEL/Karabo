7f29089a5f6cb703c7aec64d396939a622d32e18
John Wiggins <john.wiggins@xfel.eu>
2016-06-10 15:33:03 +0200

    Implement bounding rect calculation in one place

--- src/pythonGui/karabo_gui/sceneview/utils.py
+++ src/pythonGui/karabo_gui/sceneview/utils.py
@@ -7,6 +7,25 @@
 from contextlib import contextmanager
 
 
+def calc_bounding_rect(collection):
+    """ Compute the bounding rectangle for a collection of objects.
+
+    Each object in the collection must have a geometry method which returns a
+    QRect.
+    """
+    MAX_VALUE = 100000
+    left, top, right, bottom = MAX_VALUE, MAX_VALUE, 0, 0
+    for item in collection:
+        rect = item.geometry()
+        left = min(left, rect.left())
+        right = max(right, rect.right())
+        top = min(top, rect.top())
+        bottom = max(bottom, rect.bottom())
+
+    # Return x, y, width, height
+    return (left, top, right - left, bottom - top)
+
+
 @contextmanager
 def save_painter_state(painter):
     painter.save()
