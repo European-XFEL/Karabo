863a8a3f09612770ee01509ddbb98757687c4718
Kerstin Weger <kerstin.weger@xfel.eu>
2016-06-10 14:43:31 +0200

    Add create methods for actions

--- src/pythonGui/karabo_gui/sceneview/tools.py
+++ src/pythonGui/karabo_gui/sceneview/tools.py
@@ -17,7 +17,82 @@ class CreateToolAction(BaseSceneAction):
         scene_view.set_tool(tool)
 
 
+class TextSceneTool(BaseSceneTool):
+    def draw(self, painter):
+        """ The method which is responsible for drawing this tool.
+        The tool for a SceneView will be drawn after everything else in
+        the view has been drawn.
+
+        This method is optional.
+        """
+
+    def mouse_down(self, scene_view, event):
+        """ A callback which is fired whenever the user clicks in the
+        SceneView.
+        """
+
+    def mouse_move(self, scene_view, event):
+        """ A callback which is fired whenever the user moves the mouse
+        in the SceneView.
+        """
+
+    def mouse_up(self, scene_view, event):
+        """ A callback which is fired whenever the user ends a mouse click
+        in the SceneView.
+        """
+
+
 class LineSceneTool(BaseSceneTool):
+    def draw(self, painter):
+        """ The method which is responsible for drawing this tool.
+        The tool for a SceneView will be drawn after everything else in
+        the view has been drawn.
+
+        This method is optional.
+        """
+
+    def mouse_down(self, scene_view, event):
+        """ A callback which is fired whenever the user clicks in the
+        SceneView.
+        """
+
+    def mouse_move(self, scene_view, event):
+        """ A callback which is fired whenever the user moves the mouse
+        in the SceneView.
+        """
+
+    def mouse_up(self, scene_view, event):
+        """ A callback which is fired whenever the user ends a mouse click
+        in the SceneView.
+        """
+
+
+class RectangleSceneTool(BaseSceneTool):
+    def draw(self, painter):
+        """ The method which is responsible for drawing this tool.
+        The tool for a SceneView will be drawn after everything else in
+        the view has been drawn.
+
+        This method is optional.
+        """
+
+    def mouse_down(self, scene_view, event):
+        """ A callback which is fired whenever the user clicks in the
+        SceneView.
+        """
+
+    def mouse_move(self, scene_view, event):
+        """ A callback which is fired whenever the user moves the mouse
+        in the SceneView.
+        """
+
+    def mouse_up(self, scene_view, event):
+        """ A callback which is fired whenever the user ends a mouse click
+        in the SceneView.
+        """
+
+
+class SceneLinkTool(BaseSceneTool):
     def draw(self, painter):
         """ The method which is responsible for drawing this tool.
         The tool for a SceneView will be drawn after everything else in
