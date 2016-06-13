50498df3693bed0b8af08f3e8864a84668717c53
John Wiggins <john.wiggins@xfel.eu>
2016-06-13 13:13:11 +0200

    Move fill_root_layout to its own module

--- src/pythonGui/karabo_gui/sceneview/view.py
+++ src/pythonGui/karabo_gui/sceneview/view.py
@@ -9,51 +9,14 @@ import os.path
 from PyQt4.QtCore import Qt
 from PyQt4.QtGui import QPalette, QPainter, QPen, QSizePolicy, QWidget
 
-from karabo_gui.scenemodel.api import (
-    BoxLayoutModel, GridLayoutModel, LabelModel, LineModel, PathModel,
-    RectangleModel, FixedLayoutModel, UnknownXMLDataModel, read_scene,
-    SCENE_MIN_WIDTH, SCENE_MIN_HEIGHT
-)
+from karabo_gui.scenemodel.api import (read_scene, SCENE_MIN_WIDTH,
+                                       SCENE_MIN_HEIGHT)
 from .bases import BaseSceneTool
-from .const import QT_BOX_LAYOUT_DIRECTION, QT_CURSORS
-from .layouts import BoxLayout, GridLayout, GroupLayout
+from .builder import fill_root_layout
+from .const import QT_CURSORS
+from .layouts import GroupLayout
 from .selection_model import SceneSelectionModel
-from .shapes import LineShape, PathShape, RectangleShape
-from .simple_widgets import LabelWidget, UnknownSvgWidget
 from .utils import save_painter_state
-
-
-def fill_root_layout(layout, parent_model, scene_widget):
-    # Go through children and create corresponding GUI objects
-    for child in parent_model.children:
-        if isinstance(child, FixedLayoutModel):
-            obj = GroupLayout(child)
-            layout.add_layout(obj)
-            fill_root_layout(obj, child, scene_widget)
-        if isinstance(child, BoxLayoutModel):
-            obj = BoxLayout(child, QT_BOX_LAYOUT_DIRECTION[child.direction])
-            fill_root_layout(obj, child, scene_widget)
-        if isinstance(child, GridLayoutModel):
-            obj = GridLayout(child)
-            fill_root_layout(obj, child, scene_widget)
-        if isinstance(child, LineModel):
-            obj = LineShape(child)
-            layout.add_shape(obj)
-        if isinstance(child, RectangleModel):
-            obj = RectangleShape(child)
-            layout.add_shape(obj)
-        if isinstance(child, PathModel):
-            obj = PathShape(child)
-            layout.add_shape(obj)
-        if isinstance(child, LabelModel):
-            obj = LabelWidget(child, scene_widget)
-            layout.add_widget(obj)
-        if isinstance(child, UnknownXMLDataModel):
-            obj = UnknownSvgWidget.create(child, parent=scene_widget)
-            if obj is not None:
-                layout.add_widget(obj)
-            # XXX: UnknownXMLDataModel has a list of children, which might
-            # include regular models. We're ignoring those children for now.
 
 
 class SceneView(QWidget):
