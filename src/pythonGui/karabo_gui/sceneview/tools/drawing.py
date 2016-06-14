05b53d5a1046c7afce9c3b4a93c57c6b02fdffd3
John Wiggins <john.wiggins@xfel.eu>
2016-06-14 11:39:15 +0200

    Implement a line drawing tool

--- src/pythonGui/karabo_gui/sceneview/tools/drawing.py
+++ src/pythonGui/karabo_gui/sceneview/tools/drawing.py
@@ -1,27 +1,18 @@
-from PyQt4.QtGui import QDialog
+from PyQt4.QtCore import QPoint, QLine, Qt
+from PyQt4.QtGui import QDialog, QPen
+from traits.api import Instance
 
 from karabo_gui.dialogs.textdialog import TextDialog
+from karabo_gui.scenemodel.api import LineModel
 from karabo_gui.sceneview.bases import BaseSceneTool
 
 
 class TextSceneTool(BaseSceneTool):
-    def draw(self, painter):
-        """ The method which is responsible for drawing this tool.
-        The tool for a SceneView will be drawn after everything else in
-        the view has been drawn.
-
-        This method is optional.
-        """
-
     def mouse_down(self, scene_view, event):
-        """ A callback which is fired whenever the user clicks in the
-        SceneView.
-        """
+        pass
 
     def mouse_move(self, scene_view, event):
-        """ A callback which is fired whenever the user moves the mouse
-        in the SceneView.
-        """
+        pass
 
     def mouse_up(self, scene_view, event):
         """ A callback which is fired whenever the user ends a mouse click
@@ -40,28 +31,40 @@ class TextSceneTool(BaseSceneTool):
 
 
 class LineSceneTool(BaseSceneTool):
-    def draw(self, painter):
-        """ The method which is responsible for drawing this tool.
-        The tool for a SceneView will be drawn after everything else in
-        the view has been drawn.
+    visible = True
+    line = Instance(QLine)
+    start_pos = Instance(QPoint)
 
-        This method is optional.
+    def draw(self, painter):
+        """ Draw the line
         """
+        if self.line is not None:
+            painter.setPen(QPen(Qt.black))
+            painter.drawLine(self.line)
 
     def mouse_down(self, scene_view, event):
         """ A callback which is fired whenever the user clicks in the
         SceneView.
         """
+        self.start_pos = event.pos()
+        self.line = QLine(self.start_pos, self.start_pos)
 
     def mouse_move(self, scene_view, event):
         """ A callback which is fired whenever the user moves the mouse
         in the SceneView.
         """
+        if event.buttons() and self.line is not None:
+            self.line.setPoints(self.start_pos, event.pos())
 
     def mouse_up(self, scene_view, event):
         """ A callback which is fired whenever the user ends a mouse click
         in the SceneView.
         """
+        if self.line is not None:
+            model = LineModel(x1=self.line.x1(), y1=self.line.y1(),
+                              x2=self.line.x2(), y2=self.line.y2())
+            scene_view.add_model(model)
+            scene_view.set_tool(None)
 
 
 class RectangleSceneTool(BaseSceneTool):
