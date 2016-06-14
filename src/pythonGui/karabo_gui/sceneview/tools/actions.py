97db59c6fca507c6888773f334ae9f3d8decfeae
Kerstin Weger <kerstin.weger@xfel.eu>
2016-06-14 13:45:33 +0200

    Add grouping actions

--- src/pythonGui/karabo_gui/sceneview/tools/actions.py
+++ src/pythonGui/karabo_gui/sceneview/tools/actions.py
@@ -15,3 +15,45 @@ class CreateToolAction(BaseSceneAction):
         """
         tool = self.tool_factory()
         scene_view.set_tool(tool)
+
+
+class GroupSceneAction(BaseSceneAction):
+    """ Group without layout action"""
+    def perform(self, scene_view):
+        """ Perform the grouping. """
+        print("GroupSceneAction")
+
+
+class BoxVSceneAction(BaseSceneAction):
+    """ Group vertically action"""
+    def perform(self, scene_view):
+        """ Perform the vertical grouping. """
+        print("BoxVSceneAction")
+
+
+class BoxHSceneAction(BaseSceneAction):
+    """ Group horizontally action"""
+    def perform(self, scene_view):
+        """ Perform the horizontal grouping. """
+        print("BoxHSceneAction")
+
+
+class GridSceneAction(BaseSceneAction):
+    """ Group in grid action"""
+    def perform(self, scene_view):
+        """ Perform the grouping in grid. """
+        print("GridSceneAction")
+
+
+class UngroupSceneAction(BaseSceneAction):
+    """ Ungroup action"""
+    def perform(self, scene_view):
+        """ Perform the ungrouping. """
+        print("UngroupSceneAction")
+
+
+class GroupEntireSceneAction(BaseSceneAction):
+    """ Group entirely"""
+    def perform(self, scene_view):
+        """ Perform entire window grouping. """
+        print("GroupEntireSceneAction")
