a4907eaef102e0ada0c38c9663e15724f94f1437
Kerstin Weger <kerstin.weger@xfel.eu>
2016-06-16 14:52:14 +0200

    Consider MR suggestions

--- src/pythonGui/karabo_gui/sceneview/tests/test_shapes.py
+++ src/pythonGui/karabo_gui/sceneview/tests/test_shapes.py
@@ -17,16 +17,16 @@ INKSCAPE_DIR = op.join(DATA_DIR, 'inkscape')
 
 
 class TestShapes(GuiTestCase):
-    '''Test the GUI scene view'''
+    """ Test the GUI shapes"""
 
     def setUp(self):
-        '''Create the view'''
+        """ Create the view"""
         super(TestShapes, self).setUp()
         self.view = SceneView()
         self.view._set_scene_model(SceneModel())
 
     def test_line_shape(self):
-        '''Test the line shape'''
+        """ Test the line shape"""
         self.assertEqual(len(self.view._scene_obj_cache), 0)
         model = LineModel(x1=0, y1=0, x2=1, y2=1)
         self.view.add_model(model)
@@ -34,7 +34,7 @@ class TestShapes(GuiTestCase):
         self.assertIn(model, self.view._scene_obj_cache)
 
     def test_rect_shape(self):
-        '''Test the rectangle shape'''
+        """ Test the rectangle shape"""
         self.assertEqual(len(self.view._scene_obj_cache), 0)
         model = RectangleModel(x=0, y=0, width=100, height=100)
         self.view.add_model(model)
@@ -42,10 +42,10 @@ class TestShapes(GuiTestCase):
         self.assertIn(model, self.view._scene_obj_cache)
 
     def test_path_shape(self):
-        '''Test the path shape'''
+        """ Test the path shape"""
         self.assertEqual(len(self.view._scene_obj_cache), 0)
         model = PathModel()
-        model.svg_data = ("M 111.42857,389.50504 285.71429,546.6479 " +
+        model.svg_data = ("M 111.42857,389.50504 285.71429,546.6479 "
                           "428.57143,398.07647")
         self.view.add_model(model)
         self.assertEqual(len(self.view._scene_obj_cache), 1)
--- src/pythonGui/karabo_gui/sceneview/tests/test_view.py
+++ src/pythonGui/karabo_gui/sceneview/tests/test_view.py
@@ -18,20 +18,18 @@ INKSCAPE_DIR = op.join(DATA_DIR, 'inkscape')
 
 
 class TestSceneView(GuiTestCase):
-    '''Test the GUI scene view'''
+    """ Test the GUI scene view"""
 
     def setUp(self):
-        '''Create the view'''
+        """ Create the view"""
         super(TestSceneView, self).setUp()
         self.view = SceneView()
 
-    def test_loading_karabo_svg(self):
-        '''Test the view loading a karabo SVG file'''
+    def _load_file(self, dir, file_name):
         self.assertIsNone(self.view.scene_model)
         self.assertIsInstance(self.view.layout, GroupLayout)
 
-        file_name = "all.svg"
-        self.view.load(op.join(DATA_DIR, file_name))
+        self.view.load(op.join(dir, file_name))
         self.assertEqual(self.view.title, op.basename(file_name))
         self.assertIsInstance(self.view.scene_model, SceneModel)
         self.assertEqual(max(self.view.scene_model.width, SCENE_MIN_WIDTH),
@@ -39,14 +37,10 @@ class TestSceneView(GuiTestCase):
         self.assertEqual(max(self.view.scene_model.height, SCENE_MIN_HEIGHT),
                          self.view.height())
 
+    def test_loading_karabo_svg(self):
+        """ Test the view loading a karabo SVG file"""
+        self._load_file(DATA_DIR, "all.svg")
+
     def test_loading_inkscape_svg(self):
-        '''Test the view loading a inkscape SVG file'''
-        self.assertIsNone(self.view.scene_model, None)
-        file_name = "shapes.svg"
-        self.view.load(op.join(INKSCAPE_DIR, "shapes.svg"))
-        self.assertEqual(self.view.title, op.basename(file_name))
-        self.assertIsInstance(self.view.scene_model, SceneModel)
-        self.assertEqual(max(self.view.scene_model.width, SCENE_MIN_WIDTH),
-                         self.view.width())
-        self.assertEqual(max(self.view.scene_model.height, SCENE_MIN_HEIGHT),
-                         self.view.height())
+        """ Test the view loading a inkscape SVG file"""
+        self._load_file(INKSCAPE_DIR, "shapes.svg")
