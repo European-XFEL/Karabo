228d008f1dc0c89502eaef1ca61025e3ebf73a6b
John Wiggins <john.wiggins@xfel.eu>
2016-06-16 15:43:56 +0200

    Add empty actions for Copy/Cut/Paste
    Also clean up the toolbar creation code

--- src/pythonGui/karabo_gui/sceneview/tools/api.py
+++ src/pythonGui/karabo_gui/sceneview/tools/api.py
@@ -1,6 +1,7 @@
 from .actions import (BoxVSceneAction, BoxHSceneAction, CreateToolAction,  # noqa
                       GroupEntireSceneAction, GridSceneAction,  # noqa
                       GroupSceneAction, UngroupSceneAction)  # noqa
+from .clipboard import SceneCopyAction, SceneCutAction, ScenePasteAction  # noqa
 from .drawing import (LineSceneTool, RectangleSceneTool, SceneLinkTool,  # noqa
                       TextSceneTool)  # noqa
 from .selection import SceneSelectionTool  # noqa
