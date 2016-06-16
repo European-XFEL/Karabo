9bae6257bbf3d28f75a1740d499243ca8684c736
John Wiggins <john.wiggins@xfel.eu>
2016-06-16 16:57:50 +0200

    Implement copy, cut, and paste actions.
    Wow... That was surprisingly easy.

--- src/pythonGui/karabo_gui/sceneview/tools/clipboard.py
+++ src/pythonGui/karabo_gui/sceneview/tools/clipboard.py
@@ -1,19 +1,101 @@
+from contextlib import contextmanager
+import os
+from tempfile import mkstemp
+
+from PyQt4.QtCore import QByteArray, QMimeData
+from PyQt4.QtGui import QApplication
+
+from karabo_gui.scenemodel.api import SceneModel, read_scene, write_scene
 from karabo_gui.sceneview.bases import BaseSceneAction
+
+MIME_TYPE = 'image/svg+xml'
+
+
+def _add_models_to_clipboard(models, rect):
+    """ Given a list of models, add their SVG representation to the clipboard
+    """
+    scene = SceneModel(children=models, width=rect.width(),
+                       height=rect.height())
+    xml = write_scene(scene)
+    mime_data = QMimeData()
+    mime_data.setData(MIME_TYPE, QByteArray(xml))
+    QApplication.clipboard().setMimeData(mime_data)
+
+
+def _read_models_from_clipboard():
+    """ Read SVG data from the clipboard into a SceneModel and return the child
+    models.
+    """
+    mime_data = QApplication.clipboard().mimeData()
+    if not mime_data.hasFormat(MIME_TYPE):
+        return []
+
+    byte_array = mime_data.data(MIME_TYPE)
+    with _temp_file(byte_array.data().decode('utf-8')) as fn:
+        scene = read_scene(fn)
+
+    return scene.children
+
+
+@contextmanager
+def _temp_file(contents):
+    """ Create a temporary file in a context manager. Returns the path of the
+    file.
+    """
+    fd, filename = mkstemp()
+    try:
+        with open(filename, 'w') as fp:
+            fp.write(contents)
+        yield filename
+    finally:
+        os.close(fd)
+        os.unlink(filename)
 
 
 class SceneCopyAction(BaseSceneAction):
+    """ Copy all selected objects from the scene view
+    """
     def perform(self, scene_view):
-        pass
+        selection_model = scene_view.selection_model
+        if len(selection_model) == 0:
+            return
+
+        models = [o.model for o in selection_model]
+        rect = selection_model.get_selection_bounds()
+        _add_models_to_clipboard(models, rect)
 
 
 class SceneCutAction(BaseSceneAction):
+    """ Remove all selected objects from the scene view.
+    """
     def perform(self, scene_view):
-        pass
+        selection_model = scene_view.selection_model
+        if len(selection_model) == 0:
+            return
+
+        models = [o.model for o in selection_model]
+        rect = selection_model.get_selection_bounds()
+        selection_model.clear_selection()
+
+        # Make sure they can be pasted
+        _add_models_to_clipboard(models, rect)
+
+        for model in models:
+            scene_view.remove_model(model)
+        scene_view.update()
 
 
 class ScenePasteAction(BaseSceneAction):
+    """ Paste the contents of the clipboard to the scene view.
+    """
     def perform(self, scene_view):
-        pass
+        pasted_models = _read_models_from_clipboard()
+        if len(pasted_models) == 0:
+            return
+
+        for model in pasted_models:
+            scene_view.add_model(model)
+        scene_view.update()
 
 
 class SceneSelectAllAction(BaseSceneAction):
