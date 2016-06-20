60e32c0e06d580eaa6595920342e744769f3fa90
Steffen Hauf <steffen.hauf@xfel.eu>
2016-06-20 16:23:12 +0200

    Moved function defintions to .cc, silences karathon warnings

--- src/karabo/util/AlarmConditions.cc
+++ src/karabo/util/AlarmConditions.cc
@@ -20,6 +20,37 @@ namespace karabo {
             KRB_ALARM_FROM_BASE(ALARM_VARIANCE_LOW, ALARM);
             KRB_ALARM_FROM_BASE(ALARM_VARIANCE_HIGH, ALARM);
             KRB_ALARM_BASE(INTERLOCK, 3);
+            
+            Pointer returnMostSignificant(const std::vector<Pointer> & v){
+                if(v.size() == 0) return NONE;
+                Pointer s = v[0];
+                for(std::vector<Pointer>::const_iterator i = v.begin(); i != v.end(); i++){
+                    s = (*i)->returnMoreSignificant(s);
+                    if (s->isSimilar(INTERLOCK)) break; // can't go higher than this
+                }
+                if (s->basetype) {
+                    return s->basetype;
+                } else {
+                    return s;
+                }
+               
+            }
+            
+            Pointer fromString(const std::string & condition){
+                static std::map<std::string, Pointer > alarmFactory;
+                if (alarmFactory.empty()){
+                    #define KRB_ALARM_INSERT(alarmType) alarmFactory.insert(std::pair<std::string, Pointer >(std::string(#alarmType), alarmType));
+                    KRB_ALARM_INSERT(NONE)
+                    KRB_ALARM_INSERT(WARN)
+                    KRB_ALARM_INSERT(WARN_HIGH)
+                    KRB_ALARM_INSERT(WARN_LOW)
+                    KRB_ALARM_INSERT(ALARM)
+                    KRB_ALARM_INSERT(ALARM_LOW)
+                    KRB_ALARM_INSERT(ALARM_HIGH)
+                    KRB_ALARM_INSERT(INTERLOCK)
+                } 
+                return alarmFactory[condition];
+            }
         }
     }
 }
