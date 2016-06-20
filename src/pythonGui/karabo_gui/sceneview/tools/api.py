4f95b6933636c513890a38da91487776e9a307bb
Kerstin Weger <kerstin.weger@xfel.eu>
2016-06-20 14:51:20 +0200

    Consider MR style suggestions

--- src/pythonGui/karabo_gui/sceneview/tools/api.py
+++ src/pythonGui/karabo_gui/sceneview/tools/api.py
@@ -1,7 +1,7 @@
 from .actions import (BoxVSceneAction, BoxHSceneAction, CreateToolAction,  # noqa
                       GroupEntireSceneAction, GridSceneAction,  # noqa
                       GroupSceneAction, UngroupSceneAction)  # noqa
-from .clipboard import (SceneCopyAction, SceneCutAction, SceneDeleteAction, # noqa
+from .clipboard import (SceneCopyAction, SceneCutAction, SceneDeleteAction,  # noqa
                         ScenePasteAction, SceneSelectAllAction)  # noqa
 from .drawing import (LineSceneTool, RectangleSceneTool, SceneLinkTool,  # noqa
                       TextSceneTool)  # noqa
