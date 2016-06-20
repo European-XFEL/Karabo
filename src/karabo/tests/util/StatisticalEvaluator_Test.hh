6dbd3be59936915be747f042ba20dc1b8cf80838
Steffen Hauf <steffen.hauf@xfel.eu>
2016-06-20 14:53:05 +0200

    Added performance tests for rolling Stats

---
+++ src/karabo/tests/util/StatisticalEvaluator_Test.hh
@@ -0,0 +1,44 @@
+/*
+ * File:   StatisticalEvaluator.hh
+ * Author: haufs
+ *
+ * Created on Jun 16, 2016, 9:10:00 AM
+ */
+
+#ifndef STATISTICALEVALUATOR_HH
+#define	STATISTICALEVALUATOR_HH
+
+#include <cppunit/extensions/HelperMacros.h>
+#include <karabo/util/StatisticalEvaluators.hh>
+
+class StatisticalEvaluator : public CPPUNIT_NS::TestFixture {
+    CPPUNIT_TEST_SUITE(StatisticalEvaluator);
+
+    CPPUNIT_TEST(testMean);
+    CPPUNIT_TEST(testSmallNumbers);
+    CPPUNIT_TEST(testLargeNumbers);
+    CPPUNIT_TEST(testVariance);
+    CPPUNIT_TEST(testPerformance);
+    CPPUNIT_TEST(testValidatorPerformance);
+
+    CPPUNIT_TEST_SUITE_END();
+
+public:
+    StatisticalEvaluator();
+    virtual ~StatisticalEvaluator();
+    void setUp();
+    void tearDown();
+
+private:
+    void testMean();
+    void testSmallNumbers();
+    void testLargeNumbers();
+    void testVariance();
+    void testPerformance();
+    void testValidatorPerformance();
+    
+   
+};
+
+#endif	/* STATISTICALEVALUATOR_HH */
+
