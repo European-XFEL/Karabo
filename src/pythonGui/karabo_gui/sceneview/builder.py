4aeb75e9104d443f0d131a88ee2b8ac4b50efabf
John Wiggins <john.wiggins@xfel.eu>
2016-06-22 14:47:29 +0200

    Create a SceneView container for simple widgets.
    Simple widgets are ones which have no additional model data aside from the
    widget model base class.
    
    Additionally, a new widgets subpackage was created.

--- src/pythonGui/karabo_gui/sceneview/builder.py
+++ src/pythonGui/karabo_gui/sceneview/builder.py
@@ -2,16 +2,23 @@ from PyQt4.QtCore import QRect
 
 from karabo_gui.scenemodel.api import (
     BoxLayoutModel, FixedLayoutModel, GridLayoutModel, LabelModel, LineModel,
-    PathModel, RectangleModel, SceneLinkModel, UnknownXMLDataModel
+    PathModel, RectangleModel, SceneLinkModel, UnknownXMLDataModel,
+    BitfieldModel, DisplayAlignedImageModel, DisplayCommandModel,
+    DisplayIconsetModel, DisplayImageModel, DisplayImageElementModel,
+    DisplayLabelModel, DisplayPlotModel, DoubleLineEditModel,
+    EditableListModel, EditableListElementModel, EditableSpinBoxModel,
+    HexadecimalModel, IntLineEditModel, KnobModel, SliderModel, XYPlotModel
 )
 from .const import QT_BOX_LAYOUT_DIRECTION
 from .layout.api import BoxLayout, GridLayout, GroupLayout
 from .shapes import LineShape, PathShape, RectangleShape
-from .simple_widgets import LabelWidget, SceneLinkWidget, UnknownSvgWidget
+from .widget.api import (
+    GenericWidgetContainer, LabelWidget, SceneLinkWidget, UnknownSvgWidget)
 
 _LAYOUT_CLASSES = (BoxLayout, GridLayout, GroupLayout)
 _SHAPE_CLASSES = (LineShape, PathShape, RectangleShape)
-_WIDGET_CLASSES = (LabelWidget, SceneLinkWidget, UnknownSvgWidget)
+_WIDGET_CLASSES = (GenericWidgetContainer, LabelWidget, SceneLinkWidget,
+                   UnknownSvgWidget)
 _SCENE_OBJ_FACTORIES = {
     FixedLayoutModel: lambda m, p: GroupLayout(m),
     BoxLayoutModel: lambda m, p: BoxLayout(m, QT_BOX_LAYOUT_DIRECTION[m.direction]),  # noqa
@@ -19,9 +26,26 @@ _SCENE_OBJ_FACTORIES = {
     LineModel: lambda m, p: LineShape(m),
     RectangleModel: lambda m, p: RectangleShape(m),
     PathModel: lambda m, p: PathShape(m),
-    LabelModel: lambda m, p: LabelWidget(m, p),
-    SceneLinkModel: lambda m, p: SceneLinkWidget(m, p),
+    LabelModel: LabelWidget,
+    SceneLinkModel: SceneLinkWidget,
     UnknownXMLDataModel: lambda m, p: UnknownSvgWidget.create(m, parent=p),
+    BitfieldModel: GenericWidgetContainer,
+    DisplayAlignedImageModel: GenericWidgetContainer,
+    DisplayCommandModel: GenericWidgetContainer,
+    DisplayIconsetModel: GenericWidgetContainer,
+    DisplayImageModel: GenericWidgetContainer,
+    DisplayImageElementModel: GenericWidgetContainer,
+    DisplayLabelModel: GenericWidgetContainer,
+    DisplayPlotModel: GenericWidgetContainer,
+    DoubleLineEditModel: GenericWidgetContainer,
+    EditableListModel: GenericWidgetContainer,
+    EditableListElementModel: GenericWidgetContainer,
+    EditableSpinBoxModel: GenericWidgetContainer,
+    HexadecimalModel: GenericWidgetContainer,
+    IntLineEditModel: GenericWidgetContainer,
+    KnobModel: GenericWidgetContainer,
+    SliderModel: GenericWidgetContainer,
+    XYPlotModel: GenericWidgetContainer,
 }
 
 
