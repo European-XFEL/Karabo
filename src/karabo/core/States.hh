9ecd747139332dc7016e78b57a46cfd28da14205
Gero Flucke <gero.flucke@xfel.eu>
2016-06-22 12:47:31 +0200

    State globals should just be the state "name", e.g. states::ERROR.
    For now just make the state pointer the global, i.e. states::ERROR is a
    const State::Pointer. Remove global 'state references' (e.g. states::g_ERROR).
    Also
    - introduce State::operator==(const State& s),
    - Remove macro CREATE_STATE.
    - No use of strings anymore to assemble the default trump list in
      StateSignifier::returnMostSignificant(..)

--- src/karabo/core/States.hh
+++ src/karabo/core/States.hh
@@ -5,11 +5,10 @@
  * Created on May 31, 2016, 10:54 AM
  */
 
-#ifndef STATES_HH
-#define	STATES_HH
+#ifndef KARABO_CORE_STATES_HH
+#define	KARABO_CORE_STATES_HH
 
 #include <iostream>
-#include <karabo/util/Factory.hh>
 #include "State.hh"
 
 namespace karabo {
@@ -19,149 +18,161 @@ namespace karabo {
 
         namespace states {
           
-
-#define KARABO_FSM_DECLARE_FIXED_STATE(X, Parent) \
-            struct X : public Parent {\
-                KARABO_CLASSINFO(X, #X, "1.0")\
-                X() {setStateName(#X); setParent(#Parent);}\
-                virtual ~X() {}\
+            // There is a special treatment of parent name "State"
+#define KARABO_DECLARE_FIXED_BASE_STATE(X)\
+            struct X ## _class : public State {\
+                KARABO_CLASSINFO(X ## _class, #X, "1.0") \
+                X ## _class() {setStateName(#X); setParent("State");}\
+                virtual ~X ## _class() {}\
             };\
-            extern State::Pointer X ## _p;\
-            extern const State& g_ ## X;
-        
+            extern const State::Pointer X;
+
+            // Factory key for class "X ## _class" is X
+#define KARABO_DECLARE_FIXED_STATE(X, Parent)\
+            struct X ## _class : public Parent ## _class {\
+                KARABO_CLASSINFO(X ## _class, #X, "1.0")\
+                X ## _class() {setStateName(#X); setParent(#Parent);}\
+                virtual ~X ## _class() {}\
+            };\
+            extern const State::Pointer X;
 
             /**
              * Base (Meta) states.  It corresponds the pic from
              * "Karabo concept", Chapter 9, "STATES AND STATUSES"
              */
 
-            KARABO_FSM_DECLARE_FIXED_STATE(UNKNOWN, State)
+            // The base states that have no parent:
 
-            KARABO_FSM_DECLARE_FIXED_STATE(KNOWN, State)
+            KARABO_DECLARE_FIXED_BASE_STATE(UNKNOWN)
 
-            KARABO_FSM_DECLARE_FIXED_STATE(INIT, State)
+            KARABO_DECLARE_FIXED_BASE_STATE(KNOWN)
 
-            KARABO_FSM_DECLARE_FIXED_STATE(DISABLED, KNOWN)
+            KARABO_DECLARE_FIXED_BASE_STATE(INIT)
 
-            KARABO_FSM_DECLARE_FIXED_STATE(ERROR, KNOWN)
+            // The derived states with their parents:
 
-            KARABO_FSM_DECLARE_FIXED_STATE(NORMAL, KNOWN)
+            KARABO_DECLARE_FIXED_STATE(DISABLED, KNOWN)
 
-            KARABO_FSM_DECLARE_FIXED_STATE(STATIC, NORMAL)
+            KARABO_DECLARE_FIXED_STATE(ERROR, KNOWN)
 
-            KARABO_FSM_DECLARE_FIXED_STATE(CHANGING, NORMAL)
+            KARABO_DECLARE_FIXED_STATE(NORMAL, KNOWN)
 
-            KARABO_FSM_DECLARE_FIXED_STATE(PASSIVE, STATIC)
+            KARABO_DECLARE_FIXED_STATE(STATIC, NORMAL)
 
-            KARABO_FSM_DECLARE_FIXED_STATE(ACTIVE, STATIC)
+            KARABO_DECLARE_FIXED_STATE(CHANGING, NORMAL)
 
-            KARABO_FSM_DECLARE_FIXED_STATE(DECREASING, CHANGING)
+            KARABO_DECLARE_FIXED_STATE(PASSIVE, STATIC)
 
-            KARABO_FSM_DECLARE_FIXED_STATE(INCREASING, CHANGING)
+            KARABO_DECLARE_FIXED_STATE(ACTIVE, STATIC)
+
+            KARABO_DECLARE_FIXED_STATE(DECREASING, CHANGING)
+
+            KARABO_DECLARE_FIXED_STATE(INCREASING, CHANGING)
 
             /**
              *
              */
 
-            KARABO_FSM_DECLARE_FIXED_STATE(INTERLOCKED, DISABLED)
+            KARABO_DECLARE_FIXED_STATE(INTERLOCKED, DISABLED)
 
-            KARABO_FSM_DECLARE_FIXED_STATE(COOLED, ACTIVE)
+            KARABO_DECLARE_FIXED_STATE(COOLED, ACTIVE)
 
-            KARABO_FSM_DECLARE_FIXED_STATE(HEATED, ACTIVE)
+            KARABO_DECLARE_FIXED_STATE(HEATED, ACTIVE)
 
-            KARABO_FSM_DECLARE_FIXED_STATE(EVACUATED, ACTIVE)
+            KARABO_DECLARE_FIXED_STATE(EVACUATED, ACTIVE)
 
-            KARABO_FSM_DECLARE_FIXED_STATE(CLOSED, ACTIVE)
+            KARABO_DECLARE_FIXED_STATE(CLOSED, ACTIVE)
 
-            KARABO_FSM_DECLARE_FIXED_STATE(ON, ACTIVE)
+            KARABO_DECLARE_FIXED_STATE(ON, ACTIVE)
 
-            KARABO_FSM_DECLARE_FIXED_STATE(EXTRACTED, ACTIVE)
+            KARABO_DECLARE_FIXED_STATE(EXTRACTED, ACTIVE)
 
-            KARABO_FSM_DECLARE_FIXED_STATE(STARTED, ACTIVE)
+            KARABO_DECLARE_FIXED_STATE(STARTED, ACTIVE)
 
-            KARABO_FSM_DECLARE_FIXED_STATE(LOCKED, ACTIVE)
+            KARABO_DECLARE_FIXED_STATE(LOCKED, ACTIVE)
 
-            KARABO_FSM_DECLARE_FIXED_STATE(ENGAGED, ACTIVE)
-
-
-            KARABO_FSM_DECLARE_FIXED_STATE(WARM, PASSIVE)
-
-            KARABO_FSM_DECLARE_FIXED_STATE(COLD, PASSIVE)
-
-            KARABO_FSM_DECLARE_FIXED_STATE(PRESSURIZED, PASSIVE)
-
-            KARABO_FSM_DECLARE_FIXED_STATE(OPENED, PASSIVE)
-
-            KARABO_FSM_DECLARE_FIXED_STATE(OFF, PASSIVE)
-
-            KARABO_FSM_DECLARE_FIXED_STATE(INSERTED, PASSIVE)
-
-            KARABO_FSM_DECLARE_FIXED_STATE(STOPPED, PASSIVE)
-
-            KARABO_FSM_DECLARE_FIXED_STATE(UNLOCKED, PASSIVE)
-
-            KARABO_FSM_DECLARE_FIXED_STATE(DISENGAGED, PASSIVE)
+            KARABO_DECLARE_FIXED_STATE(ENGAGED, ACTIVE)
 
 
-            KARABO_FSM_DECLARE_FIXED_STATE(ROTATING, CHANGING)
+            KARABO_DECLARE_FIXED_STATE(WARM, PASSIVE)
 
-            KARABO_FSM_DECLARE_FIXED_STATE(MOVING, CHANGING)
+            KARABO_DECLARE_FIXED_STATE(COLD, PASSIVE)
 
-            KARABO_FSM_DECLARE_FIXED_STATE(SWITCHING, CHANGING)
+            KARABO_DECLARE_FIXED_STATE(PRESSURIZED, PASSIVE)
 
+            KARABO_DECLARE_FIXED_STATE(OPENED, PASSIVE)
 
-            KARABO_FSM_DECLARE_FIXED_STATE(HEATING, INCREASING)
+            KARABO_DECLARE_FIXED_STATE(OFF, PASSIVE)
 
-            KARABO_FSM_DECLARE_FIXED_STATE(MOVING_RIGHT, INCREASING)
+            KARABO_DECLARE_FIXED_STATE(INSERTED, PASSIVE)
 
-            KARABO_FSM_DECLARE_FIXED_STATE(MOVING_UP, INCREASING)
+            KARABO_DECLARE_FIXED_STATE(STOPPED, PASSIVE)
 
-            KARABO_FSM_DECLARE_FIXED_STATE(MOVING_FORWARD, INCREASING)
+            KARABO_DECLARE_FIXED_STATE(UNLOCKED, PASSIVE)
 
-            KARABO_FSM_DECLARE_FIXED_STATE(ROTATING_CLK, INCREASING)
-
-            KARABO_FSM_DECLARE_FIXED_STATE(RAMPING_UP, INCREASING)
-
-            KARABO_FSM_DECLARE_FIXED_STATE(INSERTING, INCREASING)
-
-            KARABO_FSM_DECLARE_FIXED_STATE(STARTING, INCREASING)
-
-            KARABO_FSM_DECLARE_FIXED_STATE(FILLING, INCREASING)
-
-            KARABO_FSM_DECLARE_FIXED_STATE(ENGAGING, INCREASING)
-
-            KARABO_FSM_DECLARE_FIXED_STATE(SWITCHING_ON, INCREASING)
+            KARABO_DECLARE_FIXED_STATE(DISENGAGED, PASSIVE)
 
 
-            KARABO_FSM_DECLARE_FIXED_STATE(COOLING, DECREASING)
+            KARABO_DECLARE_FIXED_STATE(ROTATING, CHANGING)
 
-            KARABO_FSM_DECLARE_FIXED_STATE(MOVING_LEFT, DECREASING)
+            KARABO_DECLARE_FIXED_STATE(MOVING, CHANGING)
 
-            KARABO_FSM_DECLARE_FIXED_STATE(MOVING_DOWN, DECREASING)
+            KARABO_DECLARE_FIXED_STATE(SWITCHING, CHANGING)
 
-            KARABO_FSM_DECLARE_FIXED_STATE(MOVING_BACK, DECREASING)
 
-            KARABO_FSM_DECLARE_FIXED_STATE(ROTATING_CNTCLK, DECREASING)
+            KARABO_DECLARE_FIXED_STATE(HEATING, INCREASING)
 
-            KARABO_FSM_DECLARE_FIXED_STATE(RAMPING_DOWN, DECREASING)
+            KARABO_DECLARE_FIXED_STATE(MOVING_RIGHT, INCREASING)
 
-            KARABO_FSM_DECLARE_FIXED_STATE(EXTRACTING, DECREASING)
+            KARABO_DECLARE_FIXED_STATE(MOVING_UP, INCREASING)
 
-            KARABO_FSM_DECLARE_FIXED_STATE(STOPPING, DECREASING)
+            KARABO_DECLARE_FIXED_STATE(MOVING_FORWARD, INCREASING)
 
-            KARABO_FSM_DECLARE_FIXED_STATE(EMPTYING, DECREASING)
+            KARABO_DECLARE_FIXED_STATE(ROTATING_CLK, INCREASING)
 
-            KARABO_FSM_DECLARE_FIXED_STATE(DISENGAGING, DECREASING)
+            KARABO_DECLARE_FIXED_STATE(RAMPING_UP, INCREASING)
 
-            KARABO_FSM_DECLARE_FIXED_STATE(SWITCHING_OFF, DECREASING)
+            KARABO_DECLARE_FIXED_STATE(INSERTING, INCREASING)
 
-#undef KARABO_FSM_DECLARE_FIXED_STATE
+            KARABO_DECLARE_FIXED_STATE(STARTING, INCREASING)
+
+            KARABO_DECLARE_FIXED_STATE(FILLING, INCREASING)
+
+            KARABO_DECLARE_FIXED_STATE(ENGAGING, INCREASING)
+
+            KARABO_DECLARE_FIXED_STATE(SWITCHING_ON, INCREASING)
+
+
+            KARABO_DECLARE_FIXED_STATE(COOLING, DECREASING)
+
+            KARABO_DECLARE_FIXED_STATE(MOVING_LEFT, DECREASING)
+
+            KARABO_DECLARE_FIXED_STATE(MOVING_DOWN, DECREASING)
+
+            KARABO_DECLARE_FIXED_STATE(MOVING_BACK, DECREASING)
+
+            KARABO_DECLARE_FIXED_STATE(ROTATING_CNTCLK, DECREASING)
+
+            KARABO_DECLARE_FIXED_STATE(RAMPING_DOWN, DECREASING)
+
+            KARABO_DECLARE_FIXED_STATE(EXTRACTING, DECREASING)
+
+            KARABO_DECLARE_FIXED_STATE(STOPPING, DECREASING)
+
+            KARABO_DECLARE_FIXED_STATE(EMPTYING, DECREASING)
+
+            KARABO_DECLARE_FIXED_STATE(DISENGAGING, DECREASING)
+
+            KARABO_DECLARE_FIXED_STATE(SWITCHING_OFF, DECREASING)
+
+#undef KARABO_DECLARE_FIXED_BASE_STATE
+#undef KARABO_DECLARE_FIXED_STATE
             
             struct StateSignifier {
 
                 StateSignifier(const std::vector<karabo::core::State::Pointer>& trumpList = std::vector<karabo::core::State::Pointer>(),
-                        const karabo::core::State::Pointer& staticMoreSignificant = createState("PASSIVE"),
-                        const karabo::core::State::Pointer& changingMoreSignificant = createState("DECREASING"));
+                        const karabo::core::State::Pointer& staticMoreSignificant = states::PASSIVE,
+                        const karabo::core::State::Pointer& changingMoreSignificant = states::DECREASING);
 
                 State::Pointer returnMostSignificant(const std::vector<State::Pointer>& listOfStates);
 
@@ -190,11 +201,6 @@ namespace karabo {
     }
 }
 
-//  Use macro CREATE_STATE to check validity of the given state at compile time
-// State::Pointer cooling;
-// CREATE_STATE(cooling, Cooling)
 
-#define CREATE_STATE(x, X) { static struct X: public karabo::core::states::X {} s_ ## X; x = karabo::core::createState(#X); }
-
-#endif	/* STATES_HH */
+#endif	/* KARABO_CORE_STATES_HH */
 
