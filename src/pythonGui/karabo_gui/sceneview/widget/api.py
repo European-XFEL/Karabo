19c6c509fdee7f6cfaa36024ceb214c97b9cb872
John Wiggins <john.wiggins@xfel.eu>
2016-06-22 16:29:50 +0200

    Create a SceneView container for SingleBit widgets

--- src/pythonGui/karabo_gui/sceneview/widget/api.py
+++ src/pythonGui/karabo_gui/sceneview/widget/api.py
@@ -4,5 +4,6 @@ from .floatspinbox import FloatSpinBoxContainer  # noqa
 from .generic import DisplayEditableWidgetContainer, GenericWidgetContainer  # noqa
 from .icons import IconsContainer  # noqa
 from .simple import LabelWidget, SceneLinkWidget, UnknownSvgWidget  # noqa
+from .singlebit import SingleBitContainer  # noqa
 from .statecolor import DisplayStateColorContainer  # noqa
 from .vacuum import VacuumWidgetContainer  # noqa
