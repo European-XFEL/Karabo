f9b8f41198a481f453cc58aa45a2d27550c61fbb
Steffen Hauf <steffen.hauf@xfel.eu>
2016-06-23 12:08:14 +0200

    Added tests for validator

--- src/karabo/tests/util/AlarmCondition_Test.cc
+++ src/karabo/tests/util/AlarmCondition_Test.cc
@@ -11,6 +11,12 @@
 #include <string>
 #include <boost/aligned_storage.hpp>
 
+#include <karabo/util/TimePeriod.hh>
+#include <karabo/util/TimeDuration.hh>
+#include <karabo/util/TimeProfiler.hh>
+#include <karabo/util/Validator.hh>
+#include <karabo/util/Schema.hh>
+#include <karabo/util/SimpleElement.hh>
 
 CPPUNIT_TEST_SUITE_REGISTRATION(AlarmCondition_Test);
 
@@ -50,5 +56,144 @@ void AlarmCondition_Test::testSignificanceEvaluation() {
     ms = karabo::util::AlarmCondition::returnMostSignificant(v);
     CPPUNIT_ASSERT(ms.isSameCriticality(karabo::util::AlarmCondition::ALARM));
     
+}
+
+
+void AlarmCondition_Test::testValidation(){
+    
+    using namespace karabo::util;
+
+    TimeProfiler profiler("TestProfiler");
+    profiler.open();
+
+    
+    Validator val;
+    Schema schema;
+    
+    for(int i = 0; i < 50; i++){
+        std::ostringstream  key_s;
+        key_s<<i;
+        INT8_ELEMENT(schema).key("i8_"+key_s.str())
+                .readOnly().initialValue(0)
+                .commit();
+        UINT16_ELEMENT(schema).key("ui16_"+key_s.str())
+                .readOnly().initialValue(5+i)
+                .alarmLow(5+i).needsAcknowledging(true)
+                .alarmHigh(50+i).needsAcknowledging(true)
+                .commit();
+        FLOAT_ELEMENT(schema).key("f_"+key_s.str())
+                .readOnly().initialValue(5+i)
+                .warnLow(5+i).needsAcknowledging(true)
+                .warnHigh(50+i).needsAcknowledging(true)
+                .commit();
+        DOUBLE_ELEMENT(schema).key("d_"+key_s.str())
+                .readOnly().initialValue(5+i)
+                .alarmLow(5+i).needsAcknowledging(true)
+                .alarmHigh(50+i).needsAcknowledging(true)
+                .commit();
+        UINT64_ELEMENT(schema).key("ui64_"+key_s.str())
+                .readOnly().initialValue(15+i)
+                .warnLow(15+i).needsAcknowledging(true)
+                .warnHigh(50+i).needsAcknowledging(true)
+                .alarmLow(i).needsAcknowledging(true)
+                .alarmHigh(75+i).needsAcknowledging(true)
+                .commit();
+    }
+    
+    
+    
+    profiler.startPeriod("validator");
+    
+    Hash h_out;
+
+    for(size_t t = 0; t< 100; t++){
+        for(int i = 0; i < 50; i++){
+            std::ostringstream  key_s;
+            key_s<<i;
+            Hash h;
+            h.set("i8_"+key_s.str(), t);
+            h.set("ui16_"+key_s.str(), t);
+            h.set("f_"+key_s.str(), t);
+            h.set("d_"+key_s.str(), t);
+            h.set("ui64_"+key_s.str(), t);
+            std::pair<bool, std::string> r = val.validate(schema, h, h_out);
+            
+            const karabo::util::Hash & alarmParms = val.getParametersInWarnOrAlarm();
+            for(karabo::util::Hash::const_iterator it = alarmParms.begin(); it != alarmParms.end(); ++it){
+                const std::string & scope = it->getKey();
+                const std::string & type =  it->getValue<Hash>().get<std::string>("type");
+                
+                if(scope.find("i8_") != std::string::npos){
+                    CPPUNIT_ASSERT(false); // should not occur
+                    continue;
+                } 
+                
+                if(scope.find("ui16_") != std::string::npos){
+                    if(t < (5 + i)){
+                        CPPUNIT_ASSERT(type == AlarmCondition::ALARM_LOW.asString());
+                    } 
+                    else if(t > (50 + i)){
+                        CPPUNIT_ASSERT(type == AlarmCondition::ALARM_HIGH.asString());
+                    } 
+                    else {
+                        std::clog<<t<<" "<<i<<std::endl;
+                        std::clog<<scope<<it->getValue<Hash>()<<std::endl;
+                        CPPUNIT_ASSERT(false); // alarm should have been raised
+                    }
+                }
+                
+                if(scope.find("f_") != std::string::npos){
+                    if(t < (5 + i)){
+                        CPPUNIT_ASSERT(type == AlarmCondition::WARN_LOW.asString());
+                    } 
+                    else if(t > (50 + i)){
+                        CPPUNIT_ASSERT(type == AlarmCondition::WARN_HIGH.asString());
+                    } 
+                    else {
+                        CPPUNIT_ASSERT(false); // alarm should have been raised
+                    }
+                }
+                
+                if(scope.find("d_") != std::string::npos){
+                    if(t < (5 + i)){
+                        CPPUNIT_ASSERT(type == AlarmCondition::ALARM_LOW.asString());
+                    } 
+                    else if(t > (50 + i)){
+                        CPPUNIT_ASSERT(type == AlarmCondition::ALARM_HIGH.asString());
+                    } 
+                    else {
+                        CPPUNIT_ASSERT(false); // alarm should have been raised
+                    }
+                }
+                
+                if(scope.find("ui64_") != std::string::npos){
+                    if(t < i){
+                        CPPUNIT_ASSERT(type == AlarmCondition::ALARM_LOW.asString());
+                    } 
+                    else if(t > (75 + i)){
+                        CPPUNIT_ASSERT(type == AlarmCondition::ALARM_HIGH.asString());
+                    }
+                    if(t < (15 + i)){
+                        CPPUNIT_ASSERT(type == AlarmCondition::WARN_LOW.asString());
+                    } 
+                    else if(t > (50 + i)){
+                        CPPUNIT_ASSERT(type == AlarmCondition::WARN_HIGH.asString());
+                    } 
+                    else {
+                        CPPUNIT_ASSERT(false); // alarm should have been raised
+                    }
+                }
+            }
+            
+            
+        }
+    }
+    profiler.stopPeriod("validator");
+
+    //clog << p << endl;
+
+    profiler.close();
+
+    std::clog << "Validation time 250 properties: " << profiler.getPeriod("validator").getDuration()/100 << " [s/per validation]" << std::endl;
 }
 
