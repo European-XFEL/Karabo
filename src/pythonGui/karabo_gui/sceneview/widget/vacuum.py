b0876f0f7792d70c8f3d267297b19340da58a8cb
John Wiggins <john.wiggins@xfel.eu>
2016-06-27 12:06:10 +0200

    Fix the VacuumWidget loader

--- src/pythonGui/karabo_gui/sceneview/widget/vacuum.py
+++ src/pythonGui/karabo_gui/sceneview/widget/vacuum.py
@@ -1,23 +1,12 @@
-from karabo_gui.vacuumwidgets.cryocoolerwidget import CryoCoolerWidget
-from karabo_gui.vacuumwidgets.hydraulicvalvewidget import HydraulicValveWidget
-from karabo_gui.vacuumwidgets.maxigaugewidget import MaxiGaugeWidget
-from karabo_gui.vacuumwidgets.membranepumpwidget import MembranePumpWidget
-from karabo_gui.vacuumwidgets.motorwidget import MotorWidget
-from karabo_gui.vacuumwidgets.pressuregaugewidget import PressureGaugeWidget
-from karabo_gui.vacuumwidgets.pressureswitchwidget import PressureSwitchWidget
-from karabo_gui.vacuumwidgets.rightanglevalvewidget import RightAngleValveWidget  # noqa
-from karabo_gui.vacuumwidgets.shutoffvalvewidget import ShutOffValveWidget
-from karabo_gui.vacuumwidgets.temperatureprobeswidget import TemperatureProbeWidget  # noqa
-from karabo_gui.vacuumwidgets.turbopumpwidget import TurboPumpWidget
-from karabo_gui.vacuumwidgets.valvewidget import ValveWidget
+from karabo_gui.widget import Widget
 from .base import BaseWidgetContainer
 
-_VACUUM_CLASSES = (
-    CryoCoolerWidget, HydraulicValveWidget, MaxiGaugeWidget,
-    MembranePumpWidget, MotorWidget, PressureGaugeWidget, PressureSwitchWidget,
-    RightAngleValveWidget, ShutOffValveWidget, TemperatureProbeWidget,
-    TurboPumpWidget, ValveWidget)
-_VACUUM_WIDGETS = {c.__class__.__name__: c for c in _VACUUM_CLASSES}
+_VACUUM_CLASS_NAMES = (
+    'CryoCoolerWidget', 'HydraulicValveWidget', 'MaxiGaugeWidget',
+    'MembranePumpWidget', 'MotorWidget', 'PressureGaugeWidget',
+    'PressureSwitchWidget', 'RightAngleValveWidget', 'ShutOffValveWidget',
+    'TemperatureProbeWidget', 'TurboPumpWidget', 'ValveWidget')
+_VACUUM_WIDGETS = {n: Widget.widgets[n] for n in _VACUUM_CLASS_NAMES}
 
 
 class VacuumWidgetContainer(BaseWidgetContainer):
