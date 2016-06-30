fdcf384f45d31ea9f0e7ad81a2899aa3b315c06e
John Wiggins <john.wiggins@xfel.eu>
2016-06-30 16:24:58 +0200

    Highlight channels when the workflow tool can connect them
    Also, make the workflow connection tool always show a cross cursor.

--- src/pythonGui/karabo_gui/sceneview/workflow/api.py
+++ src/pythonGui/karabo_gui/sceneview/workflow/api.py
@@ -1,3 +1,4 @@
-from .const import CHANNEL_INPUT, CHANNEL_OUTPUT  # noqa
+from .const import (CHANNEL_INPUT, CHANNEL_OUTPUT, CHANNEL_DIAMETER,  # noqa
+                    CHANNEL_HEIGHT, CHANNEL_WIDTH)  # noqa
 from .model import SceneWorkflowModel, WorkflowChannelModel  # noqa
 from .render import WorkflowOverlay  # noqa
