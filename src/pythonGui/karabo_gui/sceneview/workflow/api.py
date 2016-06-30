f95f2bc88e04041856097ed0618303a9385931e2
John Wiggins <john.wiggins@xfel.eu>
2016-06-30 17:35:53 +0200

    Implement workflow connection disconnects

--- src/pythonGui/karabo_gui/sceneview/workflow/api.py
+++ src/pythonGui/karabo_gui/sceneview/workflow/api.py
@@ -1,4 +1,5 @@
 from .const import (CHANNEL_INPUT, CHANNEL_OUTPUT, CHANNEL_DIAMETER,  # noqa
                     CHANNEL_HEIGHT, CHANNEL_WIDTH)  # noqa
-from .model import SceneWorkflowModel, WorkflowChannelModel  # noqa
+from .model import (SceneWorkflowModel, WorkflowChannelModel,  # noqa
+                    WorkflowConnectionModel)  # noqa
 from .render import WorkflowOverlay  # noqa
