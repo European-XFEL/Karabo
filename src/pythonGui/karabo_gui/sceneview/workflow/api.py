306f87fc37fc0fcac30d7d6b0ca1a680d12c6c54
John Wiggins <john.wiggins@xfel.eu>
2016-07-05 10:18:18 +0200

    Improve drawing in the workflow connection tool
    Also, ask the user to confirm before removing connections

--- src/pythonGui/karabo_gui/sceneview/workflow/api.py
+++ src/pythonGui/karabo_gui/sceneview/workflow/api.py
@@ -3,3 +3,4 @@ from .const import (CHANNEL_INPUT, CHANNEL_OUTPUT, CHANNEL_DIAMETER,  # noqa
 from .model import (SceneWorkflowModel, WorkflowChannelModel,  # noqa
                     WorkflowConnectionModel)  # noqa
 from .render import WorkflowOverlay  # noqa
+from .utils import get_curve_points  # noqa
