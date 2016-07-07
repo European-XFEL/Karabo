166b82efa1b83609cd7bfe3215081321e0db2fcb
John Wiggins <john.wiggins@xfel.eu>
2016-07-07 11:28:35 +0200

    Add an option for packaging the extern build artifacts

--- extern/build.sh
+++ extern/build.sh
@@ -25,6 +25,32 @@ get_abs_path() {
     echo "$ABS_PATH"
 }
 
+safeRunCommand() {
+    typeset cmnd="$*"
+    typeset ret_code
+
+    echo cmnd=$cmnd
+    eval $cmnd
+    ret_code=$?
+    if [ $ret_code != 0 ]; then
+        printf "Error : [%d] when executing command: '$cmnd'" $ret_code
+        exit $ret_code
+    fi
+}
+
+usage() {
+    echo
+    echo "Build the Karabo Dependencies"
+    echo
+    echo "Usage: $0 [args] INSTALL_DIRECTORY ALL|NOGUI|PYTHON|GUI|<list>"
+    echo "  INSTALL_DIRECTORY : The directory where build artifacts are installed"
+    echo "  ALL|NOGUI|PYTHON|GUI|<list> : The type of build to perform, OR"
+    echo "                                <list> is a list of packages to build"
+    echo "Arguments:"
+    echo "  --package | -p : After building, make a tarball of the install directory"
+    echo
+}
+
 ##############################################################################
 
 DEPENDENCIES_BASE=( bzip2 libpng snappy jpeg tiff python3.4 lapack boost
@@ -42,13 +68,30 @@ DEPENDENCIES_DARWIN=( wheel pyqwt5 guidata guiqwt boost openmqc hdf5 h5py
 log4cpp cppunit parse snappy traits pint )
 
 ##############################################################################
+# Parse command line args (anything starting with '-')
+BUILD_PACKAGE="n"
+until [ ${1:0:1} != "-" ]; do
+    case $1 in
+        --package|-p)
+        BUILD_PACKAGE="y"
+        ;;
+        *)
+        echo "Unrecognized argument '$1'"
+        usage
+        exit 1
+        ;;
+    esac
+    shift
+done
+
+##############################################################################
+# (Following all the arguments preceded by '-' or '--')
 # $1  INSTALL_PREFIX  Installation prefix
 # $2+ WHAT            Which packages to build (symbolic or specific)
 EXTERN_DIR=$(dirname $0)
 INSTALL_PREFIX=$(get_abs_path $1)
+WHAT=${@:2}
 FORCE="n"
-shift
-WHAT=$@
 
 if [ "$(uname -s)" = "Darwin" ]; then
     # No choice on OS X
@@ -116,6 +159,16 @@ do
     fi
     echo $i >> $MARKER_PATH
 done
+
+# Package everything up
+if [ "$BUILD_PACKAGE" = "y" ]; then
+    VERSION=$(git rev-list --count HEAD)
+    INSTALL_DIR=$(basename $INSTALL_PREFIX)
+    pushd $(dirname $INSTALL_PREFIX)
+    safeRunCommand "tar -zcf $INSTALL_PREFIX-$VERSION.tar.gz $INSTALL_DIR"
+    popd
+fi
+
 
 popd
 echo "### All external dependencies successfully installed/already present. ###"
