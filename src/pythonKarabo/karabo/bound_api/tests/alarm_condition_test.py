17519816815a6eec3ab9eb9dc8ffd7a681800ac4
Steffen Hauf <steffen.hauf@xfel.eu>
2016-07-07 17:07:47 +0200

    Modifications as suggested by @flucke and @wigginsj

--- src/pythonKarabo/karabo/bound_api/tests/alarm_condition_test.py
+++ src/pythonKarabo/karabo/bound_api/tests/alarm_condition_test.py
@@ -11,48 +11,33 @@ from karabo.bound import AlarmCondition
 
 
 class  AlarmCondition_TestCase(unittest.TestCase):
-    def setUp(self):
-        pass
-
-    def tearDown(self):
-        pass
 
     def test_alarm_condition_roundtrip(self):
-        try:
-            condition = AlarmCondition.ALARM_HIGH
-            self.assertEqual(condition.asString(), "ALARM_HIGH")
-            condition = AlarmCondition.fromString(condition.asString())
-            self.assertEqual(condition.asString(), "ALARM_HIGH")
-            self.assertEqual(condition, AlarmCondition.ALARM_HIGH)
-        except Exception as e:
-            self.fail("test_alarm_condition_roundtrip exception: " + str(e))
+        condition = AlarmCondition.ALARM_HIGH
+        self.assertEqual(condition.asString(), "ALARM_HIGH")
+        condition = AlarmCondition.fromString(condition.asString())
+        self.assertEqual(condition.asString(), "ALARM_HIGH")
+        self.assertEqual(condition, AlarmCondition.ALARM_HIGH)
+
 
     def test_alarm_condition_no_other_conditions(self):
-        try:
-            failing_condition = AlarmCondition("SOME_NEW_NAME", rank = 0)
-        except Exception as e:
-            self.assertEqual("only predeclared conditions are allowed" in str(e), True)
+        self.assertRaises(AttributeError, AlarmCondition, "SOME_NEW_NAME", {"rank", 0})
 
     def test_alarm_condition_significance_evaluation(self):
-        try:
-            condition_list = [AlarmCondition.ALARM_LOW, AlarmCondition.WARN, AlarmCondition.INTERLOCK]
-            most_significant = AlarmCondition.returnMostSignificant(condition_list)
-            self.assertEqual(most_significant, AlarmCondition.INTERLOCK)
-            condition_list = condition_list[:-1]
-            most_significant = AlarmCondition.returnMostSignificant(condition_list)
-            self.assertEqual(most_significant, AlarmCondition.ALARM)
-        except Exception as e:
-            self.fail("test_alarm_condition_significance_evaluation exception: " + str(e))
+        condition_list = [AlarmCondition.ALARM_LOW, AlarmCondition.WARN, AlarmCondition.INTERLOCK]
+        most_significant = AlarmCondition.returnMostSignificant(condition_list)
+        self.assertEqual(most_significant, AlarmCondition.INTERLOCK)
+        condition_list = condition_list[:-1]
+        most_significant = AlarmCondition.returnMostSignificant(condition_list)
+        self.assertEqual(most_significant, AlarmCondition.ALARM)
+
 
 
     def test_alarm_condition_comparision(self):
-        try:
-            condition = AlarmCondition.ALARM_HIGH
-            self.assertEqual(condition.isSameCriticality(AlarmCondition.ALARM), True)
-            self.assertEqual(condition.isSameCriticality(AlarmCondition.ALARM_LOW), True)
-            self.assertEqual(condition.isSameCriticality(AlarmCondition.WARN), False)
-        except Exception as e:
-            self.fail("test_alarm_condition_comparision exception: " + str(e))
+        condition = AlarmCondition.ALARM_HIGH
+        self.assertEqual(condition.isSameCriticality(AlarmCondition.ALARM), True)
+        self.assertEqual(condition.isSameCriticality(AlarmCondition.ALARM_LOW), True)
+        self.assertEqual(condition.isSameCriticality(AlarmCondition.WARN), False)
 
 
 
