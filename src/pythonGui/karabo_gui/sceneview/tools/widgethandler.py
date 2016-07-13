b6012d43a4ce1aa13b7cab2a6a3bb6e4dcfcc128
Kerstin Weger <kerstin.weger@xfel.eu>
2016-07-13 14:21:00 +0200

    Move static methods from view to builder Consider MR suggestions Remove not needed widget classes from factory in builder

--- src/pythonGui/karabo_gui/sceneview/tools/widgethandler.py
+++ src/pythonGui/karabo_gui/sceneview/tools/widgethandler.py
@@ -9,26 +9,21 @@ from PyQt4.QtGui import QMenu
 
 from traits.api import Any, ABCHasStrictTraits
 
-from .const import WIDGET_FACTORIES
 from karabo_gui.scenemodel.api import BaseWidgetObjectData
 from karabo_gui.sceneview.widget.api import BaseWidgetContainer
 from karabo_gui.widget import DisplayWidget, EditableWidget
+
+from .const import WIDGET_FACTORIES
 
 
 class WidgetSceneHandler(ABCHasStrictTraits):
     widget = Any  # Current widget
 
-    def can_handle(self):
-        """ Check whether the handler can handle ``self.widget``. """
-        if isinstance(self.widget, BaseWidgetContainer):
-            return True
-        return False
-
     def handle(self, scene_view, event):
         """ A callback which is fired whenever the user requests a context menu
         in the SceneView.
         """
-        if not self.can_handle():
+        if not isinstance(self.widget, BaseWidgetContainer):
             return
 
         box = self.widget.boxes[0]
@@ -46,7 +41,7 @@ class WidgetSceneHandler(ABCHasStrictTraits):
                                          scene_view, klass))
         main_menu.exec_(event.globalPos())
 
-    @pyqtSlot(str)
+    @pyqtSlot(object, object)
     def _change_widget(self, scene_view, widget_class):
         """ """
         model_klass = WIDGET_FACTORIES[widget_class.__name__]
@@ -55,11 +50,9 @@ class WidgetSceneHandler(ABCHasStrictTraits):
         # Get old model traits
         traits = {}
         for key in BaseWidgetObjectData.class_editable_traits():
-            if key in ('height', 'width'):
-                # Ignore width and height for correct layout recalculation
-                traits[key] = 0
-                continue
             traits[key] = getattr(old_model, key)
 
+        # Ignore width and height for correct layout recalculation
+        traits['height'] = traits['width'] = 0
         new_model = model_klass(**traits)
         scene_view.replace_model(old_model, new_model)
