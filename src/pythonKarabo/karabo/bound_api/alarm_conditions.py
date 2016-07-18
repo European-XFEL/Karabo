41d05148c2a39fc663bbd31bf39c0756bea01c1c
Steffen Hauf <steffen.hauf@xfel.eu>
2016-07-18 16:55:14 +0200

    Changes as suggested by @flucke

--- src/pythonKarabo/karabo/bound_api/alarm_conditions.py
+++ src/pythonKarabo/karabo/bound_api/alarm_conditions.py
@@ -5,6 +5,12 @@ __date__ ="$June 30, 2016 2:17:13 PM$"
 from enum import Enum
 
 class AlarmCondition(Enum):
+    """
+    A class for unified alarm conditions. Alarms
+    are categorized by severity, which can either
+    be none, warn, alarm or interlock. Interlock
+    is the most severe condition.
+    """
     NONE = "none"
     WARN = "warn"
     WARN_LOW = "warnLow"
@@ -18,19 +24,38 @@ class AlarmCondition(Enum):
     ALARM_VARIANCE_HIGH = "alarmVarianceHigh"
     INTERLOCK = "interlock"
 
-    @classmethod
-    def parentList(cls):
-     return [cls.NONE, cls.WARN, cls.ALARM, cls.INTERLOCK] #also defines trumping order
+    @staticmethod
+    def criticalityList():
+        """
+        Returns the severity list, with severity increasing
+        from left to right
+        :return: an list with alarm severities
+        """
+
+        return [AlarmCondition.NONE, AlarmCondition.WARN,
+             AlarmCondition.ALARM, AlarmCondition.INTERLOCK]
 
 
-    def parent(self):
-        for p in AlarmCondition.parentList():
+
+    def criticalityLevel(self):
+        """
+        Returns the severity level of this alarm conditoon
+        :return:
+        """
+        for p in AlarmCondition.criticalityList():
             if p.value in self.value:
                 return p
         return self
 
     @staticmethod
     def returnMostSignificant(conditionList):
+        """
+        Returns the most severe/significant condition in
+        the given list of conditions
+        :param conditionList: an iterable containing alarm
+        conditions
+        :return: The most severe condition in the list
+        """
         if len(conditionList) == 0:
             return AlarmCondition.NONE
 
@@ -38,22 +63,48 @@ class AlarmCondition(Enum):
             s = conditionList[0]
             for c in conditionList:
                 s = s.returnMoreSignificant(c)
-            return s.parent()
+            return s.criticalityLevel()
 
 
     def returnMoreSignificant(self, other):
-        return self if AlarmCondition.parentList().index(self.parent()) > AlarmCondition.parentList().index(other.parent()) else other
+        """
+        Return the more signifcant of this and the other condition
+        :param other: The condition to compare against
+        :return:
+        """
+        return self if self.criticalityList().index(self.criticalityLevel()) \
+                 > self.criticalityList().index(other.criticalityLevel()) else other
 
-    @classmethod
-    def fromString(cls, name):
-         return AlarmCondition(name)
+    @staticmethod
+    def fromString(name):
+        """
+        Return an alarm condition from its stringified representation.
+        :param name:
+        :return:
+        """
+        return AlarmCondition(name)
 
 
     def asString(self):
+        """
+        Return the string representation of the alarm condition
+        :return:
+        """
         return self.value
 
     def asBaseString(self):
+        """
+        Return the severity level, e.g. base of the alarmcondtions
+        :return:
+        """
         return self.parent().value
 
     def isSameCriticality(self, other):
-        return AlarmCondition.parentList().index(self.parent()) == AlarmCondition.parentList().index(other.parent())
+        """
+        Returns if this alarm condition is of same criticality/severity
+        as other
+        :param other: the alarm condition to compare against
+        :return:
+        """
+        return self.criticalityList().index(self.criticalityLevel()) \
+               == self.criticalityList().index(other.criticalityLevel())
