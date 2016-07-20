434571b5bbcd1e27a3c629d247861f2a87fd0e7b
Burkhard Heisen <burkhard.heisen@xfel.eu>
2016-07-20 21:21:44 +0200

    WIP: Everything is experimental yet. This commit serves mainly for backup and home-office purposes.

---
+++ src/karabo/tests/net/JmsConnection_Test.hh
@@ -0,0 +1,34 @@
+/* 
+ * File:   JmsConnection_Test.hh
+ * Author: heisenb
+ *
+ * Created on July 15, 2016, 5:05 PM
+ */
+
+#ifndef JMSCONNECTION_TEST_HH
+#define	JMSCONNECTION_TEST_HH
+
+#include <cppunit/extensions/HelperMacros.h>
+
+class JmsConnection_Test : public CPPUNIT_NS::TestFixture {
+
+
+    CPPUNIT_TEST_SUITE(JmsConnection_Test);
+    CPPUNIT_TEST(testConnect);
+    CPPUNIT_TEST(testChannel);
+        CPPUNIT_TEST_SUITE_END();
+
+public:
+    JmsConnection_Test();
+    virtual ~JmsConnection_Test();
+
+private:
+
+    void testConnect();
+
+    void testChannel();
+
+};
+
+#endif	/* JMSCONNECTION_TEST_HH */
+
