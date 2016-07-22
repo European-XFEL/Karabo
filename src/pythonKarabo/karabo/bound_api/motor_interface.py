167d266197f745bf06f16843b5fe0da4ddafc7b2
Steffen Hauf <steffen.hauf@xfel.eu>
2016-07-22 17:15:31 +0200

    Adaption to unified states

--- src/pythonKarabo/karabo/bound_api/motor_interface.py
+++ src/pythonKarabo/karabo/bound_api/motor_interface.py
@@ -4,6 +4,7 @@ from karathon import (OVERWRITE_ELEMENT, SLOT_ELEMENT, FLOAT_ELEMENT, INT32_ELEM
                       Schema, SignalSlotable, Unit, MetricPrefix)
 from .decorators import (KARABO_CLASSINFO, KARABO_CONFIGURATION_BASE_CLASS)
 from .no_fsm import NoFsm
+from karabo.common.states import State
 
 __author__ = "esenov"
 __date__ = "$Apr 22, 2015 4:14:47 PM$"
@@ -16,80 +17,78 @@ class MotorInterface(NoFsm, metaclass=ABCMeta):
     @staticmethod
     def expectedParameters(expected):
         (
+            
          OVERWRITE_ELEMENT(expected).key("state")
-                .setNewOptions("Initializing, HardwareError,Safe,Override,Off,"
-                               "Stopped,Idle,Homing,Moving")
-                .setNewDefaultValue("Initializing")
-                .commit(),
+                        .setNewOptions(State.INIT, State.ERROR, State.DISABLED, State.OFF, State.STOPPED, State.STATIC, State.HOMING, State.MOVING)
+                        .setNewDefaultValue(State.INIT)
+                        .commit(),
 
-         SLOT_ELEMENT(expected).key("resetHardware")
+        SLOT_ELEMENT(expected).key("resetHardware")
                 .description("Resets the hardware")
                 .displayedName("Reset hardware")
-                .allowedStates("HardwareError")
+                .allowedStates(State.ERROR)
                 .commit(),
 
-         SLOT_ELEMENT(expected).key("safe")
-                .description("Brings device into a safe state (as defined on "
-                             "h/w)")
+        SLOT_ELEMENT(expected).key("safe")
+                .description("Brings device into a safe operation mode (as defined on h/w)")
                 .displayedName("Safe")
                 .commit(),
 
-         SLOT_ELEMENT(expected).key("normal")
+        SLOT_ELEMENT(expected).key("normal")
                 .displayedName("Normal")
-                .description("Brings device into normal mode")
+                .description("Brings device into normal operation mode")
                 .expertAccess()
                 .commit(),
 
-         SLOT_ELEMENT(expected).key("override")
+        SLOT_ELEMENT(expected).key("override")
                 .displayedName("Override")
-                .description("Brings device into override mode (be careful, "
-                             "hardware may be broken)")
-                .expertAccess()
+                .description("Brings device into override operation mode (be careful, hardware may be broken)")
+                .adminAccess()
                 .commit(),
 
-         SLOT_ELEMENT(expected).key("off")
+        SLOT_ELEMENT(expected).key("off")
                 .displayedName("Off")
                 .description("Instructs device to switch off")
-                .allowedStates("Override Stopped Idle Homing")
+                .allowedStates(State.DISABLED, State.STOPPED, State.STATIC, State.CHANGING)
                 .commit(),
 
-         SLOT_ELEMENT(expected).key("on")
+        SLOT_ELEMENT(expected).key("on")
                 .displayedName("On")
                 .description("Instructs device to switch on")
-                .allowedStates("Override Off")
+                .allowedStates(State.DISABLED, State.OFF)
                 .commit(),
 
-         SLOT_ELEMENT(expected).key("stop")
+        SLOT_ELEMENT(expected).key("stop")
                 .displayedName("Stop")
                 .description("Instructs the device to switch on and stopped")
-                .allowedStates("Override Idle Moving Homing")
+                .allowedStates(State.DISABLED, State.STATIC, State.CHANGING)
                 .commit(),
 
-         SLOT_ELEMENT(expected).key("home")
+        SLOT_ELEMENT(expected).key("home")
                 .displayedName("Home")
                 .description("Find home position")
-                .allowedStates("Override Stopped")
+                .allowedStates(State.DISABLED, State.STOPPED)
                 .commit(),
 
-         SLOT_ELEMENT(expected).key("move")
+        SLOT_ELEMENT(expected).key("move")
                 .displayedName("Move")
                 .description("Move position")
-                .allowedStates("Override Stopped")
+                .allowedStates(State.DISABLED, State.STOPPED)
                 .commit(),
 
-         SLOT_ELEMENT(expected).key("stepUp")
+        SLOT_ELEMENT(expected).key("stepUp")
                 .displayedName("Step up")
                 .description("Step up")
-                .allowedStates("Override Idle Stopped")
+                .allowedStates(State.DISABLED, State.STATIC, State.STOPPED)
                 .commit(),
 
-         SLOT_ELEMENT(expected).key("stepDown")
+        SLOT_ELEMENT(expected).key("stepDown")
                 .displayedName("Step down")
                 .description("Step down")
-                .allowedStates("Override Idle Stopped")
+                .allowedStates(State.DISABLED, State.STATIC, State.STOPPED)
                 .commit(),
 
-         FLOAT_ELEMENT(expected).key("encoderPosition")
+        FLOAT_ELEMENT(expected).key("encoderPosition")
                 .description("Encoder position")
                 .displayedName("Encoder position")
                 .unit(Unit.METER)
@@ -97,34 +96,32 @@ class MotorInterface(NoFsm, metaclass=ABCMeta):
                 .readOnly()
                 .commit(),
 
-         FLOAT_ELEMENT(expected).key("stepCounterPosition")
+        FLOAT_ELEMENT(expected).key("stepCounterPosition")
                 .displayedName("Stepcounter position")
-                .description("The step counter position describes the motor "
-                             "position calculated from counter steps (instead "
-                             "of encoder values), and is only valid if "
-                             "connected to external encoder")
+                .description("The step counter position describes the motor position calculated from counter steps (instead of encoder values), and is only valid if connected to external encoder")
                 .expertAccess()
                 .readOnly()
                 .commit(),
 
-         FLOAT_ELEMENT(expected).key("targetPosition")
+        FLOAT_ELEMENT(expected).key("targetPosition")
                 .description("Target position in position mode")
                 .displayedName("Target position")
                 .unit(Unit.METER)
                 .metricPrefix(MetricPrefix.MILLI)
                 .assignmentOptional().noDefaultValue()
                 .reconfigurable()
-                .allowedStates("Override Stopped Off Idle Moving")
+                .allowedStates(State.DISABLED, State.STOPPED, State.OFF, State.STATIC, State.MOVING)
                 .commit(),
 
-         INT32_ELEMENT(expected).key("targetVelocity")
+        INT32_ELEMENT(expected).key("targetVelocity")
                 .description("Target velocity in velocity mode")
                 .displayedName("Target velocity")
                 .assignmentOptional().noDefaultValue()
                 .reconfigurable()
-                .allowedStates("Override Stopped Off Idle Moving")
+                .allowedStates(State.DISABLED, State.STOPPED, State.OFF, State.STATIC, State.MOVING)
                 .expertAccess()
                 .commit(),
+         
         )
 
     def __init__(self, configuration):
