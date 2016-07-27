ea15ab97ffbb691eeef9a5234b6fae3c757cd74f
Martin Teichmann <martin.teichmann@xfel.eu>
2016-07-27 13:53:26 +0200

    remove stone-age constants

--- src/pythonKarabo/karabo/middlelayer_api/enums.py
+++ src/pythonKarabo/karabo/middlelayer_api/enums.py
@@ -1,12 +1,6 @@
 from enum import Enum, IntEnum
 from functools import total_ordering
 
-ChannelSpaceType = (
-    "u_8_1", "s_8_1", "u_10_2", "s_10_2", "u_12_2", "s_12_2", "u_12_1p5",
-    "s_12_1p5", "u_16_2", "s_16_2", "f_16_2", "u_32_4", "s_32_4", "f_32_4",
-    "u_64_8", "s_64_8", "f_64_8", "u_16_1", "s_16_1", "u_32_1", "s_32_1",
-    "f_32_1", "u_64_1", "s_64_1", "f_64_1")
-
 
 @total_ordering
 class AccessLevel(Enum):
