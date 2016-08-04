a91aaabf2eedf384f4995d14a2fae3e34a825f24
Burkhard Heisen <burkhard.heisen@xfel.eu>
2016-08-04 16:46:23 +0200

    Implemented many of the useful MR comments. Refurbished test to use a Hash testing all types (maybe some are missing still).

--- src/karabo/tests/io/HashBinarySerializer_Test.cc
+++ src/karabo/tests/io/HashBinarySerializer_Test.cc
@@ -26,41 +26,77 @@ HashBinarySerializer_Test::~HashBinarySerializer_Test() {
 
 
 void HashBinarySerializer_Test::setUp() {
+    Hash h;
+    h.set<bool>("bool", true);
+    h.set<char>("char", 'c');
+    h.set<unsigned char>("uint8", 8);
+    h.set<signed char>("int8", -8);
+    h.set<unsigned short>("uint16", 16);
+    h.set<short>("int16", -16);
+    h.set<unsigned int>("uint32", 32);
+    h.set<int>("int32", -32);
+    h.set<unsigned long long>("int64", 64);
+    h.set<long long>("uint64", -64);
+    h.set<float>("float", 3.141);
+    h.set<double>("double", 3.14159265359);
+    h.set<complex<float> >("cf", complex<float>(1., 1.));
+    h.set<complex<double> >("cd", complex<float>(2., 2.));
+    h.set<string>("str", "Hello Karabo");
 
-    Hash rooted("a.b.c", 1, "a.b.d", vector<int>(5, 1), "a.b.e", vector<Hash > (2, Hash("a", 1)), "a.d", std::complex<double>(1.2, 4.2));
-    rooted.setAttribute("a", "a1", true);
-    rooted.setAttribute("a", "a2", 3.4);
-    rooted.setAttribute("a.b", "b1", "3");
-    rooted.setAttribute("a.b.c", "c1", 2);
-    rooted.setAttribute("a.b.c", "c2", vector<string > (3, "bla"));
-    m_rootedHash = rooted;
+    h.setAttribute<bool>("bool", "bool", true);
+    h.setAttribute<char>("char", "char", 'c');
+    h.setAttribute<unsigned char>("uint8", "uint8", 8);
+    h.setAttribute<signed char>("int8", "int8", -8);
+    h.setAttribute<unsigned short>("uint16", "uint16", 16);
+    h.setAttribute<short>("int16", "int16", -16);
+    h.setAttribute<unsigned int>("uint32", "uint32", 32);
+    h.setAttribute<int>("int32", "int32", -32);
+    h.setAttribute<unsigned long long>("uint64", "uint64", 64);
+    h.setAttribute<long long>("int64", "int64", -64);
+    h.setAttribute<float>("float", "float", 3.141);
+    h.setAttribute<double>("double", "double", 3.14159265359);
+    h.setAttribute<complex<float> >("cf", "cf", complex<float>(1., 1.));
+    h.setAttribute<complex<double> >("cd", "cd", complex<float>(2., 2.));
+    h.setAttribute<string>("str", "str", "Hello Karabo");
 
-    m_data = std::vector<double>(20 * 1024 * 1024, 1.0);
-    Hash big("a.b", std::pair<const double*, size_t>(&m_data[0], m_data.size()));
+    h.set < vector<bool> >("vec_bool", vector<bool>(1000, true));
+    h.set<vector<char> >("vec_char", vector<char>(1000, 'c'));
+    h.set<vector<unsigned char> >("vec_uint8", vector<unsigned char>(1000, 8));
+    h.set < vector<signed char> >("vec_int8", vector <signed char>(1000, -8));
+    h.set<vector<unsigned short> >("vec_uint16", vector<unsigned short>(1000, 16));
+    h.set<vector<short> >("vec_int16", vector<short>(1000, -16));
+    h.set<vector<unsigned int> >("vec_uint32", vector<unsigned int>(1000, 32));
+    h.set<vector<int> >("vec_int32", vector <int>(1000, -32));
+    h.set<vector<unsigned long long> >("vec_uint64", vector<unsigned long long>(1000, 64));
+    h.set<vector<long long> >("vec_int64", vector <long long>(1000, -64));
+    h.set<vector<float> >("vec_float", vector<float>(1000, 3.141));
+    h.set<vector<double> >("vec_double", vector<double>(1000, 3.14159265359));
+    h.set<vector<complex<float> > >("vec_cf", vector<complex<float> >(1000, complex<float>(1., 2.)));
+    h.set<vector<complex<double> > >("vec_cd", vector < complex<double> >(1000, complex<double>(3., 4.)));
+    h.set<vector<string> > ("vec_str", vector<string>(1000, "Hello Karabo"));
 
-    vector<Hash>& tmp = big.bindReference<vector<Hash> >("a.c");
-    tmp.resize(10);
-    for (size_t i = 0; i < tmp.size(); ++i) {
-        tmp[i] = m_rootedHash;
-    }
-    m_bigHash = big;
+    h.setAttribute < vector<bool> >("vec_bool", "vec_bool", vector<bool>(1000, true));
+    h.setAttribute<vector<char> >("vec_char", "vec_char", vector<char>(1000, 'c'));
+    h.setAttribute<vector<unsigned char> >("vec_uint8", "vec_uint8", vector<unsigned char>(1000, 8));
+    h.setAttribute < vector<signed char> >("vec_int8", "vec_int8", vector <signed char>(1000, -8));
+    h.setAttribute<vector<unsigned short> >("vec_uint16", "vec_uint16", vector<unsigned short>(1000, 16));
+    h.setAttribute<vector<short> >("vec_int16", "vec_int16", vector<short>(1000, -16));
+    h.setAttribute<vector<unsigned int> >("vec_uint32", "vec_uint32", vector<unsigned int>(1000, 32));
+    h.setAttribute<vector<int> >("vec_int32", "vec_int32", vector <int>(1000, -32));
+    h.setAttribute<vector<unsigned long long> >("vec_uint64", "vec_uint64", vector<unsigned long long>(1000, 64));
+    h.setAttribute<vector<long long> >("vec_int64", "vec_int64", vector <long long>(1000, -64));
+    h.setAttribute<vector<float> >("vec_float", "vec_float", vector<float>(1000, 3.141));
+    h.setAttribute<vector<double> >("vec_double", "vec_double", vector<double>(1000, 3.14159265359));
+    h.setAttribute<vector<complex<float> > >("vec_cf", "vec_cf", vector<complex<float> >(1000, complex<float>(1., 1.)));
+    h.setAttribute<vector<complex<double> > >("vec_cd", "vec_cd", vector < complex<double> >(1000, complex< float>(2., 2.)));
+    h.setAttribute<vector<string> > ("vec_str", "vec_str", vector<string>(1000, "Hello Karabo"));
 
+    m_hash.set<Hash>("hash", h);
+    m_hash.set<vector<Hash> >("vec_hash", vector<Hash >(100, h));
 
-    Hash unrooted("a.b.c", 1, "b.c", 2.0, "c", 3.f, "d.e", "4", "e.f.g.h", std::vector<unsigned long long > (5, 5), "F.f.f.f.f", Hash("x.y.z", 99));
-    unrooted.setAttribute("F.f.f", "attr1", true);
-    m_unrootedHash = unrooted;
-
-    for (size_t i = 0; i < 10; ++i) {
-        m_vectorOfHashes.push_back(m_rootedHash);
-    }
-
-    Hash::Pointer hp1(new Hash("a", std::vector<unsigned long long > (5, 5)));
-    Hash::Pointer hp2(new Hash("b", std::vector<float > (8, 1.0)));
-    m_sharedPtrHash = Hash("hp1", hp1, "hp2", hp2, "vec", vector<Hash::Pointer>(10, hp2));
-}
-
-
-void HashBinarySerializer_Test::tearDown() {
+    Schema s;
+    s.setParameterHash(h);
+    m_hash.set<Schema>("schema", s);
 }
 
 
@@ -68,119 +104,32 @@ void HashBinarySerializer_Test::testSerialization() {
 
     BinarySerializer<Hash>::Pointer p = BinarySerializer<Hash>::create("Bin");
 
-    {
-        Schema s = TextSerializer<Hash>::getSchema("Xml");
-        Hash schemaIncluded("a1", 3.2, "a2", s);
-        vector<char> archive;
-        p->save(schemaIncluded, archive);
-        Hash fresh;
-        p->load(fresh, archive);
-        CPPUNIT_ASSERT(karabo::util::similar(s.getParameterHash(), fresh.get<Schema>("a2").getParameterHash()));
+    vector<char> archive1;
+    vector<char> archive2;
+
+    boost::posix_time::ptime tick = boost::posix_time::microsec_clock::local_time();
+
+    for (int i = 0; i < 10; ++i) {
+        p->save(m_hash, archive1);
     }
 
-    {
-        vector<char> archive1;
-        vector<char> archive2;
+    boost::posix_time::time_duration diff = boost::posix_time::microsec_clock::local_time() - tick;
+    float ave = diff.total_milliseconds() / 10.0;
+    clog << "\n Average serialization time: " << ave << " ms for Hash of size: " << archive1.size() / 10.e6 << " MB" << endl;
 
-        p->save(m_rootedHash, archive1);
-
-        //cout << "\n\n" << archive1 << endl;
-
-        Hash h;
-        p->load(h, archive1);
-        //cout << endl << h << endl;
-
-        CPPUNIT_ASSERT(karabo::util::similar(m_rootedHash, h) == true);
-
-        p->save(h, archive2);
-
-        //cout << "\n\n" << archive2 << endl;
-
-        CPPUNIT_ASSERT(string(archive1[0], archive1.size()) == string(archive2[0], archive2.size()));
+    Hash hash;
+    tick = boost::posix_time::microsec_clock::local_time();
+    for (int i = 0; i < 10; ++i) {
+        p->load(hash, archive1);
     }
+    diff = boost::posix_time::microsec_clock::local_time() - tick;
+    ave = diff.total_milliseconds() / 10.0;
+    clog << "\n Average de-serialization time: " << ave << " ms" << endl;
 
-    {
-        vector<char> archive1;
-        vector<char> archive2;
+    CPPUNIT_ASSERT(karabo::util::similar(hash, m_hash));
 
-        boost::posix_time::ptime tick = boost::posix_time::microsec_clock::local_time();
+    p->save(hash, archive2);
 
-        for (int i = 0; i < 10; ++i) {
-            p->save(m_bigHash, archive1);
-        }
+    CPPUNIT_ASSERT(string(archive1[0], archive1.size()) == string(archive2[0], archive2.size()));
 
-        boost::posix_time::time_duration diff = boost::posix_time::microsec_clock::local_time() - tick;
-        float ave = diff.total_milliseconds() / 10.0;
-        clog << "\n Average serialization time: " << ave << " ms for Hash of size: " << archive1.size() / 10.e6 << " MB" << endl;
-
-        Hash h;
-
-        tick = boost::posix_time::microsec_clock::local_time();
-
-        for (int i = 0; i < 10; ++i) {
-            p->load(h, archive1);
-        }
-        diff = boost::posix_time::microsec_clock::local_time() - tick;
-        ave = diff.total_milliseconds() / 10.0;
-        clog << "\n Average de-serialization time: " << ave << " ms" << endl;
-
-        // This is commented as the m_bigHash has value type pair<const T*, size_t>,
-        // whilst the serialized h has a vector<T>, this is intended but will break the similar function
-        //CPPUNIT_ASSERT(karabo::util::similar(m_bigHash, h) == true);
-
-        p->save(h, archive2);
-
-        CPPUNIT_ASSERT(string(archive1[0], archive1.size()) == string(archive2[0], archive2.size()));
-
-    }
-
-    {
-        vector<char> archive1;
-        vector<char> archive2;
-
-        p->save(m_unrootedHash, archive1);
-
-        //cout << "\n\nXML string size: " << archive.size() / 1024 / 1024 << " MB" << endl;
-
-        Hash h;
-        p->load(h, archive1);
-
-        CPPUNIT_ASSERT(karabo::util::similar(m_unrootedHash, h) == true);
-
-        p->save(h, archive2);
-        CPPUNIT_ASSERT(string(archive1[0], archive1.size()) == string(archive2[0], archive2.size()));
-
-    }
-
-    {
-        vector<char> archive1;
-        vector<char> archive2;
-        p->save(m_vectorOfHashes, archive1);
-        vector<Hash> hs;
-        p->load(hs, archive1);
-        for (size_t i = 0; i < 10; ++i) {
-            CPPUNIT_ASSERT(karabo::util::similar(m_rootedHash, hs[i]) == true);
-        }
-
-        p->save(hs, archive2);
-        CPPUNIT_ASSERT(archive1 == archive2);
-    }
-
-    {
-
-        p = BinarySerializer<Hash>::create("Bin", Hash("nodesAsSharedPtr", true));
-        vector<char> archive1;
-        vector<char> archive2;
-
-        p->save(m_sharedPtrHash, archive1);
-
-        Hash h;
-        p->load(h, archive1);
-
-        CPPUNIT_ASSERT(karabo::util::similar(m_sharedPtrHash, h) == true);
-
-        p->save(h, archive2);
-        CPPUNIT_ASSERT(string(archive1[0], archive1.size()) == string(archive2[0], archive2.size()));
-
-    }
 }
