90dbcdcbd041719e56f72aba328c0cf56c78be18
Kerstin Weger <kerstin.weger@xfel.eu>
2016-08-18 13:36:59 +0200

    Re-implement the scene link widget opening using event handle mechanism

--- src/pythonGui/karabo_gui/const.py
+++ src/pythonGui/karabo_gui/const.py
@@ -28,22 +28,25 @@ SCENE_MIN_WIDTH = 1024
 SCENE_MIN_HEIGHT = 768
 
 # Parameter item properties
-INTERNAL_KEY           = Qt.UserRole
-VALUE_TYPE             = Qt.UserRole + 1
-DEFAULT_VALUE          = Qt.UserRole + 2
+INTERNAL_KEY = Qt.UserRole
+VALUE_TYPE = Qt.UserRole + 1
+DEFAULT_VALUE = Qt.UserRole + 2
 CURRENT_INSTANCE_VALUE = Qt.UserRole + 3
 CURRENT_EDITABLE_VALUE = Qt.UserRole + 4
-ALIAS                  = Qt.UserRole + 5
-TAGS                   = Qt.UserRole + 6
-DESCRIPTION            = Qt.UserRole + 7
-TIMESTAMP              = Qt.UserRole + 8
-REQUIRED_ACCESS_LEVEL  = Qt.UserRole + 9
-IS_CHOICE_ELEMENT      = Qt.UserRole + 10
-IS_LIST_ELEMENT        = Qt.UserRole + 11
-UPDATE_NEEDED          = Qt.UserRole + 12
-ACCESS_TYPE            = Qt.UserRole + 13
-CLASS_ALIAS            = Qt.UserRole + 14
-ALLOWED_STATE          = Qt.UserRole + 15
-UNIT_SYMBOL            = Qt.UserRole + 16
-METRIC_PREFIX_SYMBOL   = Qt.UserRole + 17
-ENUMERATION            = Qt.UserRole + 18
+ALIAS = Qt.UserRole + 5
+TAGS = Qt.UserRole + 6
+DESCRIPTION = Qt.UserRole + 7
+TIMESTAMP = Qt.UserRole + 8
+REQUIRED_ACCESS_LEVEL = Qt.UserRole + 9
+IS_CHOICE_ELEMENT = Qt.UserRole + 10
+IS_LIST_ELEMENT = Qt.UserRole + 11
+UPDATE_NEEDED = Qt.UserRole + 12
+ACCESS_TYPE = Qt.UserRole + 13
+CLASS_ALIAS = Qt.UserRole + 14
+ALLOWED_STATE = Qt.UserRole + 15
+UNIT_SYMBOL = Qt.UserRole + 16
+METRIC_PREFIX_SYMBOL = Qt.UserRole + 17
+ENUMERATION = Qt.UserRole + 18
+
+# Karabo broadcast event senders
+OPEN_SCENE_LINK = "SCENE_LINK"
