751f822ca1eef7e72a92cd2c5dbef1ca0580fcf5
Steffen Hauf <steffen.hauf@xfel.eu>
2016-08-18 09:12:41 +0200

    Cross checkouts from feature branch which was very unclean towards master

--- src/karabo/core/AlarmService.hh
+++ src/karabo/core/AlarmService.hh
@@ -8,6 +8,8 @@
 #ifndef KARABO_ALARMSERVICE_HH
 #define	KARABO_ALARMSERVICE_HH
 
+#include <set>
+
 #include "Device.hh"
 #include "OkErrorFsm.hh"
 
@@ -36,6 +38,61 @@ namespace karabo {
         private: // Functions
 
             void initialize();
+
+            /**
+             * Callback for the instanceNew monitor. Connects this device's slotUpdateAlarms function
+             * to the new devices signalAlarmUpdate signal.
+             */
+            void registerAlarmServiceWithNewDevice(const karabo::util::Hash& topologyEntry);
+
+            void slotRegisterExistingDevice(const karabo::util::Hash& instanceInfo);
+
+            /**
+             * This slot is to be called to the the alarm service device know of an update
+             * in device alarms. As only parameter it expects an alarmInfo Hash, which should
+             * have the following structure:
+             *
+             *   deviceId -> toClear -> property A -> alarm type 1 -> bool
+             *                       -> property A -> alarm type 2 -> bool
+             *                       -> property B -> ....
+             *
+             *  The bool field is not evaluated but serves as a placeholder
+             *
+             *   deviceId -> toClear -> property A -> alarm type 1 -> Hash()
+             *   deviceId -> toClear -> property A -> alarm type 2 -> Hash()
+             *   deviceId -> toClear -> property B -> ...
+             *
+             * Entries underneath the "toClear" hierarchy are used to evaluated clearing of existing
+             * alarms. Two scenarios are handled:
+             *
+             *  - alarm <b>does not</b> require acknowledging -> it is deleted from the alarm list
+             *  - alarm <b>does</b> require acknowledging -> it is made acknowledgable
+             *
+             * Entries in the "toAdd" hierarchy result in one of the following two scenarios
+             *
+             *  - an alarm for this property and type <b>does not</b> yet exist -> it is added
+             *  - an alarm for this property and type <b>does</b> exist -> it is updated but the
+             *    first occurance is set to that of the existing alarm.
+             */
+            void slotUpdateAlarms(const karabo::util::Hash& alarmInfo);
+
+
+
+
+        private: // members
+
+            std::map<std::string, karabo::util::Hash> m_registeredDevices;
+            karabo::util::Hash m_alarms;
+
+            /**
+             Updates the alarm table of this device to reflect the entries in m_alarms
+             */
+            void updateAlarmTable();
+
+            /**
+             Add signals and slots which need to be set up during initialization in this function
+             */
+            void setupSignalsAndSlots();
 
         };
     }
---
+++ src/karabo/tests/alarmService/AlarmTesterDevice.cc
@@ -0,0 +1,164 @@
+/*
+ * $Id: AlarmTester.cc 7755 2016-06-24 14:10:56Z haufs $
+ *
+ * Author: <steffen.hauf@xfel.eu>
+ * 
+ * Created on June, 2016, 03:03 PM
+ *
+ * Copyright (c) 2010-2013 European XFEL GmbH Hamburg. All rights reserved.
+ */
+
+#include "AlarmTesterDevice.hh"
+#include <cassert>
+
+using namespace std;
+
+USING_KARABO_NAMESPACES
+
+namespace karabo {
+
+
+    KARABO_REGISTER_FOR_CONFIGURATION(BaseDevice, Device<>, AlarmTester)
+
+    void AlarmTester::expectedParameters(Schema& expected) {
+        
+        FLOAT_ELEMENT(expected).key("floatProperty")
+            .displayedName("Float Property")
+            .readOnly().initialValue(0)
+            .alarmLow(-2).info("A description for alarmLow").needsAcknowledging(true)
+            .alarmHigh(2).info("A description for alarmHigh").needsAcknowledging(true)
+            .enableRollingStats().warnVarianceHigh(3).needsAcknowledging(false).evaluationInterval(100)
+            .commit();
+        
+        SLOT_ELEMENT(expected).key("triggerWarnLow")
+            .displayedName("Trigger WARN_LOW")
+            .commit();
+        
+        SLOT_ELEMENT(expected).key("triggerWarnHigh")
+            .displayedName("Trigger WARN_HIGH")
+            .commit();
+        
+        SLOT_ELEMENT(expected).key("triggerAlarmLow")
+            .displayedName("Trigger ALARM_LOW")
+            .commit();
+        
+        SLOT_ELEMENT(expected).key("triggerAlarmHigh")
+            .displayedName("Trigger ALARM_HIGH")
+            .commit();
+        
+        SLOT_ELEMENT(expected).key("triggerGlobalWarn")
+            .displayedName("Trigger Global Warn")
+            .commit();
+        
+        SLOT_ELEMENT(expected).key("triggerGlobalAlarm")
+            .displayedName("Trigger Global Alarm")
+            .commit();
+        
+        SLOT_ELEMENT(expected).key("triggerNormal")
+            .displayedName("Back to normal")
+            .commit();
+        
+        SLOT_ELEMENT(expected).key("triggerGlobalNormal")
+            .displayedName("Back to global normal")
+            .commit();
+        
+    }
+
+
+    AlarmTester::AlarmTester(const karabo::util::Hash& config) : Device<>(config) {
+        KARABO_SLOT(triggerWarnLow);
+        KARABO_SLOT(triggerWarnHigh);
+        KARABO_SLOT(triggerAlarmLow);
+        KARABO_SLOT(triggerAlarmHigh);
+        KARABO_SLOT(triggerGlobalAlarm);
+        KARABO_SLOT(triggerGlobalWarn);
+        KARABO_SLOT(triggerNormal);
+        KARABO_SLOT(triggerGlobalNormal);
+        
+        KARABO_INITIAL_FUNCTION(initialize);
+        
+        
+    }
+
+
+    AlarmTester::~AlarmTester() {
+    }
+
+    void AlarmTester::initialize() {
+
+        Schema schema =  getFullSchema();
+        schema.setWarnLow("floatProperty", -1.);
+        schema.setWarnHigh("floatProperty", 1.);
+        schema.setAlarmLow("floatProperty", -2.);
+        schema.setAlarmHigh("floatProperty", 2.);
+        appendSchema(schema, true);
+
+        
+    }
+    
+
+    void AlarmTester::preReconfigure(karabo::util::Hash& incomingReconfiguration) {
+    }
+
+
+    void AlarmTester::postReconfigure() {
+    }
+    
+    void AlarmTester::triggerWarnLow(){
+        set("floatProperty", -1.2);
+        reply("triggeredWarnLow");
+  
+   
+    }
+    
+    void AlarmTester::triggerWarnHigh(){
+        set("floatProperty", 1.2);
+        reply("triggeredWarnHigh");
+   
+    }
+    
+    void AlarmTester::triggerAlarmLow(){
+        set("floatProperty", -2.2);
+       
+   
+        KARABO_LOG_INFO<<getAlarmInfo();
+        reply("triggeredAlarmLow");
+    }
+    
+    void AlarmTester::triggerAlarmHigh(){
+        set("floatProperty", 2.2);
+       
+   
+        KARABO_LOG_INFO<<getAlarmInfo();
+        reply("triggeredAlarmHigh");
+    }
+    
+    void AlarmTester::triggerGlobalAlarm(){
+        setAlarmCondition(AlarmCondition::ALARM);
+        
+   
+        KARABO_LOG_INFO<<getAlarmInfo();
+        reply("triggeredGlobalAlarm");
+    }
+    
+    void AlarmTester::triggerGlobalWarn(){
+        setAlarmCondition(AlarmCondition::WARN);
+        reply("triggeredGlobalWarn");
+   
+    }
+
+    void AlarmTester::triggerNormal(){
+        set("floatProperty", 0);
+        reply("triggeredNormal");
+   
+        
+    }
+    
+    void AlarmTester::triggerGlobalNormal(){
+        setAlarmCondition(AlarmCondition::NONE);
+        reply("triggeredGlobalNormal");
+   
+    }
+    
+   
+}
