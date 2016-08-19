0c068e2b469f9752a22013c6dbb9575ab848e88d
John Wiggins <john.wiggins@xfel.eu>
2016-08-19 13:17:49 +0200

    Remove a bunch of orphaned GUI code

--- src/pythonGui/karabo_gui/registry.py
+++ src/pythonGui/karabo_gui/registry.py
@@ -1,10 +1,8 @@
 """ This module contains a mechanism to register classes that
 inherit from a certain parent. """
 
-from PyQt4.QtCore import Qt, QObject
-from PyQt4.QtGui import QAction
+from PyQt4.QtCore import QObject
 from karabo.middlelayer import Registry
-from karabo_gui.const import ns_karabo
 
 
 def Monkey(name, bases, dict):
@@ -23,35 +21,3 @@ class MetaRegistry(type(Registry), type(QObject)):
 
 class Registry(Registry, metaclass=MetaRegistry):
     """ This is a special case of a registry which inherits QObject """
-
-
-class Loadable(Registry):
-    """The registry for all loadable objects.
-
-    The XML element either has to have a krb:class attribute, which
-    is registered here under subclasses, or it is an SVG element, which
-    is registered under xmltags. Unknown elements are ignored."""
-    xmltags = { }
-    krbclasses = { }
-
-
-    @classmethod
-    def register(cls, name, dict):
-        super(Loadable, cls).register(name, dict)
-        if "xmltag" in dict:
-            Loadable.xmltags[cls.xmltag] = cls
-        cls.krbclasses[name] = cls
-
-
-    @staticmethod
-    def load(element, layout):
-        krb = element.get(ns_karabo + "class")
-        ret = None
-        if krb is not None:
-            ret = Loadable.krbclasses[krb].load(element, layout)
-        if ret is None:
-            try:
-                return Loadable.xmltags[element.tag].load(element, layout)
-            except KeyError:
-                return
-        return ret
