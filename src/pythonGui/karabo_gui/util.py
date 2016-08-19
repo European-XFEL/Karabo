2d772eccaeca13167d067e25a010e385c3b67f2f
Kerstin Weger <kerstin.weger@xfel.eu>
2016-08-19 09:16:33 +0200

    Add new module for mediator

--- src/pythonGui/karabo_gui/util.py
+++ src/pythonGui/karabo_gui/util.py
@@ -1,24 +1,8 @@
 from uuid import uuid4
 
-from PyQt4.QtCore import QEvent, QObject
-from PyQt4.QtGui import QApplication, QDialog, QFileDialog
+from PyQt4.QtGui import QDialog, QFileDialog
 
 from karabo.middlelayer import Hash
-
-# This is the global singleton for the karabo_mediator function.
-_karabo_mediator = None
-
-
-class KaraboBroadcastEvent(QEvent):
-    """ Custom event to handle GUI widget communication """
-
-    # Ask Qt for our event type
-    Type = QEvent.Type(QEvent.registerEventType())
-
-    def __init__(self, sender="", data=None):
-        super(KaraboBroadcastEvent, self).__init__(self.Type)
-        self.sender = sender  # Names the sender
-        self.data = data or {}  # Includes the data which is sent
 
 
 class SignalBlocker(object):
@@ -31,39 +15,6 @@ class SignalBlocker(object):
 
     def __exit__(self, a, b, c):
         self.object.blockSignals(self.state)
-
-
-def __get_mediator():
-    """ Return the karabo mediator singleton.
-    """
-    global _karabo_mediator
-    if _karabo_mediator is None:
-        _karabo_mediator = QObject()
-
-    return _karabo_mediator
-
-
-def broadcast_event(event):
-    """ Broadcast the given `event`.
-    """
-    mediator = __get_mediator()
-    QApplication.postEvent(mediator, event)
-
-
-def register_for_broadcasts(qobject):
-    """ Register the given `qobject` to the events coming from the singleton
-        `mediator`.
-    """
-    mediator = __get_mediator()
-    mediator.installEventFilter(qobject)
-
-
-def unregister_from_broadcasts(qobject):
-    """ Unregister the given `qobject` from the events coming from the
-        singleton mediator object.
-    """
-    mediator = __get_mediator()
-    mediator.removeEventFilter(qobject)
 
 
 def generateObjectName(widget):
