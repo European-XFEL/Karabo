dd9eba976169baa884eb54d2085b4ec9ef1d1658
Burkhard Heisen <burkhard.heisen@xfel.eu>
2016-08-23 09:33:21 +0200

    Added missing Makefiles and some missing Release configurations.

--- build/netbeans/tools/dataLoggerIndex/idxview/nbproject/Karabo-Release-Linux.mk
+++ build/netbeans/tools/dataLoggerIndex/idxview/nbproject/Karabo-Release-Linux.mk
@@ -65,7 +65,7 @@ ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/bin/karabo-idxview: ${OBJECTFILES}
 ${OBJECTDIR}/_ext/1310162774/idxView.o: ../../../../../src/tools/dataLoggerIndex/idxView.cc 
 	${MKDIR} -p ${OBJECTDIR}/_ext/1310162774
 	${RM} "$@.d"
-	$(COMPILE.cc) -O2 -I../../../../../src/tools/dataLoggerIndex -I${KARABO}/include -I${KARABO}/extern/include `pkg-config --cflags karaboDependencies-${CND_PLATFORM}`   -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/_ext/1310162774/idxView.o ../../../../../src/tools/dataLoggerIndex/idxView.cc
+	$(COMPILE.cc) -O2 -I../../../../../src/tools/dataLoggerIndex -I${KARABO}/include -I${KARABO}/extern/include `pkg-config --cflags karaboDependencies-${CND_PLATFORM}` -std=c++11  -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/_ext/1310162774/idxView.o ../../../../../src/tools/dataLoggerIndex/idxView.cc
 
 # Subprojects
 .build-subprojects:
