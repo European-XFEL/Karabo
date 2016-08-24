674a66b4905fd1d281ac12052831a6e44f749bad
Steffen Hauf <steffen.hauf@xfel.eu>
2016-08-24 09:39:43 +0200

    Refactored out devices from core to a separate devices namespace

--- src/karabo/tests/integration/AlarmService_Test.hh
+++ src/karabo/tests/integration/AlarmService_Test.hh
@@ -9,7 +9,7 @@
 #define	ALARMSERVICE_TEST_HH
 
 #include "karabo/karabo.hpp"
-#include "karabo/core/AlarmService.hh"
+#include "karabo/devices/AlarmService.hh"
 #include "karabo/core/DeviceServer.hh"
 #include "karabo/core/DeviceClient.hh"
 #include <boost/shared_ptr.hpp>
@@ -49,7 +49,7 @@ private:
     boost::thread m_deviceServerThread;
 
     karabo::core::DeviceClient::Pointer m_deviceClient;
-    karabo::core::AlarmService::Pointer m_serviceDevice;
+
 
 
 };
