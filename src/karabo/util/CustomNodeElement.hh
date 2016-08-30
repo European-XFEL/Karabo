6f818c3f8c99b1b5fbf8575be22d488c98a7989a
Burkhard Heisen <burkhard.heisen@xfel.eu>
2016-08-30 15:59:47 +0200

    Implemented MR recommendations
    Types
    - Removed old C++03 code
    
    ImageData
    - Moved expectedParameters implementation to source file
    - Renamed NDArray node key from "data" to "pixels"
    
    HashTest
    - Got rid of non needed typedef: ArrayType
    
    CustomNodeElement
    - Corrected file meta information
    - Added class documentation

--- src/karabo/util/CustomNodeElement.hh
+++ src/karabo/util/CustomNodeElement.hh
@@ -1,5 +1,5 @@
 /*
- * File:   SerializableObjectElement.hh
+ * File:   CustomNodeElement.hh
  * Author: heisenb
  *
  * Created on August 26, 2016, 3:04 PM
@@ -11,6 +11,35 @@
 namespace karabo {
     namespace util {
 
+        /**
+         * Helper class to construct custom NODE_ELEMENTs for schemas.
+         * Usage is best explained by example, say you coded a custom data class
+         * (by inheriting protected Hash) describing it's expected parameters like:
+         * @code
+         * class MyData : protected Hash {
+         *     static void expectedParameters(const Schema& s) {
+         *         // parameter definition [...]
+         *     }
+         * };
+         * @endcode
+         * Then you may generate a NODE_ELEMENT like this:
+         * @code{.cpp}
+         * class MyDataElement : public CustomNodeElement<MyData> {
+         *
+         *     MyDataElement(karabo::util::Schema& s) : CustomNodeElement<MyData>(s) {
+         *     }
+         *
+         *     // If you want to expose parameters for setting defaults do like:
+         *     CustomNodeElement<ImageData>& setFoo(const std::string& bar) {
+         *         return CustomNodeElement<ImageData >::setDefaultValue("foo", bar);
+         *     }
+         * };
+         * typedef MyDataElement MYDATA_ELEMENT;
+         * @endcode
+         *
+         *
+         *
+         */
         template <class Described>
         class CustomNodeElement {
 
--- src/karabo/util/FromTypeInfo.cc
+++ src/karabo/util/FromTypeInfo.cc
@@ -16,52 +16,6 @@
 namespace karabo {
     namespace util {
 
-#if __cplusplus < 201103L
-
-#define _KARABO_HELPER_MACRO1(ReferenceType, CppType) \
-(std::string(typeid (CppType).name()), Types::ReferenceType) \
-(std::string(typeid (std::vector<CppType>).name()), Types::VECTOR_##ReferenceType)
-
-
-#define _KARABO_HELPER_MACRO(ReferenceType, CppType) \
-(std::string(typeid (CppType).name()), Types::ReferenceType) \
-(std::string(typeid (std::vector<CppType>).name()), Types::VECTOR_##ReferenceType)\
-(std::string(typeid (CppType*).name()), Types::PTR_##ReferenceType)
-
-
-        FromTypeInfo::FromTypeInfo() {
-            _typeInfoMap = boost::assign::map_list_of
-                    _KARABO_HELPER_MACRO(BOOL, bool)
-                    _KARABO_HELPER_MACRO(CHAR, char)
-                    _KARABO_HELPER_MACRO(INT8, signed char)
-                    _KARABO_HELPER_MACRO(UINT8, unsigned char)
-                    _KARABO_HELPER_MACRO(INT16, short)
-                    _KARABO_HELPER_MACRO(UINT16, unsigned short)
-                    _KARABO_HELPER_MACRO(INT32, int)
-                    _KARABO_HELPER_MACRO(UINT32, unsigned int)
-                    _KARABO_HELPER_MACRO(INT64, long long)
-                    _KARABO_HELPER_MACRO(UINT64, unsigned long long)
-                    _KARABO_HELPER_MACRO(FLOAT, float)
-                    _KARABO_HELPER_MACRO(DOUBLE, double)
-                    _KARABO_HELPER_MACRO(COMPLEX_FLOAT, std::complex<float>)
-                    _KARABO_HELPER_MACRO(COMPLEX_DOUBLE, std::complex<double>)
-                    _KARABO_HELPER_MACRO(STRING, std::string)
-
-                    _KARABO_HELPER_MACRO1(HASH, Hash)
-                    _KARABO_HELPER_MACRO1(SCHEMA, Schema)
-                    _KARABO_HELPER_MACRO1(NONE, CppNone)
-
-                    (std::string(typeid (Hash::Pointer).name()), Types::HASH_POINTER)
-                    (std::string(typeid (std::vector<Hash::Pointer>).name()), Types::VECTOR_HASH_POINTER)
-
-                    ;
-#undef _KARABO_HELPER_MACRO
-#undef _KARABO_HELPER_MACRO1
-#undef _KARABO_HELPER_MACRO2
-#undef _KARABO_HELPER_MACRO3
-
-#else
-
 #define _KARABO_HELPER_MACRO1(ReferenceType, CppType) \
 {std::string(typeid (CppType).name()), Types::ReferenceType} \
 , {std::string(typeid (std::vector<CppType>).name()), Types::VECTOR_##ReferenceType}
@@ -109,12 +63,8 @@ namespace karabo {
                 }
 
             };
-
 #undef _KARABO_HELPER_MACRO
 #undef _KARABO_HELPER_MACRO1
-#undef _KARABO_HELPER_MACRO2
-
-#endif
         }
     }
 }
