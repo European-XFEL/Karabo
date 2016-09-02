8730cada3a16d4654b51e97f7c4aeac099ae0034
John Wiggins <john.wiggins@xfel.eu>
2016-09-02 10:19:33 +0200

    Move the karathonTest files to a better place.
    Also, clean them up considerably

---
+++ src/pythonKarabo/karabo/bound_api/tests/old/test_find.py
@@ -0,0 +1,25 @@
+from karabo.bound import Hash
+
+
+def test_find_1():
+    h = Hash("a.b.c1.d", 1)
+
+    node = h.find("a.b.c1.d")
+    assert node is not None
+    assert node.getKey() == "d"
+    assert node.getValue() == 1
+
+    node = h.find("a.b.c1.f")
+    assert node is None
+
+
+def test_find_2():
+    h = Hash("a.b.c", "1")
+
+    node = h.find("a.b.c")
+    assert node is not None
+    node.setValue(2)
+    assert h.get("a.b.c") == 2
+
+    node = h.find("a.b.c", '/')
+    assert node is None
---
+++ src/pythonKarabo/karabo/bound_api/tests/old/test_merge.py
@@ -0,0 +1,34 @@
+from karabo.bound import Hash, similar
+
+
+def test_merge():
+    h1 = Hash("a", 1,
+              "b", 2,
+              "c.b[0].g", 3,
+              "c.c[0].d", 4,
+              "c.c[1]", Hash("a.b.c", 6),
+              "d.e", 7)
+
+    h2 = Hash("a", 21,
+              "b.c", 22,
+              "c.b[0]", Hash("key", "value"),
+              "c.b[1].d", 24,
+              "e", 27)
+
+    h1 += h2
+    assert h1.has("a")
+    assert h1.get("a") == 21
+    assert h1["a"] == 21
+    assert h1.has("b")
+    assert not h1.has("c.b.d")
+    assert h1.has("c.b[0]")
+    assert h1.has("c.b[1]")
+    assert h1.has("c.b[2]")
+    assert h1.get("c.b[2].d") == 24
+    assert h1.has("c.c[0].d")
+    assert h1.has("c.c[1].a.b.c")
+    assert h1.has("d.e")
+    assert h1.has("e")
+
+    h3 = h1
+    assert similar(h1, h3)
