e863ce4a6582cd469ef8dc456768c6530955f157
John Wiggins <john.wiggins@xfel.eu>
2016-09-05 10:26:00 +0200

    MR !633 feedback
    ByteArrayElements should always be read-only.

--- src/karabo/util/ByteArrayElement.hh
+++ src/karabo/util/ByteArrayElement.hh
@@ -31,21 +31,10 @@ namespace karabo {
                 m_node->setAttribute(KARABO_SCHEMA_DISPLAY_TYPE, "ByteArray");
                 m_node->setAttribute(KARABO_SCHEMA_VALUE_TYPE, Types::to<ToLiteral>(Types::from<ByteArray >()));
 
-                if (!m_node->hasAttribute(KARABO_SCHEMA_ACCESS_MODE)) init(); // This is the default
+                m_node->setAttribute<int>(KARABO_SCHEMA_ACCESS_MODE, READ);
+                m_node->setAttribute<int>(KARABO_SCHEMA_ASSIGNMENT, Schema::OPTIONAL_PARAM);
+                m_node->setAttribute<int>(KARABO_SCHEMA_ARCHIVE_POLICY, Schema::EVERY_EVENT);
 
-                if (!m_node->hasAttribute(KARABO_SCHEMA_REQUIRED_ACCESS_LEVEL)) {
-
-                    //for init, reconfigurable elements - set default value of requiredAccessLevel to USER
-                    if (!m_node->hasAttribute(KARABO_SCHEMA_ACCESS_MODE) || //init element
-                        m_node->template getAttribute<int>(KARABO_SCHEMA_ACCESS_MODE) == INIT || //init element
-                        m_node->template getAttribute<int>(KARABO_SCHEMA_ACCESS_MODE) == WRITE) { //reconfigurable element
-
-                        userAccess();
-
-                    } else { //else set default value of requiredAccessLevel to OBSERVER
-                        observerAccess();
-                    }
-                }
             }
         };
 
