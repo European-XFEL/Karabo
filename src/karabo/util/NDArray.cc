31ffded36de05167b8abe482efee5ee1b0a253b7
John Wiggins <john.wiggins@xfel.eu>
2016-09-05 17:18:03 +0200

    Add expectedParameters() to NDArray

--- src/karabo/util/NDArray.cc
+++ src/karabo/util/NDArray.cc
@@ -6,11 +6,37 @@
  */
 
 #include "NDArray.hh"
-#include "NDArrayElement.hh"
 #include "ToSize.hh"
+#include "ByteArrayElement.hh"
+#include "NDArrayElement.hh"
+#include "SimpleElement.hh"
 
 namespace karabo {
     namespace util {
+
+        void NDArray::expectedParameters(karabo::util::Schema& s) {
+
+            BYTEARRAY_ELEMENT(s).key("data")
+                    .displayedName("Data")
+                    .description("The data of the array")
+                    .readOnly()
+                    .commit();
+            VECTOR_INT32_ELEMENT(s).key("shape")
+                    .displayedName("Shape")
+                    .description("The shape of the array reflects total dimensionality and each element the extension in this dimension")
+                    .readOnly()
+                    .commit();
+            INT32_ELEMENT(s).key("type")
+                    .displayedName("Data Type")
+                    .description("The type of the contained array data")
+                    .readOnly()
+                    .commit();
+            BOOL_ELEMENT(s).key("isBigEndian")
+                    .displayedName("Is big-endian")
+                    .description("A boolean flag which is true if the data is big-endian")
+                    .readOnly()
+                    .commit();
+        }
 
 
         NDArray::NDArray(const Dims& shape,
