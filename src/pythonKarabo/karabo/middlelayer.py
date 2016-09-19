d39f48a994b7a1fe94a867b2099fbe8e84806375
Martin Teichmann <martin.teichmann@xfel.eu>
2016-09-19 16:52:31 +0200

    rename sync.py to synchronization.py

--- src/pythonKarabo/karabo/middlelayer.py
+++ src/pythonKarabo/karabo/middlelayer.py
@@ -45,5 +45,5 @@ from .middlelayer_api.schema import (
     Configurable, Node, ChoiceOfNodes, ListOfNodes
 )
 from .middlelayer_api.signalslot import Signal, SignalSlotable, slot, coslot
-from .middlelayer_api.sync import background
+from .middlelayer_api.synchronization import background
 from .middlelayer_api.timestamp import Timestamp
