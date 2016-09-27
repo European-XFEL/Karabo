3c299fe57a9d076a0882ffccdc1e63a75fbca02a
Steffen Hauf <steffen.hauf@xfel.eu>
2016-09-27 16:32:28 +0200

    Added weak_bind to Metatools
    Fix copy paster in tests
    
    Delete device server at end, stop event loop
    
    Test for fixing segfault in tests
    
    Increase timeouts to add more buffer in tests
    
    Add more flexibility w.r.t test speed

--- src/karabo/tests/util/MetaTools_Test.hh
+++ src/karabo/tests/util/MetaTools_Test.hh
@@ -1,4 +1,4 @@
-/* 
+/*
  * File:   MetaTools_Test.hh
  * Author: heisenb
  *
@@ -11,6 +11,18 @@
 #include <karabo/util/Hash.hh>
 #include <cppunit/extensions/HelperMacros.h>
 #include <karabo/util/MetaTools.hh>
+#include <karabo/net/EventLoop.hh>
+
+
+
+#include <boost/function.hpp>
+#include <boost/bind.hpp>
+#include <boost/asio.hpp>
+#include <boost/thread.hpp>
+#include <boost/enable_shared_from_this.hpp>
+
+#include <unordered_map>
+#include <iostream>
 
 struct MyPublicHash : public karabo::util::Hash {
 
@@ -42,12 +54,87 @@ struct PointerTest {
     }
 };
 
+struct SignalSlotable : public boost::enable_shared_from_this<SignalSlotable> {
+
+    virtual ~SignalSlotable() {
+    }
+
+};
+
+struct Device : public virtual SignalSlotable {
+
+    boost::asio::deadline_timer m_timer;
+    std::vector<std::string>* m_messages;
+    boost::mutex m_messageMutex;
+
+    Device(std::vector<std::string>* messages)
+        : m_timer(karabo::net::EventLoop::getIOService()), m_messages(messages) {
+        {
+            boost::mutex::scoped_lock lock(m_messageMutex);
+            m_messages->push_back("Device created");
+        }
+    }
+
+    void init() {
+        m_timer.expires_from_now(boost::posix_time::millisec(100));
+        m_timer.async_wait(karabo::util::bind_weak(&Device::executeStepFunction, this, 5, _1));
+    }
+
+    ~Device() {
+        {
+            boost::mutex::scoped_lock lock(m_messageMutex);
+            m_messages->push_back("Device deleted");
+        }
+        m_timer.cancel();
+    }
+
+    void executeStepFunction(int arg, const boost::system::error_code& error) {
+        // Canceled timer
+        if (error == boost::asio::error::operation_aborted) {
+            {
+                boost::mutex::scoped_lock lock(m_messageMutex);
+                m_messages->push_back("Timer aborted");
+            }
+            return;
+        }
+        // Schedule next step
+        {
+            boost::mutex::scoped_lock lock(m_messageMutex);
+            m_messages->push_back("Tick " + boost::lexical_cast<std::string>(arg));
+        }
+
+        boost::this_thread::sleep(boost::posix_time::milliseconds(50));
+
+        m_timer.expires_from_now(boost::posix_time::millisec(500));
+
+        m_timer.async_wait(karabo::util::bind_weak(&Device::executeStepFunction, this, arg + 1, _1));
+    }
+};
+
+struct DeviceServer {
+
+    boost::asio::deadline_timer m_deviceDestructTimer;
+    std::map<std::string, boost::shared_ptr<Device> > m_devices;
+
+    DeviceServer(std::vector<std::string>* messages)
+        : m_deviceDestructTimer(karabo::net::EventLoop::getIOService()) {
+        m_devices["someDevice"] = boost::shared_ptr<Device>(new Device(messages));
+        m_devices["someDevice"]->init();
+
+        m_deviceDestructTimer.expires_from_now(boost::posix_time::millisec(1500));
+        m_deviceDestructTimer.async_wait(boost::bind(&DeviceServer::killDevice, this, _1, "someDevice"));
+    }
+
+    void killDevice(const boost::system::error_code& error, const std::string& deviceName) {
+        m_devices.erase(deviceName);
+    }
+};
 
 class MetaTools_Test : public CPPUNIT_NS::TestFixture {
 
-
     CPPUNIT_TEST_SUITE(MetaTools_Test);
     CPPUNIT_TEST(testMethod);
+    CPPUNIT_TEST(testWeakBind);
     CPPUNIT_TEST_SUITE_END();
 
 public:
@@ -57,6 +144,7 @@ public:
 private:
 
     void testMethod();
+    void testWeakBind();
 
 };
 
