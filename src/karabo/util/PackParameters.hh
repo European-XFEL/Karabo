aeaf5e7f5cce07db780c2f6c905f38a96a14d981
Martin Teichmann <martin.teichmann@xfel.eu>
2016-10-04 11:15:35 +0200

    the compiler need inline to inline

--- src/karabo/util/PackParameters.hh
+++ src/karabo/util/PackParameters.hh
@@ -5,36 +5,36 @@ namespace karabo {
     namespace util {
         class Hash;
 
-        void unpack_r(const Hash& hash, char i) {
+        inline void unpack_r(const Hash& hash, char i) {
         }
 
         template <class Tfirst, class ... Trest>
-        void unpack_r(const Hash& hash, char i, Tfirst &first, Trest& ... rest) {
-            char name[3] = "a ";
+        inline void unpack_r(const Hash& hash, char i, Tfirst &first, Trest& ... rest) {
+            char name[4] = "a ";
             name[1] = i;
             first = hash.get<Tfirst>(name);
             unpack_r(hash, i + 1, rest...);
         }
 
         template <class ... Ts>
-        void unpack(const Hash& hash, Ts & ... args) {
+        inline void unpack(const Hash& hash, Ts & ... args) {
             unpack_r(hash, '1', args...);
         }
 
-        void pack_r(Hash& hash, char i) {
+        inline void pack_r(Hash& hash, char i) {
         }
 
 
         template <class Tfirst, class ... Trest>
-        void pack_r(Hash& hash, char i, const Tfirst& first, const Trest & ... rest) {
-            char name[3] = "a ";
+        inline void pack_r(Hash& hash, char i, const Tfirst& first, const Trest & ... rest) {
+            char name[4] = "a ";
             name[1] = i;
             hash.set(name, first);
             pack_r(hash, i + 1, rest...);
         }
 
         template <class ... Ts>
-        void pack(Hash& hash, const Ts& ... args) {
+        inline void pack(Hash& hash, const Ts& ... args) {
             pack_r(hash, '1', args...);
         }
     }
