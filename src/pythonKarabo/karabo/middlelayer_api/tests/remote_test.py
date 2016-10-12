86e0ac6e3d1ff2dd42141392469dbe2f5432d386
Martin Teichmann <martin.teichmann@xfel.eu>
2016-10-12 14:52:47 +0200

    test prenatal getDevice

--- src/pythonKarabo/karabo/middlelayer_api/tests/remote_test.py
+++ src/pythonKarabo/karabo/middlelayer_api/tests/remote_test.py
@@ -721,6 +721,14 @@ class Tests(DeviceTest):
         self.assertFalse(hasattr(a, "dn"))
 
     @async_tst
+    def test_prenatal_proxy(self):
+        task = async(getDevice("prenatal"))
+        a = Remote({"_deviceId_": "prenatal"})
+        yield from a.startInstance()
+        proxy = yield from task
+        self.assertEqual(proxy.deviceId, "prenatal")
+
+    @async_tst
     def test_proxy_dead(self):
         a = Remote({"_deviceId_": "moriturus"})
         yield from a.startInstance()
