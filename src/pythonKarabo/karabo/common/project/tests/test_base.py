960b6739fdee78c934bda6fc421ce7c33bfb5fb1
John Wiggins <john.wiggins@xfel.eu>
2016-10-14 09:54:02 +0200

    Make project object UUIDs a bit more robust

---
+++ src/pythonKarabo/karabo/common/project/tests/test_base.py
@@ -0,0 +1,24 @@
+import uuid
+
+from nose.tools import assert_raises
+from traits.api import push_exception_handler, pop_exception_handler
+
+from karabo.common.project.api import BaseProjectObjectModel
+
+
+def setUp():
+    push_exception_handler(lambda *args: None, reraise_exceptions=True)
+
+
+def tearDown():
+    pop_exception_handler()
+
+
+def test_uuid():
+    model = BaseProjectObjectModel()
+
+    # check that this doesn't assert
+    uuid.UUID(model.uuid)
+
+    with assert_raises(ValueError):
+        model.uuid = 'foo'
