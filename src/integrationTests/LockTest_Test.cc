b52eae7fadb0f865f3797199512860f3dbb68a71
Burkhard Heisen <burkhard.heisen@xfel.eu>
2016-10-17 17:12:34 +0200

    Changed to exception based error handling on remote calls.
    - Implmented packaging of exceptions into message header
    - Changed API for deviceClient execute functions to return void
    - Changed Device code to not reply but rather trigger exceptions
    - Formatted file I touched to follow our style conventions
    - Adapted the intgration tests to play nicely with exceptions
    - All changes done for C++ and Python
    
    TODO: Forward the error text of exceptions raised in python code
    which was called from C++ (to be done in SlotWrap, tryToPrint() )

--- src/integrationTests/LockTest_Test.cc
+++ src/integrationTests/LockTest_Test.cc
@@ -14,21 +14,24 @@ USING_KARABO_NAMESPACES
 
 CPPUNIT_TEST_SUITE_REGISTRATION(LockTest_Test);
 
+
 LockTest_Test::LockTest_Test() {
 }
+
 
 LockTest_Test::~LockTest_Test() {
 }
 
-void LockTest_Test::setUp() {
 
-    Hash config("DeviceServer", Hash("serverId", "testServerLock", "scanPlugins", false, "visibility", 4, "Logger.priority", "ERROR"));
+void LockTest_Test::setUp() {
+    
+    // The test will raise exceptions by purpose, FATAL log level is higher then ERROR (max used in KARABO), so the test will be silent
+    Hash config("DeviceServer", Hash("serverId", "testServerLock", "scanPlugins", false, "visibility", 4, "Logger.priority", "FATAL"));
     m_deviceServer = boost::shared_ptr<DeviceServer>(DeviceServer::create(config));
     m_deviceServerThread = boost::thread(&DeviceServer::run, m_deviceServer);
     m_deviceClient = boost::shared_ptr<DeviceClient>(new DeviceClient());
 
 }
-
 
 
 void LockTest_Test::tearDown() {
@@ -38,23 +41,20 @@ void LockTest_Test::tearDown() {
 
 }
 
+
 void LockTest_Test::appTestRunner() {
- 
-    
+
+
     // in order to avoid recurring setup and tear down call all tests are run in a single runner
-    std::pair<bool, std::string> success  = m_deviceClient->instantiate("testServerLock", "LockTestDevice", Hash("deviceId", "lockTest3"), KRB_TEST_MAX_TIMEOUT);
+    std::pair<bool, std::string> success = m_deviceClient->instantiate("testServerLock", "LockTestDevice", Hash("deviceId", "lockTest3"), KRB_TEST_MAX_TIMEOUT);
     CPPUNIT_ASSERT(success.first);
-    
-    success =  m_deviceClient->instantiate("testServerLock", "LockTestDevice", Hash("deviceId", "lockTest1", "controlledDevice", "lockTest3"), KRB_TEST_MAX_TIMEOUT);
+
+    success = m_deviceClient->instantiate("testServerLock", "LockTestDevice", Hash("deviceId", "lockTest1", "controlledDevice", "lockTest3"), KRB_TEST_MAX_TIMEOUT);
     CPPUNIT_ASSERT(success.first);
-    
+
     success = m_deviceClient->instantiate("testServerLock", "LockTestDevice", Hash("deviceId", "lockTest2", "controlledDevice", "lockTest3"), KRB_TEST_MAX_TIMEOUT);
     CPPUNIT_ASSERT(success.first);
-    
-  
-    
-    boost::this_thread::sleep(boost::posix_time::milliseconds(5000));
-    
+
     testLocking();
     testUnlocking();
     testRecursiveLocking();
@@ -62,90 +62,90 @@ void LockTest_Test::appTestRunner() {
     testLockStealing();
 }
 
+
 void LockTest_Test::testLocking() {
+
+    // This will lock "lockTest3" and work on it for 1s (asynchronously)
     m_deviceClient->executeNoWait("lockTest1", "lockAndWait");
-   
+    // We are waiting here to give the machinery time to really lock "lockTest3"
     boost::this_thread::sleep(boost::posix_time::milliseconds(100));
-    
-    std::pair<bool, std::string> success = m_deviceClient->execute("lockTest2", "lockAndWait", KRB_TEST_MAX_TIMEOUT);
-    CPPUNIT_ASSERT(success.first);
-    
-    CPPUNIT_ASSERT(success.second.find("Could not acquire lock on lockTest3") != std::string::npos);
-    std::clog<<"Tested locking.. Ok"<<std::endl;
-    
+
+    // This should fail, as lockAndWait is configured to throw once we are trying to also lock "lockTest3"
+    CPPUNIT_ASSERT_THROW(m_deviceClient->execute("lockTest2", "lockAndWait", KRB_TEST_MAX_TIMEOUT), Exception);
+    Exception::clearTrace();
+
+    std::clog << "Tested locking.. Ok" << std::endl;
+
     //wait til lock clears
-    while(m_deviceClient->get<std::string>("lockTest3", "lockedBy") != ""){};
-    
-    m_deviceClient->set("lockTest1", "waitTime", 20000);
-    boost::this_thread::sleep(boost::posix_time::milliseconds(1500));
+    while (m_deviceClient->get<std::string>("lockTest3", "lockedBy") != "") {
+    };
+
     m_deviceClient->executeNoWait("lockTest1", "lockAndWaitLong");
-    boost::this_thread::sleep(boost::posix_time::milliseconds(1500));
-    success = m_deviceClient->execute("lockTest2", "lockAndWaitTimeout", 10);
-    CPPUNIT_ASSERT(success.first);
-    CPPUNIT_ASSERT(success.second.find("Could not acquire lock on lockTest3") != std::string::npos);
-    std::clog<<"Tested locking with timeout (fail).. Ok"<<std::endl;
-    
-    success = m_deviceClient->execute("lockTest3", "slotClearLock");
+    // We are waiting here to give the machinery time to really lock "lockTest3"
+    boost::this_thread::sleep(boost::posix_time::milliseconds(100));
+
+    CPPUNIT_ASSERT_THROW(m_deviceClient->execute("lockTest2", "lockAndWaitTimeout", 10), Exception);
+    Exception::clearTrace();
+    std::clog << "Tested locking with timeout (fail).. Ok" << std::endl;
+
+    m_deviceClient->execute("lockTest3", "slotClearLock");
     //wait til lock clears
-    while(m_deviceClient->get<std::string>("lockTest3", "lockedBy") != ""){};
+    while (m_deviceClient->get<std::string>("lockTest3", "lockedBy") != "") {
+    };
+
     m_deviceClient->executeNoWait("lockTest1", "lockAndWait");
-    success = m_deviceClient->execute("lockTest2", "lockAndWaitTimeout", 10);
-    CPPUNIT_ASSERT(success.first);
-    CPPUNIT_ASSERT(success.second.find("Acquired Lock") != std::string::npos);
-    std::clog<<"Tested locking with timeout (success).. Ok"<<std::endl;
+
+    m_deviceClient->execute("lockTest2", "lockAndWaitTimeout", 10);
+    std::clog << "Tested locking with timeout (success).. Ok" << std::endl;
 
 }
+
 
 void LockTest_Test::testUnlocking() {
-    std::pair<bool, std::string> success = m_deviceClient->execute("lockTest3", "slotClearLock");
-    boost::this_thread::sleep(boost::posix_time::milliseconds(1000));
-    success = m_deviceClient->execute("lockTest2", "lockAndWait", KRB_TEST_MAX_TIMEOUT);
-    CPPUNIT_ASSERT(success.first);
-    CPPUNIT_ASSERT(success.second == "Acquired Lock");
-    std::clog<<"Tested unlocking.. Ok"<<std::endl;
+    m_deviceClient->execute("lockTest3", "slotClearLock");
+    while (m_deviceClient->get<std::string>("lockTest3", "lockedBy") != "") {
+    };
+    m_deviceClient->execute("lockTest2", "lockAndWait", KRB_TEST_MAX_TIMEOUT);
+    std::clog << "Tested unlocking.. Ok" << std::endl;
 
 }
+
 
 void LockTest_Test::testRecursiveLocking() {
-    std::pair<bool, std::string> success = m_deviceClient->execute("lockTest3", "slotClearLock");
-    
-   
-    success = m_deviceClient->execute("lockTest1", "lockAndWaitRecursiveFail", KRB_TEST_MAX_TIMEOUT);
-    CPPUNIT_ASSERT(success.first);
-    CPPUNIT_ASSERT(success.second.find("Could not acquire lock on lockTest3") != std::string::npos);
-    
-    success = m_deviceClient->execute("lockTest3", "slotClearLock");
-    boost::this_thread::sleep(boost::posix_time::milliseconds(100));
+    m_deviceClient->execute("lockTest3", "slotClearLock");
+    CPPUNIT_ASSERT_THROW(m_deviceClient->execute("lockTest1", "lockAndWaitRecursiveFail", KRB_TEST_MAX_TIMEOUT), Exception);
+    Exception::clearTrace();
+    m_deviceClient->execute("lockTest3", "slotClearLock");
     //recursive succeeds
-    
-    success = m_deviceClient->execute("lockTest1", "lockAndWaitRecursive", KRB_TEST_MAX_TIMEOUT);
-    CPPUNIT_ASSERT(success.first);
-    CPPUNIT_ASSERT(success.second == "Acquired Lock");
-    std::clog<<"Tested recursive locking.. Ok"<<std::endl;
+    m_deviceClient->execute("lockTest1", "lockAndWaitRecursive", KRB_TEST_MAX_TIMEOUT);
+    std::clog << "Tested recursive locking.. Ok" << std::endl;
 }
 
-void LockTest_Test::testSettingOnLocked(){
-    std::pair<bool, std::string> success = m_deviceClient->execute("lockTest3", "slotClearLock");
+
+void LockTest_Test::testSettingOnLocked() {
+    m_deviceClient->execute("lockTest3", "slotClearLock");
     m_deviceClient->executeNoWait("lockTest1", "lockAndWait");
-   
-    boost::this_thread::sleep(boost::posix_time::milliseconds(100));
-    success = m_deviceClient->set("lockTest3", "intProperty", 100);
-    CPPUNIT_ASSERT(!success.first);
+    // We are waiting here to give the machinery time to really lock "lockTest3"
+    boost::this_thread::sleep(boost::posix_time::milliseconds(100));   
+    CPPUNIT_ASSERT_THROW(m_deviceClient->set("lockTest3", "intProperty", 100), Exception);
+    Exception::clearTrace();
+
     const int value = m_deviceClient->get<int>("lockTest3", "intProperty");
     CPPUNIT_ASSERT(value <= 5);
-    std::clog<<"Tested rejection of setting on locked.. Ok"<<std::endl;
+    std::clog << "Tested rejection of setting on locked.. Ok" << std::endl;
 }
 
-void LockTest_Test::testLockStealing(){
-    std::pair<bool, std::string> success = m_deviceClient->execute("lockTest3", "slotClearLock");
-     boost::this_thread::sleep(boost::posix_time::milliseconds(200));
+
+void LockTest_Test::testLockStealing() {
+    m_deviceClient->execute("lockTest3", "slotClearLock");
+    boost::this_thread::sleep(boost::posix_time::milliseconds(100));
     m_deviceClient->executeNoWait("lockTest1", "lockAndWait");
     boost::this_thread::sleep(boost::posix_time::milliseconds(100));
-    m_deviceClient->executeNoWait("lockTest3", "slotClearLock", KRB_TEST_MAX_TIMEOUT);
-    boost::this_thread::sleep(boost::posix_time::milliseconds(1500));
-    const std::string& errorMsg = m_deviceClient->get<std::string>("lockTest1", "errorMessage");
-    CPPUNIT_ASSERT(errorMsg.find("Lock was invalidated!") != std::string::npos);
-    std::clog<<"Tested stolen lock exception.. Ok"<<std::endl;
+    m_deviceClient->executeNoWait("lockTest3", "slotClearLock");
+    boost::this_thread::sleep(boost::posix_time::milliseconds(100));
+    m_deviceClient->set("lockTest3", "intProperty", 100);
+    CPPUNIT_ASSERT(m_deviceClient->get<int>("lockTest3", "intProperty") == 100);
+    std::clog << "Tested stolen lock exception.. Ok" << std::endl;
 }
 
 
