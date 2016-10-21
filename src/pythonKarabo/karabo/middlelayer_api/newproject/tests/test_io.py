9da76e6b2b2bbc3b2b4f40aa85a88eea8a4a7e2f
John Wiggins <john.wiggins@xfel.eu>
2016-10-21 16:44:58 +0200

    Add ProjectDBCache and a lazy object loader func
    These pieces are easy to decouple from the rest of the project IO code.

--- src/pythonKarabo/karabo/middlelayer_api/newproject/tests/test_io.py
+++ src/pythonKarabo/karabo/middlelayer_api/newproject/tests/test_io.py
@@ -1,33 +1,14 @@
 from contextlib import contextmanager
-from io import BytesIO
 import os.path as op
 from tempfile import TemporaryDirectory
 
 from traits.api import Dict, Enum, Int, String
 
-from karabo.common.project.api import ProjectModel, PROJECT_OBJECT_CATEGORIES
+from karabo.common.project.api import (ProjectDBCache, read_lazy_object,
+                                       PROJECT_OBJECT_CATEGORIES)
 from karabo.middlelayer import Project
-import karabo.testing as testing_mod
 from ..convert import convert_old_project
 from ..io import read_project_model, write_project_model
-
-TEST_PROJECT_PATH = op.join(op.dirname(testing_mod.__file__), 'resources',
-                            'reference.krb')
-
-
-class _ProjectStorage(object):
-    """ An object to stand in for the project database
-    """
-    def __init__(self, dirpath):
-        self.dirpath = dirpath
-
-    def store(self, name, data):
-        with open(op.join(self.dirpath, name), mode='wb') as fp:
-            fp.write(data)
-
-    def retrieve(self, name):
-        with open(op.join(self.dirpath, name), mode='rb') as fp:
-            return fp.read()
 
 
 def _compare_projects(proj0, proj1):
@@ -61,7 +42,10 @@ def _compare_projects(proj0, proj1):
 
 
 def _get_old_project():
-    old_project = Project(TEST_PROJECT_PATH)
+    import karabo.testing as mod
+
+    path = op.join(op.dirname(mod.__file__), 'resources', 'reference.krb')
+    old_project = Project(path)
     old_project.unzip()
     return old_project
 
@@ -69,36 +53,18 @@ def _get_old_project():
 @contextmanager
 def _project_storage():
     with TemporaryDirectory() as dirpath:
-        yield _ProjectStorage(dirpath)
-
-
-def _read_project(storage, name):
-    data = storage.retrieve(name)
-    lazy_project = read_project_model(BytesIO(data))
-    project = ProjectModel(uuid=lazy_project.uuid,
-                           version=lazy_project.version)
-
-    for childname in PROJECT_OBJECT_CATEGORIES:
-        lazy_children = getattr(lazy_project, childname)
-        industrious_children = []
-        for child in lazy_children:
-            name = child.uuid
-            data = storage.retrieve(name)
-            industrious_children.append(read_project_model(BytesIO(data)))
-        setattr(project, childname, industrious_children)
-
-    return project
+        yield ProjectDBCache(dirpath)
 
 
 def _write_project(project, storage):
     for childname in PROJECT_OBJECT_CATEGORIES:
         children = getattr(project, childname)
         for child in children:
-            name = child.uuid
-            storage.store(name, write_project_model(child))
+            data = write_project_model(child)
+            storage.store(child.uuid, child.version, data)
 
-    name = project.uuid
-    storage.store(name, write_project_model(project))
+    data = write_project_model(project)
+    storage.store(project.uuid, project.version, data)
 
 # -----------------------------------------------------------------------------
 
@@ -117,7 +83,7 @@ def test_project_round_trip():
 
     with _project_storage() as storage:
         _write_project(project, storage)
-        name = project.uuid
-        rt_project = _read_project(storage, name)
+        rt_project = read_lazy_object(project.uuid, project.version, storage,
+                                      read_project_model)
 
     _compare_projects(project, rt_project)
