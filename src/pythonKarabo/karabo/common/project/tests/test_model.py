437969ddab84910bd6c803025620b605a1b83f04
John Wiggins <john.wiggins@xfel.eu>
2016-10-24 13:52:18 +0200

    Implement a visitation function for Projects
    This will allow repetitive actions to be performed on all children of a project

---
+++ src/pythonKarabo/karabo/common/project/tests/test_model.py
@@ -0,0 +1,38 @@
+from karabo.common.project.api import (
+    ProjectModel, MacroModel, visit_project_objects)
+
+
+def test_project_model_visitor():
+    visited = []
+
+    def visitor(obj):
+        visited.append(obj)
+
+    macro = MacroModel(code="print('hello world')")
+    inner_proj = ProjectModel(macros=[macro])
+    proj = ProjectModel(subprojects=[inner_proj])
+
+    visit_project_objects(proj, visitor)
+    assert visited == [macro, inner_proj, proj]
+
+    del visited[:]
+    visit_project_objects(inner_proj, visitor)
+    assert visited == [macro, inner_proj]
+
+
+def test_project_modified_obj_check():
+    modified = []
+
+    def find_modified(obj):
+        if obj.modified:
+            modified.append(obj)
+
+    macro = MacroModel(code="print('hello world')")
+    proj = ProjectModel(macros=[macro])
+
+    visit_project_objects(proj, find_modified)
+    assert len(modified) == 0
+
+    macro.code = "print('I was modified!')"
+    visit_project_objects(proj, find_modified)
+    assert modified == [macro]
