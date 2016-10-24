2d34ccea41f8c2f43fb736a81121bfe808711741
John Wiggins <john.wiggins@xfel.eu>
2016-10-24 12:16:31 +0200

    Add a modified flag to project objects
    Project objects automatically record when they have been modified. It is
    however not currently possible to detect changes which are later reverted.

--- src/pythonKarabo/karabo/common/project/bases.py
+++ src/pythonKarabo/karabo/common/project/bases.py
@@ -5,7 +5,7 @@
 #############################################################################
 import uuid
 
-from traits.api import HasStrictTraits, Dict, Int, String
+from traits.api import HasStrictTraits, Bool, Dict, Int, String
 
 
 class BaseProjectObjectModel(HasStrictTraits):
@@ -15,12 +15,15 @@ class BaseProjectObjectModel(HasStrictTraits):
     # A simple, human-readable name. Doesn't need to be unique
     simple_name = String
 
+    # When True, the object contains unsaved data
+    modified = Bool(False, transient=True)
+
     # Version and unique id
-    version = Int
+    version = Int(transient=True)
     uuid = String
 
     # Database-provided attributes which need to be preserved
-    db_attrs = Dict
+    db_attrs = Dict(transient=True)
 
     def _uuid_default(self):
         """If a uuid isn't supplied, generate one
@@ -39,3 +42,15 @@ class BaseProjectObjectModel(HasStrictTraits):
             # Reset to a safe value
             self.uuid = old
             raise
+
+    def _anytrait_changed(self, name, old, new):
+        """ Listen for changes to all non-transient, non-property traits and
+        mark the object as modified accordingly.
+        """
+        if not self.traits_inited():
+            return
+
+        # copyable_trait_names() returns all the trait names which contain
+        # data which should be persisted (or copied when making a deep copy).
+        if name in self.copyable_trait_names():
+            self.modified = True
