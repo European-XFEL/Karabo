098dc89cfa988f99b0824cd4dcab0798f5fb4e8f
John Wiggins <john.wiggins@xfel.eu>
2016-10-24 13:52:18 +0200

    Implement a visitation function for Projects
    This will allow repetitive actions to be performed on all children of a project

--- src/pythonKarabo/karabo/common/project/model.py
+++ src/pythonKarabo/karabo/common/project/model.py
@@ -22,4 +22,38 @@ class ProjectModel(BaseProjectObjectModel):
     monitors = List(Instance(MonitorModel))
     scenes = List(Instance(SceneModel))
     servers = List(Instance(DeviceServerModel))
-    subprojects = List(Instance('ProjectModel'))
+
+# This is outside the class because `ProjectModel` isn't available until here
+ProjectModel.add_class_trait('subprojects', List(Instance(ProjectModel)))
+
+
+def visit_project_objects(project, visitor_func):
+    """ Recursively visit all children of a project model object.
+
+    :param project: A project model instance
+    :param visitor_func: A callable which takes a single BaseProjectObjectModel
+                         instance as its only argument.
+    """
+    def _find_iterables(obj):
+        """ Find all traits which are lists of BaseProjectObjectModel instances
+        """
+        iterables = []
+        for name, t in obj.traits().items():
+            if isinstance(t.trait_type, List):
+                inner_type = t.inner_traits[0].trait_type
+                if (isinstance(inner_type, Instance) and
+                        issubclass(inner_type.klass, BaseProjectObjectModel)):
+                    iterables.append(name)
+        return iterables
+
+    def _walk_obj(obj):
+        iterables = _find_iterables(obj)
+        for name in iterables:
+            children = getattr(obj, name)
+            for child in children:
+                for subchild in _walk_obj(child):
+                    yield subchild
+        yield obj
+
+    for obj in _walk_obj(project):
+        visitor_func(obj)
