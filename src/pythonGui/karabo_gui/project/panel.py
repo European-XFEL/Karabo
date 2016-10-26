bcf99f3e51a7b671f0d7452078be5d20bdac6598
John Wiggins <john.wiggins@xfel.eu>
2016-10-26 15:06:35 +0200

    Stub out a project-runner applet [ci skip]
    This will give us a sandbox to develop the new project GUI inside of

---
+++ src/pythonGui/karabo_gui/project/panel.py
@@ -0,0 +1,35 @@
+#############################################################################
+# Author: <john.wiggins@xfel.eu>
+# Created on October 26, 2016
+# Copyright (C) European XFEL GmbH Hamburg. All rights reserved.
+#############################################################################
+from PyQt4.QtGui import QWidget, QStackedLayout
+
+from karabo_gui.docktabwindow import Dockable
+
+
+class ProjectPanel(Dockable, QWidget):
+    """ A Dockable panel which contains a view of the project
+    """
+    def __init__(self, project_view):
+        super(ProjectPanel, self).__init__()
+
+        title = "Projects"
+        self.setWindowTitle(title)
+
+        self.project_view = project_view
+        layout = QStackedLayout(self)
+        layout.addWidget(self.project_view)
+        self.setLayout(layout)
+
+    def onDock(self):
+        pass
+
+    def onUndock(self):
+        pass
+
+    def setupToolBars(self, toolbar, widget):
+        pass
+
+    def notifyTabVisible(self, visible):
+        pass
