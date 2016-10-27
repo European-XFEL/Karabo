a9d6a81f9351dc12734b346bd74c16a847dba412
Kerstin Weger <kerstin.weger@xfel.eu>
2016-10-27 20:28:22 +0200

    Add missing dialog files and consider MR suggestions

--- src/pythonGui/karabo_gui/util.py
+++ src/pythonGui/karabo_gui/util.py
@@ -3,9 +3,7 @@ import os
 from tempfile import mkstemp
 from uuid import uuid4
 
-from PyQt4.QtGui import QAction, QDialog, QFileDialog, QIcon
-
-from traits.api import HasStrictTraits, Bool, Callable, Instance, String
+from PyQt4.QtGui import QDialog, QFileDialog
 
 from karabo.middlelayer import Hash, MetricPrefix, Unit
 import karabo_gui.globals as globals
@@ -129,40 +127,3 @@ def temp_file(suffix='', prefix='tmp', dir=None):
     finally:
         os.close(fd)
         os.unlink(filename)
-
-
-class KaraboAction(HasStrictTraits):
-    """ Base class for actions
-    """
-    # The icon for this action
-    icon = Instance(QIcon)
-    # The text label for the action
-    text = String
-    # The tooltip text shown when hovering over the action
-    tooltip = String
-    # Whether or not this action is checkable
-    checkable = Bool(False)
-    # Whether or not this action is checked
-    is_checked = Bool(False)
-    # Defines the method which is called whenever the action is triggered
-    triggered = Callable
-
-
-def build_qaction(karabo_action, parent):
-    """ Create a QAction from given ``karabo_action``
-
-    NOTE: since the callback for the ``triggered`` Callable should be defined
-    where it is needed and might have different signatures, the actual
-    ``connect`` of a QAction should be done after calling this function.
-
-    :param karabo_action: An object of type ``KaraboAction``
-    """
-    q_action = QAction(karabo_action.text, parent)
-    if karabo_action.icon is not None:
-        q_action.setIcon(karabo_action.icon)
-    q_action.setCheckable(karabo_action.checkable)
-    if karabo_action.checkable:
-        q_action.setChecked(karabo_action.is_checked)
-    q_action.setStatusTip(karabo_action.tooltip)
-    q_action.setToolTip(karabo_action.tooltip)
-    return q_action
