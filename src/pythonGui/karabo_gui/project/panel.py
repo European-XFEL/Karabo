4f9c79063dec742ec9b2cfe119a6399921bca2ea
Kerstin Weger <kerstin.weger@xfel.eu>
2016-10-27 12:48:44 +0200

    Add toolbar actions to project panel and skeletons for the dialogs [ci skip]

--- src/pythonGui/karabo_gui/project/panel.py
+++ src/pythonGui/karabo_gui/project/panel.py
@@ -3,13 +3,16 @@
 # Created on October 26, 2016
 # Copyright (C) European XFEL GmbH Hamburg. All rights reserved.
 #############################################################################
-from PyQt4.QtGui import QWidget, QStackedLayout
+from PyQt4.QtGui import QStackedLayout, QWidget
 
+from .dialogs import LoadDialog, SaveDialog
+import karabo_gui.icons as icons
 from karabo_gui.docktabwindow import Dockable
+from karabo_gui.util import build_qaction, KaraboAction
 
 
 class ProjectPanel(Dockable, QWidget):
-    """ A Dockable panel which contains a view of the project
+    """ A dockable panel which contains a view of the project
     """
     def __init__(self, project_view):
         super(ProjectPanel, self).__init__()
@@ -22,11 +25,57 @@ class ProjectPanel(Dockable, QWidget):
         layout.addWidget(self.project_view)
         self.setLayout(layout)
 
+    def _create_actions(self):
+        """ Create actions and return list of them"""
+        new = KaraboAction(
+            icon=icons.new,
+            text="&New Project",
+            tooltip="Create a New (Sub)project",
+            triggered=project_new_handler,
+        )
+        open = KaraboAction(
+            icon=icons.open,
+            text="&Open Project",
+            tooltip="Open an Existing Project",
+            triggered=project_open_handler,
+        )
+        save = KaraboAction(
+            icon=icons.save,
+            text="&Save Project",
+            tooltip="Save Project Snapshot",
+            triggered=project_save_handler,
+        )
+
+        qactions = []
+        for k_action in (new, open, save):
+            q_ac = build_qaction(k_action, self)
+            q_ac.triggered.connect(k_action.triggered)
+            qactions.append(q_ac)
+        return qactions
+
+    def setupToolBars(self, toolbar, widget):
+        """ Setup the project specific toolbar """
+        qactions = self._create_actions()
+        for ac in qactions:
+            toolbar.addAction(ac)
+
     def onDock(self):
         pass
 
     def onUndock(self):
         pass
 
-    def setupToolBars(self, toolbar, widget):
-        pass
+
+def project_new_handler():
+    dialog = LoadDialog()
+    dialog.exec()
+
+
+def project_open_handler():
+    dialog = LoadDialog()
+    dialog.exec()
+
+
+def project_save_handler():
+    dialog = SaveDialog()
+    dialog.exec()
