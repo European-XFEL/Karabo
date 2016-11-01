3baf9c98746f8d68e1cb39e6e5d8f317c4eb3cf4
John Wiggins <john.wiggins@xfel.eu>
2016-11-01 16:31:20 +0100

    Restructure the new project data model.
    Device servers now have some structure to them and device configurations are
    much simpler.

--- src/pythonKarabo/karabo/common/project/device.py
+++ src/pythonKarabo/karabo/common/project/device.py
@@ -3,7 +3,7 @@
 # Created on October 12, 2016
 # Copyright (C) European XFEL GmbH Hamburg. All rights reserved.
 #############################################################################
-from traits.api import Enum, Instance, String
+from traits.api import Instance, String
 
 from .bases import BaseProjectObjectModel
 
@@ -11,8 +11,5 @@ from .bases import BaseProjectObjectModel
 class DeviceConfigurationModel(BaseProjectObjectModel):
     """ A single device configuration
     """
-    configuration = Instance(object)
-    server_id = String
     class_id = String
-    instance_id = String
-    if_exists = Enum('ignore', 'restart')
+    configuration = Instance(object)
