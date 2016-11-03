d0b7c42289ba341fcdb669258c789ea6ea21621b
Martin Teichmann <martin.teichmann@xfel.eu>
2016-11-03 15:19:39 +0100

    allow setting of loglevel in KaraboErrors

--- src/pythonKarabo/karabo/middlelayer_api/exceptions.py
+++ src/pythonKarabo/karabo/middlelayer_api/exceptions.py
@@ -1,6 +1,11 @@
+import logging
 
 class KaraboError(Exception):
     """A :class:`KaraboError` is raised if an error occurs which is
     specific to Karabo. This is mostly because things went wrong on
     the other end of a network connection.
     """
+
+    def __init__(self, *args, loglevel=logging.ERROR, **kwargs):
+        super(KaraboError, self).__init__(*args, **kwargs)
+        self.loglevel = loglevel
