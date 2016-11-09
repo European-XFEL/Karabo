3ab458a1926a503aded1a01723ad420a4248c53e
Andrea Parenti <andrea.parenti@xfel.eu>
2016-11-09 11:45:25 +0100

    Fix camera_fsm.py
    - Fix camera_fsm.py.
    - Add unit tests for start_stop_fsm and camera_fsm.
    - Replaced print statements in FSM tests with logger calls

--- src/pythonKarabo/karabo/bound_api/tests/ok_error_fsm_test.py
+++ src/pythonKarabo/karabo/bound_api/tests/ok_error_fsm_test.py
@@ -4,34 +4,39 @@
 
 import unittest
 
-from karabo.bound import OkErrorFsm, State
+from karabo.bound import OkErrorFsm, Hash, Logger, State
 
 
 class OkErrorUser(OkErrorFsm):
+    config = Hash("priority", "DEBUG")
+    Logger.configure(config)
+    log = Logger.getCategory()
+
+
     def __init__(self, configuration):
         super(OkErrorUser, self).__init__(configuration)
 
     # The following 2 methods should be always defined 
     def noStateTransition(self):
-        print("-- OkErrorUser.noStateTransition")
+        self.log.DEBUG("-- OkErrorUser.noStateTransition")
         
     def updateState(self, currentState):
-        print("-- OkErrorUser.updateState to '{}'".format(currentState))
+        self.log.DEBUG("-- OkErrorUser.updateState to '{}'".format(currentState))
     
     def errorStateOnEntry(self):
-        print("-- OkErrorUser.errorStateOnEntry")
+        self.log.DEBUG("-- OkErrorUser.errorStateOnEntry")
         
     def errorStateOnExit(self):
-        print("-- OkErrorUser.errorStateOnExit")
+        self.log.DEBUG("-- OkErrorUser.errorStateOnExit")
 
     def resetAction(self):
-        print("-- OkErrorUser.resetAction")
+        self.log.DEBUG("-- OkErrorUser.resetAction")
         
     def okStateOnEntry(self):
-        print("-- OkErrorUser.okStateOnEntry")
+        self.log.DEBUG("-- OkErrorUser.okStateOnEntry")
         
     def okStateOnExit(self):
-        print("-- OkErrorUser.okStateOnExit")
+        self.log.DEBUG("-- OkErrorUser.okStateOnExit")
         
     
 class  Ok_error_fsm_TestCase(unittest.TestCase):
@@ -48,12 +53,12 @@ class  Ok_error_fsm_TestCase(unittest.TestCase):
         fsm = self.okerr.fsm
         fsm.start()
         self.assertIs(fsm.get_state(), State.NORMAL)
-        print ("*** State 'Ok' reached")
+        self.okerr.log.DEBUG ("*** State 'Ok' reached")
         self.okerr.errorFound("user error message", "detailed error message")
         self.assertEqual(fsm.get_state(), State.ERROR, "Assert failed")
-        print ("*** State 'Error' reached")
+        self.okerr.log.DEBUG ("*** State 'Error' reached")
         self.okerr.reset()
-        print ("*** State 'Ok' reached")
+        self.okerr.log.DEBUG ("*** State 'Ok' reached")
         self.assertEqual(fsm.get_state(), State.NORMAL, "Assert failed")
         
 
