1f1769321693457b93d4fd949c907a238ec751db
Martin Teichmann <martin.teichmann@xfel.eu>
2016-11-09 14:00:39 +0100

    improve middle layer integration test
    this make the test test things which are actually interesting

--- src/pythonKarabo/karabo/integration_tests/device_comm_test/middlelayertestdevice.py
+++ src/pythonKarabo/karabo/integration_tests/device_comm_test/middlelayertestdevice.py
@@ -1,23 +1,11 @@
-from asyncio import async, coroutine
-from itertools import count
+from asyncio import coroutine
 
-from karabo.middlelayer import Device, getDevice, Int32, sleep, Slot
+from karabo.middlelayer import Device, getDevice, Int32, sleep
 
 
 class MiddleLayerTestDevice(Device):
     something = Int32(defaultValue=333)
     counter = Int32(defaultValue=-1)
-
-    @coroutine
-    def do_count(self):
-        for i in count():
-            self.counter = i
-            yield from sleep(0.1)
-
-    @Slot()
-    @coroutine
-    def count(self):
-        async(self.do_count())
 
     @coroutine
     def onInitialization(self):
