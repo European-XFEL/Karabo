891918f33d68b61d547d248d201574218c8096fe
Dennis Goeries <dennis.goeries@xfel.eu>
2016-11-18 15:45:40 +0100

    version gui

--- src/pythonGui/setup.py
+++ src/pythonGui/setup.py
@@ -6,9 +6,9 @@ import sys
 
 from setuptools import setup, find_packages
 
-MAJOR = 1
-MINOR = 5
-MICRO = 0
+MAJOR = 2
+MINOR = 0
+MICRO = 2
 IS_RELEASED = False
 VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
 VERSION_FILE_PATH = op.join('karabo_gui', '_version.py')
