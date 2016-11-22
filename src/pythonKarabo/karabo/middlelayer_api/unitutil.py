443fcc134133285bea1f82a3298c0595e7f57867
Martin Teichmann <martin.teichmann@xfel.eu>
2016-11-22 10:54:05 +0100

    add unit-aware numpy equivalents
    most numpy functions (namely: ufuncs) work out-of-the-box with units.
    Some don't. Let's write equivalents for them, until they appear upstream
    (I'm working on that...)

---
+++ src/pythonKarabo/karabo/middlelayer_api/unitutil.py
@@ -0,0 +1,47 @@
+from functools import wraps
+import numpy
+
+from .basetypes import QuantityValue
+
+
+@wraps(numpy.linspace)
+def linspace(start, stop, *args, **kwargs):
+    if not (isinstance(start, QuantityValue) or
+            isinstance(stop, QuantityValue)):
+        return numpy.linspace(start, stop, *args, **kwargs)
+
+    if (start == 0) is True:
+        return numpy.linspace(start, stop.value, *args, **kwargs) * stop.units
+
+    if (stop == 0) is not True:
+        stop = float(stop / start.units)
+
+    return numpy.linspace(start.magnitude, stop, *args, **kwargs) * start.units
+
+
+@wraps(numpy.dot)
+def dot(a, b, *args, **kwargs):
+    if not (isinstance(a, QuantityValue) or isinstance(b, QuantityValue)):
+        return numpy.dot(a, b, *args, **kwargs)
+
+    if not isinstance(a, QuantityValue):
+        a = QuantityValue(a)
+
+    if not isinstance(b, QuantityValue):
+        b = QuantityValue(b)
+
+    return numpy.dot(a.magnitude, b.magnitude) * (a.units * b.units)
+
+
+@wraps(numpy.cross)
+def cross(a, b, *args, **kwargs):
+    if not (isinstance(a, QuantityValue) or isinstance(b, QuantityValue)):
+        return numpy.cross(a, b, *args, **kwargs)
+
+    if not isinstance(a, QuantityValue):
+        a = QuantityValue(a)
+
+    if not isinstance(b, QuantityValue):
+        b = QuantityValue(b)
+
+    return numpy.cross(a.magnitude, b.magnitude) * (a.units * b.units)
