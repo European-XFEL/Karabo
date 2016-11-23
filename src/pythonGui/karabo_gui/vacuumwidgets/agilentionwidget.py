24431d8da4643dfa23536ab1361af7d537f27896
Dennis Goeries <dennis.goeries@xfel.eu>
2016-11-23 19:12:08 +0100

    comments

--- src/pythonGui/karabo_gui/vacuumwidgets/agilentionwidget.py
+++ src/pythonGui/karabo_gui/vacuumwidgets/agilentionwidget.py
@@ -1,12 +1,3 @@
-#############################################################################
-# Author: <kerstin.weger@xfel.eu>
-# Created on October 25, 2012
-# Copyright (C) European XFEL GmbH Hamburg. All rights reserved.
-#############################################################################
-"""This module contains a class which represents a widget plugin for attributes
-   and is created by the factory class VacuumWidget.
-"""
-
 from karabo.common.states import State
 from karabo_gui.widget import VacuumWidget
 
