82c9e94fc0a0cef9b5867723874ade6f8f70d02e
Dennis Goeries <dennis.goeries@xfel.eu>
2016-11-23 19:14:51 +0100

    weger comment

--- src/pythonGui/karabo_gui/sceneview/widget/vacuum.py
+++ src/pythonGui/karabo_gui/sceneview/widget/vacuum.py
@@ -14,7 +14,6 @@ _VACUUM_WIDGETS = {n: Widget.widgets[n] for n in _VACUUM_CLASS_NAMES}
 class VacuumWidgetContainer(BaseWidgetContainer):
     """ A container for VacuumWidgets
     """
-
     def _create_widget(self, boxes):
         factory = _VACUUM_WIDGETS[self.model.klass]
         return factory(boxes[0], self)
