8c2447608108b21c342cce28aa6c4a6aaf18b66d
Kerstin Weger <kerstin.weger@xfel.eu>
2016-11-24 14:15:39 +0100

    Adapt device dialog to be able to store the different configurations

--- src/pythonKarabo/karabo/common/project/server.py
+++ src/pythonKarabo/karabo/common/project/server.py
@@ -20,6 +20,7 @@ class DeviceInstanceModel(HasStrictTraits):
     # If the device is already online, should it be ignored or restarted?
     if_exists = Enum('ignore', 'restart')
     # A list of references to possible configurations
+    # XXX use Dict here instead for faster lookup?
     configs = List(Instance(DeviceConfigurationModel))
     # UUID/Rev of the currently active configuration
     active_config_ref = Tuple(String, Int)
