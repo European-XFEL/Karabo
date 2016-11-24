a5a578448aa950ecf77be82f7cbd1798f18b680f
Martin Teichmann <martin.teichmann@xfel.eu>
2016-11-24 16:45:32 +0100

    document numeric module

--- src/pythonKarabo/karabo/numeric.py
+++ src/pythonKarabo/karabo/numeric.py
@@ -1,1 +1,22 @@
+"""
+Unit-aware replacements for :py:mod:`numpy` functions
+=====================================================
+
+Most :py:mod:`numpy` functions (namely: ufuncs) support Karabo values
+out-of-the-box.
+Some, however, do not. Karabo defines its own drop-in replacements for those
+functions in the :py:mod:`karabo.numeric` module. These are:
+
+.. function:: karabo.numeric.linspace
+
+    See :py:func:`numpy.linspace`
+
+.. function:: karabo.numeric.dot
+
+    See :py:func:`numpy.dot`
+
+.. function:: karabo.numeric.cross
+
+    See :py:func:`numpy.cross`
+"""
 from .numeric_api.unitutil import cross, dot, linspace
