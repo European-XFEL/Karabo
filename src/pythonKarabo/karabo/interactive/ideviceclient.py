de51a1d98bc1a7663955adc8bc443a7c6a17e7d5
Gero Flucke <gero.flucke@xfel.eu>
2016-11-28 13:43:25 +0100

    Also bound Python command line needs EventLoop to run.

--- src/pythonKarabo/karabo/interactive/ideviceclient.py
+++ src/pythonKarabo/karabo/interactive/ideviceclient.py
@@ -1,10 +1,19 @@
 from IPython import start_ipython
 
+from threading import Thread
+from karathon import EventLoop
 
 def main():
+    # We need the (C++) event loop in the background:
+    loopThread = Thread(target=EventLoop.work)
+    loopThread.start()
+
     code_to_run = 'from karabo.interactive.deviceClient import *'
     start_ipython(code_to_run=code_to_run, force_interact=True,
                   display_banner=False)
+
+    EventLoop.stop()
+    loopThread.join()
 
 if __name__ == '__main__':
     main()
