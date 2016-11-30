c6867628abb3c9f0b7d19657ef65ce4f08336840
Kerstin Weger <kerstin.weger@xfel.eu>
2016-11-30 14:43:01 +0100

    Fix test in middlelayer_api.newproject

--- src/pythonKarabo/karabo/common/scenemodel/io.py
+++ src/pythonKarabo/karabo/common/scenemodel/io.py
@@ -43,4 +43,4 @@ def write_single_model(model):
 def _writer_core(model, root):
     writer = get_writer()
     writer(model, root)
-    return tostring(root, encoding='utf-8')
+    return tostring(root, encoding='unicode')
