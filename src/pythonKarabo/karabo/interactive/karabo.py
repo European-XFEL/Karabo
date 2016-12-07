a836c518ebcb17331af5de07fe59bf701fa6a57a
Burkhard Heisen <burkhard.heisen@xfel.eu>
2016-12-07 15:18:21 +0100

    Added dependency tracking and from-file installation to karabo script.
    - Dependencies are automatically installed if DEPENDS file is there
    - Two more subcommands got introduced (install-file, uninstall-file)

--- src/pythonKarabo/karabo/interactive/karabo.py
+++ src/pythonKarabo/karabo/interactive/karabo.py
@@ -4,13 +4,17 @@ import sys
 import os
 import contextlib
 import multiprocessing
+import csv
 
 from karabo.packaging.device_template import configure_template
 
 DEV_NULL = open(os.devnull, 'w')
+INVOKE_DIR = ''
 
 
 def run_local():
+    global INVOKE_DIR
+    INVOKE_DIR = os.getcwd()
     try:
         karabo_dir = os.environ['KARABO']
         os.chdir(karabo_dir)
@@ -30,14 +34,20 @@ def run_cmd(cmd):
         print("Problem with system call: {}".format(e))
         exit()
 
-
-@contextlib.contextmanager
-def pushd_popd():
-    old_path = os.getcwd()
-    try:
-        yield
-    finally:
-        os.chdir(old_path)
+try:
+    @contextlib.contextmanager
+    def pushd_popd(path=os.environ['KARABO']):
+        old_path = os.getcwd()
+        os.chdir(path)
+        try:
+            yield
+        finally:
+            os.chdir(old_path)
+except KeyError:
+    print('$KARABO is not defined. Make sure you have sourced the '
+          'activate script for the Karabo Framework which you would '
+          'like to use.')
+    sys.exit(1)
 
 
 def parse_commandline():
@@ -105,6 +115,22 @@ def parse_commandline():
                             default='Debug',
                             help='Build configuration {Debug|Release}')
 
+    parser_insf = sps.add_parser('install-file',
+                                 help='Installs devices as given in the '
+                                 'configuration file')
+    parser_insf.set_defaults(command=install_file)
+
+    parser_insf.add_argument('filename',
+                             type=str,
+                             help='Path to the configuration file')
+
+    parser_insf.add_argument('-c', '--config',
+                             type=str,
+                             metavar='',
+                             choices=['Debug', 'Release'],
+                             default='Debug',
+                             help='Build configuration {Debug|Release}')
+
     parser_uins = sps.add_parser('uninstall',
                                  help='Uninstalls an existing device')
     parser_uins.set_defaults(command=uninstall)
@@ -112,6 +138,15 @@ def parse_commandline():
     parser_uins.add_argument('device',
                              type=str,
                              help='The name of the device package')
+
+    parser_uinsf = sps.add_parser('uninstall-file',
+                                  help='Uninstalls devices as given in the '
+                                       'configuration file')
+    parser_uinsf.set_defaults(command=uninstall_file)
+
+    parser_uinsf.add_argument('filename',
+                              type=str,
+                              help='Path to the configuration file')
 
     parser_dev = sps.add_parser('develop',
                                 help='Activates develop mode for a given '
@@ -204,7 +239,7 @@ def checkout(args):
 
 def install(args):
     with pushd_popd():
-        path = os.path.join('tmp', args.device)
+        path = os.path.join('installed', args.device)
         if os.path.isdir(path):
             run_cmd('rm -rf {}'.format(path))
         os.makedirs(path, exist_ok=True)
@@ -214,18 +249,43 @@ def install(args):
                 .format(args.git, args.device, args.tag, path))
         print('done.')
         os.chdir(path)
+        if os.path.isfile('DEPENDS'):
+            install_dependencies(args)
         if os.path.isfile('Makefile'):
             print('Compiling... [please wait] ', end='', flush=True)
             run_cmd('make CONF={} -j{}'.format(args.config, args.jobs))
             print('done.')
-            src = os.path.join('dist', args.config, '*', '*.so')
-            tgt = os.path.join('..', '..', 'plugins')
-            run_cmd('cp -f {} {}'.format(src, tgt))
+            if os.path.isdir('dist'):
+                src = os.path.join('dist', args.config, '*', '*.so')
+                tgt = os.path.join('..', '..', 'plugins')
+                run_cmd('cp -f {} {}'.format(src, tgt))
         else:
             run_cmd('pip install --upgrade .')
         print("Installation succeeded.")
 
-    run_cmd('rm -rf tmp')
+
+def install_file(args):
+    global INVOKE_DIR
+    filename = os.path.join(INVOKE_DIR, args.filename)
+    devices = parse_configuration_file(filename)
+    for item in devices:
+        setattr(args, 'device', item[0])
+        setattr(args, 'tag', item[1])
+        install(args)
+
+
+def parse_configuration_file(filename):
+    devices = []
+    with open(filename) as csvfile:
+        rows = csv.reader(csvfile, delimiter=',')
+        for row in rows:
+            #  print(row)
+            if len(row) != 2 or '#' in row[0]:
+                continue
+            row[0] = row[0].strip()
+            row[1] = row[1].strip()
+            devices.append(row)
+    return devices
 
 
 def uninstall(args):
@@ -234,6 +294,16 @@ def uninstall(args):
         run_cmd('rm -f {}'.format(so_path))
     else:
         run_cmd('pip uninstall -y {}'.format(args.device))
+    print('{} was successfully uninstalled'.format(args.device))
+
+
+def uninstall_file(args):
+    global INVOKE_DIR
+    filename = os.path.join(INVOKE_DIR, args.filename)
+    devices = parse_configuration_file(filename)
+    for item in devices:
+        setattr(args, 'device', item[0])
+        uninstall(args)
 
 
 def develop(args):
@@ -242,6 +312,8 @@ def develop(args):
         if not os.path.isdir(path):
             checkout(args)
         os.chdir(path)
+        if os.path.isfile('DEPENDS'):
+            install_dependencies(args)
         if os.path.isfile('Makefile'):
             print('Compiling... [please wait] ', end='', flush=True)
             run_cmd('make CONF={} -j{}'.format(args.config, args.jobs))
@@ -252,6 +324,21 @@ def develop(args):
         else:
             run_cmd('pip install -e .')
         print("Develop installation succeeded.")
+
+
+def install_dependencies(args):
+    '''Installs dependencies as specified in the DEPENDS file.
+    NOTE: This function must be run in the directory of the DEPENDS file!
+    '''
+    devices = parse_configuration_file('DEPENDS')
+    print('Found dependencies: {}'.format(', '.join('{} ({})'
+                                              .format(e[0], e[1])
+                                                    for e in devices)))
+    print('Automatically installing now.')
+    for item in devices:
+        setattr(args, 'device', item[0])
+        setattr(args, 'tag', item[1])
+        install(args)
 
 
 def undevelop(args):
