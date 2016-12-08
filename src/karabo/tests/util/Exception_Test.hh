4589a0436eebc7ca41944b509d49fccc31ba8695
Burkhard Heisen <burkhard.heisen@xfel.eu>
2016-12-08 14:23:28 +0100

    Added unit test for c++ exceptions.
    The test shows that Karabo exceptions can be caught as std::exception
    and be printed using what().

---
+++ src/karabo/tests/util/Exception_Test.hh
@@ -0,0 +1,29 @@
+/*
+ * File:   Exception_Test.hh
+ * Author: heisenb
+ *
+ * Created on September 29, 2016, 5:28 PM
+ */
+
+#ifndef EXCEPTION_TEST_HH
+#define	EXCEPTION_TEST_HH
+
+#include <cppunit/extensions/HelperMacros.h>
+
+class Exception_Test : public CPPUNIT_NS::TestFixture {
+
+    CPPUNIT_TEST_SUITE(Exception_Test);
+    CPPUNIT_TEST(testMethod);
+    CPPUNIT_TEST_SUITE_END();
+
+public:
+    Exception_Test();
+    virtual ~Exception_Test();
+
+private:
+
+    void testMethod();
+};
+
+#endif	/* EXCEPTION_TEST_HH */
+
