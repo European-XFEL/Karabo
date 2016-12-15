4f5f1089f04c328ef7f6302ac1d1509feced19ec
John Wiggins <john.wiggins@desy.de>
2016-12-15 18:35:52 +0100

    Show running macro instances in the project panel

--- src/pythonGui/karabo_gui/project/model/server.py
+++ src/pythonGui/karabo_gui/project/model/server.py
@@ -7,7 +7,7 @@ from functools import partial
 import weakref
 
 from PyQt4.QtGui import QAction, QDialog, QMenu, QMessageBox, QStandardItem
-from traits.api import Callable, Dict, Instance, List, on_trait_change
+from traits.api import Instance, on_trait_change
 
 from karabo.common.project.api import (
     DeviceConfigurationModel, DeviceInstanceModel, DeviceServerModel)
@@ -22,7 +22,6 @@ from karabo_gui.project.topo_listener import SystemTopologyListener
 from karabo_gui.project.utils import save_object
 from karabo_gui.singletons.api import get_manager
 from .bases import BaseProjectGroupItem
-from .device import DeviceInstanceModelItem
 
 
 class DeviceServerModelItem(BaseProjectGroupItem):
@@ -30,13 +29,6 @@ class DeviceServerModelItem(BaseProjectGroupItem):
     """
     # Redefine model with the correct type
     model = Instance(DeviceServerModel)
-    # A factory for shadow items wrapping children
-    child_create = Callable
-    # A callable which can gracefully destroy a child shadow object
-    child_destroy = Callable
-    # Different devices for the server
-    children = List(Instance(DeviceInstanceModelItem))
-    _child_map = Dict  # dictionary for fast lookups during removal
     # An object which listens to system topology updates
     topo_listener = Instance(SystemTopologyListener)
 
@@ -78,28 +70,7 @@ class DeviceServerModelItem(BaseProjectGroupItem):
         item.setEditable(False)
         for child in self.children:
             item.appendRow(child.qt_item)
-
         return item
-
-    def item_handler(self, added, removed):
-        """ Called for List-trait events on ``model`` (a DeviceServerModel)
-
-        This notification handler is connected and disconnected in the
-        create_device_server_model_shadow and
-        destroy_device_server_model_shadow functions.
-        """
-        removals = []
-        for model in removed:
-            item_model = self._child_map[model]
-            self.children.remove(item_model)
-            self.child_destroy(item_model)
-            removals.append(item_model)
-
-        additions = [self.child_create(model=model) for model in added]
-        self.children.extend(additions)
-
-        # Synchronize the GUI with the Traits model
-        self._update_ui_children(additions, removals)
 
     def system_topology_callback(self, devices, servers):
         """ This callback is called by the ``SystemTopologyListener`` object
@@ -115,40 +86,8 @@ class DeviceServerModelItem(BaseProjectGroupItem):
             if self.model.server_id == server_id and self.model.host == host:
                 self.model.status = status
 
-    def _children_items_changed(self, event):
-        """ Maintain ``_child_map`` by watching item events on ``children``
-
-        This is a static notification handler which is connected automatically
-        by Traits.
-        """
-        for item_model in event.removed:
-            del self._child_map[item_model.model]
-
-        for item_model in event.added:
-            self._child_map[item_model.model] = item_model
-
-    def _update_ui_children(self, additions, removals):
-        """ Propagate changes from the Traits model to the Qt item model.
-        """
-        def _find_child_qt_item(item_model):
-            for i in range(self.qt_item.rowCount()):
-                row_child = self.qt_item.child(i)
-                row_model = row_child.data(PROJECT_ITEM_MODEL_REF)()
-                if row_model is item_model:
-                    return i
-            return -1
-
-        # Stop immediately if the UI is not yet initialized
-        if not self.is_ui_initialized():
-            return
-
-        for item in removals:
-            index = _find_child_qt_item(item)
-            if index >= 0:
-                self.qt_item.removeRow(index)
-
-        for item in additions:
-            self.qt_item.appendRow(item.qt_item)
+    # ----------------------------------------------------------------------
+    # traits notification handlers
 
     @on_trait_change("model.server_id")
     def server_id_change(self):
