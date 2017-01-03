df0fe9f4d6759c31d4d8098918231ea30ce231a6
Dennis Goeries <dennis.goeries@xfel.eu>
2017-01-03 16:02:44 +0100

    Fix Evaluate Expression Init

--- src/pythonGui/karabo_gui/displaywidgets/eval.py
+++ src/pythonGui/karabo_gui/displaywidgets/eval.py
@@ -1,15 +1,11 @@
 __all__ = ["Evaluator"]
 
 import traceback
-
-from PyQt4.QtGui import QAction, QInputDialog, QLineEdit, QMessageBox
-
+from PyQt4.QtGui import QLineEdit, QInputDialog, QAction
+from PyQt4.QtGui import QMessageBox
+from karabo.middlelayer import String, Simple
 from karabo_gui.util import SignalBlocker
 from karabo_gui.widget import DisplayWidget
-
-from karabo.middlelayer import String, Simple
-
-from PyQt4.QtGui import QLineEdit, QInputDialog, QAction
 
 
 class Evaluator(DisplayWidget):
@@ -25,11 +21,12 @@ class Evaluator(DisplayWidget):
         self.widget = QLineEdit(parent)
         self.widget.setMinimumSize(160, 24)
         self.widget.setReadOnly(True)
+        self.text = "x"
+        self.value = None
+
         action = QAction("Change expression...", self.widget)
         self.widget.addAction(action)
         action.triggered.connect(self.onChangeExpression)
-        self.text = "x"
-        self.value = None
 
 
     def function(self, x):
@@ -44,14 +41,18 @@ class Evaluator(DisplayWidget):
             self.setText(text)
 
 
-    def setText(self, text):
+    def setText(self, text=None):
+        if not text:
+            return
+        
         try:
-            self.function = eval("lambda x:" + text, self.globals)
+            self.function = eval("lambda x: {}".format(text), self.globals)
         except SyntaxError as e:
             err = traceback.format_exception_only(type(e), e)
             QMessageBox.warning(None, "Error in expression",
                                 "<pre>{1}{2}</pre>{3}".format(*err))
             return
+
         self.text = text
         self.valueChanged(None, self.value)
 
