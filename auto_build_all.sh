6303bcdfc5870044f363d9adc91fbfce859355e1
John Wiggins <john.wiggins@desy.de>
2017-01-04 11:12:19 +0100

    Move the middlelayer tests to the end

--- auto_build_all.sh
+++ auto_build_all.sh
@@ -76,10 +76,10 @@ runUnitTests() {
     cd build/netbeans/pythonKarabo
     safeRunCommand "nosetests -v karabo.bound_api"
     safeRunCommand "nosetests -v karabo.common"
-    safeRunCommand "nosetests -v karabo.middlelayer_api"
     safeRunCommand "nosetests -v karabo.project_db"
     safeRunCommand "nosetests -v karabo.tests"
     safeRunCommand "nosetests -v karabo_gui"
+    safeRunCommand "nosetests -v karabo.middlelayer_api"
     echo
     echo Unit tests complete
     echo
