49844bed21f64e3f1fd9828247b694dbd6fd32df
John Wiggins <john.wiggins@desy.de>
2017-01-30 16:33:36 +0100

    Refactor the scene workflow controller code
    We are now using ProjectDeviceInstance objects and keeping the workflow
    consistent whether devices are on-/off-line.

--- src/pythonGui/karabo_gui/sceneview/workflow/model.py
+++ src/pythonGui/karabo_gui/sceneview/workflow/model.py
@@ -1,49 +1,24 @@
-from itertools import chain
-
 from PyQt4.QtCore import QPoint
-from traits.api import (HasStrictTraits, Any, Bool, Dict, Enum, Event,
-                        Instance, Int, List, Property, String,
-                        cached_property, on_trait_change)
+from traits.api import (
+    HasStrictTraits, Dict, Enum, Event, Instance, Int, List, Property, String,
+    cached_property, on_trait_change
+)
 
 from karabo.common.scenemodel.api import WorkflowItemModel
-from karabo_gui.indicators import DeviceStatus
-from karabo_gui.schema import Dummy
+from karabo_gui.schema import Box, Dummy
 from karabo_gui.singletons.api import get_topology
+from karabo_gui.topology.api import ProjectDeviceInstance
 from .const import (
     CHANNEL_INPUT, CHANNEL_OUTPUT, CHANNEL_HEIGHT, CONNECTION_OFFSET,
     DATA_DIST_COPY, DATA_DIST_SHARED)
 from .utils import get_curve_points
 
 
-class WorkflowDeviceStatusModel(HasStrictTraits):
-    """ A model for workflow item device status.
-    """
-    # The box for the device
-    box = Any
-    # The status of the device
-    status = Enum(*DeviceStatus)
-    # If device is in error
-    error = Bool
-    # The item model associated with this device status
-    model = Instance(WorkflowItemModel)
-    # The scene position of the device status
-    position = Property(Instance(QPoint),
-                        depends_on=['model.x', 'model.y', 'model.height',
-                                    'model.width'])
-
-    @cached_property
-    def _get_position(self):
-        model = self.model
-        x = model.x
-        y = model.y + model.height / 3
-        return QPoint(x, y)
-
-
 class WorkflowChannelModel(HasStrictTraits):
     """ A model for workflow channels (inputs or outputs).
     """
     # The box for this channel
-    box = Any
+    box = Instance(Box)
     # The device IDs for this channel
     device_ids = Property(List(String))
     # The index of this channel within the WorkflowItem
@@ -108,6 +83,82 @@ class WorkflowConnectionModel(HasStrictTraits):
         return self.output.position
 
 
+class WorkflowDeviceModel(HasStrictTraits):
+    """ A model for workflow item device status.
+    """
+    # Online/Offline device
+    device = Instance(ProjectDeviceInstance)
+    # The item model associated with this device status
+    model = Instance(WorkflowItemModel)
+    # The inputs and outputs of the device
+    inputs = List(Instance(WorkflowChannelModel))
+    outputs = List(Instance(WorkflowChannelModel))
+    # The scene position of the device status icon
+    position = Property(Instance(QPoint),
+                        depends_on=['model.x', 'model.y', 'model.height',
+                                    'model.width'])
+
+    @cached_property
+    def _get_position(self):
+        model = self.model
+        x = model.x
+        y = model.y + model.height / 3
+        return QPoint(x, y)
+
+    def _model_changed(self):
+        """When the model changes, set the project device instance.
+        """
+        if self.model is None:
+            self.device = None
+            return
+
+        self.device = get_topology().get_project_device(self.model.device_id)
+
+    @on_trait_change('device.online,device.boxes_updated')
+    def _refresh_channels(self):
+        """When the project device changes, or it changes between
+        online/offline states: refresh the workflow channels.
+        """
+        if self.device is None:
+            self.inputs = self.outputs = []
+            return
+
+        box = self.device.current_configuration
+        self.inputs, self.outputs = self._find_channels(box)
+
+    def _find_channels(self, box, inputs=None, outputs=None):
+        """ Recursively find all input/output channels from a box
+        """
+        inputs = inputs or []
+        outputs = outputs or []
+        if box.descriptor is not None:
+            for key, value in box.descriptor.dict.items():
+                # The child box belonging to key:
+                sub_box = getattr(box.boxvalue, key, None)
+                if sub_box is None:
+                    continue
+                # Now we check whether it is a display type. If yes, check for
+                # input/output channel and append in case.
+                # If not, we might have a node (i.e. Schema).
+                displayType = sub_box.descriptor.displayType
+                if displayType is None:
+                    if hasattr(value, 'dict'):
+                        # A Schema node that we have to dive into:
+                        self._find_channels(sub_box, inputs=inputs,
+                                            outputs=outputs)
+                elif displayType == CHANNEL_INPUT:
+                    channel = WorkflowChannelModel(
+                        box=sub_box, model=self.model,
+                        kind=CHANNEL_INPUT, index=len(inputs))
+                    inputs.append(channel)
+                elif displayType == CHANNEL_OUTPUT:
+                    channel = WorkflowChannelModel(
+                        box=sub_box, model=self.model,
+                        kind=CHANNEL_OUTPUT, index=len(outputs))
+                    outputs.append(channel)
+        return inputs, outputs
+
+
 class SceneWorkflowModel(HasStrictTraits):
     """ A model for tracking workflow connections in the SceneView.
     """
@@ -119,17 +170,16 @@ class SceneWorkflowModel(HasStrictTraits):
     # An event triggered when the connections or channels change
     updated = Event
 
-    # Ths list of current device states
-    device_states = List(Instance(WorkflowDeviceStatusModel))
+    # Ths dictionary of devices backing the workflow items
+    devices = List(Instance(WorkflowDeviceModel))
     # The list of current input channels
     input_channels = List(Instance(WorkflowChannelModel))
     # The list of current output channels
     output_channels = List(Instance(WorkflowChannelModel))
 
-    # A private mapping of item model -> _DeviceEntry objects
-    _devices = Dict
     # All of the WorkflowItemModel objects in the scene.
     _workflow_items = List(Instance(WorkflowItemModel))
+    _device_map = Dict  # WorkflowItemModel -> WorkflowDeviceModel
 
     # --------------------------------------------
     # Public interface
@@ -147,19 +197,11 @@ class SceneWorkflowModel(HasStrictTraits):
     def add_items(self, items):
         """ Add WorkflowItemModels to be monitored. """
         self._workflow_items.extend(items)
-        # Collect the remaining device states
-        self._refresh_device_states()
-        # Collect the remaining inputs and outputs
-        self._refresh_channels()
 
     def remove_items(self, items):
         """ Remove WorkflowItemModels from monitoring """
         for it in items:
             self._workflow_items.remove(it)
-        # Collect the remaining device states
-        self._refresh_device_states()
-        # Collect the remaining inputs and outputs
-        self._refresh_channels()
 
     def destroy(self):
         """ Clean up any channels (causing their Boxes to disconnect). """
@@ -174,40 +216,69 @@ class SceneWorkflowModel(HasStrictTraits):
     def _get_channels(self):
         return self.input_channels + self.output_channels
 
-    @on_trait_change('connections,input_channels,output_channels,'
-                     'device_states')
+    @on_trait_change('connections,input_channels,output_channels,devices')
     def _needs_update(self):
         # Event traits don't have a value, they just generate notifications
         self.updated = True
 
+    @on_trait_change('devices:inputs,devices:outputs')
+    def _refresh_channels(self, obj, name, old, new):
+        if name == 'inputs':
+            self._remove_device_channels(old, [])
+            self._add_device_channels(new, [])
+        elif name == 'outputs':
+            self._remove_device_channels([], old)
+            self._add_device_channels([], new)
+
     def __workflow_items_items_changed(self, event):
-        """ A trait notification handler for the `_workflow_items` list """
-        def toggle_boxes(entry, state):
-            device_status = entry.device_status
-            if state:
-                self._subscribe_device_status(device_status)
-            else:
-                self._unsubscribe_device_status(device_status)
-            for ch in chain(entry.inputs, entry.outputs):
-                ch.toggle_box_notifications(state)
-            for ch in entry.inputs:
-                if state:
-                    self._subscribe_to_input_changes(ch)
-                else:
-                    self._unsubscribe_from_input_changes(ch)
+        """ A trait notification handler for the `_workflow_items` list.
+        """
+        for model in event.added:
+            device = WorkflowDeviceModel(model=model)
+            self.devices.append(device)
+            self._device_map[model] = device
+            self._add_device_channels(device.inputs, device.outputs)
 
-        for item_model in event.added:
-            entry = _get_device_entry(item_model)
-            self._devices[item_model] = entry
-            toggle_boxes(entry, True)
-
-        for item_model in event.removed:
-            if item_model in self._devices:
-                entry = self._devices.pop(item_model)
-                toggle_boxes(entry, False)
+        for model in event.removed:
+            device = self._device_map.pop(model, None)
+            if device is not None:
+                self.devices.remove(device)
+                self._remove_device_channels(device.inputs, device.outputs)
 
     # --------------------------------------------
     # Private interface
+
+    def _add_device_channels(self, inputs, outputs):
+        for ch in inputs:
+            ch.toggle_box_notifications(True)
+            self._subscribe_to_input_changes(ch)
+        for ch in outputs:
+            ch.toggle_box_notifications(True)
+        self.input_channels.extend(inputs)
+        self.output_channels.extend(outputs)
+
+    def _remove_device_channels(self, inputs, outputs):
+        for ch in inputs:
+            ch.toggle_box_notifications(False)
+            self._unsubscribe_from_input_changes(ch)
+            self.input_channels.remove(ch)
+        for ch in outputs:
+            ch.toggle_box_notifications(False)
+            self.output_channels.remove(ch)
+
+    def _connected_outputs_cb(self, box, value, timestamp):
+        """ Callback with the connected outputs of an input channel changes.
+        """
+        # XXX: Be less ham-fisted about this later.
+        self._refresh_connections()
+
+    def _data_distribution_cb(self, box, value, timestamp):
+        """ Callback when the data distribution type of a channel changes
+        """
+        inputs = [ch for ch in self.input_channels if ch.box is box]
+        for conn in self.connections:
+            if conn.input in inputs:
+                conn.data_distribution = value
 
     def _lookup_output(self, device_id, box_path):
         for output in self.output_channels:
@@ -215,11 +286,6 @@ class SceneWorkflowModel(HasStrictTraits):
                 if '.'.join(output.box.path) == box_path:
                     return output
         return None
-
-    def _refresh_channels(self):
-        entries = list(self._devices.values())
-        self.input_channels = [c for entry in entries for c in entry.inputs]
-        self.output_channels = [c for entry in entries for c in entry.outputs]
 
     def _refresh_connections(self):
         connections = []
@@ -264,89 +330,3 @@ class SceneWorkflowModel(HasStrictTraits):
             data_dist = input.box.boxvalue.dataDistribution
             data_dist.signalUpdateComponent.disconnect(
                 self._data_distribution_cb)
-
-    def _connected_outputs_cb(self, box, value, timestamp):
-        """ Callback with the connected outputs of an input channel changes.
-        """
-        # XXX: Be less ham-fisted about this later.
-        self._refresh_connections()
-
-    def _data_distribution_cb(self, box, value, timestamp):
-        """ Callback when the data distribution type of a channel changes
-        """
-        inputs = [ch for ch in self.input_channels if ch.box is box]
-        for conn in self.connections:
-            if conn.input in inputs:
-                conn.data_distribution = value
-
-    def _refresh_device_states(self):
-        entries = list(self._devices.values())
-        self.device_states = [entry.device_status for entry in entries]
-
-    def _subscribe_device_status(self, device_status):
-        """ Subscribe from device box updates. """
-        device_box = device_status.box
-        device_box.signalStatusChanged.connect(self._device_status_changed)
-
-    def _unsubscribe_device_status(self, device_status):
-        """ Unsubscribe from device box updates. """
-        device_box = device_status.box
-        device_box.signalStatusChanged.disconnect(self._device_status_changed)
-
-    def _device_status_changed(self, device_box, status, error):
-        """ Callback when the status of the device is changes.
-        """
-        for ds in self.device_states:
-            if ds.box is device_box:
-                ds.status = status
-                ds.error = error
-
-
-class _DeviceEntry(HasStrictTraits):
-    """ A private convenience class for tracking devices
-    """
-    device_id = String
-    box = Any
-    device_status = Instance(WorkflowDeviceStatusModel)
-    inputs = List
-    outputs = List
-
-
-def _get_channels(model, box, inputs=None, outputs=None):
-    """ Recursively find all input/output channels from a box """
-    inputs = inputs or []
-    outputs = outputs or []
-    if box.descriptor is not None:
-        for key, value in box.descriptor.dict.items():
-            # The child box belonging to key:
-            sub_box = getattr(box.boxvalue, key, None)
-            if sub_box is None:
-                continue
-            # Now we check whether it is a display type. If yes, check for
-            # input/output channel and append in case.
-            # If not, we might have a node (i.e. Schema).
-            displayType = sub_box.descriptor.displayType
-            if displayType is None:
-                if hasattr(value, 'dict'):
-                    # A Schema node that we have to dive into:
-                    _get_channels(model, sub_box,
-                                  inputs=inputs, outputs=outputs)
-            elif displayType == CHANNEL_INPUT:
-                inputs.append(WorkflowChannelModel(box=sub_box, model=model,
-                                                   kind=CHANNEL_INPUT,
-                                                   index=len(inputs)))
-            elif displayType == CHANNEL_OUTPUT:
-                outputs.append(WorkflowChannelModel(box=sub_box, model=model,
-                                                    kind=CHANNEL_OUTPUT,
-                                                    index=len(outputs)))
-    return inputs, outputs
-
-
-def _get_device_entry(model):
-    """ Build a _DeviceEntry for a given WorkflowItemModel. """
-    device = get_topology().get_device(model.device_id)
-    device_status = WorkflowDeviceStatusModel(box=device, model=model)
-    inputs, outputs = _get_channels(model, device)
-    return _DeviceEntry(device_id=model.device_id, box=device,
-                        device_status=device_status,
-                        inputs=inputs, outputs=outputs)
