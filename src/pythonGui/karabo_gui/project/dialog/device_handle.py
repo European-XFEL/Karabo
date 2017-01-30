bd3b320420b881048b2ec965907ee6f255789c42
John Wiggins <john.wiggins@desy.de>
2017-01-30 13:40:15 +0100

    Move device instance creation to karabo_gui.project.utils
    This will allow sharing between the project panel and scenes

--- src/pythonGui/karabo_gui/project/dialog/device_handle.py
+++ src/pythonGui/karabo_gui/project/dialog/device_handle.py
@@ -16,7 +16,7 @@ from karabo_gui.singletons.api import get_topology
 
 class DeviceHandleDialog(QDialog):
     def __init__(self, server_id=None, model=None, add_config=False,
-                 is_online=False, parent=None):
+                 is_online=False, class_id='', parent=None):
         """ A dialog to configure device configurations
 
         :param server_id: The ID of the server the device configuration belongs
@@ -26,6 +26,8 @@ class DeviceHandleDialog(QDialog):
                            ``DeviceConfigurationModel`` should be added
         :param is_online: A boolean which is True if the device being edited
                           is currently online.
+        :param class_id: A string containing the class_id of the class which
+                         must be used by the device.
         :param parent: The parent of the dialog
         """
         super(DeviceHandleDialog, self).__init__(parent)
@@ -48,6 +50,11 @@ class DeviceHandleDialog(QDialog):
             title = 'Add device configuration'
             self._update_widgets_to_add_config()
             self.cbConfig.lineEdit().setText('default')
+
+            # If we already know the class, select it and disable editing.
+            if class_id != '':
+                self._update_plugin_widget(class_id)
+                self.cbClass.setEnabled(False)
         else:
             active_uuid, active_rev = model.active_config_ref
             active_dev_conf = model.select_config(active_uuid, active_rev)
