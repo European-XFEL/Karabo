6803fd1ed2739b4891993853e72634a36ae5c68e
John Wiggins <john.wiggins@desy.de>
2017-02-14 16:36:26 +0100

    Wrap all displaywidgets which need to display units
    DisplayWidget subclasses should show units for values which might have
    units.

--- src/pythonGui/karabo_gui/editablewidgets/floatspinbox.py
+++ src/pythonGui/karabo_gui/editablewidgets/floatspinbox.py
@@ -2,6 +2,8 @@ from numpy import log10
 from PyQt4.QtGui import QAction, QDoubleSpinBox, QInputDialog
 
 from karabo.middlelayer import Number
+from karabo_gui.displaywidgets.unitlabel import add_unit_label
+from karabo_gui.const import WIDGET_MIN_HEIGHT
 from karabo_gui.util import SignalBlocker
 from karabo_gui.widget import DisplayWidget, EditableWidget
 
@@ -12,7 +14,9 @@ class FloatSpinBox(EditableWidget, DisplayWidget):
 
     def __init__(self, box, parent):
         super().__init__(box)
-        self.widget = QDoubleSpinBox(parent)
+        self._internal_widget = QDoubleSpinBox(parent)
+        self._internal_widget.setMinimumHeight(WIDGET_MIN_HEIGHT)
+        self.widget = add_unit_label(box, self._internal_widget, parent=parent)
         action = QAction("Change Step...", self)
         action.triggered.connect(self.changeStep)
         self.widget.addAction(action)
@@ -20,32 +24,34 @@ class FloatSpinBox(EditableWidget, DisplayWidget):
     def changeStep(self):
         step, ok = QInputDialog.getDouble(
             self.widget, "Single Step", "Enter size of a single step",
-            self.widget.singleStep())
+            self._internal_widget.singleStep())
         if ok:
             self._setStep(step)
 
     def setReadOnly(self, ro):
-        self.widget.setReadOnly(ro)
+        self._internal_widget.setReadOnly(ro)
         if not ro:
-            self.widget.valueChanged[float].connect(self.onValueChanged)
+            self._internal_widget.valueChanged[float].connect(
+                self.onValueChanged)
 
     def onValueChanged(self, value):
         self.signalEditingFinished.emit(self.boxes[0], value)
 
     def typeChanged(self, box):
-        self.widget.setRange(*box.descriptor.getMinMax())
+        self._internal_widget.setRange(*box.descriptor.getMinMax())
         ae = box.descriptor.absoluteError
         if ae is not None and ae < 1:
-            self.widget.setDecimals(-log10(ae))
+            self._internal_widget.setDecimals(-log10(ae))
 
     def valueChanged(self, box, value, timestamp=None):
-        with SignalBlocker(self.widget):
-            self.widget.setValue(value)
+        self.widget.updateLabel(box)
+        with SignalBlocker(self._internal_widget):
+            self._internal_widget.setValue(value)
 
     @property
     def value(self):
-        return self.widget.value()
+        return self._internal_widget.value()
 
     def _setStep(self, step):
         """ Give derived classes a place to respond to changes. """
-        self.widget.setSingleStep(step)
+        self._internal_widget.setSingleStep(step)
--- src/pythonGui/karabo_gui/editablewidgets/hexoct.py
+++ src/pythonGui/karabo_gui/editablewidgets/hexoct.py
@@ -4,17 +4,14 @@
 # Copyright (C) European XFEL GmbH Hamburg. All rights reserved.
 #############################################################################
 
-
-__all__ = ["Hexadecimal"]
-
-
-from karabo_gui.util import SignalBlocker
-from karabo_gui.widget import DisplayWidget, EditableWidget
+from numpy import log2
+from PyQt4.QtGui import QLineEdit
 
 from karabo.middlelayer import Integer
-
-from PyQt4.QtGui import QLineEdit
-from numpy import log2
+from karabo_gui.displaywidgets.unitlabel import add_unit_label
+from karabo_gui.const import WIDGET_MIN_HEIGHT
+from karabo_gui.util import SignalBlocker
+from karabo_gui.widget import DisplayWidget, EditableWidget
 
 
 class Hexadecimal(EditableWidget, DisplayWidget):
@@ -23,35 +20,33 @@ class Hexadecimal(EditableWidget, DisplayWidget):
 
     def __init__(self, box, parent):
         super(Hexadecimal, self).__init__(box)
-        self.widget = QLineEdit(parent)
-
+        self._internal_widget = QLineEdit(parent)
+        self._internal_widget.setMinimumHeight(WIDGET_MIN_HEIGHT)
+        self.widget = add_unit_label(box, self._internal_widget, parent=parent)
 
     def setReadOnly(self, ro):
-        self.widget.setReadOnly(ro)
+        self._internal_widget.setReadOnly(ro)
         if not ro:
-            self.widget.textChanged.connect(self.onEditingFinished)
-
+            self._internal_widget.textChanged.connect(self.onEditingFinished)
 
     @property
     def value(self):
-        if self.widget.text() not in ('', '-'):
-            return int(self.widget.text(), base=16)
+        if self._internal_widget.text() not in ('', '-'):
+            return int(self._internal_widget.text(), base=16)
         else:
             return 0
-
 
     def typeChanged(self, box):
         rmin, rmax = box.descriptor.getMinMax()
         mask = 'h' * int(log2(max(abs(rmax), abs(rmin))) // 4 + 1)
         if rmin < 0:
             mask = "#" + mask
-        self.widget.setInputMask(mask)
-
+        self._internal_widget.setInputMask(mask)
 
     def valueChanged(self, box, value, timestamp=None):
-        with SignalBlocker(self.widget):
-            self.widget.setText("{:x}".format(value))
-
+        self.widget.updateLabel(box)
+        with SignalBlocker(self._internal_widget):
+            self._internal_widget.setText("{:x}".format(value))
 
     def onEditingFinished(self, value):
         EditableWidget.onEditingFinished(self, self.value)
