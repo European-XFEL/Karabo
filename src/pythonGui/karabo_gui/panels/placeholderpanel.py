5fdb051bcd39b0669cc3fb4070360274c82de511
John Wiggins <john.wiggins@desy.de>
2017-02-16 18:36:48 +0100

    Use BasePanelWidget as the base of all panels
    DivWidget is dead! Long live DivWidget! (except not really...)

--- src/pythonGui/karabo_gui/panels/placeholderpanel.py
+++ src/pythonGui/karabo_gui/panels/placeholderpanel.py
@@ -3,31 +3,17 @@
 # Created on May 20, 2014
 # Copyright (C) European XFEL GmbH Hamburg. All rights reserved.
 #############################################################################
+from PyQt4.QtGui import QLabel
+
+from .base import BasePanelWidget
 
 
-"""This module contains a class which represents a placeholder panel to start with."""
+class PlaceholderPanel(BasePanelWidget):
+    def __init__(self, container, title):
+        super(PlaceholderPanel, self).__init__(container, title)
+        self.doesDockOnClose = False
 
-__all__ = ["PlaceholderPanel"]
-
-
-import sys
-
-from karabo_gui.docktabwindow import Dockable
-
-from PyQt4.QtGui import QLabel, QVBoxLayout, QWidget
-
-
-class PlaceholderPanel(Dockable, QWidget):
-    def __init__(self, widget=None):
-        super(PlaceholderPanel, self).__init__()
-
-        vLayout = QVBoxLayout(self)
-
-        if widget is not None:
-            vLayout.addWidget(widget)
-        else:
-            startup = QLabel()
-            vLayout.addWidget(startup)
-
-        #self.setupActions()
-        self.setLayout(vLayout)
+    def get_content_widget(self):
+        """Returns a QWidget containing the main content of the panel.
+        """
+        return QLabel(self)
--- src/pythonGui/karabo_gui/project/runner.py
+++ src/pythonGui/karabo_gui/project/runner.py
@@ -5,39 +5,21 @@
 #############################################################################
 import sys
 
-from PyQt4.QtGui import QApplication, QFrame, QHBoxLayout, QVBoxLayout
+from PyQt4.QtGui import QApplication
 
+from karabo_gui.panels.container import PanelContainer
 from karabo_gui.panels.projectpanel import ProjectPanel
 from karabo_gui.singletons.api import get_manager, get_network
 
 
 def load_panel():
     import karabo_gui.icons as icons
-    from karabo_gui.toolbar import ToolBar
-
-    class _Frame(QFrame):
-        def __init__(self, panel):
-            super(_Frame, self).__init__()
-            self.toolbar = ToolBar()
-            self.dockableWidget = panel
-
-            self.toolBarLayout = QHBoxLayout()
-            self.toolBarLayout.setContentsMargins(0, 0, 0, 0)
-            self.toolBarLayout.setSpacing(0)
-            self.addToolBar(self.toolbar)
-
-            vLayout = QVBoxLayout(self)
-            vLayout.setContentsMargins(0, 0, 0, 0)
-            vLayout.addLayout(self.toolBarLayout)
-            vLayout.addWidget(self.dockableWidget)
-
-            self.dockableWidget.setupToolBars(self.toolbar, self)
-
-        def addToolBar(self, toolbar):
-            self.toolBarLayout.addWidget(toolbar)
 
     icons.init()  # Very important!
-    return _Frame(ProjectPanel())
+
+    container = PanelContainer("Project", None)
+    container.addPanel(ProjectPanel, "Project")
+    return container
 
 
 def main():
