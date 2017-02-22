c5d12272d166e6606738ad3d7daff529bca253ea
esenov <serguei.essenov@xfel.eu>
2017-02-22 15:41:45 +0100

    Fix bugs in parseCommandLine and readToken

--- src/pythonKarabo/karabo/bound_api/runner.py
+++ src/pythonKarabo/karabo/bound_api/runner.py
@@ -52,17 +52,17 @@ class Runner(object):
             arg = ''
             for a in args:
                 pos = 0
-                while True:
-                    m = re.search('[{}]', a[pos:])
+                while pos < len(a):
+                    m = re.search("[{}]", a[pos:])
                     if m is None:
                         break
                     else:
-                        pos = m.start()
+                        pos += m.start()
                         if a[pos] == '{':
                             braces += 1
                         elif a[pos] == '}':
                             braces -= 1
-                        pos = m.end()
+                        pos += 1
                         if braces < 0:
                             raise SyntaxError("CLI Syntax Error: '}' encounters before corresponding '{'")
                 if braces == 0:
@@ -70,12 +70,12 @@ class Runner(object):
                     arg = ''
                 else:
                     arg += ' ' + a
-                    
+
             if braces > 0:
                 raise SyntaxError("CLI Syntax Error: missing {} closing brace(s)".format(braces))
             elif braces < 0:
                 raise SyntaxError("CLI Syntax Error: missing {} opening brace(s)".format(-braces))
-                
+
             configuration = Hash()
 
             for a in pars[1:]:
@@ -84,9 +84,8 @@ class Runner(object):
                 configuration += tmp
             return True, configuration
 
-        except Exception as e:
-            print(str(e))
-            return False, Hash()
+        except SyntaxError:
+            raise
 
     def processOption(self, option, args):
         lowerOption = option.lower()
@@ -129,7 +128,9 @@ class Runner(object):
                 value = token[pos+1:].strip()
                 if value != "" and value[0] == "{" and value[-1] == "}":
                     value = value[1:-1].strip()
-                    tokens = value.split(" ")
+                    if value[0] == '{' or value[0] == '}':
+                        raise SyntaxError("The 'key' starts with invalid symbol: '{}'".format(value[0]))
+                    tokens = value.split(" ", 1)
                     config.set(key, Hash())
                     for subtok in tokens:
                         subtok = subtok.strip()
