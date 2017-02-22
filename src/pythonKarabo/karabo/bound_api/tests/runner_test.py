54b457c078e365ccf85303266110707b9f6dc328
esenov <serguei.essenov@xfel.eu>
2017-02-22 15:41:45 +0100

    Fix bugs in parseCommandLine and readToken

--- src/pythonKarabo/karabo/bound_api/tests/runner_test.py
+++ src/pythonKarabo/karabo/bound_api/tests/runner_test.py
@@ -5,17 +5,36 @@ from ..runner import Runner
 
 
 class  Schema_TestCase(unittest.TestCase):
+    def setUp(self):
+        self.runner = Runner('someServer')
 
     def test_argument_parser(self):
         runner = Runner('someServer')
-        cmdLine = ['foo', 'serverId=goo', 'autoStart[0]={a.b=c', 'a.c=1}']
-        res, parsed = runner.parseCommandLine(cmdLine)
+        cmdLine = ['foo', 'serverId=goo', 'autoStart[0]={a.b=c', 'x={y=12', 'a.c=1}}']
+        res, parsed = self.runner.parseCommandLine(cmdLine)
         self.assertTrue(res)
         self.assertTrue(parsed.has("serverId"))
         self.assertEqual(parsed.get("serverId"), "goo")
         self.assertTrue(parsed.has("autoStart"))
         autoStart = parsed.get("autoStart")[0]
         self.assertTrue(autoStart.has("a.b"))
-        self.assertTrue(autoStart.has("a.c"))
+        self.assertTrue(autoStart.has("x.y"))
+        self.assertTrue(autoStart.has("x.a.c"))
         self.assertEqual(autoStart.get("a.b"), "c")
-        self.assertEqual(autoStart.get("a.c"), "1")
+        self.assertEqual(autoStart.get("x.y"), "12")
+        self.assertEqual(autoStart.get("x.a.c"), "1")
+
+    def test_argument_parser_failure1(self):
+        with self.assertRaises(SyntaxError):
+            res, parsed = self.runner.parseCommandLine(
+                ['foo', 'serverId=bar', 'autoStart[0]={a.b=c', 'x={y=12', 'a.c=1}'])
+
+    def test_argument_parser_failure2(self):
+        with self.assertRaises(SyntaxError):
+            res, parsed = self.runner.parseCommandLine(
+                ['foo', 'serverId=bar', 'autoStart[0]={a.b=c', 'x={y=12', 'a.c=1}}}'])
+
+    def test_argument_parser_failure3(self):
+        with self.assertRaises(SyntaxError):
+            res, parsed = self.runner.parseCommandLine(
+                ['foo', 'serverId=bar', 'autoStart[0]=}a.b=c', 'x={y=12', 'a.c=1}}'])
