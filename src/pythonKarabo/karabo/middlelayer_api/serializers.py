afaaa928d2fbd98c6a5134125cd965c06e5fa5ea
Martin Teichmann <martin.teichmann@xfel.eu>
2017-03-13 16:52:30 +0100

    factor XML serialization out of hash.py

--- src/pythonKarabo/karabo/middlelayer_api/serializers.py
+++ src/pythonKarabo/karabo/middlelayer_api/serializers.py
@@ -1,6 +1,9 @@
 from struct import calcsize, unpack
+from xml.sax import make_parser
+from xml.sax.saxutils import unescape
+from xml.sax.handler import ContentHandler
 
-from .hash import _gettype, HashType
+from .hash import _gettype, Hash, HashList, HashType, Type
 
 
 class BinaryParser(object):
@@ -36,4 +39,116 @@ def writeBinary(data, file):
     """write the hash in `data` into `file`"""
     hashtype = _gettype(data)
     for d in hashtype.yieldBinary(data):
+        file.write(d)
+
+
+def parseHash():
+    ret = Hash()
+    try:
+        while True:
+            name, value, attrs = yield from parseOne()
+            ret[name] = value
+            ret[name, ...] = attrs
+    except EndElement:
+        return ret
+
+
+def parseOne():
+    name, attrs = yield "Start"
+    hashattrs = {}
+    for key, value in attrs.items():
+        value = unescape(value)
+        hashattrs[key] = value
+        if value.startswith("KRB_") and ":" in value:
+            dtype, svalue = value.split(":", 1)
+            dtype = Type.fromname.get(dtype[4:], None)
+            if dtype is not None:
+                hashattrs[key] = dtype.fromstring(svalue)
+    typename = hashattrs.pop("KRB_Type", "HASH")
+    if typename == "HASH":
+        value = yield from parseHash()
+    elif typename == "VECTOR_HASH":
+        value = yield from parseVectorHash()
+    else:
+        value = yield from parseString(typename)
+    return name, value, hashattrs
+
+
+def parseVectorHash():
+    ret = HashList()
+    try:
+        while True:
+            name, _ = yield "Start"
+            assert name == "KRB_Item"
+            ret.append((yield from parseHash()))
+    except EndElement:
+        return ret
+
+
+def parseString(typename):
+    ret = []
+    try:
+        while True:
+            ret.append((yield "Chars"))
+    except EndElement:
+        return Type.fromname[typename].fromstring("".join(ret))
+
+
+class EndElement(Exception):
+    pass
+
+
+class XMLParser(ContentHandler):
+    def __init__(self):
+        super().__init__()
+        self.parser = self.parseAll()
+        self.last = self.parser.send(None)
+
+    def parseAll(self):
+        self.name, self.value, self.hashattrs = yield from parseOne()
+
+    def startElement(self, name, attrs):
+        assert self.last == "Start"
+        self.last = self.parser.send((name, attrs))
+
+    def characters(self, content):
+        if self.last == "Chars":
+            self.parser.send(content)
+
+    def endElement(self, name):
+        try:
+            self.last = self.parser.throw(EndElement())
+        except StopIteration:
+            pass
+
+
+def decodeXML(data):
+    parser = make_parser()
+    handler = XMLParser()
+    parser.setContentHandler(handler)
+    parser.feed(data)
+    parser.close()
+    if handler.name == "root" and "KRB_Artificial" in handler.hashattrs:
+        return handler.value
+    else:
+        ret = Hash(handler.name, handler.value)
+        ret[handler.name, ...] = handler.hashattrs
+        return ret
+
+
+def yieldXML(data):
+    if len(data) == 1 and isinstance(next(iter(data.values())), Hash):
+        yield from HashType.yieldXML(data)
+    else:
+        yield '<root KRB_Artificial="">'
+        yield from HashType.yieldXML(data)
+        yield '</root>'
+
+
+def encodeXML(data):
+    return "".join(yieldXML(data))
+
+
+def writeXML(data, file):
+    for d in yieldXML(data):
         file.write(d)
