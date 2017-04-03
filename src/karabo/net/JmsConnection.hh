c297fe8c32f774d867e76fb065131b4b06b350a2
Gero Flucke <gero.flucke@xfel.eu>
2017-04-03 12:03:45 +0200

    Provide raw data if skipSerialisation=true in JmsConsumer.
    This has more use cases, but will slightly slow down the
    maximum rate that karabo-brokerrates can cope with.

--- src/karabo/net/JmsConnection.hh
+++ src/karabo/net/JmsConnection.hh
@@ -116,8 +116,8 @@ namespace karabo {
              * In order to consume from different topics or selectors in parallel, several instances of consumers
              * must be created.
              * The 'skipSerialisation' flag is for expert use: Instead of deserialising the message body, the body
-             * provided to the JmsConsumer::MessageHandler will be a Hash with a single key "totalSizeInBytes"
-             * containing an unsigned int with the number of bytes of the serialised message.
+             * provided to the JmsConsumer::MessageHandler will be a Hash with a single key "raw" containing an
+             * std::vector<char> of the serialised message.
              *
              * NOTE: Each call to this function will open a new thread in the central event-loop
              * @return JmsConsumer
