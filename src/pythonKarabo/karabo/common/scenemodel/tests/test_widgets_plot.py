2d321fd6a5d9037ec9154c27a600ec4442852a74
Steffen Hauf <steffen.hauf@xfel.eu>
2017-05-05 07:58:23 +0200

    Improve spark lines
    Improve sparklines
    
    Remove debug print
    
    Flake8 and better smoothing
    
    Update docs
    
    Move backward compatibiltiy to new reader
    
    Remove wrappers
    
    Feedback from review
    
    Modify scene model test hash
    
    Test that old sparkline data can still be read
    
    Also, minimally cover the sparkline widget model with tests.

--- src/pythonKarabo/karabo/common/scenemodel/tests/test_widgets_plot.py
+++ src/pythonKarabo/karabo/common/scenemodel/tests/test_widgets_plot.py
@@ -1,6 +1,37 @@
-from ..api import PlotCurveModel, LinePlotModel
+from io import StringIO
+
+from ..api import PlotCurveModel, LinePlotModel, SparklineModel, read_scene
 from .utils import (assert_base_traits, base_widget_traits,
                     single_model_round_trip)
+
+OLD_SPARKY = """
+<svg:svg
+    xmlns:krb="http://karabo.eu/scene"
+    xmlns:svg="http://www.w3.org/2000/svg"
+    height="540" width="728"
+    krb:version="2" >
+    <svg:rect
+        height="364" width="426" x="256" y="172"
+        krb:class="DisplayComponent"
+        krb:keys="thePast.sparkProp"
+        krb:widget="DisplaySparkline" >
+        <krb:box device="thePast" path="sparkProp">BINARYBLOB</krb:box>
+    </svg:rect>
+</svg:svg>
+"""
+NEW_SPARKY = """
+<svg:svg
+    xmlns:krb="http://karabo.eu/scene"
+    xmlns:svg="http://www.w3.org/2000/svg"
+    height="540" width="728"
+    krb:version="2" >
+    <svg:rect
+        height="364" width="426" x="256" y="172"
+        krb:class="DisplayComponent"
+        krb:keys="thePast.sparkProp"
+        krb:widget="DisplaySparkline" />
+</svg:svg>
+"""
 
 
 def test_line_plot_widget():
@@ -31,3 +62,29 @@ def test_line_plot_duplicate_curves():
     assert len(model.boxes) == 2
     assert model.boxes[0].curve_object_data == curve2.curve_object_data
     assert model.boxes[1] is curve1
+
+
+def test_sparkline_basics():
+    traits = base_widget_traits(parent='DisplayComponent')
+    traits['time_base'] = 42
+    traits['show_value'] = True
+    traits['show_format'] = 'whatever'
+    model = SparklineModel(**traits)
+    read_model = single_model_round_trip(model)
+    assert_base_traits(read_model)
+    assert model.time_base == 42
+    assert model.show_value
+    assert model.show_format == 'whatever'
+
+
+def test_old_sparkline_data():
+    default = SparklineModel()
+    with StringIO(OLD_SPARKY) as fp:
+        scene = read_scene(fp)
+
+    model = scene.children[0]
+    assert isinstance(model, SparklineModel)
+    assert model.keys == ['thePast.sparkProp']
+    assert model.time_base == default.time_base
+    assert model.show_value == default.show_value
+    assert model.show_format == default.show_format
