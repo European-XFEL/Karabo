10bb243f689dcdc5fb23af882f06bd71971b6578
Steffen Hauf <steffen.hauf@xfel.eu>
2017-05-09 23:29:29 +0200

    Improve timing server
    WIP

---
+++ src/integrationTests/SimulatedTimeServerDevice.cc
@@ -0,0 +1,67 @@
+/* 
+ * File:   SimulatedTimeServerDevice.cc
+ * Author: steffen.hauf@xfel.eu
+ * 
+ */
+
+#include "SimulatedTimeServerDevice.hh"
+
+using namespace std;
+
+USING_KARABO_NAMESPACES
+
+        namespace karabo {
+
+
+    KARABO_REGISTER_FOR_CONFIGURATION(BaseDevice, Device<>, SimulatedTimeServerDevice)
+
+    void SimulatedTimeServerDevice::expectedParameters(Schema& expected) {
+        UINT64_ELEMENT(expected).key("period")
+            .unit(karabo::util::Unit::SECOND)
+            .metricPrefix(karabo::util::MetricPrefix::MILLI)
+            .assignmentOptional().defaultValue(100)
+            .reconfigurable()
+            .commit();
+        
+        UINT64_ELEMENT(expected).key("updateRate")
+            .unit(karabo::util::Unit::SECOND)
+            .metricPrefix(karabo::util::MetricPrefix::MILLI)
+            .assignmentOptional().defaultValue(5000)
+            .reconfigurable()
+            .commit();
+      
+    }
+
+
+    SimulatedTimeServerDevice::SimulatedTimeServerDevice(const karabo::util::Hash& config) : Device<>(config), m_id(0), m_timeTickerTimer(karabo::net::EventLoop::getIOService()) {
+        KARABO_INITIAL_FUNCTION(initialize);
+        KARABO_SYSTEM_SIGNAL4("signalTimeTick", unsigned long long /*id */, unsigned long long /* sec */, unsigned long long /* frac */, long long /* period */);
+    }
+
+
+    SimulatedTimeServerDevice::~SimulatedTimeServerDevice() {
+        m_timeTickerTimer.cancel();
+    }
+
+
+    void SimulatedTimeServerDevice::initialize() {
+        unsigned long long period = get<unsigned long long>("period");
+        m_timeTickerTimer.expires_from_now(boost::posix_time::milliseconds(period));
+        m_timeTickerTimer.async_wait(util::bind_weak(&SimulatedTimeServerDevice::tickTock, this, boost::asio::placeholders::error));   
+    }
+    
+    void SimulatedTimeServerDevice::tickTock(const boost::system::error_code& e) {
+
+        long long period = get<unsigned long long>("period");
+        unsigned long long update_rate = get<unsigned long long>("updateRate");
+        karabo::util::Epochstamp now = karabo::util::Epochstamp();
+        if((now - m_lastTimeStamp).getFractions()/1000000u >= update_rate){
+            emit("signalTimeTick", m_id, now.getSeconds(), now.getFractionalSeconds(), (update_rate <= period) ? period : -period);
+            m_lastTimeStamp = now;
+        }
+        m_id++;
+        m_timeTickerTimer.expires_from_now(boost::posix_time::milliseconds(period));
+        m_timeTickerTimer.async_wait(util::bind_weak(&SimulatedTimeServerDevice::tickTock, this, boost::asio::placeholders::error));    
+    }
+
+}
---
+++ src/integrationTests/TimingTestDevice.hh
@@ -0,0 +1,57 @@
+/*
+ * File:   TimingTestDevice.hh
+ * Author: steffen.hauf@xfel.eu
+ *
+ */
+
+#ifndef TIMINGTESTDEVICE_HH
+#define	TIMINGTESTDEVICE_HH
+
+#include <karabo/karabo.hpp>
+
+/**
+ * The main Karabo namespace
+ */
+namespace karabo {
+
+    class TimingTestDevice : public karabo::core::Device<> {
+
+    public:
+
+        // Add reflection information and Karabo framework compatibility to this class
+        KARABO_CLASSINFO(TimingTestDevice, "TimingTestDevice", "2.0")
+
+        /**
+         * Necessary method as part of the factory/configuration system
+         * @param expected Will contain a description of expected parameters for this device
+         */
+        static void expectedParameters(karabo::util::Schema& expected);
+
+        /**
+         * Constructor providing the initial configuration in form of a Hash object.
+         * If this class is constructed using the configuration system the Hash object will
+         * already be validated using the information of the expectedParameters function.
+         * The configuration is provided in a key/value fashion.
+         */
+        TimingTestDevice(const karabo::util::Hash& config);
+
+        /**
+         * The destructor will be called in case the device gets killed (i.e. the event-loop returns)
+         */
+        virtual ~TimingTestDevice();
+
+
+
+
+
+    private:
+        
+        void initialize();
+        void onTimeUpdate(unsigned long long id, unsigned long long sec, unsigned long long frac, unsigned long long period);
+
+        karabo::util::Epochstamp m_lastTimeStamp;
+    };
+}
+
+#endif	/* TIMINGTESTDEVICE_HH */
+
