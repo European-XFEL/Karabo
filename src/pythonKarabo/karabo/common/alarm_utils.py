6ce716d98f9074aa3bbdd54524e5268031dc4468
Kerstin Weger <kerstin.weger@desy.de>
2017-05-16 22:23:46 +0200

    AGIPD SQUAD: Show alarm/warns correctly in project panel
    See merge request !1619

--- src/pythonKarabo/karabo/common/alarm_utils.py
+++ src/pythonKarabo/karabo/common/alarm_utils.py
@@ -9,7 +9,7 @@ from traits.api import Dict, HasStrictTraits, Property, String
 class AlarmInfo(HasStrictTraits):
     # Keep track of all alarm_types (key: alarm type, value: list of device
     # properties)
-    alarm_types_dict = Dict
+    alarm_dict = Dict
     # A property which returns the alarm_type of the highest priority
     alarm_type = Property(String)
 
@@ -17,22 +17,22 @@ class AlarmInfo(HasStrictTraits):
         """Append given ``alarm_type`` to dict and update list with device
         properties
         """
-        if alarm_type in self.alarm_types_dict:
-            self.alarm_types_dict[alarm_type].append(dev_property)
+        if alarm_type in self.alarm_dict:
+            self.alarm_dict[alarm_type].append(dev_property)
         else:
-            self.alarm_types_dict[alarm_type] = [dev_property]
+            self.alarm_dict[alarm_type] = [dev_property]
 
     def remove_alarm_type(self, dev_property, alarm_type):
         """Remove given ``dev_property`` for ``alarm_type`` from dict list
         or remove ``alarm_type`` from dict if list is empty afterwards
         """
-        if alarm_type in self.alarm_types_dict:
-            dev_props = self.alarm_types_dict[alarm_type]
+        if alarm_type in self.alarm_dict:
+            dev_props = self.alarm_dict[alarm_type]
             dev_props.remove(dev_property)
             if dev_props:
-                self.alarm_types_dict[alarm_type] = dev_props
+                self.alarm_dict[alarm_type] = dev_props
             else:
-                del self.alarm_types_dict[alarm_type]
+                del self.alarm_dict[alarm_type]
 
     def _get_alarm_type(self):
         """Return the ``alarm_type`` with the highest priority
@@ -40,6 +40,7 @@ class AlarmInfo(HasStrictTraits):
         Note: `alarmHigh`<`alarmLow`<`warnHigh`<`warnLow` is the priority
         the key list can be sorted alphabetically and returns the first element
         """
-        if self.alarm_types_dict:
-            keys = sorted(self.alarm_types_dict.keys())
+        if self.alarm_dict:
+            keys = sorted(self.alarm_dict.keys())
             return keys[0]
+        return ''
