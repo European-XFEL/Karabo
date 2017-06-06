adbdf96ee7863804f800462dbd75b03b557121ab
Alessandro Silenzi <alessandro.silenzi@gmail.com>
2017-06-06 11:49:18 +0200

    added RESTful api to webserver [ci skip]

--- src/pythonKarabo/karabo/interactive/webserver.py
+++ src/pythonKarabo/karabo/interactive/webserver.py
@@ -5,9 +5,9 @@ from argparse import ArgumentParser
 from datetime import datetime
 
 from tornado import ioloop, web
+from tornado.escape import json_decode
 
 from .startkarabo import absolute, defaultall, entrypoint
-
 
 mainpage = """
 <!DOCTYPE html>
@@ -22,10 +22,10 @@ mainpage = """
         <tr><th/><th>Name</th><th>Status</th><th>Since</th><tr>
         {table}
       </table>
-      <button name="cmd" value="u">Start</button>
-      <button name="cmd" value="d">Stop</button>
-      <button name="cmd" value="o">Start once</button>
-      <button name="cmd" value="k">Kill</button>
+      <button name="cmd" value="up">Start</button>
+      <button name="cmd" value="down">Stop</button>
+      <button name="cmd" value="once">Start once</button>
+      <button name="cmd" value="kill">Kill</button>
     </form>
   </body>
 </html>
@@ -40,7 +40,6 @@ row = """
 </tr>
 """
 
-
 refresh = """
 <!DOCTYPE html>
 <html>
@@ -53,6 +52,27 @@ refresh = """
   </body>
 </html>
 """
+
+EMPTY_RESPONSE = {'version': '1.0.0',
+                  'success': False,
+                  'status_ok': False,
+                  'servers': []}
+
+# A service is considered "up" if it has run for 5 seconds
+MINIMUM_UP_DURATION = 5
+
+
+def control_service(server_id, command):
+    svc_ctrl = {'up': 'u',
+                'down': 'd',
+                'once': 'o',
+                'kill': 'k'}
+    ctrl = absolute("var", "service", server_id, "supervise", "control")
+    if command not in svc_ctrl:
+        return False
+    with open(ctrl, "w") as cfile:
+        cfile.write(svc_ctrl[command])
+    return True
 
 
 def filter_services(service_id, service_list):
@@ -76,8 +96,10 @@ def getdata(name):
         tai, = unpack(">Q10x", status)
 
         timestamp = tai - 4611686018427387914  # from deamontool's tai.h
-        since = time.strftime("%a, %d %b %Y %H:%M:%S",
-                              time.localtime(timestamp))
+        tsince = time.localtime(timestamp)
+        since = time.strftime("%a, %d %b %Y %H:%M:%S", tsince)
+        start_time = time.mktime(tsince)
+        duration = time.mktime(time.localtime()) - start_time
         status = "up" if pid else "down"
         if pid and paused:
             status += "paused, "
@@ -86,9 +108,50 @@ def getdata(name):
         elif not pid and want == b"u":
             status += ", want up"
 
-        return name, status, since
+        return name, status, since, duration
     except Exception as e:
         return name, "error", str(e)
+
+
+def getserver(server_id):
+    name, status, since, duration = getdata(server_id)
+    return {'name': name, 'status': status,
+            'since': since, 'duration': duration}
+
+
+def server_up(server):
+    return (server['status'] == 'up'
+            and server['duration'] > MINIMUM_UP_DURATION)
+
+
+class DaemonHandler(web.RequestHandler):
+
+    def initialize(self, service_list=None, service_id=None):
+        self.service_list = service_list
+        self.service_id = service_id
+
+    def get(self, server_id=None):
+        response = EMPTY_RESPONSE
+        success = {'error': False, '': False}
+        response['servers'] = [getserver(server_id)]
+        response['success'] = success.get(response['servers'][0]['status'],
+                                          True)
+        response['status_ok'] = server_up(response['servers'][0])
+        self.write(response)
+
+    def put(self, server_id=None):
+        response = EMPTY_RESPONSE
+        data = json_decode(self.request.body)
+        conf = data['server']
+        allowed = filter_services(self.service_id, self.service_list)
+        if server_id != conf['name'] or server_id not in allowed:
+            self.write(response)
+            return
+        command = conf['command'].lower()
+        if control_service(server_id, command):
+            response['success'] = True
+            response['servers'] = [conf]
+        self.write(response)
 
 
 class MainHandler(web.RequestHandler):
@@ -116,9 +179,17 @@ class MainHandler(web.RequestHandler):
                       'command {} on service {}'
                       ''.format(datetime.now(), repr(self.request), cmd, s))
                 continue
-            ctrl = absolute("var", "service", s, "supervise", "control")
-            with open(ctrl, "w") as cfile:
-                cfile.write(cmd)
+            control_service(s, cmd)
+
+
+class StatusHandler(web.RequestHandler):
+    def get(self):
+        response = EMPTY_RESPONSE
+        response['servers'] = [getserver(s) for s in defaultall()]
+        statuses = [server_up(s) for s in response['servers']]
+        response['status_ok'] = all(statuses)
+        response['success'] = True
+        self.write(response)
 
 
 @entrypoint
@@ -154,8 +225,11 @@ def run_webserver():
     # need to fool the startkarabo library since it uses sys.argv and
     # sys.argv and argparse don't mix well.
     sys.argv = sys.argv[:1]
-    app = web.Application([("/", MainHandler,
-                            dict(service_list=service_list,
-                                 service_id=service_id))])
+    server_dict = {'service_list': service_list, 'service_id': service_id}
+    app = web.Application([('/', MainHandler, server_dict),
+                           ('/api/servers.json', StatusHandler),
+                           ('/api/servers/([a-zA-Z0-9_]+).json',
+                            DaemonHandler, server_dict)
+                           ])
     app.listen(args.port)
     ioloop.IOLoop.current().start()
