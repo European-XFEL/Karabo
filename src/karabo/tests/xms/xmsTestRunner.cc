069b8cdaa52a67dd505e017fa20adf652c509fd6
Gero Flucke <gero.flucke@xfel.eu>
2017-06-14 14:15:33 +0200

    Add test about registration of more than one function per slot.

--- src/karabo/tests/xms/xmsTestRunner.cc
+++ src/karabo/tests/xms/xmsTestRunner.cc
@@ -19,6 +19,9 @@
 
 
 int main() {
+    // uncomment this if ever testing against a local broker
+    // setenv("KARABO_BROKER", "tcp://localhost:7777", true);
+
     boost::thread t(karabo::net::EventLoop::work);
 
     // Create the event manager and test controller
