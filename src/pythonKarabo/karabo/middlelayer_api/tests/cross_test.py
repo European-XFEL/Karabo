eca8ee82a8383e54aca8a2e27a2a6c088d3f1733
Martin Teichmann <martin.teichmann@xfel.eu>
2017-06-15 11:26:51 +0200

    improved tests

--- src/pythonKarabo/karabo/middlelayer_api/tests/cross_test.py
+++ src/pythonKarabo/karabo/middlelayer_api/tests/cross_test.py
@@ -118,6 +118,8 @@ class Tests(DeviceTest):
             self.assertEqual(proxy.alarmCondition, AlarmCondition.NONE)
 
             self.assertEqual(proxy.ndarray[0, 1], 2)
+            self.assertEqual(proxy.ndarray.shape, (2, 3))
+            self.assertEqual(proxy.ndarray.dtype.str, "<i8")
 
             with self.assertRaises(ValueError):
                 proxy.a = 77
