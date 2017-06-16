0845e53f73791d0aa06363fa6b687ec5e1b82046
Steffen Hauf <steffen.hauf@desy.de>
2017-06-16 16:13:37 +0200

    Update comment

--- src/pythonGui/karabo_gui/displaywidgets/displaysparkline.py
+++ src/pythonGui/karabo_gui/displaywidgets/displaysparkline.py
@@ -201,10 +201,12 @@ class SparkRenderer(QWidget):
         if dt >= 1:
             # the sparkline might need to cover "gaps" in its data series
             # e.g. when for a given bin, no data was pushed to the widget
-            # in this case we fill with the value of the previous bin
-            # if dt is larger than the length of the array, we clear the
-            # array instead as there is nothing to display in the period
-
+            # in this case we fill with the value of the previous bin.
+            # If the sparkline does not receive any value updates from a
+            # device for dt > time_base, indexing by [-dt:] would result 
+            # in an IndexError. In these cases we set all values in the 
+            # sparkline to the value of the last update, consitent with the
+            # gap filling behaviour for valid "dt"s.
             if dt < self.yvals.size:
                 self.yvals = np.roll(self.yvals, -dt)
                 self.yvals[-dt:-1] = self.yvals[-dt-1]
