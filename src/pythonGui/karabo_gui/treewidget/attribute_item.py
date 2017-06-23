a1224c35d5707117dfa4a059a38193191691b5f6
John Wiggins <john.wiggins@desy.de>
2017-06-23 14:18:23 +0200

    Rearrange the treewidget deck chairs
    Gotta plow the field before I start planting...

--- src/pythonGui/karabo_gui/treewidget/attribute_item.py
+++ src/pythonGui/karabo_gui/treewidget/attribute_item.py
@@ -3,31 +3,18 @@
 # Created on May 14, 2013
 # Copyright (C) European XFEL GmbH Hamburg. All rights reserved.
 #############################################################################
-
-
-"""This module contains a class which inherits from a BaseTreeWidgetItem and represents
-   an attribute item and its parameters.
-"""
-
-__all__ = ["AttributeTreeWidgetItem"]
-
-
-from .basetreewidgetitem import BaseTreeWidgetItem
 from karabo_gui.components import DisplayComponent
 import karabo_gui.icons as icons
+from .base_item import BaseTreeWidgetItem
 
 
 class AttributeTreeWidgetItem(BaseTreeWidgetItem):
-
-
     def __init__(self, box, parent, parentItem=None):
-        
         super(AttributeTreeWidgetItem, self).__init__(box, parent, parentItem)
         self.setIcon(0, icons.signal)
         self.displayComponent = DisplayComponent("DisplayLabel", self.box)
         self.treeWidget().setItemWidget(self, 1, self.displayComponent.widget)
         self.treeWidget().resizeColumnToContents(1)
-
 
     def setupContextMenu(self):
         # item specific menu
@@ -35,10 +22,7 @@ class AttributeTreeWidgetItem(BaseTreeWidgetItem):
         if self.editableComponent is None:
             return
 
-
-### getter and setter functions ###
     def _setText(self, text):
         self.setText(0, text)
         self.treeWidget().resizeColumnToContents(0)
     displayText = property(fset=_setText)
-
--- src/pythonGui/karabo_gui/treewidget/table_item.py
+++ src/pythonGui/karabo_gui/treewidget/table_item.py
@@ -3,45 +3,33 @@
 # Created on August 11, 2015
 # Copyright (C) European XFEL GmbH Hamburg. All rights reserved.
 #############################################################################
+from PyQt4.QtCore import Qt, QSize
+from PyQt4.QtGui import QAction, QMenu
 
-
-"""This module contains a class which inherits from a BaseTreeWidgetItem and represents
-   an attribute item and its parameters.
-"""
-
-__all__ = ["TableTreeWidgetItem"]
-
-
-from .basetreewidgetitem import BaseTreeWidgetItem
 from karabo_gui.components import DisplayComponent
 import karabo_gui.icons as icons
+from .base_item import BaseTreeWidgetItem
 
-from PyQt4.QtCore import Qt, QSize
-from PyQt4.QtGui import QAction, QCursor, QMenu
 
 class TableTreeWidgetItem(BaseTreeWidgetItem):
-
-
     def __init__(self, box, parent, parentItem=None):
         super(TableTreeWidgetItem, self).__init__(box, parent, parentItem)
-        
         self.setData(0, Qt.SizeHintRole, QSize(200, 32))
         self.setIcon(0, icons.folder)
 
-        self.displayComponent = DisplayComponent("DisplayTableElement", self.box,
-                                                 self.treeWidget())
+        self.displayComponent = DisplayComponent("DisplayTableElement",
+                                                 self.box, self.treeWidget())
         self.treeWidget().setItemWidget(self, 1, self.displayComponent.widget)
         self.treeWidget().resizeColumnToContents(1)
 
         box.signalUpdateComponent.connect(self.onDisplayValueChanged)
-
 
     def setupContextMenu(self):
         # item specific menu
         # add actions from attributeWidget
         if self.editableComponent is None:
             return
-        
+
         self.mItem = QMenu()
         text = "Reset to default"
         self.__acResetToDefault = QAction(icons.revert, text, None)
@@ -49,33 +37,24 @@ class TableTreeWidgetItem(BaseTreeWidgetItem):
         self.__acResetToDefault.setToolTip(text)
         self.__acResetToDefault.setIconVisibleInMenu(True)
         self.__acResetToDefault.triggered.connect(self.onSetToDefault)
-        
+
         self.mItem.addAction(self.__acResetToDefault)
         self.mItem.addSeparator()
-        #self.mItem.addAction(self.editableComponent.acApply)
-        #self.mItem.addAction(self.editableComponent.acReset)
 
-
-### getter and setter functions ###
     def _setText(self, text):
         self.setText(0, text)
         self.treeWidget().resizeColumnToContents(0)
     displayText = property(fset=_setText)
 
-
-### public functions ###
     def setReadOnly(self, readOnly):
         if self.editableComponent is not None:
             self.editableComponent.setEnabled(not readOnly)
         BaseTreeWidgetItem.setReadOnly(self, readOnly)
 
-
     def onSetToDefault(self):
         self.box.descriptor.setDefault(self.box)
-
 
     def onDisplayValueChanged(self, box, value):
         # Update tooltip dialog, if visible
         if self.popupWidget and self.popupWidget.isVisible():
             self.updateToolTipDialog()
-
