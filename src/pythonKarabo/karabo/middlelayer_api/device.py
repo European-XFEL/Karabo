29fa9821d5e5145ba75d20bd5eeccd43be850c83
Martin Teichmann <martin.teichmann@xfel.eu>
2017-07-10 12:39:41 +0200

    move setting of logger to much earlier point
    this creates the logger once we know our name - earlier is not really
    possible.
    
    Unfortunately we cannot actually log at that time as we have no
    connection to the broker yet.

--- src/pythonKarabo/karabo/middlelayer_api/device.py
+++ src/pythonKarabo/karabo/middlelayer_api/device.py
@@ -121,10 +121,7 @@ class Device(AlarmMixin, SignalSlotable):
     @coroutine
     def _run(self, **kwargs):
         self._ss.enter_context(self.log.setBroker(self._ss))
-        self.logger = self.log.logger
-
         yield from super(Device, self)._run(**kwargs)
-
 
     @slot
     def slotGetConfiguration(self):
