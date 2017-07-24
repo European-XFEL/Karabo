adf57df40a8e15634344f1a64175bea575d7110c
Martin Teichmann <martin.teichmann@xfel.eu>
2017-07-24 11:19:43 +0200

    add mccabe checking

--- src/pythonKarabo/karabo/middlelayer_api/tests/code_quality_test.py
+++ src/pythonKarabo/karabo/middlelayer_api/tests/code_quality_test.py
@@ -8,6 +8,7 @@ import os
 from unittest import TestCase, main
 import sys
 
+from mccabe import get_module_complexity
 from pycodestyle import Checker
 from pyflakes.api import checkPath
 
@@ -22,16 +23,15 @@ flakes_exceptions = {
 
 
 class Tests(TestCase):
-    def find_modules(self):
+    @classmethod
+    def setUpClass(cls):
         mods = (obj.__module__ for obj in middlelayer.__dict__.values()
                 if hasattr(obj, "__module__")
                 and obj.__module__.startswith("karabo"))
-        mods = {sys.modules[m].__file__ for m in mods}
-        return mods
+        cls.modules = {sys.modules[m].__file__ for m in mods}
 
     def test_pep8(self):
-        mods = self.find_modules()
-        for mod in mods:
+        for mod in self.modules:
             base = os.path.basename(mod)
             with self.subTest(module=base):
                 checker = Checker(mod)
@@ -39,12 +39,19 @@ class Tests(TestCase):
                 self.assertEqual(errs, pep8_exceptions.get(base, 0))
 
     def test_flakes(self):
-        mods = self.find_modules()
-        for mod in mods:
+        for mod in self.modules:
             base = os.path.basename(mod)
             with self.subTest(module=base):
                 errs = checkPath(mod)
                 self.assertEqual(errs, flakes_exceptions.get(base, 0))
+
+    def test_mmcabe(self):
+        for mod in self.modules:
+            base = os.path.basename(mod)
+            with self.subTest(module=base):
+                complexity = get_module_complexity(mod)
+                self.assertLess(complexity, 6)
+
 
 
 if __name__ == "__main__":
