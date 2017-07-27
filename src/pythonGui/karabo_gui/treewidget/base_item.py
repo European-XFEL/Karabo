d60ed8aa5fc6001ee9f93ef8ceaca6d1f6d5c875
Kerstin Weger <kerstin.weger@xfel.eu>
2017-07-27 17:20:31 +0200

    Get rid of Editable* Components
    Now all Component classes are gone and their logic has moved into
    BaseTreeWidgetItem. This is not of course the final intent of our
    implementation, it merely gives the freedom to refactor code which
    interacts with this code.

--- src/pythonGui/karabo_gui/treewidget/base_item.py
+++ src/pythonGui/karabo_gui/treewidget/base_item.py
@@ -3,14 +3,21 @@
 # Created on April 27, 2012
 # Copyright (C) European XFEL GmbH Hamburg. All rights reserved.
 #############################################################################
-from collections import OrderedDict
+from collections import OrderedDict, defaultdict
+import numbers
+import numpy
 
-from PyQt4.QtCore import Qt
-from PyQt4.QtGui import QCursor, QTreeWidgetItem
+from PyQt4.QtCore import pyqtSlot, Qt, QTimer
+from PyQt4.QtGui import QCursor, QHBoxLayout, QTreeWidgetItem, QWidget
 
+from karabo.common.api import State
 from karabo.middlelayer import Type
+from karabo_gui import messagebox
 from karabo_gui.alarms.api import ALARM_LOW, ALARM_HIGH, WARN_LOW, WARN_HIGH
+from karabo_gui.indicators import STATE_COLORS
 from karabo_gui.popupwidget import PopupWidget
+from karabo_gui.singletons.api import get_network
+from karabo_gui.util import generateObjectName
 
 
 class BaseTreeWidgetItem(QTreeWidgetItem):
@@ -25,18 +32,32 @@ class BaseTreeWidgetItem(QTreeWidgetItem):
             super(BaseTreeWidgetItem, self).__init__(parent)
 
         self.box = box
+        self._connected_signals = defaultdict(list)
+        self.is_editable = False
+        self.has_conflict = False
+        self.apply_enabled = False
+        self.__current_value = None
+        self.__busy_timer = QTimer(parent)
+        self.__busy_timer.setSingleShot(True)
+        self.__busy_timer.timeout.connect(self._on_timeout)
 
-        self.__editableComponent = None
+        self.display_widget = None
+        self.editable_widget = None
         self._contextMenu = None
 
         # Popup widget for tooltip info
         self.popupWidget = None
 
+    def connect_signal(self, signal, receiver):
+        self._connected_signals[signal].append(receiver)
+        signal.connect(receiver)
+
     def create_display_widget(self, klass, box):
         tree_widget = self.treeWidget()
         # Create a display widget
         self.display_widget = klass(box, tree_widget)
-        box.signalUpdateComponent.connect(self.display_widget.valueChangedSlot)
+        self.connect_signal(box.signalUpdateComponent,
+                            self.display_widget.valueChangedSlot)
         if box.hasValue():
             self.display_widget.valueChanged(box, box.value, box.timestamp)
 
@@ -44,25 +65,67 @@ class BaseTreeWidgetItem(QTreeWidgetItem):
         tree_widget.setItemWidget(self, 1, self.display_widget.widget)
         tree_widget.resizeColumnToContents(1)
 
-    @property
-    def editableComponent(self):
-        """Returns the editable component of the item
-        """
-        return self.__editableComponent
-
-    @editableComponent.setter
-    def editableComponent(self, component):
-        if component is None:
-            return
-
-        self.__editableComponent = component
-
+    def create_editable_widget(self, klass, box):
+        tree_widget = self.treeWidget()
+        self.editable_widget = klass(box, tree_widget)
+        # Introduce layout to have some border to show
+        self.qt_edit_widget = QWidget()
+        layout = QHBoxLayout(self.qt_edit_widget)
+        layout.setContentsMargins(2, 2, 2, 2)
+        layout.addWidget(self.editable_widget.widget)
+        self.qt_edit_widget.setObjectName(
+            generateObjectName(self.qt_edit_widget))
+        tree_widget.setItemWidget(self, 2, self.qt_edit_widget)
+        tree_widget.resizeColumnToContents(2)
+        # Setup context menu
         self._contextMenu = self.setupContextMenu()
-        self.treeWidget().setItemWidget(self, 2, component.widget)
-        self.treeWidget().resizeColumnToContents(2)
+
+    def make_class_connections(self, box):
+        """Connect to Box signals needed when editing a class Configuration
+        before instantiation.
+        """
+        self.connect_signal(box.signalNewDescriptor,
+                            self.editable_widget.typeChangedSlot)
+        if box.descriptor is not None:
+            self.editable_widget.typeChangedSlot(box)
+        if box.hasValue():
+            self.editable_widget.valueChanged(box, box.value, box.timestamp)
+        self.connect_signal(box.signalUpdateComponent,
+                            self.editable_widget.valueChangedSlot)
+        self.connect_signal(self.editable_widget.signalEditingFinished,
+                            self._on_editing_finished)
+        self.connect_signal(box.signalUserChanged,
+                            self._on_user_edit)
+        self.editable_widget.setReadOnly(False)
+
+    def make_device_connections(self, box):
+        """Connect to Box signals needed when editing a Configuration for a
+        running device instance.
+        """
+        self.connect_signal(box.signalNewDescriptor,
+                            self.editable_widget.typeChangedSlot)
+        if box.descriptor is not None:
+            self.editable_widget.typeChangedSlot(box)
+        self.connect_signal(self.editable_widget.signalEditingFinished,
+                            self._on_editing_finished)
+        self.connect_signal(box.signalUserChanged, self._on_user_edit)
+        self.connect_signal(box.signalUpdateComponent,
+                            self._on_display_value_change)
+        if box.hasValue():
+            self._on_display_value_change(box, box.value)
+        self.connect_signal(
+            box.configuration.boxvalue.state.signalUpdateComponent,
+            self._update_edit_widget)
+        self.editable_widget.setReadOnly(False)
+        self.is_editable = True
 
     def destroy(self):
-        """Give item subclasses a chance to clean up signal connections"""
+        """Disconnect all signal connections made by this item
+        """
+        for signal, receivers in self._connected_signals.items():
+            for recvr in receivers:
+                signal.disconnect(recvr)
+        self._connected_signals.clear()
 
     def setupContextMenu(self):
         raise NotImplementedError("BaseTreeWidgetItem.setupContextMenu")
@@ -128,3 +191,124 @@ class BaseTreeWidgetItem(QTreeWidgetItem):
                 info[label] = 'n/a' if this_alarm is None else this_alarm
 
             self.popupWidget.setInfo(info)
+
+    # ---------------------------------------------------------------------
+    # Edit widget related code
+
+    def _update_edit_widget(self):
+        """Update the editable widget to reflect the current state of affairs
+        """
+        box = self.box
+        value = self.__current_value
+
+        if value is None:
+            no_changes = False
+        elif (isinstance(value, (numbers.Complex, numpy.inexact))
+                and not isinstance(value, numbers.Integral)):
+            diff = abs(value - self.editable_widget.value)
+            absErr = box.descriptor.absoluteError
+            relErr = box.descriptor.relativeError
+            if absErr is not None:
+                no_changes = diff < absErr
+            elif relErr is not None:
+                no_changes = diff < abs(value * relErr)
+            else:
+                no_changes = diff < 1e-4
+        elif isinstance(value, list):
+            if len(value) != len(self.editable_widget.value):
+                no_changes = False
+            else:
+                no_changes = True
+                for i in range(len(value)):
+                    if value[i] != self.editable_widget.value[i]:
+                        no_changes = False
+                        break
+        else:
+            no_changes = (str(value) == str(self.editable_widget.value))
+
+        object_name = self.qt_edit_widget.objectName()
+        if no_changes:
+            # No changes
+            self.has_conflict = False
+            style_sheet = ("QWidget#{} ".format(object_name) +
+                           "{{ border: 0px }}")
+        elif self.has_conflict:
+            # Conflict - the value on device got changed
+            color = STATE_COLORS[State.UNKNOWN]
+            style_sheet = ("QWidget#{} ".format(object_name) +
+                           "{{ border: 2px solid rgb{} }}".format(color))
+        else:
+            # Something which can be changed
+            color = STATE_COLORS[State.CHANGING]
+            style_sheet = ("QWidget#{} ".format(object_name) +
+                           "{{ border: 2px solid rgb{} }}".format(color))
+
+        allowed = box.isAllowed()
+        apply_changed = allowed and not no_changes
+        if self.apply_enabled != apply_changed:
+            self.apply_enabled = apply_changed
+            # Let the tree widget know...
+            self.treeWidget().onApplyChanged(self.box, self.apply_enabled)
+        # Show colored border around the wrapper widget
+        self.qt_edit_widget.setStyleSheet(style_sheet)
+
+    def apply_changes(self):
+        """All changes of this component need to be send to the GuiServerDevice
+        """
+        if not self.is_editable:
+            # Does not make sense for classes
+            return
+        network = get_network()
+        changes = []
+        self.box.signalUserChanged.emit(
+            self.box, self.editable_widget.value, None)
+        if self.box.configuration.type == "macro":
+            self.box.set(self.editable_widget.value)
+        elif self.box.descriptor is not None:
+            changes.append((self.box, self.editable_widget.value))
+
+        if changes:
+            self.__busy_timer.start(5000)
+            network.onReconfigure(changes)
+
+    def decline_changes(self):
+        """All changes of this component are declined and reset to the current
+        value on device
+        """
+        if not self.is_editable:
+            # Does not make sense for classes
+            return
+        self.editable_widget.valueChanged(self.box, self.__current_value)
+        self._update_edit_widget()
+
+    @pyqtSlot(object, object)
+    def _on_display_value_change(self, box, value):
+        if self.__current_value is None:
+            self.editable_widget.valueChanged(box, value)
+        self.__current_value = value
+        self.__busy_timer.stop()
+        self.has_conflict = True
+        self._update_edit_widget()
+
+    @pyqtSlot(object, object)
+    def _on_editing_finished(self, box, value):
+        if not self.is_editable:
+            # Class related editable_widget
+            self.box.set(value, None)
+            # Configuration changed - so project needs to be informed
+            if self.box.configuration.type == 'projectClass':
+                self.box.configuration.signalBoxChanged.emit()
+        else:
+            # Device related editable_widget
+            if self.__current_value is None:
+                return
+            self._update_edit_widget()
+
+    def _on_timeout(self):
+        msg = "The property couldn't be set in the current state"
+        messagebox.show_warning(msg)
+
+    @pyqtSlot(object, object, object)
+    def _on_user_edit(self, box, value, timestamp=None):
+        self.editable_widget.valueChangedSlot(box, value, timestamp)
+        self._update_edit_widget()
