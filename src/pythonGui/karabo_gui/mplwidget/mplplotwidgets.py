68c58d86ad39e2717833cd8043f78d942a770df6
Chen Xu <chen.xu@xfel.eu>
2017-07-27 10:45:52 +0200

    address comments, added few keyboard shortcuts to the plot tool

--- src/pythonGui/karabo_gui/mplwidget/mplplotwidgets.py
+++ src/pythonGui/karabo_gui/mplwidget/mplplotwidgets.py
@@ -10,7 +10,7 @@ class MplWidget(QWidget):
     """
 
     def __init__(self, parent=None, **kwargs):
-        super(QWidget, self).__init__()
+        super(MplWidget, self).__init__()
         self._canvas = FigureCanvas(self)
         self._toolbar = PlotToolbar(self._canvas, self)
         self.curves = []
@@ -20,15 +20,15 @@ class MplWidget(QWidget):
         l.addWidget(self._toolbar)
 
     def newCurve(self, x, y, **kwargs):
-        self.curves.extend(self._canvas.axes.plot(x, y, **kwargs))
+        self.curves.extend(self._canvas.figure.gca().plot(x, y, **kwargs))
 
     def updateCurve(self, xdata, ydata, which=-1):
         self.curves[which].set_data(xdata, ydata)
         self._canvas.draw()
 
     def axes_call(self, func, *arg, **kwargs):
-        axesfunc = getattr(self._canvas.axes, func)
+        axesfunc = getattr(self._canvas.figure.gca(), func)
         axesfunc(*arg, **kwargs)
         if 'label' in func:
-            self._canvas.fig.tight_layout()
+            self._canvas.figure.tight_layout()
             self._canvas.draw()
