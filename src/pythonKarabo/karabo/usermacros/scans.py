2a801f1634da1e71a8cbc61c2a034b826142275e
Leonce Mekinda <mekindal@exflqr8246.desy.de>
2017-07-28 22:36:09 +0200

    Base structure - UserMacros and Tests

---
+++ src/pythonKarabo/karabo/usermacros/scans.py
@@ -0,0 +1,26 @@
+from karabo.usermacro import UserMacro
+
+class AScan(UserMacro):
+    pass
+
+class AMesh(AScan):
+    pass
+
+class APathScan(AScan):
+    pass
+
+class DScan(AScan):
+    pass
+
+class TScan(UserMacro):
+    pass
+
+class DMesh(AScan):
+    pass
+
+class AMove(UserMacro):
+    pass
+
+class DMove(UserMacro):
+    pass
+
