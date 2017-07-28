e31f259f7e9e9235c41375af8784a046052ff1e3
Leonce Mekinda <mekindal@exflqr8246.desy.de>
2017-07-28 16:39:13 +0200

    Base structure

---
+++ src/pythonKarabo/karabo/usermacro_api/genericproxy.py
@@ -0,0 +1,21 @@
+
+
+
+class GenericProxy(object):
+    pass
+
+
+class Movable(GenericProxy):
+    pass
+
+class Sensible(GenericProxy):
+    pass
+
+class Coolable(GenericProxy):
+    pass
+
+class Pumpable(GenericProxy):
+    pass
+
+class Closable(GenericProxy):
+    pass
