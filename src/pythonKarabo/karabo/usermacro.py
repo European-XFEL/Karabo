a626c89460e82795f621f773b7ecc45db9acadfd
Leonce Mekinda <mekindal@exflqr8246.desy.de>
2017-07-28 22:36:09 +0200

    Base structure - UserMacros and Tests

---
+++ src/pythonKarabo/karabo/usermacro.py
@@ -0,0 +1,9 @@
+"""
+Public interface to the User Macro API
+"""
+
+from karabo.usermacro_api.usermacro import UserMacro
+from karabo.usermacro_api.genericproxy import (
+    Closable, Coolable, GenericProxy, Movable, Pumpable, Sensible
+)
+
