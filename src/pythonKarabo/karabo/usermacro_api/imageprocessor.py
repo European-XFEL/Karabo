8690319dfc8a543d57b8229adb9e83f533467347
Leonce Mekinda <mekindal@exflqr8246.desy.de>
2017-07-29 10:42:12 +0200

    flake8 compliant

---
+++ src/pythonKarabo/karabo/usermacro_api/imageprocessor.py
@@ -0,0 +1,7 @@
+""""Generalized interface to the image processor"""
+from genericproxy import Sensible
+
+
+class ImageProcessorAsSensible(Sensible):
+    """"Generalized interface to the image processor device"""
+    generalizes = ['ImageProcessor']
