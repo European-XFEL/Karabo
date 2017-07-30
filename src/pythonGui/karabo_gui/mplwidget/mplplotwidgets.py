7d1e50ce3bbddcc4cd402f8ccb36c1452f820f24
Chen Xu <chen.xu@xfel.eu>
2017-07-30 13:27:16 +0200

    highlight selected curve, address MR comment

--- src/pythonGui/karabo_gui/mplwidget/mplplotwidgets.py
+++ src/pythonGui/karabo_gui/mplwidget/mplplotwidgets.py
@@ -2,7 +2,6 @@ from PyQt4.QtGui import QWidget, QVBoxLayout
 
 from .mplbackends import (FigureCanvas, PlotToolbar)
 
-
 class MplWidget(QWidget):
     """This is a QWidget holds the canvas and plot tools. The canvas handles
     system interactions (draw, react to key/mouse press), plot tools
@@ -11,8 +10,8 @@ class MplWidget(QWidget):
 
     def __init__(self, parent=None, **kwargs):
         super(MplWidget, self).__init__()
-        self._canvas = FigureCanvas(self)
-        self._toolbar = PlotToolbar(self._canvas, self)
+        self._canvas = FigureCanvas(parent=self)
+        self._toolbar = PlotToolbar(self._canvas, parent=self)
         self.curves = []
 
         l = QVBoxLayout(self)
@@ -20,15 +19,17 @@ class MplWidget(QWidget):
         l.addWidget(self._toolbar)
 
     def newCurve(self, x, y, **kwargs):
-        self.curves.extend(self._canvas.figure.gca().plot(x, y, **kwargs))
+        l, = self._canvas.figure.gca().plot(x, y, picker=True, **kwargs)
+        self.curves.append(l)
 
     def updateCurve(self, xdata, ydata, which=-1):
-        self.curves[which].set_data(xdata, ydata)
-        self._canvas.draw()
+        if len(self.curves):
+            self.curves[which].set_data(xdata, ydata)
+            self._canvas.draw()
 
     def axes_call(self, func, *arg, **kwargs):
         axesfunc = getattr(self._canvas.figure.gca(), func)
         axesfunc(*arg, **kwargs)
-        if 'label' in func:
+        if func in ('set_xlabel', 'set_ylabel'):
             self._canvas.figure.tight_layout()
             self._canvas.draw()
