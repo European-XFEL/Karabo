f0e1a8f6be38a38b5a04df0262b3026f70b93435
Leonce Mekinda <mekindal@exflqr8246.desy.de>
2017-07-31 12:09:20 +0200

    Solving the __subclass__() visibility issue

--- src/pythonKarabo/karabo/usermacros.py
+++ src/pythonKarabo/karabo/usermacros.py
@@ -2,22 +2,22 @@
 Public interface to the User Macro API
 """
 
-from karabo.usermacro_api.agipd import AgipdAsSensible
-from karabo.usermacro_api.beckhoff import (
+from .usermacro_api.agipd import AgipdAsSensible
+from .usermacro_api.beckhoff import (
     BeckhoffMc2AsMovable, BeckhoffMotorAsMovable
 )
-from karabo.usermacro_api.epix import EpixAsSensible
+from .usermacro_api.epix import EpixAsSensible
 from karabo.usermacro_api.genicam import (
     GenicamBaslerAsSensible, PhotonicScienceAsSensible
 )
-from karabo.usermacro_api.lima import (
+from .usermacro_api.lima import (
     LimaBaslerAsSensible, LimaSimulatedAsSensible
 )
-from karabo.usermacro_api.lpd import LpdAsSensible
-from karabo.usermacro_api.usermacro import UserMacro
-from karabo.usermacro_api.genericproxy import (
+from .usermacro_api.lpd import LpdAsSensible
+from .usermacro_api.usermacro import UserMacro
+from .usermacro_api.genericproxy import (
     Closable, Coolable, GenericProxy, Movable, Pumpable, Sensible
 )
-from karabo.usermacro_api.scans import (
+from .usermacro_api.scans import (
     AScan, AMesh, AMove, APathScan, DScan, DMesh, DMove
 )
