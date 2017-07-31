aa5d6db083311b255ae380f7f7fa422f70dab4ee
John Wiggins <john.wiggins@desy.de>
2017-07-31 14:42:29 +0200

    Fix the Windows post-installation script [ci skip]
    XFEL-CLOSED #18441
    
    The entrypoint for the GUI has changed...

--- src/pythonGui/scripts/win_post_install.py
+++ src/pythonGui/scripts/win_post_install.py
@@ -1,7 +1,8 @@
 #!python
 """Windows-specific part of the installation"""
 from __future__ import print_function
-import os, sys
+import os
+import sys
 from distutils.sysconfig import get_python_lib
 
 
@@ -9,32 +10,36 @@ def create_shortcut_safe(target, description, link_file, args, workdir):
     create_shortcut(target, description, link_file, args, workdir)
     file_created(link_file)
 
+
 def install():
     prefix = sys.prefix
     python = os.path.join(prefix, 'pythonw.exe')
     title = 'KaraboGUI-VERSION'
 
     # Lookup path to common startmenu ...
-    mv_dir = get_special_folder_path('CSIDL_COMMON_PROGRAMS') 
+    mv_dir = get_special_folder_path('CSIDL_COMMON_PROGRAMS')
     mv_dir = os.path.join(mv_dir, title)
 
     if not os.path.isdir(mv_dir):
         os.mkdir(mv_dir)
         directory_created(mv_dir)
 
-    guiScript = os.path.join(get_python_lib(), 'karabo_gui', 'main.py')
-    workDir=get_special_folder_path('CSIDL_DESKTOPDIRECTORY')
+    guiScript = os.path.join(get_python_lib(), 'karabo_gui',
+                             'programs', 'gui_runner.py')
+    workDir = get_special_folder_path('CSIDL_DESKTOPDIRECTORY')
 
     # Create program shortcuts ...
     shortcut = os.path.join(mv_dir, "{}.lnk".format(title))
     create_shortcut_safe(python, title, shortcut, guiScript, workdir=workDir)
 
+
 def remove():
     pass
 
+
 if len(sys.argv) > 1:
     if sys.argv[1] == '-install':
-        f = open(r"D:\test.txt",'w')
+        f = open(r"D:\test.txt", 'w')
         print('Creating Shortcut', file=f)
         install()
     elif sys.argv[1] == '-remove':
@@ -42,4 +47,3 @@ if len(sys.argv) > 1:
     else:
         pass
         #print "Script was called with option %s" % sys.argv[1]"
-
