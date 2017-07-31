1a1aeab20ac736fe5bed1cd6bd18ba990ebfc0ac
Gero Flucke <gero.flucke@xfel.eu>
2017-07-31 19:04:25 +0200

    Adjust test to changed Schema of bound Python device

--- src/pythonKarabo/karabo/middlelayer_api/tests/cross_test.py
+++ src/pythonKarabo/karabo/middlelayer_api/tests/cross_test.py
@@ -96,7 +96,7 @@ class Tests(DeviceTest):
             stdout=PIPE)
         schema = yield from self.process.stdout.read()
         yield from self.process.wait()
-        self.assertEqual(adler32(schema), 1484824827,
+        self.assertEqual(adler32(schema), 2129202963,
             "The generated schema changed. If this is desired, change the "
             "checksum in the code.")
 
