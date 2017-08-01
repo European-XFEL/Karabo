5532c2655e8c0529997e9b8ff38384c952e0e264
wajid <wajid@exflpcx18808.desy.de>
2017-08-01 18:44:46 +0200

    [ci skip] SCANTOOL: implementation of adc and cameras as sensible

---
+++ src/pythonKarabo/karabo/usermacro_api/adc.py
@@ -0,0 +1,26 @@
+"""Generalized interface to the ADC"""
+from .genericproxy import Sensible
+from asyncio import coroutine
+from karabo.middlelayer import State
+
+
+class AdcAsSensible(Sensible):
+    """Generalized interface to the fast ADC device"""
+    generalizes = ['fastAdc', 'adqDigitizer']
+    state_mapping = {State.STARTED : State.ACQUIRING}
+
+    @property  # Will be later moved to the Generic Proxy class
+    def state(self):
+        return self.state_mapping.get(self._proxy.state, self._proxy.state)
+
+
+    @coroutine
+    def acquire(self):
+        """ start data acquisition """
+        yield from self._proxy.start()
+
+
+    @coroutine
+    def stop(self):
+        """ Stop data acquisition """
+        yield from self._proxy.stop()
