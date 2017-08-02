8587f6709478507fb47ea640da56e59e7086fe19
Gabriele Giovanetti <gabriele.giovanetti@xfel.eu>
2017-08-02 14:26:36 +0200

    ImageProcessorAsSensible and SpectrometerAsSensible written. Not yet tested

--- src/pythonKarabo/karabo/usermacro_api/imageprocessor.py
+++ src/pythonKarabo/karabo/usermacro_api/imageprocessor.py
@@ -1,7 +1,18 @@
 """"Generalized interface to the image processor"""
 from .genericproxy import Sensible
+from karabo.middlelayer import State
 
 
 class ImageProcessorAsSensible(Sensible):
     """"Generalized interface to the image processor device"""
     generalizes = ['ImageProcessor']
+
+    state_mapping = {
+        State.ACQUIRING: State.NORMAL
+    }
+
+    def acquire(self):
+        pass  # No action is needed to put imageProcessor in acquisition
+
+    def stop(self):
+        pass
