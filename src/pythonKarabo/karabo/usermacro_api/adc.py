323412112bff0592c4ebaea0dc4d4d3b4c68fd9d
wajid <wajid@exflpcx18808.desy.de>
2017-08-02 15:46:56 +0200

    [ci skip]: change coroutine to synchronize

--- src/pythonKarabo/karabo/usermacro_api/adc.py
+++ src/pythonKarabo/karabo/usermacro_api/adc.py
@@ -1,24 +1,25 @@
 """Generalized interface to the ADC"""
 from .genericproxy import Sensible
-from asyncio import coroutine
+from karabo.middlelayer_api.eventloop import synchronize
 from karabo.middlelayer import State
 
 
 class AdcAsSensible(Sensible):
-    """Generalized interface to the fast ADC device"""
+    """Generalized interface to the  ADC device"""
     generalizes = ['fastAdc', 'adqDigitizer']
     state_mapping = {State.STARTED: State.ACQUIRING}
 
     @property  # Will be later moved to the Generic Proxy class
     def state(self):
+        """ get state """
         return self.state_mapping.get(self._proxy.state, self._proxy.state)
 
-    @coroutine
+    @synchronize
     def acquire(self):
         """ start data acquisition """
         yield from self._proxy.start()
 
-    @coroutine
+    @synchronize
     def stop(self):
         """ Stop data acquisition """
         yield from self._proxy.stop()
