9b69ece1d92af75958a729297ae373fcba80f7ba
Leonce Mekinda <mekindal@exflqr8246.desy.de>
2017-08-03 23:11:28 +0200

    Fixing __repr__

--- src/pythonKarabo/karabo/usermacro_api/beckhoff.py
+++ src/pythonKarabo/karabo/usermacro_api/beckhoff.py
@@ -5,7 +5,7 @@ from .genericproxy import Movable
 
 class BeckhoffMotorAsMovable(Movable):
     """Generalized interface to Beckhoff motors"""
-    generalizes = ('BeckhoffSimpleMotor', 'BeckhoffMC2Motor', 'Device')
+    generalizes = ('BeckhoffSimpleMotor', 'BeckhoffMC2Motor')
 
     state_mapping = {
         State.STOPPED: State.ON,  # needed  for current version of MC2 device
