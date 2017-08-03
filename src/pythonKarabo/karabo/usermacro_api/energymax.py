1b72110bc7a581ad2a8e6cd689f77e0111da1b2d
Gabriele Giovanetti <gabriele.giovanetti@xfel.eu>
2017-08-03 16:03:12 +0200

    added EnergyMaxAsSensible

---
+++ src/pythonKarabo/karabo/usermacro_api/energymax.py
@@ -0,0 +1,20 @@
+"""Generalized interface to EnergyMax device """
+
+from karabo.middlelayer import State
+from .genericproxy import Sensible
+from karabo.middlelayer_api.proxy import ProxyBase, synchronize
+
+
+class EnergyMaxAsSensible(Sensible):
+    """Generalized interface to EnergyMax class """
+    generalizes = ('EnergyMax')
+
+    state_mapping = {
+        State.STARTED: State.ACQUIRING,
+    }
+
+    @synchronize
+    def acquire(self):
+        """Start acquisition"""
+        yield from self._proxy.start()
+
