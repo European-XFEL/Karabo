561964b236daa9ed487f3774e8b120b66e56ac3c
Gabriele Giovanetti <gabriele.giovanetti@xfel.eu>
2017-08-04 15:05:47 +0200

    minor style changes

--- src/pythonKarabo/karabo/usermacro_api/energymax.py
+++ src/pythonKarabo/karabo/usermacro_api/energymax.py
@@ -1,17 +1,15 @@
 """Generalized interface to EnergyMax device """
 
 from karabo.middlelayer import State
-from .genericproxy import Sensible
 from karabo.middlelayer_api.proxy import synchronize
+from .genericproxy import Sensible
 
 
 class EnergyMaxAsSensible(Sensible):
     """Generalized interface to EnergyMax class """
     generalizes = ('EnergyMax')
 
-    state_mapping = {
-        State.STARTED: State.ACQUIRING,
-    }
+    state_mapping = { State.STARTED: State.ACQUIRING }
 
     @synchronize
     def acquire(self):
