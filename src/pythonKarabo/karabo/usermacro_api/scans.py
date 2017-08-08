3931336a324b00350c0b23e113c66d154b63cd50
Leonce Mekinda <mekindal@exflqr8246.desy.de>
2017-08-08 22:07:15 +0200

    First scan

--- src/pythonKarabo/karabo/usermacro_api/scans.py
+++ src/pythonKarabo/karabo/usermacro_api/scans.py
@@ -1,42 +1,60 @@
 """"Scantool macros"""
+from asyncio import coroutine
+
+from karabo.middlelayer import State, waitUntil
 from .usermacro import UserMacro
+from .genericproxy import Movable, Sensible
 
 
 class AScan(UserMacro):
     """Absolute scan"""
-    pass
+    _movable = Movable()
+    _pos_list = []
+    _sensible = Sensible()
+
+    def __init__(self, movable, pos_list, sensible, exposure_time, steps=True, number_of_steps=0, **kwargs):
+        super().__init__(kwargs)
+        self._movable = movable if isinstance(movable, Movable) else Movable(movable)
+        self._pos_list = pos_list
+        self._sensible = sensible if isinstance(sensible, Sensible) else Sensible(sensible)
+
+    @coroutine
+    def execute(self):
+        """Body. Override UserMacro's"""
+
+        self._movable.prepare()
+        #self._sensible.prepare()
+
+        for i, pos in enumerate(self._pos_list):
+            print("Step {} - Motors at {}".format(i, self._movable.position))
+            yield from self._movable.moveto(pos)
+            yield from waitUntil(lambda: self._movable.state == State.STOPPED)
+            yield from self._sensible.acquire()
 
 
 class AMesh(AScan):
     """Absolute Mesh scan"""
-    pass
 
 
 class APathScan(AScan):
     """Absolute Mesh scan"""
-    pass
 
 
 class DScan(AScan):
     """Delta scan"""
-    pass
 
 
 class TScan(UserMacro):
     """Time scan"""
-    pass
 
 
 class DMesh(AScan):
     """Delta Mesh scan"""
-    pass
 
 
 class AMove(UserMacro):
     """Absolute move"""
-    pass
 
 
 class DMove(UserMacro):
     """Delta move"""
-    pass
