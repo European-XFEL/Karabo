a5e0580232e4d529241fddc8bcf0dfa2d62b7425
Leonce Mekinda <mekindal@exflqr8246.desy.de>
2017-08-09 08:43:59 +0200

    Stopping sensibles after exposure time

--- src/pythonKarabo/karabo/usermacro_api/scans.py
+++ src/pythonKarabo/karabo/usermacro_api/scans.py
@@ -1,32 +1,34 @@
 """"Scantool macros"""
 from asyncio import coroutine
 
-from karabo.middlelayer import Bool, Float, Int32, State, String, waitUntil
+from karabo.middlelayer import (
+    Bool, Float, Int32, State, String, sleep, waitUntil)
 from .usermacro import UserMacro
 from .genericproxy import Movable, Sensible
 
 
 class AScan(UserMacro):
     """Absolute scan"""
-    _movableId = String(displayedName="Movable")
-    _sensibleId = String(displayedName="Sensible")
-    _movable = Movable()
-    _sensible = Sensible()
-    _pos_list = []
+    movableId = String(displayedName="Movable")
+    sensibleId = String(displayedName="Sensible")
     exposure_time = Float(displayedName="Exposure time", defaultValue=0.001)
     steps = Bool(displayedName="Steps", defaultValue=True)
     number_of_steps = Int32(displayedName="Number of steps", defaultValue=0)
+    _movable = Movable()
+    _sensible = Sensible()
+    _pos_list = []
 
     def __init__(self, movable, pos_list, sensible, exposure_time,
                  steps=True, number_of_steps=0, **kwargs):
         super().__init__(kwargs)
-        self._movable = movable if isinstance(
-                                       movable, Movable) else Movable(movable)
+        self._movable = (movable
+                         if isinstance(movable, Movable)
+                         else Movable(movable))
         self._movableId = self._movable.deviceId
         self._pos_list = pos_list
-        self._sensible = sensible if isinstance(
-                                       sensible, Sensible) else Sensible(
-                                                                    sensible)
+        self._sensible = (sensible
+                          if isinstance(sensible, Sensible)
+                          else Sensible(sensible))
         self._sensibleId = self._sensible.deviceId
         self._exposure_time = exposure_time
         self._steps = steps
@@ -36,15 +38,20 @@ class AScan(UserMacro):
     def execute(self):
         """Body. Override UserMacro's"""
 
-        self._movable.prepare()
-        self._sensible.prepare()
+        # Prepare for acquition
+        yield from self._movable.prepare()
+        yield from self._sensible.prepare()
 
+        # Iterate over positions
         for i, pos in enumerate(self._pos_list):
             yield from self._movable.moveto(pos)
             yield from waitUntil(lambda: self._movable.state != State.MOVING)
             print("Step {} - Motors at {}"
                   .format(i + 1, self._movable.position))
+
             yield from self._sensible.acquire()
+            yield from sleep(self._exposure_time)
+            yield from self._sensible.stop()
 
 
 class AMesh(AScan):
