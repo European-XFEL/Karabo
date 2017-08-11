74f3d65b13f1644128fdfd38b216f4f8620e24a3
Cyril Danilevski <cyril.danilevski@desy.de>
2017-08-11 12:28:50 +0200

    Add tests for Scan
    I did not know how the Scan is meant to work, so I worte tests
    ¯\_(ツ)_/¯

--- src/pythonKarabo/karabo/usermacro_api/tests/scans_test.py
+++ src/pythonKarabo/karabo/usermacro_api/tests/scans_test.py
@@ -4,15 +4,79 @@ To run all tests, execute:
   nosetests -v
 """
 
+from asyncio import coroutine
+from contextlib import contextmanager
 import unittest
 import numpy as np
 
+from karabo.middlelayer import Device, KaraboError, Slot
 from karabo.middlelayer_api.tests.eventloop import DeviceTest, sync_tst
-from karabo.usermacros import meshTrajectory, splitTrajectory
+from karabo.usermacros import (AScan, GenericProxy, meshTrajectory,
+                               splitTrajectory)
+
+
+class TestDev(Device):
+
+    def __init__(self, configuration):
+        super().__init__(configuration)
+        self.lockedBy = ""
+        self._lock_count = 0
+
+    @Slot()
+    def ping(self):
+        print("Ping!")
+
+
+def getMockDevice(name, **kwargs):
+    cls = type(name, (TestDev,), {})
+    return cls(kwargs)
 
 
 class Tests(DeviceTest):
-    """Test the Scan classes"""
+    """The tests in this class run on behalf of the device "local".
+
+    As this class inherits from :class:`DeviceTest`, it is possible to provide
+    a :func:`lifetimeManager`, such that the test can be run within an
+    eventloop
+    """
+
+    @classmethod
+    @contextmanager
+    def lifetimeManager(cls):
+
+        cls.local = TestDev({"_deviceId_": "GenericProxy_UnitTests"})
+
+        cls.tm1 = getMockDevice("BeckhoffSimpleMotor",
+                                _deviceId_="tm1",
+                                stepLength=0)
+        cls.tm2 = getMockDevice("BeckhoffSimpleMotor",
+                                _deviceId_="tm2",
+                                stepLength=0)
+        cls.tm3 = getMockDevice("BeckhoffSimpleMotor",
+                                _deviceId_="tm3",
+                                stepLength=0)
+        cls.lsim = getMockDevice("LimaSimulatedCamera",
+                                 _deviceId_="lsim",
+                                 cameraType="Simulator")
+        with cls.deviceManager(cls.lsim, cls.tm2, cls.tm3,
+                               cls.tm1, lead=cls.local):
+            yield
+
+    @sync_tst
+    def setUp(self):
+        self.m1 = GenericProxy('tm1')
+        self.pos = [(0,0), (10, 10), (15, 15)]
+        self.sens = GenericProxy('lsim')
+        self.expo = 5
+
+    @sync_tst
+    def test_single_device_instantiation(self):
+        expected_rep = ("AScan('tm1', [(0, 0), (10, 10), (15, 15)], "
+                        "'lsim', 5, steps=True, number_of_steps=0)")
+
+        scaney = AScan(self.m1, self.pos, self.sens, self.expo)
+        self.assertEqual(type(scaney), AScan)
+        self.assertEqual(scaney.__repr__(), expected_rep)
 
     @sync_tst
     def test_len_2_1D_trajectory_split_2(self):
@@ -62,7 +126,6 @@ class Tests(DeviceTest):
                 result.append(point)
 
         oracle = list(np.linspace(traj[0], traj[-1], num=num+1))
-
         self.assertEqual(len(result), len(oracle))
 
         for res, ora in zip(result, oracle):
@@ -71,7 +134,7 @@ class Tests(DeviceTest):
     def test_mesh_trajectory(self):
         """Test the generation of a mesh trajectory"""
         result = [point for point in meshTrajectory(range(3), range(3))]
-        oracle = [(0, 0), (0, 1), (0, 2), 
+        oracle = [(0, 0), (0, 1), (0, 2),
                   (1, 2), (1, 1), (1, 0),
                   (2, 0), (2, 1), (2, 2)]
 
