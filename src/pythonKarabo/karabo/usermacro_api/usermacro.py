92d1e29e6b87e685178429a0a028aa96c3d2731c
Leonce Mekinda <mekindal@exflqr8246.desy.de>
2017-08-14 14:02:22 +0200

    Device showing-up  but only a single run allowed in ikarabo

--- src/pythonKarabo/karabo/usermacro_api/usermacro.py
+++ src/pythonKarabo/karabo/usermacro_api/usermacro.py
@@ -1,15 +1,42 @@
+#!/usr/bin/env python3
 """User Macros implement as-simple-as-possible
    domain-level routines for Light-Source operations
 """
+import argparse
 from asyncio import coroutine, get_event_loop, set_event_loop
 from contextlib import closing
 import os
 import socket
 
 from karabo.middlelayer import (
-    AccessMode, Float, Macro, MetricPrefix, Slot, State, Unit)
-from karabo.middlelayer_api.eventloop import EventLoop, NoEventLoop
-from .pipeline import OutputChannel
+    AccessMode, Device, Float, Macro, MetricPrefix, Slot, State, Unit)
+from karabo.middlelayer_api.eventloop import EventLoop
+from karabo.usermacro_api.pipeline import OutputChannel
+
+
+def run_in_event_loop(macro):
+    """ To run macro"""
+    @coroutine
+    def __run():
+        macro.state = State.ACTIVE
+        yield from macro.execute()
+        macro.state = State.PASSIVE
+        yield from macro.slotKillDevice()
+
+    loop = get_event_loop()
+    if not isinstance(loop, EventLoop):
+        loop = (
+            EventLoop.global_loop
+            if EventLoop.global_loop is not None
+            else EventLoop())
+        set_event_loop(loop)
+
+    if loop.is_running():
+        loop.call_soon_threadsafe(
+            loop.create_task, __run())
+    else:
+        with closing(loop):
+            loop.run_until_complete(__run())
 
 
 class UserMacro(Macro):
@@ -29,6 +56,16 @@ class UserMacro(Macro):
 
     outputChannel = OutputChannel()
 
+    def __init__(self, **kwargs):
+        bareHostName = socket.gethostname().partition('.')[0]
+        deviceId = "{}_{}_{}".format(
+            type(self).__name__, bareHostName, os.getpid())
+        if "deviceId" in kwargs:
+            kwargs["_deviceId_"] = kwargs["deviceId"]
+        else:
+            kwargs["_deviceId_"] = kwargs["deviceId"] = deviceId
+        super().__init__(kwargs)
+
     @Slot(displayedName="Start", allowedStates={State.PASSIVE})
     def start(self):
         """Start the user macro"""
@@ -38,54 +75,39 @@ class UserMacro(Macro):
     def _run(self, **kwargs):
         """Skip the expert macro RemoteDevice functionality"""
         self.state = State.PASSIVE
-        # yield from Device._run(self, **kwargs)
-
-    @staticmethod
-    def run_in_event_loop(macro):
-        """ To run macro, reuse the running event loop if any"""
-        loop = get_event_loop()
-        if isinstance(loop, NoEventLoop):
-            if EventLoop.global_loop is not None:
-                loop = EventLoop.global_loop
-            else:
-                loop = EventLoop()
-                set_event_loop(loop)
-
-        @coroutine
-        def __run():
-            macro.state = State.ACTIVE
-            yield from macro.execute()
-            macro.state = State.PASSIVE
-            yield from macro.slotKillDevice()
-
-        if loop.is_running():
-            loop.call_soon_threadsafe(
-                loop.create_task, __run())
-        else:
-            with closing(loop):
-                loop.run_until_complete(__run())
+        yield from Device._run(self, **kwargs)
 
     def __call__(self):
-        type(self).run_in_event_loop(self)
+        run_in_event_loop(self)
 
     @coroutine
     def execute(self):
         """Insert here the code of a UserMacro"""
 
     @classmethod
-    def main(cls, **kwargs):
+    def main(cls):
+        """Parse from command line"""
+        kwargs = {}
+        parser = argparse.ArgumentParser(
+            description="Run a {}.".format(cls.__name__))
+        parser.add_argument(
+            "--id", nargs="?",
+            metavar="deviceId", type=str, help="The macro DeviceId")
+        parser.add_argument(
+            "arg", nargs="*", type=str, help="Argument")
+        kwargs = vars(parser.parse_args())
+        args = kwargs["arg"]
+        if args:
+            del kwargs["arg"]
 
-        if "deviceId" in kwargs:
-            kwargs["_deviceId_"] = kwargs["deviceId"]
-        else:
-            bareHostName = socket.gethostname().partition('.')[0]
-            kwargs["_deviceId_"] = "{}_{}_{}".format(
-                cls.__name__, bareHostName, os.getpid())
-
-        cls.run_in_event_loop(cls(kwargs))
+        run_in_event_loop(cls(*args, **kwargs))
 
 
 def display(image):
     """Display an image into karabo GUI"""
     instance = get_event_loop().instance()
     instance.outputChannel = image
+
+
+if __name__ == "__main__":
+    UserMacro.main()
