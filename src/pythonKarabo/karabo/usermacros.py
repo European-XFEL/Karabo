b4a22139c653580e079eebad5723ead46e25fc57
Gabriele Giovanetti <gabriele.giovanetti@xfel.eu>
2017-08-16 17:51:37 +0200

    added mock-up for AcquiredFromLog child class

--- src/pythonKarabo/karabo/usermacros.py
+++ src/pythonKarabo/karabo/usermacros.py
@@ -11,7 +11,8 @@ from .usermacro_api.generalized import (
     EpixAsSensible, ImageProcessorAsSensible, LpdAsSensible
 )
 from .usermacro_api.pipeline import OutputChannel
-from .usermacro_api.dataobjects import AcquiredData
+from .usermacro_api.dataobjects import AcquiredData, AcquiredFromLog
+
 from .usermacro_api.scans import (
     AScan, AMesh, AMove, APathScan, DScan, DMesh, DMove, meshTrajectory,
     splitTrajectory, TScan
