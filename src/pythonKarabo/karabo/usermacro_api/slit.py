8d04bfdc6ef25202f12958007091bebf4c62d196
Leonce Mekinda <mekindal@exflqr8246.desy.de>
2017-08-17 10:54:00 +0200

    Supporting multidimensional slit

--- src/pythonKarabo/karabo/usermacro_api/slit.py
+++ src/pythonKarabo/karabo/usermacro_api/slit.py
@@ -11,10 +11,21 @@ class SlitSystemAsMovable(Movable):
     @property
     def position(self):
         """"Position getter """
-        return self._proxy.actualPositionX.magnitude
+        return (self._proxy.actualPositionX.magnitude,
+                self._proxy.actualPositionY.magnitude,
+                self._proxy.actualGapX.magnitude,
+                self._proxy.actualGapY.magnitude)
 
     @synchronize
     def moveto(self, pos):
-        """Move to *pos*"""
-        self._proxy.targetPositionX = pos
+        """Move to a multidimentional pos"""
+        itpos = iter(pos)
+        self._proxy.targetPositionX = next(itpos)
+        self._proxy.targetPositionY = next(itpos)
+        self._proxy.targetGapX = next(itpos)
+        self._proxy.targetGapY = next(itpos)
+
         yield from self._proxy.moveX()
+        yield from self._proxy.moveY()
+        yield from self._proxy.moveGapX()
+        yield from self._proxy.moveGapY()
