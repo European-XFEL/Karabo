55e86d95442d464421e4d05ba6dea8f4180c68c8
Leonce Mekinda <mekindal@exflqr8246.desy.de>
2017-08-18 10:59:10 +0200

    Working multidensional version

--- src/pythonKarabo/karabo/usermacro_api/slit.py
+++ src/pythonKarabo/karabo/usermacro_api/slit.py
@@ -1,6 +1,6 @@
 """Generalized interfaces to the simulated Slit System
 """
-from karabo.middlelayer import sleep, State, waitUntil
+from karabo.middlelayer import State, waitUntil
 from karabo.middlelayer_api.eventloop import synchronize
 from .genericproxy import Movable
 
@@ -22,29 +22,26 @@ class SlitSystemAsMovable(Movable):
     def moveto(self, pos):
         """Move to a multidimentional pos"""
         itpos = iter(pos)
-        self._proxy.targetPositionX = next(itpos)
-        self._proxy.targetPositionY = next(itpos)
-        self._proxy.targetGapX = next(itpos)
-        self._proxy.targetGapY = next(itpos)
+        self._proxy.targetPositionX = targetX = next(itpos)
+        self._proxy.targetPositionY = targetY = next(itpos)
+        self._proxy.targetGapX = gapX = next(itpos)
+        self._proxy.targetGapY = gapY = next(itpos)
 
-        if abs(self._proxy.targetPositionX
-               - self._proxy.actualPositionX) > self.fepsilon:
+        if abs(self._proxy.targetPositionX.magnitude
+               - targetX) > self.fepsilon:
             yield from self._proxy.moveX()
 
-        if abs(self._proxy.targetPositionY
-               - self._proxy.actualPositionY) > self.fepsilon:
-            yield from sleep(0.5)
+        if abs(self._proxy.targetPositionY.magnitude
+               - targetY) > self.fepsilon:
             yield from waitUntil(lambda: self._proxy.state != State.MOVING)
             yield from self._proxy.moveY()
 
-        if abs(self._proxy.targetGapX
-               - self._proxy.actualGapX) > self.fepsilon:
-            yield from sleep(0.5)
+        if abs(self._proxy.targetGapX.magnitude
+               - gapX) > self.fepsilon:
             yield from waitUntil(lambda: self._proxy.state != State.MOVING)
             yield from self._proxy.moveGapX()
 
-        if abs(self._proxy.targetGapY
-               - self._proxy.actualGapY) > self.fepsilon:
-            yield from sleep(0.5)
+        if abs(self._proxy.targetGapY.magnitude
+               - gapY) > self.fepsilon:
             yield from waitUntil(lambda: self._proxy.state != State.MOVING)
             yield from self._proxy.moveGapY()
