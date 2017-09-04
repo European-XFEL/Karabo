f4c61e0aa23292c3e7b9ca0946dd26a81786d9df
Gabriele Giovanetti <gabriele.giovanetti@xfel.eu>
2017-09-04 14:28:47 +0200

    Added plotLoggedData() to util.py
    Example of ikarabo commands to have it working:
    %matplotlib qt
    from karabo.usermacros import *
    ad = AcquiredFromLog("AScan_exflqr13053_ac25ccc6-08b1-4e19-8c6d-494b4f9f57a1", 1000)
    ad.queryData('BSM1.encoderPosition', 'BSM2.encoderPosition', 'SIMCAM.frameRate')
    p = plotLoggedData(ad, "2017-08-22T13:25 UTC", "2017-08-22T13:35 UTC")

--- src/pythonKarabo/karabo/usermacro_api/util.py
+++ src/pythonKarabo/karabo/usermacro_api/util.py
@@ -6,6 +6,12 @@ from karabo.middlelayer_api.device_client import (
 
 from karabo.middlelayer_api.eventloop import synchronize
 
+# imports used for plotLoggedData
+import datetime
+import matplotlib.pyplot as pl
+import matplotlib.dates as mdates
+DATE_FORMAT = "%Y-%m-%dT%H:%M:%S"
+
 
 def flatten(lis):
     """ Flattens a list that may contain sublists"""
@@ -43,3 +49,67 @@ def getConfigurationFromPast(deviceId, timepoint):
     """Calls slotGetConfigurationFromPast on dataLoglReader"""
     timepoint = _parse_date(timepoint)
     return _getConfigurationFromPast(deviceId, timepoint)
+
+
+def plotLoggedData(data, begin, end):
+    """
+    Plots acquired from log data in a given time interval
+
+    :param d: an AcquiredFromLog object with loaded data
+    :param begin: date and time when the required time interval starts
+    :param end: date and time when the required time interval ends
+    :return: data formatted in a conveinient form to plot with matplotlib, i.e.
+            a dictionary which keys are plot names with the form
+            'deviceId.property', and values are in turn dictionaries
+            with 'value' and 'timestamp' as keys and list of data as values
+
+    begin and end parameters are time string in the form
+    "2009-09-01T15:32:12 UTC", but any part of the string can be omitted,
+    like "10:32". Only giving the time, where we assume the current day.
+    Unless specified otherwise, your local timezone is assumed.
+    If matplotlib backend is set properly (e.g. %matplotlib qt) charts are
+    drown in a default plot.
+
+    """
+
+    begin = datetime.datetime.strptime(_parse_date(begin), DATE_FORMAT)
+    end = datetime.datetime.strptime(_parse_date(end), DATE_FORMAT)
+
+    data4plots = {}
+
+    for d in data:
+        ts = datetime.datetime.fromtimestamp(d.get('timestamp'))
+        print(ts)
+        if ts < begin or ts > end:
+            print(ts, "out of range", begin, end)
+            continue
+
+        did = d.get('deviceId')
+        propname = d.getKeys()[3]
+
+        plotname = "{}.{}".format(did, propname)
+        if plotname not in data4plots.keys():
+            data4plots[plotname] = {'timestamp': [], 'value': []}
+
+        data4plots[plotname]['timestamp'].append(ts)
+        data4plots[plotname]['value'].append(d.get(propname))
+
+    # draw default plot
+    ax = None
+    fig = pl.figure(figsize=(8, 6))
+    ax = fig.add_axes([0.1, 0.1, 0.8, 0.8])
+
+    for plotname in data4plots.keys():
+        ax.plot_date(data4plots[plotname]['timestamp'],
+                     data4plots[plotname]['value'], label=plotname)
+
+        ax.xaxis.set_major_locator(mdates.AutoDateLocator())
+        ax.xaxis.set_major_formatter(mdates.DateFormatter("%Y/%m/%d %H:%M"))
+        for l in ax.get_xticklabels():
+            l.set_rotation(45)
+        ax.legend()
+        pl.grid(b=True, which='major', color='b', linestyle=':')
+        pl.title(data.experimentId)
+
+    # return easy to plot data
+    return data4plots
