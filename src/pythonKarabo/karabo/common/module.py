990bfd62259c3a1e28c2cf95880b47576f26b915
John Wiggins <john.wiggins@desy.de>
2017-09-15 12:27:12 +0200

    Add a create_module function to karabo.common
    This makes it possible to build "virtual" submodules in places like
    API modules.

---
+++ src/pythonKarabo/karabo/common/module.py
@@ -0,0 +1,22 @@
+import sys
+import types
+
+
+def create_module(name, *symbols, docstring=''):
+    """Create a module, fill it with symbols, and "import" it into the system
+    module namespace.
+
+    This is useful for defining "virtual" submodules of things like API modules
+    """
+    assert name not in sys.modules
+
+    mod = types.ModuleType(name, docstring)
+    for sym in symbols:
+        sym_name = sym.__name__
+        if isinstance(sym, types.ModuleType):
+            # watch out for module objects!
+            sym_name = sym_name.split('.')[-1]
+        setattr(mod, sym_name, sym)
+
+    sys.modules[name] = mod
+    return mod
