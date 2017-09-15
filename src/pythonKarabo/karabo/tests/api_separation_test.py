95ea27e73538cb3ad00bbd5c760328766554c101
John Wiggins <john.wiggins@desy.de>
2017-09-15 13:49:30 +0200

    Put all the import check tests in one place

---
+++ src/pythonKarabo/karabo/tests/api_separation_test.py
@@ -0,0 +1,49 @@
+from functools import partial
+
+import karabo.bound_api as bound_pkg
+import karabo.common as common_pkg
+import karabo.middlelayer_api as middlelayer_pkg
+import karabo.usermacro_api as usermacro_pkg
+from karabo.testing.import_checker import (
+    check_for_disallowed_module_imports, check_for_star_imports,
+    run_checker_on_package)
+
+
+def test_bound_no_middlelayer_imports():
+    for forbidden in ('karabo.middlelayer', 'karabo.middlelayer_api'):
+        checker = partial(check_for_disallowed_module_imports, forbidden)
+        run_checker_on_package(bound_pkg, checker)
+
+
+def test_bound_no_usermacro_imports():
+    for forbidden in ('karabo.usermacros', 'karabo.usermacro_api'):
+        checker = partial(check_for_disallowed_module_imports, forbidden)
+        run_checker_on_package(bound_pkg, checker)
+
+
+def test_common_no_bound_imports():
+    for forbidden in ('karabo.bound', 'karabo.bound_api'):
+        checker = partial(check_for_disallowed_module_imports, forbidden)
+        run_checker_on_package(common_pkg, checker)
+
+
+def test_common_no_middlelayer_imports():
+    for forbidden in ('karabo.middlelayer', 'karabo.middlelayer_api'):
+        checker = partial(check_for_disallowed_module_imports, forbidden)
+        run_checker_on_package(common_pkg, checker)
+
+
+def test_common_no_star_imports():
+    run_checker_on_package(common_pkg, check_for_star_imports)
+
+
+def test_middlelayer_no_bound_imports():
+    for forbidden in ('karabo.bound', 'karabo.bound_api'):
+        checker = partial(check_for_disallowed_module_imports, forbidden)
+        run_checker_on_package(middlelayer_pkg, checker)
+
+
+def test_usermacro_no_bound_imports():
+    for forbidden in ('karabo.bound', 'karabo.bound_api'):
+        checker = partial(check_for_disallowed_module_imports, forbidden)
+        run_checker_on_package(usermacro_pkg, checker)
