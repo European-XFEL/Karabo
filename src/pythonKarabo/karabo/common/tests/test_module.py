b5df1f14ab7e5623f3c0467902df34101006b305
John Wiggins <john.wiggins@desy.de>
2017-09-15 12:27:12 +0200

    Add a create_module function to karabo.common
    This makes it possible to build "virtual" submodules in places like
    API modules.

---
+++ src/pythonKarabo/karabo/common/tests/test_module.py
@@ -0,0 +1,27 @@
+import sys
+from types import ModuleType
+
+from nose.tools import assert_raises
+
+from karabo.common.api import create_module
+
+
+def test_create_module():
+    def local_func():
+        pass
+
+    name = 'whoa.crazy.new.module'
+
+    assert name not in sys.modules
+
+    module = create_module(name, sys, local_func)
+
+    assert name in sys.modules
+    assert isinstance(module, ModuleType)
+    assert isinstance(module.sys, ModuleType)
+    assert module.local_func is local_func
+
+
+def test_create_module_overwrite():
+    with assert_raises(AssertionError):
+        create_module('sys', sys.path)
