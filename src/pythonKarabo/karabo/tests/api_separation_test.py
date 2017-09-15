4c991bfbc49b05d9e0976e0b8e91e34dc1c4b8d7
John Wiggins <john.wiggins@desy.de>
2017-09-15 12:27:44 +0200

    Clean up the ikarabo import situation

--- src/pythonKarabo/karabo/tests/api_separation_test.py
+++ src/pythonKarabo/karabo/tests/api_separation_test.py
@@ -43,6 +43,12 @@ def test_middlelayer_no_bound_imports():
         run_checker_on_package(middlelayer_pkg, checker)
 
 
+def test_middlelayer_no_usermacro_imports():
+    for forbidden in ('karabo.usermacros', 'karabo.usermacro_api'):
+        checker = partial(check_for_disallowed_module_imports, forbidden)
+        run_checker_on_package(middlelayer_pkg, checker)
+
+
 def test_usermacro_no_bound_imports():
     for forbidden in ('karabo.bound', 'karabo.bound_api'):
         checker = partial(check_for_disallowed_module_imports, forbidden)
