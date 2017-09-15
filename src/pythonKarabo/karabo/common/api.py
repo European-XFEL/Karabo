8bc6cb6a8241a042faa5c760b124b2f48d105b96
John Wiggins <john.wiggins@desy.de>
2017-09-15 12:27:12 +0200

    Add a create_module function to karabo.common
    This makes it possible to build "virtual" submodules in places like
    API modules.

--- src/pythonKarabo/karabo/common/api.py
+++ src/pythonKarabo/karabo/common/api.py
@@ -2,6 +2,7 @@
 from .alarm_conditions import AlarmCondition
 from .decorators import karabo_deprecated
 from .enums import Capabilities, DeviceStatus
+from .module import create_module
 from .savable import BaseSavableModel, set_modified_flag
 from .states import State, StateSignifier
 from .traits import walk_traits_object
