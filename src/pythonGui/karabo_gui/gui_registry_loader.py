ec6897785e82428500f1e7e9fae8a94841e95f95
John Wiggins <john.wiggins@desy.de>
2017-10-10 14:30:20 +0200

    Don't import * in karabo_gui.gui_registy_loader

--- src/pythonGui/karabo_gui/gui_registry_loader.py
+++ src/pythonGui/karabo_gui/gui_registry_loader.py
@@ -1,6 +1,16 @@
+import importlib
+
+
+def load_widgets():
+    PACKAGES = ('karabo_gui.displaywidgets', 'karabo_gui.editablewidgets')
+    for pkg in PACKAGES:
+        module = importlib.import_module(pkg)
+        for submodule in module.__all__:
+            importlib.import_module('{}.{}'.format(pkg, submodule))
+
 
 # XXX: This is crazy. It's being done for its side effects only.
 # The purpose is to initialize the registry of classes. This needs
 # to be done very early, so this module is imported from gui.init()
-from karabo_gui.displaywidgets import *
-from karabo_gui.editablewidgets import *
+load_widgets()
+del load_widgets
