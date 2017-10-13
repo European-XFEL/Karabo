e774e2929bfea5eaa797cc8cbf92377ac09b9fb4
John Wiggins <john.wiggins@desy.de>
2017-10-13 15:38:16 +0200

    Add a way to launch bound devices from the commandline
    This is really useful if you want to debug an individual device with
    pdb/gdb.

---
+++ src/pythonKarabo/karabo/bound_api/device_runner.py
@@ -0,0 +1,69 @@
+import argparse
+import os
+import threading
+
+from pkg_resources import iter_entry_points
+
+from karabo.bound import Configurator, EventLoop, Hash, PythonDevice
+
+
+def _load_entrypoint(classid):
+    """Load the module containing `classid` so that it gets registered"""
+    for ep in iter_entry_points('karabo.bound_device'):
+        if ep.name == classid:
+            ep.load()
+            return
+    raise RuntimeError('Device class is not installed!')
+
+
+def _parse_config_argument(config_str):
+    """Turn a commandline device configuration into a Hash"""
+    config = Hash()
+    for param in config_str.split(' '):
+        key, value = param.split('=')
+        config.set(key, value)
+
+    return config
+
+
+def _run_device(classid, config):
+    """Run a single Bound API device in this process
+
+    NOTE: This function should not be expected to return
+    """
+    if "Logger.file.filename" not in config:
+        deviceId = config["_deviceId_"]
+        defaultLog = "device-{}.log".format(deviceId.replace(os.path.sep, "_"))
+        config["Logger.file.filename"] = defaultLog
+
+    t = threading.Thread(target=EventLoop.work)
+    t.start()
+    try:
+        device = Configurator(PythonDevice).create(classid, config)
+        device._finalizeInternalInitialization()
+    except:
+        EventLoop.stop()
+        raise
+    finally:
+        t.join()
+
+
+def main():
+    description = ('Start a Python Bound device independent of a server. '
+                   'This is NOT recommended except if you want to debug a '
+                   'device directly (via pdb or gdb)')
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument('--classid', required=True,
+                        help='A Bound API device class installed locally')
+    parser.add_argument('--config', default='',
+                        help='A list of space separated key=value pairs '
+                             'giving the device configuration.')
+    ns = parser.parse_args()
+
+    _load_entrypoint(ns.classid)
+    config = _parse_config_argument(ns.config)
+    _run_device(ns.classid, config)
+
+
+if __name__ == '__main__':
+    main()
