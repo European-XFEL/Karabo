48b78c77e84a9732c1f942c2aeddc94d4048bc22
Chen Xu <chen.xu@xfel.eu>
2017-10-25 15:08:15 +0200

    GUI: Make DisplayAlignedImage a BaseBindingController

---
+++ src/pythonGui/karabo_gui/controllers/display/tests/image.py
@@ -0,0 +1,84 @@
+from karabo.middlelayer_api.hash import ByteArray
+from karabo.middlelayer import (
+    Bool, Configurable, Hash, Int8, Node, VectorInt8, VectorInt16)
+
+
+# Reverse map of controllers.images.
+TYPENUM_MAP = {
+    'bool': 0,
+    'char': 2,
+    'int8': 4,
+    'uint8': 6,
+    'int16': 8,
+    'uint16': 10,
+    'int32': 12,
+    'uint32': 14,
+    'int64': 16,
+    'uint64': 18,
+    'float': 20,
+    'float64': 22,
+}
+
+
+ZAXIS = 2
+
+
+class Pixels(Configurable):
+    type = Int8()
+    data = ByteArray()
+
+
+class Alignment(Configurable):
+    offsets = VectorInt8()
+    rotations = VectorInt8()
+
+
+class Geometry(Configurable):
+    update = Bool(False)
+    pixelRegion = VectorInt8()
+    alignment = Node(Alignment)
+
+
+class ImageNode(Configurable):
+    pixels = Node(Pixels)
+    dims = VectorInt16()
+    stackAxis = Int8()
+    geometry = Node(Geometry)
+
+
+class OutputNodeInner(Configurable):
+    image = Node(ImageNode, displayType='Image')
+
+
+class OutputNode(Configurable):
+    data = Node(OutputNodeInner)
+
+
+class PipelineData(Configurable):
+    output = Node(OutputNode, displayType='OutputChannel')
+
+
+def _get_geometry_hash(update):
+    alignment_hash = Hash('offsets', [0., 0., 0.],
+                          'rotations', [0., 0., 0.])
+    return Hash('update', update,
+                'pixelRegion', [0, 0, 1, 1],  # x0, y0, x1, y1
+                'alignment', alignment_hash)
+
+
+def get_image_hash(val=0, dimz=False, *, rgb=False, update=True):
+    dimX = 40
+    dimY = 30
+    npix = dimX * dimY
+    dims_val = [dimY, dimX]
+    if dimz:
+        dimZ = 3 if rgb else 2
+        dims_val.append(dimZ)
+        npix *= dimZ
+    pixel_hsh = Hash('type', TYPENUM_MAP['uint8'],
+                     'data', bytearray([val for _ in range(npix)]))
+    img_hsh = Hash('pixels', pixel_hsh,
+                   'dims', dims_val,
+                   'stackAxis', ZAXIS,
+                   'geometry', _get_geometry_hash(update))
+    return Hash('image', img_hsh)
