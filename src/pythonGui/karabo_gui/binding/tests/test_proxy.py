0bc22702e378e8516014d7ead82fc2bdeeb1e7b1
John Wiggins <john.wiggins@desy.de>
2017-10-25 17:00:26 +0200

    GUI TESTING: Stop using @patch decorators
    We're using patch for singleton getters, but don't need to because
    we're already in control of the singletons. This should make it easier
    to call into multiple modules which might otherwise need to be patch()'d

--- src/pythonGui/karabo_gui/binding/tests/test_proxy.py
+++ src/pythonGui/karabo_gui/binding/tests/test_proxy.py
@@ -1,11 +1,11 @@
-from unittest.mock import patch, Mock
+from unittest.mock import Mock
 
 from karabo.common.api import DeviceStatus
+from karabo_gui.testing import assert_trait_change, singletons
 from ..api import (
     ImageBinding, DeviceProxy, DeviceClassProxy, PropertyProxy,
     apply_default_configuration, build_binding
 )
-from ..testing import assert_trait_change
 from .schema import get_pipeline_schema, get_simple_schema, get_slotted_schema
 
 
@@ -23,128 +23,121 @@ def test_device_proxy_classes():
         assert not_there is None
 
 
-@patch('karabo_gui.binding.proxy.get_topology')
-def test_device_class_proxy_status(get_topology):
+def test_device_class_proxy_status():
     topology = Mock()
     topology.get_attributes.return_value = None
-    get_topology.return_value = topology
+    with singletons(topology=topology):
+        schema = get_simple_schema()
+        binding = build_binding(schema)
+        proxy = DeviceClassProxy(server_id='swerver', binding=binding)
 
-    schema = get_simple_schema()
-    binding = build_binding(schema)
-    proxy = DeviceClassProxy(server_id='swerver', binding=binding)
+        assert proxy.status == DeviceStatus.NOSERVER
+        topology.get_attributes.assert_called_with('server.swerver')
 
-    assert proxy.status == DeviceStatus.NOSERVER
-    topology.get_attributes.assert_called_with('server.swerver')
+        topology.get_attributes.return_value = {}
+        proxy = DeviceClassProxy(binding=binding)
+        assert proxy.status == DeviceStatus.NOPLUGIN
 
-    topology.get_attributes.return_value = {}
-    proxy = DeviceClassProxy(binding=binding)
-    assert proxy.status == DeviceStatus.NOPLUGIN
+        topology.get_attributes.return_value = {'deviceClasses': ['Simple']}
+        proxy = DeviceClassProxy(binding=binding)
+        assert proxy.status == DeviceStatus.OFFLINE
 
-    topology.get_attributes.return_value = {'deviceClasses': ['Simple']}
-    proxy = DeviceClassProxy(binding=binding)
-    assert proxy.status == DeviceStatus.OFFLINE
-
-    proxy.status = DeviceStatus.REQUESTED
-    proxy.schema_update = True
-    assert proxy.status == DeviceStatus.OFFLINE
-    assert not proxy.online
+        proxy.status = DeviceStatus.REQUESTED
+        proxy.schema_update = True
+        assert proxy.status == DeviceStatus.OFFLINE
+        assert not proxy.online
 
 
-@patch('karabo_gui.binding.proxy.get_network')
-def test_device_class_proxy_refresh(get_network):
+def test_device_class_proxy_refresh():
     network = Mock()
-    get_network.return_value = network
+    with singletons(network=network):
+        schema = get_simple_schema()
+        binding = build_binding(schema)
+        proxy = DeviceClassProxy(server_id='swerver', binding=binding)
+        proxy.refresh_schema()
 
-    schema = get_simple_schema()
-    binding = build_binding(schema)
-    proxy = DeviceClassProxy(server_id='swerver', binding=binding)
-    proxy.refresh_schema()
-
-    assert proxy.status == DeviceStatus.REQUESTED
-    network.onGetClassSchema.assert_called_with('swerver', 'Simple')
+        assert proxy.status == DeviceStatus.REQUESTED
+        network.onGetClassSchema.assert_called_with('swerver', 'Simple')
 
 
-@patch('karabo_gui.binding.proxy.get_network')
-def test_device_proxy_slots(get_network):
+def test_device_proxy_slots():
     network = Mock()
-    get_network.return_value = network
+    with singletons(network=network):
+        schema = get_slotted_schema()
+        binding = build_binding(schema)
+        apply_default_configuration(binding)
 
-    schema = get_slotted_schema()
-    binding = build_binding(schema)
-    apply_default_configuration(binding)
-
-    proxy = DeviceProxy(device_id='dev', server_id='swerver', binding=binding)
-    slot_proxy = PropertyProxy(path='callme', root_proxy=proxy)
-    slot_proxy.execute()
-    network.onExecute.assert_called_with('dev', 'callme')
+        proxy = DeviceProxy(device_id='dev', server_id='swerver',
+                            binding=binding)
+        slot_proxy = PropertyProxy(path='callme', root_proxy=proxy)
+        slot_proxy.execute()
+        network.onExecute.assert_called_with('dev', 'callme')
 
 
-@patch('karabo_gui.binding.proxy.get_network')
-def test_device_proxy_status(get_network):
+def test_device_proxy_status():
     network = Mock()
-    get_network.return_value = network
+    with singletons(network=network):
+        schema = get_simple_schema()
+        binding = build_binding(schema)
+        proxy = DeviceProxy(device_id='dev', server_id='swerver',
+                            binding=binding)
 
-    schema = get_simple_schema()
-    binding = build_binding(schema)
-    proxy = DeviceProxy(device_id='dev', server_id='swerver', binding=binding)
+        assert proxy.status == DeviceStatus.OFFLINE
+        proxy.add_monitor()
+        proxy.status = DeviceStatus.ONLINE
+        network.onGetDeviceSchema.assert_called_with('dev')
 
-    assert proxy.status == DeviceStatus.OFFLINE
-    proxy.add_monitor()
-    proxy.status = DeviceStatus.ONLINE
-    network.onGetDeviceSchema.assert_called_with('dev')
+        proxy.schema_update = True
+        network.onStartMonitoringDevice.assert_called_with('dev')
+        assert proxy.status == DeviceStatus.SCHEMA
 
-    proxy.schema_update = True
-    network.onStartMonitoringDevice.assert_called_with('dev')
-    assert proxy.status == DeviceStatus.SCHEMA
+        proxy.config_update = True
+        assert proxy.status == DeviceStatus.MONITORING
 
-    proxy.config_update = True
-    assert proxy.status == DeviceStatus.MONITORING
+        proxy.remove_monitor()
+        network.onStopMonitoringDevice.assert_called_with('dev')
+        assert proxy.status == DeviceStatus.ALIVE
 
-    proxy.remove_monitor()
-    network.onStopMonitoringDevice.assert_called_with('dev')
-    assert proxy.status == DeviceStatus.ALIVE
+        proxy.schema_update = True
+        network.onGetDeviceConfiguration.assert_called_with('dev')
 
-    proxy.schema_update = True
-    network.onGetDeviceConfiguration.assert_called_with('dev')
+        proxy.add_monitor()
+        proxy.config_update = True
+        assert proxy.status == DeviceStatus.MONITORING
 
-    proxy.add_monitor()
-    proxy.config_update = True
-    assert proxy.status == DeviceStatus.MONITORING
-
-    proxy.remove_monitor()
-    proxy.status = DeviceStatus.ONLINE
-    proxy.add_monitor()
-    assert proxy.status == DeviceStatus.REQUESTED
+        proxy.remove_monitor()
+        proxy.status = DeviceStatus.ONLINE
+        proxy.add_monitor()
+        assert proxy.status == DeviceStatus.REQUESTED
 
 
-@patch('karabo_gui.binding.proxy.get_network')
-def test_device_proxy_data_feeds(get_network):
+def test_device_proxy_data_feeds():
     network = Mock()
-    get_network.return_value = network
+    with singletons(network=network):
+        schema = get_simple_schema()
+        binding = build_binding(schema)
+        proxy = DeviceProxy(device_id='dev', server_id='swerver',
+                            binding=binding)
 
-    schema = get_simple_schema()
-    binding = build_binding(schema)
-    proxy = DeviceProxy(device_id='dev', server_id='swerver', binding=binding)
+        proxy.connect_pipeline('pipe')
+        network.onSubscribeToOutput.assert_called_with('dev', 'pipe', True)
 
-    proxy.connect_pipeline('pipe')
-    network.onSubscribeToOutput.assert_called_with('dev', 'pipe', True)
+        proxy.disconnect_pipeline('pipe')
+        network.onSubscribeToOutput.assert_called_with('dev', 'pipe', False)
 
-    proxy.disconnect_pipeline('pipe')
-    network.onSubscribeToOutput.assert_called_with('dev', 'pipe', False)
+        historic_data = None
 
-    historic_data = None
+        def _event_fired(value):
+            nonlocal historic_data
+            historic_data = value
 
-    def _event_fired(value):
-        nonlocal historic_data
-        historic_data = value
+        apply_default_configuration(proxy.binding)
+        binding = proxy.get_property_binding('foo')
+        assert binding.value  # defaults to True
 
-    apply_default_configuration(proxy.binding)
-    binding = proxy.get_property_binding('foo')
-    assert binding.value  # defaults to True
-
-    binding.on_trait_change(_event_fired, 'historic_data')
-    proxy.publish_historic_data('foo', False)
-    assert historic_data is False
+        binding.on_trait_change(_event_fired, 'historic_data')
+        proxy.publish_historic_data('foo', False)
+        assert historic_data is False
 
 
 def test_property_proxy():
@@ -165,19 +158,17 @@ def test_property_proxy():
     assert other == proxy
 
 
-@patch('karabo_gui.binding.proxy.get_network')
-def test_property_proxy_history(get_network):
+def test_property_proxy_history():
     network = Mock()
-    get_network.return_value = network
+    with singletons(network=network):
+        schema = get_simple_schema()
+        binding = build_binding(schema)
+        root_proxy = DeviceProxy(device_id='dev', binding=binding)
+        proxy = PropertyProxy(root_proxy=root_proxy, path='foo')
 
-    schema = get_simple_schema()
-    binding = build_binding(schema)
-    root_proxy = DeviceProxy(device_id='dev', binding=binding)
-    proxy = PropertyProxy(root_proxy=root_proxy, path='foo')
-
-    proxy.get_history('start_time', 'end_time', max_value_count=1)
-    network.onGetPropertyHistory.assert_called_with(
-        'dev:foo', 'start_time', 'end_time', 1)
+        proxy.get_history('start_time', 'end_time', max_value_count=1)
+        network.onGetPropertyHistory.assert_called_with(
+            'dev:foo', 'start_time', 'end_time', 1)
 
 
 def test_property_proxy_pipeline():
@@ -190,21 +181,19 @@ def test_property_proxy_pipeline():
     assert proxy.pipeline_parent_path == 'output'
 
 
-@patch('karabo_gui.binding.proxy.get_network')
-def test_property_proxy_pipeline_monitoring(get_network):
+def test_property_proxy_pipeline_monitoring():
     network = Mock()
-    get_network.return_value = network
+    with singletons(network=network):
+        schema = get_pipeline_schema()
+        binding = build_binding(schema)
+        root_proxy = DeviceProxy(device_id='dev', binding=binding)
+        proxy = PropertyProxy(root_proxy=root_proxy, path='output.data.image')
 
-    schema = get_pipeline_schema()
-    binding = build_binding(schema)
-    root_proxy = DeviceProxy(device_id='dev', binding=binding)
-    proxy = PropertyProxy(root_proxy=root_proxy, path='output.data.image')
+        proxy.start_monitoring()
+        network.onSubscribeToOutput.assert_called_with('dev', 'output', True)
 
-    proxy.start_monitoring()
-    network.onSubscribeToOutput.assert_called_with('dev', 'output', True)
-
-    proxy.stop_monitoring()
-    network.onSubscribeToOutput.assert_called_with('dev', 'output', False)
+        proxy.stop_monitoring()
+        network.onSubscribeToOutput.assert_called_with('dev', 'output', False)
 
 
 def test_schema_updates():
