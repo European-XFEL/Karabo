8091e1349b95bf5ee33935b6cb16427214f5ccd1
John Wiggins <john.wiggins@desy.de>
2017-10-25 17:00:26 +0200

    GUI TESTING: Stop using @patch decorators
    We're using patch for singleton getters, but don't need to because
    we're already in control of the singletons. This should make it easier
    to call into multiple modules which might otherwise need to be patch()'d

--- src/pythonGui/karabo_gui/topology/tests/test_system.py
+++ src/pythonGui/karabo_gui/topology/tests/test_system.py
@@ -1,9 +1,9 @@
-from unittest.mock import patch, Mock
+from unittest.mock import Mock
 
 from traits.api import push_exception_handler, pop_exception_handler
 
 from karabo.common.api import DeviceStatus
-from karabo_gui.testing import GuiTestCase
+from karabo_gui.testing import GuiTestCase, singletons
 from karabo_gui.topology.system import SystemTopology
 from .utils import system_hash
 
@@ -18,54 +18,47 @@ def tearDown():
 
 class TestSystemTopology(GuiTestCase):
 
-    @patch('karabo_gui.topology.system.get_network')
-    def test_get_class_simple(self, get_network):
+    def test_get_class_simple(self):
         network = Mock()
-        get_network.return_value = network
+        with singletons(network=network):
+            topology = SystemTopology()
 
-        topology = SystemTopology()
+            klass = topology.get_class('server_id', 'class_id')
 
-        klass = topology.get_class('server_id', 'class_id')
+            assert klass.status is DeviceStatus.REQUESTED
+            network.onGetClassSchema.assert_called_with(
+                'server_id', 'class_id')
 
-        assert klass.status is DeviceStatus.REQUESTED
-        network.onGetClassSchema.assert_called_with('server_id', 'class_id')
-
-    @patch('karabo_gui.topology.system.get_network')
-    @patch('karabo_gui.configuration.get_topology')
-    def test_get_device_simple(self, get_topology, get_network):
+    def test_get_device_simple(self):
         network = Mock()
-        get_network.return_value = network
-
         topology = SystemTopology()
         topology.update(system_hash())
-        get_topology.return_value = topology
+        with singletons(network=network, topology=topology):
+            klass = topology.get_device('divvy')
 
-        klass = topology.get_device('divvy')
+            assert klass.status is DeviceStatus.REQUESTED
+            network.onGetDeviceSchema.assert_called_with('divvy')
 
-        assert klass.status is DeviceStatus.REQUESTED
-        network.onGetDeviceSchema.assert_called_with('divvy')
-
-    @patch('karabo_gui.topology.system.get_network')
-    @patch('karabo_gui.topology.project_device.get_topology')
-    def test_get_project_device_simple(self, get_topology, get_network):
+    def test_get_project_device_simple(self):
         network = Mock()
-        get_network.return_value = network
-
         topology = SystemTopology()
         topology.update(system_hash())
-        get_topology.return_value = topology
+        with singletons(network=network, topology=topology):
+            device_id = 'divvy'
+            server_id = 'swerver'
+            class_id = 'FooClass'
+            device = topology.get_project_device(device_id, class_id=class_id,
+                                                 server_id=server_id)
 
-        device_id = 'divvy'
-        server_id = 'swerver'
-        class_id = 'FooClass'
-        device = topology.get_project_device(device_id, class_id=class_id,
-                                             server_id=server_id)
+            assert device.online
+            assert device.status is DeviceStatus.REQUESTED
 
-        assert not device.online
-        assert device.status is DeviceStatus.OFFLINE
-        assert device._online_dev_config is topology.get_device(device_id)
-        assert device._class_config is topology.get_class(server_id, class_id)
-        network.onGetClassSchema.assert_called_with(server_id, class_id)
+            topo_class = topology.get_class(server_id, class_id)
+            topo_dev = topology.get_device(device_id)
+            assert device._class_config is topo_class
+            assert device._online_dev_config is topo_dev
 
-        # Exercise the cleanup code
-        device.destroy()
+            network.onGetClassSchema.assert_called_with(server_id, class_id)
+
+            # Exercise the cleanup code
+            device.destroy()
