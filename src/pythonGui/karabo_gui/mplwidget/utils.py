39312e0a4a21baea3a075a65b01ed61665af662a
Chen Xu <chen.xu@desy.de>
2017-11-01 14:36:43 +0100

    GUI: MPL refactor

--- src/pythonGui/karabo_gui/mplwidget/utils.py
+++ src/pythonGui/karabo_gui/mplwidget/utils.py
@@ -1,5 +1,10 @@
 from functools import partial
 
+from cycler import cycler
+from matplotlib.lines import Line2D
+
+from . import const
+
 # register_shortcut will populate this dictionary
 _SHORTCUTS = {}  # {(key, classname): (funcname, funcdoc)}
 
@@ -21,3 +26,29 @@ def register_shortcut(func=None, *, key=''):
     doc = func.__doc__ if func.__doc__ else funcname
     _SHORTCUTS[(key, klass)] = (funcname, doc)
     return func
+
+
+def auto_line2D():
+    """Return a dictionary, every new key gets an empty curve with different
+    plot style. The iterator is infinite, after exhausts all styles starts
+    from the beginning.
+    """
+    colors = cycler(color=const.COLORS)
+    line_styles = cycler(linestyle=const.LINE_STYLES)
+
+    # this order means cycle over color first
+    styles = line_styles * colors
+
+    style_iter = styles()  # this an infinite iterator
+
+    def new_line2D():
+        while True:
+            yield Line2D([], [], picker=const.PICK_RADIUS,
+                         lw=const.LINE_WIDTH,
+                         # invisible marker but can be picked
+                         marker='.', markersize=const.LINE_WIDTH,
+                         markeredgecolor='red',
+                         markeredgewidth=const.MARKER_EDGE_WIDTH,
+                         **(next(style_iter)))
+
+    return new_line2D()
