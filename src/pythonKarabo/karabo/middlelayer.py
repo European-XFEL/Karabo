2cbeb4225c957cef51d6c911a9493f7468c31ac5
Dennis Goeries <dennis.goeries@xfel.eu>
2017-11-03 13:55:00 +0100

    MDL: Process Events wrapper

--- src/pythonKarabo/karabo/middlelayer.py
+++ src/pythonKarabo/karabo/middlelayer.py
@@ -64,8 +64,8 @@ from .middlelayer_api.serializers import (
 )
 from .middlelayer_api.signalslot import Signal, SignalSlotable, slot, coslot
 from .middlelayer_api.synchronization import (
-    allCompleted, background, firstCompleted, firstException, gather, sleep,
-    synchronous
+    allCompleted, background, firstCompleted, firstException, gather,
+    processEvents, sleep, synchronous
 )
 from .middlelayer_api.timestamp import Timestamp
 from .middlelayer_api import numeric
