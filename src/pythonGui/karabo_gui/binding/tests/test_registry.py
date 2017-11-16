5b4526e2e0cdbe0fa9c4162e809d306fd5de2ee5
John Wiggins <john.wiggins@desy.de>
2017-11-16 17:42:53 +0100

    Make register_binding_controller a little more flexible

--- src/pythonGui/karabo_gui/binding/tests/test_registry.py
+++ src/pythonGui/karabo_gui/binding/tests/test_registry.py
@@ -1,45 +1,78 @@
+import contextlib
 
-from karabo.middlelayer import Configurable, String, AccessMode
+from karabo.middlelayer import (
+    Configurable, Int8, Int16, UInt32, UInt64, String)
 from ..api import (
-    BaseBindingController, StringBinding, build_binding,
+    BaseBindingController, StringBinding, IntBinding, build_binding,
     register_binding_controller, get_compatible_controllers
 )
-from .. import registry as registrymod
 
 
 class SomeObject(Configurable):
-    for_display = String(accessMode=AccessMode.READONLY,
-                         options=['foo', 'bar', 'baz'])
-    unsupported = String(accessMode=AccessMode.READONLY)
-    for_editing = String(accessMode=AccessMode.RECONFIGURABLE)
+    for_display = String(options=['foo', 'bar', 'baz'])
+    for_editing = String()
+    unsupported = Int8()
 
 
-def options_checker(binding):
+class Integers(Configurable):
+    int8 = Int8()
+    int16 = Int16()
+    uint32 = UInt32()
+    uint64 = UInt64()
+
+
+def _options_checker(binding):
     return len(binding.options) > 0
 
 
-def test_registry():
-    # XXX: Flush the registry and populate it with known entries
+@contextlib.contextmanager
+def _flushed_registry():
+    """Avoid polluting the global registry with test controller classes"""
+    from .. import registry as registrymod
+
+    saved_registry = registrymod._controller_registry.copy()
     registrymod._controller_registry.clear()
+    yield
+    registrymod._controller_registry = saved_registry
 
-    @register_binding_controller(binding_type=StringBinding, read_only=True,
-                                 is_compatible=options_checker)
-    class DisplayWidget(BaseBindingController):
-        pass
 
-    @register_binding_controller(binding_type=StringBinding, read_only=False)
-    class EditWidget(BaseBindingController):
-        pass
+def test_registry():
+    with _flushed_registry():
+        @register_binding_controller(binding_type=StringBinding,
+                                     is_compatible=_options_checker)
+        class DisplayWidget(BaseBindingController):
+            pass
 
-    schema = SomeObject.getClassSchema()
-    binding = build_binding(schema)
+        @register_binding_controller(binding_type=StringBinding)
+        class EditWidget(BaseBindingController):
+            pass
 
-    for_display_widgets = get_compatible_controllers(binding.value.for_display)
-    assert DisplayWidget in for_display_widgets
-    assert EditWidget not in for_display_widgets
+        schema = SomeObject.getClassSchema()
+        binding = build_binding(schema)
 
-    for_editing_widgets = get_compatible_controllers(binding.value.for_editing)
-    assert EditWidget in for_editing_widgets
-    assert DisplayWidget not in for_editing_widgets
+        bind = binding.value.for_display
+        for_display_widgets = get_compatible_controllers(bind)
+        assert DisplayWidget in for_display_widgets
+        assert EditWidget in for_display_widgets
 
-    assert get_compatible_controllers(binding.value.unsupported) == []
+        bind = binding.value.for_editing
+        for_editing_widgets = get_compatible_controllers(bind)
+        assert EditWidget in for_editing_widgets
+        assert DisplayWidget not in for_editing_widgets
+
+        bind = binding.value.unsupported
+        assert get_compatible_controllers(bind) == []
+
+
+def test_expanded_registry():
+    with _flushed_registry():
+        @register_binding_controller(binding_type=IntBinding)
+        class DisplayWidget(BaseBindingController):
+            pass
+
+        schema = Integers.getClassSchema()
+        binding = build_binding(schema)
+
+        for name in ('int8', 'int16', 'uint32', 'uint64'):
+            bind = getattr(binding.value, name)
+            assert DisplayWidget in get_compatible_controllers(bind)
