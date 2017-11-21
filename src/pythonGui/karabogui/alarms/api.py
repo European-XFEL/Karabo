f880041f16a4e179a3d79287307997f2eb185c81
John Wiggins <john.wiggins@desy.de>
2017-11-21 15:02:48 +0100

    Move the binding and topology to the new package

--- src/pythonGui/karabogui/alarms/api.py
+++ src/pythonGui/karabogui/alarms/api.py
@@ -10,15 +10,3 @@ from .const import (ACKNOWLEDGE, ACKNOWLEGDABLE_UPDATE_TYPE, ADD_ALARM_TYPES,
 from .info import AlarmInfo
 from .model import AlarmModel, get_alarm_key_index
 from .utils import extract_alarms_data
-
-# Make pyflakes happy
-__pyflakes = [
-    ACKNOWLEDGE, ACKNOWLEGDABLE_UPDATE_TYPE, ADD_ALARM_TYPES, ADD_UPDATE_TYPE,
-    ALARM_COLOR, ALARM_DATA, ALARM_HIGH, ALARM_ID, ALARM_LOW, ALARM_TYPE,
-    DEVICE_ID, DEVICE_KILLED_UPDATE_TYPE, INIT_UPDATE_TYPE, NORM_COLOR,
-    PROPERTY, REFUSE_ACKNOWLEDGEMENT_UPDATE_TYPE, REMOVE_ALARM_TYPES,
-    REMOVE_UPDATE_TYPE, SHOW_DEVICE, UPDATE_ALARM_TYPES, UPDATE_UPDATE_TYPE,
-    WARN_COLOR, WARN_GLOBAL, WARN_HIGH, WARN_LOW,
-    AlarmEntry, AlarmInfo, AlarmModel,
-    get_alarm_icon, get_alarm_pixmap, get_alarm_key_index, extract_alarms_data
-]
---
+++ src/pythonGui/karabogui/binding/recursive.py
@@ -0,0 +1,116 @@
+from traits.api import (
+    Event, Instance, List, Property, Str, Trait, TraitHandler)
+
+from karabo.middlelayer import Hash
+from .types import BaseBinding, BindingNamespace, BindingRoot, NodeBinding
+from .util import fast_deepcopy
+
+
+class ChoiceOfNodesBinding(BaseBinding):
+    """A node whose type can be changed by assigning a new classname to its
+    value. Valid classnames are stored in the `choices` namespace. When reading
+    or traversing this node, it appears to be a normal node whose value is a
+    `BindingNamespace`.
+    """
+    value = Property
+    # Backing trait for `value`
+    choice = Str(transient=True)
+    # Namespace of possible values
+    choices = Instance(BindingNamespace, kw={'item_type': BindingRoot},
+                       transient=True)
+    # An event which fires when subnodes/leaves are updated
+    config_update = Event
+
+    def _choice_default(self):
+        return next(iter(self.choices))
+
+    def _get_value(self, value):
+        choice = self.choice
+        return getattr(self.choices, choice).value
+
+    def _set_value(self, value):
+        assert isinstance(value, str) and value in self.choices
+        self.choice = value
+
+
+class _ListOfNodesHandler(TraitHandler):
+    """Handle ListOfNodes value assignments
+    """
+    def validate(self, instance, name, value):
+        from .config import apply_configuration, apply_default_configuration
+
+        def _get_dupe_binding(klassname):
+            binding = getattr(instance.choices, klassname, None)
+            if binding is None:
+                self.error(instance, name, value)
+            return duplicate_binding(binding)
+
+        if isinstance(value, str):
+            duplicate = _get_dupe_binding(value)
+            apply_default_configuration(duplicate)
+            return duplicate
+        elif isinstance(value, Hash):
+            klassname = next(iter(value))
+            duplicate = _get_dupe_binding(klassname)
+            apply_configuration(value[klassname], duplicate)
+            return duplicate
+        elif isinstance(value, BindingRoot):
+            if value.class_id in instance.choices:
+                return value
+        self.error(instance, name, value)
+
+
+class ListOfNodesBinding(BaseBinding):
+    """A node whose values is a list of objects (nodes). The types of the
+    objects must be present in the `choices` namespace.
+
+    Either string values or object values can be written to the binding value,
+    but only object values will be stored (as BindingRoot instances). When a
+    string is written, it will be interpreted as a classname in the `choices`
+    namespace and an instance with a default configuration will be produced.
+    When an object is written, either as a Hash or as a BindingRoot, its value
+    will be validated against its corresponding class in `choices`.
+    """
+    value = List(Trait(_ListOfNodesHandler()))
+    # Namespace of possible list values
+    choices = Instance(BindingNamespace, kw={'item_type': BindingRoot},
+                       transient=True)
+
+
+# =============================================================================
+# Helper functions
+#
+
+def duplicate_binding(binding):
+    """Given a `BindingRoot` object, create a new object which is a copy of
+    the binding, but does not contain the source binding's value.
+    """
+    assert isinstance(binding, BindingRoot)
+    duplicate = BindingRoot(class_id=binding.class_id)
+    for name in binding.value:
+        node = _duplicate_node(getattr(binding.value, name))
+        setattr(duplicate.value, name, node)
+
+    return duplicate
+
+
+def _duplicate_node(node):
+    """Duplicate a `BaseBinding` object
+    """
+    traits = {'attributes': fast_deepcopy(node.attributes)}
+    if isinstance(node, (ChoiceOfNodesBinding, ListOfNodesBinding)):
+        choices = node.choices
+        traits['choices'] = ns = BindingNamespace(item_type=BindingRoot)
+        for name in choices:
+            setattr(ns, name, duplicate_binding(getattr(choices, name)))
+
+    klass = node.__class__
+    dupe = klass(**traits)
+    if isinstance(node, NodeBinding):
+        dupe.value = BindingNamespace(item_type=BaseBinding)
+        for name in node.value:
+            subnode = _duplicate_node(getattr(node.value, name))
+            setattr(dupe.value, name, subnode)
+        dupe.modified = False
+
+    return dupe
