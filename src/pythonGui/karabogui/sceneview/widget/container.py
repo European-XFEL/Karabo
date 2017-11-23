57a64d364176ffc5d240e6fdb8ddf4332fd0b1a1
John Wiggins <john.wiggins@desy.de>
2017-11-23 15:44:41 +0100

    Port BaseWidgetContainer to a binding world
    It's name is now ControllerContainer.

---
+++ src/pythonGui/karabogui/sceneview/widget/container.py
@@ -0,0 +1,245 @@
+from PyQt4.QtCore import QRect, Qt
+from PyQt4.QtGui import QHBoxLayout, QLabel, QStackedLayout, QWidget
+
+from karabo.common.api import AlarmCondition, DeviceStatus, State
+from karabogui.alarms.api import get_alarm_pixmap
+from karabogui.binding.api import extract_sparse_configurations
+from karabogui.indicators import get_device_status_pixmap, STATE_COLORS
+from karabogui.singletons.api import get_network
+from karabogui.util import generateObjectName
+from .utils import get_proxy
+
+
+class ControllerContainer(QWidget):
+    """A container for scene widgets which provides the interface needed
+    by the scene tools.
+    """
+    def __init__(self, klass, model, parent):
+        super(ControllerContainer, self).__init__(parent)
+        self.model = model
+        # Variables used by editable widgets
+        self._is_editable = False
+        self._style_sheet = ''
+
+        self.alarm_symbol = QLabel('', self)
+        self.status_symbol = QLabel('', self)
+        self.status_symbol.setAttribute(Qt.WA_TransparentForMouseEvents)
+
+        self.layout = QStackedLayout(self)
+        self.layout.setStackingMode(QStackedLayout.StackAll)
+        self.layout.addWidget(self.status_symbol)
+
+        proxies = [get_proxy(*key.split('.', 1)) for key in self.model.keys]
+        self.widget_controller = self._create_controller(klass, proxies)
+        self._setup_wrapped_widget()
+
+        self.setGeometry(QRect(model.x, model.y, model.width, model.height))
+        self.setToolTip(', '.join(self.model.keys))
+        self.update_alarm_symbol()
+
+    def add_proxies(self, proxies):
+        """Add more proxies to a controller which allows more than one proxy.
+        ``True`` is returned when this was possible, otherwise ``False`` is
+        returned.
+        """
+        if self.model.parent_component != 'DisplayComponent':
+            return False
+
+        controller = self.widget_controller
+        proxies_added = []
+        for proxy in proxies:
+            if controller.visualize_additional_property(proxy):
+                proxies_added.append(proxy)
+        return len(proxies_added) == len(proxies)
+
+    def apply_changes(self, devices=None, send_immediately=False):
+        """Apply user-entered values to the remote device properties
+
+        Returns a dictionary of device -> configuration pairs for the proxies
+        in this widget.
+        """
+        if not (self._is_editable and self.widget_controller.proxy.visible):
+            return {}
+
+        configs = extract_sparse_configurations(self.widget_controller.proxies,
+                                                devices=devices)
+
+        if not send_immediately:
+            return configs
+
+        # Otherwise we were probably called from keyPressEvent and no other
+        # widgets need to have their configuration collected.
+        network = get_network()
+        for dev, config in configs.items():
+            network.onReconfigure(dev, config)
+        return {}
+
+    def decline_changes(self):
+        """Undo any user-entered changes
+        """
+        if not (self._is_editable and self.widget_controller.proxy.visible):
+            return
+
+        for proxy in self.widget_controller.proxies:
+            # FIXME: Consider doing this in the property proxy
+            value = proxy.get_device_value()
+            proxy.value = value
+            proxy.binding.modified = False
+
+        self._update_background_color()
+
+    def destroy(self):
+        """Tell the controller to clean up
+        """
+        device_proxy = self.widget_controller.proxy.root_proxy
+        device_proxy.on_trait_change(self._device_status_changed, 'status',
+                                     remove=True)
+
+        self.widget_controller.destroy()
+
+    def set_geometry(self, rect):
+        self.model.set(x=rect.x(), y=rect.y(),
+                       width=rect.width(), height=rect.height())
+        self.setGeometry(rect)
+
+    def set_visible(self, visible):
+        """Set whether this widget is seen by the user."""
+        if visible:
+            self.widget_controller.show()
+        else:
+            self.widget_controller.hide()
+
+    def translate(self, offset):
+        new_pos = self.pos() + offset
+        self.model.set(x=new_pos.x(), y=new_pos.y())
+        self.move(new_pos)
+
+    def update_alarm_symbol(self):
+        """Update the alarm symbol with a pixmap matching the given
+        ``alarm_type``
+        """
+        widget_alarms = []
+        for p in self.widget_controller.proxies:
+            system_topo_node = p.root_proxy.topology_node
+            if system_topo_node is None:
+                continue
+
+            alarm_dict = system_topo_node.alarm_info.alarm_dict
+            property_name = p.path
+            for alarm_type, properties in alarm_dict.items():
+                # feature: show global alarm_type for 'state' property
+                if property_name == 'state' or property_name in properties:
+                    widget_alarms.append(AlarmCondition.fromString(alarm_type))
+
+        pixmap = None
+        if widget_alarms:
+            widget_alarms.sort()  # AlarmCondition is sortable
+            pixmap = get_alarm_pixmap(widget_alarms[-1].asString())
+
+        if pixmap is not None:
+            self.alarm_symbol.setPixmap(pixmap)
+            self.alarm_symbol.show()
+        else:
+            self.alarm_symbol.hide()
+
+    def update_global_access_level(self):
+        """Update the widget based on a new global access level.
+        """
+        # FIXME: Figure this out. We don't get a traits notification for
+        # access level changes
+        self.widget_controller
+
+    # ---------------------------------------------------------------------
+    # Qt methods
+
+    def keyPressEvent(self, event):
+        """Watch for editing events involving the enter and escape keys
+        """
+        key_code = event.key()
+        if key_code == Qt.Key_Escape:
+            self.decline_changes()
+        elif key_code in (Qt.Key_Enter, Qt.Key_Return):
+            self.apply_changes(send_immediately=True)
+        super(ControllerContainer, self).keyPressEvent(event)
+
+    # ---------------------------------------------------------------------
+    # Internal methods
+
+    def _add_alarm_symbol(self, layout):
+        """Add alarm symbol to the given ``layout`` and return the widget this
+        layout then belongs to
+        """
+        layout.addWidget(self.alarm_symbol)
+        layout_widget = QWidget()
+        layout_widget.setLayout(layout)
+        objectName = generateObjectName(self)
+        layout_widget.setObjectName(objectName)
+        self._style_sheet = ('QWidget#{}'.format(objectName) +
+                             ' {{ background-color : rgba{}; }}')
+        return layout_widget
+
+    def _create_controller(self, klass, proxies):
+        """Create the binding controller instance.
+        """
+        controller = klass(proxy=proxies[0], model=self.model)
+        controller.create(self)
+        for proxy in proxies[1:]:
+            # XXX: Assert that this returns True??
+            controller.visualize_additional_property(proxy)
+
+        # Attach a handler for the 'status' trait of our main device
+        device_proxy = proxies[0].root_proxy
+        device_proxy.on_trait_change(self._device_status_changed, 'status')
+
+        return controller
+
+    def _device_status_changed(self, status):
+        """Traits notification callback when the status of the device changes.
+        """
+        error = (status is DeviceStatus.ERROR)
+        pixmap = get_device_status_pixmap(status, error)
+        if pixmap is not None:
+            self.status_symbol.setPixmap(pixmap)
+            self.status_symbol.show()
+        else:
+            self.status_symbol.hide()
+
+    def _setup_wrapped_widget(self):
+        """Wrap up the alarm symbol and possible edit buttons in a layout with
+        the `widget_controller`.
+        """
+        layout = QHBoxLayout()
+        layout.addWidget(self.widget_controller.widget)
+        disp_widgets = self._add_alarm_symbol(layout)
+        self.layout.addWidget(disp_widgets)
+
+        if self.model.parent_component == 'EditableApplyLaterComponent':
+            self._is_editable = True
+            layout.setContentsMargins(2, 2, 2, 2)
+        else:
+            layout.setContentsMargins(0, 0, 0, 0)
+
+    # ---------------------------------------------------------------------
+    # Editing related code
+
+    def _on_user_edit(self):
+        self._update_background_color()
+
+    def _update_background_color(self):
+        proxy = self.widget_controller.proxy
+        if not self._is_editable or proxy.binding is None:
+            return
+
+        if proxy.binding.modified:
+            device_value = proxy.get_device_value()
+            # FIXME: We need to find a way to determine when a property with
+            # local modifications has a newer remote value which is different
+            # than that entered by the user.
+            if device_value is not None:
+                color = STATE_COLORS[State.UNKNOWN] + (128,)
+            else:
+                color = STATE_COLORS[State.CHANGING] + (128,)
+            formatted_sheet = self._style_sheet.format(color)
+            self.setStyleSheet(formatted_sheet)
+        else:
+            self.setStyleSheet('')
