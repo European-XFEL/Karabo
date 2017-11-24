17ee5a9e7ec33bf1e9dcaefbf50c23cbef4893e7
John Wiggins <john.wiggins@desy.de>
2017-11-24 15:13:15 +0100

    Copy non-Box related scene code to new package
    This will cut down on review noise in future MRs

---
+++ src/pythonGui/karabogui/sceneview/const.py
@@ -0,0 +1,57 @@
+#############################################################################
+# Author: <kerstin.weger@xfel.eu>
+# Created on June 7, 2016
+# Copyright (C) European XFEL GmbH Hamburg. All rights reserved.
+#############################################################################
+from PyQt4.QtCore import Qt
+from PyQt4.QtGui import QBoxLayout
+
+# A handy limit for things measured in pixels
+SCREEN_MAX_VALUE = 100000
+
+SCENE_BORDER_WIDTH = 10
+
+# For convenience association of strings to Qt cursors
+QT_CURSORS = {
+    'arrow': Qt.ArrowCursor,
+    'blank': Qt.BlankCursor,
+    'busy': Qt.BusyCursor,
+    'cross': Qt.CrossCursor,
+    'closed-hand': Qt.ClosedHandCursor,
+    'open-hand': Qt.OpenHandCursor,
+    'pointing-hand': Qt.PointingHandCursor,
+    'resize-diagonal-trbl': Qt.SizeBDiagCursor,
+    'resize-diagonal-tlbr': Qt.SizeFDiagCursor,
+    'resize-horizontal': Qt.SizeHorCursor,
+    'resize-vertical': Qt.SizeVerCursor,
+}
+
+# For convenience association of strings to Qt enums
+QT_PEN_CAP_STYLE_FROM_STR = {
+    'butt': Qt.FlatCap,
+    'square': Qt.SquareCap,
+    'round': Qt.RoundCap,
+}
+
+# For convenience association reverse mapping of Qt enums to string
+QT_PEN_CAP_STYLE_TO_STR = {v: k for k, v in QT_PEN_CAP_STYLE_FROM_STR.items()}
+
+# For convenience association of strings to Qt enums
+QT_PEN_JOIN_STYLE_FROM_STR = {
+    'miter': Qt.MiterJoin,
+    'svgmiter': Qt.SvgMiterJoin,
+    'round': Qt.RoundJoin,
+    'bevel': Qt.BevelJoin,
+}
+
+# For convenience association reverse mapping of Qt enums to string
+QT_PEN_JOIN_STYLE_TO_STR = {v: k
+                            for k, v in QT_PEN_JOIN_STYLE_FROM_STR.items()}
+
+# For convenience association of ints to Qt enums
+QT_BOX_LAYOUT_DIRECTION = (
+    QBoxLayout.LeftToRight,
+    QBoxLayout.RightToLeft,
+    QBoxLayout.TopToBottom,
+    QBoxLayout.BottomToTop
+)
---
+++ src/pythonGui/karabogui/sceneview/tools/api.py
@@ -0,0 +1,11 @@
+# flake8: noqa
+from .actions import (BoxVSceneAction, BoxHSceneAction, CreateToolAction,
+                      GroupEntireSceneAction, GridSceneAction,
+                      GroupSceneAction, SceneBringToFrontAction,
+                      SceneSendToBackAction, UngroupSceneAction)
+from .clipboard import (SceneCopyAction, SceneCutAction, SceneDeleteAction,
+                        ScenePasteAction, ScenePasteReplaceAction,
+                        SceneSelectAllAction)
+from .drawing import (LineSceneTool, RectangleSceneTool, SceneLinkTool,
+                      TextSceneTool)
+from .selection import BoxSelectionTool, SceneSelectionTool
