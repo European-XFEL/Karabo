e5fc398a42807d89246bb3777f1a8aafe8716365
Chen Xu <chen.xu@desy.de>
2017-11-28 14:16:30 +0100

    GUI: Introducing project device in the topology package to binding world

--- src/pythonGui/karabogui/topology/project_device.py
+++ src/pythonGui/karabogui/topology/project_device.py
@@ -4,12 +4,14 @@
 # Copyright (C) European XFEL GmbH Hamburg. All rights reserved.
 #############################################################################
 
-from traits.api import HasStrictTraits, Instance, Property, String, WeakRef
+from traits.api import (
+    Bool, DelegatesTo, HasStrictTraits, Instance, on_trait_change, Property,
+    String, WeakRef)
 
 from karabo.middlelayer import Hash
 from karabogui.binding.api import (
-    BaseDeviceProxy, BindingRoot, DeviceProxy, DeviceClassProxy,
-    apply_configuration, apply_default_configuration, build_binding
+    BaseDeviceProxy, DeviceProxy, ProjectDeviceProxy,
+    apply_configuration, apply_default_configuration, extract_configuration
 )
 from karabogui.singletons.api import get_topology
 from .tree import SystemTreeNode
@@ -25,36 +27,49 @@ class ProjectDeviceInstance(HasStrictTraits):
     device_id = String
     class_id = String
     server_id = String
-    # The proxy for this device
-    proxy = Property(Instance(BaseDeviceProxy),
-                     depends_on='_online_proxy.online')
 
     # A weak reference to the system topology device node
     device_node = WeakRef(SystemTreeNode, allow_none=True)
 
+    # The proxy for this device
+    proxy = Property(Instance(BaseDeviceProxy),
+                     depends_on='_online_proxy.online')
+    online = DelegatesTo('_online_proxy')
+
     # The online/offline proxies and configurations
     _online_proxy = Instance(DeviceProxy)
-    _offline_proxy = Instance(DeviceClassProxy)
-    _offline_config = Instance(Hash)
+    _offline_proxy = Instance(ProjectDeviceProxy)
 
-    def __init__(self, device_id, class_id, server_id):
+    # Internal storage for applying offline configuration
+    _offline_config = Instance(Hash)
+    _deferred_update = Bool(False)
+
+    def __init__(self, device_id, server_id, class_id):
         super(ProjectDeviceInstance, self).__init__()
 
-        self._init_object_state(device_id, class_id, server_id)
+        self._init_object_state(device_id, server_id, class_id)
 
-    def rename(self, device_id='', class_id='', server_id=''):
-        """Assign a new device_id, class_id, or server_id.
+    def get_current_config_hash(self):
+        """Extract a complete device config hash from the proxy
+        """
+        if len(self.proxy.binding.value) > 0:
+            return extract_configuration(self.proxy.binding,
+                                         modified_only=False)
+        return Hash()
+
+    def rename(self, device_id='', server_id='', class_id=''):
+        """Assign a new device_id, server_id, class_id.
         """
         device_id = device_id or self.device_id
-        class_id = class_id or self.class_id
         server_id = server_id or self.server_id
+        class_id = class_id or self.class_id
 
         # First check to see if anything is changing!
         if (device_id == self.device_id and class_id == self.class_id
                 and server_id == self.server_id):
             return
 
-        self._init_object_state(device_id, class_id, server_id)
+        self._init_object_state(device_id, server_id, class_id)
 
     def set_project_config_hash(self, config):
         """Forcibly set the offline configuration Hash of the device.
@@ -62,9 +77,13 @@ class ProjectDeviceInstance(HasStrictTraits):
         if config is None:
             return
 
-        apply_default_configuration(self._offline_proxy.binding)
-        apply_configuration(config, self._offline_proxy.binding)
-        self._offline_config = config
+        # Make sure the offline proxy has schema first
+        if len(self._offline_proxy.binding.value) > 0:
+            apply_default_configuration(self._offline_proxy.binding)
+            apply_configuration(config, self._offline_proxy.binding)
+        else:
+            self._deferred_update = True
+            self._offline_config = config
 
     # ---------------------------------------------------------------------
     # Traits Handlers
@@ -74,22 +93,33 @@ class ProjectDeviceInstance(HasStrictTraits):
             return self._online_proxy
         return self._offline_proxy
 
+    @on_trait_change('_offline_proxy:schema_update')
+    def _apply_offline_config(self):
+        """Triggered when offline proxy schema arrrived
+        """
+        if self._deferred_update:
+            apply_default_configuration(self._offline_proxy.binding)
+            apply_configuration(self._offline_config,
+                                self._offline_proxy.binding)
+            self._deferred_update = False
+
     # ---------------------------------------------------------------------
     # utils
 
-    def _init_object_state(self, device_id, class_id, server_id):
+    def _init_object_state(self, device_id, server_id, class_id):
         """Initialize the object for a given device_id/server_id/class_id.
         """
         topology = get_topology()
+
+        self._deferred_update = False
+        if self._offline_proxy is not None:
+            topology.remove_project_device_proxy(
+                 self.device_id, self.server_id, self.class_id
+            )
+
         self._online_proxy = topology.get_device(device_id)
-        schema = topology.get_schema(server_id, class_id)
-        binding = (BindingRoot(class_id=class_id)
-                   if schema is None else build_binding(schema))
-        self._offline_proxy = DeviceClassProxy(server_id=server_id,
-                                               binding=binding)
-        if self._offline_config is not None and schema is not None:
-            apply_configuration(self._offline_config,
-                                self._offline_proxy.binding)
+        self._offline_proxy = topology.get_project_device_proxy(
+            device_id, server_id, class_id)
 
         # Remember the IDs (also notifies the outside world of changes)
         self.device_id = device_id
