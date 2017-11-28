39db1714566c5f3de70096ac0ba5ecb51abff085
Gero Flucke <gero.flucke@xfel.eu>
2017-11-28 15:41:34 +0100

    More tests on ImageData.

--- src/karabo/tests/xms/ImageData_Test.cc
+++ src/karabo/tests/xms/ImageData_Test.cc
@@ -36,7 +36,7 @@ void ImageData_Test::tearDown() {
 
 void ImageData_Test::testConstructor() {
 
-    NDArray arr(Dims(200, 100), 2);
+    NDArray arr(Dims(200, 100), 2); // filled with 2s (which are int and thus ReferenceType INT32)
     {
         ImageData image(arr);
 
@@ -44,6 +44,7 @@ void ImageData_Test::testConstructor() {
         Dims imageOffsets(image.getROIOffsets());
 
         CPPUNIT_ASSERT(image.getData().size() == 100 * 200);
+        CPPUNIT_ASSERT_EQUAL(2, image.getData().getData<int>()[0]);
         CPPUNIT_ASSERT(image.getEncoding() == Encoding::GRAY);
         CPPUNIT_ASSERT(image.isIndexable());
 
@@ -55,6 +56,32 @@ void ImageData_Test::testConstructor() {
         CPPUNIT_ASSERT(imageOffsets.rank() == 2);
         CPPUNIT_ASSERT(imageOffsets.x1() == 0);
         CPPUNIT_ASSERT(imageOffsets.x2() == 0);
+
+        CPPUNIT_ASSERT_EQUAL(32, image.getBitsPerPixel());
+
+        CPPUNIT_ASSERT_EQUAL(std::string(), image.getDimensionScales());
+        CPPUNIT_ASSERT_EQUAL((size_t) 2u, image.getDimensionTypes().size());
+
+        // No header was specified: get an empty one
+        karabo::util::Hash header;
+        CPPUNIT_ASSERT_NO_THROW(header = image.getHeader());
+        CPPUNIT_ASSERT(header.empty());
+
+        // Don't care about what kind of geometry - but it must not throw...
+        CPPUNIT_ASSERT_NO_THROW(image.getGeometry());
+    }
+    {
+        // Default constructor - we do not mind values, but all getters must not throw!
+        ImageData image;
+        CPPUNIT_ASSERT_NO_THROW(image.getBitsPerPixel());
+        CPPUNIT_ASSERT_NO_THROW(image.getData());
+        CPPUNIT_ASSERT_NO_THROW(image.getDimensionScales());
+        CPPUNIT_ASSERT_NO_THROW(image.getDimensionTypes());
+        CPPUNIT_ASSERT_NO_THROW(image.getDimensions());
+        CPPUNIT_ASSERT_NO_THROW(image.getEncoding());
+        CPPUNIT_ASSERT_NO_THROW(image.getGeometry());
+        CPPUNIT_ASSERT_NO_THROW(image.getHeader());
+        CPPUNIT_ASSERT_NO_THROW(image.getROIOffsets());
     }
 }
 
@@ -65,7 +92,7 @@ void ImageData_Test::testSetAndGetMethods() {
     Dims offsets(10, 50);
     int tmp[] = {Dimension::DATA, Dimension::STACK};
     std::vector<int> dimTypes(tmp, tmp + 2);
-    std::vector<unsigned char> someData(dims.size(), 2);
+    std::vector<unsigned char> someData(dims.size(), 2); // i.e. type UINT8
 
     {
         NDArray arr(&someData[0], someData.size());
@@ -75,6 +102,7 @@ void ImageData_Test::testSetAndGetMethods() {
         image.setDimensions(dims);
         image.setROIOffsets(offsets);
         image.setDimensionTypes(dimTypes);
+        image.setHeader(Hash("one", 1));
 
         // Get
         Dims imageDims(image.getDimensions());
@@ -93,6 +121,14 @@ void ImageData_Test::testSetAndGetMethods() {
         CPPUNIT_ASSERT(imageDimTypes[0] == Dimension::DATA);
         CPPUNIT_ASSERT(imageDimTypes[1] == Dimension::STACK);
 
+        CPPUNIT_ASSERT_EQUAL(1, image.getHeader().get<int>("one"));
+
+        CPPUNIT_ASSERT_EQUAL(8, image.getBitsPerPixel()); // as determined for UINT8
+        image.setBitsPerPixel(5);
+        CPPUNIT_ASSERT_EQUAL(5, image.getBitsPerPixel());
+        // Setting to larger than what the type can carry will be manipulated to maximum of type
+        image.setBitsPerPixel(10);
+        CPPUNIT_ASSERT_EQUAL(8, image.getBitsPerPixel());
     }
 
 }
