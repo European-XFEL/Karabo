efde158c471f5b7578586149a5243908430a4fc2
Chen Xu <chen.xu@desy.de>
2017-11-29 09:55:19 +0100

    Move the project controllers that don't touch bindings

---
+++ src/pythonGui/karabogui/project/controller/project_groups.py
@@ -0,0 +1,229 @@
+#############################################################################
+# Author: <john.wiggins@xfel.eu>
+# Created on October 27, 2016
+# Copyright (C) European XFEL GmbH Hamburg. All rights reserved.
+#############################################################################
+from functools import partial
+import os.path as op
+
+from PyQt4.QtGui import QAction, QDialog, QMenu
+from traits.api import Instance, Property, String
+
+from karabo.common.project.api import (
+    DeviceServerModel, MacroModel, ProjectModel, read_macro)
+from karabo.common.scenemodel.api import SceneModel, read_scene
+from karabogui import icons, messagebox
+from karabogui.dialogs.device_scenes import DeviceScenesDialog
+from karabogui.enums import KaraboSettings
+from karabogui.project.dialog.object_handle import ObjectEditDialog
+from karabogui.project.dialog.server_handle import ServerHandleDialog
+from karabogui.request import call_device_slot
+from karabogui.util import (
+    getOpenFileName, get_setting, set_setting, handle_scene_from_server)
+from .bases import BaseProjectGroupController, ProjectControllerUiData
+
+
+class ProjectSubgroupController(BaseProjectGroupController):
+    """ A controller for ProjectModel subgroups (devices, scenes, macros, etc.)
+    """
+    # Redefine model with the correct type
+    model = Instance(ProjectModel)
+    # The name of the group shown in the GUI
+    group_name = String
+    # The name of the trait on ``model`` which is controlled by ``children``
+    trait_name = String
+
+    # NOTE: We're overriding the base class here
+    ui_data = Property(Instance(ProjectControllerUiData))
+
+    def context_menu(self, project_controller, parent=None):
+        menu_fillers = {
+            'macros': _fill_macros_menu,
+            'scenes': _fill_scenes_menu,
+            'servers': _fill_servers_menu,
+        }
+        filler = menu_fillers.get(self.trait_name)
+        menu = QMenu(parent)
+        filler(menu, project_controller)
+        return menu
+
+    def create_ui_data(self):
+        """Satisfy the ABC
+        """
+        return None
+
+    def ui_item_text(self):
+        """The name for this controller's item in the GUI view
+
+        NOTE: We are overriding this here to avoid the addition of a '*' when
+        the project object is in a modified state.
+        """
+        return self.display_name
+
+    def _get_display_name(self):
+        """Traits property getter for ``display_name``
+        """
+        return self.group_name
+
+    def _get_ui_data(self):
+        return ProjectControllerUiData(icon=icons.folder)
+
+    def _set_ui_data(self, value):
+        """Explicitly ignore assignment to the ``ui_data`` trait.
+        """
+        pass
+
+
+def _fill_macros_menu(menu, project_controller):
+    add_action = QAction('Add macro', menu)
+    add_action.triggered.connect(partial(_add_macro, project_controller))
+    load_action = QAction('Load macro...', menu)
+    load_action.triggered.connect(partial(_load_macro, project_controller))
+    menu.addAction(add_action)
+    menu.addAction(load_action)
+
+
+def _fill_scenes_menu(menu, project_controller):
+    add_action = QAction('Add scene', menu)
+    add_action.triggered.connect(partial(_add_scene, project_controller))
+    load_action = QAction('Load scene...', menu)
+    load_action.triggered.connect(partial(_load_scene, project_controller))
+    load_from_device = QAction('Load from device...', menu)
+    load_from_device.triggered.connect(partial(_load_scene_from_device,
+                                               project_controller))
+    menu.addAction(add_action)
+    menu.addAction(load_action)
+    menu.addAction(load_from_device)
+
+
+def _fill_servers_menu(menu, project_controller):
+    add_action = QAction('Add server', menu)
+    add_action.triggered.connect(partial(_add_server, project_controller))
+    menu.addAction(add_action)
+
+
+# ----------------------------------------------------------------------
+# action handlers
+
+_macro_template = """\
+from karabo.middlelayer import Macro, Slot, String
+
+class {0}(Macro):
+    name = String(defaultValue="{0}")
+
+    @Slot()
+    def execute(self):
+        print("Hello {{}}!".format(self.name))
+"""
+
+
+def _add_macro(project_controller):
+    """ Add a macro to the associated project
+    """
+    project = project_controller.model
+    dialog = ObjectEditDialog(object_type='macro')
+    if dialog.exec() == QDialog.Accepted:
+        classname = dialog.simple_name.title()
+        classname = "".join(c for c in classname if c.isalpha())
+        # XXX: TODO check for existing
+        macro = MacroModel(simple_name=dialog.simple_name,
+                           code=_macro_template.format(classname))
+        # Set initialized and modified last
+        macro.initialized = macro.modified = True
+        project.macros.append(macro)
+
+
+def _load_macro(project_controller):
+    path = get_setting(KaraboSettings.MACRO_DIR)
+    directory = path if path and op.isdir(path) else ""
+
+    fn = getOpenFileName(caption='Load macro', filter='Python Macros (*.py)',
+                         directory=directory)
+    if not fn:
+        return
+
+    # Store old macro dialog path
+    set_setting(KaraboSettings.MACRO_DIR, op.dirname(fn))
+
+    project = project_controller.model
+    # Read MacroModel
+    macro = read_macro(fn)
+    # Set the scene model title
+    macro.simple_name = op.splitext(op.basename(fn))[0]
+    macro.modified = True
+    project.macros.append(macro)
+
+
+def _add_scene(project_controller):
+    """ Add a scene to the associated project
+    """
+    project = project_controller.model
+    dialog = ObjectEditDialog(object_type='scene')
+    if dialog.exec() == QDialog.Accepted:
+        # XXX: TODO check for existing
+        scene = SceneModel(simple_name=dialog.simple_name)
+        # Set initialized and modified last
+        scene.initialized = scene.modified = True
+        project.scenes.append(scene)
+
+
+def _load_scene(project_controller):
+    """ Load a scene from local disk
+    """
+    path = get_setting(KaraboSettings.SCENE_DIR)
+    directory = path if path and op.isdir(path) else ""
+
+    fn = getOpenFileName(caption='Load scene', filter='SVG Files (*.svg)',
+                         directory=directory)
+    if not fn:
+        return
+
+    # Store old scene dialog path
+    set_setting(KaraboSettings.SCENE_DIR, op.dirname(fn))
+
+    project = project_controller.model
+    # Read SceneModel
+    scene = read_scene(fn)
+    scene.simple_name = op.splitext(op.basename(fn))[0]
+    scene.reset_uuid()
+    scene.modified = True
+    project.scenes.append(scene)
+
+
+def _load_scene_from_device(project_controller):
+    """Request a scene directly from a device
+    """
+    dialog = DeviceScenesDialog()
+    if dialog.exec() == QDialog.Accepted:
+        device_id = dialog.device_id
+        scene_name = dialog.scene_name
+        project = project_controller.model
+        project_scenes = {s.simple_name for s in project.scenes}
+
+        if scene_name in project_scenes:
+            msg = ('A scene with that name already exists in the selected '
+                   'project.')
+            messagebox.show_warning(msg, title='Cannot Load Scene')
+            return
+
+        handler = partial(handle_scene_from_server, device_id, scene_name,
+                          project)
+        call_device_slot(handler, device_id, 'requestScene', name=scene_name)
+
+
+def _add_server(project_controller):
+    """ Add a server to the associated project
+    """
+    project = project_controller.model
+    dialog = ServerHandleDialog()
+    if dialog.exec() == QDialog.Accepted:
+        # XXX: TODO check for existing
+        traits = {
+            'server_id': dialog.server_id,
+            'host': dialog.host,
+            'description': dialog.description
+        }
+        server = DeviceServerModel(**traits)
+        # Set initialized and modified last
+        server.initialized = server.modified = True
+        project.servers.append(server)
---
+++ src/pythonGui/karabogui/project/controller/subproject.py
@@ -0,0 +1,138 @@
+#############################################################################
+# Author: <john.wiggins@xfel.eu>
+# Created on January 26, 2016
+# Copyright (C) European XFEL GmbH Hamburg. All rights reserved.
+#############################################################################
+from collections import defaultdict
+
+from PyQt4.QtCore import pyqtSlot
+from PyQt4.QtGui import QAction, QDialog, QMenu
+from traits.api import Instance, List, on_trait_change
+
+from karabo.common.api import walk_traits_object
+from karabo.common.project.api import (
+    DeviceInstanceModel, ProjectModel, device_instance_exists)
+from karabogui import messagebox
+from karabogui.project.dialog.project_handle import NewProjectDialog
+from karabogui.project.loading_watcher import ProjectLoadingWatcher
+from karabogui.project.utils import load_project
+from karabogui.singletons.api import get_project_model
+from .project_groups import ProjectSubgroupController
+
+
+class SubprojectController(ProjectSubgroupController):
+    """ A controller for 'subprojects' subgroups.
+    """
+    # These are subprojects which are asynchronously loading
+    _watchers = List(Instance(ProjectLoadingWatcher))
+
+    def context_menu(self, project_controller, parent=None):
+        menu = QMenu(parent)
+        add_action = QAction('Add new project...', menu)
+        add_action.triggered.connect(self._add_project)
+        load_action = QAction('Load project...', menu)
+        load_action.triggered.connect(self._load_project)
+        menu.addAction(add_action)
+        menu.addAction(load_action)
+        return menu
+
+    # ----------------------------------------------------------------------
+    # action handlers
+
+    @pyqtSlot()
+    def _add_project(self):
+        """ Add a new subproject to the associated project
+        """
+        dialog = NewProjectDialog()
+        if dialog.exec() == QDialog.Accepted:
+            # XXX: TODO check for existing
+            project = ProjectModel(simple_name=dialog.simple_name)
+            # Set initialized and modified last
+            project.initialized = project.modified = True
+            self.model.subprojects.append(project)
+
+    @pyqtSlot()
+    def _load_project(self):
+        """ Add an existing project as a subproject.
+        """
+        project = load_project(is_subproject=True)
+        if project is None:
+            return
+
+        # Loading projects is complex, due to asynchronicity. Use a helper
+        # to determine when all the project data is present.
+        watcher = ProjectLoadingWatcher(project=project)
+        if watcher.finished:
+            self._check_and_add_project(project)
+        else:
+            self._watchers.append(watcher)
+
+    # ----------------------------------------------------------------------
+    # trait handlers
+
+    @on_trait_change('_watchers:finished_event')
+    def _watcher_finished(self, obj, name, old, new):
+        """Called when a project finishes loading all of its data
+        """
+        self._watchers.remove(obj)
+        self._check_and_add_project(obj.project)
+
+    # ----------------------------------------------------------------------
+    # util methods
+
+    def _check_and_add_project(self, project):
+        """Make sure a subproject doesn't have conflicts with the open project
+        """
+        root_project = get_project_model().root_model
+        if self._check_for_duplicate_projects(project, root_project):
+            return
+        if self._check_for_duplicate_devices(project, root_project):
+            return
+
+        # Finally add the subproject
+        self.model.subprojects.append(project)
+
+    def _check_for_duplicate_devices(self, project, root_project):
+        """Make sure a subproject doesn't have conflicting device IDs
+        """
+        device_ids = []
+
+        def visitor(model):
+            if isinstance(model, DeviceInstanceModel):
+                device_ids.append(model.instance_id)
+
+        # collect all the device ids in the project being added
+        walk_traits_object(project, visitor)
+
+        # ... and make sure they don't already exist in any project
+        if device_instance_exists(root_project, device_ids):
+            # Computer says no....
+            msg = ('The subproject which you wish to load contains devices<br>'
+                   'which already exist in the current project. Therefore <br>'
+                   'it will not be added.')
+            messagebox.show_warning(msg, title='Device already exists')
+            return True
+        return False
+
+    def _check_for_duplicate_projects(self, project, root_project):
+        """Make sure a subproject doesn't include any projects which are
+        already loaded.
+        """
+        project_uuids = defaultdict(int)
+
+        def _visitor(model):
+            """Count the number of times each UUID appears in the tree"""
+            nonlocal project_uuids
+            if isinstance(model, ProjectModel):
+                project_uuids[model.uuid] += 1
+
+        # Walk both projects and count the instances of project models
+        walk_traits_object(project, _visitor)
+        walk_traits_object(root_project, _visitor)
+
+        # If a UUID appeared more than once, we have a problem!
+        if any(v > 1 for v in project_uuids.values()):
+            msg = 'That project OR one of its sub-projects is already loaded!'
+            messagebox.show_warning(msg, title='Load Subproject Failed')
+            return True
+        return False
