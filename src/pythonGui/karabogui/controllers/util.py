4e5cd5395cb3cfc899aac589b2916180a0e2603c
John Wiggins <john.wiggins@desy.de>
2017-11-30 13:05:43 +0100

    Resurrect the GUI registry loader
    This time, it's a function whose side effect is the registration of all
    widgets in karabogui.controllers

--- src/pythonGui/karabogui/controllers/util.py
+++ src/pythonGui/karabogui/controllers/util.py
@@ -1,3 +1,6 @@
+import importlib
+import os
+
 from karabogui.binding.api import (
     KARABO_SCHEMA_DISPLAYED_NAME, KARABO_SCHEMA_DISPLAY_TYPE)
 
@@ -17,6 +20,28 @@ def axis_label(proxy):
 def has_options(binding):
     """Returns True if a binding has any `options` defined."""
     return len(binding.options) > 0
+
+
+def populate_controller_registry():
+    """Make sure all the `BaseBindingController` subclasses are registered.
+
+    Yup. gui_registry_loader lives on! Hopefully a little less ugly in this
+    form?
+
+    NOTE: This will not work if the GUI is ever packaged as compiled modules
+    (ie: if the .py files are not present).
+    """
+    ROOT_PACKAGE = 'karabogui.controllers'
+    SUBPACKAGES = ('display', 'edit')
+    for pkg in SUBPACKAGES:
+        pkg = ROOT_PACKAGE + '.' + pkg
+        module = importlib.import_module(pkg)
+        # Only the Python files
+        mod_files = [fn for fn in os.listdir(os.path.dirname(module.__file__))
+                     if fn.endswith('.py')]
+        for mod_fname in mod_files:
+            submodule = os.path.splitext(mod_fname)[0]
+            importlib.import_module('{}.{}'.format(pkg, submodule))
 
 
 def with_display_type(display_type):
