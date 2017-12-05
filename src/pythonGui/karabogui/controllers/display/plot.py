287d4ba0ac02bf0005ebaf79daacebebe4b3a9f4
silenzi <alessandro.silenzi@xfel.eu>
2017-12-05 15:16:05 +0100

    GUI: DisplayPlot fails with offline

--- src/pythonGui/karabogui/controllers/display/plot.py
+++ src/pythonGui/karabogui/controllers/display/plot.py
@@ -17,7 +17,6 @@ from karabogui.controllers.registry import register_binding_controller
 
 COLORS = ("red", "green", "blue", "gray", "violet", "orange", "lightgreen",
           "black")
-COLOR_GEN = cycle(COLORS)
 
 
 @register_binding_controller(ui_name='Plot', klassname='DisplayPlot',
@@ -29,6 +28,8 @@ class DisplayPlot(BaseBindingController):
     _plot = Instance(object)  # some Qwt bullshit
     _curves = Dict
 
+    _line_colors = Instance(cycle, allow_none=False)
+
     def create_widget(self, parent):
         widget = CurveDialog(edit=False, toolbar=True, wintitle="Plot")
         self._plot = widget.get_plot()
@@ -36,12 +37,19 @@ class DisplayPlot(BaseBindingController):
         return widget
 
     def add_proxy(self, proxy):
+        if proxy.binding is not None:
+            self.binding_update(proxy)
+        return True
+
+    def binding_update(self, proxy):
+        curve = self._curves.pop(proxy, None)
+        if curve is not None:
+            self._plot.del_item(curve)
         attrs = proxy.binding.attributes
         title = attrs.get(KARABO_SCHEMA_DISPLAYED_NAME, proxy.path)
-        curve = make.curve([0, 1], [0, 1], title, next(COLOR_GEN))
+        curve = make.curve([0, 1], [0, 1], title, next(self._line_colors))
         self._curves[proxy] = curve
         self._plot.add_item(curve)
-        return True
 
     def value_update(self, proxy):
         if self._plot is None:
@@ -49,3 +57,6 @@ class DisplayPlot(BaseBindingController):
         value = proxy.value
         self._curves[proxy].set_data(arange(len(value)), value)
         self._plot.replot()
+
+    def __line_colors_default(self):
+        return cycle(COLORS)
