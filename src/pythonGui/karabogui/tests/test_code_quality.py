d91ec476a444021ff8fe2b3c11bfa87cc256b619
John Wiggins <john.wiggins@desy.de>
2017-12-06 11:44:26 +0100

    Simplify the GUI code quality unit test

--- src/pythonGui/karabogui/tests/test_code_quality.py
+++ src/pythonGui/karabogui/tests/test_code_quality.py
@@ -1,50 +1,10 @@
-import os
 import os.path as op
-from unittest import TestCase
-
-from mccabe import get_code_complexity
-from pycodestyle import Checker
-from pyflakes.api import checkPath
+import subprocess
 
 import karabogui
 
-GUI_MODULES = [op.join(r, name)
-               for r, d, fs in os.walk(op.dirname(karabogui.__file__))
-               for name in fs if name.endswith('.py') and name != 'api.py']
-PEP8_EXCEPTIONS = {
-    'pathparser.py': 1,
-}
-FLAKES_EXCEPTIONS = {
-    'globals.py': 2,
-    'test_sparkline.py': 1,
-}
-MAX_COMPLEXITY = 6
 
-
-class TestCodeQuality(TestCase):
-    def test_pep8(self):
-        for mod in GUI_MODULES:
-            base = os.path.basename(mod)
-            with self.subTest(module=base):
-                checker = Checker(mod)
-                errs = checker.check_all()
-                msg = 'PEP8 failed for {}'.format(mod)
-                assert errs == PEP8_EXCEPTIONS.get(base, 0), msg
-
-    def test_flakes(self):
-        for mod in GUI_MODULES:
-            base = os.path.basename(mod)
-            with self.subTest(module=base):
-                errs = checkPath(mod)
-                msg = 'pyflakes failed for {}'.format(mod)
-                assert errs == FLAKES_EXCEPTIONS.get(base, 0), msg
-
-    def test_mmcabe(self):
-        for mod in GUI_MODULES:
-            base = os.path.basename(mod)
-            with self.subTest(module=base):
-                with open(mod, "r", encoding="utf-8") as fin:
-                    code = fin.read()
-                complexity = get_code_complexity(code, filename=base)
-                msg = 'mccabe failed for {}'.format(mod)
-                assert complexity < MAX_COMPLEXITY, msg
+def test_code_quality_flake8():
+    # Just run flake8 as if from the commandline
+    command = ['flake8', op.dirname(op.abspath(karabogui.__file__))]
+    subprocess.check_call(command)
