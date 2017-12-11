96c332514eb3618014e549220ce8dd78decf00c9
Dennis Goeries <dennis.goeries@xfel.eu>
2017-12-11 12:06:34 +0100

    Refactor Camera FSM

--- src/pythonKarabo/karabo/bound_api/camera_fsm.py
+++ src/pythonKarabo/karabo/bound_api/camera_fsm.py
@@ -41,7 +41,7 @@ class CameraFsm(base.BaseFsm):
             SLOT_ELEMENT(expected).key("acquire")
                 .displayedName("Acquire")
                 .description("Instructs camera to go into acquisition state")
-                .allowedStates(State.STOPPED)
+                .allowedStates(State.ON)
                 .commit(),
 
             SLOT_ELEMENT(expected).key("trigger")
@@ -84,7 +84,7 @@ class CameraFsm(base.BaseFsm):
         KARABO_FSM_STATE_EE(State.INIT, self.initializationStateOnEntry, self.initializationStateOnExit)
         KARABO_FSM_STATE_EE(State.UNKNOWN, self.unknownStateOnEntry, self.unknownStateOnExit)
         KARABO_FSM_STATE_EE(State.ACQUIRING, self.acquisitionStateOnEntry, self.acquisitionStateOnExit)
-        KARABO_FSM_STATE_EE(State.STOPPED, self.readyStateOnEntry, self.readyStateOnExit)
+        KARABO_FSM_STATE_EE(State.ON, self.readyStateOnEntry, self.readyStateOnExit)
 
         #**************************************************************
         #*                    Transition Actions                      *
@@ -105,12 +105,12 @@ class CameraFsm(base.BaseFsm):
         #**************************************************************
         okStt=[
         # Source-State    Event           Target-State     Action           Guard
-        (State.STOPPED,    'AcquireEvent', State.ACQUIRING, 'AcquireAction', 'none'),
-        (State.ACQUIRING, 'StopEvent',    State.STOPPED,    'StopAction',    'none'),
+        (State.ON,    'AcquireEvent', State.ACQUIRING, 'AcquireAction', 'none'),
+        (State.ACQUIRING, 'StopEvent',    State.ON,    'StopAction',    'none'),
         (State.ACQUIRING, 'TriggerEvent', None,            'TriggerAction', 'none')
         ]
         #                        Name  Transition-Table  Initial-State
-        KARABO_FSM_STATE_MACHINE(State.NORMAL, okStt, State.STOPPED)
+        KARABO_FSM_STATE_MACHINE(State.NORMAL, okStt, State.ON)
 
         #**************************************************************
         #*                      Known Machine                         *
