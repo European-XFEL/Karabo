0db0c6c758f1b7c73fee8b14734fa611defbbd4c
Dennis Goeries <dennis.goeries@xfel.eu>
2017-12-19 18:02:41 +0100

    GUI: Fix pop up widget updating after each edit

--- src/pythonGui/karabogui/controllers/display/popup.py
+++ src/pythonGui/karabogui/controllers/display/popup.py
@@ -6,6 +6,7 @@ from PyQt4.QtSvg import QSvgWidget
 from traits.api import Instance
 
 from karabo.common.scenemodel.api import PopUpModel
+from karabo.middlelayer import Timestamp
 from karabogui.binding.api import (
     PropertyProxy, SlotBinding, StringBinding, KARABO_SCHEMA_DISPLAYED_NAME)
 from karabogui.controllers.api import (
@@ -27,6 +28,7 @@ class PopUp(BaseBindingController):
     _dialog = Instance(QMessageBox)
     _ok = Instance(PropertyProxy)
     _cancel = Instance(PropertyProxy)
+    _timestamp = Instance(Timestamp, args=())
 
     def add_proxy(self, proxy):
         if not isinstance(proxy.binding, SlotBinding):
@@ -60,8 +62,11 @@ class PopUp(BaseBindingController):
 
     def value_update(self, proxy):
         value = proxy.value
-        self._dialog.setText(value)
-        self._dialog.setVisible(value != '')
+        timestamp = proxy.binding.timestamp
+        if timestamp != self._timestamp:
+            self._dialog.setText(value)
+            self._timestamp = timestamp
+            self._dialog.setVisible(value != '')
 
     @pyqtSlot(object)
     def _on_finished(self, result):
