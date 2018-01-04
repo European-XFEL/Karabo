567f95ff803024647297ba65f6a31b2a349d7704
Steffen Hauf <steffen.hauf@xfel.eu>
2018-01-04 13:52:32 +0100

    Update cross-test checksum

--- src/pythonKarabo/karabo/middlelayer_api/tests/cross_test.py
+++ src/pythonKarabo/karabo/middlelayer_api/tests/cross_test.py
@@ -100,7 +100,7 @@ class Tests(DeviceTest):
             stdout=PIPE)
         schema = yield from self.process.stdout.read()
         yield from self.process.wait()
-        self.assertEqual(adler32(schema), 2143514750,
+        self.assertEqual(adler32(schema), 2257022425,
             "The generated schema changed. If this is desired, change the "
             "checksum in the code.")
 
