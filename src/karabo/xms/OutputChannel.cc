04d02f90c3836ee0bd8d212d0e03dfc541621db6
esenov <serguei.essenov@xfel.eu>
2018-02-27 12:08:43 +0100

    Upgrade boost to 1.66.0 and adapt framework

--- src/karabo/xms/OutputChannel.cc
+++ src/karabo/xms/OutputChannel.cc
@@ -213,7 +213,7 @@ namespace karabo {
             // Prepare to accept more connections
             if (m_dataConnection) m_dataConnection->startAsync(bind_weak(&karabo::xms::OutputChannel::onTcpConnect, this, _1, _2));
             TcpChannel::Pointer tch = boost::dynamic_pointer_cast<TcpChannel>(channel);
-            KARABO_LOG_FRAMEWORK_DEBUG << "***** Connection established to socket " << tch->socket().native() << " *****";
+            KARABO_LOG_FRAMEWORK_DEBUG << "***** Connection established *****";
             channel->readAsyncHash(bind_weak(&karabo::xms::OutputChannel::onTcpChannelRead, this, _1, channel, _2));
         }
 
@@ -221,8 +221,7 @@ namespace karabo {
         void OutputChannel::onTcpChannelError(const karabo::net::ErrorCode& error, const karabo::net::Channel::Pointer& channel) {
             using namespace karabo::net;
             TcpChannel::Pointer tch = boost::dynamic_pointer_cast<TcpChannel>(channel);
-            KARABO_LOG_FRAMEWORK_INFO << "Tcp channel (socket " << tch->socket().native()
-                    << ") error on \"" << m_instanceId << "\", code #" << error.value() << " -- \""
+            KARABO_LOG_FRAMEWORK_INFO << "Tcp channel error on \"" << m_instanceId << "\", code #" << error.value() << " -- \""
                     << error.message() << "\".  Channel closed.";
             // Unregister channel
             onInputGone(channel);
