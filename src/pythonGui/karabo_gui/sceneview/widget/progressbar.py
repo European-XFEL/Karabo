01e05c843ccc2aaabb83a1c60c7c4229195595d4
degon <dennis.goeries@xfel.eu>
2018-04-12 19:00:37 +0200

    FW: Remove old GUI from Karabo

--- src/pythonGui/karabo_gui/sceneview/widget/progressbar.py
+++
@@ -1,7 +0,0 @@
-from karabo_gui.displaywidgets.displayprogressbar import DisplayProgressBar
-from .base import BaseWidgetContainer
-
-
-class ProgressBarContainer(BaseWidgetContainer):
-    def _create_widget(self, boxes):
-        return DisplayProgressBar(self.model, boxes[0], self)
--- src/pythonGui/karabo_gui/topology/api.py
+++
@@ -1,5 +0,0 @@
-# flake8: noqa
-from .project_device import ProjectDeviceInstance
-from .system import SystemTopology
-from .tree import SystemTree, SystemTreeNode
-from .util import clear_configuration_instance
