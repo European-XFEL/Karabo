918afb6648dd41621bcf6508a541c2e8ba1b008d
degon <dennis.goeries@xfel.eu>
2018-04-12 19:00:37 +0200

    FW: Remove old GUI from Karabo

--- src/pythonGui/karabo_gui/dialogs/__init__.py
+++
@@ -1,7 +0,0 @@
-#############################################################################
-# Author: <kerstin.weger@xfel.eu>
-# Created on May 3, 2014
-# Copyright (C) European XFEL GmbH Hamburg. All rights reserved.
-#############################################################################
-
-# define package dialogs
--- src/pythonGui/karabogui/tests/test_imports.py
+++ src/pythonGui/karabogui/tests/test_imports.py
@@ -1,16 +1,8 @@
-from functools import partial
-
 from karabo.testing.import_checker import (
-    check_for_disallowed_module_imports, check_for_star_imports,
-    run_checker_on_package
+    check_for_star_imports, run_checker_on_package
 )
 import karabogui as gui_pkg
 
 
 def test_no_star_imports():
     run_checker_on_package(gui_pkg, check_for_star_imports)
-
-
-def test_no_old_imports():
-    checker = partial(check_for_disallowed_module_imports, 'karabo_gui')
-    run_checker_on_package(gui_pkg, checker, skip_tests=False)
