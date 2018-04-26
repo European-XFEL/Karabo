655a347410a8d6b52fbd13c0114d1b592e2d19f2
degon <dennis.goeries@xfel.eu>
2018-04-26 19:25:55 +0200

    MDL: mostSignificantState utils

--- src/pythonKarabo/karabo/middlelayer_api/utils.py
+++ src/pythonKarabo/karabo/middlelayer_api/utils.py
@@ -1,5 +1,9 @@
 import os
 
+from karabo.common.states import State, StateSignifier
+
+from .basetypes import newest_timestamp
+
 
 def get_karabo_version():
     """Return the karabo version from the KARABO VERSION file
@@ -13,3 +17,22 @@ def get_karabo_version():
     with open(path, 'r') as fp:
         version = fp.read()
     return version
+
+
+def mostSignificantState(iterable, signifier=None):
+    """Return the most significant state from the iterable with KaraboValues
+
+    Attaches the newest timestamp to the most significant state
+
+    :param iterable: iterable with KaraboValues (States)
+    :param signifier: Optional state signifier, otherwise default is used.
+    """
+    if signifier is None:
+        signifier = StateSignifier(
+            staticMoreSignificant=State.PASSIVE,
+            changingMoreSignificant=State.DECREASING)
+
+    state = signifier.returnMostSignificant(iterable)
+    state.timestamp = newest_timestamp(iterable)
+
+    return state
