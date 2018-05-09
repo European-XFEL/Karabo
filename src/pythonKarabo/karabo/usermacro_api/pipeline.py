f126f6a8d13480132ef78154cd819a987e99b3d1
degon <dennis.goeries@xfel.eu>
2018-05-09 14:46:22 +0200

    Macro api: Alarm service

--- src/pythonKarabo/karabo/usermacro_api/pipeline.py
+++
@@ -1,7 +0,0 @@
-"""OutputChannel implemented to be retrofitted to the middlelayer
-"""
-from .middlelayer import VectorUInt8
-
-
-class OutputChannel(VectorUInt8):
-    """Create an output channel"""
