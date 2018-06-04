790c43de8ee6fbd237245d78142616684c540ed8
Gero Flucke <gero.flucke@xfel.eu>
2018-06-04 12:16:48 +0200

    Avoid writing header keys in one class and interpreting it elsewhere
    Even for those kept for backward compatibility

--- src/karabo/xms/Memory.cc
+++ src/karabo/xms/Memory.cc
@@ -172,16 +172,8 @@ namespace karabo {
             }
             const MetaDataEntries& metaData = m_metaData[channelIdx][chunkIdx];
             header.clear();
-            header.set("sourceInfo", *reinterpret_cast<const std::vector<karabo::util::Hash>*>(&metaData));
-
-            // The next lines are NOT kept for pre-karabo 2.2.4 backward compatibility(?)
-            header.set<unsigned int>("nData", data.size());
-            std::vector<unsigned int> byteSizes;
-            byteSizes.reserve(data.size());
-            for (const auto& bp : data) {
-                byteSizes.push_back(bp->totalSize());
-            }
-            header.set<std::vector<unsigned int> >("byteSizes", byteSizes);
+            header.set("sourceInfo", *reinterpret_cast<const std::vector<karabo::util::Hash>*> (&metaData));
+            header.set("nData", false);
         }
 
         void Memory::assureAllDataIsCopied(const size_t channelIdx, const size_t chunkIdx) {
