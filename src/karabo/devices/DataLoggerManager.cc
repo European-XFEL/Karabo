ddd45e6ccc5230bb55cf45e6b6cd31324b44db4c
Gabriele Giovanetti <gabriele.giovanetti@xfel.eu>
2018-08-22 16:29:41 +0200

    Slow down DataLoggers instantiation
    Added to DataLoggerManager parameter 'instantiationDelay' (reconfigurable).
    If it is set to 0 behaviour is unchanged (i.e. datalogger device is instantiated as soon as possible).
    If its value is T > 10 ms Datalogger instantiation is done in sequence and at least T ms pass between
    one instantiaion and the following. For values 0 < T < 10 ms a delay of 10 ms is used.
    Delayed instantiations are queued to be instantiated on available datalogger servers.

--- src/karabo/devices/DataLoggerManager.cc
+++ src/karabo/devices/DataLoggerManager.cc
@@ -12,6 +12,8 @@
 #include <string.h>     // strlen
 
 #include <boost/algorithm/string.hpp>
+#include <boost/thread/pthread/mutex.hpp>
+#include <boost/asio/deadline_timer.hpp>
 
 #include "karabo/io/Input.hh"
 #include "karabo/io/FileTools.hh"
@@ -27,7 +29,6 @@ namespace karabo {
         using namespace std;
         using namespace karabo::util;
         using namespace karabo::io;
-
 
         KARABO_REGISTER_FOR_CONFIGURATION(karabo::core::BaseDevice, karabo::core::Device<>, DataLoggerManager)
 
@@ -65,9 +66,9 @@ namespace karabo {
             VECTOR_STRING_ELEMENT(expected).key("serverList")
                     .displayedName("Server list")
                     .description("List of device server IDs where the DataLogger instance run. "
-                                 "The load balancing is round-robin. If empty, try to get from logger map "
-                                 "or, as last source, just use the server of this device.")
+                                 "The load balancing is round-robin. Must not be empty")
                     .init()
+                    .minSize(1)
                     .assignmentMandatory()
                     .commit();
 
@@ -87,13 +88,26 @@ namespace karabo {
             OVERWRITE_ELEMENT(expected).key("heartbeatInterval")
                     .setNewDefaultValue(60)
                     .commit();
+
+            UINT32_ELEMENT(expected).key("instantiationDelay")
+                    .displayedName("Instantiation Delay")
+                    .description("Time to wait between dataloggers instantiation."
+                                 "If it is 0 no wait is done. "
+                                 "WARNING: if a value greater than 0 but lower than 10 ms is spevcified, 10 ms will be used"
+                                 "NOTE: on a single datalogger server delay time will be instantiationDelay * n_of_servers")
+                    .unit(karabo::util::Unit::SECOND).metricPrefix(MetricPrefix::MILLI)
+                    .adminAccess()
+                    .reconfigurable()
+                    .minInc(0).maxInc(1000)
+                    .assignmentOptional().defaultValue(200)
+                    .commit();
         }
 
-
         DataLoggerManager::DataLoggerManager(const Hash& input)
-            : karabo::core::Device<>(input),
-            m_serverList(input.get<vector<string> >("serverList")),
-            m_serverIndex(0), m_loggerMapFile("loggermap.xml") {
+        : karabo::core::Device<>(input),
+        m_serverList(input.get<vector<string> >("serverList")),
+            m_serverIndex(0), m_loggerMapFile("loggermap.xml"),
+            m_instantiateDelayTimer(boost::asio::deadline_timer(karabo::net::EventLoop::getIOService())) {
             m_loggerMap.clear();
             if (boost::filesystem::exists(m_loggerMapFile)) {
                 karabo::io::loadFromFile(m_loggerMap, m_loggerMapFile);
@@ -105,15 +119,17 @@ namespace karabo {
             KARABO_INITIAL_FUNCTION(initialize);
         }
 
-
         DataLoggerManager::~DataLoggerManager() {
             KARABO_LOG_INFO << "dead.";
         }
 
-
         void DataLoggerManager::initialize() {
 
             checkLoggerMap(); // throws if loggerMap and serverList are inconsistent
+
+            for (const std::string& serverId : m_serverList) {
+                m_instantiationQueues.emplace(serverId, std::deque<karabo::util::Hash>());
+            }
 
             // Switch on instance tracking
             remote().enableInstanceTracking();
@@ -126,16 +142,23 @@ namespace karabo {
             // try to restart readers and loggers if needed - it works reliably on "stopped" system
             restartReadersAndLoggers();
 
-            // Publish logger map read from disc. Do that as late as possible in the initialisation procedure
+            // Publish logger map read from disc. Do that as late as possible in the initialization procedure
             // to give those interested the chance to register their slots after we sent signalInstanceNew.
             {
                 boost::mutex::scoped_lock lock(m_loggerMapMutex); // m_loggerMap must not be changed while we process it
                 emit<Hash>("signalLoggerMap", m_loggerMap);
             }
 
+            m_instantiateDelayTimer.expires_from_now(boost::posix_time::milliseconds(calcInstantiationTimerDelay()));
+            m_instantiateDelayTimer.async_wait(karabo::util::bind_weak(&DataLoggerManager::doInstantiateHandler, this, boost::asio::placeholders::error, m_instantiationQueues.begin()));
+
             updateState(State::NORMAL);
         }
 
+        void DataLoggerManager::preDestruction() {
+            // cancel all timers
+            m_instantiateDelayTimer.cancel();
+        }
 
         void DataLoggerManager::checkLoggerMap() {
             // Check that all servers that are supposed to host DataLoggers are in server list.
@@ -152,11 +175,10 @@ namespace karabo {
             for (const std::string& serverInMap : serversInMap) {
                 if (find(m_serverList.begin(), m_serverList.end(), serverInMap) == m_serverList.end()) {
                     throw KARABO_LOGIC_EXCEPTION("Inconsistent '" + m_loggerMapFile + "' and \"serverList\" configuration: '"
-                                                 + serverInMap + "' is in map, but not in list.");
+                            + serverInMap + "' is in map, but not in list.");
                 }
             }
         }
-
 
         void DataLoggerManager::restartReadersAndLoggers() {
             const Hash runtimeInfo = remote().getSystemInformation();
@@ -186,32 +208,75 @@ namespace karabo {
             }
         }
 
-
         void DataLoggerManager::instantiateReaders(const std::string& serverId) {
             for (unsigned int i = 0; i < DATALOGREADERS_PER_SERVER; ++i) {
                 const std::string readerId = DATALOGREADER_PREFIX + toString(i) + "-" + serverId;
                 if (!remote().exists(readerId).first) {
                     const Hash hash("classId", "DataLogReader", "deviceId", readerId,
-                                    "configuration.directory", get<string>("directory"));
+                            "configuration.directory", get<string>("directory"));
                     KARABO_LOG_FRAMEWORK_INFO << "Trying to instantiate '" << readerId << "' on server '" << serverId << "'";
+
                     remote().instantiateNoWait(serverId, hash);
                 }
             }
         }
-
 
         void DataLoggerManager::slotGetLoggerMap() {
             boost::mutex::scoped_lock lock(m_loggerMapMutex); // m_loggerMap must not be changed while we process it
             reply(m_loggerMap);
         }
 
+        void DataLoggerManager::delayedInstantiation(const std::string serverId, const karabo::util::Hash& hash) {
+            if (get<unsigned int>("instantiationDelay") <= 0) {
+                remote().instantiateNoWait(serverId, hash);
+            } else {
+
+                {
+                    boost::mutex::scoped_lock lock(m_instantiateMutex);
+                    m_instantiationQueues[serverId].push_back(hash);
+                }
+            }
+        }
+
+
+        void DataLoggerManager::doInstantiateHandler(const boost::system::error_code& error, std::unordered_map<std::string, std::deque<karabo::util::Hash>>::iterator queueMapIter) {
+            m_instantiateDelayTimer.expires_from_now(boost::posix_time::milliseconds(calcInstantiationTimerDelay())); // re-arm timer
+
+            const std::string& serverId = queueMapIter->first;
+            std::deque < karabo::util::Hash>& queue = queueMapIter->second;
+
+            {
+                boost::mutex::scoped_lock lock(m_instantiateMutex);
+                if (!queue.empty()) {
+                    karabo::util::Hash cfg = queue.front();
+
+                    queue.pop_front();
+                    remote().instantiateNoWait(serverId, cfg);
+                }
+            }
+
+            if (++queueMapIter == m_instantiationQueues.end()) {
+                queueMapIter = m_instantiationQueues.begin();
+            }
+
+            m_instantiateDelayTimer.async_wait(karabo::util::bind_weak(&DataLoggerManager::doInstantiateHandler, this, boost::asio::placeholders::error, queueMapIter));
+            // if timer is already expired when async_wait() is called, doInstantateHandler is called immediately
+        }
+
+
+        unsigned int DataLoggerManager::calcInstantiationTimerDelay() {
+            //if delay is less than 10 ms, set it at 10
+            unsigned int delay = get<unsigned int>("instantiationDelay");
+            delay = (delay < 10) ? 10 : delay;
+            return delay;
+        }
 
         void DataLoggerManager::ensureLoggerRunning(const karabo::util::Hash& topologyEntry) {
             try {
                 const std::string& type = topologyEntry.begin()->getKey(); // fails if empty...
                 // const ref is fine even for temporary std::string
                 const std::string& instanceId = (topologyEntry.has(type) && topologyEntry.is<Hash>(type) ?
-                                                 topologyEntry.get<Hash>(type).begin()->getKey() : std::string("?"));
+                        topologyEntry.get<Hash>(type).begin()->getKey() : std::string("?"));
                 KARABO_LOG_FRAMEWORK_INFO << "ensureLoggerRunning --> instanceId: '" << instanceId
                         << "', type: '" << type << "'";
 
@@ -236,9 +301,9 @@ namespace karabo {
                                 serverId = m_loggerMap.get<string>(loggerId);
                             } else {
                                 if (m_serverList.empty()) {
-                                    // Cannot happen (see okStateOnEntry), but for better diagnostics in case it does:
+                                    // Cannot happen but for better diagnostics in case it does:
                                     throw KARABO_PARAMETER_EXCEPTION("List of servers for data logging is empty."
-                                                                     " You have to define one data logger server, at least!");
+                                            " You have to define one data logger server, at least!");
                                 }
                                 m_serverIndex %= m_serverList.size();
                                 serverId = m_serverList[m_serverIndex++];
@@ -247,13 +312,15 @@ namespace karabo {
                                 newMap = true;
                             }
                             const Hash config("deviceToBeLogged", deviceId,
-                                              "directory", get<string>("directory"),
-                                              "maximumFileSize", get<int>("maximumFileSize"),
-                                              "flushInterval", get<int>("flushInterval"));
+                                    "directory", get<string>("directory"),
+                                    "maximumFileSize", get<int>("maximumFileSize"),
+                                    "flushInterval", get<int>("flushInterval"));
                             const Hash hash("classId", "DataLogger", "deviceId", loggerId, "configuration", config);
                             KARABO_LOG_FRAMEWORK_INFO << "Trying to instantiate '" << loggerId << "' on server '"
                                     << serverId << "' since device '" << deviceId << "' appeared (or its logger died)";
-                            remote().instantiateNoWait(serverId, hash);
+
+                            delayedInstantiation(serverId, hash);
+
                             // First instantiate the new logger - now we have time to update the logger map file.
                             if (newMap) {
                                 karabo::io::saveToFile(m_loggerMap, m_loggerMapFile);
@@ -292,10 +359,9 @@ namespace karabo {
                         }
                         // Now, without mutex lock, treat the collected deviceIds (to keep mutex lock short)
                         const Hash config("directory", get<string>("directory"),
-                                          "maximumFileSize", get<int>("maximumFileSize"),
-                                          "flushInterval", get<int>("flushInterval"));
+                                "maximumFileSize", get<int>("maximumFileSize"),
+                                "flushInterval", get<int>("flushInterval"));
                         Hash hash("classId", "DataLogger", "configuration", config);
-
 
                         BOOST_FOREACH(const std::string& deviceId, devicesToLog) {
                             const string loggerId = DATALOGGER_PREFIX + deviceId;
@@ -304,23 +370,21 @@ namespace karabo {
                             hash.set("configuration.deviceToBeLogged", deviceId);
                             KARABO_LOG_FRAMEWORK_INFO << "Trying to instantiate '" << loggerId << "' on server '" << serverId
                                     << "' which just appeared";
-                            remote().instantiateNoWait(serverId, hash);
+                            delayedInstantiation(serverId, hash);
                         }
                     }
                 }
-
             } catch (const std::exception& e) {
                 KARABO_LOG_FRAMEWORK_ERROR << "In ensureLoggerRunning: " << e.what();
             }
         }
 
-
         void DataLoggerManager::instanceGoneHandler(const std::string& instanceId, const karabo::util::Hash& instanceInfo) {
             // const ref is fine even for temporary std::string
             const std::string& type = (instanceInfo.has("type") && instanceInfo.is<std::string>("type") ?
-                                       instanceInfo.get<std::string>("type") : std::string("unknown"));
+                    instanceInfo.get<std::string>("type") : std::string("unknown"));
             const std::string& serverId = (instanceInfo.has("serverId") && instanceInfo.is<std::string>("serverId") ?
-                                           instanceInfo.get<string>("serverId") : std::string("?"));
+                    instanceInfo.get<string>("serverId") : std::string("?"));
 
             KARABO_LOG_FRAMEWORK_INFO << "instanceGoneHandler -->  instanceId : '"
                     << instanceId << "', type : " << type << " on server '" << serverId << "'";
@@ -329,10 +393,11 @@ namespace karabo {
                     const vector<string> onlineDevices = remote().getDevices();
                     boost::optional<const Hash::Node&> classIdNode = instanceInfo.find("classId");
                     if (classIdNode && classIdNode->is<std::string>() && classIdNode->getValue<std::string>() == "DataLogger"
-                        && instanceId.find(DATALOGGER_PREFIX) == 0) {
+                            && instanceId.find(DATALOGGER_PREFIX) == 0) {
                         // A DataLogger with the expected prefix - check whether its logged device is still running:
                         const std::string loggedId(instanceId.substr(strlen(DATALOGGER_PREFIX))); // cut prefix
                         const bool loggedExists = std::find(onlineDevices.begin(), onlineDevices.end(), loggedId) != onlineDevices.end();
+
                         if (loggedExists) {
                             // Logged device still online - restart logger:
                             const Hash runtimeInfo = remote().getSystemInformation();
@@ -357,9 +422,26 @@ namespace karabo {
                         const bool loggerExists = std::find(onlineDevices.begin(), onlineDevices.end(), loggerId) != onlineDevices.end();
 
                         // Safety check
-                        if (!deviceExists && loggerExists) {
-                            this->call(loggerId, "slotTagDeviceToBeDiscontinued", true, 'D');
-                            remote().killDeviceNoWait(loggerId);
+                        if (!deviceExists) {
+                            if (loggerExists) {
+                                this->call(loggerId, "slotTagDeviceToBeDiscontinued", true, 'D');
+                                remote().killDeviceNoWait(loggerId);
+                            } else {
+                                //check if logger is still in some queue to be instantiated
+                                boost::mutex::scoped_lock lock(m_instantiateMutex);
+                                for (auto &q : m_instantiationQueues) {
+                                    for (auto it = q.second.begin(); it != q.second.end();) {
+                                        const std::string& loggedId = it->get<std::string>("configuration.deviceToBeLogged");
+
+                                        //erase item from queue if loggedId matches instanceId
+                                        if (loggedId == instanceId) {
+                                            it = q.second.erase(it); // erase return iterator pointing to next element
+                                        } else {
+                                            ++it;
+                                        }
+                                    }
+                                }
+                            }
                         }
                     }
                 }
