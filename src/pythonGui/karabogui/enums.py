c7e19c62d6e0c0a222194d0d297dcc3c7b9d5b56
degon <dennis.goeries@xfel.eu>
2018-10-02 13:06:36 +0200

    GUI: Delete KaraboSettings from GUI

--- src/pythonGui/karabogui/enums.py
+++ src/pythonGui/karabogui/enums.py
@@ -3,7 +3,6 @@
 # Created on February 21, 2012
 # Copyright (C) European XFEL GmbH Hamburg. All rights reserved.
 #############################################################################
-from enum import Enum
 
 
 class NavigationItemTypes(object):
@@ -12,15 +11,3 @@ class NavigationItemTypes(object):
     SERVER = 1
     CLASS = 2
     DEVICE = 3
-
-
-class KaraboSettings(Enum):
-    CONFIG_DIR = "LastUsedFolder/ConfigurationDialog"
-    MACRO_DIR = "LastUsedFolder/MacroDialog"
-    SCENE_DIR = "LastUsedFolder/SceneDialog"
-    PROJECT_DOMAIN = "Project/LastUsedDbDomain"
-    MACRO_SERVER = "Server/LastUsedMacroServer"
-    USERNAME = "Login/LastUsername"
-    GUI_SERVERS = "Login/LastConnectionAddresses"
-    MAX_GUI_SERVERS = "Login/MaxConnectionAddresses"
-    BROKER_TOPIC = "Login/Topic"
