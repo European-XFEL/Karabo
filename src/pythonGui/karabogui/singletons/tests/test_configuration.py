8267c6eadb87583920a75117f291995f4f6148b6
degon <dennis.goeries@xfel.eu>
2018-10-05 11:03:48 +0200

    Grouping in the configuration singleton

--- src/pythonGui/karabogui/singletons/tests/test_configuration.py
+++ src/pythonGui/karabogui/singletons/tests/test_configuration.py
@@ -1,5 +1,5 @@
 from karabogui.testing import GuiTestCase
-from ..configuration import Configuration
+from ..configuration import Configuration, NETWORK, PROJECT, ECOSYSTEM, DIR
 
 
 class TestConfiguration(GuiTestCase):
@@ -10,15 +10,43 @@ class TestConfiguration(GuiTestCase):
         config['db_token'] = 'admin'
         config['macro_server'] = 'karabo/macroServer'
 
-        self.assertEqual(len(config), 9)
+        self.assertEqual(len(config), 10)
         self.assertEqual(config['db_token'], 'admin')
         self.assertEqual(config['domain'], 'CAS_INTERNAL')
         self.assertEqual(config['macro_server'], 'karabo/macroServer')
 
         self.assertEqual(list(config.keys()),
                          ['broker_topic', 'config_dir', 'db_token', 'domain',
-                          'guiServers', 'macro_dir', 'macro_server',
-                          'scene_dir', 'username'])
+                          'gui_servers', 'macro_dir', 'macro_server',
+                          'project_manager', 'scene_dir', 'username'])
 
+    def test_set_wrong_key(self):
+        config = Configuration()
         with self.assertRaises(KeyError):
             config['noitem'] = "Beep"
+
+        with self.assertRaises(KeyError):
+            config.noitem = "Boom"
+
+    def test_configuration_groups(self):
+        config = Configuration()
+        groups = config.groups()
+        self.assertEqual(len(groups), 4)
+        network_group = groups[NETWORK]
+        self.assertEqual(len(network_group), 2)
+        self.assertIn('username', network_group)
+        self.assertIn('gui_servers', network_group)
+        project_group = groups[PROJECT]
+        self.assertEqual(len(project_group), 2)
+        self.assertIn('db_token', project_group)
+        self.assertIn('domain', project_group)
+        dir_group = groups[DIR]
+        self.assertEqual(len(dir_group), 3)
+        self.assertIn('macro_dir', dir_group)
+        self.assertIn('scene_dir', dir_group)
+        self.assertIn('config_dir', dir_group)
+        eco_group = groups[ECOSYSTEM]
+        self.assertEqual(len(eco_group), 3)
+        self.assertIn('macro_server', eco_group)
+        self.assertIn('project_manager', eco_group)
+        self.assertIn('broker_topic', eco_group)
