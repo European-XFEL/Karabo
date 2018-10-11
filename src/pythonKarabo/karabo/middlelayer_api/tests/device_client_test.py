96e2573ca807884fcbd63783ad75cc1b3c7c9e07
degon <dennis.goeries@xfel.eu>
2018-10-11 13:50:33 +0200

    ikarabo: add getConfigurationFromPast import

--- src/pythonKarabo/karabo/middlelayer_api/tests/device_client_test.py
+++ src/pythonKarabo/karabo/middlelayer_api/tests/device_client_test.py
@@ -5,7 +5,8 @@ from unittest.mock import Mock, patch
 from nose.tools import assert_raises
 
 from karabo.middlelayer import (
-    getConfigurationFromPast, Hash, KaraboError, Schema, SignalSlotable
+    getConfigurationFromPast, getSchemaFromPast, Hash, KaraboError, Schema,
+    SignalSlotable
 )
 
 
@@ -23,10 +24,11 @@ def _get_log_reader_id(device_id):
 @patch('karabo.middlelayer_api.device_client.get_instance')
 @patch('karabo.middlelayer_api.device_client._getLogReaderId',
        side_effect=_get_log_reader_id)
-def test_getConfigurationFromPast(_getLogReaderId, get_instance):
+def test_getConfigurationSchemaFromPast(_getLogReaderId, get_instance):
     # REMOVE the @synchronize wrapping
-    global getConfigurationFromPast
+    global getConfigurationFromPast, getSchemaFromPast
     getConfigurationFromPast = getConfigurationFromPast.__wrapped__
+    getSchemaFromPast = getSchemaFromPast.__wrapped__
 
     call_args = []
 
@@ -44,12 +46,20 @@ def test_getConfigurationFromPast(_getLogReaderId, get_instance):
     def sub_test_1():
         with assert_raises(KaraboError):
             yield from getConfigurationFromPast("aDeviceNotInHistory", time)
+
     get_event_loop().run_until_complete(sub_test_1())
 
     @coroutine
     def sub_test_2():
-        h, s = yield from getConfigurationFromPast("aDeviceInHistory", time)
+        h = yield from getConfigurationFromPast("aDeviceInHistory", time)
         assert isinstance(h, Hash)
-        assert isinstance(s, Schema)
         assert h['value'] == 42
+
     get_event_loop().run_until_complete(sub_test_2())
+
+    @coroutine
+    def sub_test_3():
+        s = yield from getSchemaFromPast("aDeviceInHistory", time)
+        assert isinstance(s, Schema)
+
+    get_event_loop().run_until_complete(sub_test_3())
