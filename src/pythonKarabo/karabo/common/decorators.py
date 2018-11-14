faa96a9f7b485928599841794568ab301c2e00fc
degon <dennis.goeries@xfel.eu>
2018-11-14 14:29:40 +0100

    Common: Add timeit decorator

--- src/pythonKarabo/karabo/common/decorators.py
+++ src/pythonKarabo/karabo/common/decorators.py
@@ -3,8 +3,9 @@
 # Created on June 13, 2017
 # Copyright (C) European XFEL GmbH Hamburg. All rights reserved.
 #############################################################################
+from functools import wraps
+from time import perf_counter
 import warnings
-import functools
 
 
 def karabo_deprecated(func):
@@ -23,7 +24,7 @@ def karabo_deprecated(func):
     def my_func():
         pass
     """
-    @functools.wraps(func)
+    @wraps(func)
     def new_func(*args, **kwargs):
         warnings.warn_explicit(
             "Call to deprecated function {}.".format(func.__name__),
@@ -33,3 +34,15 @@ def karabo_deprecated(func):
         )
         return func(*args, **kwargs)
     return new_func
+
+
+def timeit(func):
+    """A timing decorator"""
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        t_start = perf_counter()
+        ret = func(*args, **kwargs)
+        elapsed = perf_counter() - t_start
+        print("{} took {}".format(func.__name__, elapsed))
+        return ret
+    return wrapper
