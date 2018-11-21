3d010409c8b50d74531b0464db39250c23233508
degon <dennis.goeries@xfel.eu>
2018-11-21 17:27:23 +0100

    MDL: Remove not required code

--- src/pythonKarabo/karabo/middlelayer_api/tests/cross_schema_test.py
+++ src/pythonKarabo/karabo/middlelayer_api/tests/cross_schema_test.py
@@ -30,7 +30,7 @@ class Tests(DeviceTest):
             stdout=PIPE)
         schema = yield from self.process.stdout.read()
         yield from self.process.wait()
-        self.assertEqual(adler32(schema), 1693509334,
+        self.assertEqual(adler32(schema), 2425351263,
                          "The generated schema changed. If this is "
                          "desired, change the checksum in the code.")
 
