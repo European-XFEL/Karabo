7c01d9de65d528532eaf5761c2d8ffddb7f0b497
silenzi <alessandro.silenzi@xfel.eu>
2018-12-12 18:45:10 +0100

    MDL: units and alarms behave
    If a property has a unit the AlarmMixin
    gets confused on comparison.
    
    This MR addresses this problem

--- src/pythonKarabo/karabo/middlelayer_api/tests/alarm_test.py
+++ src/pythonKarabo/karabo/middlelayer_api/tests/alarm_test.py
@@ -2,7 +2,8 @@ from unittest import main, TestCase
 
 from karabo.middlelayer_api.alarm import AlarmMixin
 from karabo.middlelayer import (
-    AccessLevel, AlarmCondition, Configurable, Int32, Node)
+    AccessLevel, AlarmCondition, Configurable, Int32, 
+    MetricPrefix, Node, Unit, unit)
 
 
 class Tests(TestCase):
@@ -72,6 +73,47 @@ class Tests(TestCase):
         a.wmin = 3
         self.assertEqual(a.alarmCondition, AlarmCondition.WARN)
         a.right.wmax = 1
+        self.assertEqual(a.alarmCondition, AlarmCondition.WARN)
+
+    def test_units(self):
+        class N(Configurable):
+            amin = Int32(
+                    unitSymbol=Unit.METER,
+                    metricPrefixSymbol=MetricPrefix.MILLI,
+                    alarmLow=30)
+            wmax = Int32(
+                    unitSymbol=Unit.METER,
+                    warnHigh=3)
+
+        class A(AlarmMixin):
+            left = Node(N)
+            right = Node(N)
+            wmin = Int32(warnLow=22)
+
+        a = A({})
+        self.assertEqual(a.alarmCondition, AlarmCondition.NONE)
+        a.left.amin = 1
+        self.assertEqual(a.alarmCondition, AlarmCondition.ALARM)
+        # 40 is larger than 30 all ok
+        a.left.amin = 40
+        self.assertEqual(a.alarmCondition, AlarmCondition.NONE)
+        # 4000 mm are larger than 30 all ok
+        a.left.amin = 4 * unit.meter
+        self.assertEqual(a.alarmCondition, AlarmCondition.NONE)
+        # 100 um are smaller than 30 mm ALARM!!!
+        a.left.amin = 100 * unit.micrometer
+        self.assertEqual(a.alarmCondition, AlarmCondition.ALARM)
+        # 22 is larger than 3 but alarm trumps warning
+        a.right.wmax = 22
+        self.assertEqual(a.alarmCondition, AlarmCondition.ALARM)
+        # alarm goes away, but warning stays
+        a.left.amin = 5e3
+        self.assertEqual(a.alarmCondition, AlarmCondition.WARN)
+        a.wmin = 23
+        self.assertEqual(a.alarmCondition, AlarmCondition.WARN)
+        a.right.wmax = 1
+        self.assertEqual(a.alarmCondition, AlarmCondition.NONE)
+        a.wmin = 3
         self.assertEqual(a.alarmCondition, AlarmCondition.WARN)
 
     def test_init(self):
