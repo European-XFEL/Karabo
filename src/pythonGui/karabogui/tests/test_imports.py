ea839a2ac1bc4245de2e06c2dea19af886852804
Denivy Ruck <denivyruck1@gmail.com>
2019-02-07 11:24:11 +0100

    Make sure karabogui does not import from middlelayer

--- src/pythonGui/karabogui/tests/test_imports.py
+++ src/pythonGui/karabogui/tests/test_imports.py
@@ -1,8 +1,16 @@
-from karabo.testing.import_checker import (
-    check_for_star_imports, run_checker_on_package
-)
+from functools import partial
+
 import karabogui as gui_pkg
+from karabo.testing.import_checker import (check_for_disallowed_module_imports,
+                                           check_for_star_imports,
+                                           run_checker_on_package)
 
 
 def test_no_star_imports():
     run_checker_on_package(gui_pkg, check_for_star_imports)
+
+
+def test_no_middlelayer_imports():
+    for forbidden in ('karabo.middlelayer', 'karabo.middlelayer_api'):
+        checker = partial(check_for_disallowed_module_imports, forbidden)
+        run_checker_on_package(gui_pkg, checker)
