3d5683f240c78820ad7e43fa7ed8914fdd44d7ce
degon <dennis.goeries@xfel.eu>
2019-02-11 18:54:24 +0100

    MDL: Flake the init files and provide Hash and timestamp in CLI

--- src/pythonKarabo/karabo/native/project/__init__.py
+++ src/pythonKarabo/karabo/native/project/__init__.py
@@ -1,6 +1,6 @@
 # flake8: noqa
 from karabo.native.project.io import (
-	read_project_model, write_project_model)
+    read_project_model, write_project_model)
 from karabo.native.project.convert import convert_old_project
 from karabo.native.project.old import (
-	Project as OldProject, BaseDevice, BaseDeviceGroup)
+    Project as OldProject, BaseDevice, BaseDeviceGroup)
