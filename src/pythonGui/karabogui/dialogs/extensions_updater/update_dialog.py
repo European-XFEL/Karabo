7aaf33fd31469867cb25e5feba14d72903344443
Denivy Ruck <denivyruck1@gmail.com>
2019-02-13 12:15:16 +0100

    Split dialog into model

--- src/pythonGui/karabogui/dialogs/extensions_updater/update_dialog.py
+++ src/pythonGui/karabogui/dialogs/extensions_updater/update_dialog.py
@@ -1,12 +1,7 @@
-from lxml import etree
-from pathlib import Path
-import pkg_resources
 from PyQt4 import uic
-from PyQt4.QtCore import QProcess
 from PyQt4.QtGui import QDialog
-import re
-import requests
-import os
+from karabogui.dialogs.extensions_updater import ExtensionsUpdater
+from pathlib import Path
 
 from karabogui import icons
 
@@ -17,15 +12,11 @@ class UpdateDialog(QDialog):
     karabo-extensions package. The user may also update the package, where a
     pip install install call is done.
     """
-    _PKG_NAME = 'GUI_Extensions'
-    _WHEEL_TEMPLATE = 'GUI_Extensions-{}-py3-none-any.whl'
-    _REMOTE_SVR = 'http://exflserv05.desy.de/karabo/karaboExtensions/tags/'
-
-    UNDEFINED_VERSION = 'Undefined'
-
     def __init__(self, parent=None):
         super().__init__(parent)
         uic.loadUi(Path(__file__).parent.joinpath('update_dialog.ui'), self)
+
+        self.updater = ExtensionsUpdater(self)
 
         self.lb_current.setText(self.UNDEFINED_VERSION)
         self.lb_latest.setText(self.UNDEFINED_VERSION)
@@ -59,11 +50,7 @@ class UpdateDialog(QDialog):
         self.pb_current.setVisible(True)
         self.lb_current.setVisible(False)
 
-        try:
-            package = pkg_resources.get_distribution('GUI-Extensions')
-            current_version = package.version
-        except pkg_resources.DistributionNotFound:
-            current_version = self.UNDEFINED_VERSION
+        current_version = self.updater.get_current_version()
 
         self.pb_current.setVisible(False)
         self.lb_current.setVisible(True)
@@ -74,33 +61,11 @@ class UpdateDialog(QDialog):
         self.pb_latest.setVisible(True)
         self.lb_latest.setVisible(False)
 
-        # Request and parse the current tag list
-        result = requests.get(self._REMOTE_SVR)
-        html = result.content.decode()
-
-        if not html:
-            return self.UNDEFINED_VERSION
-
-        table = etree.HTML(html).find('body/table')
-        if not table:
-            return self.UNDEFINED_VERSION
-
-        tag_regex = re.compile('^\d+\.\d+\.\d+$')
-
-        tags = []
-        for href in table.xpath('//a/@href'):
-            tag = href.strip('/')
-            if tag_regex.match(tag):
-                tags.append(tag)
-
-        if not tags:
-            return self.UNDEFINED_VERSION
-
-        latest_tag = tags[-1]
+        latest_version = self.updater.get_latest_version()
 
         self.pb_latest.setVisible(False)
         self.lb_latest.setVisible(True)
-        self.lb_latest.setText(latest_tag)
+        self.lb_latest.setText(latest_version)
 
     def _on_update_clicked(self):
         """Updates guiextensions to the latest tag"""
@@ -108,30 +73,10 @@ class UpdateDialog(QDialog):
         self.bt_update.setEnabled(False)
         self.bt_update.setText('Updating...')
 
-        tag = self.lb_latest.text()
+        self.updater.update_to_latest(self.lb_latest.text())
 
-        wheel_file = self._WHEEL_TEMPLATE.format(tag)
-        wheel_path = '{}{}/{}'.format(self._REMOTE_SVR,
-                                      tag,
-                                      wheel_file)
+        self.bt_update.setText('Update')
+        self.bt_refresh.setEnabled(True)
+        self.bt_update.setEnabled(True)
 
-        with requests.get(wheel_path, stream=True) as wheel_request:
-            if not wheel_request.status_code == requests.codes.ok:
-                return
-
-            temp_file = Path(wheel_file)
-            temp_file.write_bytes(wheel_request.content)
-
-            # Install it using a system call
-            process = QProcess(self)
-            process.start('pip install --upgrade {}'.format(temp_file))
-            process.waitForFinished()
-
-            self.bt_update.setText('Update')
-            self.bt_refresh.setEnabled(True)
-            self.bt_update.setEnabled(True)
-
-            # Remove downloaded wheel
-            os.remove(temp_file)
-
-            self.refresh_versions()
+        self.refresh_versions()
