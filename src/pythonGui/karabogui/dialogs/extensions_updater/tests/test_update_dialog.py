903e3190f302c075e9af9830524b228d8fc665fe
Denivy Ruck <denivyruck1@gmail.com>
2019-02-13 12:29:05 +0100

    Add tests for the dialog and model

---
+++ src/pythonGui/karabogui/dialogs/extensions_updater/tests/test_update_dialog.py
@@ -0,0 +1,99 @@
+import requests
+from contextlib import contextmanager
+from pathlib import Path
+from unittest import mock, skip
+from PyQt4.QtCore import QProcess
+
+from karabogui.dialogs.extensions_updater import (
+    ExtensionsUpdater, UpdateDialog)
+from karabogui.testing import GuiTestCase
+
+
+class TestCase(GuiTestCase):
+
+    @skip(reason='Cannot test dialogs in the current Qt version. Should be '
+                 'tested and reviewed after PyQt5')
+    def test_extensions_dialog(self):
+        dialog = UpdateDialog()
+        updater = dialog.updater
+
+        self.assertTrue(dialog.bt_update.isDisabled())
+        self.assertEqual(dialog.lb_current.text(), updater.UNDEFINED_VERSION)
+        self.assertEqual(dialog.lb_latest.text(), updater.UNDEFINED_VERSION)
+
+        with mock.patch.object(
+                updater, 'get_current_version',
+                return_value='1'):
+            with mock.patch.object(
+                    updater, 'get_latest_version',
+                    return_value='2'):
+                self.click(dialog.bt_refresh)
+
+                # Should refresh current and latest versions
+                self.assertEqual(dialog.lb_current.text(), '1')
+                self.assertEqual(dialog.lb_latest.text(), '2')
+
+                self.assertTrue(dialog.bt_update.isEnabled())
+
+    def test_latest_version(self):
+        """Tests the model of the extension updater"""
+        updater = ExtensionsUpdater()
+
+        html = Path(__file__).parent.joinpath('test_html.html').read_text()
+
+        with mock.patch.object(updater,
+                               '_retrieve_remote_html',
+                               return_value=html):
+            latest_version = updater.get_latest_version()
+            assert latest_version == '2.3.4'
+
+    def _create_get_mock(self, content, status_code):
+        """Used to mock a requests.get method call with a fake content
+        and status code"""
+        class MockResponse:
+            def __init__(self, content, status):
+                self.content = content
+                self.status_code = status
+
+        @contextmanager
+        def _mock_response(*args, **kwargs):
+            response = MockResponse(content, status_code)
+            yield response
+
+        return _mock_response
+
+    @mock.patch.object(requests, 'get')
+    @mock.patch.object(QProcess, 'start')
+    def test_outgoing_messages(self, start_mock, get_mock):
+        """Tests if the outgoing messages are called with the right
+        parameters"""
+        expected_wheel = 'http://exflserv05.desy.de/karabo/karaboExtensions' \
+                         '/tags/0.0.0/GUI_Extensions-0.0.0-py3-none-any.whl'
+        expected_cmd = 'pip install --upgrade ' \
+                       'GUI_Extensions-0.0.0-py3-none-any.whl'
+
+        updater = ExtensionsUpdater()
+
+        get_mock.side_effect = self._create_get_mock(b'', requests.codes.ok)
+        updater.update_to_latest('0.0.0')
+
+        # Assert outgoing messages
+        get_mock.assert_called_once_with(
+            expected_wheel,
+            stream=True)
+
+        assert start_mock.call_count == 1
+        start_mock.assert_called_with(expected_cmd)
+
+        # Emulate a <not ok> get request
+        get_mock.side_effect = self._create_get_mock(b'',
+                                                     requests.codes.not_found)
+        updater.update_to_latest('0.0.0')
+
+        assert get_mock.call_count == 2
+        get_mock.assert_called_with(
+            expected_wheel,
+            stream=True)
+
+        # Not called again as the wheel couldn't be retrieved
+        assert start_mock.call_count == 1
