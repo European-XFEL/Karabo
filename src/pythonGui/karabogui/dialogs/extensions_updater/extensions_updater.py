bb951cbc73ed2dc407c7271b7befa54355d5c184
Denivy Ruck <denivyruck1@gmail.com>
2019-02-14 10:47:12 +0100

    Remove redundant class definition

--- src/pythonGui/karabogui/dialogs/extensions_updater/extensions_updater.py
+++ src/pythonGui/karabogui/dialogs/extensions_updater/extensions_updater.py
@@ -6,71 +6,72 @@ import pkg_resources
 import re
 import requests
 
+_TAG_REGEX = '^\d+\.\d+\.\d+$'
+_PKG_NAME = 'GUI_Extensions'
+_WHEEL_TEMPLATE = 'GUI_Extensions-{}-py3-none-any.whl'
+_REMOTE_SVR = 'http://exflserv05.desy.de/karabo/karaboExtensions/tags/'
 
-class ExtensionsUpdater:
+UNDEFINED_VERSION = 'Undefined'
 
-    _TAG_REGEX = '^\d+\.\d+\.\d+$'
-    _PKG_NAME = 'GUI_Extensions'
-    _WHEEL_TEMPLATE = 'GUI_Extensions-{}-py3-none-any.whl'
-    _REMOTE_SVR = 'http://exflserv05.desy.de/karabo/karaboExtensions/tags/'
 
-    UNDEFINED_VERSION = 'Undefined'
+def get_current_version():
+    """Gets the current version of the package"""
+    try:
+        package = pkg_resources.get_distribution('GUI-Extensions')
+        return package.version
+    except pkg_resources.DistributionNotFound:
+        return UNDEFINED_VERSION
 
-    def get_current_version(self):
-        """Gets the current version of the package"""
-        try:
-            package = pkg_resources.get_distribution('GUI-Extensions')
-            return package.version
-        except pkg_resources.DistributionNotFound:
-            return self.UNDEFINED_VERSION
 
-    def _retrieve_remote_html(self):
-        """Retrieves the remote tag url html and decodes it"""
-        result = requests.get(self._REMOTE_SVR)
-        html = result.content.decode()
+def _retrieve_remote_html():
+    """Retrieves the remote tag url html and decodes it"""
+    result = requests.get(_REMOTE_SVR)
+    html = result.content.decode()
 
-        return html
+    return html
 
-    def get_latest_version(self):
-        """Gets the latest version of the package"""
-        html = self._retrieve_remote_html()
 
-        if not html:
-            return self.UNDEFINED_VERSION
+def get_latest_version():
+    """Gets the latest version of the package"""
+    html = _retrieve_remote_html()
 
-        table = etree.HTML(html).find('body/table')
-        if not table:
-            return self.UNDEFINED_VERSION
+    if not html:
+        return UNDEFINED_VERSION
 
-        # Match entries of the type N.N.N
-        tag_regex = re.compile(self._TAG_REGEX)
+    table = etree.HTML(html).find('body/table')
+    if not table:
+        return UNDEFINED_VERSION
 
-        # All the tags are in an href tag
-        for href in reversed(table.xpath('//a/@href')):
-            tag = href.strip('/')
-            if tag_regex.match(tag):
-                return tag
+    # Match entries of the type N.N.N
+    tag_regex = re.compile(_TAG_REGEX)
 
-        return self.UNDEFINED_VERSION
+    # All the tags are in an href tag
+    for href in reversed(table.xpath('//a/@href')):
+        tag = href.strip('/')
+        if tag_regex.match(tag):
+            return tag
 
-    @contextmanager
-    def download_file_for_tag(self, tag):
-        """Downloads the wheel for the given tag and writes it to a file,
-        removing it when the context is finished."""
-        wheel_file = self._WHEEL_TEMPLATE.format(tag)
-        wheel_path = '{}{}/{}'.format(self._REMOTE_SVR,
-                                      tag,
-                                      wheel_file)
+    return UNDEFINED_VERSION
 
-        with requests.get(wheel_path, stream=True) as wheel_request:
-            if not wheel_request.status_code == requests.codes.ok:
-                yield None
-                return
 
-            temp_file = Path(wheel_file)
-            temp_file.write_bytes(wheel_request.content)
+@contextmanager
+def download_file_for_tag(tag):
+    """Downloads the wheel for the given tag and writes it to a file,
+    removing it when the context is finished."""
+    wheel_file = _WHEEL_TEMPLATE.format(tag)
+    wheel_path = '{}{}/{}'.format(_REMOTE_SVR,
+                                  tag,
+                                  wheel_file)
 
-            yield temp_file
+    with requests.get(wheel_path, stream=True) as wheel_request:
+        if not wheel_request.status_code == requests.codes.ok:
+            yield None
+            return
 
-            # Remove downloaded wheel
-            os.remove(temp_file)
+        temp_file = Path(wheel_file)
+        temp_file.write_bytes(wheel_request.content)
+
+        yield temp_file
+
+        # Remove downloaded wheel
+        os.remove(temp_file)
