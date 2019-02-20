6077e1150132291726bb2941d9579195f0b3ae6e
Istvan Mohacsi <mohacsii@exflqr23727.desy.de>
2019-02-20 11:31:59 +0100

    Fresh cleanup round 2

--- src/pythonKarabo/karabo/bound_api/ok_error_fsm.py
+++ src/pythonKarabo/karabo/bound_api/ok_error_fsm.py
@@ -1,8 +1,8 @@
 # To change this template, choose Tools | Templates
 # and open the template in the editor.
 
-__author__="Sergey Esenov <serguei.essenov at xfel.eu>"
-__date__ ="$May 12, 2013 5:09:05 PM$"
+__author__ = "Sergey Esenov <serguei.essenov at xfel.eu>"
+__date__ = "$May 12, 2013 5:09:05 PM$"
 
 from karathon import SLOT_ELEMENT
 
@@ -18,79 +18,75 @@ from .fsm import (
 
 @KARABO_CLASSINFO("OkErrorFsm", "1.0")
 class OkErrorFsm(base.BaseFsm):
-    
+
     @staticmethod
     def expectedParameters(expected):
-        
         e = SLOT_ELEMENT(expected).key("reset")
-        e.displayedName("Reset").description("Resets the device in case of an error")
+        e.displayedName("Reset").description(
+            "Resets the device in case of an error")
         e.allowedStates(State.ERROR)
         e.commit()
 
     def __init__(self, configuration):
         super(OkErrorFsm, self).__init__(configuration)
-        
-        #**************************************************************
-        #*                        Events                              *
-        #**************************************************************
+
+        # **************************************************************
+        # *                        Events                              *
+        # **************************************************************
         KARABO_FSM_EVENT2(self, 'ErrorFoundEvent', 'errorFound')
         KARABO_FSM_EVENT0(self, 'ResetEvent', 'reset')
 
-        #**************************************************************
-        #*                        States                              *
-        #**************************************************************
+        # **************************************************************
+        # *                        States                              *
+        # **************************************************************
         KARABO_FSM_STATE_EE(State.ERROR, self.errorStateOnEntry,
                             self.errorStateOnExit)
         KARABO_FSM_STATE_EE(State.NORMAL, self.okStateOnEntry,
                             self.okStateOnExit)
 
-        #**************************************************************
-        #*                    Transition Actions                      *
-        #**************************************************************
-        #KARABO_FSM_NO_TRANSITION_ACTION(self.noStateTransition)    # <-- this is defined in BaseFsm
+        # **************************************************************
+        # *                    Transition Actions                      *
+        # **************************************************************
+        # This is defined in BaseFsm
+        # KARABO_FSM_NO_TRANSITION_ACTION(self.noStateTransition)
         KARABO_FSM_ACTION2('ErrorFoundAction', self.errorFoundAction, str, str)
         KARABO_FSM_ACTION0('ResetAction', self.resetAction)
 
-        #**************************************************************
-        #*                      Top Machine                           *
-        #**************************************************************
+        # **************************************************************
+        # *                      Top Machine                           *
+        # **************************************************************
 
         stateMachineTransitionTable = [
             (State.NORMAL, 'ErrorFoundEvent', State.ERROR,
-                'ErrorFoundAction', 'none'),
+             'ErrorFoundAction', 'none'),
             (State.ERROR, 'ResetEvent', State.NORMAL,
-                'ResetAction', 'none')]
+             'ResetAction', 'none')]
 
         KARABO_FSM_STATE_MACHINE('StateMachine', stateMachineTransitionTable,
                                  State.NORMAL)
         self.fsm = KARABO_FSM_CREATE_MACHINE('StateMachine')
-    
+
     def getFsm(self):
         return self.fsm
-    
+
     def initFsmSlots(self, sigslot):
         sigslot.registerSlot(self.reset)
         sigslot.registerSlot(self.errorFound)
-        
+
     def errorStateOnEntry(self):
-        '''Actions executed on entry to 'Error' state
-        '''
-        
+        """Actions executed on entry to 'Error' state"""
+
     def errorStateOnExit(self):
-        '''Actions executed on exit from 'Error' state
-        '''
-    
+        """Actions executed on exit from 'Error' state"""
+
     def errorFoundAction(self, m1, m2):
         print("***", m1, m2)
-        
+
     def okStateOnEntry(self):
-        '''Actions executed on entry to 'Ok' state
-        '''
-        
+        """Actions executed on entry to 'Ok' state"""
+
     def okStateOnExit(self):
-        '''Actions executed on exit from 'Ok' state
-        '''
+        """Actions executed on exit from 'Ok' state"""
 
     def resetAction(self):
-        '''Reset action on the way to 'Ok' dtate
-        '''
+        """Reset action on the way to 'Ok' dtate"""
