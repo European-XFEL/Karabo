51300c31e8b782f9e261d5d97a647d693d95eff8
degon <dennis.goeries@xfel.eu>
2019-02-25 13:18:16 +0100

    GUI: Remove wrong parent setting for message box in manager

--- src/pythonGui/karabogui/singletons/manager.py
+++ src/pythonGui/karabogui/singletons/manager.py
@@ -200,7 +200,7 @@ class Manager(QObject):
         success = info.get('success', False)
         if not success:
             reason = info.get('reason')
-            messagebox.show_error(reason, parent=self)
+            messagebox.show_error(reason)
             return
         deviceId = info.get('deviceId')
         config = info.get('config')
