7997cd0e44fe719cd0048355e33051c3941ed76a
degon <dennis.goeries@xfel.eu>
2019-02-27 19:06:37 +0100

    GUI: Add double wheelbox to the controllers

--- src/pythonKarabo/karabo/common/scenemodel/tests/test_widgets_simple.py
+++ src/pythonKarabo/karabo/common/scenemodel/tests/test_widgets_simple.py
@@ -110,6 +110,17 @@ def test_display_command():
     assert read_model.requires_confirmation
 
 
+def test_edit_doublewheel():
+    traits = _geometry_traits()
+    traits['integers'] = 2
+    traits['decimals'] = 4
+    model = api.DoubleWheelBoxModel(**traits)
+    read_model = single_model_round_trip(model)
+    _assert_geometry_traits(read_model)
+    assert read_model.integers == 2
+    assert read_model.decimals == 4
+
+
 def test_scene_link_model():
     traits = _geometry_traits()
     traits['target'] = 'other.svg'
