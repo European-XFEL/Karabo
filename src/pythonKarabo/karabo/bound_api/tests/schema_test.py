e88448887726c0249aa66c5585c5661925f92827
Cyril Danilevski <cyril.danilevski@desy.de>
2019-03-18 17:17:52 +0100

    Fix scope of schema subtraction
    Added test to ensure that schema parsing check is correct.

--- src/pythonKarabo/karabo/bound_api/tests/schema_test.py
+++ src/pythonKarabo/karabo/bound_api/tests/schema_test.py
@@ -6,7 +6,7 @@ from karabo.bound import (
     AccessLevel, AccessType, ArchivePolicy, AssemblyRules, AssignmentType,
     Hash, Logger, MetricPrefix, NodeType, Schema, Types, Unit, Validator,
     AlarmCondition, DaqDataType, State, IMAGEDATA_ELEMENT, INT32_ELEMENT,
-    NDARRAY_ELEMENT, OVERWRITE_ELEMENT, PythonDevice,
+    NDARRAY_ELEMENT, OVERWRITE_ELEMENT,
 )
 from .configuration_example_classes import (
     Base, GraphicsRenderer, GraphicsRenderer1, GraphicsRenderer2, SomeClass,
@@ -1043,93 +1043,6 @@ class Schema_TestCase(unittest.TestCase):
         # Only (custom) nodes can have allowed actions:
         with self.assertRaises(RuntimeError):
             s.setAllowedActions("node.int", ["bla", "blue"])
-
-    def test_schemaInjection(self):
-        device = Configurator(PythonDevice).create(
-                        "PythonDevice", Hash())
-        device.startFsm()
-
-        # Test appendSchema appends
-        schema = Schema()
-        (
-            INT32_ELEMENT(schema).key("injectedInt32")
-            .assignmentOptional().defaultValue(1)
-            .reconfigurable()
-            .commit()
-        )
-
-        device.appendSchema(schema)
-        device.parameters.set("injectedInt32", 5)
-        self.assertIn("injectedInt32", device.parameters.getPaths())
-        self.assertEqual(device.parameters.get("injectedInt32"), 5)
-
-        # Test that injecting a new attribute keeps the set value
-        schema = Schema()
-        (
-            INT32_ELEMENT(schema).key("injectedInt32")
-            .assignmentOptional().defaultValue(2)
-            .reconfigurable().minInc(1)
-            .commit()
-        )
-
-        device.appendSchema(schema)
-        self.assertIn("injectedInt32", device.parameters.getPaths())
-        self.assertEqual(device.parameters.get("injectedInt32"), 5)
-        self.assertEqual(device.fullSchema.getMinInc("injectedInt32"), 1)
-
-        # Test that doing updateSchema keeps previously set value
-        schema = Schema()
-        (
-            INT32_ELEMENT(schema).key("injectedInt32")
-            .assignmentOptional().defaultValue(3)
-            .reconfigurable().minInc(2).maxInc(10)
-            .commit()
-        )
-
-        device.updateSchema(schema)
-        self.assertIn("injectedInt32", device.parameters.getPaths())
-        self.assertEqual(device.parameters.get("injectedInt32"), 5)
-        self.assertEqual(device.fullSchema.getMinInc("injectedInt32"), 2)
-        self.assertEqual(device.fullSchema.getMaxInc("injectedInt32"), 10)
-
-        # Test that doing updateSchema with something else loses injectedInt32
-        schema = Schema()
-        (
-            INT32_ELEMENT(schema).key("somethingElse")
-            .assignmentOptional().defaultValue(4)
-            .reconfigurable()
-            .commit()
-        )
-
-        device.updateSchema(schema)
-        self.assertNotIn("injectedInt32", device.parameters.getPaths())
-        self.assertIn("somethingElse", device.parameters.getPaths())
-
-        # Test that doing updateSchema with an empty schema reset the device to
-        # its base schema
-        schema = Schema()
-        device.updateSchema(schema)
-
-        self.assertNotIn("somethingElse", device.parameters.getPaths())
-        self.assertEqual(device._injectedSchema.getPaths(), Schema().getPaths())
-        self.assertEqual(device.fullSchema.getPaths(), device.staticSchema.getPaths())
-
-        # Test that appending several times in a row, quickly, set all values
-        for idx in range(10):
-            schema = Schema()
-            (
-                INT32_ELEMENT(schema).key(f"property{idx}")
-                .assignmentOptional().defaultValue(idx)
-                .reconfigurable()
-                .commit()
-            )
-            device.appendSchema(schema)
-
-        for idx in range(10):
-            key = f"property{idx}"
-            self.assertIn(key, device.parameters.getPaths())
-            self.assertIn(key, device.fullSchema.getPaths())
-            self.assertEqual(idx, device.parameters.get(key))
 
 
 if __name__ == '__main__':
