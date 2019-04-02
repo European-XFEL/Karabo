ebce0353c21cda53d0cec3cb5c6c29fee0ba8d76
Gero Flucke <gero.flucke@xfel.eu>
2019-04-02 12:45:31 +0200

    Fix device client used for multiple Python integration tests

--- src/pythonKarabo/karabo/integration_tests/utils.py
+++ src/pythonKarabo/karabo/integration_tests/utils.py
@@ -26,7 +26,7 @@ def start_bound_server(server_id, args, plugin_dir=''):
 
 def start_cpp_server(server_id, args, plugin_dir=''):
     """
-    Start a C++ device server with givenargs in its own process.
+    Start a C++ device server with given args in its own process.
     Ignore plugin_dir so far
     """
     env = os.environ.copy()  # needed?
@@ -110,8 +110,13 @@ class BoundDeviceTestCase(TestCase):
         self._eventLoopThread.daemon = True
         self._eventLoopThread.start()
 
-        if self.dc is None:
-            self.dc = DeviceClient()
+        # Looks like testing framework parses the class scope again and
+        # again for each test, so self.dc mightbe None for the second
+        # test, but the first client is not yet properly destructed
+        # (as Python does not offer any control for that :-().
+        # But that is fine, we can live witha single client for everybody.
+        if BoundDeviceTestCase.dc is None:
+            BoundDeviceTestCase.dc = DeviceClient()
 
     def tearDown(self):
         # Stop the servers
