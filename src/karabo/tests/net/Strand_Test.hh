77fdecccc036c7a51492100a9abcffe9619c8cc0
Raul Costa Junior <raul.costa@xfel.eu>
2019-04-15 09:27:57 +0200

    Reestablishing net unit tests.
    Tests had been temporarily disabled to accelerate test cycle while developing writeAsync unit tests in the same project.

--- src/karabo/tests/net/Strand_Test.hh
+++ src/karabo/tests/net/Strand_Test.hh
@@ -16,10 +16,8 @@
 class Strand_Test : public CPPUNIT_NS::TestFixture {
 
     CPPUNIT_TEST_SUITE(Strand_Test);
-    /*
     CPPUNIT_TEST(testSequential);
     CPPUNIT_TEST(testStrandDies);
-    */
     CPPUNIT_TEST_SUITE_END();
 
 public:
