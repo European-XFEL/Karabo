764657ea3f8f5c380869be3897c5b1970a1b1f52
Istvan Mohacsi <mohacsii@exflqr23727.desy.de>
2019-04-23 12:12:56 +0200

    Removed old blacklist

--- src/pythonKarabo/karabo/interactive/tests/test_code_quality.py
+++ src/pythonKarabo/karabo/interactive/tests/test_code_quality.py
@@ -1,34 +1,13 @@
 import os
 import subprocess
 
-import karabo.interactive as interactive
 
-
-BLACKLIST = ['__init__.py', 'convert_device_project.py']
-WHITELIST = ['__init__.py','convert_device_project.py', 'deviceClient.py',
+FILE_LIST = ['__init__.py','convert_device_project.py', 'deviceClient.py',
              'ideviceclient.py','ikarabo.py', 'jupyter_kernel.py', 'karabo.py',
              'scene2python.py', 'startkarabo.py', 'webserver.py']
 
 
-
-def get_python_files():
-    """Get all python files from this package"""
-    common_dir = os.path.abspath(os.path.dirname(interactive.__file__))
-    flake_check = []
-    for dirpath, _, filenames in os.walk(common_dir):
-        if dirpath.endswith('tests'):
-            continue
-        for fn in filenames:
-            if os.path.splitext(fn)[-1].lower() == '.py' and \
-                    fn not in BLACKLIST:
-                path = os.path.join(dirpath, fn)
-                flake_check.append(path)
-
-    return flake_check
-
-
 def test_code_quality_flake8():
-    files = WHITELIST
-    for py_file in files:
+    for py_file in FILE_LIST:
         command = ['flake8', os.path.abspath(py_file)]
         subprocess.check_call(command)
