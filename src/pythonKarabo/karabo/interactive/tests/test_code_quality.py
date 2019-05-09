a07b2ae2a20bc4e217d23c576e7666fd40cca323
Istvan Mohacsi <mohacsii@exflqr23727.desy.de>
2019-05-09 11:05:21 +0200

    Updated flake runner syntax

--- src/pythonKarabo/karabo/interactive/tests/test_code_quality.py
+++ src/pythonKarabo/karabo/interactive/tests/test_code_quality.py
@@ -2,12 +2,11 @@ import os
 import subprocess
 
 
-FILE_LIST = ['__init__.py','convert_device_project.py', 'deviceClient.py',
-             'ideviceclient.py','ikarabo.py', 'jupyter_kernel.py', 'karabo.py',
-             'scene2python.py', 'startkarabo.py', 'webserver.py']
+FILE_LIST = ['__init__.py', 'convert_device_project.py', 'deviceClient.py',
+             'ideviceclient.py', 'ikarabo.py', 'jupyter_kernel.py',
+             'karabo.py', 'scene2python.py', 'startkarabo.py', 'webserver.py']
 
 
 def test_code_quality_flake8():
-    for py_file in FILE_LIST:
-        command = ['flake8', os.path.abspath(py_file)]
-        subprocess.check_call(command)
+    command = ['flake8', *[os.path.abspath(py_file) for py_file in FILE_LIST]]
+    subprocess.check_call(command)
