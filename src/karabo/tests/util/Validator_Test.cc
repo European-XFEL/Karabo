daf5cafb109888176d946cd4d1e1f3c50423b9da
Raul Costa Junior <raul.costa@xfel.eu>
2019-05-21 10:09:45 +0200

    Added checking of number and names of columns for table elements.

--- src/karabo/tests/util/Validator_Test.cc
+++ src/karabo/tests/util/Validator_Test.cc
@@ -20,6 +20,7 @@
 using namespace karabo;
 using util::TABLE_ELEMENT;
 using util::INT32_ELEMENT;
+using util::STRING_ELEMENT;
 
 CPPUNIT_TEST_SUITE_REGISTRATION(Validator_Test);
 
@@ -47,6 +48,10 @@ void Validator_Test::testTableDefault() {
     util::Schema rowSchema;
     INT32_ELEMENT(rowSchema).key("int")
             .assignmentOptional().defaultValue(1)
+            .readOnly()
+            .commit();
+    STRING_ELEMENT(rowSchema).key("str")
+            .assignmentOptional().defaultValue("a string")
             .readOnly()
             .commit();
 
@@ -81,12 +86,26 @@ void Validator_Test::testTableDefault() {
     validated.clear();
 
     // Test to accept a table with fitting row.
-    res = validator.validate(schema, util::Hash("table", std::vector<util::Hash>(1, util::Hash("int", -2))), validated);
+    res = validator.validate(schema,
+                             util::Hash("table", std::vector<util::Hash>(1, util::Hash("int", -2, "str", "testing"))),
+                             validated);
 
     CPPUNIT_ASSERT(res.first);
     CPPUNIT_ASSERT(validated.has("table"));
     CPPUNIT_ASSERT(validated.is<std::vector<util::Hash> >("table"));
     CPPUNIT_ASSERT(1u == validated.get<std::vector<util::Hash> >("table").size());
     CPPUNIT_ASSERT_EQUAL(-2, validated.get<std::vector<util::Hash> >("table")[0].get<int>("int"));
+
+    // Test to refuse a table with a bad row - wrong number of columns.
+    res = validator.validate(schema,
+                             util::Hash("table", std::vector<util::Hash>(1, util::Hash("int", -2))),
+                             validated);
+    CPPUNIT_ASSERT(!res.first);
+
+    // Test to refuse a table with a bad row - bad column name.
+    res = validator.validate(schema,
+                             util::Hash("table", std::vector<util::Hash>(1, util::Hash("unknownKey", 123, "str", "testing"))),
+                             validated);
+    CPPUNIT_ASSERT(!res.first);
 }
 
