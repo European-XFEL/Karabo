d1e1c75238486881135fa515dcaa57effbd91be3
degon <dennis.goeries@xfel.eu>
2019-05-22 18:05:31 +0200

    Extract image data from Hash

--- src/pythonKarabo/karabo/native/data/utils.py
+++ src/pythonKarabo/karabo/native/data/utils.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from .hash import Type
+from .hash import Hash, Type
 
 
 def dtype_from_number(number):
@@ -11,3 +11,22 @@ def dtype_from_number(number):
     >> dtype('int64')
     """
     return np.dtype(Type.types[number].numpy)
+
+
+def get_image_data(data):
+    assert isinstance(data, Hash)
+
+    def extract_data(hsh):
+        hsh = hsh["pixels"]
+        dtype = dtype_from_number(hsh["type"])
+        dtype = dtype.newbyteorder(">" if hsh["isBigEndian"] else "<")
+        array = np.frombuffer(hsh["data"], count=hsh["shape"].prod(),
+                              dtype=dtype)
+        array.shape = hsh["shape"]
+        return array
+
+    if "image" in data:
+        return extract_data(data["image"])
+
+    elif "data.image" in data:
+        return extract_data(data["data.image"])
