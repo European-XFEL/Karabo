477ec3047dfe6924751dc675c58a24c97b3a1bf7
Gero Flucke <gero.flucke@xfel.eu>
2019-05-24 18:50:06 +0200

    Stream line stress test, add one for receiveAsync

--- src/cppLongTests/xms/SignalSlotable_LongTest.cc
+++ src/cppLongTests/xms/SignalSlotable_LongTest.cc
@@ -10,12 +10,13 @@
 #include "SignalSlotable_LongTest.hh"
 
 #include "karabo/xms/SignalSlotable.hh"
-#include "karabo/util/Hash.hh"
+#include "karabo/util/StringTools.hh"
 
-#include "boost/thread/mutex.hpp"
 #include "boost/shared_ptr.hpp"
 
 #include <string>
+#include <chrono>
+#include <iostream>
 #include <future>
 
 using namespace karabo::util;
@@ -44,7 +45,7 @@ void SignalSlotable_LongTest::tearDown() {
 }
 
 
-void SignalSlotable_LongTest::testStressReplies() {
+void SignalSlotable_LongTest::testStressSyncReplies() {
 
     auto instance = boost::make_shared<SignalSlotable>("instance");
     instance->start();
@@ -57,25 +58,72 @@ void SignalSlotable_LongTest::testStressReplies() {
 
 
     // High number of iterations:
-    // Saw it succeeding > 1000000 successful trials at times when a mutex was missing in SignalSlotable!
-    const size_t numIterations(30000);
-    std::clog << "Long test starting (till " << numIterations << "): " << std::flush;
+    const size_t numIterations(1000000); // saw failures after > 500,000 successes before a mutex bug was fixed...
+    std::clog << "Long testStressSyncReplies starting (till " << numIterations << "): " << std::flush;
+    const auto testStartTime = std::chrono::high_resolution_clock::now();
+
     size_t sentRequests(0); // Count the number of sent requests
     size_t receivedReplies(0);
     for (size_t counter(0); counter < numIterations; ++counter) {
-        if (++sentRequests % 100000 == 0) { // 100000 seem to take about 15 seconds
+        if (++sentRequests % 100000 == 0) {
             std::clog << sentRequests << " ";
         }
         // Our slot functions do not place any answers, so an empty one will be added.
-        try {
-            // Self messaging...
-            instance->request("", "slot").timeout(10000).receive();
-            ++receivedReplies;
-        } catch (const TimeoutException&) {
-            CPPUNIT_ASSERT_MESSAGE("Lost synchronous reply #" + toString(sentRequests), false);
-        }
+        // We do self messaging...
+        CPPUNIT_ASSERT_NO_THROW_MESSAGE("Lost synchronous reply #" + toString(sentRequests),
+                                        instance->request("", "slot").timeout(1000).receive());
+        ++receivedReplies;
     }
     std::clog << std::endl;
     CPPUNIT_ASSERT_EQUAL(sentRequests, firstCalledCounter.load());
     CPPUNIT_ASSERT_EQUAL(sentRequests, receivedReplies);
+
+    auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - testStartTime).count();
+    std::clog << "Test duration: " << ms << " ms, i.e. " << numIterations / static_cast<float> (ms)
+            << " request-receive per ms" << std::endl;
+}
+
+
+void SignalSlotable_LongTest::testStressAsyncReplies() {
+
+    auto instance = boost::make_shared<SignalSlotable>("instance");
+    instance->start();
+
+    auto slot = [&instance](unsigned int counter) {
+        instance->reply(counter);
+    };
+    instance->registerSlot<unsigned int>(slot, "slot");
+
+    std::promise <unsigned int> promise;
+    unsigned int counter = 0;
+    boost::function<void(bool, unsigned int) > handler = [&instance, &promise, &counter, &handler](bool failure, unsigned int countdown) {
+        ++counter;
+        if (failure || --countdown == 0) {
+            promise.set_value(countdown);
+        } else {
+            if (counter % 100000 == 0) {
+                std::clog << counter << " ";
+            }
+            auto successHandler = boost::bind(handler, false, _1);
+            auto failureHandler = boost::bind(handler, true, countdown);
+            instance->request("", "slot", countdown).timeout(1000)
+                    .receiveAsync<unsigned int>(successHandler, failureHandler);
+        }
+    };
+
+    // Now trigger messaging
+    std::future<unsigned int> future = promise.get_future();
+    const unsigned int numIterations = 1000000;
+    std::clog << "Long testStressAsyncReplies starting (till " << numIterations << "): " << std::flush;
+    const auto testStartTime = std::chrono::high_resolution_clock::now();
+    handler(false, numIterations);
+
+    CPPUNIT_ASSERT_EQUAL(std::future_status::ready, future.wait_for(std::chrono::seconds(600)));
+    std::clog << std::endl;
+    CPPUNIT_ASSERT_EQUAL(0u, future.get());
+    CPPUNIT_ASSERT_EQUAL(numIterations, counter);
+
+    auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - testStartTime).count();
+    std::clog << "Test duration: " << ms << " ms, i.e. " << numIterations / static_cast<float> (ms)
+            << " request-receiveAsync per ms" << std::endl;
 }
