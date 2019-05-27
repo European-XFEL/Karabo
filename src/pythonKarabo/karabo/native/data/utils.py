4ee0ae43bb0ddba20fe1714fb658bbc056baf6ce
degon <dennis.goeries@xfel.eu>
2019-05-27 15:43:01 +0200

    Update image data from hash

--- src/pythonKarabo/karabo/native/data/utils.py
+++ src/pythonKarabo/karabo/native/data/utils.py
@@ -14,7 +14,16 @@ def dtype_from_number(number):
 
 
 def get_image_data(data):
-    assert isinstance(data, Hash)
+    """Method to extract an ``image`` from a Hash into a numpy array
+
+    This is a common use case when processing pipeline data (receiving from an
+    output channel)
+
+    :param data: A hash containing the image data hash
+    :returns : A numpy array containing the extracted pixel data
+    """
+    text = "Expected a Hash, but got type %s instead!" % type(data)
+    assert isinstance(data, Hash), text
 
     def extract_data(hsh):
         hsh = hsh["pixels"]
@@ -25,8 +34,8 @@ def get_image_data(data):
         array.shape = hsh["shape"]
         return array
 
-    if "image" in data:
-        return extract_data(data["image"])
-
-    elif "data.image" in data:
+    if "data.image" in data:
         return extract_data(data["data.image"])
+
+    elif "image" in data:
+        return extract_data(data["image"])
