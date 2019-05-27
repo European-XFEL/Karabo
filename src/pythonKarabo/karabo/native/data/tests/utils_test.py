dc3d1a9baae8fe1bcd88fde2ca9abaaea563255c
degon <dennis.goeries@xfel.eu>
2019-05-27 15:57:20 +0200

    MDL: Add unit test for get_image_data

--- src/pythonKarabo/karabo/native/data/tests/utils_test.py
+++ src/pythonKarabo/karabo/native/data/tests/utils_test.py
@@ -2,7 +2,7 @@ import numpy as np
 
 from unittest import TestCase, main
 
-from ..utils import dtype_from_number
+from karabo.native.data import dtype_from_number, get_image_data, Hash
 
 
 class Tests(TestCase):
@@ -17,6 +17,18 @@ class Tests(TestCase):
     def test_numpy_int64_dtype(self):
         dtype = dtype_from_number(16)
         self.assertEqual(dtype, np.int64)
+
+    def test_get_image_data(self):
+        h = Hash()
+        h['data.image.pixels'] = Hash()
+        h['data.image.pixels.data'] = np.array([[2, 4, 6], [6, 8, 10]],
+                                               np.int64)
+        h['data.image.pixels.type'] = 16
+        h['data.image.pixels.shape'] = np.array([2, 3], np.uint64)
+        h['data.image.pixels.isBigEndian'] = False
+        image = get_image_data(h)
+        np.testing.assert_array_equal(
+            image, np.array([[2, 4, 6], [6, 8, 10]], np.int64))
 
 
 if __name__ == "__main__":
