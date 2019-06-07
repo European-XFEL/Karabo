42174cf634a2a26c5025c09f1cda364ff8e14bd2
Raul Costa Junior <raul.costa@xfel.eu>
2019-06-07 11:37:04 +0200

    Removed FIXME comments from StateSignifier.

--- src/karabo/util/StateSignifier.cc
+++ src/karabo/util/StateSignifier.cc
@@ -112,7 +112,7 @@ namespace karabo {
                             m_trumpList.insert(std::find(m_trumpList.begin(), m_trumpList.end(), State::CHANGING), State::DECREASING);
                             m_trumpList.insert(std::find(m_trumpList.begin(), m_trumpList.end(), State::CHANGING), State::INCREASING);
                         }
-                    }// FIXME: Shouldn't this be executed in sequence to "complete" the list? (two sequential if's without the elses)
+                    }
                     else if (!inList(m_trumpList, State::INCREASING)) {
                         m_trumpList.insert(std::find(m_trumpList.begin(), m_trumpList.end(), State::CHANGING), State::INCREASING);
                     } else if (!inList(m_trumpList, State::DECREASING)) {
@@ -120,7 +120,6 @@ namespace karabo {
                     }
                 }
 
-                // FIXME: The same doubt on the State::CHANGING branch applies to the State::STATIC and State::KNOWN branches.
                 if (inList(m_trumpList, State::STATIC)) {
                     if (!inList(m_trumpList, State::ACTIVE) && !inList(m_trumpList, State::PASSIVE)) {
                         if (staticMoreSignificant == State::PASSIVE) {
