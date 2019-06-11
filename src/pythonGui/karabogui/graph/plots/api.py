1c0f9f904c3cf6ceafdd904f923ca9986e1ec594
degon <dennis.goeries@xfel.eu>
2019-06-11 18:23:32 +0200

    GUI: Implement graph plot package

---
+++ src/pythonGui/karabogui/graph/plots/api.py
@@ -0,0 +1,8 @@
+# flake8: noqa
+from .base import KaraboPlotView
+from .dialogs.axes_config import AxesDialog
+from .dialogs.range_config import RangeDialog
+from .dialogs.hist_config import HistogramDialog
+from .items import VectorFillGraphPlot, VectorBarGraphPlot, ScatterGraphPlot
+from .tools.target import CrossTargetController
+from .utils import get_view_range, generate_down_sample
