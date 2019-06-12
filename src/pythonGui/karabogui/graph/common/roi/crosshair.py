84b096e73cea0f02bf18060189c2efb7ae8cc029
degon <dennis.goeries@xfel.eu>
2019-06-12 14:08:04 +0200

    Imports in common

--- src/pythonGui/karabogui/graph/common/roi/crosshair.py
+++ src/pythonGui/karabogui/graph/common/roi/crosshair.py
@@ -3,8 +3,8 @@ from PyQt4.QtCore import pyqtSlot
 from PyQt4.QtGui import QPainterPathStroker, QPainterPath
 from pyqtgraph import Point
 
-from graph.common.roi.base import KaraboROI
-from graph.image.utils import float_to_string
+from karabogui.graph.common.roi.base import KaraboROI
+from .utils import float_to_string
 
 from .utils import ROI_CENTER_HTML, ImageRegion
 
