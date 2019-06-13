19b55044466d2bfbead34befd98ef2502d000f4f
Cammille Carinan <cammille.carinan@xfel.eu>
2019-06-13 20:46:51 +0200

    Remove unused get_configuration import

--- src/pythonGui/karabogui/graph/plots/base.py
+++ src/pythonGui/karabogui/graph/plots/base.py
@@ -9,7 +9,7 @@ from karabogui import icons
 
 from karabogui.graph.common.api import (
     AxesLabelsDialog, BaseROIController, get_axis_items, get_default_brush,
-    get_default_pen, get_configuration, MouseMode, KaraboLegend, KaraboToolBar,
+    get_default_pen, MouseMode, KaraboLegend, KaraboToolBar,
     KaraboViewBox, PointCanvas, ROITool, ROIToolset)
 from karabogui.graph.common.const import (
     AXIS_ITEMS, AXIS_X, AXIS_Y, DEFAULT_BAR_WIDTH,
