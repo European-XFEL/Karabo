3238f14467f47c07ad4452cc4d0b4104e89d9060
degon <dennis.goeries@xfel.eu>
2019-06-17 10:57:53 +0200

    GUI: Add a gui timeit decorator

--- src/pythonGui/karabogui/debug.py
+++ src/pythonGui/karabogui/debug.py
@@ -1,8 +1,11 @@
+from functools import wraps
 import inspect
 import os.path as op
 import pdb
+from time import perf_counter
 
-from PyQt4.QtCore import pyqtRemoveInputHook
+from PyQt4.QtGui import QApplication
+from PyQt4.QtCore import pyqtRemoveInputHook, QEventLoop
 
 
 def set_trace():
@@ -18,7 +21,7 @@ def print_stack_trace(num_frames=0):
     frames = inspect.stack()
     IGNORED = 2  # The frames including this function and inspect.stack()
     num_frames = num_frames if num_frames != 0 else (len(frames) - IGNORED)
-    frames_subset = frames[IGNORED:IGNORED+num_frames]
+    frames_subset = frames[IGNORED:IGNORED + num_frames]
     try:
         print()
         for tup in frames_subset[::-1]:
@@ -28,3 +31,22 @@ def print_stack_trace(num_frames=0):
     finally:
         del frames
         del frames_subset
+
+
+def timeit(func):
+    """A timing decorator for the GUI"""
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        # Empty the eventloop stack before!
+        QApplication.processEvents(QEventLoop.AllEvents, 10)
+        t_start = perf_counter()
+        ret = func(*args, **kwargs)
+        # But process the generated eventloop stack for the performance
+        # measurement!
+        QApplication.processEvents()
+        elapsed = perf_counter() - t_start
+        print("{} took {}".format(func.__name__, elapsed))
+        return ret
+
+    return wrapper
