45204b6339bc511ac695ec05e8fc83ce06021bba
degon <dennis.goeries@xfel.eu>
2019-06-20 16:55:46 +0200

    Length check

--- src/pythonGui/karabogui/controllers/display/tests/test_vector_hist.py
+++ src/pythonGui/karabogui/controllers/display/tests/test_vector_hist.py
@@ -25,7 +25,7 @@ class TestVectorHistGraph(GuiTestCase):
         self.controller.destroy()
         self.assertIsNone(self.controller.widget)
 
-    def test_scatter_graph_basics(self):
+    def test_hist_basics(self):
         self.controller.model.auto = False
         self.controller.model.start = 0
         self.controller.model.stop = 10
@@ -36,3 +36,13 @@ class TestVectorHistGraph(GuiTestCase):
             x, [-0.454545, 0.545455, 1.545455, 2.545455, 3.545455, 4.545455,
                 5.545455, 6.545455, 7.545455, 8.545455, 9.545455])
         np.testing.assert_array_almost_equal(y, [0, 1, 2, 4, 0, 0, 0, 0, 1, 0])
+
+    def test_hist_empty(self):
+        self.controller.model.auto = False
+        self.controller.model.start = 0
+        self.controller.model.stop = 10
+        set_proxy_value(self.proxy, 'prop', [])
+        curve = self.controller._plot
+        x, y = curve.getData()
+        self.assertIsNone(x)
+        self.assertIsNone(y)
