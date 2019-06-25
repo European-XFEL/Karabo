ba6112d5f81be53f68ed32f1a827951bc3aad915
Raul Costa Junior <raul.costa@xfel.eu>
2019-06-25 10:37:52 +0200

    Added xml serialization for schema attributes and the corresponding checkings in the test case.

--- src/karabo/util/Element.hh
+++ src/karabo/util/Element.hh
@@ -32,6 +32,7 @@ namespace karabo {
 
         // Class forward (needed to prevent assignment of Hash to Attribute)
         class Hash;
+        class Schema;
 
 #define KARABO_HASH_CLASS_ID "__classId"
 
@@ -873,7 +874,7 @@ namespace karabo {
                 case Types::VECTOR_NONE:
                     return karabo::util::toString(getValue<std::vector<CppNone> >());
                 case Types::SCHEMA:
-                    return std::string("Schema Object");
+                    return karabo::util::toString(getValue<Schema>());
                 case Types::BYTE_ARRAY: {
                     const karabo::util::ByteArray& array = getValue<karabo::util::ByteArray>();
                     const unsigned char* data = reinterpret_cast<unsigned char*> (array.first.get());
