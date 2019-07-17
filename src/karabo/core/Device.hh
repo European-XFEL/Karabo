c6fdaf64d66a14baa74941ada205028ab4284480
silenzi <alessandro.silenzi@xfel.eu>
2019-07-17 15:40:43 +0200

    FIX: Device::getAlarmCondition works
    The method Device::getAlarmCondition was neither working nor tested.
    This fixes that.

--- src/karabo/core/Device.hh
+++ src/karabo/core/Device.hh
@@ -1198,7 +1198,7 @@ namespace karabo {
              * @return
              */
             const karabo::util::AlarmCondition & getAlarmCondition() const {
-                return karabo::util::AlarmCondition::fromString(this->get<std::string>("alarmCondition"));
+                return this->get<karabo::util::AlarmCondition>("alarmCondition");
             }
 
             /**
