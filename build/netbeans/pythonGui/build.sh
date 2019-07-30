3ad12a8e43495cd6328c59b30d00a68ec35853b8
silenzi <alessandro.silenzi@xfel.eu>
2019-07-30 15:55:40 +0200

    Remove Darwin from build system
    Karabo was never supported for the Darwin architecture.
    In case it will ever be implemented, it will not be done by following the
    current deployment system.
    This removes the reference to this Platform from the karabo build process.
    The references are still present for the dependency scripts.
    
    Given the cleaning mood, I removed the serverControl directory altogether
    it was a botched plan now substituted by the ansible deployment system

--- build/netbeans/pythonGui/build.sh
+++ build/netbeans/pythonGui/build.sh
@@ -4,17 +4,10 @@ CWD=$(pwd)
 OS=$(uname -s)
 DIST=dist/$OS
 
-if [ "$OS" = "Darwin" ]; then
-    PYTHON=/opt/local/bin/python
-    KARABO=$($PYTHON -c 'import os,sys;print(os.path.realpath(sys.argv[1]))' "$1")
-    PIP=/opt/local/bin/pip
-    PIP_EXTRA_ARGS="--user"
-else
-    KARABO=$(readlink -f "$1")
-    PYTHON=$KARABO/extern/bin/python
-    PIP=$KARABO/extern/bin/pip
-    PIP_EXTRA_ARGS=
-fi
+KARABO=$(readlink -f "$1")
+PYTHON=$KARABO/extern/bin/python
+PIP=$KARABO/extern/bin/pip
+PIP_EXTRA_ARGS=
 
 # clean previous dist folder - in case of file/folders deletion/creation in original source folder
 rm -rf dist
