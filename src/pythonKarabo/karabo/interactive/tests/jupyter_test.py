e3b0b025c3ebc545d0b1cf95b0313ac8c67a99f4
silenzi <alessandro.silenzi@xfel.eu>
2019-07-31 16:46:22 +0200

    FIX: karabo.interactive tests work in develop mode
    this fixes the tests when run in develop mode.

--- src/pythonKarabo/karabo/interactive/tests/jupyter_test.py
+++ src/pythonKarabo/karabo/interactive/tests/jupyter_test.py
@@ -1,9 +1,21 @@
+import os.path as op
+import sys
 from unittest import TestCase, main
-from jupyter_client.kernelspec import get_kernel_spec
+from jupyter_client.kernelspec import (
+    get_kernel_spec, find_kernel_specs,
+    install_kernel_spec
+)
 
 
 class Tests(TestCase):
     def test_kernel(self):
+        kernels = find_kernel_specs()
+        # might be not installed if run in develop mode
+        if 'Karabo' not in kernels:
+            source_dir = op.join(
+                op.dirname(__file__), op.pardir, "jupyter_spec")
+            install_kernel_spec(
+                source_dir, kernel_name='Karabo', prefix=sys.prefix)
         get_kernel_spec("Karabo")
 
 
