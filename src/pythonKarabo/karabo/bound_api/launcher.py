62ccae43fe03c2aaa78b6aca88365acc65e1485f
silenzi <alessandro.silenzi@xfel.eu>
2019-07-31 16:51:05 +0200

    TESTS: remove the launcher.py exception
    in the run_python_tests.sh script.

--- src/pythonKarabo/karabo/bound_api/launcher.py
+++ src/pythonKarabo/karabo/bound_api/launcher.py
@@ -9,49 +9,57 @@ from pkg_resources import working_set
 from karabo.bound import (EventLoop, Hash, TextSerializerHash,
                           BinarySerializerHash)
 
-command, namespace, name = sys.argv[1:4]
 
-entrypoint = next(working_set.iter_entry_points(namespace, name))
+def main():
+    # this helper script is only used in the integration tests, hence the
+    # lack of documentation.
+    command, namespace, name = sys.argv[1:4]
 
-if command == "schema":
-    # print the schema of a device class to stdout
-    schema = entrypoint.load().getSchema(entrypoint.name)
-    h = Hash()
-    h[name] = schema
-    ser = BinarySerializerHash.create("Bin")
-    sys.stdout.buffer.write(ser.save(h))
-elif command == "run":
-    # run a device class. The configuration is read from stdin.
-    config = sys.stdin.read()
-    ser = TextSerializerHash.create("Xml")
-    config = ser.load(config)
+    entrypoint = next(working_set.iter_entry_points(namespace, name))
 
-    # in case our parent process (the device server) dies,
-    # tell linux to terminate this device process
-    PR_SET_PDEATHSIG = 1  # from linux/prctl.h
-    libc = ctypes.cdll.LoadLibrary("libc.so.6")
-    libc.prctl(PR_SET_PDEATHSIG, signal.SIGTERM)
+    if command == "schema":
+        # print the schema of a device class to stdout
+        schema = entrypoint.load().getSchema(entrypoint.name)
+        h = Hash()
+        h[name] = schema
+        ser = BinarySerializerHash.create("Bin")
+        sys.stdout.buffer.write(ser.save(h))
+    elif command == "run":
+        # run a device class. The configuration is read from stdin.
+        config = sys.stdin.read()
+        ser = TextSerializerHash.create("Xml")
+        config = ser.load(config)
 
-    # If log filename not specified, make use of device name to avoid
-    # that different processes write to the same file.
-    # TODO:
-    # "karabo.log" is default from RollingFileAppender::expectedParameters
-    # Unfortunately, the GUI sends full config, including defaults.
-    if ("Logger.file.filename" not in config
-            or config["Logger.file.filename"] == "karabo.log"):
-        deviceId = config["_deviceId_"]
-        defaultLog = "device-{}.log".format(
-            deviceId.replace(os.path.sep, "_"))
-        config["Logger.file.filename"] = defaultLog
+        # in case our parent process (the device server) dies,
+        # tell linux to terminate this device process
+        PR_SET_PDEATHSIG = 1  # from linux/prctl.h
+        libc = ctypes.cdll.LoadLibrary("libc.so.6")
+        libc.prctl(PR_SET_PDEATHSIG, signal.SIGTERM)
 
-    # Load the module containing classid so that it gets registered.
-    t = threading.Thread(target=EventLoop.work)
-    t.start()
-    try:
-        device = entrypoint.load().create(name, config)
-        device._finalizeInternalInitialization()
-    except:
-        EventLoop.stop()
-        raise
-    finally:
-        t.join()
+        # If log filename not specified, make use of device name to avoid
+        # that different processes write to the same file.
+        # TODO:
+        # "karabo.log" is default from RollingFileAppender::expectedParameters
+        # Unfortunately, the GUI sends full config, including defaults.
+        if ("Logger.file.filename" not in config
+                or config["Logger.file.filename"] == "karabo.log"):
+            deviceId = config["_deviceId_"]
+            defaultLog = "device-{}.log".format(
+                deviceId.replace(os.path.sep, "_"))
+            config["Logger.file.filename"] = defaultLog
+
+        # Load the module containing classid so that it gets registered.
+        t = threading.Thread(target=EventLoop.work)
+        t.start()
+        try:
+            device = entrypoint.load().create(name, config)
+            device._finalizeInternalInitialization()
+        except:
+            EventLoop.stop()
+            raise
+        finally:
+            t.join()
+
+
+if __name__ == "__main__":
+    main()
