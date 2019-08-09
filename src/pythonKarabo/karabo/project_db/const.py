813f8dab8067c49a30c6a8cc16deefb3bf35653f
silenzi <alessandro.silenzi@xfel.eu>
2019-08-09 16:31:24 +0200

    FIX: ProjectDB is not initialized on a local DB
    at the same time:
    - code duplication has been adressed,
    - injeciting a LOCAL domain on production servers is avoided

--- src/pythonKarabo/karabo/project_db/const.py
+++ src/pythonKarabo/karabo/project_db/const.py
@@ -1,3 +1,11 @@
 # The default password of the docker image:
 # https://git.xfel.eu/gitlab/ITDM/docker_existdb
 TESTDB_ADMIN_PASSWORD = "change_me_please"
+
+LIST_DOMAINS_QUERY = """
+    xquery version "3.0";
+    <collections>{{
+    for $c in xmldb:get-child-collections("{}")
+    return <item>{{$c}}</item>}}
+    </collections>
+    """
