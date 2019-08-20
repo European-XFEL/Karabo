ea21e83de9f495ac3fb735b97c52bd3184f618d1
Gero Flucke <gero.flucke@xfel.eu>
2019-08-20 11:06:06 +0200

    Revert "Revert "CONDA: Karabo Gui conda environment""
    This reverts commit 017018db0bb254a4dd9752e1ccacd1469ee71c15.
    To be merged again via a merge request.

--- src/pythonGui/setup.py
+++ src/pythonGui/setup.py
@@ -1,76 +1,21 @@
-import distutils.command.build
 import json
-import os.path as op
+import os
 import sys
 
-from setuptools import setup, find_packages
+from distutils.command.build import build
+from setuptools import find_packages, setup
 
-VERSION_FILE_PATH = op.join('karabogui', '_version.py')
-
-
-def _get_src_dist_version():
-    if not op.exists(VERSION_FILE_PATH):
-        return 'Unknown', '0', '0.0.0'
-
-    # must be a source distribution, use existing version file
-    try:
-        from karabogui._version import full_version as version
-        from karabogui._version import vcs_revision as vcs_rev
-        from karabogui._version import vcs_revision_count as dev_num
-    except ImportError:
-        msg = ("Unable to import vcs_revision. Try removing {} and the build "
-               "directory before building.").format(VERSION_FILE_PATH)
-        raise ImportError(msg)
-
-    return vcs_rev, dev_num, version
-
-
-def _get_version(path):
-    vcs_rev, dev_num, version = _get_src_dist_version()
-
-    if vcs_rev != 'Unknown':
-        return {'vcs_revision': vcs_rev, 'revision_count': dev_num,
-                'version': version, 'released': True}
-
-    from karabo.packaging.versioning import git_version
-    return git_version(path)
-
-
-def _write_version_py(filename=VERSION_FILE_PATH):
-    template = """\
-# THIS FILE IS GENERATED FROM SETUP.PY
-version = '{version}'
-full_version = '{full_version}'
-vcs_revision = '{vcs_revision}'
-vcs_revision_count = '{vcs_revision_count}'
-is_released = {is_released}
-"""
-    path = op.normpath(op.dirname(__file__))
-
-    vers_dict = _get_version(path)
-    version = vers_dict['version']
-    fullversion = version
-
-    if not vers_dict['released']:
-        fullversion += '.dev{0}'.format(vers_dict['revision_count'])
-
-    if not op.exists(filename):
-        with open(filename, "wt") as fp:
-            fp.write(template.format(
-                version=version, full_version=fullversion,
-                vcs_revision=vers_dict['vcs_revision'],
-                vcs_revision_count=vers_dict['revision_count'],
-                is_released=vers_dict['released']))
-
-    return fullversion
-
+CURRENT_FOLDER = os.path.dirname(os.path.realpath(__file__))
+VERSION_FILE_PATH = os.path.join(CURRENT_FOLDER, 'karabogui', '_version.py')
+ROOT_FOLDER = os.path.dirname(os.path.dirname(CURRENT_FOLDER))
 
 if __name__ == '__main__':
-    version = _write_version_py()
 
     metadata = {
         'name': 'KaraboGUI',
-        'version': version,
+        'use_scm_version': {
+            'root': ROOT_FOLDER, 'write_to': VERSION_FILE_PATH
+        },
         'author': 'Karabo Team',
         'author_email': 'karabo@xfel.eu',
         'description': 'This is the Karabo GUI',
@@ -97,16 +42,11 @@ if __name__ == '__main__':
         metadata['package_dir'] = {'karabo': "../../src/pythonKarabo/karabo"}
         metadata['packages'].extend(
             ["karabo", "karabo.common", "karabo.common.scenemodel",
-             "karabo.common.scenemodel.widgets", "karabo.common.project",
-             "karabo.interactive", "karabo.native", "karabo.native.data",
-             "karabo.native.project", "karabo.packaging",
+             "karabo.common.packaging", "karabo.common.scenemodel.widgets",
+             "karabo.common.project", "karabo.interactive", "karabo.native",
+             "karabo.native.data", "karabo.native.project", "karabo.packaging",
              "karabo.testing"]
         )
-        # Write out useful data
-        with open('VERSION', 'w') as fp:
-            fp.write(version + '\n')
-        with open('METADATA', 'w') as fp:
-            json.dump(metadata, fp)
 
     setup(
         entry_points={
@@ -119,6 +59,14 @@ if __name__ == '__main__':
             ]
         },
         # Add an alias for 'build' so we can prepare data for Windows
-        cmdclass={WINDOWS_BUILDER: distutils.command.build.build},
+        cmdclass={WINDOWS_BUILDER: build},
         **metadata
     )
+
+    # Write out useful data
+    if WINDOWS_BUILDER in sys.argv:
+        with open('VERSION', 'w') as fp:
+            from karabogui.globals import GUI_VERSION
+            fp.write(GUI_VERSION)
+        with open('METADATA', 'w') as fp:
+            json.dump(metadata, fp)
