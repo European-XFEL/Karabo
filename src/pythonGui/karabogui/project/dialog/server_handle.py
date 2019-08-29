be03bd50c89e707383ab657b869e2cc181af27f1
degon <dennis.goeries@xfel.eu>
2019-08-29 17:49:22 +0200

    GUI: Deprecate host information in the karabo project

--- src/pythonGui/karabogui/project/dialog/server_handle.py
+++ src/pythonGui/karabogui/project/dialog/server_handle.py
@@ -22,12 +22,9 @@ class ServerHandleDialog(QDialog):
                            'server_handle.ui')
         uic.loadUi(filepath, self)
 
-        avail_hosts, avail_servers = self._get_available_hosts_servers()
+        avail_servers = self.get_available_servers()
         for server_id in avail_servers:
             self.ui_server_id.addItem(server_id)
-
-        for host in avail_hosts:
-            self.ui_host.addItem(host)
 
         if model is None:
             title = 'Add server'
@@ -40,29 +37,19 @@ class ServerHandleDialog(QDialog):
             else:
                 self.ui_server_id.setCurrentIndex(index)
 
-            index = self.ui_host.findText(model.host)
-            if index < 0:
-                host_edit = self.ui_host.lineEdit()
-                host_edit.setText(model.host)
-            else:
-                self.ui_host.setCurrentIndex(index)
             self.ui_description.setPlainText(model.description)
         self.setWindowTitle(title)
 
         validator = InputValidator()
         self.ui_server_id.setValidator(validator)
-        self.ui_host.setValidator(validator)
 
         self.ui_server_id.currentIndexChanged.connect(self._update_button_box)
         self.ui_server_id.editTextChanged.connect(self._update_button_box)
-        self.ui_host.currentIndexChanged.connect(self._update_button_box)
-        self.ui_host.editTextChanged.connect(self._update_button_box)
         self._update_button_box()
 
-    def _get_available_hosts_servers(self):
-        """ Get all available hosts and servers of the `systemTopology`
+    def get_available_servers(self):
+        """ Get all available servers of the `systemTopology`
         """
-        available_hosts = set()
         available_servers = set()
 
         def visitor(node):
@@ -73,28 +60,20 @@ class ServerHandleDialog(QDialog):
             if visibility < krb_globals.GLOBAL_ACCESS_LEVEL:
                 available_servers.add(node.node_id)
 
-            host = node.attributes.get('host', '')
-            if host:
-                available_hosts.add(host)
-
         get_topology().visit_system_tree(visitor)
-        return sorted(available_hosts), sorted(available_servers)
+
+        return sorted(available_servers)
 
     @pyqtSlot()
     def _update_button_box(self):
         """Only enable Ok button, if title and configuration is set
         """
-        enabled = (len(self.ui_server_id.currentText()) and
-                   len(self.ui_host.currentText()))
+        enabled = len(self.ui_server_id.currentText())
         self.buttonBox.button(QDialogButtonBox.Ok).setEnabled(enabled)
 
     @property
     def server_id(self):
         return self.ui_server_id.currentText()
-
-    @property
-    def host(self):
-        return self.ui_host.currentText()
 
     @property
     def description(self):
