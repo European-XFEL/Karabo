03b0ba5425cb2f4a47316cc8932edeeb05d061ae
degon <dennis.goeries@xfel.eu>
2019-09-27 14:38:26 +0200

    GUI: Use Sans Serif if no Font is specified

--- src/pythonGui/karabogui/dialogs/textdialog.py
+++ src/pythonGui/karabogui/dialogs/textdialog.py
@@ -2,8 +2,8 @@ import os.path as op
 
 from PyQt4 import uic
 from PyQt4.QtCore import pyqtSlot, Qt
-from PyQt4.QtGui import (QApplication, QColor, QColorDialog, QDialog, QFont,
-                         QFontDialog, QIcon, QPixmap)
+from PyQt4.QtGui import (QColor, QColorDialog, QDialog, QFont, QFontDialog,
+                         QIcon, QPixmap)
 
 from karabo.common.scenemodel.api import LabelModel
 
@@ -15,15 +15,16 @@ class TextDialog(QDialog):
         uic.loadUi(op.join(op.dirname(__file__), 'textdialog.ui'), self)
 
         if label_model is None:
+            # NOTE: Fonts similar on all OS are Arial, Helvetica, sans-serif!
             self.label_model = LabelModel()
-            self.text_font = QApplication.font()
-            self.text_font.setStyleName("Regular")
+            self.text_font = QFont()
+            self.text_font.setFamily("Sans Serif")
+            self.text_font.setStyleName("Normal")
             self.label_model.font = self.text_font.toString()
         else:
             self.label_model = label_model.clone_traits()
             self.text_font = QFont()
             self.text_font.fromString(self.label_model.font)
-
         self.leText.setText(self.label_model.text)
 
         self.set_text_font_button()
