46e11958f5742b70b4a8d93db098326e0a7f9c0a
Sergey Esenov <serguei.essenov@xfel.eu>
2019-10-16 17:07:26 +0200

    Further unit test simplification

--- src/karabo/tests/net/ReadAsyncStringUntil_Test.cc
+++ src/karabo/tests/net/ReadAsyncStringUntil_Test.cc
@@ -37,75 +37,15 @@ CPPUNIT_TEST_SUITE_REGISTRATION(ReadAsyncStringUntil_Test);
 // Create simple synchronous echo server that will return us what we send.
 // It runs on separate thread.
 
-struct EchoServer {
-
-
-    EchoServer(unsigned short port) : m_port(port) {
-    }
-
-
-    /**
-     * write back the data we read
-     * @param sock connected to the client
-     */
-    void loopback(tcp::socket sock) {
-        try {
-            while (1) {
-                char data[MAX_DATA_LEN];
-                boost::system::error_code ec;
-
-                size_t length = sock.read_some(boost::asio::buffer(data), ec);
-                if (ec == boost::asio::error::eof) break; // The other side closed connection
-                if (ec) throw boost::system::system_error(ec); // some other error.
-                // no errors : synchronously write back
-                boost::asio::write(sock, boost::asio::buffer(data, length));
-            }
-        } catch (std::exception& e) {
-            std::cerr << "Exception in thread: " << e.what() << "\n";
-        }
-    }
-
-
-    /**
-     * Wait for incoming client connection
-     */
-    void listen() {
-        boost::asio::io_context io_context;
-        tcp::acceptor a(io_context, tcp::endpoint(tcp::v4(), m_port));
-        // Block on 'accept' function until client is knocked...
-        loopback(a.accept());   // one client can be accepted!
-    }
-
-
-    /**
-     * Start thread where we are awaiting client connection
-     */
-    void start() {
-        m_listener = new std::thread(&EchoServer::listen, this);
-    }
-
-
-    void join() {
-        m_listener->join();
-    }
-
-private:
-    unsigned short m_port;
-    std::thread* m_listener;
-};
-
-
 struct TestClient {
 
     KARABO_CLASSINFO(TestClient, "TestClient", "1.0");
 
 
     TestClient(const karabo::util::Hash& input)
-        : m_repetition(5)
-        , m_terminator("\r\n")
-        , m_expected("") {
+            : m_repetition(5)
+            , m_expected("") {
         m_connection = karabo::net::Connection::create("Tcp", input);
-        //                                                                     ec, channel
         m_connection->startAsync(boost::bind(&TestClient::connectHandler, this, _1, _2));
     }
 
@@ -120,56 +60,48 @@ struct TestClient {
             return;
         }
 
-        const std::string& tmp = "When the going gets tough... the tough get going\r\n";
-        m_terminator = "...";
         m_expected = "When the going gets tough...";
 
-        boost::shared_ptr<vector <char>> datap(new vector <char>());
-        datap->assign(tmp.begin(), tmp.end());
-        channel->writeAsyncVectorPointer(datap, boost::bind(&TestClient::writeCompleteHandler, this, _1, channel));
+        const std::string& tmp = "When the going gets tough... the tough get going\r\n";
+        auto datap = boost::make_shared<std::vector<char>>(tmp.begin(), tmp.end());
+        channel->writeAsyncVectorPointer(datap, boost::bind(&TestClient::writeCompleteHandler, this, _1, channel, "..."));
     }
 
 
-    void writeCompleteHandler(const karabo::net::ErrorCode& ec, const karabo::net::Channel::Pointer& channel) {
+    void writeCompleteHandler(const karabo::net::ErrorCode& ec,
+                              const karabo::net::Channel::Pointer& channel,
+                              const std::string& terminator) {
         if (ec) {
             CPPUNIT_FAIL("error on write");
             channel->close();
         }
+        auto readHandler = [this, channel](const karabo::net::ErrorCode& ec, const std::string& data) {
+            if (ec) {
+                CPPUNIT_FAIL("Error reading");
+                channel->close();
+                return;
+            }
 
-        channel->readAsyncStringUntil(m_terminator, boost::bind(&TestClient::readStringHandler, this, _1, channel, _2));
-    }
+            //std::cerr << data;
+            CPPUNIT_ASSERT(data == m_expected || data == " the tough get going\r\n");
+            if (--m_repetition == 0) {
+                channel->close();
+                return;
+            }
 
-
-    void readStringHandler(const karabo::net::ErrorCode& ec, const karabo::net::Channel::Pointer& channel, const std::string& read_str) {
-        if (ec) {
-            CPPUNIT_FAIL("Error reading");
-            channel->close();
-            return;
-        }
-
-        //std::cerr << read_str;
-
-        CPPUNIT_ASSERT(read_str == m_expected || read_str == " the tough get going\r\n");
-        
-        --m_repetition;
-
-        if (m_repetition == 0) {
-            channel->close();
-            return;
-        }
-
-        m_terminator = "\r\n";
-        m_expected = "Yet another test string\r\n";
-        boost::shared_ptr<vector <char>> datap(new vector <char>());
-        datap->assign(m_expected.begin(), m_expected.end());
-        channel->writeAsyncVectorPointer(datap, boost::bind(&TestClient::writeCompleteHandler, this, _1, channel));
+            m_expected = "Yet another test string\r\n";
+            auto datap = boost::make_shared<std::vector<char>>(m_expected.begin(), m_expected.end());
+            channel->writeAsyncVectorPointer(datap,
+                                             boost::bind(&TestClient::writeCompleteHandler,
+                                                         this, _1, channel, "\r\n"));
+        };
+        channel->readAsyncStringUntil(terminator, readHandler);
     }
 
 private:
     karabo::net::Connection::Pointer m_connection;
     int m_timeout;
     int m_repetition;
-    std::string m_terminator;
     std::string m_expected;
 };
 
@@ -194,20 +126,31 @@ void ReadAsyncStringUntil_Test::runTest() {
     using namespace std;
 
     // Start echo server thread ...
-    EchoServer srv(54345);
-    srv.start();
+    int port = 54345;
+    auto fut = std::async(std::launch::async, [port]() {
+        boost::asio::io_context io_context;
+        tcp::acceptor a(io_context, tcp::endpoint(tcp::v4(), port));
+        // Block on 'accept' function until client is knocked...
+        auto sock = a.accept();
+        try {
+            while (1) {
+                char data[MAX_DATA_LEN];
+                boost::system::error_code ec;
 
+                size_t length = sock.read_some(boost::asio::buffer(data), ec);
+                if (ec == boost::asio::error::eof) break; // The other side closed connection
+                if (ec) throw boost::system::system_error(ec); // some other error.
+                // no errors : synchronously write back
+                boost::asio::write(sock, boost::asio::buffer(data, length));
+            }
+        } catch (std::exception& e) {
+            std::cerr << "Exception in thread: " << e.what() << "\n";
+        }        
+    });
     // Start client ...
-    karabo::util::Hash connectionCfg;
-    connectionCfg.set("hostname", "localhost");
-    connectionCfg.set("port", 54345);
-    connectionCfg.set("type", "client");
-    connectionCfg.set("sizeofLength", 0); //default is 4
-
-    TestClient client(connectionCfg);
-
-    // Start event loop
+    karabo::util::Hash input("hostname", "localhost", "port", port, "type", "client", "sizeofLength", 0);
+    TestClient client(input);
     karabo::net::EventLoop::run();
     // Join server thread
-    srv.join();
+    fut.wait();
 }
