bf8f096f1abd063ae558dd433b7160d40c6e5421
Dennis Goeries <dennis.goeries@xfel.eu>
2019-11-06 16:41:04 +0100

    GUI: Create a base trendline

--- src/pythonGui/karabogui/controllers/display/display_trendgraph.py
+++ src/pythonGui/karabogui/controllers/display/display_trendgraph.py
@@ -1,6 +1,6 @@
 import os.path as op
 from collections import OrderedDict
-from datetime import datetime
+import datetime
 from itertools import cycle
 
 from PyQt5 import uic
@@ -22,13 +22,11 @@ from karabogui.graph.plots.base import KaraboPlotView
 
 # NOTE: We limit ourselves to selected karabo actions!
 ALLOWED_ACTIONS = ['x_grid', 'y_grid', 'y_invert', 'y_log', 'axes']
+MIN_TIMESTAMP = datetime.datetime(1970, 1, 31).timestamp()
+MAX_TIMESTAMP = datetime.datetime(2038, 12, 31).timestamp()
 
 
-@register_binding_controller(
-    ui_name='Trend Graph', klassname='DisplayTrendGraph',
-    binding_type=(BoolBinding, FloatBinding, IntBinding),
-    can_show_nothing=False)
-class DisplayTrendGraph(BaseBindingController):
+class BaseSeriesGraph(BaseBindingController):
     # The scene model class used by this controller
     model = Instance(TrendGraphModel, args=())
 
@@ -38,22 +36,34 @@ class DisplayTrendGraph(BaseBindingController):
 
     # Holds if the user is rescaling via mouse interaction
     _auto_scale = Bool(False)
-
     _curves = Dict
     _curves_start = Set
-
     _pens = Instance(cycle, factory=get_pen_cycler, args=())
 
     # Map time strs to QPushButtons
-    _x_axis_str_btns = Instance(OrderedDict, args=())
+    _x_axis_buttons = Instance(OrderedDict, args=())
     _x_detail = String(UPTIME)
 
-    def create_widget(self, parent):
-        widget = self._init_ui(parent)
+    def _init_ui(self, parent, axis):
+        """Setup all widgets correctly"""
 
         self._start_time = QDateTime.currentDateTime()
 
-        self._plot = KaraboPlotView(axis=AxisType.Time,
+        widget = QWidget(parent)
+        uic.loadUi(op.join(op.dirname(__file__), "ui_trendline.ui"), widget)
+
+        widget.dt_start.setDateTime(self._start_time)
+        widget.dt_end.setDateTime(self._start_time)
+
+        # Init x-axis buttons
+        self._x_axis_buttons[widget.bt_one_week] = ONE_WEEK
+        self._x_axis_buttons[widget.bt_one_day] = ONE_DAY
+        self._x_axis_buttons[widget.bt_one_hour] = ONE_HOUR
+        self._x_axis_buttons[widget.bt_ten_minutes] = TEN_MINUTES
+        self._x_axis_buttons[widget.bt_uptime] = UPTIME
+        widget.bg_x_axis.buttonClicked.connect(self._x_axis_btns_toggled)
+
+        self._plot = KaraboPlotView(axis=axis,
                                     actions=ALLOWED_ACTIONS,
                                     parent=widget.time_frame)
         self._plot.stateChanged.connect(self._change_model)
@@ -62,29 +72,23 @@ class DisplayTrendGraph(BaseBindingController):
         self._plot.add_toolbar()
         self._plot.enable_data_toggle()
 
-        # Limit the panning zoom
-        self._plot.plotItem.setLimits(
-            xMin=datetime(1970, 12, 31).timestamp(),
-            xMax=datetime(2038, 12, 31).timestamp())
-
-        self._plot.plotItem.vb.sigRangeChangedManually.connect(
-            self._on_range_manually_changed)
-
-        self._plot.plotItem.vb.autoRangeTriggered.connect(
-            self._uncheck_current_button)
-
-        # Update datetime widgets everytime range changes
-        self._plot.plotItem.sigXRangeChanged.connect(
-            self._update_datetime_widgets)
-
+        # Restore previous configuration!
         self._plot.restore(build_graph_config(self.model))
+
+        viewbox = self._plot.plotItem.vb
+        viewbox.sigRangeChangedManually.connect(self._range_change_manually)
+        viewbox.autoRangeTriggered.connect(self._uncheck_button)
+
+        # Update datetime widgets everytime range changes and limit zoom
+        self._plot.plotItem.setLimits(xMin=MIN_TIMESTAMP, xMax=MAX_TIMESTAMP)
+        self._plot.plotItem.sigXRangeChanged.connect(self._update_date_widgets)
 
         layout = QVBoxLayout()
         layout.addWidget(self._plot)
         widget.time_frame.setLayout(layout)
 
-        # have a 1s timeout to request data, thus avoid frequent re-loading
-        # while scaling
+        # XXX: Have a 1s timeout to request data, thus avoid frequent
+        # re-loading while scaling
         self._timer = QTimer(parent)
         self._timer.setInterval(1000)
         self._timer.setSingleShot(True)
@@ -93,26 +97,8 @@ class DisplayTrendGraph(BaseBindingController):
         # Add the first curve
         self.add_proxy(self.proxy)
 
+        # Finally, add the plot actions to the general widget
         widget.addActions(self._plot.actions())
-
-        return widget
-
-    def _init_ui(self, parent):
-        """Setup all widgets correctly"""
-        widget = QWidget(parent)
-        uic.loadUi(op.join(op.dirname(__file__), "ui_trendline.ui"), widget)
-
-        current_date_time = QDateTime.currentDateTime()
-        widget.dt_start.setDateTime(current_date_time)
-        widget.dt_end.setDateTime(current_date_time)
-
-        # Init x-axis buttons
-        self._x_axis_str_btns[widget.bt_one_week] = ONE_WEEK
-        self._x_axis_str_btns[widget.bt_one_day] = ONE_DAY
-        self._x_axis_str_btns[widget.bt_one_hour] = ONE_HOUR
-        self._x_axis_str_btns[widget.bt_ten_minutes] = TEN_MINUTES
-        self._x_axis_str_btns[widget.bt_uptime] = UPTIME
-        widget.bg_x_axis.buttonClicked.connect(self._x_axis_btns_toggled)
 
         return widget
 
@@ -132,33 +118,19 @@ class DisplayTrendGraph(BaseBindingController):
         return True
 
     def value_update(self, proxy):
-        if self.widget is None or abs(proxy.value) >= MAX_NUMBER_LIMIT:
-            return
+        raise NotImplementedError
 
-        timestamp = proxy.binding.timestamp
-        t = timestamp.toTimestamp()
+    # ----------------------------------------------------------------
+    # PyQt Slots
 
-        self._curves[proxy].add_point(proxy.value, t)
-        if proxy in self._curves_start:
-            self._curves[proxy].add_point(proxy.value,
-                                          self._start_time.toTime_t())
-            self._curves_start.remove(proxy)
-
-    def set_time_interval(self, t0, t1):
-        """Update the x axis scale interval of the curves"""
-        for v in self._curves.values():
-            v.changeInterval(t0, t1)
-
-    # @pyqtSlot()
-    def _on_range_manually_changed(self):
+    def _range_change_manually(self):
         """When the range changes manually, we stop automatically
         updating the ranges and start the timer"""
         self._timer.start()
         self._auto_scale = False
-        self._uncheck_current_button()
+        self._uncheck_button()
 
-    # @pyqtSlot()
-    def _uncheck_current_button(self):
+    def _uncheck_button(self):
         """Uncheck any checked button"""
         button_group = self.widget.bg_x_axis
         checked_button = button_group.checkedButton()
@@ -171,15 +143,13 @@ class DisplayTrendGraph(BaseBindingController):
     def _change_model(self, content):
         self.model.trait_set(**restore_graph_config(content))
 
-    # @pyqtSlot()
     def _update_intervals(self):
         """This slot is called whenever the timer timed out and previously the
         x axis scale was changed"""
         x_axis = self._plot.plotItem.getAxis("bottom")
         self.set_time_interval(*x_axis.range)
 
-    # @pyqtSlot()
-    def _update_datetime_widgets(self, vb, x_range):
+    def _update_date_widgets(self, vb, x_range):
         """This slot is called whenever the xRange changes"""
         # Note that the time comes in seconds
         x_min, x_max = x_range
@@ -190,14 +160,15 @@ class DisplayTrendGraph(BaseBindingController):
         self.widget.dt_start.setDateTime(start)
         self.widget.dt_end.setDateTime(end)
 
-    # @pyqtSlot(object)
     def _x_axis_btns_toggled(self, button):
         """Update the x axis scale when a time button is clicked"""
-        self._x_detail = self._x_axis_str_btns[button]
+        self._x_detail = self._x_axis_buttons[button]
         # We're updating the ranges via the buttons now
         self._auto_scale = True
         if self._update_axis_scale():
             self.update_later()
+
+    # ----------------------------------------------------------------
 
     def deferred_update(self):
         self._update_ranges()
@@ -259,3 +230,31 @@ class DisplayTrendGraph(BaseBindingController):
             return QDateTime.currentDateTime()
         else:
             return QDateTime.fromTime_t(max(timestamps))
+
+    def set_time_interval(self, t0, t1):
+        """Update the x axis scale interval of the curves"""
+        for v in self._curves.values():
+            v.changeInterval(t0, t1)
+
+
+@register_binding_controller(
+    ui_name='Trend Graph', klassname='DisplayTrendGraph',
+    binding_type=(BoolBinding, FloatBinding, IntBinding),
+    can_show_nothing=False)
+class DisplayTrendGraph(BaseSeriesGraph):
+    def create_widget(self, parent):
+        widget = self._init_ui(parent, axis=AxisType.Time)
+        return widget
+
+    def value_update(self, proxy):
+        if self.widget is None or abs(proxy.value) >= MAX_NUMBER_LIMIT:
+            return
+
+        timestamp = proxy.binding.timestamp
+        t = timestamp.toTimestamp()
+
+        self._curves[proxy].add_point(proxy.value, t)
+        if proxy in self._curves_start:
+            self._curves[proxy].add_point(proxy.value,
+                                          self._start_time.toTime_t())
+            self._curves_start.remove(proxy)
