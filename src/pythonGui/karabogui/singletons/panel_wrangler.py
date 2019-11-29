4927fa266e05b2ba94d60ec789680b20751695d2
Cammille Carinan <cammille.carinan@xfel.eu>
2019-11-29 19:10:36 +0100

    GUI: Increase connect dialog delay

--- src/pythonGui/karabogui/singletons/panel_wrangler.py
+++ src/pythonGui/karabogui/singletons/panel_wrangler.py
@@ -15,6 +15,9 @@ from karabogui.panels.api import AlarmPanel, MacroPanel, ScenePanel
 from karabogui.singletons.api import get_config, get_project_model, get_db_conn
 
 
+LOGIN_DIALOG_DELAY = 100  # ms
+
+
 class PanelWrangler(QObject):
     """An object which handles wrangler duties for the myriad panels and
     windows shown in the GUI.
@@ -196,7 +199,7 @@ class PanelWrangler(QObject):
         self.main_window.show()
         # Show the connection dialog after processing all pending events since
         # the main window needs to render and update its geometry first.
-        QApplication.processEvents(QEventLoop.AllEvents, 10)
+        QApplication.processEvents(QEventLoop.AllEvents, LOGIN_DIALOG_DELAY)
         self.main_window.acServerConnect.trigger()
 
     def _open_macro(self, model):
