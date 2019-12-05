949f22a8b6bcfec40184ac530bbbea8894d33371
Alessandro Silenzi <alessandro.silenzi@xfel.eu>
2019-12-05 17:41:21 +0100

    CONDA: set environment variable
    for OMP_NUM_THREADS

--- src/pythonGui/conda-recipe/bld.bat
+++ src/pythonGui/conda-recipe/bld.bat
@@ -4,3 +4,15 @@ python setup.py install
 
 cd ../pythonGui
 python setup.py install
+
+:: from https://docs.conda.io/projects/conda-build/en/latest/resources/activate-scripts.html
+setlocal EnableDelayedExpansion
+
+:: Copy the [de]activate scripts to %PREFIX%\etc\conda\[de]activate.d.
+:: This will allow them to be run on environment activation.
+for %%F in (activate deactivate) DO (
+    if not exist %PREFIX%\etc\conda\%%F.d mkdir %PREFIX%\etc\conda\%%F.d
+    copy %RECIPE_DIR%\env_scripts\%%F.bat %PREFIX%\etc\conda\%%F.d\%PKG_NAME%_%%F.bat
+    :: Copy unix shell activation scripts, needed by Windows Bash users
+    copy %RECIPE_DIR%\env_scripts\%%F.sh %PREFIX%\etc\conda\%%F.d\%PKG_NAME%_%%F.sh
+)
---
+++ src/pythonGui/conda-recipe/env_scripts/deactivate.bat
@@ -0,0 +1,7 @@
+@REM Restore previous OMP_NUM_THREADS value
+
+@set "OMP_NUM_THREADS="
+@if defined _CONDA_SET_OMP_NUM_THREADS (
+    set "OMP_NUM_THREADS=%_CONDA_SET_OMP_NUM_THREADS%"
+    set "_CONDA_SET_OMP_NUM_THREADS="
+)
