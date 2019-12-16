3f57ce07d26b9ffc9f05291c398b20ab71f98bd2
Dennis Goeries <dennis.goeries@xfel.eu>
2019-12-16 12:52:25 +0100

    GUI: More cleanup in the table element

--- src/pythonGui/karabogui/controllers/tableeditor.py
+++ src/pythonGui/karabogui/controllers/tableeditor.py
@@ -12,8 +12,9 @@ from PyQt5.QtGui import QBrush, QColor
 from PyQt5.QtWidgets import QTableView, QComboBox, QItemDelegate
 
 from karabo.common.api import (
-    KARABO_SCHEMA_DISPLAY_TYPE, KARABO_SCHEMA_VALUE_TYPE)
-from karabo.common.display_types import KARABO_SCHEMA_DISPLAY_TYPE_STATE
+    KARABO_SCHEMA_ACCESS_MODE, KARABO_SCHEMA_DEFAULT_VALUE,
+    KARABO_SCHEMA_DISPLAY_TYPE, KARABO_SCHEMA_VALUE_TYPE,
+    KARABO_SCHEMA_DISPLAY_TYPE_STATE, KARABO_SCHEMA_DISPLAYED_NAME)
 from karabo.native import AccessMode, Hash
 from karabogui.enums import NavigationItemTypes, ProjectItemTypes
 from karabogui.indicators import get_state_color
@@ -25,55 +26,50 @@ def _get_value_type(schema, key):
 
 
 def _get_display_type(schema, key):
+    """Extract the display type of a key from a schema"""
     return schema.hash[key, ...].get(KARABO_SCHEMA_DISPLAY_TYPE, '')
+
+
+def _get_access_mode(schema, key):
+    """Extract the access mode of a key from a schema"""
+    access = schema.hash[key, ...].get(KARABO_SCHEMA_ACCESS_MODE)
+    return AccessMode(access)
 
 
 class TableModel(QAbstractTableModel):
     def __init__(self, row_schema, editing_finished, parent=None):
         super(QAbstractTableModel, self).__init__(parent)
-
-        self._editing_finished = editing_finished
+        self._set_edit_value = editing_finished
         self._row_schema = row_schema
         self._row_hash = row_schema.hash
         self._role = Qt.EditRole
         self._data = []
 
-    # ------------------------------------------------------------------------
-    # Public interface
-
-    def duplicate_row(self, pos):
-        self.insertRows(pos + 1, 1, QModelIndex(), copy_row=self._data[pos])
-
-    def set_role(self, role):
-        self._role = role
-
     def rowCount(self, parent=None):
-        """Reimplemented function of QAbstractTableModel.
-        """
+        """Reimplemented function of QAbstractTableModel"""
         return len(self._data)
 
     def columnCount(self, parent=None):
-        """Reimplemented function of QAbstractTableModel.
-        """
+        """Reimplemented function of QAbstractTableModel"""
         return len(self._row_hash)
 
     def data(self, index, role):
-        """Reimplemented function of QAbstractTableModel.
-        """
+        """Reimplemented function of QAbstractTableModel"""
         if not index.isValid():
             return None
 
-        row, col = index.row(), index.column()
-
+        row, column = index.row(), index.column()
         if role == Qt.CheckStateRole and self._role == Qt.EditRole:
-            key = self._row_hash.getKeys()[col]
+            # XXX: Again the Qt.EditRole check is to distinguish INITONLY
+            # and READONLY access!
+            key = self._row_hash.getKeys()[column]
             value = self._data[row][key]
             vtype = _get_value_type(self._row_schema, key)
             if vtype == 'BOOL':
                 return Qt.Checked if value else Qt.Unchecked
 
-        if role == Qt.DisplayRole or role == Qt.EditRole:
-            key = self._row_hash.getKeys()[col]
+        if role in (Qt.DisplayRole, Qt.EditRole):
+            key = self._row_hash.getKeys()[column]
             value = self._data[row][key]
             vtype = _get_value_type(self._row_schema, key)
             if vtype.startswith('VECTOR'):
@@ -83,7 +79,7 @@ class TableModel(QAbstractTableModel):
             return str(value)
 
         elif role == Qt.BackgroundRole:
-            key = self._row_hash.getKeys()[col]
+            key = self._row_hash.getKeys()[column]
             display_type = _get_display_type(self._row_schema, key)
             if display_type == KARABO_SCHEMA_DISPLAY_TYPE_STATE:
                 value = self._data[row][key]
@@ -94,8 +90,7 @@ class TableModel(QAbstractTableModel):
         return None
 
     def headerData(self, section, orientation, role):
-        """Reimplemented function of QAbstractTableModel.
-        """
+        """Reimplemented function of QAbstractTableModel"""
         if role != Qt.DisplayRole:
             return None
 
@@ -103,31 +98,26 @@ class TableModel(QAbstractTableModel):
             return str(section)
 
         if orientation == Qt.Horizontal:
-            if section >= len(self._row_hash):
-                return None
-
             key = self._row_hash.getKeys()[section]
             attrs = self._row_hash[key, ...]
-            if 'displayedName' in attrs:
-                return attrs['displayedName']
-            return key
+            return attrs.get(KARABO_SCHEMA_DISPLAYED_NAME, key)
 
         return None
 
     def flags(self, index):
-        """Reimplemented function of QAbstractTableModel.
-        """
+        """Reimplemented function of QAbstractTableModel"""
         if not index.isValid():
             return Qt.NoItemFlags
 
         flags = Qt.ItemIsEnabled | Qt.ItemIsSelectable | Qt.ItemIsEditable
         key = self._row_hash.getKeys()[index.column()]
-        access = AccessMode(self._row_schema.hash[key, 'accessMode'])
+        access = _get_access_mode(self._row_schema, key)
         vtype = _get_value_type(self._row_schema, key)
 
         if vtype == 'BOOL' and self._role == Qt.EditRole:
             flags &= ~Qt.ItemIsEditable
             if access is AccessMode.READONLY:
+                # XXX: Remove the `enabled` flag due to INITONLY!
                 flags &= ~Qt.ItemIsEnabled
             else:
                 flags |= Qt.ItemIsUserCheckable
@@ -137,8 +127,7 @@ class TableModel(QAbstractTableModel):
         return flags
 
     def setData(self, index, value, role, from_device_update=False):
-        """Reimplemented function of QAbstractTableModel.
-        """
+        """Reimplemented function of QAbstractTableModel"""
         if not index.isValid():
             return False
 
@@ -151,7 +140,7 @@ class TableModel(QAbstractTableModel):
                 self._data[row][key] = value
                 self.dataChanged.emit(index, index)
                 if not from_device_update:
-                    self._editing_finished(self._data)
+                    self._set_edit_value(self._data)
                 return True
 
             return False
@@ -171,49 +160,61 @@ class TableModel(QAbstractTableModel):
 
             self.dataChanged.emit(index, index)
             if role == Qt.EditRole and not from_device_update:
-                self._editing_finished(self._data)
+                self._set_edit_value(self._data)
             return True
 
         return False
 
     def insertRows(self, pos, rows, index, *,
                    copy_row=None, from_device_update=False):
+        """Reimplemented function of QAbstractTableModel"""
         self.beginInsertRows(QModelIndex(), pos, pos + rows - 1)
         try:
-            for r in range(rows):
+            for row_nr in range(rows):
                 row_hash = copy.copy(copy_row)
                 if row_hash is None:
                     row_hash = Hash()
                     for key in self._row_hash.getKeys():
                         attrs = self._row_hash[key, ...]
-                        val = attrs.get('defaultValue', None)
+                        val = attrs.get(KARABO_SCHEMA_DEFAULT_VALUE, None)
 
                         # XXX: Formerly, the value was 'cast' here...
                         # val = valueType.cast(val)
                         row_hash[key] = val
-                if pos + r < len(self._data):
-                    self._data.insert(pos + r, row_hash)
+                if pos + row_nr < len(self._data):
+                    self._data.insert(pos + row_nr, row_hash)
                 else:
                     self._data.append(row_hash)
         finally:
             self.endInsertRows()
 
         if not from_device_update:
-            self._editing_finished(self._data)
+            self._set_edit_value(self._data)
         return True
 
     def removeRows(self, pos, rows, index, *, from_device_update=False):
+        """Reimplemented function of QAbstractTableModel"""
         self.beginRemoveRows(QModelIndex(), pos, pos + rows - 1)
         try:
-            for r in range(rows, 0, -1):
-                self._data.pop(pos + r - 1)
+            for row_nr in range(rows, 0, -1):
+                self._data.pop(pos + row_nr - 1)
         finally:
             self.endRemoveRows()
 
         if not from_device_update:
-            self._editing_finished(self._data)
+            self._set_edit_value(self._data)
 
         return True
+
+    # ------------------------------------------------------------------------
+    # Additional methods
+
+    def duplicate_row(self, pos):
+        self.insertRows(pos + 1, 1, QModelIndex(), copy_row=self._data[pos])
+
+    def set_role(self, role=Qt.DisplayRole):
+        """Set the role of the table element"""
+        self._role = role
 
 
 class ComboBoxDelegate(QItemDelegate):
@@ -227,22 +228,30 @@ class ComboBoxDelegate(QItemDelegate):
         self._options = options
 
     def createEditor(self, parent, option, index):
+        """Reimplemented function of QItemDelegate"""
         combo = QComboBox(parent)
         combo.addItems([str(o) for o in self._options])
         combo.currentIndexChanged.connect(self._on_current_index_changed)
         return combo
 
     def setEditorData(self, editor, index):
+        """Reimplemented function of QItemDelegate"""
         editor.blockSignals(True)
         selection = index.model().data(index, Qt.DisplayRole)
         editor.setCurrentIndex(self._options.index(selection))
         editor.blockSignals(False)
 
     def setModelData(self, editor, model, index):
+        """Reimplemented function of QItemDelegate"""
         model.setData(index, self._options[editor.currentIndex()], Qt.EditRole)
 
     @pyqtSlot()
     def _on_current_index_changed(self):
+        """The current index of the combobox changed. Notify the model.
+
+        This signal MUST be emitted when the editor widget has completed
+        editing the data, and wants to write it back into the model.
+        """
         self.commitData.emit(self.sender())
 
 
@@ -258,64 +267,64 @@ class KaraboTableView(QTableView):
         self._row_schema = row_schema
         self._row_hash = row_schema.hash
         self._row_keys = self._row_hash.getKeys()
-        self._first_string_column = None
-
-        for count, key in enumerate(self._row_keys):
+        self._drag_column = None
+        # XXX: Evaluate the eventual `drag-column`. If a string element is
+        # found in the row schema, the first appearance is taken!
+        for column_index, key in enumerate(self._row_keys):
             vtype = _get_value_type(self._row_schema, key)
             if vtype == 'STRING':
-                self._first_string_column = count
+                self._drag_column = column_index
                 break
 
     def dragEnterEvent(self, event):
-        self._check_drag_event(event)
+        self._validate_drag_event(event)
 
     def dragMoveEvent(self, event):
-        self._check_drag_event(event)
+        self._validate_drag_event(event)
 
     def dropEvent(self, event):
-        acceptable, index, new_row, device_id = self._check_drag_event(event)
+        is_valid, index, new_row, deviceId = self._validate_drag_event(event)
 
-        if acceptable:
+        if is_valid:
             model = self.model()
             if new_row:
-                if self._first_string_column is not None:
+                if self._drag_column is not None:
                     model.insertRows(model.rowCount(), 1, QModelIndex())
                     index = model.index(model.rowCount() - 1,
-                                        self._first_string_column,
+                                        self._drag_column,
                                         QModelIndex())
                     # scroll to the end and pad with new whitespace to drop
                     # next item
                     self.scrollToBottom()
                 else:
                     return
-            model.setData(index, device_id, Qt.EditRole)
+            model.setData(index, deviceId, Qt.EditRole)
 
-    def _check_drag_event(self, event):
-        items_data = event.mimeData().data('treeItems').data()
-        if len(items_data) == 0:
+    def _validate_drag_event(self, event):
+        items = event.mimeData().data('treeItems').data()
+        if len(items) == 0:
             event.ignore()
-            return False, None, False, ''
+            return False, None, False, 'None'
 
-        items = json.loads(items_data.decode())
-        item = items[0]
+        item = json.loads(items.decode())[0]
         index = self.indexAt(event.pos())
-        drag_type = item.get('type')
-        from_navigation = drag_type == NavigationItemTypes.DEVICE
-        from_project = drag_type == ProjectItemTypes.DEVICE
-        usable = from_navigation or from_project
-        device_id = item.get('deviceId', '')
+        item_type = item.get('type')
+        is_navigation_device = item_type == NavigationItemTypes.DEVICE
+        is_project_device = item_type == ProjectItemTypes.DEVICE
+        is_valid = is_navigation_device or is_project_device
+        deviceId = item.get('deviceId', 'None')
 
         # Drop in empty area is also okay but must trigger new_row
-        if not index.isValid() and usable:
+        if not index.isValid() and is_valid:
             event.accept()
-            return True, index, True, device_id
+            return True, index, True, deviceId
 
         key = self._row_keys[index.column()]
         vtype = _get_value_type(self._row_schema, key)
-        if usable:
+        if is_valid:
             event.accept()
             not_string = (vtype != 'STRING')
-            return True, index, not_string, device_id
+            return True, index, not_string, deviceId
 
         event.ignore()
-        return False, None, False, device_id
+        return False, None, False, deviceId
