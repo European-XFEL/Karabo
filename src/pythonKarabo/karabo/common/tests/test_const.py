511b0e7f5404732c9534b09132dd8c07ca81fe14
Dennis Goeries <dennis.goeries@xfel.eu>
2019-12-17 14:34:42 +0100

    Common: Provide a const type definition

--- src/pythonKarabo/karabo/common/tests/test_const.py
+++ src/pythonKarabo/karabo/common/tests/test_const.py
@@ -1,14 +1,15 @@
 from karabo.common import const as constmod
 
 
-def test_const_def():
+def test_schema_attributes_def():
     tuple_symbol = 'KARABO_SCHEMA_ATTRIBUTES'
     ignored_symbols = ('KARABO_EDITABLE_ATTRIBUTES',
                        'KARABO_RUNTIME_ATTRIBUTES_MDL',
                        'KARABO_SCHEMA_DEFAULT_SCENE')
 
     all_symbols = dir(constmod)
-    all_symbols = [s for s in all_symbols if not s.startswith('__')]
+    all_symbols = [s for s in all_symbols if not s.startswith('__')
+                   and not s.startswith('KARABO_TYPE')]
     all_symbols.remove(tuple_symbol)
     for symbol in ignored_symbols:
         all_symbols.remove(symbol)
@@ -18,4 +19,16 @@ def test_const_def():
     # KARABO_SCHEMA_ATTRIBUTES tuple.
     # NOTE: If this test fails, go to const.py and add entries to
     # KARABO_SCHEMA_ATTRIBUTES until this test passes
+    assert all_values == set(getattr(constmod, tuple_symbol))
+
+
+def test_schema_types():
+    """Make sure that all the schema types are appearing in the tuple summary
+    """
+    tuple_symbol = 'KARABO_TYPES'
+    all_symbols = dir(constmod)
+    all_symbols = [s for s in all_symbols if s.startswith('KARABO_TYPE')]
+    all_symbols.remove(tuple_symbol)
+
+    all_values = {getattr(constmod, s) for s in all_symbols}
     assert all_values == set(getattr(constmod, tuple_symbol))
