5f27f3c2bb9cff19f7a0b62b5eb4a89f0feecf1f
Dennis Goeries <dennis.goeries@xfel.eu>
2019-12-17 17:17:07 +0100

    GUI: Clean a bit the globals file

--- src/pythonGui/karabogui/globals.py
+++ src/pythonGui/karabogui/globals.py
@@ -24,15 +24,12 @@ except (ImportError, AttributeError):
     print('Version file not found! Please generate the _version.py file.')
     exit(1)
 
-
 # XXX: Karabo will support an global access level and an exception list which
 # is deviceId specific.
 # This requires a function like in SignalSlotable: getAccessLevel(deviceId) in
 # the end!
 
 GLOBAL_ACCESS_LEVEL = AccessLevel.OBSERVER
-# KARABO_DEFAULT_ACCESS_LEVEL = AccessLevel.OBSERVER  # Inside XFEL
-KARABO_DEFAULT_ACCESS_LEVEL = AccessLevel.ADMIN  # Outside XFEL
 
 # Hidden karabo folder which includes certain karabo related files
 if platform.startswith('win'):
@@ -42,22 +39,20 @@ else:
 # Project folder
 KARABO_PROJECT_FOLDER = path.join(HIDDEN_KARABO_FOLDER, 'projects')
 
-MAX_INT8 = (2**7)-1
-MIN_INT8 = -(2**7)
 
-MAX_UINT8 = (2**8)-1
-
-MAX_INT16 = (2**15)-1
-MIN_INT16 = -(2**15)
-
-MAX_UINT16 = (2**16)-1
-
-MAX_INT32 = (2**31)-1  # 0x7fffffff
-MIN_INT32 = -(2**31)  # -0x80000000
-
-MAX_UINT32 = (2**32)-1  # 0xffffffff
-
-MAX_INT64 = (2**63)-1
-MIN_INT64 = -(2**63)
-
-MAX_UINT64 = (2**64)-1
+MAX_UINT8 = (2 ** 8) - 1
+MIN_UINT8 = (2 ** 8)
+MAX_UINT16 = (2 ** 16) - 1
+MIN_UINT16 = (2 ** 16)
+MAX_UINT32 = (2 ** 32) - 1  # 0xffffffff
+MIN_UINT32 = (2 ** 32)
+MAX_UINT64 = (2 ** 64) - 1
+MIN_UINT64 = (2 ** 64)
+MAX_INT8 = (2 ** 7) - 1
+MIN_INT8 = -(2 ** 7)
+MAX_INT16 = (2 ** 15) - 1
+MIN_INT16 = -(2 ** 15)
+MAX_INT32 = (2 ** 31) - 1  # 0x7fffffff
+MIN_INT32 = -(2 ** 31)  # -0x80000000
+MAX_INT64 = (2 ** 63) - 1
+MIN_INT64 = -(2 ** 63)
