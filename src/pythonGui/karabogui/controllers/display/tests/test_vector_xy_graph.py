54613ae4423e8b1f89e5135523c82b031aec2c18
Dennis Goeries <dennis.goeries@xfel.eu>
2020-01-21 15:49:12 +0100

    GUI: Implement vector xy graph

---
+++ src/pythonGui/karabogui/controllers/display/tests/test_vector_xy_graph.py
@@ -0,0 +1,42 @@
+import numpy as np
+
+from karabo.native import Configurable, VectorInt32
+from karabogui.binding.api import PropertyProxy
+from karabogui.testing import (
+    GuiTestCase, get_class_property_proxy, set_proxy_value)
+from ..display_xy_vector import DisplayVectorXYGraph
+
+
+class Object(Configurable):
+    index = VectorInt32()
+    value = VectorInt32()
+
+
+class TestXYVectorGraph(GuiTestCase):
+    def setUp(self):
+        super(TestXYVectorGraph, self).setUp()
+
+        schema = Object.getClassSchema()
+        self.index = get_class_property_proxy(schema, 'index')
+        device = self.index.root_proxy
+        self.value = PropertyProxy(root_proxy=device, path='value')
+
+        self.controller = DisplayVectorXYGraph(proxy=self.index)
+        self.controller.create(None)
+        self.controller.visualize_additional_property(self.value)
+
+    def tearDown(self):
+        super(TestXYVectorGraph, self).tearDown()
+        self.controller.destroy()
+        self.assertIsNone(self.controller.widget)
+
+    def test_set_value(self):
+        """Test the setting of both x and y values in the vector xy graph"""
+        index = [1, 2, 3, 4, 5]
+        value = [42] * 5
+        set_proxy_value(self.index, 'index', index)
+        set_proxy_value(self.value, 'value', value)
+
+        curve = self.controller._plot
+        np.testing.assert_almost_equal(list(curve.xData), index)
+        np.testing.assert_almost_equal(list(curve.yData), value)
