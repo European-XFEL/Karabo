1262ca526c03982a9dea73701451e9d50ab78c8f
Alessandro Silenzi <alessandro.silenzi@xfel.eu>
2020-03-02 15:28:00 +0100

    BUILD: use the xkarabo user
    [ci-skip]

--- ci/miniconda/deploy.sh
+++ ci/miniconda/deploy.sh
@@ -13,7 +13,7 @@ printAndRun() {
 
 CONDA_CHANNEL_PATH=/var/www/html/karabo/channel
 PLATFORM=linux-64
-REMOTE_SERVER=xdata@exflserv05
+REMOTE_SERVER=xkarabo@exflserv05
 REMOTE_CHANNEL_PATH=${REMOTE_SERVER}:${CONDA_CHANNEL_PATH}/
 
 # Get the package
@@ -22,16 +22,18 @@ KARABOGUI_PKG=$(conda info --root)/conda-bld/${PLATFORM}/karabogui-${CI_COMMIT_R
 . ci/utils/enable_internet.sh
 
 # Deploy the package
-printAndRun rsync --exclude \".git\" --rsh=\"sshpass -p \"${XDATA_PWD}\" ssh -o StrictHostKeyChecking=no -l xdata\" \
+printAndRun rsync --exclude \".git\" --rsh=\"sshpass -p \"${XKARABO_PWD}\" ssh -o StrictHostKeyChecking=no -l xkarabo\" \
                   --progress ${KARABOGUI_PKG} ${REMOTE_CHANNEL_PATH}/${PLATFORM}/
 
 # Deploy the mirror if exists
 if [ "$(ls -A /tmp/mirror/)" ]; then
-    printAndRun rsync -r --exclude \".git\" --rsh=\"sshpass -p \"${XDATA_PWD}\" ssh -o StrictHostKeyChecking=no -l xdata\" \
+    printAndRun rsync -r --exclude \".git\" --rsh=\"sshpass -p \"${XKARABO_PWD}\" ssh -o StrictHostKeyChecking=no -l xkarabo\" \
                          --progress /tmp/mirror/* ${REMOTE_CHANNEL_PATH}/mirror/
 fi
 
+# the remote host should have a conda installation with conda-build available in the home folder
+
 # Rebuild channel indexes
-printAndRun sshpass -p ${XDATA_PWD} ssh -o StrictHostKeyChecking=no ${REMOTE_SERVER} bash \"~/scripts/rebuild_channel_index.sh\"
+printAndRun sshpass -p ${XKARABO_PWD} ssh -o StrictHostKeyChecking=no ${REMOTE_SERVER} bash \"source ~/miniconda3/bin/activate; cd ${CONDA_CHANNEL_PATH}; conda index .;\"
 
 . ci/utils/disable_internet.sh
