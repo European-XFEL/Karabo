cd91f7d7b7878c255cf630d7d4f2fe6a48ff7d85
Cammille Carinan <cammille.carinan@xfel.eu>
2020-04-29 18:46:47 +0200

    GUI: Convert Qwt models to Graph models

--- src/pythonKarabo/karabo/common/scenemodel/widgets/plot.py
+++ src/pythonKarabo/karabo/common/scenemodel/widgets/plot.py
@@ -1,61 +1,13 @@
 from xml.etree.ElementTree import SubElement
 
-from traits.api import Bool, Enum, Instance, Int, List, String
+from traits.api import Bool, Int, String
 
-from karabo.common.api import BaseSavableModel
 from karabo.common.scenemodel.bases import BaseWidgetObjectData
 from karabo.common.scenemodel.const import NS_KARABO, WIDGET_ELEMENT_TAG
 from karabo.common.scenemodel.io_utils import (
-    read_base_widget_data, read_empty_display_editable_widget,
-    write_base_widget_data)
+    read_base_widget_data, write_base_widget_data)
 from karabo.common.scenemodel.registry import (
     register_scene_reader, register_scene_writer)
-
-
-class PlotCurveModel(BaseSavableModel):
-    """ A model for plot curve data
-    """
-    # The device with the data
-    device = String
-    # The property name for that data
-    path = String
-    # The encoded pickle of the curve object
-    curve_object_data = String
-
-    def __hash__(self):
-        """ Return a unique hash value for this object.
-            Device + Path is the uniqueness criteria.
-        """
-        return hash(self.device) ^ hash(self.path)
-
-
-class LinePlotModel(BaseWidgetObjectData):
-    """ A model for line plot objects
-    """
-    # The actual type of the widget
-    klass = Enum('DisplayTrendline', 'XYVector')
-    # The plots for this object
-    boxes = List(Instance(PlotCurveModel))
-
-    def add_curve(self, curve_model):
-        """Maybe add a curve (box) to the model, but only if it is not a
-        duplicate of an existing curve in the model. If it is an existing
-        curve, then update the data.
-        """
-        existing = self._find_all_matching_boxes(curve_model)
-        if not existing:
-            self.boxes.append(curve_model)
-        else:
-            assert len(existing) == 1
-            existing[0].curve_object_data = curve_model.curve_object_data
-
-    def _find_all_matching_boxes(self, curve_model):
-        matches = []
-        model_hash = hash(curve_model)
-        for box in self.boxes:
-            if hash(box) == model_hash:
-                matches.append(box)
-        return matches
 
 
 class SparklineModel(BaseWidgetObjectData):
@@ -69,37 +21,6 @@ class SparklineModel(BaseWidgetObjectData):
     alarm_range = Bool(True)
     # String format for the value when it is shown
     show_format = String("0.2f")
-
-
-@register_scene_reader('DisplayTrendline', version=1)
-@register_scene_reader('XYVector', version=1)
-def _line_plot_reader(read_func, element):
-    traits = read_empty_display_editable_widget(element)
-    boxes = []
-    for child_elem in element:
-        assert child_elem.tag == NS_KARABO + 'box'
-        box_traits = {
-            'device': child_elem.get("device"),
-            'path': child_elem.get("path"),
-            'curve_object_data': child_elem.text,
-        }
-        model = PlotCurveModel(**box_traits)
-        model.initialized = True  # Satisfy BaseSavableModel
-        boxes.append(model)
-    traits['boxes'] = boxes
-    return LinePlotModel(**traits)
-
-
-@register_scene_writer(LinePlotModel)
-def _line_plot_writer(write_func, model, parent):
-    element = SubElement(parent, WIDGET_ELEMENT_TAG)
-    write_base_widget_data(model, element, model.klass)
-    for box in model.boxes:
-        elem = SubElement(element, NS_KARABO + 'box')
-        elem.set("device", box.device)
-        elem.set("path", box.path)
-        elem.text = box.curve_object_data
-    return element
 
 
 @register_scene_reader('DisplaySparkline', version=2)
