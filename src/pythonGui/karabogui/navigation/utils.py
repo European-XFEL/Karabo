ea6afaad374ca6364e915e8977bb02287d4aa641
Dennis Goeries <dennis.goeries@xfel.eu>
2020-05-13 12:44:00 +0200

    GUI: Add Macro Icon for Topology

--- src/pythonGui/karabogui/navigation/utils.py
+++ src/pythonGui/karabogui/navigation/utils.py
@@ -1,14 +1,15 @@
 import karabogui.icons as icons
 
+ICONS = {"python": icons.python,
+         "bound": icons.bound,
+         "cpp": icons.cpp,
+         "macro": icons.macro}
+
 
 def get_language_icon(node):
     """Return the appropriate language (`API`) icon from a `SystemTreeNode`
     """
     attrs = node.attributes
     language = attrs.get('lang', 'unknown')
-
-    ICONS = {"python": icons.python,
-             "bound": icons.bound,
-             "cpp": icons.cpp}
 
     return ICONS.get(language, None)
