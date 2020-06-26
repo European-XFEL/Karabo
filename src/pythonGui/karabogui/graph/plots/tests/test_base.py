8838289797ab68eff24c690fc878a2446f6b3884
Cammille Carinan <cammille.carinan@xfel.eu>
2020-06-26 15:00:23 +0200

    GUI: Fix plot log range

--- src/pythonGui/karabogui/graph/plots/tests/test_base.py
+++ src/pythonGui/karabogui/graph/plots/tests/test_base.py
@@ -2,6 +2,7 @@ import os
 from unittest.mock import patch
 
 import numpy as np
+from numpy.testing import assert_allclose, assert_array_equal
 
 from karabogui.testing import GuiTestCase
 
@@ -15,14 +16,14 @@ NPZ = ".npz"
 NAMES = ["curve_1", "curve_2"]
 
 
-class TestKaraboImageView(GuiTestCase):
+class TestPlotViewExport(GuiTestCase):
 
     def setUp(self):
-        super(TestKaraboImageView, self).setUp()
+        super(TestPlotViewExport, self).setUp()
         self.widget = KaraboPlotView()
 
     def tearDown(self):
-        super(TestKaraboImageView, self).tearDown()
+        super(TestPlotViewExport, self).tearDown()
         self.widget.destroy()
         self.widget = None
         for ext in [NPY, NPZ]:
@@ -100,3 +101,82 @@ class TestKaraboImageView(GuiTestCase):
     def _delete_file(self, filename):
         if os.path.isfile(filename):
             os.remove(filename)
+
+
+orders = np.arange(-5, 5, dtype=np.float)
+LOG_ARRAY = 10 ** orders
+RANGE_TOLERANCE = 0.12  # or 12% percent
+
+
+class _BasePlotTest(GuiTestCase):
+
+    def setUp(self):
+        super(_BasePlotTest, self).setUp()
+        self.widget = KaraboPlotView()
+        self.widget.configuration.update({'x_log': False, 'y_log': False})
+        self.widget.show()
+        self.viewbox = self.widget.plotItem.vb
+        self._plot = None
+
+    def tearDown(self):
+        super(_BasePlotTest, self).tearDown()
+        self.widget.close()
+        self.widget.destroy()
+        self.widget = None
+
+    def _set_data(self, x, y):
+        self._plot.setData(x, y)
+        # Trigger auto range
+        self.viewbox.updateAutoRange()
+
+    def _set_log_scale(self, x=None, y=None):
+        if x is not None:
+            self.widget.set_log_x(x)
+            self.widget.configuration['x_log'] = x
+        if y is not None:
+            self.widget.set_log_y(y)
+            self.widget.configuration['y_log'] = y
+        self.viewbox.updateAutoRange()
+
+    def _assert_data(self, x_expected, y_expected, log_x=False, log_y=False):
+        # Check getData values
+        x_data, y_data = self._plot.getData()
+        if log_x:
+            x_expected = np.log10(x_expected)
+        if log_y:
+            y_expected = np.log10(y_expected)
+        assert_array_equal(x_data, x_expected)
+        assert_array_equal(y_data, y_expected)
+
+    def _assert_range(self, x_expected, y_expected, log_x=False, log_y=False):
+        assert_x_range = (self._assert_log_range if log_x
+                          else self._assert_linear_range)
+        assert_y_range = (self._assert_log_range if log_y
+                          else self._assert_linear_range)
+
+        assert_x_range(actual=self.widget.get_view_range_x(),
+                       expected=x_expected)
+        assert_y_range(actual=self.widget.get_view_range_y(),
+                       expected=y_expected)
+
+    def _assert_log_range(self, actual, expected):
+        """The tolerance is by its order with the deviation of +/-1 """
+        actual = np.ceil(np.log10(actual))
+        expected = np.log10([expected[0], expected[-1]])
+        assert_allclose(actual, expected, atol=1)
+
+    def _assert_linear_range(self, actual, expected):
+        tol = (expected[-1] - expected[0]) * RANGE_TOLERANCE
+        assert_allclose(actual, [expected[0], expected[-1]], atol=tol)
+
+
+class TestCurveItem(_BasePlotTest):
+
+    def setUp(self):
+        super(TestCurveItem, self).setUp()
+        self._plot = self.widget.add_curve_item()
+
+    def test_basics(self):
+        self._set_data(x=X_ARRAY, y=LOG_ARRAY)
+        self._assert_data(x_expected=X_ARRAY, y_expected=LOG_ARRAY)
+        self._assert_range(x_expected=X_ARRAY, y_expected=LOG_ARRAY)
