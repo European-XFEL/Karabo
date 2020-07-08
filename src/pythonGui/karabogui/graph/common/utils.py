d7273cc5768f5dfca9b9e1d4b02eb429628da220
Cammille Carinan <cammille.carinan@xfel.eu>
2020-07-08 18:16:24 +0200

    GUI: More plot range fixes

--- src/pythonGui/karabogui/graph/common/utils.py
+++ src/pythonGui/karabogui/graph/common/utils.py
@@ -1,2 +1,17 @@
+import numpy as np
+
+MAX_PRECISION = -24
+
+
 def float_to_string(f):
     return "{:.2f}".format(f).rstrip('0').rstrip('.')
+
+
+def safe_log10(value):
+    """Return the max precision if we receive zero when evaluating a value
+       with log10. This is to protect the GUI from -inf."""
+
+    if value == 0:
+        return MAX_PRECISION
+
+    return np.log10(value)
