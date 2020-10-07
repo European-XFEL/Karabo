cb6a8b1c60f63eb779c039e1a7d8799dd5c043a9
Cammille Carinan <cammille.carinan@xfel.eu>
2020-10-07 19:10:40 +0200

    GUI: Substitute fonts with the Source Pro Family

---
+++ src/pythonGui/karabogui/tests/test_fonts.py
@@ -0,0 +1,24 @@
+from PyQt5.QtGui import QFont
+
+from ..fonts import FONT_STYLE_HINTS, get_font_from_string
+
+
+# This maps the font family to a Qt style hint to ease font replacement
+SUGGESTED_STYLE_HINTS = {
+    QFont.AnyStyle: "Sans Serif",
+    QFont.Helvetica: "Helvetica",
+    QFont.Times: "Times",
+    QFont.Courier: "Courier"
+}
+
+
+def test_get_font_from_string():
+    for style, family in FONT_STYLE_HINTS.items():
+        # Check if font is loaded
+        old_qfont = QFont(family)
+        assert old_qfont.family() == family
+
+        # Check if suggested font family is replaced by the application font
+        # accordingly
+        new_qfont = get_font_from_string(family)
+        assert new_qfont.family() == FONT_STYLE_HINTS[style]
