70972d0755ef9a76d04d83ebf99fa2c458283056
Martin Teichmann <martin.teichmann@xfel.eu>
2020-10-19 18:12:41 +0200

    remove references to async
    using async as a function name is deprecated. asyncio.ensure_future
    is a valid relacement. This should not change anything.

--- src/pythonKarabo/karabo/native/data/hash.py
+++ src/pythonKarabo/karabo/native/data/hash.py
@@ -6,7 +6,7 @@ The C++ types are mostly implemented by using the corresponding numpy type.
 """
 
 from asyncio import (
-    async, coroutine, get_event_loop, iscoroutinefunction)
+    coroutine, ensure_future, get_event_loop, iscoroutinefunction)
 import base64
 from collections import OrderedDict
 from collections.abc import Iterable
@@ -610,7 +610,7 @@ class Slot(Descriptor):
                 yield from device._onException(self, exc, tb)
                 device._ss.replyException(message, e)
 
-        return async(wrapper())
+        return ensure_future(wrapper())
 
     def _initialize(self, instance, value=None):
         return []  # nothing to initialize in a Slot
