629a8b4659412d5af65ad45e94e39b606e56accd
Dennis Goeries <dennis.goeries@xfel.eu>
2020-10-26 14:55:06 +0100

    GUI: Provide Histogram Controller for NDArrays

---
+++ src/pythonGui/karabogui/controllers/display/tests/test_array_hist_graph.py
@@ -0,0 +1,85 @@
+import numpy as np
+
+from ..array_hist_graph import NDArrayHistogramGraph, VectorHistogramGraph
+
+from karabo.native import Configurable, Double, Hash, VectorFloat, NDArray
+from karabogui.testing import (
+    GuiTestCase, get_class_property_proxy, set_proxy_value)
+
+
+class VectorObject(Configurable):
+    prop = VectorFloat(defaultValue=[2.3, 4.5, 7.9])
+
+
+class NDArrayObject(Configurable):
+    prop = NDArray(
+        defaultValue=[2.3, 4.5, 7.9, 1.0, 2.0, 3.0, 4.0, 3.0, 9.0, 10.1],
+        shape=(10,),
+        dtype=Double)
+
+
+class TestVectorHistGraph(GuiTestCase):
+    def setUp(self):
+        super(TestVectorHistGraph, self).setUp()
+        schema = VectorObject.getClassSchema()
+        self.proxy = get_class_property_proxy(schema, 'prop')
+        self.controller = VectorHistogramGraph(proxy=self.proxy)
+        self.controller.create(None)
+        self.assertIsNotNone(self.controller.widget)
+
+    def tearDown(self):
+        super(TestVectorHistGraph, self).tearDown()
+        self.controller.destroy()
+        self.assertIsNone(self.controller.widget)
+
+    def test_hist_basics(self):
+        self.controller.model.auto = False
+        self.controller.model.start = 0
+        self.controller.model.stop = 10
+        set_proxy_value(self.proxy, 'prop', [1, 2, 2, 3, 3, 3, 3, 8])
+        curve = self.controller._plot
+        x, y = curve.getData()
+        np.testing.assert_array_almost_equal(
+            x, [-0.5, 0.5, 1.5, 2.5, 3.5, 4.5, 5.5, 6.5, 7.5, 8.5, 9.5])
+        np.testing.assert_array_almost_equal(y, [0, 1, 2, 4, 0, 0, 0, 0, 1, 0])
+
+    def test_hist_empty(self):
+        self.controller.model.auto = False
+        self.controller.model.start = 0
+        self.controller.model.stop = 10
+        set_proxy_value(self.proxy, 'prop', [])
+        curve = self.controller._plot
+        x, y = curve.getData()
+        self.assertIsNone(x)
+        self.assertIsNone(y)
+
+
+class TestsNDArrayHistGraph(GuiTestCase):
+    def setUp(self):
+        super(TestsNDArrayHistGraph, self).setUp()
+        schema = NDArrayObject.getClassSchema()
+        self.proxy = get_class_property_proxy(schema, 'prop')
+        self.controller = NDArrayHistogramGraph(proxy=self.proxy)
+        self.controller.create(None)
+        self.assertIsNotNone(self.controller.widget)
+
+    def tearDown(self):
+        super(TestsNDArrayHistGraph, self).tearDown()
+        self.controller.destroy()
+        self.assertIsNone(self.controller.widget)
+
+    def test_hist_basics(self):
+        self.controller.model.auto = False
+        self.controller.model.start = 0
+        self.controller.model.stop = 10
+        value = np.array([1.0, 2.0, 2.0, 3.0, 3.0, 3.0, 3.0, 8.1, 9.2, 10.1],
+                         dtype=np.float)
+        array_hash = Hash('type', 20,
+                          'data', value.tobytes())
+        set_proxy_value(self.proxy, 'prop', array_hash)
+
+        curve = self.controller._plot
+        x, y = curve.getData()
+        np.testing.assert_array_almost_equal(
+            x, [-0.5, 0.5, 1.5, 2.5, 3.5, 4.5, 5.5, 6.5, 7.5, 8.5, 9.5])
+        np.testing.assert_array_almost_equal(y, [9, 1, 9, 0, 0, 0, 0, 0, 0, 0])
