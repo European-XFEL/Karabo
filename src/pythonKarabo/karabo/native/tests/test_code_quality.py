69a4c8386d6b594721728df8319820b83465e6ec
Dennis Goeries <dennis.goeries@xfel.eu>
2020-11-04 11:01:05 +0100

    MDL: Code quality check in native

---
+++ src/pythonKarabo/karabo/native/tests/test_code_quality.py
@@ -0,0 +1,12 @@
+import os.path as op
+import subprocess
+import sys
+
+import karabo.native as native
+
+
+def test_code_quality_flake8():
+    # Just run flake8 as if from the commandline
+    command = [sys.executable, '-m', 'flake8',
+               op.dirname(op.abspath(native.__file__))]
+    subprocess.check_call(command)
