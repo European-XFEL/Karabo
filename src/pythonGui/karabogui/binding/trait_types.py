4033109b1ef323d09065c7fcf8a6d62428299e17
Dennis Goeries <dennis.goeries@xfel.eu>
2020-12-03 19:34:55 +0100

    GUI: Provide a float binding preserving float type

--- src/pythonGui/karabogui/binding/trait_types.py
+++ src/pythonGui/karabogui/binding/trait_types.py
@@ -1,4 +1,5 @@
-from traits.api import Range
+import numpy as np
+from traits.api import BaseFloat, Range
 
 
 class NumpyRange(Range):
@@ -17,3 +18,33 @@ class NumpyRange(Range):
         """
         ret = super().validate(object, name, value)
         return self._dtype(ret)
+
+
+def _validate_float(value):
+    """ Convert a Python object to a float, or raise TypeError.
+
+    Note: This validator preserves the numpy `dtype` if present
+    """
+    value_type = type(value)
+    if value_type in (float, np.float32, np.float64):
+        return value
+    else:
+        try:
+            nb_float = value_type.__float__
+        except AttributeError:
+            raise TypeError(
+                "Object of type {!r} not convertible to float".format(
+                    type(value)))
+        return nb_float(value)
+
+
+class Float(BaseFloat):
+    """ A fast-validating trait type who can handle numpy dtypes
+    """
+
+    def validate(self, object, name, value):
+        """ Validates that a specified value is valid for this trait."""
+        try:
+            return _validate_float(value)
+        except TypeError:
+            self.error(object, name, value)
