56ee97c97f1ae4f9648b857ca5e6d4f616f00538
Dennis Goeries <dennis.goeries@xfel.eu>
2020-12-17 17:11:18 +0100

    GUI: Dialogs get an own ui folder

---
+++ src/pythonGui/karabogui/dialogs/utils.py
@@ -0,0 +1,9 @@
+import os.path as op
+
+
+def get_dialog_ui(ui_name):
+    """Retrieve the appropriate `.ui` file from the dialog ui folder
+    """
+    assert ui_name.endswith(".ui"), f"{ui_name} is not a `.ui` file"
+
+    return op.join(op.abspath(op.dirname(__file__)), "ui", ui_name)
