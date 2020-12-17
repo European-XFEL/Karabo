366baa155c5320cee72459ebc76e81ce6c3ad109
Dennis Goeries <dennis.goeries@xfel.eu>
2020-12-17 17:11:18 +0100

    GUI: Dialogs get an own ui folder

--- src/pythonGui/setup.py
+++ src/pythonGui/setup.py
@@ -26,7 +26,7 @@ if __name__ == '__main__':
             "karabogui.graph.plots.dialogs": ["*.ui"],
             "karabogui.graph.common.dialogs": ["*.ui"],
             "karabogui.graph.image.dialogs": ["*.ui"],
-            "karabogui.dialogs": ["*.ui", 'tests/*.html'],
+            "karabogui.dialogs": ["ui/*.ui", "tests/*.html"],
             "karabogui.fonts": ["*.*", "*/*"],
             "karabogui.icons": ["*.*", "statefulicons/iconset/*.svg"],
             "karabogui.panels": ["*.ui"],
