2bebd493e15fb695168c95595976742254d6cc66
Alessandro Silenzi <alessandro.silenzi@xfel.eu>
2021-01-29 13:24:45 +0100

    PYTHON: make project_db modular

--- src/pythonKarabo/karabo/project_db/exist_db/database.py
+++ src/pythonKarabo/karabo/project_db/exist_db/database.py
@@ -7,16 +7,15 @@ from time import gmtime, strftime, strptime
 
 from eulexistdb.db import ExistDB
 from eulexistdb.exceptions import ExistDBException
-from lxml import etree
 
-from .const import LIST_DOMAINS_QUERY
-from .util import assure_running, ProjectDBError
+from ..bases import DatabaseBase
+from .util import assure_running, LIST_DOMAINS_QUERY, ProjectDBError
 from .dbsettings import DbSettings
 
 DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
 
 
-class ProjectDatabase(ContextDecorator):
+class ProjectDatabase(DatabaseBase):
 
     def __init__(self, user, password, server=None, port=None,
                  test_mode=False, init_db=False):
@@ -39,9 +38,10 @@ class ProjectDatabase(ContextDecorator):
            will be added if missing on context entry.
         :return: a ProjectDatabase context
         """
+        super().__init__()
         if test_mode:
             server = os.getenv('KARABO_TEST_PROJECT_DB', 'localhost')
-            port = os.getenv('KARABO_TEST_PROJECT_DB_PORT', 8080)
+            port = os.getenv('KARABO_TEST_PROJECT_DB_PORT', 8181)
 
         # get our environment straightened out
         if server is None:
@@ -52,93 +52,15 @@ class ProjectDatabase(ContextDecorator):
                                    " found, nor was a server to connect to"
                                    " given!")
         if port is None:
-            port = os.getenv('KARABO_PROJECT_DB_PORT', 8080)
+            port = os.getenv('KARABO_PROJECT_DB_PORT', 8181)
 
+        port = int(port)
         self.settings = DbSettings(user, password, server, port, init_db)
         self.root = (self.settings.root_collection if not test_mode else
                      self.settings.root_collection_test)
 
-    def __enter__(self):
-        # assure there is a database running where we assume one would be
-        self.dbhandle = assure_running(self.settings)
-        return self
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        """
-        Clean-up action for the ProjectDatabase context. As the database
-        handle doesn't carry any state nothing needs to be done here.
-        :param exc_type:
-        :param exc_val:
-        :param exc_tb:
-        :return:
-        """
-        # nothing to do as dbhandle does not keep the connection open
-        pass
-
-    def domain_exists(self, domain):
-        """
-        Checks if a given domain exists.
-        :param domain: the domain to check
-        :return: True if it exists, false otherwise
-        :raises: ExistDBException if user is not authorized for this query
-        """
-        path = "{}/{}".format(self.root, domain)
-        return self.dbhandle.hasCollection(path)
-
-    def add_domain(self, domain):
-        """
-        Adds a domain to the project database. A domain is a top-level
-        collection located directly underneath the self.root collection. When
-        created the following collections will be added to the domain:
-        projects, scenes, macros, configs, device_servers, and resources.
-
-        :param domain: the name of the domain to be created
-        :return:None
-        :raises: ExistDBException if user is not authorized to create this
-                 collection. RuntimeError if domain creation failed otherwise
-        """
-        path = "{}/{}".format(self.root, domain)
-        success = self.dbhandle.createCollection(path)
-
-        if not success:
-            raise RuntimeError("Failed to create domain at {}".format(path))
-
-    @staticmethod
-    def _make_xml_if_needed(xml_rep):
-        """
-        Returns an etree xml object from xml_rep
-        :param xml_rep: the xml
-        :return: a root node for the xml object
-        :raises: ValueError if the object passed is not of type str or type
-                 etree.ElementBase
-        """
-        if isinstance(xml_rep, etree._Element):
-            return xml_rep
-        if isinstance(xml_rep, str):
-            try:
-                return etree.fromstring(xml_rep)
-            except etree.XMLSyntaxError:
-                raise ValueError("XML syntax error encountered while parsing!")
-        raise ValueError("Cannot handle type {}".format(type(xml_rep)))
-
-    @staticmethod
-    def _make_str_if_needed(xml_rep):
-        """
-        Returns a string representation of xml_rep
-        :param xml_rep: the xml
-        :return: a string representation of xml_rep
-        :raises: ValueError if the object passed is not of type str or type
-                 etree.ElementBase
-        """
-        if isinstance(xml_rep, str):
-            return xml_rep
-        if isinstance(xml_rep, etree._Element):
-            xml_bytes = etree.tostring(xml_rep, pretty_print=True,
-                                       encoding="unicode",
-                                       xml_declaration=False)
-            return xml_bytes
-
-        raise ValueError("Cannot handle type {}".format(type(xml_rep)))
+    def onEnter(self):
+        return assure_running(self.settings)
 
     def _check_for_modification(self, domain, uuid, old_date):
         """ Check whether the item with of the given `domain` and `uuid` was
@@ -180,95 +102,6 @@ class ProjectDatabase(ContextDecorator):
             modified = True
             reason = "Versioning conflict! Document modified inbetween."
         return modified, reason
-
-    def save_item(self, domain, uuid, item_xml, overwrite=False):
-        """
-        Saves a item xml file into the domain. It will
-        create a new entry if the item does not exist yet, or create a new
-        version of the item if it does exist. In case of a versioning
-        conflict the update will fail and the most current version of the
-        item file is returned.
-
-        The root node of the xml should contain a `item_type` entry identifying
-        the type of the item as one of the following:
-
-        'projects', 'scenes', 'macros', 'device_configs', 'device_servers'
-
-        If domain does not exist it is created given a user has appropriate
-        access rights.
-
-        :param domain: the domain under which this item is to be stored
-        :param uuid: the item's uuid
-        :param item_xml: the xml containing the item information
-        :param overwrite: defaults to False. If set to True versioning
-                          information is removed prior to database injection,
-                          allowing to overwrite in case of versioning
-                          conflicts.
-
-        :return: (True, dict) if successful, (false, dict) with dict
-                 representing the item's versioning information. In case of
-                 version conflict this will contain the information of the
-                 *newest existing* entry in the database. If saving was
-                 successful (no conflict), it will contain the updated
-                 versioning information after saving the item.
-
-
-        :raises: ExistDBException if user is not authorized for this query.
-            RuntimeError if item saving failed otherwise.
-            AttributeError if a non-supported type is passed
-        """
-
-        # create domain if necessary
-        if not self.domain_exists(domain):
-            self.add_domain(domain)
-
-        # Extract some information
-        try:
-            # NOTE: The client might send us garbage
-            item_tree = self._make_xml_if_needed(item_xml)
-        except ValueError:
-            msg = 'XML parse error for item "{}"'.format(uuid)
-            raise ProjectDBError(msg)
-
-        if 'user' not in item_tree.attrib:
-            item_tree.attrib['user'] = 'Karabo User'
-        if not item_tree.attrib.get('date'):
-            item_tree.attrib['date'] = strftime(DATE_FORMAT, gmtime())
-        else:
-            modified, reason = self._check_for_modification(
-                domain, uuid, item_tree.attrib['date'])
-            if modified:
-                message = "The <b>{}</b> item <b>{}</b> could not be saved: " \
-                          "{}".format(item_tree.attrib.get('item_type', ''),
-                                      item_tree.attrib.get('simple_name', ''),
-                                      reason)
-                raise ProjectDBError(message)
-            # Update time stamp
-            item_tree.attrib['date'] = strftime(DATE_FORMAT, gmtime())
-
-        # XXX: Add a revision/alias to keep old code from blowing up
-        item_tree.attrib['revision'] = '0'
-        item_tree.attrib['alias'] = 'default'
-
-        item_xml = self._make_str_if_needed(item_tree)
-        # XXX: Add a '_0' suffix to keep old code from wetting its pants
-        path = "{}/{}/{}_0".format(self.root, domain, uuid)
-
-        try:
-            if self.dbhandle.hasDocument(path) and not overwrite:
-                raise ExistDBException("Versioning conflict. Document exists!")
-            # NOTE: The underlying HTTP code needs bytes here...
-            success = self.dbhandle.load(item_xml.encode('utf8'), path)
-        except ExistDBException as e:
-            raise ProjectDBError(e)
-        if not success:
-            raise ProjectDBError("Saving item failed!")
-
-        meta = {}
-        meta['domain'] = domain
-        meta['uuid'] = uuid
-        meta['date'] = item_tree.attrib['date']
-        return meta
 
     def load_item(self, domain, items):
         """
@@ -494,7 +327,7 @@ class ProjectDatabase(ContextDecorator):
         try:
             res = self.dbhandle.query(query)
         except ExistDBException as e:
-            raise ("Failed creating backup: {}".format(e))
+            raise ProjectDBError("Failed creating backup: {}".format(e))
 
         print("Sucessfully created backup, now proceeding with sanitizing")
 
@@ -546,7 +379,7 @@ class ProjectDatabase(ContextDecorator):
         try:
             res = self.dbhandle.query(query).results[0]
         except ExistDBException as e:
-            raise ("Failed sanitizing: {}".format(e))
+            raise ProjectDBError("Failed sanitizing: {}".format(e))
 
         msg = dedent("""
             Sanitized database:
@@ -586,8 +419,7 @@ class ProjectDatabase(ContextDecorator):
             uuid = it.get('uuid')
             attr_name = it.get('attr_name')
             attr_value = it.get('attr_value')
-            # XXX: Add a '_0' suffix to keep old code from wetting its pants
-            path = "{}/{}/{}_0".format(self.root, domain, uuid)
+            path = self.path(domain, uuid)
             query = """
                 xquery version "3.0";
 
@@ -614,7 +446,7 @@ class ProjectDatabase(ContextDecorator):
                                   'attr_name': root.attrib['attr_name'],
                                   'attr_value': root.attrib['attr_value']})
             except ExistDBException as e:
-                raise ("Failed updating attribute: {}".format(e))
+                raise ProjectDBError("Failed updating attribute: {}".format(e))
         return res_items
 
     def get_configurations_from_device_name(self, domain, instance_id):
@@ -692,27 +524,3 @@ class ProjectDatabase(ContextDecorator):
             projects |= set([r.attrib["projectname"] for r in
                              res.results[0].getchildren()])
         return projects
-
-    def get_projects_with_conf(self, domain, device_id):
-        """
-        Returns a dict with projects and active configurations from a device
-        name.
-
-        :param domain: DB domain
-        :param device_id: the device to return the information for.
-        :return: a dict:
-            {"project name": configuration uuid,
-             ...}
-        """
-        try:
-            configs = self.get_configurations_from_device_name(domain,
-                                                               device_id)
-            projects = dict()
-            for config in configs:
-                instance_id = config["instanceid"]
-                for project in self.get_projects_from_device(domain,
-                                                             instance_id):
-                    projects[project] = config["configid"]
-            return projects
-        except ExistDBException as e:
-            raise ProjectDBError(e)
