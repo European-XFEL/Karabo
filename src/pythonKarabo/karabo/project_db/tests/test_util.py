81e8f83e08f58349988db4ce6a8f0b089b0e757b
Alessandro Silenzi <alessandro.silenzi@xfel.eu>
2021-02-11 14:36:57 +0100

    Project DB: create an injectable project_db node

---
+++ src/pythonKarabo/karabo/project_db/tests/test_util.py
@@ -0,0 +1,22 @@
+from unittest import TestCase
+
+from karabo.middlelayer import Configurable, Node
+
+from ..bases import DatabaseBase
+from ..util import DEFAULT_PROTOCOL, get_node, get_project
+
+
+class TestPluginLoading(TestCase):
+    def test_node(self):
+        node = get_node()
+        self.assertTrue(isinstance(node, Node))
+
+    def test_project_default(self):
+        class Manager(Configurable):
+            node = get_node()
+
+        manager = Manager()
+        project = get_project(manager.node)
+        self.assertTrue(isinstance(project, DatabaseBase))
+        self.assertEqual(manager.node.protocol, DEFAULT_PROTOCOL)
+        self.assertFalse(manager.node.testMode)
