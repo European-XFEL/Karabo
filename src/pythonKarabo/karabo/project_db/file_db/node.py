d4551ef856dcbcc4029c90f461738abc3d2e2346
Alessandro Silenzi <alessandro.silenzi@xfel.eu>
2021-02-11 14:36:57 +0100

    Project DB: create an injectable project_db node

---
+++ src/pythonKarabo/karabo/project_db/file_db/node.py
@@ -0,0 +1,12 @@
+from karabo.native import String
+from karabo.project_db.util import DbConnectionNodeBase
+
+from .database import ProjectDatabase
+
+
+class DbConnectionNode(DbConnectionNodeBase):
+    dir = String(defaultValue="projectDB")
+
+    def get_db(self, test_mode=False, init_db=False):
+        return ProjectDatabase(
+            dir=self.dir, test_mode=test_mode, init_db=init_db)
