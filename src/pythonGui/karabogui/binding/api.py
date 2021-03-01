98fcd3bd34984cac3fee32c0200b5dd67f15d28d
Dennis Goeries <dennis.goeries@xfel.eu>
2021-03-01 14:46:36 +0100

    GUI: Sanitize table configuration from projects

--- src/pythonGui/karabogui/binding/api.py
+++ src/pythonGui/karabogui/binding/api.py
@@ -2,8 +2,9 @@
 from .builder import build_binding
 from .config import (
     apply_configuration, apply_default_configuration, apply_fast_data,
-    extract_attribute_modifications, extract_configuration, extract_edits,
-    extract_sparse_configurations, validate_table_value, validate_value
+    apply_project_configuration, extract_attribute_modifications,
+    extract_configuration, extract_edits, extract_sparse_configurations,
+    validate_table_value, validate_value
 )
 from .proxy import (
     BaseDeviceProxy, DeviceProxy, DeviceClassProxy, ProjectDeviceProxy,
