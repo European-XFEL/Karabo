4447ac5b0821d5dfb9f6510ad838eec3694afd4f
Dennis Goeries <dennis.goeries@xfel.eu>
2021-03-16 16:34:06 +0100

    GUI: Cleanup image utils

--- src/pythonGui/karabogui/graph/image/tests/test_utils.py
+++ src/pythonGui/karabogui/graph/image/tests/test_utils.py
@@ -1,8 +1,7 @@
 import numpy as np
-from numpy.testing import assert_almost_equal
 import pytest
 
-from ..utils import bytescale, rescale
+from karabogui.graph.image.api import bytescale, rescale
 
 SIZE = 10
 LOW, HIGH = (-10.0, 10.0)
@@ -10,6 +9,27 @@ PRECISION_TOLERANCE = 4
 
 
 def test_rescale():
+    """Test the rescale function of the image utils"""
+
+    def _assert_dtype(dtype):
+        """Check according to different `dtype`"""
+        min_level, max_level = (0, 127)
+        image = np.linspace(min_level, max_level, endpoint=True,
+                            num=SIZE, dtype=dtype)
+
+        # Check rescale with the low and high as the data extrema.
+        # Note: It should not rescale!
+        rescaled = rescale(image, min_level, max_level,
+                           low=min_level, high=max_level)
+        # Use `array_almost_equal` here to avoid precision inequality
+        np.testing.assert_almost_equal(image, rescaled,
+                                       decimal=PRECISION_TOLERANCE)
+
+        # Check different low and high
+        rescaled = rescale(image, min_level, max_level, low=LOW, high=HIGH)
+        assert rescaled[0] == LOW
+        assert rescaled[-1] == HIGH
+
     _assert_dtype(np.uint8)
     _assert_dtype(np.int8)
     _assert_dtype(np.uint32)
@@ -17,33 +37,17 @@ def test_rescale():
     _assert_dtype(np.float32)
 
 
-def _assert_dtype(dtype):
-    """Check the rescale on varying dtype. We use `array_almost_equal`
-     here to avoid precision inequality"""
-    min_level, max_level = (0, 127)
-    image = np.linspace(min_level, max_level, endpoint=True,
-                        num=SIZE, dtype=dtype)
-
-    # Check rescale with the low and high as the data extrema.
-    # It should not rescale
-    rescaled = rescale(image, min_level, max_level,
-                       low=min_level, high=max_level)
-    assert_almost_equal(image, rescaled, decimal=PRECISION_TOLERANCE)
-
-    # Check different low and high
-    rescaled = rescale(image, min_level, max_level, low=LOW, high=HIGH)
-    assert rescaled[0] == LOW
-    assert rescaled[-1] == HIGH
-
-
 def test_bytescale():
+    """Test the `bytescale` function of the image utils"""
+
     min_level, max_level = (0, 127)
     image = np.linspace(min_level, max_level, endpoint=True,
                         num=SIZE, dtype=np.uint8)
 
     bytescaled = bytescale(image, cmin=min_level, cmax=max_level,
                            low=min_level, high=max_level)
-    assert_almost_equal(image, bytescaled, decimal=PRECISION_TOLERANCE)
+    np.testing.assert_almost_equal(image, bytescaled,
+                                   decimal=PRECISION_TOLERANCE)
 
     bytescaled = bytescale(image, cmin=min_level, cmax=max_level,
                            low=5, high=100)
