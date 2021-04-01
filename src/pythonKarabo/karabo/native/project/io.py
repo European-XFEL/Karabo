6d07d4f2b8f5f8b6321835dee0c1382b9a74ac06
Dennis Goeries <dennis.goeries@xfel.eu>
2021-04-01 19:04:12 +0200

    MDL: Rename karabo_hash to data folder

--- src/pythonKarabo/karabo/native/project/io.py
+++ src/pythonKarabo/karabo/native/project/io.py
@@ -12,7 +12,7 @@ from karabo.common.project.api import (
     write_device_server
 )
 from karabo.common.scenemodel.api import SceneModel, read_scene, write_scene
-from karabo.native.karabo_hash import decodeXML, encodeXML, Hash
+from karabo.native.data import decodeXML, encodeXML, Hash
 
 _ITEM_TYPES = {
     DeviceConfigurationModel: PROJECT_DB_TYPE_DEVICE_CONFIG,
