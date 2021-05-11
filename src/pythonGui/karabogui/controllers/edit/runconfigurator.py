65766e8b29c938a2374fb2f6ae5929e19b5e17cb
Dennis Goeries <dennis.goeries@xfel.eu>
2021-05-11 14:25:27 +0200

    GUI: Fix runconfigurator widget parenting

--- src/pythonGui/karabogui/controllers/edit/runconfigurator.py
+++ src/pythonGui/karabogui/controllers/edit/runconfigurator.py
@@ -36,7 +36,7 @@ class RunConfiguratorEdit(BaseBindingController):
     def create_widget(self, parent):
         widget = QTreeView(parent=parent)
         widget.setUniformRowHeights(True)
-        item_model = QStandardItemModel(parent=self.widget)
+        item_model = QStandardItemModel(parent=widget)
         item_model.setHorizontalHeaderLabels(HEADER_LABELS)
         item_model.itemChanged.connect(self._item_edited)
 
