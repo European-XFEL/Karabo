8041dc641864a6d18315795eeda594bd7599e35c
Dennis Goeries <dennis.goeries@xfel.eu>
2021-05-28 13:52:56 +0200

    Crack the GUI CI

--- conda-recipes/karabogui/_run_test.sh
+++ conda-recipes/karabogui/_run_test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+python -m pytest -v --disable-warnings --pyargs --junitxml=junit.karabo.common.xml karabo.common && \
+python -m pytest -v --disable-warnings --pyargs --junitxml=junit.karabo.native.xml karabo.native && \
 python -m pytest -v --disable-warnings --pyargs --junitxml=junit.karabogui.xml karabogui
-python -m pytest -v --disable-warnings --pyargs --junitxml=junit.karabo.native.xml karabo.native
-python -m pytest -v --disable-warnings --pyargs --junitxml=junit.karabo.common.xml karabo.common
+exit $?
