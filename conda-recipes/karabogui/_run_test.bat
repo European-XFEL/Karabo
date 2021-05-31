dcda25ed57ebe55cb71a3da457c7fbf405983906
Alessandro Silenzi <alessandro.silenzi@xfel.eu>
2021-05-31 12:16:55 +0200

    CI: make the GUI tests fails when necessary
    See merge request Karabo/Framework!5596
    
    (cherry picked from commit dd6ee239e4b34acfe68221e6d949f386c8819113)
    
    177ec169 GUI: Make CI return not 0 on failures
    b3127565 skip a couple more tests on mac
    4826385c nm
    d81b8053 skip this one too...
    3ec42ca5 more skipped stuff
    253c0677 more skipped stuff

--- conda-recipes/karabogui/_run_test.bat
+++ conda-recipes/karabogui/_run_test.bat
@@ -1,3 +1,6 @@
-python -m pytest -v --disable-warnings --pyargs --junitxml=junit.karabogui.xml karabogui
-python -m pytest -v --disable-warnings --pyargs --junitxml=junit.karabo.native.xml karabo.native
-python -m pytest -v --disable-warnings --pyargs --junitxml=junit.karabo.common.xml karabo.common
+@echo off 
+python -m pytest -v --disable-warnings --pyargs --junitxml=junit.karabo.common.xml karabo.common && (
+    python -m pytest -v --disable-warnings --pyargs --junitxml=junit.karabo.native.xml karabo.native && (
+        python -m pytest -v --disable-warnings --pyargs --junitxml=junit.karabogui.xml karabogui
+    )
+) || EXIT 1
