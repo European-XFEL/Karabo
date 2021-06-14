9ad42cc463104fad9c4a2ecf9c487e11ff18a6e5
Raul Costa <raul.costa@xfel.eu>
2021-06-14 11:37:10 +0200

    Set of template files for CMake-based device projects.

---
+++ src/templates/cpp/minimal-cmake/cmake/import_karabo_lib_legacy.cmake
@@ -0,0 +1,81 @@
+# Creates an IMPORTED target for the Karabo Framework Library by obtaining
+# link libraries dependencies from a "karaboDependencies.pc" file expected
+# at $KARABO/lib/karaboDependencies.pc.
+#
+# Also creates a variable, KARABO_LIB_TARGET_NAME, with the name of the
+# IMPORTED target created for the library.
+#
+# Note: this is aimed at versions of the Karabo Framework built using the
+# Netbeans generated Makefiles.
+macro (importKaraboLibLegacy)
+
+    if(NOT EXISTS "$ENV{KARABO}/lib/karaboDependencies.pc")
+        message(FATAL_ERROR
+                "'karaboDependencies.pc' file not found. Can't import Karabo Lib."
+        )
+    endif()
+
+    set(KARABO_LIB_TARGET_NAME "karabo")
+
+    find_library(
+        KARABO_LIB ${KARABO_LIB_TARGET_NAME}
+        PATHS $ENV{KARABO}/lib
+    )
+
+    # Retrieve the dependency libraries from $KARABO/lib/karaboDependencies.pc
+    set(KARABO_LIB_LINK_LIBRARIES "")
+    file(
+        STRINGS
+        "$ENV{KARABO}/lib/karaboDependencies.pc"
+        LINES_READ
+    )
+    foreach(LINE ${LINES_READ})
+        string(FIND ${LINE} "Libs: " LIBS_STR_POS)
+        if(LIBS_STR_POS EQUAL 0)
+            # Found the line of karaboDependencies with the dependency libs.
+            # Extracts them.
+            string(
+                REGEX MATCHALL
+                "-l[A-Za-z0-9]+[A-Za-z0-9_]*"
+                KARABO_DEPS_LIBS
+                ${LINE}
+            )
+            break()
+        endif()
+    endforeach()
+    # Gathers the dependency libs that are available at $KARABO/extern/lib.
+    # Assumes that other libs that are not in there, like libpthread, can
+    # be found from the current CMAKE_PREFIX_PATH value.
+    foreach(DEP_LIB ${KARABO_DEPS_LIBS})
+        string(REPLACE "-l" "lib" DEP_LIB_SO ${DEP_LIB})
+        string(APPEND DEP_LIB_SO ".so")
+        if(EXISTS "$ENV{KARABO}/extern/lib/${DEP_LIB_SO}")
+            list(
+                APPEND
+                KARABO_LIB_LINK_LIBRARIES
+                "$ENV{KARABO}/extern/lib/${DEP_LIB_SO}"
+            )
+        endif()
+    endforeach()
+    list(LENGTH KARABO_LIB_LINK_LIBRARIES KARABO_LIB_DEPS_COUNT)
+    if(${KARABO_LIB_DEPS_COUNT} EQUAL 0)
+        message(
+            FATAL_ERROR
+            "No dependency of 'libkarabo' found at '$ENV{KARABO}/extern/lib'."
+        )
+    endif()
+
+    add_library(${KARABO_LIB_TARGET_NAME} SHARED IMPORTED)
+
+    set_target_properties(${KARABO_LIB_TARGET_NAME} PROPERTIES
+        INTERFACE_COMPILE_DEFINITIONS "__SO__"
+        INTERFACE_COMPILE_OPTIONS "-Wfatal-errors;-Wno-unused-local-typedefs;-Wno-deprecated-declarations;-Wall"
+        INTERFACE_INCLUDE_DIRECTORIES "$ENV{KARABO}/include;$ENV{KARABO}/extern/include"
+        INTERFACE_LINK_LIBRARIES "${KARABO_LIB_LINK_LIBRARIES}"
+    )
+
+    set_target_properties(${KARABO_LIB_TARGET_NAME} PROPERTIES
+        IMPORTED_LOCATION ${KARABO_LIB}
+    )
+
+endmacro()
