f42f7239cf8d16b7e1c9d861080286501e15ca35
Dennis Goeries <dennis.goeries@xfel.eu>
2021-07-01 16:37:57 +0200

    GUI: Test the base application

---
+++ src/pythonGui/karabogui/programs/tests/test_base.py
@@ -0,0 +1,31 @@
+import os
+import sys
+from unittest import TestCase
+
+from qtpy.QtCore import QLocale
+
+from karabogui.const import IS_MAC_SYSTEM
+from karabogui.programs.base import create_gui_app, init_gui
+
+
+class TestMainApplication(TestCase):
+
+    def test_start_app(self):
+        """Make sure that we can start a gui application"""
+        os.environ["KARABO_TEST_GUI"] = "1"
+        app = create_gui_app(sys.argv)
+        init_gui(app, use_splash=True)
+        self.assertEqual(app.organizationName(), "XFEL")
+        self.assertEqual(app.organizationDomain(), "xfel.eu")
+        self.assertEqual(app.applicationName(), "KaraboGUI")
+
+        # Test standard settings
+        font = app.font()
+        self.assertEqual(font.family(), "Source Sans Pro")
+
+        psize = 10 if not IS_MAC_SYSTEM else 13
+        self.assertEqual(font.pointSize(), psize)
+
+        locale = QLocale()
+        self.assertEqual(locale.country(), QLocale.UnitedStates)
+        self.assertEqual(locale.language(), QLocale.English)
