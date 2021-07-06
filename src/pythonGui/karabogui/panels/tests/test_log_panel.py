3c5214ecb3ef2d92aa3d1634199e48c20616b5d6
Dennis Goeries <dennis.goeries@xfel.eu>
2021-07-06 14:38:21 +0200

    GUI: Test the log panel and log widget

---
+++ src/pythonGui/karabogui/panels/tests/test_log_panel.py
@@ -0,0 +1,36 @@
+from unittest import main
+
+from karabo.native import Hash
+from karabogui.events import KaraboEvent, broadcast_event
+from karabogui.panels.loggingpanel import LoggingPanel
+from karabogui.singletons.mediator import Mediator
+from karabogui.testing import GuiTestCase, singletons
+
+
+class TestLogPanel(GuiTestCase):
+
+    def test_log_panel(self):
+        """Test the logging panel"""
+
+        data = [Hash(
+            "timestamp", "2021-07-05T15:08:35.250",
+            "type", "INFO",
+            "category", "Karabo_GuiServer_1",
+            "message", "Hello!")]
+        info = {'messages': data}
+
+        mediator = Mediator()
+        with singletons(mediator=mediator):
+            panel = LoggingPanel()
+            broadcast_event(KaraboEvent.LogMessages, info)
+            self.process_qt_events()
+            self.assertEqual(panel._log_widget.queryModel.rowCount(None), 1)
+
+            info = {'status': False}
+            broadcast_event(KaraboEvent.NetworkConnectStatus, info)
+            self.process_qt_events()
+            self.assertEqual(panel._log_widget.queryModel.rowCount(None), 0)
+
+
+if __name__ == '__main__':
+    main()
