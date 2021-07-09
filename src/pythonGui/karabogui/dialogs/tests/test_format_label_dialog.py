4549b5b6157eacfb5fab5cf713f9e5d27725ccae
Dennis Goeries <dennis.goeries@xfel.eu>
2021-07-09 13:42:02 +0200

    GUI: Test the format label dialog

---
+++ src/pythonGui/karabogui/dialogs/tests/test_format_label_dialog.py
@@ -0,0 +1,22 @@
+from unittest import main
+
+from karabogui.dialogs.api import FormatLabelDialog
+from karabogui.testing import GuiTestCase
+
+
+class TestFormatLabelDialog(GuiTestCase):
+
+    def test_basic_dialog(self):
+        dialog = FormatLabelDialog()
+        assert dialog.font_size == 10
+        assert dialog.font_weight == "normal"
+
+        assert dialog.font_weight_combobox.count() == 2
+        assert dialog.font_size_combobox.count() == 17
+
+        dialog = FormatLabelDialog(font_size=12)
+        assert dialog.font_size == 12
+
+
+if __name__ == "__main__":
+    main()
