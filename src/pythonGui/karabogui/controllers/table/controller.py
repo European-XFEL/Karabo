19e137da5812e23a2230cd7207c95cbe3bb9ef39
Dennis Goeries <dennis.goeries@xfel.eu>
2021-07-16 13:25:00 +0200

    GUI: Enable to subclass table widgets for filter models

--- src/pythonGui/karabogui/controllers/table/controller.py
+++ src/pythonGui/karabogui/controllers/table/controller.py
@@ -28,6 +28,7 @@ class BaseTableController(BaseBindingController):
     _bindings = Dict
     _readonly = Bool(True)
     _item_model = WeakRef(TableModel, allow_none=True)
+    _table_widget = WeakRef(KaraboTableView)
 
     # ---------------------------------------------------------------------
     # Abstract Methods
@@ -35,38 +36,42 @@ class BaseTableController(BaseBindingController):
     def create_widget(self, parent):
         assert self.model is not Undefined
 
-        widget = KaraboTableView(parent=parent)
-        widget.setSelectionBehavior(QAbstractItemView.SelectItems
-                                    | QAbstractItemView.SelectRows)
-        widget.horizontalHeader().setStretchLastSection(True)
-        return widget
+        table_widget = KaraboTableView(parent=parent)
+        table_widget.setSelectionBehavior(QAbstractItemView.SelectItems
+                                          | QAbstractItemView.SelectRows)
+        table_widget.horizontalHeader().setStretchLastSection(True)
+
+        # Create an internal weak ref to subclass create widget
+        self._table_widget = table_widget
+
+        return table_widget
 
     def set_read_only(self, ro):
+        widget = self._table_widget
         if ro:
             self._readonly = True
-            self.widget.setEditTriggers(QAbstractItemView.NoEditTriggers)
-            self.widget.setAlternatingRowColors(True)
+            widget.setEditTriggers(QAbstractItemView.NoEditTriggers)
+            widget.setAlternatingRowColors(True)
             if self.hasCustomMenu:
-                self.widget.setContextMenuPolicy(Qt.CustomContextMenu)
-                self.widget.customContextMenuRequested.connect(
-                    self._custom_menu)
-                self.widget.setSelectionMode(QAbstractItemView.SingleSelection)
+                widget.setContextMenuPolicy(Qt.CustomContextMenu)
+                widget.customContextMenuRequested.connect(self._custom_menu)
+                widget.setSelectionMode(QAbstractItemView.SingleSelection)
             else:
-                self.widget.setSelectionMode(QAbstractItemView.NoSelection)
+                widget.setSelectionMode(QAbstractItemView.NoSelection)
         else:
             self._readonly = False
-            self.widget.setSelectionMode(QAbstractItemView.SingleSelection)
+            widget.setSelectionMode(QAbstractItemView.SingleSelection)
             flags = (QAbstractItemView.DoubleClicked
                      | QAbstractItemView.AnyKeyPressed
                      | QAbstractItemView.SelectedClicked)
-            self.widget.setEditTriggers(flags)
-            self.widget.setContextMenuPolicy(Qt.CustomContextMenu)
+            widget.setEditTriggers(flags)
+            widget.setContextMenuPolicy(Qt.CustomContextMenu)
             # A reconfigurable table element always has a menu
             handler = (self._custom_menu if self.hasCustomMenu
                        else self._context_menu)
-            self.widget.customContextMenuRequested.connect(handler)
-            self.widget.setAcceptDrops(True)
-        self.widget.setFocusPolicy(Qt.NoFocus if ro else Qt.ClickFocus)
+            widget.customContextMenuRequested.connect(handler)
+            widget.setAcceptDrops(True)
+        widget.setFocusPolicy(Qt.NoFocus if ro else Qt.ClickFocus)
         if self._item_model is not None:
             self._item_model.set_readonly(self._readonly)
             # We must set the delegates firstly on readOnly information!
@@ -124,13 +129,13 @@ class BaseTableController(BaseBindingController):
         if self._readonly:
             # If we are readOnly, we erase all edit delegates
             for column, key in enumerate(keys):
-                self.widget.setItemDelegateForColumn(column, None)
+                self._table_widget.setItemDelegateForColumn(column, None)
         else:
             # Create binding specific edit delegates
             for column, key in enumerate(keys):
                 binding = bindings[key]
-                delegate = get_table_delegate(binding, self.widget)
-                self.widget.setItemDelegateForColumn(column, delegate)
+                delegate = get_table_delegate(binding, self._table_widget)
+                self._table_widget.setItemDelegateForColumn(column, delegate)
 
     def custom_menu(self, pos):
         """Subclass method for own custom menu if ``hasCustomMenu`` is `True`
@@ -138,44 +143,32 @@ class BaseTableController(BaseBindingController):
         :param: pos: The position of the context menu event
         """
 
+    def createModel(self, item_model):
+        """Subclass the `createModel` to create a filter model"""
+        return item_model
+
+    # ---------------------------------------------------------------------
+    # Public interface
+
+    def currentIndex(self):
+        """Convenience method to get the currentIndex of the selection"""
+        return self._table_widget.selectionModel().currentIndex()
+
+    def sourceModel(self):
+        """Return the `sourceModel` of the table controller"""
+        return self._item_model
+
+    def tableWidget(self):
+        """Return the table widget of the controller"""
+        return self._table_widget
+
     def getBindings(self):
-        """Subclass access for table bindings of the controller"""
+        """Access for table bindings of the controller"""
         return self._bindings
 
     def isReadOnly(self):
-        """Subclass access for readOnly property of the table controller"""
+        """Access for readOnly property of the table controller"""
         return self._readonly
-
-    # ---------------------------------------------------------------------
-
-    def _on_user_edit(self, data):
-        """Callback method used by `self._item_model` when data changes"""
-        self.proxy.edit_value = data
-
-    def _set_bindings(self, binding):
-        """Configure the column schema hashes and keys
-
-        The schema must not be `None` and is protected when calling this func.
-        """
-        if self._item_model is not None:
-            self._item_model.setParent(None)
-            self._item_model = None
-
-        self._bindings = binding.bindings
-        self._item_model = TableModel(binding, self._on_user_edit,
-                                      parent=self.widget)
-        self._item_model.set_readonly(self._readonly)
-        self.widget.setModel(self._item_model)
-        self.widget.set_bindings(binding.bindings)
-        self.create_delegates()
-
-    def _custom_menu(self, pos):
-        """Private method to direct to the custom context menu"""
-        selection_model = self.widget.selectionModel()
-        if selection_model is None:
-            return
-
-        return self.custom_menu(pos)
 
     # ---------------------------------------------------------------------
     # Action Slots
@@ -198,12 +191,12 @@ class BaseTableController(BaseBindingController):
     def _move_row_up(self):
         row = self.currentIndex().row()
         self._item_model.move_row_up(row)
-        self.widget.selectRow(row - 1)
+        self._table_widget.selectRow(row - 1)
 
     def _move_row_down(self):
         row = self.currentIndex().row()
         self._item_model.move_row_down(row)
-        self.widget.selectRow(row + 1)
+        self._table_widget.selectRow(row + 1)
 
     def _remove_row(self):
         index = self.currentIndex()
@@ -211,14 +204,14 @@ class BaseTableController(BaseBindingController):
 
     def _context_menu(self, pos):
         """The custom context menu of a reconfigurable table element"""
-        selection_model = self.widget.selectionModel()
+        selection_model = self._table_widget.selectionModel()
         if selection_model is None:
             # Note: We did not yet receive a schema and thus have no table and
             # selection model!
             return
 
         index = selection_model.currentIndex()
-        menu = QMenu(parent=self.widget)
+        menu = QMenu(parent=self._table_widget)
         if index.isValid():
             set_default_action = menu.addAction('Set Cell Default')
             set_default_action.triggered.connect(self._set_index_default)
@@ -254,10 +247,37 @@ class BaseTableController(BaseBindingController):
             add_action = menu.addAction(icons.add, 'Add Row below')
             add_action.triggered.connect(self._add_row)
 
-        menu.exec_(self.widget.viewport().mapToGlobal(pos))
+        menu.exec_(self._table_widget.viewport().mapToGlobal(pos))
 
+    # Private interface
     # ---------------------------------------------------------------------
 
-    def currentIndex(self):
-        """Convenience method to get the currentIndex of the selection"""
-        return self.widget.selectionModel().currentIndex()
+    def _on_user_edit(self, data):
+        """Callback method used by `self._item_model` when data changes"""
+        self.proxy.edit_value = data
+
+    def _set_bindings(self, binding):
+        """Configure the column schema hashes and keys
+
+        The schema must not be `None` and is protected when calling this func.
+        """
+        if self._item_model is not None:
+            self._item_model.setParent(None)
+            self._item_model = None
+
+        self._bindings = binding.bindings
+        self._item_model = TableModel(binding, self._on_user_edit,
+                                      parent=self._table_widget)
+        self._item_model.set_readonly(self._readonly)
+        model = self.createModel(self._item_model)
+        self._table_widget.setModel(model)
+        self._table_widget.set_bindings(binding.bindings)
+        self.create_delegates()
+
+    def _custom_menu(self, pos):
+        """Private method to direct to the custom context menu"""
+        selection_model = self._table_widget.selectionModel()
+        if selection_model is None:
+            return
+
+        return self.custom_menu(pos)
