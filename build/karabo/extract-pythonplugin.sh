2fb840b0b3ccbb857dd213d006c2bd3153494855
Raul Costa <raul.costa@xfel.eu>
2021-08-10 09:34:40 +0200

    CMake support for auto_build_all (no dependency on build/netbeans) and some fixes to CMake projects.

---
+++ build/karabo/extract-pythonplugin.sh
@@ -0,0 +1,107 @@
+usage()
+{
+  cat <<EOF
+Usage: $0 [options]
+Options:
+  --help       print this message
+  --prefix=dir install directory
+EOF
+  exit 1
+}
+
+echo_exit()
+{
+  echo $1
+  exit 1
+}
+
+
+interactive=TRUE
+install_prefix_dir=""
+for a in $@; do
+  if echo $a | grep "^--prefix=" > /dev/null 2> /dev/null; then
+     install_prefix_dir=`echo $a | sed "s/^--prefix=//"`
+     install_prefix_dir=${install_prefix_dir/#\~/$HOME}
+     if [ ! -d ${install_prefix_dir} ]; then
+       mkdir -p  ${install_prefix_dir} ||  echo_exit "Cannot create directory ${install_prefix_dir}"
+     fi
+     install_prefix_dir=`cd "${install_prefix_dir}"; pwd`
+  fi
+  if echo $a | grep "^--help" > /dev/null 2> /dev/null; then
+    usage
+  fi
+done
+
+if [ "x${install_prefix_dir}x" != "xx" ]; then
+  interactive=FALSE
+fi
+echo
+echo " #####################################################################"
+echo "                                INSTALLATION"
+echo   
+echo "                       Karabo plugin: $PLUGINNAME"
+echo "       version: $VERSION, karaboFramework version: $KARABOVERSION"
+echo                                            
+echo " #####################################################################"
+echo
+installDir=$HOME/packages
+if [ -z $KARABO ]; then
+    echo "\$KARABO is not defined. Make sure you have sourced the activate script for the Karabo Framework which you would like to use."
+    exit 1
+else
+    LOCALKARABOVERSION=$(cat $KARABO/VERSION)
+    if [ "$LOCALKARABOVERSION" == "$KARABOVERSION" ]; then
+       installDir=$KARABO/plugins
+    else
+       echo "Plugin was compiled with different karaboFramework version"
+       echo "than installed one: $KARABOVERSION vs. $LOCALKARABOVERSION"
+       echo " "
+    fi
+fi
+
+if [ "x${install_prefix_dir}x" != "xx" ]
+then
+  installDir="${install_prefix_dir}"
+fi
+
+echo "This is a self-extracting archive."
+if [ "x${interactive}x" = "xTRUEx" ]
+then
+  read -e -p " Installation path [$installDir]: " dir
+  dir=${dir/#\~/$HOME}
+    # Always resolve to absolute path
+    #installDir=`[[ $dir = /* ]] && echo "$dir" || echo "$PWD/${dir#./}"`
+    #mkdir -p $installDir
+  if [ "x${dir}x" != "xx" ]; then
+     if [ ! -d ${dir} ]; then
+       mkdir -p  ${dir} ||  echo_exit "Cannot create directory $dir"
+     fi
+     installDir=`cd "${dir}"; pwd`
+  else
+     if [ ! -d ${installDir} ]; then
+       mkdir -p  ${installDir} ||  echo_exit "Cannot create directory $installDir"
+     fi
+  fi
+fi
+
+echo -n " Extracting files, please wait..."
+# searches for the line number where finish the script and start the .whl
+SKIP=`awk '/^__WHEELFILE_FOLLOWS__/ { print NR + 1; exit 0; }' $0`
+tail -n +$SKIP $0 | (cd  $installDir && cat - > $WHEELNAME) || echo_exit "Problem unpacking the file $0"
+echo  " unpacking finished successfully"
+# Any script here will happen after the wheel file is extracted.
+echo
+echo -n "Running wheel installation..."
+if [ ! -z $KARABO ]; then
+    PIP=$KARABO/extern/bin/pip
+    destPath=$installDir/$WHEELNAME
+    $PIP --disable-pip-version-check install -U --no-index --target $installDir $destPath
+fi
+echo " done."
+echo
+echo " Package was successfully installed to: $installDir"
+echo
+echo
+exit 0
+# NOTE: Don't place any newline characters after the last line below.
+__WHEELFILE_FOLLOWS__
---
+++ build/karabo/fix-python-scripts.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Script for fixing up the shebang lines of Python entry-point scripts of
+# Karabo entry points.
+#
+# Author: <john.wiggins@xfel.eu>
+#
+
+OS=$(uname -s)
+
+PACKAGEDIR=$1
+
+NEW_SHEBANG_LINE="#!/usr/bin/env python3"
+CAPTURE="#![^#]*python.*"
+SED_PROGRAM='1 s%^'$CAPTURE'$%'$NEW_SHEBANG_LINE'%'
+
+for script_name in $PACKAGEDIR/extern/bin/*
+do
+  if grep -s -e '^'$CAPTURE'$' $script_name > /dev/null
+  then
+    sed -i "$SED_PROGRAM" $script_name
+  fi
+done
