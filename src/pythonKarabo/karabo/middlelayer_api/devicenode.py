7505e9c0ba21023bff8715ed715233a95eaa3d35
Dennis Goeries <dennis.goeries@xfel.eu>
2021-08-25 09:27:37 +0200

    MDL: Remove device node properties and commands

--- src/pythonKarabo/karabo/middlelayer_api/devicenode.py
+++ src/pythonKarabo/karabo/middlelayer_api/devicenode.py
@@ -1,12 +1,9 @@
-from asyncio import (
-    CancelledError, ensure_future, TimeoutError, wait_for, Queue)
-from itertools import chain
+from asyncio import TimeoutError, wait_for
 
 from karabo.native import isSet, KaraboError, String
-from karabo.native import AccessMode, Assignment, NodeType, Hash
+from karabo.native import AccessMode, Assignment
 
 from .device_client import getDevice, updateDevice
-from .signalslot import coslot
 
 
 class DeviceNode(String):
@@ -36,72 +33,15 @@ class DeviceNode(String):
     wait until the proxy connectiion has been established!
     """
 
-    def __init__(self, properties=(), commands=(), timeout=None, **kwargs):
+    def __init__(self, timeout=None, **kwargs):
         super().__init__(**kwargs)
-        self.properties = properties
-        self.commands = commands
         self.timeout = timeout
-        if self.properties or self.commands:
-            import warnings
-            warnings.warn(
-                "Using `properties` or `commands` in a DeviceNode is "
-                "deprecated and will be removed in Karabo 2.12",
-                DeprecationWarning, stacklevel=2)
-            for default in ("deviceId", "state", "alarmCondition"):
-                if default not in self.properties:
-                    self.properties.append(default)
 
     def toDataAndAttrs(self, proxy):
-        if self.properties or self.commands:
-            return proxy._current, {}
-        else:
-            deviceId = proxy.deviceId
-            data, attrs = super().toDataAndAttrs(deviceId)
+        deviceId = proxy.deviceId
+        data, attrs = super().toDataAndAttrs(deviceId)
 
-            return data, attrs
-
-    def _copy_properties(self, data):
-        """return a Hash that contains our properties in Hash data"""
-        ret = Hash()
-        for key in self.properties:
-            val = data.get(key)
-            if val is not None:
-                attrs = data[key, ...]
-                ret[key] = val
-                ret[key, ...].update(attrs)
-
-        return ret
-
-    def _setter(self, instance, data):
-        proxy = instance.__dict__[self.key]
-
-        async def setter():
-            await instance.call(proxy._deviceId,
-                                "slotReconfigure", data)
-
-        return [setter]
-
-    async def _main_loop(self, proxy, instance):
-        """relay data coming from *proxy* on behalf of *instance*"""
-        try:
-            queue = Queue()
-            proxy._queues[None].add(queue)
-            proxy._current = Hash()
-            with proxy:
-                await updateDevice(proxy)
-                while True:
-                    data = await queue.get()
-                    out = self._copy_properties(data)
-                    if out.empty():
-                        continue
-                    proxy._current.merge(out)
-                    instance.signalChanged(Hash(self.key, out),
-                                           instance.deviceId)
-        except CancelledError:
-            raise
-        except Exception:
-            instance.logger.exception(
-                'device node "{}" failed'.format(self.key))
+        return data, attrs
 
     def checkedInit(self, instance, value=None):
         """Device Nodes cannot have an empty string
@@ -130,43 +70,15 @@ class DeviceNode(String):
                     'The DeviceNode with key "{}" timed out and could not '
                     'establish a proxy to "{}"'.format(self.key, value))
 
-            proxy._current = Hash()
             instance.__dict__[self.key] = proxy
-            if self.commands or self.properties:
-                instance._notifyNewSchema()
-
-            def register(command):
-                @coslot
-                async def slot():
-                    await instance._ss.request(proxy._deviceId, command)
-
-                instance._ss.register_slot(
-                    "{}.{}".format(self.key, command), slot)
-
-            for command in self.commands:
-                register(command)
-
             instance._ss.exitStack.enter_context((await updateDevice(proxy)))
-            if self.properties:
-                ensure_future(self._main_loop(proxy, instance))
 
         return [inner()]
 
     def toSchemaAndAttrs(self, device, state):
         h, attrs = super().toSchemaAndAttrs(device, state)
-        if device is None or (not self.properties and not self.commands):
-            attrs["accessMode"] = AccessMode.INITONLY.value
-            attrs["assignment"] = Assignment.MANDATORY.value
-            return h, attrs
-        attrs["nodeType"] = NodeType.Node
+        attrs["accessMode"] = AccessMode.INITONLY.value
+        attrs["assignment"] = Assignment.MANDATORY.value
         attrs["displayType"] = "deviceNode"
-
-        proxy = getattr(device, self.key, None)
-        h = Hash()
-        # test whether or not proxy is a NoneValue or None
-        if isSet(proxy):
-            for name in chain(self.properties, self.commands):
-                h[name] = proxy._schema_hash[name]
-                h[name, ...] = proxy._schema_hash[name, ...]
 
         return h, attrs
