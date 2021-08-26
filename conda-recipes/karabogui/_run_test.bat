ff00b602bd643f3ca805c0fa8ce500d86a4ff328
Alessandro Silenzi <alessandro.silenzi@xfel.eu>
2021-08-26 16:41:24 +0200

    CI: Output coverage report on GUI tests

--- conda-recipes/karabogui/_run_test.bat
+++
@@ -1,6 +0,0 @@
-@echo off 
-python -m pytest -v --disable-warnings --pyargs --junitxml=junit.karabo.common.xml karabo.common && (
-    python -m pytest -v --disable-warnings --pyargs --junitxml=junit.karabo.native.xml karabo.native && (
-        python -m pytest -v --disable-warnings --pyargs --junitxml=junit.karabogui.xml karabogui
-    )
-) || EXIT 1
