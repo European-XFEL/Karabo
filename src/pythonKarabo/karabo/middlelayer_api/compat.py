8d62254ba046b2421ac14448f17162d9df5524ec
Gero Flucke <gero.flucke@xfel.eu>
2021-09-21 11:29:54 +0200

    Make things work of KARABO_BROKER not defined
    e.g. "pip install -e ." for beckhoffsuplementary

--- src/pythonKarabo/karabo/middlelayer_api/compat.py
+++ src/pythonKarabo/karabo/middlelayer_api/compat.py
@@ -1,6 +1,12 @@
 import os
 
-amqp = os.environ["KARABO_BROKER"].startswith("amqp://")
-mqtt = os.environ["KARABO_BROKER"].startswith("mqtt://")
-redis = os.environ["KARABO_BROKER"].startswith("redis://")
-jms = os.environ["KARABO_BROKER"].startswith("tcp://")
+broker = os.environ.get("KARABO_BROKER")
+if broker:
+    amqp = broker.startswith("amqp://")
+    mqtt = broker.startswith("mqtt://")
+    redis = broker.startswith("redis://")
+    jms = broker.startswith("tcp://")
+else:
+    # For now, keep JMS as the default
+    amqp = mqtt = redis = False
+    jms = True
