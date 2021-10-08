484cd208d925fd382f8f814997588895aace3bc2
Dennis Goeries <dennis.goeries@xfel.eu>
2021-10-08 10:57:35 +0200

    GUI: Test the karabo cinema

--- src/pythonGui/karabogui/programs/cinema.py
+++ src/pythonGui/karabogui/programs/cinema.py
@@ -10,14 +10,7 @@ from karabogui.programs.base import create_gui_app, init_gui
 from karabogui.singletons.api import get_db_conn, get_network, get_topology
 
 
-def run_cinema(ns):
-    """The cinema is meant to directly download a scene from the project db
-
-    From the initial scene the operator is allowed to request additional
-    scene or device scene links.
-
-    All scenes have the name ProjectDB|SceneName and are not editable!
-    """
+def create_cinema(ns):
     app = create_gui_app(sys.argv)
     init_gui(app, use_splash=not ns.nosplash)
 
@@ -49,6 +42,8 @@ def run_cinema(ns):
             topology.system_tree.on_trait_change(
                 trigger_scenes, 'initialized', remove=True)
             app.quit()
+        else:
+            network.onSubscribeLogs(False)
 
     # We might want to connect directly to the gui server
     if ns.host and ns.port:
@@ -59,14 +54,23 @@ def run_cinema(ns):
         # Connect to the GUI Server via dialog
         success = network.connectToServer()
 
+    return success, app
+
+
+def run_cinema(ns):
+    """The cinema is meant to directly download a scene from the project db
+
+    From the initial scene the operator is allowed to request additional
+    scene or device scene links.
+
+    All scenes have the name ProjectDB|SceneName and are not editable!
+    """
+    success, app = create_cinema(ns)
     if success:
         app.exec_()
         app.deleteLater()
         sys.exit()
     else:
-        # If we are not successful in connection, we don't leave a remnant!
-        topology.system_tree.on_trait_change(
-            trigger_scenes, 'initialized', remove=True)
         app.quit()
 
 
