543e963d43ea572af5849f6d2078ad91e37c51b6
Dennis Goeries <dennis.goeries@xfel.eu>
2021-10-20 16:55:07 +0200

    GUI: Increase priority of default controllers for alarm and state

--- src/pythonGui/karabogui/controllers/display/statecolor.py
+++ src/pythonGui/karabogui/controllers/display/statecolor.py
@@ -15,7 +15,7 @@ from karabogui.widgets.hints import FrameWidget
 @register_binding_controller(ui_name='State Color Field',
                              klassname='DisplayStateColor',
                              binding_type=StringBinding,
-                             priority=20,
+                             priority=90,
                              is_compatible=with_display_type('State'),
                              can_show_nothing=False)
 class DisplayStateColor(BaseBindingController):
