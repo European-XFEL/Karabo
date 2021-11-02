b141400fd56e005d4cf1515ac516afca95e6aa0b
Alessandro Silenzi <alessandro.silenzi@xfel.eu>
2021-11-02 22:08:25 +0100

    INFLUX: add a filter for strings larger than 64kB

--- src/karabo/devices/InfluxDataLogger.cc
+++ src/karabo/devices/InfluxDataLogger.cc
@@ -32,6 +32,7 @@ namespace karabo {
             , m_serializer(karabo::io::BinarySerializer<karabo::util::Hash>::create("Bin"))
             , m_archive()
             , m_maxTimeAdvance(input.get<int>("maxTimeAdvance"))
+            , m_maxStringSize(input.get<unsigned int>("maxStringSize"))
             , m_hasRejectedData(false) {
         }
 
@@ -77,7 +78,7 @@ namespace karabo {
 
             // store the local unix timestamp to compare the time difference w.r.t. incoming data.
             Epochstamp nowish;
-            std::vector<std::string> rejectedPaths;
+            std::ostringstream rejectionStream;
             // To write log I need schema - but that has arrived before connecting signal[State]Changed to slotChanged
             // and thus before any data can arrive here in handleChanged.
             std::vector<std::string> paths;
@@ -119,10 +120,10 @@ namespace karabo {
 
                 // no check needed if the maxTimeDifference is negative or 0
                 if (m_maxTimeAdvance > 0 && t.getEpochstamp() > nowish) {
-                    // substract the 2 Epochstamp to get a TimeDuration.
+                    // substract the Epochstamps to get a TimeDuration.
                     const double dt = t.getEpochstamp() - nowish;
                     if ( dt > m_maxTimeAdvance) {
-                        rejectedPaths.push_back(path);
+                        rejectionStream << "path '" << path << "' rejected due to being too much in the future : current " << dt << " - max "<< m_maxTimeAdvance;
                         continue;
                     }
                 }
@@ -212,15 +213,20 @@ namespace karabo {
                     m_dbClientWrite->enqueueQuery(ss.str());
                 }
 
+                // no check needed if the m_maxStringSize is 0
+                if (m_maxStringSize > 0 && value.size() > m_maxStringSize) {
+                    rejectionStream << "path '" << path << "' rejected due to being larger than the size limit : current " << value.size()<< " - max "<< m_maxStringSize;
+                    continue;
+                }
                 // isFinite matters only for FLOAT/DOUBLE
                 logValue(query, deviceId, path, value, leafNode.getType(), isFinite);
             }
 
-            if (!m_hasRejectedData && rejectedPaths.size() != 0ul) {
-                KARABO_LOG_FRAMEWORK_WARN << "Skip properties '" << toString(rejectedPaths) << "' of '" << deviceId
-                    << "' - Since they are too far in the future.";
+            const std::string rejectionMessage = rejectionStream.str();
+            if (!m_hasRejectedData && rejectionMessage.size() != 0ul) {
+                KARABO_LOG_FRAMEWORK_WARN << rejectionMessage;
             }
-            m_hasRejectedData = rejectedPaths.size() != 0ul;
+            m_hasRejectedData = rejectionMessage.size() != 0ul;
 
             terminateQuery(query, lineTimestamp);
 
@@ -459,13 +465,23 @@ namespace karabo {
                     .init()
                     .commit();
 
+            UINT32_ELEMENT(expected).key("maxStringSize")
+                    .displayedName("Max String Size")
+                    .description("Maximum size of a value serialized as a string. "
+                                 "Influxdb has a limit of 64kB defined in its documentation. "
+                                 "Hence the default value. 0 means no limit.")
+                    .assignmentOptional().defaultValue(64000)
+                    .init()
+                    .commit();
+
         }
 
 
         InfluxDataLogger::InfluxDataLogger(const karabo::util::Hash& input)
             : DataLogger(input)
             , m_dbName(input.get<std::string>("dbname"))
-            , m_maxTimeAdvance(input.get<int>("maxTimeAdvance")) {
+            , m_maxTimeAdvance(input.get<int>("maxTimeAdvance"))
+            , m_maxStringSize(input.get<unsigned int>("maxStringSize")) {
 
             // We have to work in cluster environment where we have 2 nodes and proxy
             // that runs 'telegraf' working as a proxy and load balancer
@@ -559,6 +575,7 @@ namespace karabo {
             config.set("dbClientReadPointer", m_clientRead);
             config.set("dbClientWritePointer", m_clientWrite);
             config.set("maxTimeAdvance", m_maxTimeAdvance);
+            config.set("maxStringSize", m_maxStringSize);
             DeviceData::Pointer deviceData =
                     Factory<DeviceData>::create<karabo::util::Hash>("InfluxDataLoggerDeviceData", config);
             return deviceData;
