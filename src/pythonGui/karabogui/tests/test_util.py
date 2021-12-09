49e416a5b304154b56c295cae7481d8ce824be45
Dennis Goeries <dennis.goeries@xfel.eu>
2021-12-09 14:20:10 +0100

    GUI: Extend Signalblocker for multiple widgets

--- src/pythonGui/karabogui/tests/test_util.py
+++ src/pythonGui/karabogui/tests/test_util.py
@@ -1,16 +1,67 @@
-import unittest
+from qtpy.QtWidgets import QSlider, QSpinBox
 
-from ..util import _get_invalid_chars
+from karabogui.testing import GuiTestCase
+from karabogui.util import SignalBlocker, _get_invalid_chars
 
 
-class InvalidFilenameTestCase(unittest.TestCase):
+class TestUtilsGUI(GuiTestCase):
 
-    def test_basics(self):
-        self._assert_filename("foo", invalid='')
-        self._assert_filename("FoO/Bar-bAz_123", invalid='')
-        self._assert_filename("foo bar", invalid=' ')
-        self._assert_filename("foo!@#$%&*()+<>@:", invalid='!@#$%&*()+<>:')
+    def test_assert_filename(self):
+        def assert_filename(filename, *, invalid):
+            invalid_chars = _get_invalid_chars(filename)
+            self.assertEqual(set(list(invalid)), set(invalid_chars))
 
-    def _assert_filename(self, filename, *, invalid):
-        invalid_chars = _get_invalid_chars(filename)
-        self.assertEqual(set(list(invalid)), set(invalid_chars))
+        assert_filename("foo", invalid='')
+        assert_filename("FoO/Bar-bAz_123", invalid='')
+        assert_filename("foo bar", invalid=' ')
+        assert_filename("foo!@#$%&*()+<>@:", invalid='!@#$%&*()+<>:')
+
+    def test_signal_blocker(self):
+        count = 0
+
+        def slotReceived(*args, **kwargs):
+            nonlocal count
+            count += 1
+
+        spinbox = QSpinBox()
+        spinbox.valueChanged.connect(slotReceived)
+        self.assertEqual(count, 0)
+        spinbox.setValue(100)
+        self.assertEqual(count, 1)
+
+        # Update the spinbox, but block the signals
+        with SignalBlocker(spinbox):
+            for i in range(10):
+                spinbox.setValue(i)
+            self.assertEqual(spinbox.value(), i)
+
+        # Still only once
+        self.assertEqual(count, 1)
+        spinbox.setValue(100)
+        self.assertEqual(count, 2)
+
+        # Reset the counter and setup a slider
+        count = 0
+
+        slider = QSlider()
+        slider.valueChanged.connect(slotReceived)
+        slider.setRange(0, 100)
+        slider.setValue(20)
+        self.assertEqual(count, 1)
+
+        # Works, now block both
+        with SignalBlocker(spinbox, slider):
+            for i in range(10):
+                spinbox.setValue(i)
+                slider.setValue(i)
+            self.assertEqual(spinbox.value(), i)
+            self.assertEqual(slider.value(), i)
+        self.assertEqual(count, 1)
+
+        # Other initialization
+        count = 0
+        blocker = SignalBlocker(spinbox)
+        self.assertEqual(len(blocker.objects), 1)
+        with blocker:
+            spinbox.setValue(10000)
+        self.assertEqual(count, 0)
