66720835c4f0ab0b328281ae133cd8a755602ae8
Dennis Goeries <dennis.goeries@xfel.eu>
2021-12-10 16:31:03 +0100

    GUI: Enable dynamic table width for table dialog of Configurator

--- src/pythonGui/karabogui/configurator/dialog/table_view.py
+++ src/pythonGui/karabogui/configurator/dialog/table_view.py
@@ -3,12 +3,15 @@
 # Created on November 9, 2017
 # Copyright (C) European XFEL GmbH Hamburg. All rights reserved.
 #############################################################################
-import os.path as op
+from pathlib import Path
 
 from qtpy import uic
 from qtpy.QtWidgets import QDialog, QDialogButtonBox, QHBoxLayout
 
 from karabogui.controllers.edit.table import EditableTableElement
+
+MAX_WIDTH = 1024
+COLUMN_FACTOR = 120
 
 
 class TableDialog(QDialog):
@@ -16,11 +19,11 @@ class TableDialog(QDialog):
         """A dialog to configure a VectorHash
 
         :param proxy: The `PropertyProxy` object representing the VectorHash
+        :param editable: If the controller inside the dialog is editable
         :param parent: The parent of the dialog
         """
-        super(TableDialog, self).__init__(parent)
-        filepath = op.join(op.abspath(op.dirname(__file__)),
-                           'table_view.ui')
+        super().__init__(parent)
+        filepath = Path(__file__).parent / 'table_view.ui'
         uic.loadUi(filepath, self)
         # XXX: Do not change the name of this variable since the
         # `EditDelegate` method `setModelData` refers to this dialog as the
@@ -41,6 +44,16 @@ class TableDialog(QDialog):
         cancel_button = self.buttonBox.button(QDialogButtonBox.Cancel)
         cancel_button.setVisible(editable)
 
+        width = 450
+        height = 350
+
+        binding = proxy.binding
+        if binding is not None:
+            columns = len(binding.bindings)
+            # minimum width of 450, but can go up to `MAX_WIDTH`
+            width = min(max(columns * COLUMN_FACTOR, width), MAX_WIDTH)
+        self.resize(width, height)
+
     def done(self, result):
         self.controller.destroy_widget()
-        return super(TableDialog, self).done(result)
+        return super().done(result)
