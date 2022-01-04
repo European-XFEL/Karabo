c7a3347d30faf5af69d9ca815b8d743cad77db2a
Dennis Goeries <dennis.goeries@xfel.eu>
2022-01-04 15:20:31 +0100

    GUI: Fix some mediator clashes in tests

--- src/pythonGui/karabogui/project/controller/tests/test_project_controller.py
+++ src/pythonGui/karabogui/project/controller/tests/test_project_controller.py
@@ -9,6 +9,8 @@ from karabo.native import Hash
 from karabogui.project.controller.device import DeviceInstanceController
 from karabogui.project.controller.device_config import (
     DeviceConfigurationController)
+from karabogui.singletons.mediator import Mediator
+from karabogui.testing import singletons
 
 STATUS_ICON = 'karabogui.project.controller.' \
               'device.get_project_device_status_icon'
@@ -16,50 +18,52 @@ STATUS_ICON = 'karabogui.project.controller.' \
 
 class TestProjectController(TestCase):
     def test_active_configuration(self):
-        fconfig = Hash()
-        fconfig['fkey'] = 'fvalue'
-        fconfig['skey'] = 'svalue'
-        fconfig = DeviceConfigurationModel(class_id='BazClass',
-                                           configuration=fconfig)
-        fconfig.initialized = True
+        with singletons(mediator=Mediator()):
+            # Changing configurations might emit signals
+            fconfig = Hash()
+            fconfig['fkey'] = 'fvalue'
+            fconfig['skey'] = 'svalue'
+            fconfig = DeviceConfigurationModel(class_id='BazClass',
+                                               configuration=fconfig)
+            fconfig.initialized = True
 
-        sconfig = Hash()
-        sconfig['fkey'] = 'value_diff'
-        sconfig = DeviceConfigurationModel(class_id='BazClass',
-                                           configuration=sconfig)
-        sconfig.initialized = True
+            sconfig = Hash()
+            sconfig['fkey'] = 'value_diff'
+            sconfig = DeviceConfigurationModel(class_id='BazClass',
+                                               configuration=sconfig)
+            sconfig.initialized = True
 
-        config_list = [fconfig, sconfig]
+            config_list = [fconfig, sconfig]
 
-        model = DeviceInstanceModel(class_id='BazClass',
-                                    instance_id='fooDevice',
-                                    configs=config_list)
-        model.initialized = True
-        model.active_config_ref = fconfig.uuid
-        DeviceServerModel(server_id='testServer', host='serverFoo',
-                          devices=[model])
+            model = DeviceInstanceModel(class_id='BazClass',
+                                        instance_id='fooDevice',
+                                        configs=config_list)
+            model.initialized = True
+            model.active_config_ref = fconfig.uuid
+            DeviceServerModel(server_id='testServer', host='serverFoo',
+                              devices=[model])
 
-        def assert_active_configuration():
-            self.assertTrue('fkey' in fconfig.configuration)
-            self.assertEqual(fconfig.configuration['fkey'], 'fvalue')
+            def assert_active_configuration():
+                self.assertTrue('fkey' in fconfig.configuration)
+                self.assertEqual(fconfig.configuration['fkey'], 'fvalue')
 
-            self.assertTrue('skey' in fconfig.configuration)
-            self.assertEqual(fconfig.configuration['skey'], 'svalue')
+                self.assertTrue('skey' in fconfig.configuration)
+                self.assertEqual(fconfig.configuration['skey'], 'svalue')
 
-            self.assertTrue('fkey' in sconfig.configuration)
-            self.assertEqual(sconfig.configuration['fkey'], 'value_diff')
+                self.assertTrue('fkey' in sconfig.configuration)
+                self.assertEqual(sconfig.configuration['fkey'], 'value_diff')
 
-            self.assertFalse('skey' in sconfig.configuration)
+                self.assertFalse('skey' in sconfig.configuration)
 
-        controller = DeviceInstanceController(model=model)
-        controller.active_config_changed(sconfig)
-        assert_active_configuration()
+            controller = DeviceInstanceController(model=model)
+            controller.active_config_changed(sconfig)
+            assert_active_configuration()
 
-        controller.active_config_changed(fconfig)
-        assert_active_configuration()
+            controller.active_config_changed(fconfig)
+            assert_active_configuration()
 
-        controller.active_config_changed(sconfig)
-        assert_active_configuration()
+            controller.active_config_changed(sconfig)
+            assert_active_configuration()
 
     def test_check_mark_configuration(self):
         config = Hash()
