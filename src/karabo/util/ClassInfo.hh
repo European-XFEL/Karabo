e62386b08c98053598be18426fb6cb5e6090c55c
Raul Costa <raul.costa@xfel.eu>
2022-01-08 08:34:47 +0100

    Fix for header includes.
    These changes prevent clang-format re-sort of includes from breaking the builds. The headers were dependent on the order of inclusion.

--- src/karabo/util/ClassInfo.hh
+++ src/karabo/util/ClassInfo.hh
@@ -7,10 +7,11 @@
  */
 
 #ifndef KARABO_UTIL_CLASSINFO_HH
-#define	KARABO_UTIL_CLASSINFO_HH
+#define KARABO_UTIL_CLASSINFO_HH
 
-#include <string>
 #include <boost/current_function.hpp>
+#include <boost/shared_ptr.hpp>
+#include <string>
 
 #include "karaboDll.hh"
 
@@ -22,66 +23,66 @@ namespace karabo {
          * @brief ClassInfo holds meta-data to a factorized class
          */
         class KARABO_DECLSPEC ClassInfo {
-
-            public:
-
+           public:
             ClassInfo(const std::string& classId, const std::string& signature, const std::string& classVersion);
             /**
              * Return the C++ class name of this class
-             * @return 
+             * @return
              */
             const std::string& getClassName() const;
-            
+
             /**
              * Return the C++ name space of this class
-             * @return 
+             * @return
              */
             const std::string& getNamespace() const;
-            
+
             /**
              * Return the Karabo ClassId of this class
-             * @return 
+             * @return
              */
             const std::string& getClassId() const;
-            
+
             /**
              * Return the LogCategory for this class
-             * @return 
+             * @return
              */
             const std::string& getLogCategory() const;
-            
+
             /**
              * Return the version number of this class - currently does not
              * convey much meaning.
-             * @return 
+             * @return
              */
             const std::string& getVersion() const;
 
-        private: // Functions
+           private: // Functions
             void initClassNameAndSpace(const std::string& signature);
             void initLogCategory();
 
-        private: // Members
+           private: // Members
             std::string m_classId;
             std::string m_namespace;
             std::string m_className;
             std::string m_logCategory;
             std::string m_configVersion;
-
         };
-    }
-}
+    } // namespace util
+} // namespace karabo
 
 // This macro should be called in header file, inside public section of the class declaration (see tests/BobbyCar.hh)
 // It provides two methods which can be used for type introspection. The macro can be used in any class.
-#define KARABO_CLASSINFO(className, classId, classVersion)\
-static karabo::util::ClassInfo classInfo() { return karabo::util::ClassInfo(classId, BOOST_CURRENT_FUNCTION, classVersion); } \
-virtual karabo::util::ClassInfo getClassInfo() const { return classInfo(); } \
-typedef className Self; \
-typedef boost::shared_ptr<className> Pointer; \
-typedef boost::shared_ptr<const className> ConstPointer; \
-typedef boost::weak_ptr<className> WeakPointer;
+#define KARABO_CLASSINFO(className, classId, classVersion)                             \
+    static karabo::util::ClassInfo classInfo() {                                       \
+        return karabo::util::ClassInfo(classId, BOOST_CURRENT_FUNCTION, classVersion); \
+    }                                                                                  \
+    virtual karabo::util::ClassInfo getClassInfo() const {                             \
+        return classInfo();                                                            \
+    }                                                                                  \
+    typedef className Self;                                                            \
+    typedef boost::shared_ptr<className> Pointer;                                      \
+    typedef boost::shared_ptr<const className> ConstPointer;                           \
+    typedef boost::weak_ptr<className> WeakPointer;
 
 
 #endif
-
