996095f0cb905fb5ea4047f1323f73e87bedda93
Dennis Goeries <dennis.goeries@xfel.eu>
2022-01-28 14:24:21 +0100

    GUI: Provide more delegates, button delegate, progressbar and colordelegate

--- src/pythonGui/karabogui/controllers/table/tests/test_utils.py
+++ src/pythonGui/karabogui/controllers/table/tests/test_utils.py
@@ -1,7 +1,10 @@
+from qtpy.QtGui import QBrush
+
 from karabo.common.const import KARABO_SCHEMA_DISPLAY_TYPE
 from karabogui.binding.api import StringBinding
 
-from ..utils import is_state_display_type, list2string, string2list
+from ..utils import (
+    create_brushes, is_state_display_type, list2string, string2list)
 
 
 def test_display_type_state():
@@ -34,3 +37,35 @@ def test_convert_list_string():
     assert list2string(value) == "2,4.2"
     value = ["2", "2", "3"]
     assert list2string(value) == "2,2,3"
+
+
+def test_create_brushes():
+    display_string = "TableColor|offline=red&online=green&default=blue"
+    default, brushes = create_brushes(display_string)
+    assert default is not None
+    assert isinstance(default, QBrush)
+    assert "offline" in brushes
+    assert "online" in brushes
+    assert isinstance(brushes["online"], QBrush)
+    assert isinstance(brushes["offline"], QBrush)
+
+    # Wonky typos, e.g. ? as uri identifier
+    display_string = "TableColor?offline=red&online=green&default=blue"
+    default, brushes = create_brushes(display_string)
+    assert default is None
+    assert not brushes
+
+    # Wrong delimiter, no exceptions
+    display_string = "TableColor|offline=red%online=green%nodefault=blue"
+    default, brushes = create_brushes(display_string)
+    assert default is None
+    assert len(brushes) == 1
+    assert "offline" in brushes
+
+    # No default brush
+    display_string = "TableColor|offline=red&online=green&nodefault=blue"
+    default, brushes = create_brushes(display_string)
+    assert default is None
+    assert "offline" in brushes
+    assert "online" in brushes
+    assert "nodefault" in brushes
