089193128c8411967b40ff1284e5eeb122a03583
Alessandro Silenzi <alessandro.silenzi@xfel.eu>
2022-02-16 16:35:42 +0100

    BOUND: Implement slotLoggerContent

---
+++ src/pythonKarabo/karabo/integration_tests/device_comm_test/stuckLoggerDevice.py
@@ -0,0 +1,8 @@
+from karabo.bound import KARABO_CLASSINFO, PythonDevice
+
+
+@KARABO_CLASSINFO("StuckLoggerDevice", "2.0")
+class StuckLoggerDevice(PythonDevice):
+    def slotLoggerContent(self, info):
+        # bad behaviour. Implement a device that will never reply
+        self._ss.createAsyncReply()
