e1006326bbabc0211a11f927d7999e29efaea138
Dennis Goeries <dennis.goeries@xfel.eu>
2022-03-05 20:44:09 +0100

    DEPS: Add pytest asyncio

--- extern/build.sh
+++ extern/build.sh
@@ -9,11 +9,12 @@ daemontools libzmq nlohmann_json pugixml mqtt redisclient amqp libev)
 
 DEPENDENCIES_PYTHON=( setuptools setuptools_scm pip wheel cython pybind numpy scipy
 six nose py pyparsing packaging toml more-itertools zipp importlib-metadata pluggy
-attrs colorama iniconfig atomicwrites pytest pytest_runner pytest_cov pillow sip backports
-backports_abc tornado dateutil ptyprocess pytz pexpect pyzmq markupsafe jinja2 pygments decorator
-traitlets parso ipython_genutils jedi pickleshare wcwidth backcall prompt_toolkit ipython
-jupyter_core jupyter_client ipykernel simplegeneric dill pkgconfig h5py pyusb parse
-jsonschema ecdsa tzlocal httplib2 traits pint nbformat isort
+attrs colorama iniconfig atomicwrites pytest pytest_runner pytest_cov
+pytest_asyncio pillow sip backports backports_abc tornado dateutil ptyprocess
+pytz pexpect pyzmq markupsafe jinja2 pygments decorator traitlets parso
+ipython_genutils jedi pickleshare wcwidth backcall prompt_toolkit ipython
+jupyter_core jupyter_client ipykernel simplegeneric dill pkgconfig h5py pyusb
+parse jsonschema ecdsa tzlocal httplib2 traits pint nbformat isort
 notebook ipyparallel ipcluster_tools cycler pyelftools rpathology lxml certifi
 chardet idna urllib3 requests ply psutil pycodestyle pyflakes mccabe flake8
 msgpack msgpack-numpy flaky pyyaml coverage matplotlib eulxml eulexist
