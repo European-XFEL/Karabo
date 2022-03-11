52862818b333ba43f52eb0b48c508e7d1600e4af
Dennis Goeries <dennis.goeries@xfel.eu>
2022-03-11 14:03:56 +0100

    GUI: Use pytest-mock in sticker dialog test

--- src/pythonGui/karabogui/dialogs/tests/test_sticker_dialog.py
+++ src/pythonGui/karabogui/dialogs/tests/test_sticker_dialog.py
@@ -1,4 +1,3 @@
-from unittest import mock
 
 from qtpy.QtGui import QColor, QFont
 from qtpy.QtWidgets import QDialog
@@ -9,7 +8,7 @@ from karabogui.dialogs.api import StickerDialog
 from karabogui.testing import click_button, gui_app
 
 
-def test_sticker_dialog(gui_app: gui_app):
+def test_sticker_dialog(gui_app: gui_app, mocker):
 
     """Test the sticker dialog"""
     model = StickerModel(
@@ -35,13 +34,13 @@ def test_sticker_dialog(gui_app: gui_app):
     assert dialog.model.text == "XFEL2"
 
     path = "karabogui.dialogs.sticker_dialog.FontDialog"
-    with mock.patch(path) as d:
-        font = QFont()
-        font.setPointSize(20)
-        d().qfont = font
-        d().exec.return_value = QDialog.Accepted
-        click_button(dialog.pbFont)
-        assert dialog.model.font == font.toString()
+    d = mocker.patch(path)
+    font = QFont()
+    font.setPointSize(20)
+    d().qfont = font
+    d().exec.return_value = QDialog.Accepted
+    click_button(dialog.pbFont)
+    assert dialog.model.font == font.toString()
 
     assert dialog.pbBackground.isEnabled() is True
     assert dialog.model.background == "white"
@@ -52,18 +51,18 @@ def test_sticker_dialog(gui_app: gui_app):
     # Enable again to change background
     click_button(dialog.cbBackground)
     path = "karabogui.dialogs.sticker_dialog.QColorDialog"
-    with mock.patch(path) as d:
-        c = QColor(255, 0, 0)
-        d.getColor.return_value = c
-        click_button(dialog.pbBackground)
-        assert dialog.model.background == c.name()
-        assert dialog.model.background != "transparent"
+    d = mocker.patch(path)
+    c = QColor(255, 0, 0)
+    d.getColor.return_value = c
+    click_button(dialog.pbBackground)
+    assert dialog.model.background == c.name()
+    assert dialog.model.background != "transparent"
 
     # Change the foreground
     assert dialog.model.foreground == ""
     path = "karabogui.dialogs.sticker_dialog.QColorDialog"
-    with mock.patch(path) as d:
-        c = QColor(0, 255, 0)
-        d.getColor.return_value = c
-        click_button(dialog.pbTextColor)
-        assert dialog.model.foreground == c.name()
+    d = mocker.patch(path)
+    c = QColor(0, 255, 0)
+    d.getColor.return_value = c
+    click_button(dialog.pbTextColor)
+    assert dialog.model.foreground == c.name()
