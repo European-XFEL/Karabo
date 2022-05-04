f929d1076d55a3cb9caf959040e21cc542c4d3ad
Dennis Goeries <dennis.goeries@xfel.eu>
2022-05-04 16:12:50 +0200

    Common: Provide WeakMethodRef in Karabo.common

---
+++ src/pythonKarabo/karabo/common/tests/test_util.py
@@ -0,0 +1,21 @@
+from karabo.common.api import WeakMethodRef
+
+
+def test_weak_method_ref():
+    """Test that we can safely call bound methods"""
+    global called
+    called = False
+
+    class Device:
+        def move(self):
+            global called
+            called = True
+
+    device = Device()
+    func = WeakMethodRef(device.move)
+    func()
+    assert called is True
+    called = False
+    del device
+    func()
+    assert called is False
