26e11f3dec03488b9127afbaffec1095d040627c
Dennis Goeries <dennis.goeries@xfel.eu>
2022-05-17 13:56:26 +0200

    CI: Flake naming test

--- src/templates/middlelayer/minimal/src/__PACKAGE_NAME__/tests/property_naming_test.py
+++ src/templates/middlelayer/minimal/src/__PACKAGE_NAME__/tests/property_naming_test.py
@@ -4,7 +4,7 @@ import __PACKAGE_NAME__
 def test_property_code_guideline():
     """Test that all properties and slots follow common code style"""
     try:
-       from karabo.middlelayer.testing import check_device_package_properties
+        from karabo.middlelayer.testing import check_device_package_properties
     except (ImportError, ModuleNotFoundError):
         print("Checking device properties not possible with existing "
               "karabo version.")
