47b210f1e00a14ecd057a558f6471c1c2a6c1dd7
Dennis Goeries <dennis.goeries@xfel.eu>
2022-05-22 21:17:12 +0200

    GUI: Drop Weakmethod ref since in common

--- src/pythonGui/karabogui/util.py
+++ src/pythonGui/karabogui/util.py
@@ -2,12 +2,10 @@ import html
 import os
 import os.path as op
 import re
-import weakref
 import webbrowser
 from contextlib import contextmanager
 from datetime import datetime
 from tempfile import mkstemp
-from types import MethodType
 from uuid import uuid4
 
 from dateutil.tz import tzlocal, tzutc
@@ -51,35 +49,6 @@ class SignalBlocker:
     def __exit__(self, a, b, c):
         for obj, state in zip(self.objects, self.states):
             obj.blockSignals(state)
-
-
-class WeakMethodRef(object):
-    """A weakref.ref() for bound methods
-    """
-
-    def __init__(self, bound_method, num_args=-1):
-        # Preconditions...
-        # bound_method MUST be a bound method
-        assert type(bound_method) is MethodType
-        if num_args > -1:
-            # bound_method MUST take N args (- 1 because of self)!
-            needed_args = bound_method.__func__.__code__.co_argcount - 1
-            assert needed_args == num_args
-
-        obj = bound_method.__self__
-        if obj is not None:
-            self.obj = weakref.ref(obj, self._owner_deleted)
-            self.name = bound_method.__name__
-
-    def __call__(self, *args, **kwargs):
-        if self.obj is not None:
-            obj = self.obj()
-            if obj is not None:
-                method = getattr(obj, self.name)
-                method(*args, **kwargs)
-
-    def _owner_deleted(self, ref):
-        self.obj = None
 
 
 def generateObjectName(widget):
