021ee2f39ab32dce880266bb1ffad8083f67d271
Dennis Goeries <dennis.goeries@xfel.eu>
2022-05-31 12:49:15 +0200

    GUI: Enable to get default scene of device from scene elements

--- src/pythonGui/karabogui/navigation/tools.py
+++ src/pythonGui/karabogui/navigation/tools.py
@@ -5,16 +5,12 @@
 #############################################################################
 from abc import abstractmethod
 
-from traits.api import ABCHasStrictTraits, Undefined
+from traits.api import ABCHasStrictTraits
 
-from karabo.common.api import Capabilities
-from karabogui import messagebox
 from karabogui.dialogs.api import LogDialog
 from karabogui.itemtypes import NavigationItemTypes
 from karabogui.logger import get_logger
-from karabogui.request import (
-    get_scene_from_server, onConfigurationUpdate, onSchemaUpdate)
-from karabogui.singletons.api import get_topology
+from karabogui.request import retrieve_default_scene
 from karabogui.util import move_to_cursor, version_compatible
 
 
@@ -31,6 +27,7 @@ class NavigationHandler(ABCHasStrictTraits):
 class DeviceSceneHandler(NavigationHandler):
     """Device DoubleClick for the navigation view
     """
+
     def can_handle(self, info):
         navigation_type = info.get('type')
         if navigation_type is NavigationItemTypes.DEVICE:
@@ -39,54 +36,7 @@ class DeviceSceneHandler(NavigationHandler):
 
     def handle(self, info, parent=None):
         device_id = info.get("deviceId")
-        capabilities = info.get("capabilities")
-        bit = Capabilities.PROVIDES_SCENES
-
-        if (capabilities & bit) != bit:
-            msg = f"The device <b>{device_id}</b> does not provide a scene."
-            get_logger().info(msg)
-            return
-
-        def _config_handler():
-            """Act on the arrival of the configuration"""
-            scenes = proxy["availableScenes"].value
-            if scenes is Undefined or not len(scenes):
-                messagebox.show_warning(
-                    "The device <b>{}</b> does not specify a scene "
-                    "name!".format(device_id))
-            else:
-                scene_name = scenes[0]
-                get_scene_from_server(device_id, scene_name)
-
-        def _schema_handler():
-            """Act on the arrival of the schema"""
-            scenes = proxy["availableScenes"].value
-            if scenes is Undefined:
-                onConfigurationUpdate(proxy, _config_handler)
-            elif not len(scenes):
-                messagebox.show_warning(
-                    "The device <b>{}</b> does not specify a scene "
-                    "name!".format(device_id))
-            else:
-                scene_name = scenes[0]
-                get_scene_from_server(device_id, scene_name)
-
-        proxy = get_topology().get_device(device_id)
-        if not len(proxy.binding.value):
-            # We completely miss our schema and wait for it.
-            onSchemaUpdate(proxy, _schema_handler)
-        elif proxy["availableScenes"].value is Undefined:
-            onConfigurationUpdate(proxy, _config_handler)
-        else:
-            scenes = proxy["availableScenes"].value
-            if not len(scenes):
-                # The device might not have a scene name in property
-                messagebox.show_warning(
-                    "The device <b>{}</b> does not specify a scene "
-                    "name!".format(device_id))
-            else:
-                scene_name = scenes[0]
-                get_scene_from_server(device_id, scene_name)
+        retrieve_default_scene(device_id)
 
 
 class ServerLogHandler(NavigationHandler):
