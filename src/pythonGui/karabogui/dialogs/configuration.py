0c3cc6d89a1575a36cbc34e5bbdda30a80f0e98a
Dennis Goeries <dennis.goeries@xfel.eu>
2022-06-09 19:02:48 +0200

    GUI: Make the application configuration editable for booleans

--- src/pythonGui/karabogui/dialogs/configuration.py
+++ src/pythonGui/karabogui/dialogs/configuration.py
@@ -1,14 +1,15 @@
 from qtpy import uic
 from qtpy.QtCore import QAbstractItemModel, QModelIndex, Qt
+from qtpy.QtGui import QColor
 from qtpy.QtWidgets import QAbstractItemView, QDialog, QHeaderView
-from traits.api import (
-    Any, Bool, HasStrictTraits, Instance, List, Property, String)
+from traits.api import Any, Bool, HasStrictTraits, Instance, Int, List, String
 
+from karabogui.indicators import PROPERTY_READONLY_COLOR
 from karabogui.singletons.api import get_config
 
 from .utils import get_dialog_ui
 
-HEADER_LABELS = ['Name', 'Setting']
+HEADER_LABELS = ["Name", "Setting"]
 NAME_COLUMN = 0
 SETTING_COLUMN = 1
 
@@ -16,23 +17,21 @@ SETTING_COLUMN = 1
 class ConfigNode(HasStrictTraits):
     # The name of our configuration item
     name = String
-    # Our configuration item node
-    internal = Any
-    # The internally stored value. Can be derived from QSettings
-    value = Property
     # The parent node
-    parent_node = Instance('ConfigNode', allow_none=True)
+    parent_node = Instance("ConfigNode", allow_none=True)
     # The list of children
-    children = List(Instance('ConfigNode'))
+    children = List(Instance("ConfigNode"))
     # Check whether this config node is configurable or not
     editable = Bool
+    # Level whether this config node belongs to a group or child
+    level = Int(0)
+    # data type of the property, e.g. bool
+    dtype = Any
 
-    def _get_value(self):
-        if self.internal is None:
-            return None
-        return "{}".format(get_config()[self.name])
+    def get_value(self):
+        return get_config()[self.name]
 
-    def _set_value(self, value):
+    def set_value(self, value):
         get_config()[self.name] = value
 
     def appendChild(self, item):
@@ -60,21 +59,6 @@ class ConfigurationModel(QAbstractItemModel):
         """Reimplemented function of QAbstractItemModel
         """
         return len(HEADER_LABELS)
-
-    def data(self, index, role=Qt.DisplayRole):
-        """Reimplemented function of QAbstractItemModel
-        """
-        if not index.isValid():
-            return None
-
-        item = index.internalPointer()
-        column = index.column()
-        if column == NAME_COLUMN:
-            if role == Qt.DisplayRole:
-                return item.name
-        elif column == SETTING_COLUMN:
-            if role == Qt.DisplayRole:
-                return item.value
 
     def headerData(self, column, orientation, role=Qt.DisplayRole):
         """Reimplemented function of QAbstractItemModel
@@ -115,19 +99,22 @@ class ConfigurationModel(QAbstractItemModel):
         return self.createIndex(parent_node.row(), 0, parent_node)
 
     def flags(self, index):
-        """Reimplemented function of QAbstractItemModel.
-        """
+        """Reimplemented function of QAbstractItemModel"""
         if not index.isValid():
             return Qt.NoItemFlags
+        if index.column() != 1:
+            return Qt.ItemIsEnabled
 
         node = index.internalPointer()
         if node is None:
             return Qt.NoItemFlags
 
-        ret = Qt.ItemIsEnabled | Qt.ItemIsSelectable
-        if node.editable:
-            ret |= Qt.ItemIsEditable
-        return ret
+        flags = Qt.ItemIsEnabled | Qt.ItemIsSelectable
+        if node.dtype == bool and node.editable:
+            flags |= Qt.ItemIsUserCheckable
+        elif node.editable:
+            flags |= Qt.ItemIsEditable
+        return flags
 
     def rowCount(self, parent=QModelIndex()):
         """Reimplemented function of QAbstractItemModel
@@ -150,14 +137,16 @@ class ConfigurationModel(QAbstractItemModel):
                                     parent_node=self.root)
             self.root.appendChild(group_node)
             for item in groups[group_name]:
-                attr_item = ConfigNode(name=item.name, editable=item.editable,
-                                       internal=item, parent_node=group_node)
+                attr_item = ConfigNode(parent_node=group_node,
+                                       **item.toDict(), level=1)
                 group_node.appendChild(attr_item)
 
-    def setData(self, index, value, role):
-        """Reimplemented function of QAbstractItemModel.
+    def setData(self, index, value, role=Qt.EditRole):
+        """Reimplemented function of QAbstractItemModel
+
+        Active roles are CheckStateRole, DisplayRole and EditRole
         """
-        if role != Qt.EditRole or value is None:
+        if value is None:
             return False
 
         # Get the index's stored object
@@ -165,10 +154,39 @@ class ConfigurationModel(QAbstractItemModel):
         if node is None:
             return False
 
-        node.value = value
+        if role == Qt.CheckStateRole and node.dtype == bool:
+            value = value == Qt.Checked
+            node.set_value(value)
+            self.dataChanged.emit(index, index)
+            return True
 
-        # A value was successfully set!
-        return True
+        elif role in (Qt.DisplayRole, Qt.EditRole):
+            node.set_value(value)
+            self.dataChanged.emit(index, index)
+            return True
+
+        return False
+
+    def data(self, index, role=Qt.DisplayRole):
+        """Reimplemented function of QAbstractItemModel"""
+        if not index.isValid():
+            return None
+
+        node = index.internalPointer()
+        column = index.column()
+        if column == NAME_COLUMN:
+            if role == Qt.DisplayRole:
+                return node.name
+        elif column == SETTING_COLUMN:
+            if node.level == 1:
+                data = node.get_value()
+                if role == Qt.CheckStateRole:
+                    if node.dtype == bool:
+                        return Qt.Checked if data else Qt.Unchecked
+                elif role == Qt.DisplayRole:
+                    return str(data)
+                elif role == Qt.ForegroundRole and not node.editable:
+                    return QColor(*PROPERTY_READONLY_COLOR)
 
 
 class ConfigurationDialog(QDialog):
@@ -178,9 +196,9 @@ class ConfigurationDialog(QDialog):
     def __init__(self, parent=None):
         super(ConfigurationDialog, self).__init__(parent)
         self.setAttribute(Qt.WA_DeleteOnClose)
-        # Don't block the event loop!
+        # Don"t block the event loop!
         self.setModal(False)
-        filepath = get_dialog_ui('configuration.ui')
+        filepath = get_dialog_ui("configuration.ui")
         uic.loadUi(filepath, self)
 
         self.tree_view.setSelectionBehavior(QAbstractItemView.SelectRows)
