8d94df87bd57bce16f6caef94852a00c6a8fd10b
Dennis Goeries <dennis.goeries@xfel.eu>
2022-06-10 15:17:50 +0200

    GUI: Add Historic Text Widget for String retrieval

--- src/pythonKarabo/karabo/common/scenemodel/generic_scenes.py
+++ src/pythonKarabo/karabo/common/scenemodel/generic_scenes.py
@@ -3,7 +3,7 @@ from .shapes import LineModel
 from .widgets.graph_image import WebCamGraphModel
 from .widgets.graph_plots import (
     AlarmGraphModel, StateGraphModel, TrendGraphModel, VectorGraphModel)
-from .widgets.simple import DisplayLabelModel, LabelModel
+from .widgets.simple import DisplayLabelModel, HistoricTextModel, LabelModel
 
 
 def _get_plot_scene(model, device_id, path):
@@ -44,6 +44,50 @@ def _get_plot_scene(model, device_id, path):
     scene = SceneModel(
         height=440.0, width=730.0, children=[line, plot, label, display_label]
     )
+
+    return scene
+
+
+def get_text_history_scene(device_id, path):
+    """Return a history scene for a device_id with string path
+
+    :returns: SceneModel with a historic table and a deviceId label
+    """
+    line = LineModel(stroke="#000000", x1=15.0, x2=700.0, y1=65.0, y2=65.0)
+
+    text = HistoricTextModel(
+        height=600.0,
+        keys=["{}.{}".format(device_id, path)],
+        parent_component="DisplayComponent",
+        width=700.0,
+        x=10.0,
+        y=80.0,
+    )
+
+    label = LabelModel(
+        font="Source Sans Pro,10,-1,5,50,0,0,0,0,0",
+        foreground="#000000",
+        height=31.0,
+        parent_component="DisplayComponent",
+        text="DeviceID",
+        width=60.0,
+        x=20.0,
+        y=20.0,
+    )
+
+    display_label = DisplayLabelModel(
+        height=30.0,
+        keys=["{}.deviceId".format(device_id)],
+        parent_component="DisplayComponent",
+        width=600.0,
+        x=80.0,
+        y=20.0,
+    )
+
+    scene = SceneModel(
+        height=700.0, width=730.0, children=[line, text, label, display_label]
+    )
+    scene.simple_name = "HistoricText_{}_{}".format(device_id, path)
 
     return scene
 
