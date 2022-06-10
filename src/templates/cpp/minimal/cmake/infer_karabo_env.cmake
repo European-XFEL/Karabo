e77c1c0ece37a10c10985b81c2fea219d948f8ae
Raul Costa <raul.costa@xfel.eu>
2022-06-10 11:50:14 +0200

    C++: Sync template with the latest changes in Beckhoff's CMake project.

--- src/templates/cpp/minimal/cmake/infer_karabo_env.cmake
+++ src/templates/cpp/minimal/cmake/infer_karabo_env.cmake
@@ -3,10 +3,11 @@
 # behavior of "karabo new" and "karabo install" commands, which place the
 # device source files at "$KARABO/devices".
 #
-# If the inference succeeds, KARABO env var is set to the infered directory and
-# CMAKE_PREFIX_PATH is set to its "extern" subdirectory.
+# If the inference succeeds, KARABO env var is set to the infered directory.
+# Success is reflected by bool variable "KARABO_ENV_VAR_SET".
 #
 macro(inferKaraboEnv)
+  set(KARABO_ENV_VAR_SET FALSE)
   string(FIND ${CMAKE_SOURCE_DIR} "/devices/" DEVICE_DIR_POS)
   if (DEVICE_DIR_POS GREATER 0
       AND EXISTS "${CMAKE_SOURCE_DIR}/../../bin/karabo-cppserver"
@@ -21,7 +22,7 @@ macro(inferKaraboEnv)
         )
         # sets the KARABO env var so it can be used downstream.
         set(ENV{KARABO} ${KARABO_INST})
-        set(CMAKE_PREFIX_PATH
-            ${CMAKE_PREFIX_PATH} ${KARABO_INST} ${KARABO_INST}/extern)
+        set(KARABO_ENV_VAR_SET TRUE)
+
   endif()
 endmacro()
