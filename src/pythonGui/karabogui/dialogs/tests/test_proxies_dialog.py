5715d6c18290ab32cce68e26a756e13d44857ce5
Dennis Goeries <dennis.goeries@xfel.eu>
2022-06-28 14:15:38 +0200

    GUI: Keep the tooltip of the ControllerContainer in sync

--- src/pythonGui/karabogui/dialogs/tests/test_proxies_dialog.py
+++ src/pythonGui/karabogui/dialogs/tests/test_proxies_dialog.py
@@ -1,39 +1,22 @@
 from qtpy.QtWidgets import QMessageBox
 
-from karabo.native import Configurable, Slot
+from karabo.common.scenemodel.api import DisplayCommandModel
 from karabogui.controllers.display.command import DisplayCommand
 from karabogui.dialogs.api import ProxiesDialog
-from karabogui.testing import click_button, get_property_proxy
-
-
-class SlottedDevice(Configurable):
-
-    @Slot()
-    def hello(self):
-        pass
-
-    @Slot()
-    def bye(self):
-        pass
-
-
-def get_slot_proxy():
-    schema = SlottedDevice.getClassSchema()
-    hello_proxy = get_property_proxy(schema, "hello")
-    bye_proxy = get_property_proxy(schema, "bye")
-    return hello_proxy, bye_proxy
+from karabogui.sceneview.widget.api import ControllerContainer
+from karabogui.testing import click_button
 
 
 def test_proxies_dialog(gui_app, mocker):
-    proxies = get_slot_proxy()
-    controller = DisplayCommand(proxy=proxies[0])
-    controller.create(None)
-    controller.visualize_additional_property(proxies[1])
+    model = DisplayCommandModel(keys=["TestDevice.hello", "TestDevice.bye"])
+    container = ControllerContainer(DisplayCommand, model, None)
 
+    controller = container.widget_controller
     assert controller.widget is not None
     assert len(controller._actions) == 2
 
-    dialog = ProxiesDialog(controller)
+    assert container.toolTip() == "TestDevice.hello, TestDevice.bye"
+    dialog = ProxiesDialog(container)
     mbox = mocker.patch("karabogui.dialogs.proxies_dialog.QMessageBox")
     mbox.return_value = QMessageBox.Yes
     view = dialog.ui_list_view
@@ -42,5 +25,5 @@ def test_proxies_dialog(gui_app, mocker):
     click_button(dialog.ui_remove_button)
     # A button is removed
     assert len(controller._actions) == 1
-    controller.destroy()
-    assert controller.widget is None
+    assert container.toolTip() == "TestDevice.hello"
+    container.destroy()
