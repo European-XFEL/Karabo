3b4d4610d8890a1036b3eae94eb45ee13f64adbc
Gero Flucke <gero.flucke@xfel.eu>
2022-07-04 15:16:17 +0200

    C++: Strand construction via Configurator

--- src/karabo/net/Strand.hh
+++ src/karabo/net/Strand.hh
@@ -9,7 +9,7 @@
  *
  */
 
-#include <boost/asio/io_service.hpp>
+#include <boost/asio/io_context.hpp>
 #include <boost/function.hpp>
 #include <boost/smart_ptr/enable_shared_from_this.hpp>
 #include <boost/thread/mutex.hpp>
@@ -22,25 +22,29 @@
 #define KARABO_NET_STRAND_HH
 
 namespace karabo {
+    namespace util {
+        class Schema;
+    }
     namespace net {
 
         /**
-         * A poor man's substitute for boost::asio::io_service::strand because that does not guarantee that
+         * A poor man's substitute for boost::asio::strand because that does not guarantee that
          * handlers posted on different strands can run in parallel ("strand collision").
-         * Compared to boost::asio::io_service::strand, this
+         * Compared to boost::asio::strand, this
          * - lacks dispatch:               we usually do not want that in Karabo since it allows the handler to be
          *                                 called now in this scope
          * - lacks running_in_this_thread: probably not too important
          * - has a more restrictive wrap:  would be useful to support more, but a proper implementation would also
          *                                 need dispatch
          *
-         * Every handler posted will be put into a FIFO queue and the FIFO will be emptied one-by-one in the background
-         * by posting the handlers to the boost::asio::io_service given in the constructor.
+         * Every handler posted will be put into a FIFO queue and the FIFO will be emptied in the background
+         * by posting the handlers to the given boost::asio::io_context (either from net::EventLoop, passed in
+         * constructor, or defined by setContext(..)).
          *
          * NOTE:
-         * Do not create a Strand on the stack, but do it on the heap using a shared pointer:
+         * Do not create a Strand on the stack, but do it on the heap using the Configurator:
          *
-         * auto stack = boost::make_shared<Strand>(karabo::net::EventLoop::getIOService());
+         * auto stack = karabo::util::Configurator<Strand>::create(Hash(...));
          *
          * Otherwise 'enable_shared_from_this' does not work which is needed to guarantee (via usage of
          * karab::util::bind_weak) that an internal Strand method is executed on the event loop when the Strand
@@ -51,40 +55,58 @@ namespace karabo {
            public:
             KARABO_CLASSINFO(Strand, "Strand", "2.1")
 
+            static void expectedParameters(karabo::util::Schema& expected);
+
             /**
-             * Construct the Strand. The configuration can take two keys:
+             * Contructor only kept for backward compatibility.
              *
-             * - "maxInARow" (unsigned int > 0): Up to this number of handlers are run in a row before control is given
-             *                                   back to the event loop (default: 1).
-             * - "guaranteeToRun" (bool): If true (default: false), all handlers posted are guaranteed to run, even
-             *                            those that are left when destruction of the Strand starts.
+             * Better use karabo::util::Configurator<Strand>::create("Strand", Hash())
              */
-            explicit Strand(boost::asio::io_service& ioService,
-                            const karabo::util::Hash& config = karabo::util::Hash());
+            explicit Strand(boost::asio::io_context& ioContext);
+
+            /**
+             * Construct the Strand.
+             *
+             * The boost::asio::io_context of the karabo::net::EventLoop will be used.
+             *
+             * Best use this constructor indirectly via karabo::util::Configurator<Strand>::create("Strand", cfg) which
+             * will validate cfg and create the Strand properly on the heap.
+             *
+             * Keys of cfg are "maxInARow" (unsigned int) and "guaranteeToRun" (bool), see expectedParameters.
+             */
+            explicit Strand(const karabo::util::Hash& config);
 
             Strand(const Strand& orig) = delete;
 
             virtual ~Strand();
 
             /**
-             * Post a handler to the io_service given to the constructor with the guarantee that it is not executed
-             * before any other handler posted before has finished.
+             * Set the context to which the handlers are to be posted.
+             *
+             * No concurrency protection:
+             * Must be called directly after Strand creation, before it is used.
+             */
+            void setContext(boost::asio::io_context& ioContext);
+
+            /**
+             * Post a handler to the io_context with the guarantee that it is not executed before any other handler
+             * posted before has finished.
              * Handlers posted on different Strands can always be run in parallel.
              *
              * Note that "guaranteeToRun" flag of the constructor determines what happens with yet unhandled handlers
-             * when the strand is destructed.
+             * when the Strand is destructed.
              *
              * @param handler function without arguments and return value - will be copied
              */
             void post(const boost::function<void()>& handler);
 
             /**
-             * Post a handler to the io_service given to the constructor with the guarantee that it is not executed
-             * before any other handler posted before has finished.
+             * Post a handler to the io_context with the guarantee that it is not executed before any other handler
+             * posted before has finished.
              * Handlers posted on different Strands can always be run in parallel.
              *
              * Note that "guaranteeToRun" flag of the constructor determines what happens with yet unhandled handlers
-             * when the strand is destructed.
+             * when the Strand is destructed.
              *
              * @param handler function without arguments and return value as r-value reference - will be moved to avoid
              * a copy
@@ -106,12 +128,21 @@ namespace karabo {
             boost::function<void()> wrap(boost::function<void()> handler);
 
             /**
-             * This function may be used to obtain the io_service object that the strand uses to post handlers.
+             * This function may be used to obtain the io_context object that the strand uses to post handlers.
              *
-             * @return A reference to the io_service of the Strand. Ownership is not transferred to the caller.
+             * @return A reference to the io_context of the Strand. Ownership is not transferred to the caller.
              */
-            boost::asio::io_service& get_io_service() const {
-                return m_ioService;
+            boost::asio::io_context& getContext() const {
+                return *m_ioContext;
+            }
+
+            /**
+             * Deprecated.
+             *
+             * Use getContext() instead.
+             */
+            boost::asio::io_context& get_io_service() const {
+                return getContext();
             }
 
            private:
@@ -123,7 +154,7 @@ namespace karabo {
 
             void postWrapped(boost::function<void()> handler);
 
-            boost::asio::io_service& m_ioService;
+            boost::asio::io_context* m_ioContext; // pointer to be able to re-assign it
 
             boost::mutex m_tasksMutex; // to protect both, m_tasksRunning and m_tasks
             bool m_tasksRunning;
