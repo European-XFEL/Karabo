74cdff0e0da73c16504c732bf7c2d692d08e6539
Dennis Goeries <dennis.goeries@xfel.eu>
2022-08-11 13:37:44 +0200

    GUI: Clean and refactor toolbar for table

--- src/pythonGui/karabogui/configurator/dialog/table_view.py
+++ src/pythonGui/karabogui/configurator/dialog/table_view.py
@@ -3,10 +3,11 @@
 # Created on November 9, 2017
 # Copyright (C) European XFEL GmbH Hamburg. All rights reserved.
 #############################################################################
+import weakref
 from pathlib import Path
 
 from qtpy import uic
-from qtpy.QtCore import QModelIndex
+from qtpy.QtCore import QItemSelection, Qt, Slot
 from qtpy.QtWidgets import (
     QDialog, QDialogButtonBox, QHBoxLayout, QPushButton, QVBoxLayout, QWidget)
 
@@ -17,79 +18,82 @@ MAX_WIDTH = 1024
 COLUMN_FACTOR = 120
 
 
-class TableButtonToolbar(QWidget):
-    def __init__(self, table_controller, parent=None):
+class TableToolBar(QWidget):
+    def __init__(self, controller, parent=None):
         super().__init__(parent=parent)
-
-        self.table_controller = table_controller
-        self.table_model = self.table_controller.sourceModel()
-
-        self._move_up_bt = QPushButton(icons.arrowFancyUp, "", self)
-        self._move_down_bt = QPushButton(icons.arrowFancyDown, "", self)
-        self._add_bt = QPushButton(icons.add, "", self)
-        self._du_bt = QPushButton(icons.editCopy, "", self)
-        self._remove_bt = QPushButton(icons.delete, "", self)
-
-        self._move_up_bt.clicked.connect(self._move_row_up)
-        self._move_down_bt.clicked.connect(self._move_row_down)
-        self._add_bt.clicked.connect(self._add_row)
-        self._du_bt.clicked.connect(self._duplicate_row)
-        self._remove_bt.clicked.connect(self._remove_row)
-
-        widget_layout = QVBoxLayout()
-        widget_layout.setContentsMargins(0, 0, 0, 0)
-        widget_layout.addWidget(self._move_up_bt)
-        widget_layout.addWidget(self._move_down_bt)
-        widget_layout.addWidget(self._add_bt)
-        widget_layout.addWidget(self._du_bt)
-        widget_layout.addWidget(self._remove_bt)
-        widget_layout.addStretch(10)
-        widget_layout.setContentsMargins(0, 0, 0, 0)
-        self.setLayout(widget_layout)
-
-        selection_model = self.table_controller.tableWidget().selectionModel()
+        self.setAttribute(Qt.WA_DeleteOnClose)
+        self._controller = weakref.ref(controller)
+        selection_model = controller.tableWidget().selectionModel()
         selection_model.selectionChanged.connect(self._table_selection_changed)
-        self.set_button_enabled()
 
+        self._move_up_button = QPushButton(icons.arrowFancyUp, "", self)
+        self._move_down_button = QPushButton(icons.arrowFancyDown, "", self)
+        self._add_button = QPushButton(icons.add, "", self)
+        self._du_button = QPushButton(icons.editCopy, "", self)
+        self._remove_button = QPushButton(icons.delete, "", self)
+
+        self._move_up_button.clicked.connect(self._move_row_up)
+        self._move_down_button.clicked.connect(self._move_row_down)
+        self._add_button.clicked.connect(self._add_row)
+        self._du_button.clicked.connect(self._duplicate_row)
+        self._remove_button.clicked.connect(self._remove_row)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self._move_up_button)
+        layout.addWidget(self._move_down_button)
+        layout.addWidget(self._add_button)
+        layout.addWidget(self._du_button)
+        layout.addWidget(self._remove_button)
+        layout.addStretch(10)
+        layout.setContentsMargins(0, 0, 0, 0)
+        self.setLayout(layout)
+
+        self._set_button_states()
+
+    @property
+    def controller(self):
+        """Return the weakly stored controller variable"""
+        return self._controller()
+
+    @Slot()
     def _add_row(self):
-        row = self.table_controller.currentIndex().row()
-        self.table_model.insertRows(row + 1, 1, QModelIndex())
-        self.set_button_enabled()
+        self.controller.add_row()
+        self._set_button_states()
 
+    @Slot()
     def _duplicate_row(self):
-        row = self.table_controller.currentIndex().row()
-        self.table_model.duplicate_row(row)
-        self.set_button_enabled()
+        self.controller.duplicate_row()
+        self._set_button_states()
 
+    @Slot()
     def _move_row_up(self):
-        row = self.table_controller.currentIndex().row()
-        self.table_model.move_row_up(row)
-        self.table_controller.tableWidget().selectRow(row - 1)
-        self.set_button_enabled()
+        self.controller.move_row_up()
+        self._set_button_states()
 
+    @Slot()
     def _move_row_down(self):
-        row = self.table_controller.currentIndex().row()
-        self.table_model.move_row_down(row)
-        self.table_controller.tableWidget().selectRow(row + 1)
-        self.set_button_enabled()
+        self.controller.move_row_down()
 
+    @Slot()
     def _remove_row(self):
-        index = self.table_controller.currentIndex()
-        self.table_model.removeRows(index.row(), 1, QModelIndex())
-        self.set_button_enabled()
+        self.controller.remove_row()
+        self._set_button_states()
 
-    def set_button_enabled(self):
-        selection_model = self.table_controller.tableWidget().selectionModel()
-        has_selection = selection_model.hasSelection()
-        current_row = self.table_controller.currentIndex().row()
-        self._move_up_bt.setEnabled(has_selection and current_row > 0)
-        self._move_down_bt.setEnabled(has_selection and current_row <
-                                      self.table_model.rowCount() - 1)
-        self._du_bt.setEnabled(has_selection)
-        self._remove_bt.setEnabled(has_selection)
-
+    @Slot(QItemSelection, QItemSelection)
     def _table_selection_changed(self, selected, deselected):
-        self.set_button_enabled()
+        self._set_button_states()
+
+    def _set_button_states(self):
+        selection_model = self.controller.tableWidget().selectionModel()
+        has_selection = selection_model.hasSelection()
+        current_row = self.controller.currentIndex().row()
+
+        model = self.controller.sourceModel()
+        self._move_up_button.setEnabled(has_selection and current_row > 0)
+        self._move_down_button.setEnabled(has_selection and current_row <
+                                          model.rowCount() - 1)
+        self._du_button.setEnabled(has_selection)
+        self._remove_button.setEnabled(has_selection)
 
 
 class TableDialog(QDialog):
@@ -113,14 +117,14 @@ class TableDialog(QDialog):
         self.controller.set_read_only(not editable)
         self.controller.binding_update(proxy)
         self.controller.value_update(proxy)
-
-        self.toolbar_controller = TableButtonToolbar(
-            table_controller=self.controller)
+        self.toolbar = None
 
         layout = QHBoxLayout()
         layout.setContentsMargins(2, 2, 2, 2)
         layout.addWidget(self.controller.widget)
-        layout.addWidget(self.toolbar_controller)
+        if editable:
+            self.toolbar = TableToolBar(controller=self.controller)
+            layout.addWidget(self.toolbar)
         # `empty_widget` is defined in table_edit.ui
         self.empty_widget.setLayout(layout)
         cancel_button = self.buttonBox.button(QDialogButtonBox.Cancel)
@@ -132,10 +136,11 @@ class TableDialog(QDialog):
         binding = proxy.binding
         if binding is not None:
             columns = len(binding.bindings)
-            # minimum width of 450, but can go up to `MAX_WIDTH`
+            # minimum width of 480, but can go up to `MAX_WIDTH`
             width = min(max(columns * COLUMN_FACTOR, width), MAX_WIDTH)
         self.resize(width, height)
 
     def done(self, result):
         self.controller.destroy_widget()
+        self.toolbar = None
         return super().done(result)
