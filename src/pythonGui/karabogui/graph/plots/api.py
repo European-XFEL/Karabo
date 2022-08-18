52bef06363890f64d741f08988ae88fdb4c6749b
Dennis Goeries <dennis.goeries@xfel.eu>
2022-08-18 17:28:43 +0200

    GUI: Move VectorFillGraph on pyqtgraph builtin

--- src/pythonGui/karabogui/graph/plots/api.py
+++ src/pythonGui/karabogui/graph/plots/api.py
@@ -4,6 +4,6 @@ from .dialogs.hist_config import HistogramDialog
 from .dialogs.range_config import RangeDialog
 from .dialogs.transform_config import TransformDialog
 from .dialogs.view import GraphViewDialog
-from .items import ScatterGraphPlot, VectorBarGraphPlot, VectorFillGraphPlot
+from .items import ScatterGraphPlot, VectorBarGraphPlot
 from .tools.target import CrossTargetController
 from .utils import generate_baseline, generate_down_sample, get_view_range
