5309f50df568e6b48bc427e955521d27a56ebbc0
Dennis Goeries <dennis.goeries@xfel.eu>
2022-08-30 14:05:15 +0200

    GUI: Prevent forbidden ui library imports and rewrite mainwindow test

--- src/pythonGui/karabogui/tests/test_mainwindow.py
+++ src/pythonGui/karabogui/tests/test_mainwindow.py
@@ -1,55 +1,47 @@
-from unittest import main, mock
-
 from qtpy.QtWidgets import QFrame
 
-from karabogui.testing import GuiTestCase, singletons
+from karabogui.testing import singletons
 
 
-class TestCase(GuiTestCase):
+def test_mainwindow(gui_app, mocker, subtests):
+    """Small test for verifying that at least the imports are right"""
 
-    def test_mainwindow(self):
-        """Small test for verifying that at least the imports are right"""
+    network = mocker.Mock()
+    manager = mocker.Mock()
+    mediator = mocker.Mock()
+    with singletons(network=network, manager=manager, mediator=mediator):
+        logger = mocker.patch("karabogui.logger._logger")
         from karabogui.mainwindow import MainWindow
+        mw = MainWindow()
+        logger.info.assert_called_with("Started Karabo GUI application ...")
+        assert mw is not None
 
-        network = mock.Mock()
-        manager = mock.Mock()
-        mediator = mock.Mock()
-        with singletons(network=network, manager=manager, mediator=mediator):
-            mw = MainWindow()
-            self.assertIsNotNone(mw)
+    with subtests.test("Test project conflict event in main window"):
+        from karabo.common.project.api import ProjectModel
+        from karabogui.singletons.project_model import ProjectViewItemModel
 
-        with self.subTest("Test project conflict event in main window"):
-            from karabo.common.project.api import ProjectModel
-            from karabogui.singletons.project_model import ProjectViewItemModel
+        proj = ProjectModel()
+        assert proj.conflict is False
+        qt_model = ProjectViewItemModel(parent=None)
+        qt_model.root_model = proj
+        with singletons(project_model=qt_model):
+            logger = mocker.patch("karabogui.mainwindow.get_logger")
+            data = {"uuids": [proj.uuid]}
+            mw._event_project_updated(data)
+            assert proj.conflict is True
+            logger.assert_called()
 
-            proj = ProjectModel()
-            self.assertEqual(proj.conflict, False)
-            qt_model = ProjectViewItemModel(parent=None)
-            qt_model.root_model = proj
-            with singletons(project_model=qt_model), \
-                    mock.patch("karabogui.mainwindow.get_logger") as logger:
-                data = {"uuids": [proj.uuid]}
-                mw._event_project_updated(data)
-                self.assertEqual(proj.conflict, True)
-                logger.assert_called()
+    with subtests.test("Test the banner notification"):
+        assert mw.notification_banner.frameStyle() == QFrame.NoFrame
 
-        with self.subTest("Test the banner notification"):
-            self.assertEqual(mw.notification_banner.frameStyle(),
-                             QFrame.NoFrame)
+        data = {"message": "Accelerator goes down in 5 min",
+                "trash": "doepianango"}
+        mw._event_server_notification(data)
+        assert mw.notification_banner.frameStyle() == QFrame.Box
+        t = "Accelerator goes down in 5 min"
+        assert mw.notification_banner.toPlainText() == t
 
-            data = {"message": "Accelerator goes down in 5 min",
-                    "trash": "doepianango"}
-            mw._event_server_notification(data)
-            self.assertEqual(mw.notification_banner.frameStyle(), QFrame.Box)
-            self.assertEqual(mw.notification_banner.toPlainText(),
-                             "Accelerator goes down in 5 min")
-
-            data = {"message": "", "trash": "doepianango"}
-            mw._event_server_notification(data)
-            self.assertEqual(mw.notification_banner.toPlainText(), "")
-            self.assertEqual(mw.notification_banner.frameStyle(),
-                             QFrame.NoFrame)
-
-
-if __name__ == "__main__":
-    main()
+        data = {"message": "", "trash": "doepianango"}
+        mw._event_server_notification(data)
+        assert mw.notification_banner.toPlainText() == ""
+        assert mw.notification_banner.frameStyle() == QFrame.NoFrame
