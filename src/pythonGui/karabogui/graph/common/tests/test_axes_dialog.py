434aed39a78317b71f5b7bde1e9a87bbe264f593
Dennis Goeries <dennis.goeries@xfel.eu>
2022-08-31 15:26:20 +0200

    GUI: Align and check window title of axes dialog

--- src/pythonGui/karabogui/graph/common/tests/test_axes_dialog.py
+++ src/pythonGui/karabogui/graph/common/tests/test_axes_dialog.py
@@ -1,39 +1,31 @@
-from unittest import main, mock
-
 from qtpy.QtWidgets import QDialog
 
 from karabogui.graph.common.api import AxesLabelsDialog
-from karabogui.testing import GuiTestCase
 
 
-class TestDialogs(GuiTestCase):
+def test_view_dialog(gui_app, mocker):
+    # Test no config dialog
+    empty_config = {"x_label": "",
+                    "y_label": "",
+                    "x_units": "",
+                    "y_units": ""}
 
-    def test_view_dialog(self):
-        # Test no config dialog
-        empty_config = {"x_label": "",
-                        "y_label": "",
-                        "x_units": "",
-                        "y_units": ""}
+    dialog = AxesLabelsDialog()
+    assert dialog.windowTitle() == "Edit Axes Details"
+    assert dialog.labels == empty_config
 
-        dialog = AxesLabelsDialog()
-        self.assertEqual(dialog.labels, empty_config)
+    config = {"x_label": "Time",
+              "y_label": "#",
+              "x_units": "s",
+              "y_units": ""}
+    dialog = AxesLabelsDialog(config=config)
+    assert dialog.labels == config
 
-        config = {"x_label": "Time",
-                  "y_label": "#",
-                  "x_units": "s",
-                  "y_units": ""}
-        dialog = AxesLabelsDialog(config=config)
-        self.assertEqual(dialog.labels, config)
-
-        path = "karabogui.graph.common.dialogs.axes_labels.AxesLabelsDialog"
-        with mock.patch(path) as d:
-            d().exec.return_value = QDialog.Accepted
-            d().labels = dialog.labels
-            # We fake a success return and test the boolean, content reply
-            content, success = dialog.get(None, None)
-            self.assertTrue(success)
-            self.assertEqual(content, config)
-
-
-if __name__ == "__main__":
-    main()
+    path = "karabogui.graph.common.dialogs.axes_labels.AxesLabelsDialog"
+    d = mocker.patch(path)
+    d().exec.return_value = QDialog.Accepted
+    d().labels = dialog.labels
+    # We fake a success return and test the boolean, content reply
+    content, success = dialog.get(None, None)
+    assert success
+    assert content == config
