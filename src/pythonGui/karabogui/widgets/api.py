00aca25f4de8186bf17d8e99065464cf61091907
Dennis Goeries <dennis.goeries@xfel.eu>
2022-10-18 15:47:40 +0200

    GUI: Align the karabogui api, add DeviceProxy, widget hints, Events, ...

--- src/pythonGui/karabogui/widgets/api.py
+++ src/pythonGui/karabogui/widgets/api.py
@@ -1,4 +1,6 @@
 # flake8: noqa
 from .codeeditor import CodeBook, CodeEditor
-from .hints import DoubleSpinBox, FrameWidget, SpinBox, SvgWidget
+from .hints import (
+    DoubleSpinBox, ElidingLabel, FrameWidget, Label, LineEdit, SpinBox,
+    SvgWidget)
 from .range_slider import RangeSlider
