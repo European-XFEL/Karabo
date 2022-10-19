6791898afa945a27c450938fef9c4c3562ac8f47
Dennis Goeries <dennis.goeries@xfel.eu>
2022-10-19 12:39:40 +0200

    GUI: Refactor project device controller test

--- src/pythonGui/karabogui/project/controller/tests/test_device_instance_controller.py
+++ src/pythonGui/karabogui/project/controller/tests/test_device_instance_controller.py
@@ -1,6 +1,3 @@
-from unittest import TestCase
-from unittest.mock import Mock, patch
-
 from karabo.common.project.device import DeviceInstanceModel
 from karabo.common.project.device_config import DeviceConfigurationModel
 from karabo.common.project.server import DeviceServerModel
@@ -11,146 +8,149 @@ from karabogui.singletons.mediator import Mediator
 from karabogui.testing import singletons, system_hash
 from karabogui.topology.system_topology import SystemTopology
 
-D_ICON = 'karabogui.project.controller.device.get_project_device_status_icon'
-S_ICON = 'karabogui.project.controller.server.get_project_server_status_icon'
-PARENT_PATH = 'karabogui.project.controller.device.find_parent_object'
+from .utils import status_icon_path
+
+PARENT_PATH = "karabogui.project.controller.device.find_parent_object"
 
 
-class TestDeviceInstanceController(TestCase):
+def test_empty_fields_configurator(mocker):
+    device_icon = mocker.patch(status_icon_path("device"))
+    device_icon.return_value = None
 
-    @patch(D_ICON)
-    def test_empty_fields_configurator(self, patch_get_device_icon):
-        patch_get_device_icon.return_value = None
-        network = Mock()
-        mediator = Mediator()
-        topology = SystemTopology()
-        topology.initialize(system_hash())
-        with singletons(network=network, topology=topology, mediator=mediator):
-            foo_config = Hash()
-            foo_config['test1'] = 'value1'
-            foo_config['test2'] = 'value2'
-            foo_config = DeviceConfigurationModel(class_id='BazClass',
-                                                  configuration=foo_config)
-            foo_config.initialized = True
+    network = mocker.Mock()
+    mediator = Mediator()
+    topology = SystemTopology()
+    topology.initialize(system_hash())
+    with singletons(network=network, topology=topology, mediator=mediator):
+        foo_config = Hash()
+        foo_config["test1"] = "value1"
+        foo_config["test2"] = "value2"
+        foo_config = DeviceConfigurationModel(
+            class_id="BazClass",
+            configuration=foo_config)
+        foo_config.initialized = True
 
-            bar_config = Hash()
-            bar_config['test1'] = 'diff_value1'
-            bar_config = DeviceConfigurationModel(class_id='BazClass',
-                                                  configuration=bar_config)
-            bar_config.initialized = True
+        bar_config = Hash()
+        bar_config["test1"] = "diff_value1"
+        bar_config = DeviceConfigurationModel(
+            class_id="BazClass",
+            configuration=bar_config)
+        bar_config.initialized = True
 
-            config_list = [foo_config, bar_config]
+        config_list = [foo_config, bar_config]
 
-            device_model = DeviceInstanceModel(class_id='BazClass',
-                                               server_id='testServer',
-                                               instance_id='fooDevice',
-                                               configs=config_list)
-            device_model.initialized = True
-            device_model.active_config_ref = foo_config.uuid
+        device_model = DeviceInstanceModel(
+            class_id="BazClass",
+            server_id="testServer",
+            instance_id="fooDevice",
+            configs=config_list)
+        device_model.initialized = True
+        device_model.active_config_ref = foo_config.uuid
 
-            def assert_active_configuration():
-                self.assertTrue('test1' in foo_config.configuration)
-                self.assertEqual(foo_config.configuration['test1'], 'value1')
+        def assert_active_configuration():
+            assert "test1" in foo_config.configuration
+            assert foo_config.configuration["test1"] == "value1"
 
-                self.assertTrue('test2' in foo_config.configuration)
-                self.assertEqual(foo_config.configuration['test2'], 'value2')
+            assert "test2" in foo_config.configuration
+            assert foo_config.configuration["test2"] == "value2"
 
-                self.assertTrue('test1' in bar_config.configuration)
-                self.assertEqual(bar_config.configuration['test1'],
-                                 'diff_value1')
+            assert "test1" in bar_config.configuration
+            assert bar_config.configuration["test1"] == "diff_value1"
 
-                self.assertFalse('test2' in bar_config.configuration)
+            assert "test2" not in bar_config.configuration
 
-            controller = DeviceInstanceController(model=device_model)
-            controller.active_config_changed(bar_config)
-            assert_active_configuration()
+        controller = DeviceInstanceController(model=device_model)
+        controller.active_config_changed(bar_config)
+        assert_active_configuration()
 
-            controller.active_config_changed(foo_config)
-            assert_active_configuration()
+        controller.active_config_changed(foo_config)
+        assert_active_configuration()
 
-            controller.active_config_changed(bar_config)
-            assert_active_configuration()
+        controller.active_config_changed(bar_config)
+        assert_active_configuration()
 
-    @patch(D_ICON)
-    @patch(S_ICON)
-    def test_move_devices(self, patch_get_device_icon, patch_get_server_icon):
-        patch_get_device_icon.return_value = None
-        patch_get_server_icon.return_value = None
-        network = Mock()
-        topology = SystemTopology()
-        topology.initialize(system_hash())
-        with singletons(network=network, topology=topology):
-            # 1. Foo device with config
-            foo_config = Hash()
-            foo_config['value'] = '1'
-            foo_config = DeviceConfigurationModel(class_id='BazClass',
-                                                  configuration=foo_config)
-            foo_config.initialized = True
-            foo_model = DeviceInstanceModel(class_id='BazClass',
-                                            instance_id='fooDevice',
-                                            server_id='testServer',
-                                            configs=[foo_config])
-            foo_model.initialized = True
-            foo_model.active_config_ref = foo_config.uuid
 
-            # 2. Bar device with config
-            bar_config = Hash()
-            bar_config['value'] = '2'
-            bar_config = DeviceConfigurationModel(class_id='BazClass',
-                                                  configuration=bar_config)
-            bar_config.initialized = True
-            bar_model = DeviceInstanceModel(class_id='BazClass',
-                                            server_id='testServer',
-                                            instance_id='fooDevice2',
-                                            configs=[bar_config])
-            bar_model.initialized = True
-            bar_model.active_config_ref = bar_config.uuid
+def test_move_devices(mocker):
+    device_icon = mocker.patch(status_icon_path("device"))
+    device_icon.return_value = None
+    server_icon = mocker.patch(status_icon_path("device"))
+    server_icon.return_value = None
 
-            # 3. Server
-            server_model = DeviceServerModel(
-                server_id='testServer', host='serverFoo',
-                devices=[foo_model, bar_model])
-            server_model.initialized = True
+    network = mocker.Mock()
+    topology = SystemTopology()
+    topology.initialize(system_hash())
+    with singletons(network=network, topology=topology):
+        # 1. Foo device with config
+        foo_config = DeviceConfigurationModel(
+            class_id="BazClass",
+            configuration=Hash("value", "1"))
+        foo_config.initialized = True
+        foo_model = DeviceInstanceModel(
+            class_id="BazClass",
+            instance_id="fooDevice",
+            server_id="testServer",
+            configs=[foo_config])
+        foo_model.initialized = True
+        foo_model.active_config_ref = foo_config.uuid
 
-            # 4. Controllers
-            foo_controller = DeviceInstanceController(model=foo_model)
-            bar_controller = DeviceInstanceController(model=bar_model)
-            server_controller = DeviceServerController(model=server_model)
+        # 2. Bar device with config
+        bar_config = DeviceConfigurationModel(
+            class_id="BazClass",
+            configuration=Hash("value", "2"))
+        bar_config.initialized = True
+        bar_model = DeviceInstanceModel(
+            class_id="BazClass",
+            server_id="testServer",
+            instance_id="fooDevice2",
+            configs=[bar_config])
+        bar_model.initialized = True
+        bar_model.active_config_ref = bar_config.uuid
 
-            self.assertEqual(server_controller.model.devices[0], foo_model)
-            self.assertEqual(server_controller.model.devices[1], bar_model)
+        # 3. Server
+        server_model = DeviceServerModel(
+            server_id="testServer", host="serverFoo",
+            devices=[foo_model, bar_model])
+        server_model.initialized = True
 
-            # 5. Fake project class for testing with a model member
-            class Project:
-                model = None
+        # 4. Controllers
+        foo_controller = DeviceInstanceController(model=foo_model)
+        bar_controller = DeviceInstanceController(model=bar_model)
+        server_controller = DeviceServerController(model=server_model)
 
-            project = Project()
+        assert server_controller.model.devices[0] == foo_model
+        assert server_controller.model.devices[1] == bar_model
 
-            with patch(PARENT_PATH) as find_parent_mock:
-                find_parent_mock.return_value = server_model
-                foo_controller._move_down(project)
+        # 5. Fake project class for testing with a model member
+        class Project:
+            model = None
 
-                self.assertEqual(server_controller.model.devices[0], bar_model)
-                self.assertEqual(server_controller.model.devices[1], foo_model)
+        project = Project()
 
-                foo_controller._move_up(project)
+        find_parent = mocker.patch(PARENT_PATH)
+        find_parent.return_value = server_model
+        foo_controller._move_down(project)
 
-                self.assertEqual(server_controller.model.devices[0], foo_model)
-                self.assertEqual(server_controller.model.devices[1], bar_model)
+        assert server_controller.model.devices[0] == bar_model
+        assert server_controller.model.devices[1] == foo_model
 
-                bar_controller._move_up(project)
+        foo_controller._move_up(project)
 
-                self.assertEqual(server_controller.model.devices[0], bar_model)
-                self.assertEqual(server_controller.model.devices[1], foo_model)
+        assert server_controller.model.devices[0] == foo_model
+        assert server_controller.model.devices[1] == bar_model
 
-                # Lets try to crack it and move out of range (< 0)
-                bar_controller._move_up(project)
+        bar_controller._move_up(project)
 
-                self.assertEqual(server_controller.model.devices[0], bar_model)
-                self.assertEqual(server_controller.model.devices[1], foo_model)
+        assert server_controller.model.devices[0] == bar_model
+        assert server_controller.model.devices[1] == foo_model
 
-                # Lets try to crack it and move out of range (> len)
-                foo_controller._move_down(project)
+        # Lets try to crack it and move out of range (< 0)
+        bar_controller._move_up(project)
 
-                self.assertEqual(server_controller.model.devices[0], bar_model)
-                self.assertEqual(server_controller.model.devices[1], foo_model)
+        assert server_controller.model.devices[0] == bar_model
+        assert server_controller.model.devices[1] == foo_model
+
+        # Lets try to crack it and move out of range (> len)
+        foo_controller._move_down(project)
+
+        assert server_controller.model.devices[0] == bar_model
+        assert server_controller.model.devices[1] == foo_model
