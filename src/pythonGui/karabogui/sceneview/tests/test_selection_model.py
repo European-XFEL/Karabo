8ed500e2aa3f6004243f923a274d802158fecc39
Dennis Goeries <dennis.goeries@xfel.eu>
2022-10-20 10:46:12 +0200

    GUI: Fix selection model handling of UnknownSVG's

--- src/pythonGui/karabogui/sceneview/tests/test_selection_model.py
+++ src/pythonGui/karabogui/sceneview/tests/test_selection_model.py
@@ -3,47 +3,87 @@
 # Created on June 16, 2016
 # Copyright (C) European XFEL GmbH Hamburg. All rights reserved.
 #############################################################################
-import unittest
 
-from karabo.common.scenemodel.api import LineModel, RectangleModel
-from karabogui.sceneview.selection_model import SceneSelectionModel
-from karabogui.sceneview.shapes import LineShape, RectangleShape
+from pathlib import Path
+
+from qtpy.QtCore import QByteArray
+from qtpy.QtSvg import QSvgRenderer
+
+from karabo.common.scenemodel.api import (
+    LabelModel, LineModel, RectangleModel, UnknownWidgetDataModel)
+from karabogui import icons
+from karabogui.sceneview.api import (
+    LabelWidget, LineShape, RectangleShape, SceneSelectionModel,
+    UnknownSvgWidget, UnknownWidget)
 
 
-class TestSelectionModel(unittest.TestCase):
-    def setUp(self):
-        self.selection_model = SceneSelectionModel()
+def test_selection_model_types(gui_app):
+    selection_model = SceneSelectionModel()
+    assert not selection_model.has_selection()
+    endpoint_x, endpoint_y = (100, 100)
 
-    def test_selection_model(self):
-        self.assertFalse(self.selection_model.has_selection())
-        endpoint_x, endpoint_y = (100, 100)
+    line_model = LineModel(x1=0, y1=0, x2=endpoint_x, y2=endpoint_y)
+    line_shape = LineShape(model=line_model)
+    selection_model.select_object(line_shape)
+    assert selection_model.has_selection()
 
-        line_model = LineModel(x1=0, y1=0, x2=endpoint_x, y2=endpoint_y)
-        line_shape = LineShape(model=line_model)
-        self.selection_model.select_object(line_shape)
-        self.assertTrue(self.selection_model.has_selection())
+    selection_model.clear_selection()
+    assert not selection_model.has_selection()
 
-        rect_model = RectangleModel(x=endpoint_x, y=endpoint_y,
-                                    width=100, height=100)
-        rect_shape = RectangleShape(model=rect_model)
-        self.selection_model.select_object(rect_shape)
-        self.assertTrue(self.selection_model.has_selection())
+    rect_model = RectangleModel(x=endpoint_x, y=endpoint_y,
+                                width=100, height=100)
+    rect_shape = RectangleShape(model=rect_model)
+    selection_model.select_object(rect_shape)
+    assert selection_model.has_selection()
 
-        self.selection_model.clear_selection()
-        self.assertFalse(self.selection_model.has_selection())
+    selection_model.clear_selection()
+    assert not selection_model.has_selection()
 
-        self.selection_model.select_object(line_shape)
-        self.selection_model.select_object(rect_shape)
+    selection_model.select_object(line_shape)
+    selection_model.select_object(rect_shape)
 
-        for obj in self.selection_model:
-            self.assertIn(obj, (line_shape, rect_shape))
+    for obj in selection_model:
+        assert obj in (line_shape, rect_shape)
 
-        exp_rect = line_shape.geometry().united(rect_shape.geometry())
-        sel_rect = self.selection_model.get_selection_bounds()
-        self.assertEqual(exp_rect, sel_rect)
+    exp_rect = line_shape.geometry().united(rect_shape.geometry())
+    sel_rect = selection_model.get_selection_bounds()
+    assert exp_rect == sel_rect
 
-        self.selection_model.deselect_object(line_shape)
-        self.assertTrue(self.selection_model.has_selection())
+    selection_model.deselect_object(line_shape)
+    assert selection_model.has_selection()
 
-        self.selection_model.deselect_object(rect_shape)
-        self.assertFalse(self.selection_model.has_selection())
+    selection_model.deselect_object(rect_shape)
+    assert not selection_model.has_selection()
+
+
+def test_selection_model_unknown(gui_app):
+    selection_model = SceneSelectionModel()
+    assert not selection_model.has_selection()
+
+    label_model = LabelModel()
+    label_widget = LabelWidget(label_model)
+
+    unknown_model = UnknownWidgetDataModel(
+        klass="Notthere",
+        data="",
+        attributes={})
+    unknown_widget = UnknownWidget(unknown_model)
+
+    selection_model.select_object(label_widget)
+    selection_model.select_object(unknown_widget)
+    assert selection_model.has_selection()
+    assert len(selection_model) == 2
+
+    path = Path(icons.__file__).parent / "about.svg"
+    with open(path, "rb") as fp:
+        b = fp.read()
+
+    ar = QByteArray.fromRawData(b)
+    renderer = QSvgRenderer(ar)
+    assert renderer.isValid()
+    svg_widget = UnknownSvgWidget(renderer=renderer)
+
+    # Try to select an unknown svg widget does nothing
+    selection_model.select_object(svg_widget)
+    assert selection_model.has_selection()
+    assert len(selection_model) == 2
