eab5871bc47214788903e9fe6f8d2c1f7f8761fa
Dennis Goeries <dennis.goeries@xfel.eu>
2022-10-25 18:47:50 +0200

    GUI: Provide helper tool to check for project instance duplicates

---
+++ src/pythonGui/karabogui/dialogs/tests/test_data_view_dialog.py
@@ -0,0 +1,13 @@
+from karabo.native import Hash
+from karabogui.dialogs.api import DataViewDialog
+
+
+def test_data_view(gui_app):
+    data = Hash("Data", "Karabo")
+
+    title = "This is the title"
+    info = "This is the description info"
+    dialog = DataViewDialog(title, info, data)
+    assert dialog.windowTitle() == title
+    assert dialog.ui_info.text() == info
+    assert dialog.ui_text_info.toPlainText() == "\nData\nKarabo\n"
