e1aa42307c04cbad2a39048bca5772f00bfb8289
Dennis Goeries <dennis.goeries@xfel.eu>
2022-10-26 12:36:34 +0200

    GUI: Abstract more the BaselineEditController

---
+++ src/pythonGui/karabogui/controllers/tests/test_base_line_edit.py
@@ -0,0 +1,68 @@
+from traits.api import Instance, Int
+
+from karabo.common.scenemodel.api import BaseWidgetObjectData
+from karabo.native import AccessMode, Configurable, String
+from karabogui.binding.api import (
+    DeviceClassProxy, PropertyProxy, ProxyStatus, StringBinding, build_binding)
+from karabogui.testing import flushed_registry, set_proxy_value
+
+from ..baselineedit import BaseLineEditController
+from ..registry import register_binding_controller
+
+
+class Object(Configurable):
+    string = String(accessMode=AccessMode.RECONFIGURABLE)
+
+
+class UniqueWidgetModel(BaseWidgetObjectData):
+    pass
+
+
+def _define_binding_classes():
+    @register_binding_controller(klassname="NewEdit",
+                                 binding_type=StringBinding,
+                                 can_edit=True, priority=-30)
+    class CustomLineEdit(BaseLineEditController):
+        model = Instance(UniqueWidgetModel, args=())
+
+        counter = Int(0)
+
+        def onText(self, text):
+            super().onText(text)
+            self.counter += 1
+
+    return {
+        "CustomLineEdit": CustomLineEdit,
+    }
+
+
+def test_custom_line_edit(gui_app):
+    """Test the creation of a simple `BaseLineEditController`"""
+    with flushed_registry():
+        klasses = _define_binding_classes()
+
+    # Save the classes so they only need to be declared once
+    edit_controller = klasses["CustomLineEdit"]
+
+    schema = Object.getClassSchema()
+    binding = build_binding(schema)
+    device = DeviceClassProxy(binding=binding, server_id="NoDeviceServer",
+                              status=ProxyStatus.OFFLINE)
+    proxy = PropertyProxy(root_proxy=device, path="string")
+    assert proxy.binding is not None
+
+    controller = edit_controller(proxy=proxy)
+    controller.create(None)
+    controller.set_read_only(False)
+    assert controller.widget is not None
+    assert controller.counter == 0
+
+    controller.finish_initialization()
+    assert controller.internal_widget.validator() is None
+    set_proxy_value(proxy, "string", "Update")
+    assert controller.internal_widget.text() == "Update"
+    assert controller.counter == 0
+
+    controller.internal_widget.setText("NoBeam")
+    assert controller.counter == 1
+    assert controller.internal_widget.text() == "NoBeam"
