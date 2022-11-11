5c34c5ba333c674c255e623635ae0cc73ac018f9
Noushadali Anakkappalla <noushadali.anakkappalla@xfel.edu>
2022-11-11 19:10:17 +0100

    Move test out of unittest framework.
    This mainly removes the test out of unittest framework and uses pytest fixture instead.
    
    Also cleaned the code a bit.
    (I guess we can still reduce a few lines but avoiding it in favour of readability)

--- src/pythonGui/karabogui/dialogs/tests/test_list_edit_dialog.py
+++ src/pythonGui/karabogui/dialogs/tests/test_list_edit_dialog.py
@@ -1,214 +1,158 @@
-from unittest import main, mock
+import pytest
 
 from karabogui.binding.api import (
     VectorBoolBinding, VectorDoubleBinding, VectorInt64Binding,
     VectorStringBinding)
 from karabogui.controllers.table.api import string2list
 from karabogui.dialogs.api import ListEditDialog
-from karabogui.testing import GuiTestCase
 
 
-class TestListEditDialog(GuiTestCase):
+@pytest.fixture
+def dialogs(gui_app):
+    string_binding = VectorStringBinding()
+    string_dialog = ListEditDialog(binding=string_binding)
+    string_dialog.set_list(["1", "2"])
 
-    def setUp(self):
-        super().setUp()
-        string_binding = VectorStringBinding()
-        self.string_dialog = ListEditDialog(binding=string_binding)
-        self.string_dialog.set_list(["1", "2"])
+    bool_binding = VectorBoolBinding()
+    bool_dialog = ListEditDialog(binding=bool_binding)
+    bool_dialog.set_list([True, False, True])
 
-        bool_binding = VectorBoolBinding()
-        self.bool_dialog = ListEditDialog(binding=bool_binding)
-        self.bool_dialog.set_list([True, False, True])
+    int_binding = VectorInt64Binding()
+    int_dialog = ListEditDialog(binding=int_binding)
+    int_dialog.set_list([1, 2, 3])
 
-        int_binding = VectorInt64Binding()
-        self.int_dialog = ListEditDialog(binding=int_binding)
-        self.int_dialog.set_list([1, 2, 3])
+    double_binding = VectorDoubleBinding()
+    double_dialog = ListEditDialog(binding=double_binding)
+    double_dialog.set_list([1.0, 2.0, 3.0])
 
-        double_binding = VectorDoubleBinding()
-        self.double_dialog = ListEditDialog(binding=double_binding)
-        self.double_dialog.set_list([1.0, 2.0, 3.0])
+    return string_dialog, bool_dialog, int_dialog, double_dialog
 
-    def test_allowed_choices(self):
-        self.assertEqual(self.string_dialog._allowed_choices, {})
-        self.assertEqual(self.int_dialog._allowed_choices, {})
-        self.assertEqual(self.bool_dialog._allowed_choices, {"False": False,
-                                                             "True": True})
-        self.assertEqual(self.double_dialog._allowed_choices, {})
 
-        self.assertEqual(self.string_dialog.windowTitle(), "Edit list")
-        self.assertEqual(self.int_dialog.windowTitle(), "Edit list")
-        self.assertEqual(self.bool_dialog.windowTitle(), "Edit list")
-        self.assertEqual(self.double_dialog.windowTitle(), "Edit list")
+def test_allowed_choices(dialogs):
 
-    def test_set_list(self):
-        dialog = self.string_dialog
-        self.assertEqual(dialog.list_widget.count(), 2)
-        dialog.set_list(["1", "2", "3"])
-        self.assertEqual(dialog.list_widget.count(), 3)
-        self.assertEqual(dialog.values, ["1", "2", "3"])
+    string_dialog, bool_dialog, int_dialog, double_dialog = dialogs
 
-        dialog = self.bool_dialog
-        self.assertEqual(dialog.list_widget.count(), 3)
-        self.assertEqual(dialog.values, [True, False, True])
-        dialog.set_list([])
-        self.assertEqual(dialog.list_widget.count(), 0)
-        self.assertEqual(dialog.values, [])
+    assert string_dialog._allowed_choices == {}
+    assert int_dialog._allowed_choices == {}
+    assert bool_dialog._allowed_choices == {"False": False, "True": True}
+    assert double_dialog._allowed_choices == {}
 
-        dialog = self.int_dialog
-        self.assertEqual(dialog.list_widget.count(), 3)
-        dialog.set_list([1, 2, 3, 4, 5])
-        self.assertEqual(dialog.list_widget.count(), 5)
-        self.assertEqual(dialog.values, [1, 2, 3, 4, 5])
+    for dialog in dialogs:
+        assert dialog.windowTitle() == "Edit list"
 
-        dialog = self.double_dialog
-        self.assertEqual(dialog.list_widget.count(), 3)
-        dialog.set_list([1.1, 2.2, 3.3, 4.4, 5.5])
-        self.assertEqual(dialog.list_widget.count(), 5)
-        self.assertEqual(dialog.values, [1.1, 2.2, 3.3, 4.4, 5.5])
 
-    def test_on_add_slot(self):
-        """Test the add slot of the list edit dialog"""
-        dialog = self.string_dialog
-        with mock.patch("karabogui.dialogs.listedit.QInputDialog") as d:
-            d.getText.return_value = "new", True
-            dialog._add_clicked()
-            self.assertEqual(dialog.list_widget.count(), 3)
-        self.assertEqual(dialog.values, ["1", "2", "new"])
+def test_set_list(dialogs):
+    string_dialog, bool_dialog, int_dialog, double_dialog = dialogs
 
-        dialog = self.int_dialog
-        with mock.patch("karabogui.dialogs.listedit.QInputDialog") as d:
-            d.getInt.return_value = 55, True
-            dialog._add_clicked()
-            self.assertEqual(dialog.list_widget.count(), 4)
-        self.assertEqual(dialog.values, [1, 2, 3, 55])
+    assert string_dialog.list_widget.count() == 2
+    string_dialog.set_list(["1", "2", "3"])
+    assert string_dialog.list_widget.count() == 3
+    assert string_dialog.values == ["1", "2", "3"]
 
-        dialog = self.bool_dialog
-        with mock.patch("karabogui.dialogs.listedit.QInputDialog") as d:
-            # Item dialog with keys of choices, we return first choice
-            d.getItem.return_value = "False", True
-            dialog._add_clicked()
-            self.assertEqual(dialog.list_widget.count(), 4)
-        self.assertEqual(dialog.values, [True, False, True, False])
+    assert bool_dialog.list_widget.count() == 3
+    assert bool_dialog.values == [True, False, True]
+    bool_dialog.set_list([])
+    assert bool_dialog.list_widget.count() == 0
+    assert bool_dialog.values == []
 
-        dialog = self.double_dialog
-        with mock.patch("karabogui.dialogs.listedit.QInputDialog") as d:
-            d.getDouble.return_value = 42.2, True
-            dialog._add_clicked()
-            self.assertEqual(dialog.list_widget.count(), 4)
-        self.assertEqual(dialog.values, [1.0, 2.0, 3.0, 42.2])
+    assert int_dialog.list_widget.count() == 3
+    int_dialog.set_list([1, 2, 3, 4, 5])
+    assert int_dialog.list_widget.count() == 5
+    assert int_dialog.values == [1, 2, 3, 4, 5]
 
-    def test_on_remove_slot(self):
-        """Test the remove slot of the list edit dialog"""
-        dialog = self.string_dialog
-        self.assertEqual(dialog.list_widget.count(), 2)
+    assert double_dialog.list_widget.count() == 3
+    double_dialog.set_list([1.1, 2.2, 3.3, 4.4, 5.5])
+    assert double_dialog.list_widget.count() == 5
+    assert double_dialog.values == [1.1, 2.2, 3.3, 4.4, 5.5]
+
+
+def test_on_add_slot(dialogs, mocker):
+    """Test the add slot of the list edit dialog"""
+    string_dialog, bool_dialog, int_dialog, double_dialog = dialogs
+
+    mock_input_dialog = mocker.patch("karabogui.dialogs.listedit.QInputDialog")
+
+    mock_input_dialog.getText.return_value = "new", True
+    string_dialog._add_clicked()
+    assert string_dialog.list_widget.count() == 3
+    assert string_dialog.values == ["1", "2", "new"]
+
+    mock_input_dialog.getItem.return_value = "False", True
+    bool_dialog._add_clicked()
+    assert bool_dialog.list_widget.count() == 4
+    assert bool_dialog.values == [True, False, True, False]
+
+    mock_input_dialog.getInt.return_value = 55, True
+    int_dialog._add_clicked()
+    assert int_dialog.list_widget.count() == 4
+    assert int_dialog.values == [1, 2, 3, 55]
+
+    mock_input_dialog.getDouble.return_value = 42.2, True
+    double_dialog._add_clicked()
+    assert double_dialog.list_widget.count() == 4
+    assert double_dialog.values == [1.0, 2.0, 3.0, 42.2]
+
+
+def test_on_remove_slot(dialogs):
+    """Test the remove slot of the list edit dialog"""
+    for dialog in dialogs:
+        expected_values = dialog.values[1:]
         dialog.list_widget.setCurrentRow(0)
         dialog._remove_clicked()
-        self.assertEqual(dialog.list_widget.count(), 1)
-        self.assertEqual(dialog.values, ["2"])
+        assert dialog.list_widget.count() == len(expected_values)
+        assert dialog.values == expected_values
 
-        dialog = self.int_dialog
-        self.assertEqual(dialog.list_widget.count(), 3)
-        dialog.list_widget.setCurrentRow(0)
-        dialog._remove_clicked()
-        self.assertEqual(dialog.list_widget.count(), 2)
-        self.assertEqual(dialog.values, [2, 3])
 
-        dialog = self.bool_dialog
-        self.assertEqual(dialog.list_widget.count(), 3)
-        dialog.list_widget.setCurrentRow(0)
-        dialog._remove_clicked()
-        self.assertEqual(dialog.list_widget.count(), 2)
-        # XXX: Booleans as integers
-        self.assertEqual(dialog.values, [0, 1])
-
-        dialog = self.double_dialog
-        self.assertEqual(dialog.list_widget.count(), 3)
-        dialog.list_widget.setCurrentRow(0)
-        dialog._remove_clicked()
-        self.assertEqual(dialog.list_widget.count(), 2)
-        self.assertEqual(dialog.values, [2.0, 3.0])
-
-    def test_move_up_slot(self):
-        dialog = self.string_dialog
+def test_move_up_slot(dialogs):
+    """Move up button on second element should move it up by one row"""
+    for dialog in dialogs:
+        values = dialog.values
+        values[1], values[0] = values[0], values[1]
         dialog.list_widget.setCurrentRow(1)
         dialog._move_up_clicked()
-        self.assertEqual(dialog.values, ["2", "1"])
+        assert values == dialog.values
 
-        dialog = self.int_dialog
-        dialog.list_widget.setCurrentRow(1)
-        dialog._move_up_clicked()
-        self.assertEqual(dialog.values, [2, 1, 3])
 
-        dialog = self.bool_dialog
-        dialog.list_widget.setCurrentRow(1)
-        dialog._move_up_clicked()
-        self.assertEqual(dialog.values, [0, 1, 1])
-
-        dialog = self.double_dialog
-        dialog.list_widget.setCurrentRow(1)
-        dialog._move_up_clicked()
-        self.assertEqual(dialog.values, [2.0, 1.0, 3.0])
-
-    def test_move_down_slot(self):
-        dialog = self.string_dialog
+def test_move_down_slot(dialogs):
+    """Move down button on first element should move it down by one row"""
+    for dialog in dialogs:
+        values = dialog.values
+        values[0], values[1] = values[1], values[0]
         dialog.list_widget.setCurrentRow(0)
         dialog._move_down_clicked()
-        self.assertEqual(dialog.values, ["2", "1"])
-
-        dialog = self.int_dialog
-        dialog.list_widget.setCurrentRow(0)
-        dialog._move_down_clicked()
-        self.assertEqual(dialog.values, [2, 1, 3])
-
-        dialog = self.bool_dialog
-        dialog.list_widget.setCurrentRow(0)
-        dialog._move_down_clicked()
-        self.assertEqual(dialog.values, [0, 1, 1])
-
-        dialog = self.double_dialog
-        dialog.list_widget.setCurrentRow(0)
-        dialog._move_down_clicked()
-        self.assertEqual(dialog.values, [2.0, 1.0, 3.0])
-
-    def test_edit_slot(self):
-        dialog = self.string_dialog
-        dialog.list_widget.setCurrentRow(0)
-        with mock.patch("karabogui.dialogs.listedit.QInputDialog") as d:
-            d.getText.return_value = "new", True
-            dialog._edit_clicked()
-            self.assertEqual(dialog.values, ["new", "2"])
-
-        dialog = self.int_dialog
-        dialog.list_widget.setCurrentRow(0)
-        with mock.patch("karabogui.dialogs.listedit.QInputDialog") as d:
-            d.getInt.return_value = 55, True
-            dialog._edit_clicked()
-            self.assertEqual(dialog.values, [55, 2, 3])
-
-        dialog = self.bool_dialog
-        dialog.list_widget.setCurrentRow(0)
-        with mock.patch("karabogui.dialogs.listedit.QInputDialog") as d:
-            # Item dialog with keys of choices, we return first choice
-            d.getItem.return_value = "False", True
-            dialog._edit_clicked()
-            self.assertEqual(dialog.values, [0, 0, 1])
-
-        dialog = self.double_dialog
-        dialog.list_widget.setCurrentRow(0)
-        with mock.patch("karabogui.dialogs.listedit.QInputDialog") as d:
-            d.getDouble.return_value = 42.2, True
-            dialog._edit_clicked()
-            self.assertEqual(dialog.values, [42.2, 2.0, 3.0])
-
-    def test_binding_validation(self):
-
-        for dialog in (self.string_dialog, self.bool_dialog, self.int_dialog,
-                       self.double_dialog):
-            binding = dialog.binding
-            values = string2list(dialog.string_values)
-            assert binding.validate_trait("value", values) is not None, \
-                   f"Validation of {values} for {binding} failed"
+        assert values == dialog.values
 
 
-if __name__ == "__main__":
-    main()
+def test_edit_slot(dialogs, mocker):
+    string_dialog, bool_dialog, int_dialog, double_dialog = dialogs
+
+    mock_input_dialog = mocker.patch("karabogui.dialogs.listedit.QInputDialog")
+
+    string_dialog.list_widget.setCurrentRow(0)
+    mock_input_dialog.getText.return_value = "new", True
+    string_dialog._edit_clicked()
+    assert string_dialog.values == ["new", "2"]
+
+    bool_dialog.list_widget.setCurrentRow(0)
+    # Item dialog with keys of choices, we return first choice
+    mock_input_dialog.getItem.return_value = "False", True
+    bool_dialog._edit_clicked()
+    assert bool_dialog.values == [False, False, True]
+
+    int_dialog.list_widget.setCurrentRow(0)
+    mock_input_dialog.getInt.return_value = 55, True
+    int_dialog._edit_clicked()
+    assert int_dialog.values == [55, 2, 3]
+
+    double_dialog.list_widget.setCurrentRow(0)
+    mock_input_dialog.getDouble.return_value = 42.2, True
+    double_dialog._edit_clicked()
+    assert double_dialog.values == [42.2, 2.0, 3.0]
+
+
+def test_binding_validation(dialogs):
+    for dialog in dialogs:
+        binding = dialog.binding
+        values = string2list(dialog.string_values)
+        assert binding.validate_trait("value", values) is not None, \
+            f"Validation of {values} for {binding} failed"
