f95324d0f3dac071d254b650a2b14ab1bb080728
Alessandro Silenzi <alessandro.silenzi@xfel.eu>
2022-11-25 16:18:16 +0100

    GUI: Implement InstanceStatus widget

--- src/pythonGui/karabogui/controllers/display/tests/test_checkbox.py
+++ src/pythonGui/karabogui/controllers/display/tests/test_checkbox.py
@@ -1,28 +1,13 @@
-from xml.etree.ElementTree import parse
-
 from karabo.native import Bool, Configurable
 from karabogui.testing import (
-    GuiTestCase, get_class_property_proxy, set_proxy_value)
+    GuiTestCase, check_renderer_against_svg, get_class_property_proxy,
+    set_proxy_value)
 
 from ..checkbox import CHECKED, UNCHECKED, DisplayCheckBox
 
 
 class Object(Configurable):
     prop = Bool(defaultValue=True)
-
-
-def _check_renderer_against_svg(renderer, svgfile):
-    """Yeah... Check an SVG for elements which we suspect it should have.
-    """
-    def _get_path_ids(filename):
-        tree = parse(filename)
-        ids = []
-        for elem in tree.iter('{http://www.w3.org/2000/svg}path'):
-            ids.append(elem.get('id'))
-        return ids
-
-    for name in _get_path_ids(svgfile):
-        assert renderer.elementExists(name)
 
 
 class TestDisplayCheckBox(GuiTestCase):
@@ -41,9 +26,9 @@ class TestDisplayCheckBox(GuiTestCase):
 
     def test_set_value(self):
         set_proxy_value(self.proxy, 'prop', True)
-        _check_renderer_against_svg(self.controller.widget.renderer(),
-                                    CHECKED)
+        check_renderer_against_svg(self.controller.widget.renderer(),
+                                   CHECKED)
 
         set_proxy_value(self.proxy, 'prop', False)
-        _check_renderer_against_svg(self.controller.widget.renderer(),
-                                    UNCHECKED)
+        check_renderer_against_svg(self.controller.widget.renderer(),
+                                   UNCHECKED)
