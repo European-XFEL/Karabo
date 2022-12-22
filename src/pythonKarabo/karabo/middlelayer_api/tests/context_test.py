2c49b01dfc076eaf0b5c3f4f2c3e807fa21a271a
Dennis Goeries <dennis.goeries@xfel.eu>
2022-12-23 00:59:38 +0100

    MDL: Enhance pytest loop tests with instance attachment

--- src/pythonKarabo/karabo/middlelayer_api/tests/context_test.py
+++ src/pythonKarabo/karabo/middlelayer_api/tests/context_test.py
@@ -1,14 +1,17 @@
 import uuid
+from asyncio import ensure_future, get_event_loop, sleep
 
 import pytest
 
-from karabo.middlelayer import Device, Slot, String, connectDevice, isSet
-from karabo.middlelayer_api.tests.eventloop import (
-    AsyncDeviceContext, create_device_server, event_loop)
+from karabo.middlelayer import (
+    Device, Slot, String, connectDevice, getDevice, isSet, lock)
+from karabo.middlelayer.testing import (
+    AsyncDeviceContext, create_device_server, event_loop, run_test)
 
 
-class WW(Device):
+class Figure(Device):
     name = String()
+    figure = ""
 
     def __init__(self, configuration):
         super().__init__(configuration)
@@ -16,12 +19,21 @@ class WW(Device):
 
     @Slot()
     async def sayMyName(self):
-        self.name = "Heisenberg"
+        self.name = self.figure
 
     async def onDestruction(self):
         self.destructed = True
 
 
+class WW(Figure):
+    figure = "Heisenberg"
+
+
+class JP(Figure):
+    figure = "PinkMan"
+
+
+@pytest.mark.timeout(30)
 @pytest.mark.asyncio
 async def test_device_context(event_loop: event_loop):
     deviceId = f"test-mdl-{uuid.uuid4()}"
@@ -48,6 +60,7 @@ async def test_device_context(event_loop: event_loop):
     assert device.destructed is True
 
 
+@pytest.mark.timeout(30)
 @pytest.mark.asyncio
 async def test_server_context(event_loop: event_loop):
     serverId = f"testserver-mdl-{uuid.uuid4()}"
@@ -56,3 +69,53 @@ async def test_server_context(event_loop: event_loop):
         server_instance = ctx.instances["server"]
         assert server_instance.serverId == serverId
         assert "WW" in server_instance.plugins
+
+
+@pytest.fixture(scope="function")
+@pytest.mark.asyncio
+async def deviceTest(event_loop):
+    ww = WW({"_deviceId_": f"test-ww-{uuid.uuid4()}"})
+    jp = JP({"_deviceId_": f"test-jp-{uuid.uuid4()}"})
+    event_loop.lead = ww
+    async with AsyncDeviceContext(jp=jp, ww=ww) as ctx:
+        yield ctx
+    event_loop.lead = None
+
+
+@pytest.mark.timeout(30)
+@run_test
+async def test_loop_lead(deviceTest):
+    wwId = deviceTest["ww"].deviceId
+    jp = deviceTest["jp"]
+    assert get_event_loop().lead is deviceTest["ww"]
+    assert get_event_loop().instance() is deviceTest["ww"]
+    async with getDevice(jp.deviceId) as proxy:
+        with await lock(proxy):
+            assert jp.lockedBy == wwId
+
+
+@pytest.mark.timeout(30)
+@pytest.mark.asyncio
+async def test_loop_instance(event_loop):
+    ww = WW({"_deviceId_": f"test-ww-{uuid.uuid4()}"})
+    async with AsyncDeviceContext(ww=ww):
+        task = ensure_future(sleep(5))
+        await sleep(0.1)
+        # instance is signal slotable
+        assert task.instance() is not ww
+        assert task.instance() is not None
+        task.cancel()
+
+        sleep_task = None
+
+        async def sleepalot():
+            nonlocal sleep_task
+            sleep_task = ensure_future(sleep(5))
+
+        task = event_loop.create_task(sleepalot(), instance=ww)
+        await sleep(0.1)
+        assert task.instance() is ww
+        task.cancel()
+        assert sleep_task is not None
+        assert sleep_task.instance() is ww
+        sleep_task.cancel()
