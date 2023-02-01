6c4929d7e49c2d9bad4019e715a65a72f19fd410
Raul Costa <raul.costa@xfel.eu>
2023-02-01 13:30:36 +0100

    C++: Fix a bug in the parsing of HTTP headers by the InfluxDbClient and add a test.

---
+++ src/karabo/tests/net/InfluxDbClient_Test.hh
@@ -0,0 +1,34 @@
+/*
+ * InfluxDbClient_Test.hh
+ *
+ * Created on January, 31, 2023.
+ *
+ * Copyright (C) European XFEL GmbH Hamburg. All rights reserved.
+ */
+
+#ifndef INFLUXDBCLIENT_TEST_HH
+#define INFLUXDBCLIENT_TEST_HH
+
+#include <cppunit/extensions/HelperMacros.h>
+
+#include <boost/thread/thread.hpp>
+
+#include "karabo/net/InfluxDbClient.hh"
+
+class InfluxDbClient_Test : public CPPUNIT_NS::TestFixture {
+    CPPUNIT_TEST_SUITE(InfluxDbClient_Test);
+    CPPUNIT_TEST(testShowDatabases);
+    CPPUNIT_TEST_SUITE_END();
+
+   public:
+    void setUp() override;
+    void tearDown() override;
+
+   private:
+    void testShowDatabases();
+
+    karabo::net::InfluxDbClient::Pointer m_influxClient;
+    boost::thread m_eventLoopThread;
+};
+
+#endif
