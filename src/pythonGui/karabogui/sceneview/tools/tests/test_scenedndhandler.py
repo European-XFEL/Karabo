1d7de141ed25589841061193502b7075b024352e
Dennis Goeries <dennis.goeries@xfel.eu>
2023-02-07 17:23:58 +0100

    GUI: Initialize models with defaults from bindings if desired

--- src/pythonGui/karabogui/sceneview/tools/tests/test_scenedndhandler.py
+++ src/pythonGui/karabogui/sceneview/tools/tests/test_scenedndhandler.py
@@ -1,17 +1,22 @@
-from unittest.mock import patch
-
+import pytest
 from qtpy.QtCore import QEvent, QPoint, Qt
 from qtpy.QtGui import QDropEvent
+from qtpy.QtWidgets import QWidget
+from traits.api import Dict, Instance
 
-from karabo.common.scenemodel.api import BoxLayoutModel, SceneModel
+from karabo.common.scenemodel.api import (
+    BaseWidgetObjectData, BoxLayoutModel, SceneModel)
 from karabo.common.states import State
 from karabo.native import AccessMode, Configurable, Double, String
+from karabogui.binding.api import FloatBinding, StringBinding
 from karabogui.configurator.utils import dragged_configurator_items
+from karabogui.controllers.api import (
+    BaseBindingController, register_binding_controller)
 from karabogui.sceneview.tools.scenedndhandler import ConfigurationDropHandler
 from karabogui.sceneview.view import SceneView
-from karabogui.testing import GuiTestCase, get_property_proxy
+from karabogui.testing import flushed_registry, get_property_proxy
 
-GET_PROXY_PATH = 'karabogui.sceneview.tools.scenedndhandler.get_proxy'
+GET_PROXY_PATH = "karabogui.sceneview.tools.scenedndhandler.get_proxy"
 
 
 class TypeObject(Configurable):
@@ -26,59 +31,81 @@ class TypeObject(Configurable):
         allowedStates=[State.ON])
 
 
-class TestSceneDnDHandler(GuiTestCase):
-    def setUp(self):
-        super().setUp()
-        schema = TypeObject.getClassSchema()
-        self.proxy = get_property_proxy(schema, 'prop')
-        self.state_proxy = get_property_proxy(schema, 'state')
-        self.assertIsNotNone(self.proxy.binding)
-        self.assertIsNotNone(self.state_proxy.binding)
-        self.scene_model = SceneModel(children=[])
-        self.scene_view = SceneView(model=self.scene_model)
+class UniqueModel(BaseWidgetObjectData):
+    attributes = Dict
 
-    def tearDown(self):
-        super().tearDown()
-        self.scene_view.destroy()
-        self.scene_view = None
-        self.scene_model = None
 
-    def test_configuration_drop_handler(self):
-        # 1. Reconfigurable item
-        items = dragged_configurator_items([self.proxy])
-        self.assertIsNotNone(items)
+@pytest.fixture(scope="module")
+def display_widget():
+    with flushed_registry():
+        @register_binding_controller(
+            klassname="Editor",
+            binding_type=(StringBinding, FloatBinding))
+        class DisplayWidget(BaseBindingController):
+            model = Instance(UniqueModel)
 
-        event = QDropEvent(QPoint(0, 0), Qt.CopyAction, items,
-                           Qt.LeftButton, Qt.NoModifier, QEvent.Drop)
-        handler = ConfigurationDropHandler()
-        self.assertTrue(handler.can_handle(event))
+            @staticmethod
+            def initialize_model(proxy, model):
+                model.attributes = proxy.binding.attributes
 
-        self.assertEqual(len(self.scene_model.children), 0)
+            def create_widget(self, parent=None):
+                return QWidget(parent)
 
-        def get_proxy(*args, **kwargs):
-            return self.proxy
+        yield DisplayWidget
 
-        with patch(GET_PROXY_PATH, new=get_proxy):
-            handler.handle(self.scene_view, event)
 
-        self.assertEqual(len(self.scene_model.children), 1)
-        self.assertIsInstance(self.scene_model.children[0], BoxLayoutModel)
+def test_scene_dnd_handler(gui_app, display_widget, mocker):
+    """Test the scene drag and drop handler"""
+    schema = TypeObject.getClassSchema()
+    proxy = get_property_proxy(schema, "prop")
+    state_proxy = get_property_proxy(schema, "state")
+    assert proxy.binding is not None
+    assert state_proxy.binding is not None
+    scene_model = SceneModel(children=[])
+    scene_view = SceneView(model=scene_model)
 
-        # 2. Readonly param
-        items = dragged_configurator_items([self.state_proxy])
-        self.assertIsNotNone(items)
+    # 1. Reconfigurable item
+    items = dragged_configurator_items([proxy])
+    assert items is not None
 
-        event = QDropEvent(QPoint(0, 0), Qt.CopyAction, items,
-                           Qt.LeftButton, Qt.NoModifier, QEvent.Drop)
-        self.assertTrue(handler.can_handle(event))
+    event = QDropEvent(QPoint(0, 0), Qt.CopyAction, items,
+                       Qt.LeftButton, Qt.NoModifier, QEvent.Drop)
+    handler = ConfigurationDropHandler()
+    assert handler.can_handle(event)
+    assert len(scene_model.children) == 0
 
-        self.assertEqual(len(self.scene_model.children), 1)
+    def get_proxy(*args, **kwargs):
+        return proxy
 
-        def get_proxy(*args, **kwargs):
-            return self.state_proxy
+    mocker.patch(GET_PROXY_PATH, new=get_proxy)
+    handler.handle(scene_view, event)
 
-        with patch(GET_PROXY_PATH, new=get_proxy):
-            handler.handle(self.scene_view, event)
+    assert len(scene_model.children) == 1
+    assert isinstance(scene_model.children[0], BoxLayoutModel)
+    model = scene_model.children[0].children[1]
+    assert isinstance(model, UniqueModel)
+    assert model.attributes["minInc"] == -1000
+    assert model.attributes["maxInc"] == 1000
 
-        self.assertEqual(len(self.scene_model.children), 2)
-        self.assertIsInstance(self.scene_model.children[1], BoxLayoutModel)
+    # 2. Readonly param
+    items = dragged_configurator_items([state_proxy])
+    assert items is not None
+
+    event = QDropEvent(QPoint(0, 0), Qt.CopyAction, items,
+                       Qt.LeftButton, Qt.NoModifier, QEvent.Drop)
+    assert handler.can_handle(event)
+    assert len(scene_model.children) == 1
+
+    def get_proxy(*args, **kwargs):
+        return state_proxy
+
+    mocker.patch(GET_PROXY_PATH, new=get_proxy)
+    handler.handle(scene_view, event)
+
+    assert len(scene_model.children) == 2
+    assert isinstance(scene_model.children[1], BoxLayoutModel)
+    model = scene_model.children[1].children[1]
+    assert isinstance(model, UniqueModel)
+    assert model.attributes["displayType"] == "State"
+
+    scene_view.destroy()
