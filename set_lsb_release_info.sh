42127f712f80aec4dcd78d9024a3bf2c3ea38e18
Raul Costa <raul.costa@xfel.eu>
2023-02-21 13:22:33 +0100

    Add support to AlmaLinux 9.

---
+++ set_lsb_release_info.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#
+# Sets the environment variables LSB_RELEASE_DIST and
+# LSB_RELEASE_VERSION with the system distribution and
+# version information, respectively.
+# Tries to retrieve to information from "/etc/os-release"
+# and if that is not available, uses "lsb_release" as a
+# fallback. Both Ubuntu and RHEL based distributions like
+# AlmaLinux have "/ect/os-release" with the information
+# needed.
+#
+
+if [ -f "/etc/os-release" ]; then
+    LSB_RELEASE_DIST=$(cat "/etc/os-release" | awk -F= '{if($1 == "NAME") print $2}' | tr -d \" | awk '{print $1}')
+    export LSB_RELEASE_DIST
+    LSB_RELEASE_VERSION=$(cat "/etc/os-release" | awk -F= '{if($1 == "VERSION_ID") print $2}' | tr -d \")
+    export LSB_RELEASE_DIST
+else
+  eval which lsb_release &> /dev/null
+  if [ $? = 0 ]; then
+    # lsb_release is available; use it to set the variables
+    LSB_RELEASE_DIST=$(lsb_release -is)
+    export LSB_RELEASE_DIST
+    LSB_RELEASE_VERSION=$(lsb_release -rs)
+    export LSB_RELEASE_VERSION
+  fi
+fi
