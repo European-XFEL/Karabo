9f7f0e6e02284002efe8b9f9691af44555626741
Noushadali Anakkappalla <noushadali.anakkappalla@xfel.eu>
2023-04-05 13:02:06 +0200

    GUI: Data Analysis Dialog : initial Version

--- src/pythonGui/karabogui/graph/common/fitting.py
+++ src/pythonGui/karabogui/graph/common/fitting.py
@@ -1,5 +1,26 @@
 import numpy as np
+from scipy.stats import norm
 
 
 def gaussian_fit(x, height, x0, sigma, offset):
     return height * np.exp(-0.5 * ((x - x0) / sigma) ** 2) + offset
+
+
+def normal_cdf(x, mu, sigma):
+    """
+    :param mu: Mean of the normal distribution.
+    :type mu: float
+    :param sigma: Standard deviation of the normal distribution.
+    :type sigma: float
+    """
+    return norm.cdf(x, loc=mu, scale=sigma)
+
+
+def linear_function_fit(x, m, b):
+    """
+    :param m: Slope of the linear function
+    :type m: Float
+    :param b: Intercept of the linear function
+    :type b: Float
+    """
+    return m * x + b
