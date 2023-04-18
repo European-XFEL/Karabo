694a0dd0cf21844ec59bcfdfaeba7d9d3944d2c0
Milan Staffehl <milan.staffehl@xfel.eu>
2023-04-18 16:48:13 +0200

    GUI: Transport icon command controller test to pytest

--- src/pythonGui/karabogui/controllers/display/tests/test_icon_command.py
+++ src/pythonGui/karabogui/controllers/display/tests/test_icon_command.py
@@ -1,5 +1,4 @@
-from unittest.mock import Mock, patch
-
+import pytest
 from qtpy.QtCore import Qt
 from qtpy.QtWidgets import QToolButton
 
@@ -7,7 +6,7 @@ from karabo.common.states import State
 from karabo.native import AccessLevel, Configurable, Slot, String
 from karabogui.binding.api import (
     DeviceProxy, PropertyProxy, apply_default_configuration, build_binding)
-from karabogui.testing import GuiTestCase, set_proxy_value, singletons
+from karabogui.testing import set_proxy_value, singletons
 
 
 class EmptyDevice(Configurable):
@@ -18,7 +17,7 @@ class SlottedDevice(Configurable):
     state = String(defaultValue=State.INIT)
 
     @Slot(allowedStates=[State.INIT],
-          displayedName='Call ME',
+          displayedName="Call ME",
           requiredAccessLevel=AccessLevel.OBSERVER)
     def callme(self):
         pass
@@ -34,140 +33,143 @@ def get_slot_proxy(additional=False, klass=SlottedDevice):
     binding = build_binding(schema)
     apply_default_configuration(binding)
 
-    device = DeviceProxy(device_id='dev', server_id='Test', binding=binding)
-    slot_proxy = PropertyProxy(root_proxy=device, path='callme')
+    device = DeviceProxy(device_id="dev", server_id="Test", binding=binding)
+    slot_proxy = PropertyProxy(root_proxy=device, path="callme")
     if additional:
-        return slot_proxy, PropertyProxy(root_proxy=device, path='yep')
+        return slot_proxy, PropertyProxy(root_proxy=device, path="yep")
     else:
         return slot_proxy
 
 
-class TestDisplayIconCommand(GuiTestCase):
+@pytest.fixture
+def icon_command_setup(gui_app):
+    # import required at this point
+    from ..icon_command import DisplayIconCommand
 
-    def test_basics(self):
-        """Test the basics of the display icon command"""
-        from ..icon_command import DisplayIconCommand
-
-        slot_proxy = get_slot_proxy()
-        controller = DisplayIconCommand(proxy=slot_proxy)
-        controller.create(None)
-
-        assert controller.widget is not None
-        assert isinstance(controller._button, QToolButton)
-
-        controller.destroy()
-        assert controller.widget is None
-
-    def test_trigger(self):
-        from ..icon_command import DisplayIconCommand
-
-        slot_proxy = get_slot_proxy()
-        controller = DisplayIconCommand(proxy=slot_proxy)
-        controller.create(None)
-        network = Mock()
-        with singletons(network=network):
-            controller._button.clicked.emit(True)
-            network.onExecute.assert_called_with('dev', 'callme', False)
-
-    def test_state_change(self):
-        from ..icon_command import DisplayIconCommand
-
-        schema = SlottedDevice.getClassSchema()
-        binding = build_binding(schema)
-        apply_default_configuration(binding)
-
-        dev_proxy = DeviceProxy(device_id='dev', server_id='swerver',
-                                binding=binding)
-        slot_proxy = PropertyProxy(root_proxy=dev_proxy, path='yep')
-        controller = DisplayIconCommand(proxy=slot_proxy)
-        controller.create(None)
-
-        # device state is INIT
-        assert not controller._button.isEnabled()
-
-        set_proxy_value(slot_proxy, 'state', State.ACTIVE.value)
-        # now `yep` should be enabled
-        assert controller._button.isEnabled()
-
-    def test_icon_change(self):
-        from ..icon_command import BUTTON_ICONS, DisplayIconCommand
-
-        slot_proxy = get_slot_proxy()
-        controller = DisplayIconCommand(proxy=slot_proxy)
-        controller.create(None)
-
-        actions = controller.widget.actions()
-        assert len(actions) == 1
-
-        exec_path = ("karabogui.controllers.display.icon_command."
-                     "IconSelectionDialog.exec")
-        new_icon = 'Left'
-
-        def exec_dialog(self):
-            self.icon = BUTTON_ICONS[new_icon]
-            self.name = new_icon
-
-        mocked_exec = patch(exec_path, exec_dialog)
-        with mocked_exec:
-            actions[0].trigger()
-            assert controller._icon == BUTTON_ICONS[new_icon]
-            assert controller.model.icon_name == new_icon
+    # setup
+    slot_proxy = get_slot_proxy()
+    controller = DisplayIconCommand(proxy=slot_proxy)
+    controller.create(None)
+    yield controller
+    # teardown
+    controller.destroy()
+    assert controller.widget is None
 
 
-class TestIconSelectionDialog(GuiTestCase):
+@pytest.fixture
+def icon_command_dialog_setup(gui_app):
+    from ..icon_command import IconSelectionDialog
+    dialog = IconSelectionDialog()
+    yield dialog
+    dialog.destroy()
 
-    def setUp(self):
-        from ..icon_command import IconSelectionDialog
 
-        super(TestIconSelectionDialog, self).setUp()
-        self.dialog = IconSelectionDialog()
+def test_icon_command_basics(icon_command_setup):
+    """Test the basics of the display icon command"""
+    controller = icon_command_setup
+    assert controller.widget is not None
+    assert isinstance(controller._button, QToolButton)
 
-    def tearDown(self):
-        super(TestIconSelectionDialog, self).tearDown()
-        self.dialog.destroy()
 
-    def test_init(self):
-        from ..icon_command import BUTTON_ICONS, NO_SELECTION
+def test_icon_command_trigger(icon_command_setup, mocker):
+    controller = icon_command_setup
+    network = mocker.Mock()
+    with singletons(network=network):
+        controller._button.clicked.emit(True)
+        network.onExecute.assert_called_with("dev", "callme", False)
 
-        assert self.dialog.icon == NO_SELECTION
-        assert self.dialog.name == ''
 
-        model = self.dialog.list_view.model()
-        for row in range(model.rowCount()):
-            index = model.index(row, 0)
-            name = index.data(Qt.DisplayRole)
+def test_icon_command_state_change(gui_app):
+    from ..icon_command import DisplayIconCommand
+    schema = SlottedDevice.getClassSchema()
+    binding = build_binding(schema)
+    apply_default_configuration(binding)
 
-            assert name in BUTTON_ICONS
+    dev_proxy = DeviceProxy(device_id="dev", server_id="swerver",
+                            binding=binding)
+    slot_proxy = PropertyProxy(root_proxy=dev_proxy, path="yep")
+    controller = DisplayIconCommand(proxy=slot_proxy)
+    controller.create(None)
 
-    def test_valid_double_click(self):
-        from ..icon_command import BUTTON_ICONS
-        new_icon = 'Left'
+    # device state is INIT
+    assert not controller._button.isEnabled()
 
-        def mocked_returns(role):
-            if role == Qt.DisplayRole:
-                return new_icon
-            if role == Qt.UserRole + 1:
-                return BUTTON_ICONS[new_icon]
+    set_proxy_value(slot_proxy, "state", State.ACTIVE.value)
+    # now `yep` should be enabled
+    assert controller._button.isEnabled()
 
-        mocked_index = Mock()
-        mocked_index.data.side_effect = mocked_returns
-        self.dialog.handleDoubleClick(mocked_index)
 
-        assert self.dialog.icon == BUTTON_ICONS[new_icon]
-        assert self.dialog.name == new_icon
+def test_icon_command_icon_change(icon_command_setup, mocker):
+    from ..icon_command import BUTTON_ICONS
+    controller = icon_command_setup
 
-    def test_invalid_double_click(self):
-        from ..icon_command import NO_SELECTION
+    actions = controller.widget.actions()
+    assert len(actions) == 1
 
-        def mocked_returns(role):
-            if role == Qt.DisplayRole:
-                return None
-            if role == Qt.UserRole + 1:
-                return None
+    exec_path = ("karabogui.controllers.display.icon_command."
+                 "IconSelectionDialog.exec")
+    new_icon = "Left"
 
-        mocked_index = Mock()
-        mocked_index.data.side_effect = mocked_returns
-        self.dialog.handleDoubleClick(mocked_index)
+    def exec_dialog(self):
+        self.icon = BUTTON_ICONS[new_icon]
+        self.name = new_icon
 
-        assert self.dialog.icon == NO_SELECTION
-        assert self.dialog.name == ''
+    mocker.patch(exec_path, exec_dialog)
+    actions[0].trigger()
+    assert controller._icon == BUTTON_ICONS[new_icon]
+    assert controller.model.icon_name == new_icon
+
+
+def test_dialog_init(icon_command_dialog_setup):
+    from ..icon_command import BUTTON_ICONS, NO_SELECTION
+
+    dialog = icon_command_dialog_setup
+
+    assert dialog.icon == NO_SELECTION
+    assert dialog.name == ""
+
+    model = dialog.list_view.model()
+    for row in range(model.rowCount()):
+        index = model.index(row, 0)
+        name = index.data(Qt.DisplayRole)
+
+        assert name in BUTTON_ICONS
+
+
+def test_dialog_valid_double_click(icon_command_dialog_setup, mocker):
+    from ..icon_command import BUTTON_ICONS
+    new_icon = "Left"
+
+    def mocked_returns(role):
+        if role == Qt.DisplayRole:
+            return new_icon
+        if role == Qt.UserRole + 1:
+            return BUTTON_ICONS[new_icon]
+
+    dialog = icon_command_dialog_setup
+
+    mocked_index = mocker.Mock()
+    mocked_index.data.side_effect = mocked_returns
+    dialog.handleDoubleClick(mocked_index)
+
+    assert dialog.icon == BUTTON_ICONS[new_icon]
+    assert dialog.name == new_icon
+
+
+def test_dialog_invalid_double_click(icon_command_dialog_setup, mocker):
+    from ..icon_command import NO_SELECTION
+
+    def mocked_returns(role):
+        if role == Qt.DisplayRole:
+            return None
+        if role == Qt.UserRole + 1:
+            return None
+
+    dialog = icon_command_dialog_setup
+
+    mocked_index = mocker.Mock()
+    mocked_index.data.side_effect = mocked_returns
+    dialog.handleDoubleClick(mocked_index)
+
+    assert dialog.icon == NO_SELECTION
+    assert dialog.name == ""
