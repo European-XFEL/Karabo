87d2166ced62459dc7f6fb13a3ea58e9434a3fc8
Dennis Goeries <dennis.goeries@xfel.eu>
2023-04-21 12:10:54 +0200

    MDL Test: Add missing timeout protections in output injection test

--- src/pythonKarabo/karabo/middlelayer_api/tests/remote_pipeline_test.py
+++ src/pythonKarabo/karabo/middlelayer_api/tests/remote_pipeline_test.py
@@ -814,14 +814,15 @@ async def test_injected_output_channel_connection(event_loop):
             sender_proxy.output.connect()
 
             # Receiver device
-            await sleepUntil(lambda: connected is True)
+            await sleepUntil(lambda: connected is True, timeout=5)
             assert receiver.connected
             await sender_proxy.sendData()
             await sleepUntil(lambda: receiver.received > 0, timeout=5)
             assert receiver.received > 0
             await sleepUntil(lambda: received is True, timeout=5)
             assert received
-            await waitUntil(lambda: input_proxy.received > 0)
+            await waitUntil(lambda: input_proxy.received > 0,
+                            timeout=5)
             assert input_proxy.received > 0
 
             # Kill the device and bring up again
