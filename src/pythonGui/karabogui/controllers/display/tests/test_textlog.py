a6e4926a952e49b695d7407c9ee14ef234243e3e
Milan Staffehl <milan.staffehl@xfel.eu>
2023-04-24 11:34:35 +0200

    GUI: Transport textlog controller test to pytest

--- src/pythonGui/karabogui/controllers/display/tests/test_textlog.py
+++ src/pythonGui/karabogui/controllers/display/tests/test_textlog.py
@@ -1,10 +1,10 @@
 from platform import system
-from unittest import skipIf
+
+import pytest
 
 from karabo.common.scenemodel.api import DisplayTextLogModel
 from karabo.native import Configurable, String
-from karabogui.testing import (
-    GuiTestCase, get_class_property_proxy, set_proxy_value)
+from karabogui.testing import get_class_property_proxy, set_proxy_value
 
 from ..textlog import DisplayTextLog
 
@@ -13,24 +13,19 @@ class Object(Configurable):
     prop = String()
 
 
-class TestDisplayTextLog(GuiTestCase):
-    def setUp(self):
-        super(TestDisplayTextLog, self).setUp()
-        schema = Object.getClassSchema()
-        self.proxy = get_class_property_proxy(schema, 'prop')
-        self.controller = DisplayTextLog(proxy=self.proxy,
-                                         model=DisplayTextLogModel())
-        self.controller.create(None)
-        assert self.controller.widget is not None
-
-    def tearDown(self):
-        super(TestDisplayTextLog, self).tearDown()
-        self.controller.destroy()
-        self.assertIsNone(self.controller.widget)
-
-    @skipIf(system() == "Windows",
-            reason='toPlainText returns an empty string in windows')
-    def test_set_value(self):
-        self.controller.log_widget.clear()
-        set_proxy_value(self.proxy, 'prop', 'Line 1')
-        self.assertIn('Line 1', self.controller.log_widget.toPlainText())
+@pytest.mark.skipif(system() == "Windows",
+                    reason="toPlainText returns an empty string in windows")
+def test_textlog_controller(gui_app):
+    # setup
+    schema = Object.getClassSchema()
+    proxy = get_class_property_proxy(schema, "prop")
+    controller = DisplayTextLog(proxy=proxy, model=DisplayTextLogModel())
+    controller.create(None)
+    assert controller.widget is not None
+    # set value
+    controller.log_widget.clear()
+    set_proxy_value(proxy, "prop", "Line 1")
+    assert "Line 1" in controller.log_widget.toPlainText()
+    # teardown
+    controller.destroy()
+    assert controller.widget is None
