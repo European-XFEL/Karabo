2d712b5c7844ff5f34c899d2e03ad18be3afce8c
Milan Staffehl <milan.staffehl@xfel.eu>
2023-04-24 13:00:06 +0200

    GUI: Transport sparkline controller test to pytest

--- src/pythonGui/karabogui/controllers/display/tests/test_sparkline.py
+++ src/pythonGui/karabogui/controllers/display/tests/test_sparkline.py
@@ -1,15 +1,15 @@
-from unittest.mock import Mock, patch
+import pytest
 
 from karabo.common.scenemodel.api import SparklineModel
 from karabo.native import Configurable, Float
 from karabogui.testing import (
-    GuiTestCase, get_class_property_proxy, get_property_proxy, set_proxy_value,
-    singletons)
+    get_class_property_proxy, get_property_proxy, set_proxy_value, singletons)
+from karabogui.util import process_qt_events
 
 from ..sparkline import DisplaySparkline
 from .data import build_historic_data_float
 
-TIMEBASES = {'60s': 60, '10m': 600, '10h': 36000}
+TIMEBASES = {"60s": 60, "10m": 600, "10h": 36000}
 
 
 class Object(Configurable):
@@ -17,76 +17,76 @@ class Object(Configurable):
                  alarmHigh=10.0)
 
 
-class TestDisplaySparkline(GuiTestCase):
-    def setUp(self):
-        super(TestDisplaySparkline, self).setUp()
-        self.schema = Object.getClassSchema()
+@pytest.fixture
+def sparkline_setup(gui_app):
+    schema = Object.getClassSchema()
+    proxy = get_class_property_proxy(schema, "prop")
+    model = SparklineModel()
+    controller = DisplaySparkline(proxy=proxy, model=model)
+    controller.create(None)
+    assert controller.widget is not None
 
-    def test_basics(self):
-        prop = get_class_property_proxy(self.schema, 'prop')
+    yield controller, proxy
 
-        model = SparklineModel()
-        controller = DisplaySparkline(proxy=prop, model=model)
-        controller.create(None)
-        assert controller.widget is not None
-        # Make sure there is a value with timestamp on it
-        set_proxy_value(prop, 'prop', 12.0)
+    controller.destroy()
+    assert controller.widget is None
 
-        with patch.object(controller.line_edit, 'setVisible'):
-            sym = 'karabogui.controllers.display.sparkline.QInputDialog'
-            with patch(sym) as QInputDialog:
-                for ac in controller.widget.actions():
-                    ac_name = ac.text()
-                    if ac_name == 'Set value format':
-                        QInputDialog.getText.return_value = '.3e', True
-                        ac.trigger()
-                        assert controller.model.show_format == '.3e'
-                    elif ac_name == 'Show value':
-                        assert controller.model.show_value == ac.isChecked()
-                        ac.setChecked(not ac.isChecked())
-                        assert controller.model.show_value == ac.isChecked()
-                    elif ac_name == 'Use alarm range':
-                        assert controller.model.alarm_range == ac.isChecked()
-                        ac.setChecked(not ac.isChecked())
-                        assert controller.model.alarm_range == ac.isChecked()
-                    else:
-                        tb_val = TIMEBASES.get(ac_name[:3], None)
-                        if tb_val is None:
-                            continue
-                        ac.trigger()
-                        assert tb_val == controller.model.time_base
 
-        controller.destroy()
-        assert controller.widget is None
+def test_basics(sparkline_setup, mocker):
+    controller, proxy = sparkline_setup
 
-    def test_device(self):
-        network = Mock()
-        with singletons(network=network):
-            schema = Object.getClassSchema()
-            val_proxy = get_property_proxy(schema, 'prop')
+    # Make sure there is a value with timestamp on it
+    set_proxy_value(proxy, "prop", 12.0)
 
-            model = SparklineModel()
-            controller = DisplaySparkline(proxy=val_proxy, model=model)
-            controller.create(None)
+    mocker.patch.object(controller.line_edit, "setVisible")
+    sym = "karabogui.controllers.display.sparkline.QInputDialog"
+    QInputDialog = mocker.patch(sym)
+    for ac in controller.widget.actions():
+        ac_name = ac.text()
+        if ac_name == "Set value format":
+            QInputDialog.getText.return_value = ".3e", True
+            ac.trigger()
+            assert controller.model.show_format == ".3e"
+        elif ac_name == "Show value":
+            assert controller.model.show_value == ac.isChecked()
+            ac.setChecked(not ac.isChecked())
+            assert controller.model.show_value == ac.isChecked()
+        elif ac_name == "Use alarm range":
+            assert controller.model.alarm_range == ac.isChecked()
+            ac.setChecked(not ac.isChecked())
+            assert controller.model.alarm_range == ac.isChecked()
+        else:
+            tb_val = TIMEBASES.get(ac_name[:3], None)
+            if tb_val is None:
+                continue
+            ac.trigger()
+            assert tb_val == controller.model.time_base
 
-            val_proxy.start_monitoring()
-            # can't guess the time argument because it's now
-            network.onGetPropertyHistory.assert_called()
 
-    def test_historic_data(self):
-        prop = get_class_property_proxy(self.schema, 'prop')
+def test_device(gui_app, mocker):
+    network = mocker.Mock()
+    with singletons(network=network):
+        schema = Object.getClassSchema()
+        proxy = get_property_proxy(schema, "prop")
 
         model = SparklineModel()
-        controller = DisplaySparkline(proxy=prop, model=model)
+        controller = DisplaySparkline(proxy=proxy, model=model)
         controller.create(None)
 
-        set_proxy_value(prop, 'prop', 42.0)
+        proxy.start_monitoring()
+        # can't guess the time argument because it's now
+        network.onGetPropertyHistory.assert_called()
 
-        historic_data = build_historic_data_float()
-        prop.binding.historic_data = historic_data
-        # mock the QPainter, so the code path can be walked deeper down
-        # without the need of creating a backend draw device
-        sym = 'karabogui.controllers.display.sparkline.QPainter'
-        with patch(sym) as QPainter:  # noqa
-            controller.render_area.paintEvent(None)
-        self.process_qt_events()
+
+def test_historic_data(sparkline_setup, mocker):
+    controller, proxy = sparkline_setup
+
+    set_proxy_value(proxy, "prop", 42.0)
+
+    historic_data = build_historic_data_float()
+    proxy.binding.historic_data = historic_data
+    # mock the QPainter, so the code path can be walked deeper down
+    # without the need of creating a backend draw device
+    mocker.patch("karabogui.controllers.display.sparkline.QPainter")
+    controller.render_area.paintEvent(None)
+    process_qt_events()
