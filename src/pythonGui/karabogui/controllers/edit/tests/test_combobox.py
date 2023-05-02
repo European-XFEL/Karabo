9177251ff68d8752fbca5fa740483df46499974e
Milan Staffehl <milan.staffehl@xfel.eu>
2023-05-02 12:15:11 +0200

    GUI: Transport combobox controller test to pytest

--- src/pythonGui/karabogui/controllers/edit/tests/test_combobox.py
+++ src/pythonGui/karabogui/controllers/edit/tests/test_combobox.py
@@ -1,19 +1,19 @@
 # Copyright (C) European XFEL GmbH Schenefeld. All rights reserved.
+import pytest
 from qtpy.QtCore import Qt
 
 from karabo.common.scenemodel.api import EditableComboBoxModel
 from karabo.common.states import State
 from karabo.native import Configurable, Int32, String
 from karabogui.binding.api import build_binding
-from karabogui.testing import (
-    GuiTestCase, get_class_property_proxy, set_proxy_value)
+from karabogui.testing import get_class_property_proxy, set_proxy_value
 
 from ..combobox import EditableComboBox
 
 
 class Object(Configurable):
     state = String(defaultValue=State.ON)
-    prop = String(options=['foo', 'bar', 'baz', 'qux'],
+    prop = String(options=["foo", "bar", "baz", "qux"],
                   allowedStates=[State.ON])
 
 
@@ -21,53 +21,64 @@ class Other(Configurable):
     prop = Int32(options=[1, 2, 3, 5, 8])
 
 
-class TestEditableComboBox(GuiTestCase):
-    def setUp(self):
-        super(TestEditableComboBox, self).setUp()
-        self.proxy = get_class_property_proxy(Object.getClassSchema(), 'prop')
-        self.controller = EditableComboBox(proxy=self.proxy,
-                                           model=EditableComboBoxModel())
-        self.controller.create(None)
+@pytest.fixture
+def combobox_setup(gui_app):
+    # setup
+    proxy = get_class_property_proxy(Object.getClassSchema(), "prop")
+    controller = EditableComboBox(proxy=proxy, model=EditableComboBoxModel())
+    controller.create(None)
+    assert controller.widget is not None
+    yield controller, proxy
+    # teardown
+    controller.destroy()
+    assert controller.widget is None
 
-    def tearDown(self):
-        self.controller.destroy()
-        assert self.controller.widget is None
 
-    def test_allowed(self):
-        set_proxy_value(self.proxy, 'state', 'CHANGING')
-        assert self.controller.widget.isEnabled() is False
-        set_proxy_value(self.proxy, 'state', 'ON')
-        assert self.controller.widget.isEnabled() is True
+def test_combobox_allowed(combobox_setup):
+    controller, proxy = combobox_setup
+    set_proxy_value(proxy, "state", "CHANGING")
+    assert controller.widget.isEnabled() is False
+    set_proxy_value(proxy, "state", "ON")
+    assert controller.widget.isEnabled() is True
 
-    def test_set_value(self):
-        set_proxy_value(self.proxy, 'prop', 'bar')
-        assert self.controller.widget.currentIndex() == 1
 
-        set_proxy_value(self.proxy, 'prop', 'nobar')
-        assert self.controller.widget.currentIndex() == -1
+def test_combobox_set_value(combobox_setup):
+    controller, proxy = combobox_setup
+    set_proxy_value(proxy, "prop", "bar")
+    assert controller.widget.currentIndex() == 1
 
-        set_proxy_value(self.proxy, 'prop', 'baz')
-        assert self.controller.widget.currentIndex() == 2
+    set_proxy_value(proxy, "prop", "nobar")
+    assert controller.widget.currentIndex() == -1
 
-    def test_edit_value(self):
-        self.controller.widget.setCurrentIndex(3)
-        assert self.proxy.edit_value == 'qux'
+    set_proxy_value(proxy, "prop", "baz")
+    assert controller.widget.currentIndex() == 2
 
-    def test_schema_update(self):
-        proxy = get_class_property_proxy(Other.getClassSchema(), 'prop')
-        controller = EditableComboBox(proxy=proxy)
-        controller.create(None)
-        assert controller.widget.currentIndex() == -1
-        set_proxy_value(proxy, 'prop', 2)
-        assert controller.widget.currentIndex() == 1
 
-        assert controller.widget.count() == 5
+def test_combobox_edit_value(combobox_setup):
+    controller, proxy = combobox_setup
+    controller.widget.setCurrentIndex(3)
+    assert proxy.edit_value == "qux"
 
-        build_binding(Object.getClassSchema(),
-                      existing=proxy.root_proxy.binding)
-        assert controller.widget.currentIndex() == -1
-        assert controller.widget.count() == 4
 
-    def test_focus(self):
-        combobox = self.controller.widget
-        assert combobox.focusPolicy() == Qt.StrongFocus
+def test_combobox_schema_update(gui_app):
+    proxy = get_class_property_proxy(Other.getClassSchema(), "prop")
+    controller = EditableComboBox(proxy=proxy)
+    controller.create(None)
+    assert controller.widget.currentIndex() == -1
+    set_proxy_value(proxy, "prop", 2)
+    assert controller.widget.currentIndex() == 1
+
+    assert controller.widget.count() == 5
+
+    build_binding(Object.getClassSchema(),
+                  existing=proxy.root_proxy.binding)
+    assert controller.widget.currentIndex() == -1
+    assert controller.widget.count() == 4
+
+    controller.destroy()
+
+
+def test_combobox_focus(combobox_setup):
+    controller, _ = combobox_setup
+    combobox = controller.widget
+    assert combobox.focusPolicy() == Qt.StrongFocus
