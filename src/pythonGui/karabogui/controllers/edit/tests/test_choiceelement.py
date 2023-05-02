5c07ff5538e5102d81baf542d8579c3ed53492f6
Milan Staffehl <milan.staffehl@xfel.eu>
2023-05-02 10:47:23 +0200

    GUI: Transport choiceelement controller test to pytest

--- src/pythonGui/karabogui/controllers/edit/tests/test_choiceelement.py
+++ src/pythonGui/karabogui/controllers/edit/tests/test_choiceelement.py
@@ -1,9 +1,10 @@
 # Copyright (C) European XFEL GmbH Schenefeld. All rights reserved.
+import pytest
 from qtpy.QtCore import Qt
 
 from karabo.native import Bool, ChoiceOfNodes, Configurable
 from karabogui.binding.api import apply_default_configuration
-from karabogui.testing import GuiTestCase, get_class_property_proxy
+from karabogui.testing import get_class_property_proxy
 
 from ..choiceelement import EditableChoiceElement
 
@@ -21,38 +22,39 @@ class ChoiceTwo(ChoicesBase):
 
 
 class Object(Configurable):
-    prop = ChoiceOfNodes(ChoicesBase, defaultValue='ChoiceOne')
+    prop = ChoiceOfNodes(ChoicesBase, defaultValue="ChoiceOne")
 
 
-class TestEditableChoiceElement(GuiTestCase):
-    def setUp(self):
-        super(TestEditableChoiceElement, self).setUp()
+@pytest.fixture
+def choiceelement_setup(gui_app):
+    # setup
+    schema = Object.getClassSchema()
+    proxy = get_class_property_proxy(schema, "prop")
+    controller = EditableChoiceElement(proxy=proxy)
+    controller.create(None)
+    assert controller.widget is not None
+    yield controller, proxy
+    # teardown
+    controller.destroy()
+    assert controller.widget is None
 
-        schema = Object.getClassSchema()
-        self.proxy = get_class_property_proxy(schema, 'prop')
-        self.controller = EditableChoiceElement(proxy=self.proxy)
-        self.controller.create(None)
 
-    def tearDown(self):
-        self.controller.destroy()
-        assert self.controller.widget is None
+def test_choiceelement_basics(choiceelement_setup):
+    # focus policy
+    controller, proxy = choiceelement_setup
+    assert controller.widget.focusPolicy() == Qt.StrongFocus
 
-    def test_focus_policy(self):
-        assert self.controller.widget.focusPolicy() == Qt.StrongFocus
+    # test set up
+    assert controller.widget.count() == 2
 
-    def test_setup(self):
-        assert self.controller.widget.count() == 2
+    # set value
+    apply_default_configuration(proxy.root_proxy.binding)
+    assert controller.widget.currentText() == "ChoiceOne"
+    proxy.value = "ChoiceTwo"
+    assert controller.widget.currentText() == "ChoiceTwo"
 
-    def test_set_value(self):
-        apply_default_configuration(self.proxy.root_proxy.binding)
-        assert self.controller.widget.currentText() == 'ChoiceOne'
-
-        self.proxy.value = 'ChoiceTwo'
-        assert self.controller.widget.currentText() == 'ChoiceTwo'
-
-    def test_edit_value(self):
-        widget = self.controller.widget
-        index = widget.findText('ChoiceTwo')
-
-        widget.setCurrentIndex(index)
-        assert self.proxy.binding.choice == 'ChoiceTwo'
+    # edit value
+    widget = controller.widget
+    index = widget.findText("ChoiceTwo")
+    widget.setCurrentIndex(index)
+    assert proxy.binding.choice == "ChoiceTwo"
