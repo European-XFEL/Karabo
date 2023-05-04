b27114cc11319ce31296861fc5693e8495d310f6
Milan Staffehl <milan.staffehl@xfel.eu>
2023-05-04 15:21:53 +0200

    GUI: Transport strlist controller test to pytest

--- src/pythonGui/karabogui/controllers/edit/tests/test_strlist.py
+++ src/pythonGui/karabogui/controllers/edit/tests/test_strlist.py
@@ -1,6 +1,4 @@
 # Copyright (C) European XFEL GmbH Schenefeld. All rights reserved.
-from unittest.mock import patch
-
 from qtpy.QtCore import Qt
 from qtpy.QtWidgets import QDialog
 
@@ -8,44 +6,43 @@ from karabo.native import Configurable, VectorString
 from karabogui.binding.api import apply_default_configuration
 from karabogui.const import IS_MAC_SYSTEM
 from karabogui.dialogs.listedit import ListEditDialog
-from karabogui.testing import GuiTestCase, get_class_property_proxy
+from karabogui.testing import get_class_property_proxy
 
 from ..strlist import EditableListElement
 
 
 class Object(Configurable):
-    prop = VectorString(defaultValue=['hi there'])
+    prop = VectorString(defaultValue=["hi there"])
 
 
 class ListEditMock(ListEditDialog):
     @property
     def values(self):
-        return ['foo', 'bar']
+        return ["foo", "bar"]
 
     def exec(self):
         return QDialog.Accepted
 
 
-class TestEditableListElement(GuiTestCase):
-    def setUp(self):
-        super(TestEditableListElement, self).setUp()
-        self.proxy = get_class_property_proxy(Object.getClassSchema(), 'prop')
-        self.controller = EditableListElement(proxy=self.proxy)
-        self.controller.create(None)
-        apply_default_configuration(self.proxy.root_proxy.binding)
+def test_strlist_basics(gui_app, mocker):
+    # setup
+    proxy = get_class_property_proxy(Object.getClassSchema(), "prop")
+    controller = EditableListElement(proxy=proxy)
+    controller.create(None)
+    apply_default_configuration(proxy.root_proxy.binding)\
 
-    def tearDown(self):
-        self.controller.destroy()
-        assert self.controller.widget is None
+    # focus policy
+    if IS_MAC_SYSTEM:
+        assert controller.widget.focusPolicy() == Qt.TabFocus
+    else:
+        assert controller.widget.focusPolicy() == Qt.StrongFocus
 
-    def test_focus_policy(self):
-        if IS_MAC_SYSTEM:
-            assert self.controller.widget.focusPolicy() == Qt.TabFocus
-        else:
-            assert self.controller.widget.focusPolicy() == Qt.StrongFocus
+    # edit dialog
+    target = "karabogui.controllers.edit.strlist.ListEditDialog"
+    mocker.patch(target, new=ListEditMock)
+    controller._on_edit_clicked()
+    assert proxy.edit_value == ["foo", "bar"]
 
-    def test_edit_dialog(self):
-        target = 'karabogui.controllers.edit.strlist.ListEditDialog'
-        with patch(target, new=ListEditMock):
-            self.controller._on_edit_clicked()
-            assert self.proxy.edit_value == ['foo', 'bar']
+    # teardown
+    controller.destroy()
+    assert controller.widget is None
