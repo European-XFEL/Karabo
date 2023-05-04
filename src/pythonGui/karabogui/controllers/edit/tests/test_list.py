48e77f1971ff960601993efb7c10ac897cd9aecf
Milan Staffehl <milan.staffehl@xfel.eu>
2023-05-04 14:26:14 +0200

    GUI: Transport list controller test to pytest

--- src/pythonGui/karabogui/controllers/edit/tests/test_list.py
+++ src/pythonGui/karabogui/controllers/edit/tests/test_list.py
@@ -1,6 +1,5 @@
 # Copyright (C) European XFEL GmbH Schenefeld. All rights reserved.
-from unittest.mock import patch
-
+import pytest
 from qtpy.QtCore import Qt
 from qtpy.QtWidgets import QDialog
 
@@ -10,8 +9,7 @@ from karabo.native import (
     Configurable, VectorInt8, VectorInt32, VectorRegexString)
 from karabogui.binding.api import apply_default_configuration, get_min_max_size
 from karabogui.dialogs.listedit import ListEditDialog
-from karabogui.testing import (
-    GuiTestCase, get_class_property_proxy, set_proxy_value)
+from karabogui.testing import get_class_property_proxy, set_proxy_value
 
 from ..list import EditableList, EditableRegexList
 
@@ -37,133 +35,160 @@ class RegexObject(Configurable):
 class ListEditMock(ListEditDialog):
     @property
     def values(self):
-        return ['-1', '42', '-1']
+        return ["-1", "42", "-1"]
 
     def exec(self):
         return QDialog.Accepted
 
 
-class TestEditableList(GuiTestCase):
-    def setUp(self):
-        super(TestEditableList, self).setUp()
-        self.proxy = get_class_property_proxy(Object.getClassSchema(), 'prop')
-        self.controller = EditableList(proxy=self.proxy,
-                                       model=EditableListModel())
-        self.controller.create(None)
-        apply_default_configuration(self.proxy.root_proxy.binding)
-
-        self.size_proxy = get_class_property_proxy(SizeObject.getClassSchema(),
-                                                   'prop')
-        self.size_controller = EditableList(proxy=self.size_proxy,
-                                            model=EditableListModel())
-        self.size_controller.create(None)
-        apply_default_configuration(self.size_proxy.root_proxy.binding)
-
-        self.short_proxy = get_class_property_proxy(
-            ShortObject.getClassSchema(), 'prop')
-        self.short_controller = EditableList(proxy=self.short_proxy,
-                                             model=EditableListModel())
-        self.short_controller.create(None)
-        apply_default_configuration(self.short_proxy.root_proxy.binding)
-
-    def tearDown(self):
-        self.controller.destroy()
-        assert self.controller.widget is None
-
-    def test_focus_policy(self):
-        assert self.controller._internal_widget.focusPolicy() == Qt.StrongFocus
-
-    def test_set_value(self):
-        self.controller.last_cursor_position = 0
-        set_proxy_value(self.proxy, 'prop', [0, 2])
-        assert self.controller._internal_widget.text() == '0,2'
-        assert self.controller._internal_widget.cursorPosition() == 0
-
-    def test_edit_value(self):
-        self.controller.set_read_only(False)
-        self.controller._internal_widget.setText('3,4')
-        assert all(self.proxy.edit_value == [3, 4])
-
-    def test_edit_empty_value(self):
-        self.controller._internal_widget.setText('NOT IMPORTANT')
-        self.controller.set_read_only(False)
-        self.controller._internal_widget.setText('')
-        value = self.proxy.edit_value
-        assert len(value) == 0 and all(value == [])
-
-    def test_edit_dialog(self):
-        target = 'karabogui.controllers.edit.list.ListEditDialog'
-        with patch(target, new=ListEditMock):
-            self.controller.set_read_only(False)
-            self.controller._on_edit_clicked()
-            assert all(self.proxy.edit_value == [-1, 42, -1])
-
-    def test_size_list(self):
-        assert get_min_max_size(self.size_proxy.binding) == (1, 3)
-        self.size_controller.set_read_only(False)
-        self.size_controller._internal_widget.setText('3,4')
-        assert all(self.size_proxy.edit_value == [3, 4])
-        self.size_controller._internal_widget.setText('3,4,5,6')
-        assert self.size_proxy.edit_value is None
-        self.size_controller._internal_widget.setText('')
-        assert self.size_proxy.edit_value is None
-
-    def test_trailing_zeros(self):
-        assert get_min_max_size(self.size_proxy.binding) == (1, 3)
-        self.size_controller.set_read_only(False)
-        self.size_controller._internal_widget.setText('03,4')
-        assert self.size_proxy.edit_value is None
-        self.size_controller._internal_widget.setText('3,4')
-        assert all(self.size_proxy.edit_value == [3, 4])
-        self.size_controller._internal_widget.setText('000000,4')
-        assert all(self.size_proxy.edit_value == [0, 4])
-
-    def test_shorts(self):
-        self.short_controller.set_read_only(False)
-        self.short_controller._internal_widget.setText('03,4')
-        assert self.short_proxy.edit_value is None
-        self.short_controller._internal_widget.setText('3,4')
-        assert all(self.short_proxy.edit_value == [3, 4])
-        # out of range
-        self.short_controller._internal_widget.setText('10300')
-        assert self.short_proxy.edit_value is None
+@pytest.fixture
+def list_setup(gui_app):
+    proxy = get_class_property_proxy(Object.getClassSchema(), "prop")
+    controller = EditableList(proxy=proxy, model=EditableListModel())
+    controller.create(None)
+    apply_default_configuration(proxy.root_proxy.binding)
+    yield controller, proxy
+    # teardown
+    controller.destroy()
+    assert controller.widget is None
 
 
-class TestEditableRegexList(GuiTestCase):
-    def setUp(self):
-        super(TestEditableRegexList, self).setUp()
-        self.proxy = get_class_property_proxy(
-            RegexObject.getClassSchema(), 'prop')
-        self.controller = EditableRegexList(
-            proxy=self.proxy, model=EditableRegexListModel())
-        self.controller.create(None)
-        self.controller.binding_update(self.proxy)
-        apply_default_configuration(self.proxy.root_proxy.binding)
+def test_list_focus_policy(list_setup):
+    controller, _ = list_setup
+    assert controller._internal_widget.focusPolicy() == Qt.StrongFocus
 
-    def tearDown(self):
-        self.controller.destroy()
-        assert self.controller.widget is None
 
-    def test_binding_update_validator(self):
-        assert self.controller._internal_widget.validator() is not None
+def test_list_set_value(list_setup):
+    controller, proxy = list_setup
+    controller.last_cursor_position = 0
+    set_proxy_value(proxy, "prop", [0, 2])
+    assert controller._internal_widget.text() == "0,2"
+    assert controller._internal_widget.cursorPosition() == 0
 
-    def test_set_value(self):
-        self.controller.last_cursor_position = 0
-        set_proxy_value(self.proxy, 'prop', ["remote:outputDaq"])
-        assert self.controller._internal_widget.text() == "remote:outputDaq"
-        assert self.controller._internal_widget.cursorPosition() == 0
 
-    def test_edit_value(self):
-        self.controller.set_read_only(False)
-        self.controller._internal_widget.setText(
-            "remote:outputDaq,remote:output")
-        self.assertEqual(self.proxy.edit_value,
-                         ["remote:outputDaq", "remote:output"])
+def test_list_edit_value(list_setup):
+    controller, proxy = list_setup
+    controller.set_read_only(False)
+    controller._internal_widget.setText("3,4")
+    assert all(proxy.edit_value == [3, 4])
 
-    def test_edit_empty_value(self):
-        self.controller.set_read_only(False)
-        self.controller._internal_widget.setText('remote:output')
-        self.assertEqual(self.proxy.edit_value, ["remote:output"])
-        self.controller._internal_widget.setText('')
-        value = self.proxy.edit_value
-        self.assertEqual(value, [])
+
+def test_list_edit_empty_value(list_setup):
+    controller, proxy = list_setup
+    controller._internal_widget.setText("NOT IMPORTANT")
+    controller.set_read_only(False)
+    controller._internal_widget.setText("")
+    value = proxy.edit_value
+    assert len(value) == 0 and all(value == [])
+
+
+def test_list_edit_dialog(list_setup, mocker):
+    controller, proxy = list_setup
+    target = "karabogui.controllers.edit.list.ListEditDialog"
+    mocker.patch(target, new=ListEditMock)
+    controller.set_read_only(False)
+    controller._on_edit_clicked()
+    assert all(proxy.edit_value == [-1, 42, -1])
+
+
+@pytest.fixture
+def list_size_setup(gui_app):
+    size_proxy = get_class_property_proxy(SizeObject.getClassSchema(), "prop")
+    size_controller = EditableList(proxy=size_proxy,
+                                   model=EditableListModel())
+    size_controller.create(None)
+    apply_default_configuration(size_proxy.root_proxy.binding)
+    yield size_controller, size_proxy
+    # teardown
+    size_controller.destroy()
+    assert size_controller.widget is None
+
+
+def test_size_list(list_size_setup):
+    size_controller, size_proxy = list_size_setup
+    assert get_min_max_size(size_proxy.binding) == (1, 3)
+    size_controller.set_read_only(False)
+    size_controller._internal_widget.setText("3,4")
+    assert all(size_proxy.edit_value == [3, 4])
+    size_controller._internal_widget.setText("3,4,5,6")
+    assert size_proxy.edit_value is None
+    size_controller._internal_widget.setText("")
+    assert size_proxy.edit_value is None
+
+
+def test_size_list_trailing_zeros(list_size_setup):
+    size_controller, size_proxy = list_size_setup
+    assert get_min_max_size(size_proxy.binding) == (1, 3)
+    size_controller.set_read_only(False)
+    size_controller._internal_widget.setText("03,4")
+    assert size_proxy.edit_value is None
+    size_controller._internal_widget.setText("3,4")
+    assert all(size_proxy.edit_value == [3, 4])
+    size_controller._internal_widget.setText("000000,4")
+    assert all(size_proxy.edit_value == [0, 4])
+
+
+def test_list_shorts(gui_app):
+    # setup
+    short_proxy = get_class_property_proxy(ShortObject.getClassSchema(),
+                                           "prop")
+    short_controller = EditableList(proxy=short_proxy,
+                                    model=EditableListModel())
+    short_controller.create(None)
+    apply_default_configuration(short_proxy.root_proxy.binding)
+    # test body
+    short_controller.set_read_only(False)
+    short_controller._internal_widget.setText("03,4")
+    assert short_proxy.edit_value is None
+    short_controller._internal_widget.setText("3,4")
+    assert all(short_proxy.edit_value == [3, 4])
+    # out of range
+    short_controller._internal_widget.setText("10300")
+    assert short_proxy.edit_value is None
+    # teardown
+    short_controller.destroy()
+    assert short_controller.widget is None
+
+
+@pytest.fixture
+def editable_regex_list_setup(gui_app):
+    proxy = get_class_property_proxy(RegexObject.getClassSchema(), "prop")
+    controller = EditableRegexList(proxy=proxy, model=EditableRegexListModel())
+    controller.create(None)
+    controller.binding_update(proxy)
+    apply_default_configuration(proxy.root_proxy.binding)
+    yield controller, proxy
+    # teardown
+    controller.destroy()
+    assert controller.widget is None
+
+
+def test_editable_regex_list_binding_update_validator(
+        editable_regex_list_setup):
+    controller, _ = editable_regex_list_setup
+    assert controller._internal_widget.validator() is not None
+
+
+def test_editable_regex_list_set_value(editable_regex_list_setup):
+    controller, proxy = editable_regex_list_setup
+    controller.last_cursor_position = 0
+    set_proxy_value(proxy, "prop", ["remote:outputDaq"])
+    assert controller._internal_widget.text() == "remote:outputDaq"
+    assert controller._internal_widget.cursorPosition() == 0
+
+
+def test_editable_regex_list_edit_value(editable_regex_list_setup):
+    controller, proxy = editable_regex_list_setup
+    controller.set_read_only(False)
+    controller._internal_widget.setText("remote:outputDaq,remote:output")
+    assert proxy.edit_value == ["remote:outputDaq", "remote:output"]
+
+
+def test_editable_regex_list_edit_empty_value(editable_regex_list_setup):
+    controller, proxy = editable_regex_list_setup
+    controller.set_read_only(False)
+    controller._internal_widget.setText("remote:output")
+    assert proxy.edit_value == ["remote:output"]
+    controller._internal_widget.setText("")
+    value = proxy.edit_value
+    assert value == []
