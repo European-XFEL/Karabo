02a795809c3d822f457654e69522c07076f10e08
Milan Staffehl <milan.staffehl@xfel.eu>
2023-05-08 11:57:16 +0200

    GUI: Transport textedit controller test to pytest

--- src/pythonGui/karabogui/controllers/edit/tests/test_textedit.py
+++ src/pythonGui/karabogui/controllers/edit/tests/test_textedit.py
@@ -3,8 +3,7 @@ from qtpy.QtCore import Qt
 
 from karabo.common.scenemodel.api import LineEditModel
 from karabo.native import Char, Configurable, String
-from karabogui.testing import (
-    GuiTestCase, get_class_property_proxy, set_proxy_value)
+from karabogui.testing import get_class_property_proxy, set_proxy_value
 
 from ..textedit import EditableLineEdit
 
@@ -17,55 +16,49 @@ class ObjectChar(Configurable):
     prop = Char()
 
 
-class TestEditableLineEdit(GuiTestCase):
-    def setUp(self):
-        super(TestEditableLineEdit, self).setUp()
-        self.proxy = get_class_property_proxy(Object.getClassSchema(), 'prop')
-        self.controller = EditableLineEdit(proxy=self.proxy,
-                                           model=LineEditModel())
-        self.controller.create(None)
+def test_editable_line_edit(gui_app):
+    # set up
+    proxy = get_class_property_proxy(Object.getClassSchema(), "prop")
+    controller = EditableLineEdit(proxy=proxy, model=LineEditModel())
+    controller.create(None)
 
-    def tearDown(self):
-        self.controller.destroy()
-        self.assertIsNone(self.controller.widget)
+    # test focus policy
+    assert controller.widget.focusPolicy() == Qt.StrongFocus
 
-    def test_focus_policy(self):
-        assert self.controller.widget.focusPolicy() == Qt.StrongFocus
+    # test set value
+    controller._last_cursor_pos = 0
+    set_proxy_value(proxy, "prop", "Blah")
+    assert controller.widget.text() == "Blah"
+    assert controller.widget.cursorPosition() == 0
 
-    def test_set_value(self):
-        self.controller._last_cursor_pos = 0
-        set_proxy_value(self.proxy, 'prop', 'Blah')
-        self.assertEqual(self.controller.widget.text(), 'Blah')
-        self.assertEqual(self.controller.widget.cursorPosition(), 0)
+    # test edit value
+    controller.widget.textChanged.emit("Wha??")
+    assert proxy.edit_value == "Wha??"
 
-    def test_edit_value(self):
-        self.controller.widget.textChanged.emit('Wha??')
-        self.assertEqual(self.proxy.edit_value, 'Wha??')
+    # teardown
+    controller.destroy()
+    assert controller.widget is None
 
 
-class TestCharEditableLineEdit(GuiTestCase):
-    def setUp(self):
-        super(TestCharEditableLineEdit, self).setUp()
-        self.proxy = get_class_property_proxy(ObjectChar.getClassSchema(),
-                                              'prop')
-        self.controller = EditableLineEdit(proxy=self.proxy,
-                                           model=LineEditModel())
-        self.controller.create(None)
+def test_char_editable_line_edit(gui_app):
+    # setup
+    proxy = get_class_property_proxy(ObjectChar.getClassSchema(), "prop")
+    controller = EditableLineEdit(proxy=proxy, model=LineEditModel())
+    controller.create(None)
 
-    def tearDown(self):
-        super(TestCharEditableLineEdit, self).tearDown()
-        self.controller.destroy()
-        self.assertIsNone(self.controller.widget)
+    # test set value
+    controller._last_cursor_pos = 0
+    set_proxy_value(proxy, "prop", "D")
+    assert controller.widget.text() == "D"
+    assert controller.widget.cursorPosition() == 0
 
-    def test_set_value(self):
-        self.controller._last_cursor_pos = 0
-        set_proxy_value(self.proxy, 'prop', 'D')
-        self.assertEqual(self.controller.widget.text(), 'D')
-        self.assertEqual(self.controller.widget.cursorPosition(), 0)
+    # test edit value
+    set_proxy_value(proxy, "prop", "$")
+    assert controller.widget.text() == "$"
+    controller.widget.setText("12345")
+    # stop at the first char
+    assert proxy.edit_value == "1"
 
-    def test_edit_value(self):
-        set_proxy_value(self.proxy, 'prop', '$')
-        self.assertEqual(self.controller.widget.text(), '$')
-        self.controller.widget.setText('12345')
-        # stop at the first char
-        self.assertEqual(self.proxy.edit_value, '1')
+    # teardown
+    controller.destroy()
+    assert controller.widget is None
