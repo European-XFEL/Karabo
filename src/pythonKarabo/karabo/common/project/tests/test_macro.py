6cf33efb1c929b58c557a9d86a55b6aae490ab59
Dennis Goeries <dennis.goeries@xfel.eu>
2023-05-22 09:18:26 +0200

    Common: Format python to newer style

--- src/pythonKarabo/karabo/common/project/tests/test_macro.py
+++ src/pythonKarabo/karabo/common/project/tests/test_macro.py
@@ -8,6 +8,6 @@ def test_instance_id_property():
     UUID = str(uuid.uuid4())
     macro = MacroModel(simple_name="foo", uuid=UUID)
 
-    assert macro.instance_id == "Macro-foo-{}".format(UUID)
+    assert macro.instance_id == f"Macro-foo-{UUID}"
     macro.simple_name = "bar"
-    assert macro.instance_id == "Macro-bar-{}".format(UUID)
+    assert macro.instance_id == f"Macro-bar-{UUID}"
