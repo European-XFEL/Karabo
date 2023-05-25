4d72570db2c0e07ee00fcae8b5bce579d30b7bdf
Dennis Goeries <dennis.goeries@xfel.eu>
2023-05-25 13:28:37 +0200

    MDL: Include config_changes to cli namespace

--- src/pythonKarabo/karabo/middlelayer/cli/__init__.py
+++ src/pythonKarabo/karabo/middlelayer/cli/__init__.py
@@ -5,15 +5,16 @@ import karabo
 from karabo.common.states import State
 from karabo.middlelayer_api.device_client import (
     call, callNoWait, compareConfigurationsFromPast,
-    compareDeviceConfiguration, compareDeviceWithPast, disconnectDevice,
-    execute, executeNoWait, findDevices, findServers, getClasses, getClients,
-    getConfiguration, getConfigurationFromName, getConfigurationFromPast,
-    getDevices, getHistory, getInstanceInfo, getLastConfiguration,
-    getProperties, getSchema, getSchemaFromPast, getServers, getTimeInfo,
-    getTopology, instantiate, instantiateFromName, instantiateNoWait, isAlive,
-    listConfigurationFromName, listDevicesWithConfiguration, printHistory,
-    saveConfigurationFromName, setNoWait, setWait, shutdown, shutdownNoWait,
-    updateDevice, waitUntil, waitUntilNew, waitWhile)
+    compareDeviceConfiguration, compareDeviceWithPast, config_changes,
+    disconnectDevice, execute, executeNoWait, findDevices, findServers,
+    getClasses, getClients, getConfiguration, getConfigurationFromName,
+    getConfigurationFromPast, getDevices, getHistory, getInstanceInfo,
+    getLastConfiguration, getProperties, getSchema, getSchemaFromPast,
+    getServers, getTimeInfo, getTopology, instantiate, instantiateFromName,
+    instantiateNoWait, isAlive, listConfigurationFromName,
+    listDevicesWithConfiguration, printHistory, saveConfigurationFromName,
+    setNoWait, setWait, shutdown, shutdownNoWait, updateDevice, waitUntil,
+    waitUntilNew, waitWhile)
 from karabo.middlelayer_api.eventloop import global_sync
 from karabo.middlelayer_api.ikarabo import connectDevice, getDevice
 from karabo.middlelayer_api.pipeline import PipelineContext, PipelineMetaData
