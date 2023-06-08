b262f18c8da0b3ee021467bee3be80d90d696032
Gero Flucke <gero.flucke@xfel.eu>
2023-06-08 16:30:33 +0200

    Extend copyright statement, point to licence (step 1, C++)

--- src/karathon/PyIoFileTools.cc
+++ src/karathon/PyIoFileTools.cc
@@ -4,7 +4,21 @@
  * Author: <irina.kozlova@xfel.eu>
  * Modified (2 May 2013): <burkhard.heisen@xfel.eu>
  *
+ * This file is part of Karabo.
+ *
+ * http://www.karabo.eu
+ *
  * Copyright (C) European XFEL GmbH Schenefeld. All rights reserved.
+ *
+ * Karabo is free software: you can redistribute it and/or modify it under
+ * the terms of the MPL-2 Mozilla Public License.
+ *
+ * You should have received a copy of the MPL-2 Public License along with
+ * Karabo. If not, see <https://www.mozilla.org/en-US/MPL/2.0/>.
+ *
+ * Karabo is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.
  */
 
 #include <boost/pointer_cast.hpp>
