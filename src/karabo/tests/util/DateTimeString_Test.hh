f7f2c6336471f07cd9f0cded8effb59478a97882
Gero Flucke <gero.flucke@xfel.eu>
2023-06-08 16:30:33 +0200

    Extend copyright statement, point to licence (step 1, C++)

--- src/karabo/tests/util/DateTimeString_Test.hh
+++ src/karabo/tests/util/DateTimeString_Test.hh
@@ -4,7 +4,21 @@
  *
  * Created on March 19, 2014, 11:01 AM
  *
+ * This file is part of Karabo.
+ *
+ * http://www.karabo.eu
+ *
  * Copyright (C) European XFEL GmbH Schenefeld. All rights reserved.
+ *
+ * Karabo is free software: you can redistribute it and/or modify it under
+ * the terms of the MPL-2 Mozilla Public License.
+ *
+ * You should have received a copy of the MPL-2 Public License along with
+ * Karabo. If not, see <https://www.mozilla.org/en-US/MPL/2.0/>.
+ *
+ * Karabo is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.
  */
 
 #ifndef DATETIMESTRING_TEST_HH
