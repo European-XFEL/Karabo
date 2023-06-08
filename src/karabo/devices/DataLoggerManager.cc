9972308fb5ceb6b11f290f1e3ba5c586038da861
Gero Flucke <gero.flucke@xfel.eu>
2023-06-08 16:30:33 +0200

    Extend copyright statement, point to licence (step 1, C++)

--- src/karabo/devices/DataLoggerManager.cc
+++ src/karabo/devices/DataLoggerManager.cc
@@ -1,5 +1,19 @@
 /**
+ * This file is part of Karabo.
+ *
+ * http://www.karabo.eu
+ *
  * Copyright (C) European XFEL GmbH Schenefeld. All rights reserved.
+ *
+ * Karabo is free software: you can redistribute it and/or modify it under
+ * the terms of the MPL-2 Mozilla Public License.
+ *
+ * You should have received a copy of the MPL-2 Public License along with
+ * Karabo. If not, see <https://www.mozilla.org/en-US/MPL/2.0/>.
+ *
+ * Karabo is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.
  *
  * The main logic of the managing of the data loggers and of the devices that they should log is as follows:
  * * Register handlers for new and gone instances before topology gathering starts.
