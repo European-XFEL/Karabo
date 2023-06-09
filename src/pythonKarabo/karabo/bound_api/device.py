5e0ae393a2847957e2a4a1fd629abbaa634a9e82
Gero Flucke <gero.flucke@xfel.eu>
2023-06-09 10:12:58 +0200

    MDL/Bound: Extend copyright statement, point to license
    Treat all src/pythonKarabo

--- src/pythonKarabo/karabo/bound_api/device.py
+++ src/pythonKarabo/karabo/bound_api/device.py
@@ -1,4 +1,18 @@
+# This file is part of Karabo.
+#
+# http://www.karabo.eu
+#
 # Copyright (C) European XFEL GmbH Schenefeld. All rights reserved.
+#
+# Karabo is free software: you can redistribute it and/or modify it under
+# the terms of the MPL-2 Mozilla Public License.
+#
+# You should have received a copy of the MPL-2 Public License along with
+# Karabo. If not, see <https://www.mozilla.org/en-US/MPL/2.0/>.
+#
+# Karabo is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.
 __author__ = "Sergey Esenov <serguei.essenov at xfel.eu>"
 __date__ = "$Jul 30, 2012 9:03:51 PM$"
 
