415486065e08942b15b678ef8eac9430b2799a43
Gero Flucke <gero.flucke@xfel.eu>
2023-06-12 10:40:57 +0200

    GUI: Extend copyright statement, point to license

--- src/pythonGui/karabogui/controllers/display/textlog.py
+++ src/pythonGui/karabogui/controllers/display/textlog.py
@@ -1,7 +1,22 @@
 #############################################################################
 # Author: <dennis.goeries@xfel.eu>
 # Created on October 27, 2012
+# This file is part of the Karabo Gui.
+#
+# http://www.karabo.eu
+#
 # Copyright (C) European XFEL GmbH Schenefeld. All rights reserved.
+#
+# The Karabo Gui is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License, version 3 or higher.
+#
+# You should have received a copy of the General Public License, version 3,
+# along with the Karabo Gui.
+# If not, see <https://www.gnu.org/licenses/gpl-3.0>.
+#
+# The Karabo Gui is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.
 #############################################################################
 from datetime import datetime
 
