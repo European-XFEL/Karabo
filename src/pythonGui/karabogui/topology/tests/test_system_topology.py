3c67def512dcc9ed27fabbe27341c6fe9e701ba0
Gero Flucke <gero.flucke@xfel.eu>
2023-06-12 10:40:57 +0200

    GUI: Extend copyright statement, point to license

--- src/pythonGui/karabogui/topology/tests/test_system_topology.py
+++ src/pythonGui/karabogui/topology/tests/test_system_topology.py
@@ -1,4 +1,19 @@
+# This file is part of the Karabo Gui.
+#
+# http://www.karabo.eu
+#
 # Copyright (C) European XFEL GmbH Schenefeld. All rights reserved.
+#
+# The Karabo Gui is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License, version 3 or higher.
+#
+# You should have received a copy of the General Public License, version 3,
+# along with the Karabo Gui.
+# If not, see <https://www.gnu.org/licenses/gpl-3.0>.
+#
+# The Karabo Gui is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.
 from unittest.mock import Mock
 
 from traits.api import pop_exception_handler, push_exception_handler
