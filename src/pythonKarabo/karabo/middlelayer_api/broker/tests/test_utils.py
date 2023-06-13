b8992c9257faf89245b03306162bba616ffad561
Dennis Goeries <dennis.goeries@xfel.eu>
2023-06-13 16:10:51 +0200

    MDL: Change broker scheme tests

--- src/pythonKarabo/karabo/middlelayer_api/broker/tests/test_utils.py
+++ src/pythonKarabo/karabo/middlelayer_api/broker/tests/test_utils.py
@@ -20,22 +20,22 @@ from ..utils import check_broker_scheme
 
 def test_check_broker_scheme():
     # 1. different schemes
-    hosts = ["amqp://xfel:karabo@exflc1:1111",
+    hosts = ["amqp://control:spirit@exflc1:1111",
              "tcp://exfl-broker:7777"]
     with pytest.raises(RuntimeError):
         check_broker_scheme(hosts)
 
     # 2. typo in schemes
-    hosts = ["amqp://xfel:karabo@exflc1:1111",
-             "amqp//xfel:karabo@exflc1:1111"]
+    hosts = ["amqp://control:spirit@exflc1:1111",
+             "amqp//control:spirit@exflc1:1111"]
     with pytest.raises(RuntimeError):
         check_broker_scheme(hosts)
 
     # 3.1 correct scheme amqp, also with `-`
-    hosts = ["amqp://xfel:karabo@exflc1:1111",
-             "amqp://xfel:karabo@exfl-bkr-1:1111"]
+    hosts = ["amqp://control:spirit@exflc1:1111",
+             "amqp://control:spirit@exfl-bkr-1:1111"]
     assert not check_broker_scheme(hosts)
-    hosts = ["amqp://xfel:karabo@exflc1:1111"]
+    hosts = ["amqp://control:spirit@exflc1:1111"]
     assert not check_broker_scheme(hosts)
 
     # 3.2 correct scheme jms
@@ -46,25 +46,25 @@ def test_check_broker_scheme():
     assert not check_broker_scheme(hosts)
 
     # 4.1 incomplete scheme amqp, first missing port
-    hosts = ["amqp://xfel:karabo@exflc1",
-             "amqp://xfel:karabo@exflc1:1111"]
+    hosts = ["amqp://control:spirit@exflc1",
+             "amqp://control:spirit@exflc1:1111"]
     with pytest.raises(RuntimeError):
         check_broker_scheme(hosts)
     # 4.2 incomplete amqp, missing password, etc.
-    hosts = ["amqp://xfel@exflc1:1111",
-             "amqp://xfel:karabo@exflc1"]
+    hosts = ["amqp://control@exflc1:1111",
+             "amqp://control:spirit@exflc1"]
     with pytest.raises(RuntimeError):
         check_broker_scheme(hosts)
 
     # 4.3 No @ sign
-    hosts = ["amqp://xfel:karaboexflc1:1111",
-             "amqp://xfel:karabo@exflc1:1111"]
+    hosts = ["amqp://control:spiritexflc1:1111",
+             "amqp://control:spirit@exflc1:1111"]
     with pytest.raises(RuntimeError):
         check_broker_scheme(hosts)
 
     # 4.4 No : second
-    hosts = ["amqp://xfel:karabo@exflc1:1111",
-             "amqp://xfelkarabo@exflc1:1111"]
+    hosts = ["amqp://control:spirit@exflc1:1111",
+             "amqp://controlspirit@exflc1:1111"]
     with pytest.raises(RuntimeError):
         check_broker_scheme(hosts)
 
