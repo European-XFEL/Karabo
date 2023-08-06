87632ac987deccb4881363dc20faf6a5ed47846b
Noushadali Anakkappalla <noushadali.anakkappalla@xfel.eu>
2023-08-06 14:19:12 +0200

    GUI: KaraboLogBook - Table Preview

--- src/pythonGui/karabogui/dialogs/tests/test_logbook_preview.py
+++ src/pythonGui/karabogui/dialogs/tests/test_logbook_preview.py
@@ -1,19 +1,78 @@
+import pytest
+
 from karabogui.binding.api import DeviceClassProxy, build_binding
 from karabogui.dialogs.logbook_preview import LogBookPreview
 from karabogui.panels.api import ConfigurationPanel
 from karabogui.singletons.mediator import Mediator
+from karabogui.singletons.network import Network
 from karabogui.testing import get_simple_schema, singletons
 
+title = ("{}: <ConfigurationPanel proxy=<DeviceClassProxy classId=Simple "
+         "serverId=test_server>>")
 
-def test_logbook_preview(gui_app):
-    """Test the logbook preview with a configuration panel"""
+
+@pytest.fixture
+def dialog(gui_app):
     mediator = Mediator()
     with singletons(mediator=mediator):
         panel = ConfigurationPanel()
-        dialog = LogBookPreview(parent=panel)
 
         schema = get_simple_schema()
         binding = build_binding(schema)
         proxy = DeviceClassProxy(server_id="test_server", binding=binding)
         panel._show_configuration(proxy)
-        dialog.done(0)
+        dialog = LogBookPreview(parent=panel)
+    return dialog
+
+
+def test_logbook_preview(dialog):
+    """Test the logbook preview with a configuration panel"""
+    assert dialog.stackedWidget.currentIndex() == 0
+    assert dialog.table.columnCount() == 2
+    assert dialog.table.rowCount() == 3
+    dialog.done(0)
+
+
+def test_image_preview(dialog, mocker):
+    assert dialog.combo_datatype.currentText() == "Image"
+    network = Network()
+    with singletons(network=network):
+        saveLogBook = mocker.patch.object(network, "onSaveLogBook")
+        dialog.done(1)
+        assert saveLogBook.call_count == 1
+        _, args = saveLogBook.call_args
+        assert args["dataType"] == "image"
+        assert args["title"] == title.format("Image")
+        assert args["data"].startswith("data:image/png;base64,")
+        assert not args["caption"]
+
+
+def test_logbook_table_preview(dialog, mocker):
+    dialog.combo_datatype.setCurrentIndex(1)
+    assert dialog.combo_datatype.currentText() == "Data"
+    assert dialog.stackedWidget.currentIndex() == 1
+    assert all([checkbox.isChecked() for checkbox in dialog.checkboxes])
+    dialog._deselect_all()
+    assert not any([checkbox.isChecked() for checkbox in dialog.checkboxes])
+    dialog._select_all()
+    assert all([checkbox.isChecked() for checkbox in dialog.checkboxes])
+
+    network = Network()
+    with singletons(network=network):
+        saveLogBook = mocker.patch.object(network, "onSaveLogBook")
+        dialog.done(1)
+        assert saveLogBook.call_count == 1
+        _, args = saveLogBook.call_args
+        assert args["dataType"] == "hash"
+        assert args["title"] == title.format("Data")
+        for item in ("foo", "bar", "charlie"):
+            assert item in args["data"]
+
+        # Unselected row should not be saved
+        dialog.checkboxes[0].setChecked(False)
+        dialog.done(1)
+        _, args = saveLogBook.call_args
+        data = args["data"]
+        assert "foo" not in data
+        for item in ("bar", "charlie"):
+            assert item in data
