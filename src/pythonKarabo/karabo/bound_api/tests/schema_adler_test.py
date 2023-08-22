1f1a641a6093e37d28a4ef49c6dff447b0f233d9
Dennis Goeries <dennis.goeries@xfel.eu>
2023-08-22 21:35:25 +0200

    MOre fixes for alarm

--- src/pythonKarabo/karabo/bound_api/tests/schema_adler_test.py
+++ src/pythonKarabo/karabo/bound_api/tests/schema_adler_test.py
@@ -43,6 +43,6 @@ class Tests(unittest.TestCase):
             stdout=subprocess.PIPE)
         schema = process.communicate()[0]
         print(f"\nCross schema test: schema length is {len(schema)}")
-        self.assertEqual(adler32(schema), 3971668174,
+        self.assertEqual(adler32(schema), 1165152544,
                          "The generated schema changed. If this is "
                          "desired, change the checksum in the code.")
