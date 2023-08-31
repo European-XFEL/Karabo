e55d85612c7d1eb3fce382ceb5d1697c230eeec4
Dennis Goeries <dennis.goeries@xfel.eu>
2023-08-31 13:47:33 +0200

    MDL: Remove attr fast deepcopy and extract attribute modifications

--- src/pythonKarabo/karabo/middlelayer/configuration.py
+++ src/pythonKarabo/karabo/middlelayer/configuration.py
@@ -13,13 +13,11 @@
 # Karabo is distributed in the hope that it will be useful, but WITHOUT ANY
 # WARRANTY; without even the implied warranty of MERCHANTABILITY or
 # FITNESS FOR A PARTICULAR PURPOSE.
-from karabo.common import const
 from karabo.common.api import (
     KARABO_SCHEMA_ACCESS_MODE, KARABO_SCHEMA_ASSIGNMENT,
     KARABO_SCHEMA_CLASS_ID, KARABO_SCHEMA_OPTIONS)
 from karabo.native import (
-    AccessMode, Assignment, Hash, MetricPrefix, NodeType, Schema, Unit,
-    is_equal)
+    AccessMode, Assignment, Hash, NodeType, Schema, is_equal)
 
 # Broken path accumulated in the history of karabo ...
 # Choice of Nodes connection and Beckhoff properties that will throw!
@@ -127,118 +125,6 @@ def extract_configuration(schema, config, init=False):
             retval.erase(key)
 
     return retval
-
-
-def attr_fast_deepcopy(d, ref=None):
-    """copy.deepcopy is criminally slow. We can bypass its fanciness as long
-    as we only copy 'simple' datastructures.
-
-    Pass a not None attributes dict to `ref` to get only changed attributes
-    """
-    out = {}
-
-    for k, v in d.items():
-        if ref is not None:
-            if (k not in const.KARABO_EDITABLE_ATTRIBUTES or
-                    is_equal(v, ref.get(k))):
-                continue
-        try:
-            out[k] = v.copy()  # dicts, sets, ndarrays
-        except TypeError:
-            # This is a Schema, which has a terrible API
-            assert isinstance(v, Schema)
-            cpy = Schema()
-            cpy.copy(v)
-            out[k] = cpy
-        except AttributeError:
-            try:
-                out[k] = v[:]  # lists, tuples, strings, unicode
-            except TypeError:
-                out[k] = v  # simple values
-
-    return out
-
-
-def extract_modified_schema_attributes(runtime_schema, class_schema):
-    """Extract modified attributes from a runtime schema relative to a
-    class schema
-
-    :param runtime_schema: Runtime schema of device
-    :param class_schema: Class schema of device
-
-    Returns a list of Hashes containing the differences, or None if there are
-    none.
-
-    NOTE: `_NAME_MAP` and `_remap_value` are handling conversion of str
-    'Symbol' values to C++ enumeration values (integers). This is because the
-    C++ Schema code only knows how to assign from enumeration values, not from
-    their stringified representations.
-    """
-
-    NODE_TYPE = (NodeType.Node.value,)
-    SPECIAL_TYPES = (NodeType.ChoiceOfNodes.value,
-                     NodeType.ListOfNodes.value)
-
-    def flat_iter_schema_hash(schema_hash, base=''):
-        """Expose a flat iteration over a schema Hash.
-
-        :param schema: The schema Hash or Schema object
-
-        Note: Schema Hashes are special because every property
-        comes with an empty `Hash` as value. Hence, we ask for the Nodetype!
-        """
-        assert isinstance(schema_hash, Hash)
-
-        base = base + '.' if base else ''
-        for key, value, attrs in schema_hash.iterall():
-            subkey = base + key
-            is_node = attrs["nodeType"] in NODE_TYPE
-            is_special = attrs["nodeType"] in SPECIAL_TYPES
-            if is_node:
-                yield from flat_iter_schema_hash(value, base=subkey)
-            elif not is_special:
-                yield subkey
-
-    assert isinstance(runtime_schema, Schema)
-    assert isinstance(class_schema, Schema)
-
-    _NAME_MAP = {
-        const.KARABO_SCHEMA_METRIC_PREFIX_SYMBOL:
-            const.KARABO_SCHEMA_METRIC_PREFIX_ENUM,
-        const.KARABO_SCHEMA_UNIT_SYMBOL: const.KARABO_SCHEMA_UNIT_ENUM
-    }
-    _SYMBOL_MAP = {const.KARABO_SCHEMA_METRIC_PREFIX_SYMBOL: MetricPrefix,
-                   const.KARABO_SCHEMA_UNIT_SYMBOL: Unit}
-
-    def _remap_value(name, value):
-        enum = _SYMBOL_MAP.get(name, None)
-        return list(enum).index(enum(value)) if enum else value
-
-    def _get_updates(path, attrs):
-        # Format is specified by Device::slotUpdateSchemaAttributes
-        return [Hash("path", path, "attribute", k, "value", v)
-                for k, v in attrs.items()]
-
-    retval = []
-    runtime_schema_hash = runtime_schema.hash
-    class_schema_hash = class_schema.hash
-    for key in flat_iter_schema_hash(runtime_schema_hash):
-        if key not in class_schema_hash:
-            # If key is not there offline, we don't care
-            continue
-        offline_attrs = class_schema_hash[key, ...]
-        online_attrs = runtime_schema_hash[key, ...]
-        # Reference is the class schema, offline
-        diff = attr_fast_deepcopy(online_attrs, ref=offline_attrs)
-        if not diff:
-            continue
-        diff = {_NAME_MAP.get(k, k): _remap_value(k, v)
-                for k, v in diff.items()}
-        retval.extend(_get_updates(key, diff))
-
-    if retval:
-        return retval
-    return None
 
 
 def config_changes(a, b):
