2dd628a9525ab6ee6a714b31073609524f3f5162
Gero Flucke <gero.flucke@xfel.eu>
2023-09-04 13:53:17 +0200

    Test bindings of synchronous TCP read and write
    Fix channel.write(Hash(..)) for karabind

--- src/pythonKarabo/karabo/bound_api/tests/karabind/test_tcp.py
+++ src/pythonKarabo/karabo/bound_api/tests/karabind/test_tcp.py
@@ -15,14 +15,124 @@
 # FITNESS FOR A PARTICULAR PURPOSE.
 
 import threading
+import time
 
 import karabind
 import pytest
 
-# import karathon
+import karathon
 
 debugFlag = False
 lock = threading.Lock()
+
+
+@pytest.fixture(scope="module")
+def event_loop():
+    # We test the TCP code here and it does not matter which event loop
+    # bindings we use - so choose karabind.
+    EventLoop = karabind.EventLoop
+    loop_thread = threading.Thread(target=EventLoop.work)
+    loop_thread.start()
+
+    yield  # now test is executed
+
+    EventLoop.stop()
+    loop_thread.join()
+
+
+@pytest.mark.parametrize(
+    "Connection, Hash, fullyEqual",
+    [(karabind.Connection, karabind.Hash, karabind.fullyEqual),
+     (karathon.Connection, karathon.Hash, karathon.fullyEqual)
+     ])
+def test_synch_write_read(event_loop, Connection, Hash, fullyEqual):
+    # Alice as server, bob as client. Server cannot be started synchronously
+    # since then the port that the client should connect to is not available.
+
+    aliceConn = Connection.create("Tcp", Hash("type", "server"))
+    alice = None  # to become the Channel
+
+    def onConnect(ec, channel):
+        nonlocal alice
+        alice = channel
+
+    aPort = aliceConn.startAsync(onConnect)
+
+    bobCfg = Hash("type", "client", "hostname", "localhost", "port", aPort)
+    bobConn = Connection.create("Tcp", bobCfg)
+    bob = bobConn.start()  # bob is the Channel
+
+    # Wait until alice took note of the connection and her channel is available
+    timeout = 2
+    while alice is None and timeout > 0:
+        time.sleep(0.01)
+        timeout -= 0.01
+
+    assert alice is not None
+
+    # Alice and Bob are prepared, so we start testing write and read methods.
+
+    alice.write("messageStr")  # str
+    readMsg = bob.readStr()
+    assert readMsg == "messageStr"
+
+    alice.write(b"messageBytes")  # bytes
+    readMsg = bob.readStr()
+    assert readMsg == "messageBytes"
+
+    alice.write(bytearray("messageBytearray", "utf-8"))  # bytearray
+    readMsg = bob.readStr()
+    assert readMsg == "messageBytearray"
+
+    alice.write(Hash("hash", "message"))  # Hash
+    readMsg = bob.readHash()
+    assert fullyEqual(readMsg, Hash("hash", "message"))
+
+    if type(aliceConn) is not karathon.Connection:  # TODO FIXME
+        # With karathon I get
+        # TypeError: Python type in parameters is not supported
+        # The above exception was the direct cause of the following exception:
+        # <snip>
+        #        with pytest.raises(RuntimeError) as excinfo:
+        # >           alice.write(1)  # not supported
+        # E           SystemError: <Boost.Python.function object at 0x...> \
+        #                                  returned a result with an error set
+        with pytest.raises(RuntimeError) as excinfo:
+            alice.write(1)  # not supported to write int
+            assert "Python Exception: Not supported type" in str(excinfo.value)
+
+    alice.write(Hash("str", "header"), "messageStr")  # Hash, str
+    readHeader, readMsg = bob.readHashStr()
+    assert fullyEqual(readHeader, Hash("str", "header"))
+    assert readMsg == "messageStr"
+
+    alice.write(Hash("bytes", "header"), b"messageBytes")  # Hash, bytes
+    readHeader, readMsg = bob.readHashStr()
+    assert fullyEqual(readHeader, Hash("bytes", "header"))
+    assert readMsg == "messageBytes"
+
+    alice.write(Hash("bytearray", "header"),
+                bytearray("messageBytearray", "utf-8"))  # bytearray
+    readHeader, readMsg = bob.readHashStr()
+    assert fullyEqual(readHeader, Hash("bytearray", "header"))
+    assert readMsg == "messageBytearray"
+
+    alice.write(Hash("a", "header"), Hash("hash", "message"))  # Hash, Hash
+    readHeader, readMsg = bob.readHashHash()
+    assert fullyEqual(readHeader, Hash("a", "header"))
+    assert fullyEqual(readMsg, Hash("hash", "message"))
+
+    if type(aliceConn) is not karathon.Connection:  # TODO FIXME
+        # With karathon I get TypeError etc., as above
+        with pytest.raises(RuntimeError) as excinfo:
+            alice.write(Hash("a", "header"), 1)  # not supported
+            assert "Python Exception: Not supported type" in str(excinfo.value)
+
+        # Not sure which error we would get here with karathon
+        with pytest.raises(TypeError) as excinfo:
+            alice.write(1, "message")  # not supported header type
+            assert ("TypeError:  write(): incompatible function arguments."
+                    in str(excinfo.value))
 
 
 def DEBUG(s):
