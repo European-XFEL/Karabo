3015c9abfa6647132291f56a0b68734bec38cf46
Michael Smith <michael.smith@xfel.eu>
2023-11-02 11:55:41 +0100

    DEPS: use conan to build daemontools

---
+++ extern/resources/daemontools/conanfile.py
@@ -0,0 +1,29 @@
+import os
+import shutil
+
+from conans import ConanFile
+from conans.tools import download, unzip
+
+
+class KaraboDaemonToolsConan(ConanFile):
+    name = "daemontools-encore"
+    version = "1.11-karabo3"
+    description = "Karabo specific fork of daemontools"
+    settings = "os", "arch", "compiler", "build_type"
+    url = "https://github.com/tecki/daemontools-encore/"
+    license="https://github.com/tecki/daemontools-encore/blob/master/LICENSE"
+
+    def source(self):
+        zip_name = "daemontools-encore-%s.zip" % self.version
+        url = "https://github.com/tecki/daemontools-encore/archive/refs/heads/karabo.zip"
+        download(url, zip_name)
+        unzip(zip_name, strip_root=True)
+        os.unlink(zip_name)
+
+    def build(self):
+        self.run("bash makemake")
+        self.run("make programs")
+        self.run(f"mkdir bin; cp $(cut -d: -f6 BIN) bin")
+
+    def package(self):
+        self.copy("*", "bin", "bin")
