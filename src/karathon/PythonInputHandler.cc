8fb65689a3d6f647f87553203842efb9c0b76ec5
Geogin Varghese <geogin.varghese@xfel.eu>
2023-12-06 13:20:56 +0100

    Try clang

--- src/karathon/PythonInputHandler.cc
+++ src/karathon/PythonInputHandler.cc
@@ -19,14 +19,10 @@
 
 #include <karabo/io/Input.hh>
 
-namespace karathon {
-    // KARABO_REGISTER_IN_FACTORY(karabo::io::InputEventHandler, PythonInputHandler);
+KARABO_REGISTER_IN_FACTORY_1(karabo::io::InputHandler,
+                             karathon::PythonInputHandler<karabo::io::Input<karabo::util::Hash> >,
+                             karabo::io::AbstractInput::Pointer);
 
-    // KARABO_REGISTER_IN_FACTORY_1(karabo::io::InputEventHandler, PythonInputHandler,
-    // karabo::io::AbstractInput::Pointer);
-
-    KARABO_REGISTER_IN_FACTORY_1(karabo::io::InputHandler, PythonInputHandler<karabo::io::Input<karabo::util::Hash> >,
-                                 karabo::io::AbstractInput::Pointer);
-    KARABO_REGISTER_IN_FACTORY_1(karabo::io::InputHandler, PythonInputHandler<karabo::io::Input<std::vector<char> > >,
-                                 karabo::io::AbstractInput::Pointer);
-} // namespace karathon
+KARABO_REGISTER_IN_FACTORY_1(karabo::io::InputHandler,
+                             karathon::PythonInputHandler<karabo::io::Input<std::vector<char> > >,
+                             karabo::io::AbstractInput::Pointer);
