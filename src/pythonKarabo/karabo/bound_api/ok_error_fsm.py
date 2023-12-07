8ef69157ccb239e15c7981f52daa7bf65dce471b
Sergey Esenov <serguei.essenov@xfel.eu>
2023-12-07 19:53:06 +0100

    Py11: Complete tests on DeviceClient

--- src/pythonKarabo/karabo/bound_api/ok_error_fsm.py
+++ src/pythonKarabo/karabo/bound_api/ok_error_fsm.py
@@ -20,8 +20,8 @@ __author__ = "Sergey Esenov <serguei.essenov at xfel.eu>"
 __date__ = "$May 12, 2013 5:09:05 PM$"
 
 import karabo.bound_api.base_fsm as base
+from karabo.bound_tool import SLOT_ELEMENT
 from karabo.common.states import State
-from karathon import SLOT_ELEMENT
 
 from .decorators import KARABO_CLASSINFO
 from .fsm import (
