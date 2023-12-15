514b4af8274747a38845f4617b81f0a3c4e652b6
Gero Flucke <gero.flucke@xfel.eu>
2023-12-15 08:52:24 +0100

    perf(C++): Slot call without copy of arguments

---
+++ src/karabo/tests/xms/Slot_Test.hh
@@ -0,0 +1,41 @@
+/*
+ * This file is part of Karabo.
+ *
+ * http://www.karabo.eu
+ *
+ * Copyright (C) European XFEL GmbH Schenefeld. All rights reserved.
+ *
+ * Karabo is free software: you can redistribute it and/or modify it under
+ * the terms of the MPL-2 Mozilla Public License.
+ *
+ * You should have received a copy of the MPL-2 Public License along with
+ * Karabo. If not, see <https://www.mozilla.org/en-US/MPL/2.0/>.
+ *
+ * Karabo is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+#ifndef SLOT_TEST_HH
+#define SLOT_TEST_HH
+
+#include <cppunit/extensions/HelperMacros.h>
+
+class Slot_Test : public CPPUNIT_NS::TestFixture {
+    CPPUNIT_TEST_SUITE(Slot_Test);
+
+    CPPUNIT_TEST(testCallSlot);
+
+    CPPUNIT_TEST_SUITE_END();
+
+   public:
+    Slot_Test();
+    virtual ~Slot_Test();
+    void setUp();
+    void tearDown();
+
+   private:
+    void testCallSlot();
+};
+
+#endif /* SLOT_TEST_HH */
