462939987306285d95884d5e6183b018f7d1f854
Dennis Goeries <dennis.goeries@xfel.eu>
2024-01-03 20:34:44 +0100

    MDL: Align templates

--- src/templates/middlelayer/minimal/src/__PACKAGE_NAME__/tests/property_naming_test.py
+++ src/templates/middlelayer/minimal/src/__PACKAGE_NAME__/tests/property_naming_test.py
@@ -16,12 +16,7 @@ import __PACKAGE_NAME__
 
 def test_property_code_guideline():
     """Test that all properties and slots follow common code style"""
-    try:
-        from karabo.middlelayer.testing import check_device_package_properties
-    except (ImportError, ModuleNotFoundError):
-        print("Checking device properties not possible with existing "
-              "karabo version.")
-        return
+    from karabo.middlelayer.testing import check_device_package_properties
     keys = check_device_package_properties(__PACKAGE_NAME__)
     msg = ("The key naming does not comply with our coding guidelines. "
            f"Please have a look at (class: paths): {keys}")
