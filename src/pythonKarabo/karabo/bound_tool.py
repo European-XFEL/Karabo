a91a103f5757aa67c2fb97eea0be7f38aa0ddf70
Gero Flucke <gero.flucke@xfel.eu>
2024-01-12 10:00:32 +0100

    Py11:: Patch Types.__str__ to match karathon

--- src/pythonKarabo/karabo/bound_tool.py
+++ src/pythonKarabo/karabo/bound_tool.py
@@ -102,6 +102,15 @@ else:
         VectorHashPointer, _DimsIntern, fullyEqual, loadFromFile, saveToFile,
         setDims, similar)
 
+    # For comptibility with old karathon bindings, take care that e.g.
+    # str(Types.INT32) is 'INT32' and not 'Types.INT32'
+    def __typesToString(self):
+        repStr = repr(self)  # i.e. '<Types.{this we want}: {enum value}>'
+        return repStr[7:repStr.find(':')]
+
+    setattr(Types, "__str__", __typesToString)
+        
+
     class AbstractInput:
         pass
 
