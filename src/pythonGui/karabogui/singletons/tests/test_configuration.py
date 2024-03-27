6a05144da1481e7fa0a8b8ccbf43c0af0b7dc200
Milan Staffehl <milan.staffehl@xfel.eu>
2024-03-27 10:17:57 +0100

    refactor(GUI): transport configuration test to pytest

--- src/pythonGui/karabogui/singletons/tests/test_configuration.py
+++ src/pythonGui/karabogui/singletons/tests/test_configuration.py
@@ -14,11 +14,7 @@
 # The Karabo Gui is distributed in the hope that it will be useful, but
 # WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
 # or FITNESS FOR A PARTICULAR PURPOSE.
-from unittest.mock import patch
-
 import pytest
-
-from karabogui.testing import GuiTestCase
 
 from ..configuration import (
     BACKBONE, DIRECTORIES, NETWORK, PANEL, PROJECT, USER, Configuration)
@@ -44,116 +40,120 @@ class MockSettings:
         return self
 
 
-class TestConfiguration(GuiTestCase):
-    def test_configuration_namespace(self):
-        config = Configuration()
-        config['broker_topic'] = 'FXE'
-        config['device_domain'] = 'CAS_INTERNAL'
-        config['domain'] = 'CAS_INTERNAL'
-        config['db_token'] = 'admin'
-        assert len(config) == 21
-        assert config['db_token'] == 'admin'
-        assert config['device_domain'] == 'CAS_INTERNAL'
-        assert config['domain'] == 'CAS_INTERNAL'
+def test_configuration_namespace():
+    config = Configuration()
+    config['broker_topic'] = 'FXE'
+    config['device_domain'] = 'CAS_INTERNAL'
+    config['domain'] = 'CAS_INTERNAL'
+    config['db_token'] = 'admin'
+    assert len(config) == 21
+    assert config['db_token'] == 'admin'
+    assert config['device_domain'] == 'CAS_INTERNAL'
+    assert config['domain'] == 'CAS_INTERNAL'
 
-        keys = [
-            'access_level',
-            'broker_topic',
-            'console_visible',
-            'data_dir',
-            'db_token',
-            'development',
-            'device_domain',
-            'documentation',
-            'domain',
-            'gui_servers',
-            'highDPI',
-            'logbook_header_style',
-            'logbook_stream',
-            'logbook_topic',
-            'macro_development',
-            'main_geometry',
-            'project_sort_column',
-            'project_sort_order',
-            'reactive_login',
-            'username',
-            'wizard']
+    keys = [
+        'access_level',
+        'broker_topic',
+        'console_visible',
+        'data_dir',
+        'db_token',
+        'development',
+        'device_domain',
+        'documentation',
+        'domain',
+        'gui_servers',
+        'highDPI',
+        'logbook_header_style',
+        'logbook_stream',
+        'logbook_topic',
+        'macro_development',
+        'main_geometry',
+        'project_sort_column',
+        'project_sort_order',
+        'reactive_login',
+        'username',
+        'wizard']
 
-        assert list(config.keys()) == keys
+    assert list(config.keys()) == keys
 
-    def test_set_wrong_key(self):
-        config = Configuration()
-        with pytest.raises(KeyError):
-            config['noitem'] = "Beep"
 
-        with pytest.raises(KeyError):
-            config.noitem = "Boom"
+def test_set_wrong_key():
+    config = Configuration()
+    with pytest.raises(KeyError):
+        config['noitem'] = "Beep"
 
-    def test_default_value(self):
-        """Every item that specifies a data type must have a default value"""
-        config = Configuration()
-        counter = 0
-        for key in config.keys():
-            item = getattr(config.__class__, key)
-            if item.dtype is not None:
-                counter += 1
-                assert item.default is not None
-        assert counter == 7
+    with pytest.raises(KeyError):
+        config.noitem = "Boom"
 
-    def test_set_bool_value(self):
-        target = 'karabogui.singletons.configuration.QSettings'
-        with patch(target, new=MockSettings):
-            config = Configuration()
-            assert config["wizard"] is True
-            config["wizard"] = False
-            config['db_token'] = "observer"
-            assert config["wizard"] is False
-            assert config["db_token"] == "observer"
 
-    def test_erase_value(self):
-        target = 'karabogui.singletons.configuration.QSettings'
-        mock = MockSettings()
-        with patch(target, new=mock):
-            config = Configuration()
-            assert config["wizard"] is True
-            config["wizard"] = False
-            del config["wizard"]
-            assert mock.last_path == "user/wizard"
+def test_default_value():
+    """Every item that specifies a data type must have a default value"""
+    config = Configuration()
+    counter = 0
+    for key in config.keys():
+        item = getattr(config.__class__, key)
+        if item.dtype is not None:
+            counter += 1
+            assert item.default is not None
+    assert counter == 7
 
-    def test_configuration_groups_info(self):
-        config = Configuration()
-        groups = config.groups()
-        assert len(groups) == 6
-        user_group = [item.name for item in groups[USER]]
-        assert len(user_group) == 9
-        assert 'wizard' in user_group
-        assert 'main_geometry' in user_group
-        assert 'highDPI' in user_group
-        assert 'development' in user_group
-        assert 'project_sort_column' in user_group
-        assert 'project_sort_order' in user_group
-        network_group = [item.name for item in groups[NETWORK]]
-        assert len(network_group) == 5
-        assert 'access_level' in network_group
-        assert 'username' in network_group
-        assert 'gui_servers' in network_group
-        assert 'reactive_login' in network_group
-        project_group = [item.name for item in groups[PROJECT]]
-        assert len(project_group) == 3
-        assert 'db_token' in project_group
-        assert 'domain' in project_group
-        assert 'device_domain' in project_group
-        dir_group = [item.name for item in groups[DIRECTORIES]]
-        assert len(dir_group) == 1
-        assert 'data_dir' in dir_group
-        bone_group = [item.name for item in groups[BACKBONE]]
-        assert len(bone_group) == 2
-        assert 'broker_topic' in bone_group
-        assert 'documentation' in bone_group
-        panel_group = [item.name for item in groups[PANEL]]
-        assert len(panel_group) == 1
-        assert 'console_visible' in panel_group
 
-        info = config.info()
-        assert PANEL in info
-        assert USER in info
+def test_set_bool_value(mocker):
+    target = 'karabogui.singletons.configuration.QSettings'
+    mocker.patch(target, new=MockSettings)
+    config = Configuration()
+    assert config["wizard"] is True
+    config["wizard"] = False
+    config['db_token'] = "observer"
+    assert config["wizard"] is False
+    assert config["db_token"] == "observer"
+
+
+def test_erase_value(mocker):
+    target = 'karabogui.singletons.configuration.QSettings'
+    mock = MockSettings()
+    mocker.patch(target, new=mock)
+    config = Configuration()
+    assert config["wizard"] is True
+    config["wizard"] = False
+    del config["wizard"]
+    assert mock.last_path == "user/wizard"
+
+
+def test_configuration_groups_info():
+    config = Configuration()
+    groups = config.groups()
+    assert len(groups) == 6
+    user_group = [item.name for item in groups[USER]]
+    assert len(user_group) == 9
+    assert 'wizard' in user_group
+    assert 'main_geometry' in user_group
+    assert 'highDPI' in user_group
+    assert 'development' in user_group
+    assert 'project_sort_column' in user_group
+    assert 'project_sort_order' in user_group
+    network_group = [item.name for item in groups[NETWORK]]
+    assert len(network_group) == 5
+    assert 'access_level' in network_group
+    assert 'username' in network_group
+    assert 'gui_servers' in network_group
+    assert 'reactive_login' in network_group
+    project_group = [item.name for item in groups[PROJECT]]
+    assert len(project_group) == 3
+    assert 'db_token' in project_group
+    assert 'domain' in project_group
+    assert 'device_domain' in project_group
+    dir_group = [item.name for item in groups[DIRECTORIES]]
+    assert len(dir_group) == 1
+    assert 'data_dir' in dir_group
+    bone_group = [item.name for item in groups[BACKBONE]]
+    assert len(bone_group) == 2
+    assert 'broker_topic' in bone_group
+    assert 'documentation' in bone_group
+    panel_group = [item.name for item in groups[PANEL]]
+    assert len(panel_group) == 1
+    assert 'console_visible' in panel_group
+
+    info = config.info()
+    assert PANEL in info
+    assert USER in info
