5deb57a978b698d2968203a75bd91c169e1e8e68
Milan Staffehl <milan.staffehl@xfel.eu>
2024-03-27 11:34:11 +0100

    refactor(GUI): transport DB connection test to pytest

--- src/pythonGui/karabogui/singletons/tests/test_db_connection.py
+++ src/pythonGui/karabogui/singletons/tests/test_db_connection.py
@@ -15,7 +15,8 @@
 # WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
 # or FITNESS FOR A PARTICULAR PURPOSE.
 import uuid
-from unittest import main, mock
+
+import pytest
 
 from karabo.common.api import KARABO_PROJECT_MANAGER
 from karabo.common.project.api import ProjectModel
@@ -24,173 +25,173 @@ from karabogui.events import KaraboEvent, broadcast_event
 from karabogui.singletons.configuration import Configuration
 from karabogui.singletons.db_connection import ProjectDatabaseConnection
 from karabogui.singletons.mediator import Mediator
-from karabogui.testing import GuiTestCase, singletons
+from karabogui.testing import singletons
+from karabogui.util import process_qt_events
 
 
-class TestDBConnection(GuiTestCase):
-    def test_connection(self):
-        mediator = Mediator()
+def test_connection(gui_app, mocker):
+    mediator = Mediator()
 
-        # Configure the default domain
-        config = Configuration()
-        domain = f"KaraboGUI-{uuid.uuid4()}"
-        config["domain"] = domain
+    # Configure the default domain
+    config = Configuration()
+    domain = f"KaraboGUI-{uuid.uuid4()}"
+    config["domain"] = domain
 
-        network = mock.Mock()
-        with singletons(network=network, configuration=config,
-                        mediator=mediator):
-            assert len(mediator._listeners.keys()) == 0
-            db_conn = ProjectDatabaseConnection()
-            # db connection registers handlers in the mediator
-            assert len(mediator._listeners.keys()) == 4
-            assert db_conn.default_domain == domain
+    network = mocker.Mock()
+    with singletons(network=network, configuration=config,
+                    mediator=mediator):
+        assert len(mediator._listeners.keys()) == 0
+        db_conn = ProjectDatabaseConnection()
+        # db connection registers handlers in the mediator
+        assert len(mediator._listeners.keys()) == 4
+        assert db_conn.default_domain == domain
 
-            # 1. Get the available domains
-            db_conn.get_available_domains()
-            network.onListProjectDomains.assert_called_once()
+        # 1. Get the available domains
+        db_conn.get_available_domains()
+        network.onListProjectDomains.assert_called_once()
 
-            # 2. Get available projects in domain
-            db_conn.get_available_project_data(domain, "project")
-            network.onProjectListItems.assert_called_with(
-                KARABO_PROJECT_MANAGER, domain, 'project')
+        # 2. Get available projects in domain
+        db_conn.get_available_project_data(domain, "project")
+        network.onProjectListItems.assert_called_with(
+            KARABO_PROJECT_MANAGER, domain, 'project')
 
-            # 3. Get projects
-            db_conn.get_projects_with_device(domain, "karaboDevice")
-            network.onProjectListProjectsWithDevice.assert_called_with(
-                KARABO_PROJECT_MANAGER, domain, "karaboDevice"
-            )
+        # 3. Get projects
+        db_conn.get_projects_with_device(domain, "karaboDevice")
+        network.onProjectListProjectsWithDevice.assert_called_with(
+            KARABO_PROJECT_MANAGER, domain, "karaboDevice"
+        )
 
-            # 4. Read data from project db
-            assert db_conn.is_reading() is False
-            assert db_conn.is_writing() is False
-            assert db_conn.is_processing() is False
+        # 4. Read data from project db
+        assert db_conn.is_reading() is False
+        assert db_conn.is_writing() is False
+        assert db_conn.is_processing() is False
 
-            project_uuid = str(uuid.uuid4())
-            model = ProjectModel(uuid=project_uuid)
-            model.initialized = True
+        project_uuid = str(uuid.uuid4())
+        model = ProjectModel(uuid=project_uuid)
+        model.initialized = True
 
-            db_conn.retrieve(domain, project_uuid, model)
-            assert db_conn.is_writing() is False
-            assert db_conn.is_reading() is True
-            assert db_conn.is_processing() is True
+        db_conn.retrieve(domain, project_uuid, model)
+        assert db_conn.is_writing() is False
+        assert db_conn.is_reading() is True
+        assert db_conn.is_processing() is True
 
-            # Network did not send yet
-            network.onProjectLoadItems.assert_not_called()
-            db_conn.flush()
-            network.onProjectLoadItems.assert_called_with(
-                KARABO_PROJECT_MANAGER,
-                [Hash("domain", domain, "uuid", project_uuid)]
-            )
+        # Network did not send yet
+        network.onProjectLoadItems.assert_not_called()
+        db_conn.flush()
+        network.onProjectLoadItems.assert_called_with(
+            KARABO_PROJECT_MANAGER,
+            [Hash("domain", domain, "uuid", project_uuid)]
+        )
 
-            data = {"success": True,
-                    "items": [Hash("domain", domain, "uuid", project_uuid,
-                                   "xml", "")]}
-            broadcast_event(KaraboEvent.ProjectItemsLoaded, data)
-            self.process_qt_events()
-            assert db_conn.is_reading() is False
-            assert db_conn.is_processing() is False
+        data = {"success": True,
+                "items": [Hash("domain", domain, "uuid", project_uuid,
+                               "xml", "")]}
+        broadcast_event(KaraboEvent.ProjectItemsLoaded, data)
+        process_qt_events(timeout=10)
+        assert db_conn.is_reading() is False
+        assert db_conn.is_processing() is False
 
-            # And the `False` success case
-            db_conn.retrieve(domain, project_uuid, model)
-            data = {"success": False,
-                    "items": [Hash("domain", domain, "uuid", project_uuid,
-                                   "xml", "")]}
-            broadcast_event(KaraboEvent.ProjectItemsLoaded, data)
-            self.process_qt_events()
-            assert db_conn.is_reading() is False
-            assert db_conn.is_processing() is False
+        # And the `False` success case
+        db_conn.retrieve(domain, project_uuid, model)
+        data = {"success": False,
+                "items": [Hash("domain", domain, "uuid", project_uuid,
+                               "xml", "")]}
+        broadcast_event(KaraboEvent.ProjectItemsLoaded, data)
+        process_qt_events(timeout=10)
+        assert db_conn.is_reading() is False
+        assert db_conn.is_processing() is False
 
-            # 5. Save data in project db
-            db_conn.store(domain, project_uuid, model)
-            assert db_conn.is_writing() is True
-            assert db_conn.is_reading() is False
-            assert db_conn.is_processing() is True
-            network.onProjectSaveItems.assert_not_called()
+        # 5. Save data in project db
+        db_conn.store(domain, project_uuid, model)
+        assert db_conn.is_writing() is True
+        assert db_conn.is_reading() is False
+        assert db_conn.is_processing() is True
+        network.onProjectSaveItems.assert_not_called()
 
-            db_conn.flush()
-            network.onProjectSaveItems.assert_called_once()
-            data = {"success": "True",
-                    "items": [Hash("domain", domain, "uuid", project_uuid,
-                                   "entry", {}, "success", True,
-                                   "xml", "")]}
+        db_conn.flush()
+        network.onProjectSaveItems.assert_called_once()
+        data = {"success": "True",
+                "items": [Hash("domain", domain, "uuid", project_uuid,
+                               "entry", {}, "success", True,
+                               "xml", "")]}
 
-            broadcast_event(KaraboEvent.ProjectItemsSaved, data)
-            self.process_qt_events()
-            assert db_conn.is_writing() is False
-            assert db_conn.is_processing() is False
+        broadcast_event(KaraboEvent.ProjectItemsSaved, data)
+        process_qt_events(timeout=10)
+        assert db_conn.is_writing() is False
+        assert db_conn.is_processing() is False
 
-            # 6 Save a failed item top level success
-            db_conn.store(domain, project_uuid, model)
-            db_conn.flush()
-            data = {"success": False,
-                    "items": [Hash("domain", domain, "uuid", project_uuid,
-                                   "entry", {}, "success", False,
-                                   "xml", "")]}
-            broadcast_event(KaraboEvent.ProjectItemsSaved, data)
-            self.process_qt_events()
-            assert db_conn.is_processing() is False
+        # 6 Save a failed item top level success
+        db_conn.store(domain, project_uuid, model)
+        db_conn.flush()
+        data = {"success": False,
+                "items": [Hash("domain", domain, "uuid", project_uuid,
+                               "entry", {}, "success", False,
+                               "xml", "")]}
+        broadcast_event(KaraboEvent.ProjectItemsSaved, data)
+        process_qt_events(timeout=10)
+        assert db_conn.is_processing() is False
 
-            # 7. Save an item, low level success failure
-            db_conn.store(domain, project_uuid, model)
-            db_conn.flush()
-            assert db_conn.is_processing() is True
-            data = {"success": True,
-                    "items": [Hash("domain", domain, "uuid", project_uuid,
-                                   "entry", {}, "success", False,
-                                   "xml", "", "reason", "Conflict")]}
-            mbox_path = "karabogui.singletons.db_connection.messagebox"
-            with mock.patch(mbox_path) as mb:
-                broadcast_event(KaraboEvent.ProjectItemsSaved, data)
-                self.process_qt_events()
-                assert db_conn.is_processing() is False
-                mb.show_error.assert_called_with("Conflict")
+        # 7. Save an item, low level success failure
+        db_conn.store(domain, project_uuid, model)
+        db_conn.flush()
+        assert db_conn.is_processing() is True
+        data = {"success": True,
+                "items": [Hash("domain", domain, "uuid", project_uuid,
+                               "entry", {}, "success", False,
+                               "xml", "", "reason", "Conflict")]}
+        mbox_path = "karabogui.singletons.db_connection.messagebox"
+        mb = mocker.patch(mbox_path)
+        broadcast_event(KaraboEvent.ProjectItemsSaved, data)
+        process_qt_events(timeout=10)
+        assert db_conn.is_processing() is False
+        mb.show_error.assert_called_with("Conflict")
 
-            # 8. Update project attribute
-            db_conn.update_attribute(domain, "project", project_uuid,
-                                     "trashed", True)
+        # 8. Update project attribute
+        db_conn.update_attribute(domain, "project", project_uuid,
+                                 "trashed", True)
 
-            network.onProjectUpdateAttribute.assert_called_with(
-                KARABO_PROJECT_MANAGER, [Hash("domain", domain,
-                                              "item_type", "project",
-                                              "uuid", project_uuid,
-                                              "attr_name", "trashed",
-                                              "attr_value", True)])
+        network.onProjectUpdateAttribute.assert_called_with(
+            KARABO_PROJECT_MANAGER, [Hash("domain", domain,
+                                          "item_type", "project",
+                                          "uuid", project_uuid,
+                                          "attr_name", "trashed",
+                                          "attr_value", True)])
 
-            # 9. Topology events, disconnect and come back with device
-            network.reset_mock()
-            assert db_conn._have_logged_in is True
+        # 9. Topology events, disconnect and come back with device
+        network.reset_mock()
+        assert db_conn._have_logged_in is True
 
-            data = {"status": False}
-            broadcast_event(KaraboEvent.NetworkConnectStatus, data)
-            self.process_qt_events()
-            assert db_conn._have_logged_in is False
+        data = {"status": False}
+        broadcast_event(KaraboEvent.NetworkConnectStatus, data)
+        process_qt_events(timeout=10)
+        assert db_conn._have_logged_in is False
 
-            data = {"status": True}
-            broadcast_event(KaraboEvent.NetworkConnectStatus, data)
-            self.process_qt_events()
-            assert db_conn._have_logged_in is False
+        data = {"status": True}
+        broadcast_event(KaraboEvent.NetworkConnectStatus, data)
+        process_qt_events(timeout=10)
+        assert db_conn._have_logged_in is False
 
-            data = {"device": KARABO_PROJECT_MANAGER}
-            broadcast_event(KaraboEvent.ProjectDBConnect, data)
-            self.process_qt_events()
-            network.onProjectBeginSession.assert_called_once()
-            assert db_conn._have_logged_in is True
+        data = {"device": KARABO_PROJECT_MANAGER}
+        broadcast_event(KaraboEvent.ProjectDBConnect, data)
+        process_qt_events(timeout=10)
+        network.onProjectBeginSession.assert_called_once()
+        assert db_conn._have_logged_in is True
 
-            # 9. Configuration
-            db_conn.default_domain = "New"
-            assert config["domain"] == "New"
+        # 9. Configuration
+        db_conn.default_domain = "New"
+        assert config["domain"] == "New"
 
-            # 10. Database scene
-            scene_uuid = str(uuid.uuid4())
-            token = db_conn.get_database_scene("scene", scene_uuid, 1)
-            network.onExecuteGeneric.assert_called_with(
-                KARABO_PROJECT_MANAGER, 'slotGetScene',
-                Hash(
-                    "name", "scene",
-                    "uuid", scene_uuid,
-                    "domain", "New",
-                    "db_token", "admin"), token=token)
+        # 10. Database scene
+        scene_uuid = str(uuid.uuid4())
+        token = db_conn.get_database_scene("scene", scene_uuid, 1)
+        network.onExecuteGeneric.assert_called_with(
+            KARABO_PROJECT_MANAGER, 'slotGetScene',
+            Hash(
+                "name", "scene",
+                "uuid", scene_uuid,
+                "domain", "New",
+                "db_token", "admin"), token=token)
 
 
 if __name__ == "__main__":
-    main()
+    pytest.main()
