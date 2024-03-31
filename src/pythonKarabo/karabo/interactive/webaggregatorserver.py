5b274b636e991f7920932cef7fd14a169beae25b
Dennis Goeries <dennis.goeries@xfel.eu>
2024-03-31 23:26:51 +0200

    MDL: DaemonManager can provide network information

--- src/pythonKarabo/karabo/interactive/webaggregatorserver.py
+++ src/pythonKarabo/karabo/interactive/webaggregatorserver.py
@@ -25,15 +25,16 @@ from tornado.httpclient import AsyncHTTPClient
 from tornado.platform.asyncio import AsyncIOMainLoop, to_asyncio_future
 
 from .startkarabo import entrypoint
-from .webserver import EMPTY_RESPONSE
+from .webserver import EMPTY_NETWORK_RESPONSE, EMPTY_RESPONSE
 
 HEARTBEAT_PENALTY = 10  # the server is polled every 5 seconds see webserver.py
 CHECK_INTERVAL = 20
 
 
 class MainHandler(web.RequestHandler):
-    def initialize(self, servers, client, last_updates):
+    def initialize(self, servers, client, last_updates, network):
         self.servers = servers
+        self.network = network
         self.client = client
         self.last_updates = last_updates
 
@@ -56,6 +57,18 @@ class MainHandler(web.RequestHandler):
         table.sort(key=lambda x: x['karabo_name'])
         server["services"] = table
 
+    async def request_network(self, hostname, port):
+        khostname, *_ = hostname.split('.')  # the hostname without domain
+        url = f"http://{hostname}:{port}"
+        api_url = f"{url}/api/network.json"
+        net = self.network.setdefault(
+            khostname, dict(hostname=hostname))
+        f = to_asyncio_future(self.client.fetch(api_url, method="GET"))
+        r = await wait_for(f, timeout=2)
+        assert r.code == 200
+        h = json.loads(r.body)
+        net["network"] = h["network"]
+
     async def post(self):
         try:
             port = self.get_argument("port")
@@ -65,23 +78,44 @@ class MainHandler(web.RequestHandler):
                 status_code=400,  # bad request
                 log_message='')
         ensure_future(self.request_servers(hostname, port))
+
+        # New web servers also provide the version
+        try:
+            self.get_argument("version")
+            ensure_future(self.request_network(hostname, port))
+        except web.MissingArgumentError:
+            khostname, *_ = hostname.split('.')
+            net = self.network.setdefault(
+                khostname, dict(hostname=hostname))
+            net["error"] = "Not supported in current version: 1.0.0"
         self.last_updates[(hostname, port)] = time.time()
         self.write("OK")
 
 
 class StatusHandler(web.RequestHandler):
-    def initialize(self, servers, client, last_updates):
+    def initialize(self, servers, client, last_updates, network):
         self.servers = servers
 
     def get(self):
-        response = EMPTY_RESPONSE
+        response = deepcopy(EMPTY_RESPONSE)
         response['servers'] = self.servers
         response['success'] = True
         self.write(response)
 
 
+class NetworkHandler(web.RequestHandler):
+    def initialize(self, servers, client, last_updates, network):
+        self.network = network
+
+    def get(self):
+        response = deepcopy(EMPTY_NETWORK_RESPONSE)
+        response['network'] = self.network
+        response['success'] = True
+        self.write(response)
+
+
 class HeartbeatChecker:
-    def __init__(self, servers, client, last_updates):
+    def __init__(self, servers, client, last_updates, network):
         self.servers = servers
         self.last_updates = last_updates
 
@@ -126,11 +160,12 @@ def run_webserver():
             AsyncIOMainLoop().install()
 
     server_dict = {'servers': servers, 'client': AsyncHTTPClient(),
-                   'last_updates': {}}
+                   'last_updates': {}, 'network': {}}
     heartbeat = HeartbeatChecker(**server_dict)
 
     app = web.Application([('/', MainHandler, server_dict),
-                           ('/status.json', StatusHandler, server_dict)],
+                           ('/status.json', StatusHandler, server_dict),
+                           ('/network.json', NetworkHandler, server_dict)],
                           template_path=os.path.join(
                               os.path.dirname(__file__), "templates"),
                           static_path=os.path.join(
