b7fbaa5289ef57d7085c4cb5e58c5bdbab961d33
Dennis Goeries <dennis.goeries@xfel.eu>
2024-04-29 11:50:34 +0200

    feat(GUI): Extension dialog can use full package index

--- src/pythonGui/karabogui/dialogs/tests/test_update_dialog.py
+++ src/pythonGui/karabogui/dialogs/tests/test_update_dialog.py
@@ -15,72 +15,45 @@
 # WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
 # or FITNESS FOR A PARTICULAR PURPOSE.
 import json
-import sys
 from os import path as op
-from unittest import mock, skip
 
 from karabogui.dialogs import update_dialog
-from karabogui.dialogs.update_dialog import UNDEFINED_VERSION
-from karabogui.testing import GuiTestCase
+from karabogui.dialogs.update_dialog import UNDEFINED_VERSION, UpdateDialog
+
+_PKG = "GUIExtensions"
 
 
-class TestCase(GuiTestCase):
+class MockReturn:
+    def __init__(self, info: dict):
+        self._info = info
 
-    def _get_loaded_modules(self):
-        extensions = []
-        for module in sys.modules:
-            if module.startswith("extensions"):
-                extensions.append(module)
-        return extensions
+    def json(self) -> dict:
+        return self._info
 
-    @skip(reason="Install is not working properly in CI")
-    def test_current_version_bug(self):
-        """Bug found when updating a package version. After the
-        package is removed pkg_resources still thinks it"s there."""
-        update_dialog.uninstall_package()
-        assert update_dialog.get_current_version() == UNDEFINED_VERSION
-        assert self._get_loaded_modules() == []
 
-        update_dialog.install_package("0.1.0")
-        assert update_dialog.get_current_version() == "0.1.0"
-        assert self._get_loaded_modules() == [
-            "extensions", "extensions.display_ipm_quadrant",
-            "extensions.models", "extensions.models.simple"]
+def update_pkg(package_name: str):
+    if package_name == _PKG:
+        return "1.0.0"
+    return UNDEFINED_VERSION
 
-        update_dialog.uninstall_package()
-        assert update_dialog.get_current_version() == UNDEFINED_VERSION
-        assert self._get_loaded_modules() == []
 
-    def test_extensions_dialog(self):
-        mock_obj = mock.patch.object
-        m1 = mock_obj(update_dialog, "get_current_version", return_value="1")
-        m2 = mock_obj(update_dialog, "get_latest_version", return_value="1")
+def test_update_dialog(gui_app, mocker):
+    """Tests the model of the extension updater"""
+    mocker.patch.object(update_dialog, "get_pkg_version",
+                        side_effect=update_pkg)
+    request = mocker.patch("requests.get")
+    request.return_value = MockReturn({})
+    dialog = UpdateDialog()
+    assert dialog.label_current.text() == "1.0.0"
+    assert dialog.label_latest.text() == UNDEFINED_VERSION
 
-        with m1, m2:
-            dialog = update_dialog.UpdateDialog()
 
-            assert not dialog.button_update.isEnabled()
-            assert dialog.label_current.text() == "1"
-            assert dialog.label_latest.text() == "1"
+def test_dialog_index_parsing(gui_app, mocker):
+    """Test the proper parsing of a json file from the index"""
+    json_file = op.join(op.dirname(__file__), "test_json.json")
+    with open(json_file) as f:
+        package = json.loads(f.read())
 
-            update_dialog.get_latest_version.return_value = "2"
-
-            self.click(dialog.button_refresh)
-
-            # Should refresh current and latest versions
-            assert dialog.label_current.text() == "1"
-            assert dialog.label_latest.text() == "2"
-
-            assert dialog.button_update.isEnabled()
-
-    def test_latest_version(self):
-        """Tests the model of the extension updater"""
-        json_file = op.join(op.dirname(__file__), "test_json.json")
-        with open(json_file) as f:
-            packages = json.loads(f.read())
-
-        with mock.patch.object(update_dialog,
-                               "_retrieve_package_list",
-                               return_value=packages):
-            latest_version = update_dialog.get_latest_version()
-            assert latest_version == "0.0.1"
+    mocker.patch("requests.get", return_value=MockReturn(package))
+    packages = update_dialog.get_index_list()
+    assert packages.get(_PKG) == "0.0.1"
