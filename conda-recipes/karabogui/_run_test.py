4d654aedd81a6cb5bd6cb1a91011e181d6a42ad9
Dennis Goeries <dennis.goeries@xfel.eu>
2024-05-08 18:30:18 +0200

    refactor(CI): Use less workers for GUI testing

--- conda-recipes/karabogui/_run_test.py
+++ conda-recipes/karabogui/_run_test.py
@@ -20,7 +20,7 @@ import pytest
 
 
 def run(module, generate_coverage=False):
-    opts = ["-v", "--disable-warnings", "-n", "2", "--pyargs",
+    opts = ["-v", "--disable-warnings", "--pyargs",
             f"--junitxml=junit.{module}.xml", module]
     if generate_coverage:
         opts.extend([f"--cov={module}", "--cov-report", "term",
