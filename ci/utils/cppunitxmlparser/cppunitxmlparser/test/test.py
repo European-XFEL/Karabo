6c9bb076d5d3081ee2eef7a4d6ef6a93ca1dd4bc
Dennis Goeries <dennis.goeries@xfel.eu>
2024-05-30 13:02:36 +0200

    refactor(CI): Lint ci scripts

--- ci/utils/cppunitxmlparser/cppunitxmlparser/test/test.py
+++ ci/utils/cppunitxmlparser/cppunitxmlparser/test/test.py
@@ -7,13 +7,14 @@ from ..merge_cppunitxml import merge_results
 from ..merge_junit import merge_junit
 
 FILES = [
-    'coreTest.xml',
-    'ioTest.xml',
-    'logTest.xml',
-    'netTest.xml',
-    'utilTest.xml',
-    'xmsTest.xml'
+    "coreTest.xml",
+    "ioTest.xml",
+    "logTest.xml",
+    "netTest.xml",
+    "utilTest.xml",
+    "xmsTest.xml",
 ]
+
 
 def test_merge():
     with tempfile.TemporaryFile() as f:
@@ -22,16 +23,14 @@ def test_merge():
         ret = merge_results(fns, f)
         f.seek(0)
         root = ET.parse(f).getroot()
-        assert root.attrib['errors'] == '1'
-        assert root.attrib['failures'] == '1'
-        assert root.attrib['tests'] == '245'
+        assert root.attrib["errors"] == "1"
+        assert root.attrib["failures"] == "1"
+        assert root.attrib["tests"] == "245"
         assert ret == 2, f"{ret}"
 
 
 def test_check():
-    files = [
-        'logTest.xml',
-        'missingFile.xml']
+    files = ["logTest.xml", "missingFile.xml"]
     with tempfile.TemporaryFile() as f:
         here = os.path.dirname(__file__)
         fns = [os.path.join(here, fn) for fn in files]
@@ -41,23 +40,21 @@ def test_check():
         os.remove(fns[1])
         f.seek(0)
         root = ET.parse(f).getroot()
-        assert root.attrib['errors'] == '1'
-        assert root.attrib['failures'] == '0'
-        assert root.attrib['tests'] == '4'
+        assert root.attrib["errors"] == "1"
+        assert root.attrib["failures"] == "0"
+        assert root.attrib["tests"] == "4"
         assert ret == 1, f"{ret}"
 
 
 def test_merge_junit():
     here = os.path.dirname(__file__)
     fns = [os.path.join(here, fn) for fn in FILES]
-    junit1 = os.path.join(here, '1testjunit.xml')
+    junit1 = os.path.join(here, "1testjunit.xml")
     sum_ = merge_results(fns, junit1)
-    files = [
-        'logTest.xml',
-        'missingFile.xml']
+    files = ["logTest.xml", "missingFile.xml"]
     fns = [os.path.join(here, fn) for fn in files]
     generate_placeholder(fns[1])
-    junit2 = os.path.join(here, '2testjunit.xml')
+    junit2 = os.path.join(here, "2testjunit.xml")
     sum_ += merge_results(fns, junit2)
     with tempfile.TemporaryFile() as f:
         total = merge_junit([junit1, junit2], f)
@@ -68,5 +65,5 @@ def test_merge_junit():
         assert total == 3
         f.seek(0)
         root = ET.parse(f).getroot()
-        assert root.attrib['errors'] == '2'
-        assert root.attrib['failures'] == '1'
+        assert root.attrib["errors"] == "2"
+        assert root.attrib["failures"] == "1"
