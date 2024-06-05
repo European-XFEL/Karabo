815f95aa42f97b8c90018577733b19c91f6b61a9
Dennis Goeries <dennis.goeries@xfel.eu>
2024-06-05 14:22:11 +0200

    feat(GUI): Enhance historic text widget with zero bit padding and align timestamp

--- src/pythonGui/karabogui/binding/api.py
+++ src/pythonGui/karabogui/binding/api.py
@@ -45,9 +45,10 @@ from .recursive import ChoiceOfNodesBinding, ListOfNodesBinding
 from .util import (
     REFERENCE_TYPENUM_TO_DTYPE, attr_fast_deepcopy, get_binding_array_value,
     get_binding_format, get_binding_value, get_dtype_format, get_editor_value,
-    get_min_max, get_min_max_size, get_native_min_max, has_min_max_attributes,
-    is_signed_vector_integer, is_unsigned_vector_integer, is_vector_floating,
-    is_vector_integer, realign_hash)
+    get_min_max, get_min_max_size, get_native_min_max, get_numpy_binding,
+    has_min_max_attributes, is_signed_vector_integer,
+    is_unsigned_vector_integer, is_vector_floating, is_vector_integer,
+    realign_hash)
 from .validate import (
     convert_string, get_default_value, validate_binding_configuration,
     validate_table_value, validate_value)
