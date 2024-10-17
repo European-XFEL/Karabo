12677c5346e1fcc16e6a2ea1e075ae324aa5ea1b
Dennis Goeries <dennis.goeries@xfel.eu>
2024-10-17 13:44:05 +0200

    feat(Bound): Provide eventLoop fixture and ServerContext

---
+++ src/pythonKarabo/karabo/bound/tests/context_test.py
@@ -0,0 +1,29 @@
+import json
+
+from karabo.bound import State
+from karabo.bound.testing import ServerContext, eventLoop, sleepUntil
+
+
+def test_server_context(eventLoop: eventLoop):
+    config = {"PropTestA": {"classId": "PropertyTest"}}
+    initA = json.dumps(config)
+    serverA = ServerContext("testServerA",
+                            ["Logger.priority=DEBUG", f"init={initA}"])
+    with serverA:
+        remote = serverA.remote()
+        sleepUntil(lambda: "PropTestA" in remote.getDevices(), timeout=10)
+        assert len(remote.getDevices()) > 0
+        assert remote.get("PropTestA", "state") == State.NORMAL
+        remote.execute("PropTestA", "startWritingOutput")
+        sleepUntil(lambda: remote.get("PropTestA", "state") == State.STARTED,
+                   timeout=5)
+        assert remote.get("PropTestA", "state") == State.STARTED
+
+        config = {"PropTestB": {"classId": "PropertyTest"}}
+        initB = json.dumps(config)
+
+        serverB = ServerContext("testServerB",
+                                ["Logger.priority=DEBUG", f"init={initB}"],
+                                client=remote)
+        with serverB:
+            sleepUntil(lambda: "PropTestB" in remote.getDevices(), timeout=10)
