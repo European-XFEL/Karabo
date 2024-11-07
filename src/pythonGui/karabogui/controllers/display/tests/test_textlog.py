d5732bf003310d9f7bbf5f7cc6bd71ed1ee59e25
Dennis Goeries <dennis.goeries@xfel.eu>
2024-11-07 17:48:56 +0100

    feat(GUI): Provide history launch option for text log widget

--- src/pythonGui/karabogui/controllers/display/tests/test_textlog.py
+++ src/pythonGui/karabogui/controllers/display/tests/test_textlog.py
@@ -17,9 +17,11 @@
 from platform import system
 
 import pytest
+from qtpy.QtWidgets import QLayout
 
 from karabo.common.scenemodel.api import DisplayTextLogModel
 from karabo.native import Configurable, String
+from karabogui.events import KaraboEvent
 from karabogui.testing import get_class_property_proxy, set_proxy_value
 
 from ..textlog import DisplayTextLog
@@ -29,9 +31,25 @@ class Object(Configurable):
     prop = String()
 
 
+def get_widgets_from_layout(layout: QLayout) -> list:
+    widgets = []
+    for i in range(layout.count()):
+        item = layout.itemAt(i)
+
+        widget = item.widget()
+        if widget is not None:
+            widgets.append(widget)
+
+        child_layout = item.layout()
+        if child_layout is not None:
+            widgets.extend(get_widgets_from_layout(child_layout))
+
+    return widgets
+
+
 @pytest.mark.skipif(system() == "Windows",
                     reason="toPlainText returns an empty string in windows")
-def test_textlog_controller(gui_app):
+def test_textlog_controller(gui_app, mocker):
     # setup
     schema = Object.getClassSchema()
     proxy = get_class_property_proxy(schema, "prop")
@@ -42,6 +60,19 @@ def test_textlog_controller(gui_app):
     controller.log_widget.clear()
     set_proxy_value(proxy, "prop", "Line 1")
     assert "Line 1" in controller.log_widget.toPlainText()
+    widgets = get_widgets_from_layout(controller.widget.layout())
+    assert widgets[0].toolTip() == ""
+    assert widgets[1].toolTip() == "Historic Text Log"
+    broadcast = mocker.patch(
+        "karabogui.controllers.display.textlog.broadcast_event")
+    widgets[1].click()
+    broadcast.assert_called_once()
+    args, kwargs = broadcast.call_args
+    event = args[0]
+    assert event is KaraboEvent.ShowUnattachedController
+    assert "model" in args[1]
+    assert args[1]["model"].keys == ["prop"]
+    assert len(widgets) == 3
     # teardown
     controller.destroy()
     assert controller.widget is None
