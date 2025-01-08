fe35a788359cabdd1487542c0df6d7b21f2a5a36
Gero Flucke <gero.flucke@xfel.eu>
2025-01-08 09:22:36 +0100

    feat(C++): Provide helper for migration to Karabo 3 (forward port to master)

--- src/karabo/tests/xms/SignalSlotable_Test.cc
+++ src/karabo/tests/xms/SignalSlotable_Test.cc
@@ -1185,8 +1185,7 @@ void SignalSlotable_Test::testAsyncReply() {
     _loopFunction(__FUNCTION__, [this] { this->_testAsyncReply(); });
 }
 
-
-void SignalSlotable_Test::_testAsyncReply() {
+namespace {
     class SignalSlotableAsyncReply : public karabo::xms::SignalSlotable {
        public:
         KARABO_CLASSINFO(SignalSlotableAsyncReply, "SignalSlotAsyncReply", "1.0")
@@ -1201,7 +1200,6 @@ void SignalSlotable_Test::_testAsyncReply() {
             KARABO_SLOT(slotAsyncReply, int);
             KARABO_SLOT(slotAsyncErrorReply);
         }
-
 
         void slotAsyncReply(int i) {
             const AsyncReply reply(this);
@@ -1241,7 +1239,9 @@ void SignalSlotable_Test::_testAsyncReply() {
        private:
         boost::asio::steady_timer m_timer;
     };
+} // namespace
 
+void SignalSlotable_Test::_testAsyncReply() {
     auto slotter = std::make_shared<SignalSlotableAsyncReply>("slotter");
     auto sender = std::make_shared<SignalSlotable>("sender");
     sender->start();
