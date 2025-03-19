077fd1c6504e9d72e8f9ca69fe2624d87dbde175
Dennis Goeries <dennis.goeries@xfel.eu>
2025-03-19 13:58:34 +0100

    fix(GUI): Fix the slider fetch in detector graph

--- src/pythonGui/karabogui/controllers/display/util.py
+++ src/pythonGui/karabogui/controllers/display/util.py
@@ -21,5 +21,5 @@ def get_ui_file(ui_name):
     """Retrieve the appropriate `.ui` file from the ui folder"""
     assert ui_name.endswith(".ui"), f"{ui_name} is not a `.ui` file"
 
-    path = Path(__file__).parent / "ui" / ui_name
-    return path
+    path = Path(__file__).parent.joinpath("ui", ui_name)
+    return str(path)
