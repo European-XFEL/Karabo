a8d1609cbb8750568f1f059354957164aeb1d52c
Dennis Goeries <dennis.goeries@xfel.eu>
2025-04-22 19:57:53 +0200

    refactor(Bound): Remove custom launcher

--- src/pythonKarabo/karabo/bound/tests/schema_adler_test.py
+++ src/pythonKarabo/karabo/bound/tests/schema_adler_test.py
@@ -13,36 +13,33 @@
 # Karabo is distributed in the hope that it will be useful, but WITHOUT ANY
 # WARRANTY; without even the implied warranty of MERCHANTABILITY or
 # FITNESS FOR A PARTICULAR PURPOSE.
-import os
-import os.path
-import subprocess
-import sys
-import unittest
+import json
 from zlib import adler32
 
+from karabo.bound import BinarySerializerSchema
+from karabo.bound.testing import ServerContext, eventLoop, sleepUntil
 
-class Tests(unittest.TestCase):
-    def setUp(self):
-        self.__starting_dir = os.curdir
-        os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-    def tearDown(self):
-        os.chdir(self.__starting_dir)
+def test_adler():
+    s = b'I love python, Hello world'
+    t = adler32(s)
+    assert t == 2073102698, "The adler32's behavior changed."
 
-    def test_adler(self):
-        s = b'I love python, Hello world'
-        t = adler32(s)
-        self.assertEqual(t, 2073102698,
-                         "The adler32's behavior changed.")
 
-    def test_schema(self):
-        process = subprocess.Popen(
-            [sys.executable, "-m", "karabo.bound.launcher",
-             "schemaOverwriteVersion",  # fixed default karabo version
-             "karabo.bound_device_test", "TestDevice"],
-            stdout=subprocess.PIPE)
-        schema = process.communicate()[0]
-        print(f"\nCross schema test: schema length is {len(schema)}")
-        self.assertEqual(adler32(schema), 1645897357,
-                         "The generated schema changed. If this is "
-                         "desired, change the checksum in the code.")
+def test_schema(eventLoop: eventLoop):
+    config = {"AdlerTestDevice": {"classId": "TestDevice",
+                                  "middlelayerDevice": "Notthere"}}
+    server = ServerContext(
+        "testAdler",
+        [f"init={json.dumps(config)}",
+         "pluginNamespace=karabo.bound_device_test"])
+    with server:
+        remote = server.remote()
+        sleepUntil(lambda: "AdlerTestDevice" in remote.getDevices(),
+                   timeout=10)
+        schema = remote.getDeviceSchema("AdlerTestDevice")
+        ser = BinarySerializerSchema.create("Bin")
+        schema = ser.save(schema)
+        text = ("The generated schema changed. If this "
+                "is desired, change the checksum in the code.")
+        assert adler32(schema) == 2708299605, text
