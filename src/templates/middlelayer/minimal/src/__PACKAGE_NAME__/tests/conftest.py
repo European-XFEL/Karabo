6c490777062851678ca844a7c3f7838b1f3948d2
Dennis Goeries <dennis.goeries@xfel.eu>
2025-05-26 12:51:08 +0200

    refactor: Align MDL device template for pytest asyncio

---
+++ src/templates/middlelayer/minimal/src/__PACKAGE_NAME__/tests/conftest.py
@@ -0,0 +1,8 @@
+# This file is intended to be used together with Karabo:
+#
+# http://www.karabo.eu
+#
+# IF YOU REQUIRE ANY LICENSING AND COPYRIGHT TERMS, PLEASE ADD THEM HERE.
+# Karabo itself is licensed under the terms of the MPL 2.0 license.
+# flake8: noqa
+from karabo.middlelayer.testing import event_loop_policy
