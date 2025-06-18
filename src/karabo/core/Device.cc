d366f35837ca8a592d50c1a4cf255935ddece7aa
Dennis Goeries <dennis.goeries@xfel.eu>
2025-06-18 18:12:07 +0200

    refactor(Bound/C++): Make deviceId and serverId formally init only parameters

--- src/karabo/core/Device.cc
+++ src/karabo/core/Device.cc
@@ -70,7 +70,9 @@ namespace karabo {
                   .key("deviceId")
                   .displayedName("DeviceID")
                   .description("The device instance ID uniquely identifies a device instance in the distributed system")
-                  .readOnly()
+                  .assignmentInternal()
+                  .noDefaultValue()
+                  .init()
                   .commit();
 
             INT32_ELEMENT(expected)
@@ -88,7 +90,9 @@ namespace karabo {
                   .displayedName("ServerID")
                   .description("The device-server on which this device is running on")
                   .expertAccess()
-                  .readOnly()
+                  .assignmentInternal()
+                  .noDefaultValue()
+                  .init()
                   .commit();
 
             STRING_ELEMENT(expected)
