79d8fe7fdeb1ef52d0ef101964c2cbf0cac0502c
Dennis Goeries <dennis.goeries@xfel.eu>
2025-06-18 11:23:20 +0200

    refactor(GUI): Cleanup change of project device state

--- src/pythonGui/karabogui/project/controller/device.py
+++ src/pythonGui/karabogui/project/controller/device.py
@@ -482,43 +482,42 @@ class DeviceInstanceController(BaseProjectController):
         if not self.model.initialized:
             return
 
-        device = self.model
-        server_model = find_parent_object(device, project_controller.model,
-                                          DeviceServerModel)
+        device_model = self.model
+        server_model = find_parent_object(
+            device_model, project_controller.model, DeviceServerModel)
         dialog = DeviceHandleDialog(server_id=server_model.server_id,
-                                    model=device,
+                                    model=device_model,
                                     is_online=self.project_device.online,
                                     parent=parent)
         move_to_cursor(dialog)
         result = dialog.exec()
         if result == QDialog.Accepted:
             # Check for existing device
-            renamed = device.instance_id != dialog.instance_id
-            if renamed and check_device_instance_exists(dialog.instance_id):
+            new_name = device_model.instance_id != dialog.instance_id
+            if new_name and check_device_instance_exists(dialog.instance_id):
                 return
 
             # We might create a new project device here when renaming!
-            device.instance_id = dialog.instance_id
+            device_model.instance_id = dialog.instance_id
 
             project_device = self.project_device
-            new_class = device.class_id != dialog.class_id
+            new_class = device_model.class_id != dialog.class_id
             # Look for existing DeviceConfigurationModel
-            config_model = device.select_config(dialog.active_uuid)
+            config_model = device_model.select_config(dialog.active_uuid)
             assert config_model is not None, "Missing configuration"
             config_model.class_id = dialog.class_id
 
             if new_class:
                 class_id = dialog.class_id
-                # Can only happen for offline project device
+                # Note: Can only happen for offline project device
                 assert not project_device.online
                 # Set device model to take care of all configurations
-                device.class_id = class_id
-                # And now the project device
-                project_device.change_class(class_id)
+                # and set new device
+                device_model.class_id = class_id
 
-            device.active_config_ref = dialog.active_uuid
+            device_model.active_config_ref = dialog.active_uuid
             # When renaming, we have to apply the config hash!
-            if renamed or new_class:
+            if new_name or new_class:
                 self._update_icon(self.ui_data)
                 self._update_instance_status(self.ui_data)
                 config_hash = config_model.configuration
