302afe41ca1e1fdad2705e1ebfcfa63e9a186a12
Dennis Goeries <dennis.goeries@xfel.eu>
2026-04-02 16:47:14 +0200

    fix(GUI): Fix scene view test segfault

--- src/pythonGui/karabogui/sceneview/tools/tests/test_actions.py
+++ src/pythonGui/karabogui/sceneview/tools/tests/test_actions.py
@@ -25,87 +25,87 @@ from ..actions import BoxHSceneAction, BoxVSceneAction
 
 
 @pytest.fixture()
-def scene_model(gui_app):
+def scene_view(gui_app):
+    # Initialize the labels for the SceneModel
     foo_label = LabelModel(x=20, y=20, text="foo")
     bar_label = LabelModel(x=10, y=10, text="bar")
     baz_label = LabelModel(x=10, y=20, text="baz")
     labels = [foo_label, bar_label, baz_label]
+
+    # Create the SceneModel
     scene_model = SceneModel(children=labels)
-    return scene_model
 
-
-@pytest.fixture()
-def scene_view(gui_app, scene_model):
+    # Create the SceneView using the SceneModel
     scene_view = SceneView(model=scene_model)
+
     return scene_view
 
 
-def test_basics(scene_view, scene_model):
-    _assert_sceneview_contents(scene_view, scene_model, changed=False)
+def test_basics(scene_view):
+    # Testing the initial state
+    _assert_sceneview_contents(scene_view, changed=False)
 
 
 @pytest.mark.skipif(IS_MAC_SYSTEM, reason="Segfault on MacOS")
-def test_horz_layout_action(scene_view, scene_model):
+def test_horz_layout_action(scene_view):
     action = BoxHSceneAction()
 
     # Not selected, so the values are not changed
     action.perform(scene_view)
-    _assert_sceneview_contents(scene_view, scene_model, changed=False)
+    _assert_sceneview_contents(scene_view, changed=False)
     _assert_layout_in_scene(scene_view, model_klass=BoxLayoutModel,
                             valid=False)
 
     # Select all label models
     scene_view.select_all()
     action.perform(scene_view)
-    _assert_sceneview_contents(scene_view, scene_model, changed=True)
-    _assert_layout_in_scene(scene_view, model_klass=BoxLayoutModel,
-                            valid=True)
+    _assert_sceneview_contents(scene_view, changed=True)
+    _assert_layout_in_scene(scene_view, model_klass=BoxLayoutModel, valid=True)
 
     # Check layout model
     layout_model = _get_layout_model(scene_view, model_klass=BoxLayoutModel)
     assert layout_model.direction == QBoxLayout.LeftToRight
 
-    # Check order. The expected is as follows: (x, y)
+    # Check order. The expected is: (x, y)
     # 1. The label bar goes first (10, 10)
     # 2. The label baz goes next (10, 20)
-    # 3, The label foo goes last (20, 20)
+    # 3. The label foo goes last (20, 20)
     labels = ["bar", "baz", "foo"]
     texts = [child.text for child in layout_model.children]
     assert labels == texts
-    scene_view.destroy()
 
 
 @pytest.mark.skipif(IS_MAC_SYSTEM, reason="Segfault on MacOS")
-def test_vert_layout_action(scene_view, scene_model):
+def test_vert_layout_action(scene_view):
     action = BoxVSceneAction()
 
     # Not selected, so the values are not changed
     action.perform(scene_view)
-    _assert_sceneview_contents(scene_view, scene_model, changed=False)
+    _assert_sceneview_contents(scene_view, changed=False)
     _assert_layout_in_scene(scene_view, model_klass=BoxLayoutModel,
                             valid=False)
 
     # Select all label models
     scene_view.select_all()
     action.perform(scene_view)
-    _assert_sceneview_contents(scene_view, scene_model, changed=True)
+    _assert_sceneview_contents(scene_view, changed=True)
     _assert_layout_in_scene(scene_view, model_klass=BoxLayoutModel, valid=True)
 
     # Check layout model
     layout_model = _get_layout_model(scene_view, model_klass=BoxLayoutModel)
     assert layout_model.direction == QBoxLayout.TopToBottom
 
-    # Check order. The expected is as follows: (x, y)
+    # Check order. The expected is: (x, y)
     # 1. The label bar goes first (10, 10)
     # 2. The label baz goes next (10, 20)
-    # 3, The label foo goes last (20, 20)
+    # 3. The label foo goes last (20, 20)
     labels = ["bar", "baz", "foo"]
     texts = [child.text for child in layout_model.children]
     assert labels == texts
-    scene_view.destroy()
 
 
-def _assert_sceneview_contents(view, model, changed=True):
+def _assert_sceneview_contents(view, changed=True):
+    model = view.scene_model
     assertion = not changed
     obj_cache = list(view._scene_obj_cache.keys())
     assert (model.children == obj_cache) == assertion
@@ -113,10 +113,7 @@ def _assert_sceneview_contents(view, model, changed=True):
 
 def _assert_layout_in_scene(view, model_klass=None, valid=True):
     layout_model = _get_layout_model(view, model_klass)
-    if valid:
-        assert layout_model is not None
-    else:
-        assert layout_model is None
+    assert layout_model is not None if valid else layout_model is None
 
 
 def _get_layout_model(view, model_klass=None):
